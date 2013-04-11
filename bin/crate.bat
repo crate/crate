eb65076b59f97a51e16c970bfa991e1b0e12dc19
Bernhard Kuzel <bernhard.kuzel@gmail.com>
2013-04-11 11:35:52 +0200

    renamed ES vars to CRATE and file names of scripts

--- bin/crate.bat
+++ bin/crate.bat
@@ -5,32 +5,32 @@ SETLOCAL
 if NOT DEFINED JAVA_HOME goto err
 
 set SCRIPT_DIR=%~dp0
-for %%I in ("%SCRIPT_DIR%..") do set ES_HOME=%%~dpfI
+for %%I in ("%SCRIPT_DIR%..") do set CRATE_HOME=%%~dpfI
 
 
 REM ***** JAVA options *****
 
-if "%ES_MIN_MEM%" == "" (
-set ES_MIN_MEM=256m
+if "%CRATE_MIN_MEM%" == "" (
+set CRATE_MIN_MEM=256m
 )
 
-if "%ES_MAX_MEM%" == "" (
-set ES_MAX_MEM=1g
+if "%CRATE_MAX_MEM%" == "" (
+set CRATE_MAX_MEM=1g
 )
 
-if NOT "%ES_HEAP_SIZE%" == "" (
-set ES_MIN_MEM=%ES_HEAP_SIZE%
-set ES_MAX_MEM=%ES_HEAP_SIZE%
+if NOT "%CRATE_HEAP_SIZE%" == "" (
+set CRATE_MIN_MEM=%CRATE_HEAP_SIZE%
+set CRATE_MAX_MEM=%CRATE_HEAP_SIZE%
 )
 
-set JAVA_OPTS=%JAVA_OPTS% -Xms%ES_MIN_MEM% -Xmx%ES_MAX_MEM%
+set JAVA_OPTS=%JAVA_OPTS% -Xms%CRATE_MIN_MEM% -Xmx%CRATE_MAX_MEM%
 
-if NOT "%ES_HEAP_NEWSIZE%" == "" (
-set JAVA_OPTS=%JAVA_OPTS% -Xmn%ES_HEAP_NEWSIZE%
+if NOT "%CRATE_HEAP_NEWSIZE%" == "" (
+set JAVA_OPTS=%JAVA_OPTS% -Xmn%CRATE_HEAP_NEWSIZE%
 )
 
-if NOT "%ES_DIRECT_SIZE%" == "" (
-set JAVA_OPTS=%JAVA_OPTS% -XX:MaxDirectMemorySize=%ES_DIRECT_SIZE%
+if NOT "%CRATE_DIRECT_SIZE%" == "" (
+set JAVA_OPTS=%JAVA_OPTS% -XX:MaxDirectMemorySize=%CRATE_DIRECT_SIZE%
 )
 
 set JAVA_OPTS=%JAVA_OPTS% -Xss256k
@@ -54,18 +54,18 @@ REM JAVA_OPTS=%JAVA_OPTS% -XX:+PrintGCTimeStamps
 REM JAVA_OPTS=%JAVA_OPTS% -XX:+PrintClassHistogram
 REM JAVA_OPTS=%JAVA_OPTS% -XX:+PrintTenuringDistribution
 REM JAVA_OPTS=%JAVA_OPTS% -XX:+PrintGCApplicationStoppedTime
-REM JAVA_OPTS=%JAVA_OPTS% -Xloggc:/var/log/elasticsearch/gc.log
+REM JAVA_OPTS=%JAVA_OPTS% -Xloggc:/var/log/crate/gc.log
 
 REM Causes the JVM to dump its heap on OutOfMemory.
 set JAVA_OPTS=%JAVA_OPTS% -XX:+HeapDumpOnOutOfMemoryError
 REM The path to the heap dump location, note directory must exists and have enough
 REM space for a full heap dump.
-REM JAVA_OPTS=%JAVA_OPTS% -XX:HeapDumpPath=$ES_HOME/logs/heapdump.hprof
+REM JAVA_OPTS=%JAVA_OPTS% -XX:HeapDumpPath=$CRATE_HOME/logs/heapdump.hprof
 
-set ES_CLASSPATH=%ES_CLASSPATH%;%ES_HOME%/lib/${project.build.finalName}.jar;%ES_HOME%/lib/*;%ES_HOME%/lib/sigar/*
-set ES_PARAMS=-Delasticsearch -Des-foreground=yes -Des.path.home="%ES_HOME%"
+set CRATE_CLASSPATH=%CRATE_CLASSPATH%;%CRATE_HOME%/lib/${project.build.finalName}.jar;%CRATE_HOME%/lib/*;%CRATE_HOME%/lib/sigar/*
+set CRATE_PARAMS=-Delasticsearch -Des-foreground=yes -Des.path.home="%CRATE_HOME%"
 
-"%JAVA_HOME%\bin\java" %JAVA_OPTS% %ES_JAVA_OPTS% %ES_PARAMS% %* -cp "%ES_CLASSPATH%" "org.elasticsearch.bootstrap.ElasticSearch"
+"%JAVA_HOME%\bin\java" %JAVA_OPTS% %CRATE_JAVA_OPTS% %CRATE_PARAMS% %* -cp "%CRATE_CLASSPATH%" "org.elasticsearch.bootstrap.ElasticSearch"
 goto finally
 
 
