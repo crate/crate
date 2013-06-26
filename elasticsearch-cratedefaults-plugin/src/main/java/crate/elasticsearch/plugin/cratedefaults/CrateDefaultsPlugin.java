345822fd6b251455acc2f61c85d7f5e6d855fd37
Bernhard Kuzel <bernhard.kuzel@gmail.com>
2013-06-26 14:15:53 +0200

    don't allow elasticsearch config files

--- elasticsearch-cratedefaults-plugin/src/main/java/crate/elasticsearch/plugin/cratedefaults/CrateDefaultsPlugin.java
+++ elasticsearch-cratedefaults-plugin/src/main/java/crate/elasticsearch/plugin/cratedefaults/CrateDefaultsPlugin.java
@@ -1,19 +1,23 @@
 package crate.elasticsearch.plugin.cratedefaults;
 
 
+import org.elasticsearch.ElasticSearchException;
 import org.elasticsearch.common.settings.ImmutableSettings;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.env.Environment;
 import org.elasticsearch.env.FailedToResolveConfigException;
 import org.elasticsearch.plugins.AbstractPlugin;
 
+import java.net.URL;
+
 /**
  * Crate Defaults Plugin sets some default crate settings:
  *
  * <ul>
- *     <li>Default cluster name "crate"</li>
- *     <li>Delete all indices is disabled</li>
- *     <li>Apply additional crate settings from "crate.yml", "crate.json" or "crate.properties" files</li>
+ * <li>Default cluster name "crate"</li>
+ * <li>Delete all indices is disabled</li>
+ * <li>Apply additional crate settings from "crate.yml", "crate.json" or "crate.properties" files</li>
+ * </ul>
  */
 public class CrateDefaultsPlugin extends AbstractPlugin {
 
@@ -25,6 +29,7 @@ public class CrateDefaultsPlugin extends AbstractPlugin {
                 .put("action.disable_delete_all_indices", true);
 
         Environment environment = new Environment(settingsBuilder.build());
+        elasticSearchCustomSettings(environment);
 
         try {
             settingsBuilder.loadFromUrl(environment.resolveConfig("crate.yml"));
@@ -55,5 +60,30 @@ public class CrateDefaultsPlugin extends AbstractPlugin {
     @Override
     public String description() {
         return "Crate defaults Plugin";
+    }
+
+    private void elasticSearchCustomSettings(Environment environment) {
+        URL url = null;
+        try {
+            url = environment.resolveConfig("elasticsearch.yml");
+        } catch (FailedToResolveConfigException e) {
+            // good
+        } catch (NoClassDefFoundError e) {
+            // good
+        }
+        try {
+            url = environment.resolveConfig("elasticsearch.json");
+        } catch (FailedToResolveConfigException e) {
+            // good
+        }
+        try {
+            url = environment.resolveConfig("elasticsearch.properties");
+        } catch (FailedToResolveConfigException e) {
+            // good
+        }
+        if (url != null) {
+            throw new ElasticSearchException("Elasticsearch configuration found at '" + url.getPath() +
+                                             "'. Only crate configuration files allowed.");
+        }
     }
 }
--- elasticsearch-cratedefaults-plugin/src/test/java/crate/elasticsearch/plugin/cratedefaults/CrateDefaultsModuleTest.java
+++ elasticsearch-cratedefaults-plugin/src/test/java/crate/elasticsearch/plugin/cratedefaults/CrateDefaultsModuleTest.java
@@ -2,11 +2,14 @@ package crate.elasticsearch.plugin.cratedefaults;
 
 import static com.github.tlrx.elasticsearch.test.EsSetup.createIndex;
 import static com.github.tlrx.elasticsearch.test.EsSetup.deleteAll;
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
 
 import com.github.tlrx.elasticsearch.test.EsSetup;
 import com.github.tlrx.elasticsearch.test.support.junit.runners.ElasticsearchRunner;
 import junit.framework.Assert;
 import junit.framework.TestCase;
+import org.elasticsearch.ElasticSearchException;
 import org.elasticsearch.ElasticSearchIllegalArgumentException;
 import org.junit.After;
 import org.junit.Before;
@@ -26,6 +29,17 @@ public class CrateDefaultsModuleTest {
     private void doSetUp() {
         esSetup = new EsSetup();
         esSetup.execute(createIndex("test"));
+    }
+
+    @Before
+    public void setUp() {
+        // remove files created in tests if existing
+        new File("crate.yml").delete();
+        new File("crate.json").delete();
+        new File("crate.properties").delete();
+        new File("elasticsearch.yml").delete();
+        new File("elasticsearch.json").delete();
+        new File("elasticsearch.properties").delete();
     }
 
     @After
@@ -48,7 +62,7 @@ public class CrateDefaultsModuleTest {
     @Test
     public void testClusterName() {
         doSetUp();
-        Assert.assertEquals("crate",
+        assertEquals("crate",
                 esSetup.client().admin().cluster().prepareHealth().
                         setWaitForGreenStatus().execute().actionGet().getClusterName());
     }
@@ -66,7 +80,7 @@ public class CrateDefaultsModuleTest {
         writer.close();
         doSetUp();
         file.delete();
-        Assert.assertEquals("myYMLCluster",
+        assertEquals("myYMLCluster",
                 esSetup.client().admin().cluster().prepareHealth().
                         setWaitForGreenStatus().execute().actionGet().getClusterName());
     }
@@ -84,7 +98,7 @@ public class CrateDefaultsModuleTest {
         writer.close();
         doSetUp();
         file.delete();
-        Assert.assertEquals("myJSONCluster",
+        assertEquals("myJSONCluster",
                 esSetup.client().admin().cluster().prepareHealth().
                         setWaitForGreenStatus().execute().actionGet().getClusterName());
     }
@@ -101,11 +115,70 @@ public class CrateDefaultsModuleTest {
         prop.store(new FileOutputStream("crate.properties"), null);
         doSetUp();
         new File("crate.properties").delete();
-        Assert.assertEquals("myPropCluster",
+        assertEquals("myPropCluster",
                 esSetup.client().admin().cluster().prepareHealth().
                         setWaitForGreenStatus().execute().actionGet().getClusterName());
     }
 
+    /**
+     * A crate.yml file can be used to override crate settings in YML format.
+     * That way it is for example possible to override the cluster name.
+     * @throws IOException
+     */
+    @Test
+    public void testElasticsearchYMLSettings() throws IOException {
+        boolean ex = false;
+        File file = new File("elasticsearch.yml");
+        FileWriter writer = new FileWriter(file, false);
+        writer.write("cluster.name: elasticsearch");
+        writer.close();
+        try {
+            doSetUp();
+        } catch (ElasticSearchException e) {
+            ex = true;
+            assertTrue(e.getDetailedMessage().endsWith(
+                    "elasticsearch.yml'. Only crate configuration files allowed."));
+        } finally {
+            file.delete();
+        }
+        assertTrue(ex);
+    }
 
+    @Test
+    public void testElasticsearchJSONSettings() throws IOException {
+        boolean ex = false;
+        File file = new File("elasticsearch.json");
+        FileWriter writer = new FileWriter(file, false);
+        writer.write("{\"cluster\": {\"name\": \"elasticsearch\"}}");
+        writer.close();
+        try {
+            doSetUp();
+        } catch (ElasticSearchException e) {
+            ex = true;
+            assertTrue(e.getDetailedMessage().endsWith(
+                    "elasticsearch.json'. Only crate configuration files allowed."));
+        } finally {
+            file.delete();
+        }
+        assertTrue(ex);
+    }
+
+    @Test
+    public void testElasticsearchPropertiesSettings() throws IOException {
+        boolean ex = false;
+        Properties prop = new Properties();
+        prop.setProperty("cluster.name", "elasticsearch");
+        prop.store(new FileOutputStream("elasticsearch.properties"), null);
+        try {
+            doSetUp();
+        } catch (ElasticSearchException e) {
+            ex = true;
+            assertTrue(e.getDetailedMessage().endsWith(
+                    "elasticsearch.properties'. Only crate configuration files allowed."));
+        } finally {
+            new File("elasticsearch.properties").delete();
+        }
+        assertTrue(ex);
+    }
 
 }
