bd5869640cb0f765c427930ac6a9c85532006f76
Bernd Rössl <bernd.roessl@gmail.com>
2013-07-04 16:08:23 +0200

    altered the Gruntfile to make sure the app opens in development environment with a proper prefix

--- elasticsearch-crate-plugin/crate-admin/Gruntfile.js
+++ elasticsearch-crate-plugin/crate-admin/Gruntfile.js
@@ -57,7 +57,8 @@ module.exports = function (grunt) {
       options: {
         port: 9000,
         // Change this to '0.0.0.0' to access the server from outside.
-        hostname: 'localhost'
+        hostname: 'localhost',
+        path: '/#/?prefix=http://localhost:9200'
       },
       livereload: {
         options: {
@@ -92,7 +93,7 @@ module.exports = function (grunt) {
     },
     open: {
       server: {
-        url: 'http://localhost:<%= connect.options.port %>'
+        url: 'http://localhost:<%= connect.options.port %><%= connect.options.path %>'
       }
     },
     clean: {
