bff6cf9715c3e4025bc5537053c9f20e1d0fe3d1
dobe <bernddorn@gmail.com>
2013-09-04 16:48:35 +0200

    crate transport client implementation

---
+++ client/src/main/java/crate/elasticsearch/client/CrateClientModule.java
@@ -0,0 +1,10 @@
+package crate.elasticsearch.client;
+
+import org.elasticsearch.common.inject.AbstractModule;
+
+public class CrateClientModule extends AbstractModule {
+    @Override
+    protected void configure() {
+        bind(InternalCrateClient.class).asEagerSingleton();
+    }
+}
