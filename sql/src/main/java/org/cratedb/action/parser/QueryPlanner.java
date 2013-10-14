9d3bbdfc07efe46d76e7e6d0521b413389307fc8
Sebastian Utz <su@rtme.net>
2013-10-14 18:17:02 +0200

    refactored 1st phase, implemented 2nd phase, TODO: add tests for 2nd phase

--- sql/src/main/java/org/cratedb/action/parser/QueryPlanner.java
+++ sql/src/main/java/org/cratedb/action/parser/QueryPlanner.java
@@ -2,9 +2,7 @@ package org.cratedb.action.parser;
 
 import org.cratedb.action.sql.ParsedStatement;
 import org.cratedb.sql.parser.StandardException;
-import org.cratedb.sql.parser.parser.BinaryRelationalOperatorNode;
-import org.cratedb.sql.parser.parser.ColumnReference;
-import org.cratedb.sql.parser.parser.ValueNode;
+import org.cratedb.sql.parser.parser.*;
 import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.common.settings.Settings;
 
@@ -13,6 +11,7 @@ import java.util.List;
 public class QueryPlanner {
 
     public static final String PRIMARY_KEY_VALUE = "primaryKeyValue";
+    public static final String ROUTING_VALUE = "routingValue";
 
     public static final String SETTINGS_OPTIMIZE_PK_QUERIES = "crate.planner.optimize_pk_queries";
 
@@ -26,6 +25,11 @@ public class QueryPlanner {
     /**
      * Check if we can optimize queries based on the SQL WHERE clause.
      * Returns true if Visitor/Generator should stop operating, otherwise false.
+     * Besides of the information to stop operating/generating, the planner write optional
+     * values to the {@link ParsedStatement}.
+     * The {@link org.cratedb.action.sql.TransportSQLAction} can then make decisions using
+     * this values if e.g. a {@link org.elasticsearch.action.get.GetRequest} should be used
+     * instead of a {@link org.elasticsearch.action.search.SearchRequest}.
      *
      * @param stmt
      * @param node
@@ -41,32 +45,40 @@ public class QueryPlanner {
 
         assert stmt.tableContext() != null;
 
-        if (checkSinglePrimaryAndRouting(stmt, node)) {
+        // First check if we found only one operational node with a primary key.
+        // If so we can set the primary key value and return true, so any generator can stop.
+        Object primaryKeyValue = extractPrimaryKeyValue(stmt, node);
+        if (primaryKeyValue != null) {
+            stmt.setPlannerResult(PRIMARY_KEY_VALUE, primaryKeyValue.toString());
             return true;
+        }
+
+        // Second check for a primary key in multi-operational nodes
+        // If so we can set the routing value but won't return true, generators should finish.
+        Object routingValue = extractRoutingValue(stmt, node);
+        if (routingValue != null) {
+            stmt.setPlannerResult(ROUTING_VALUE, routingValue.toString());
         }
 
         return false;
     }
 
     /**
-     * If a primary_key equals a constant value in the given node, and also is defined as the
-     * routing key, write the value to the {@link ParsedStatement} and return true.
-     * The {@link org.cratedb.action.sql.TransportSQLAction} can then make decisions using
-     * this values if e.g. a {@link org.elasticsearch.action.get.GetRequest} should be used
-     * instead of a {@link org.elasticsearch.action.search.SearchRequest}.
+     * If a primary key equals a constant value in the given node, and also is defined as the
+     * routing key, return this value.
      *
      * @param stmt
      * @param node
      * @return
      * @throws StandardException
      */
-    private Boolean checkSinglePrimaryAndRouting(ParsedStatement stmt, ValueNode node)
-            throws StandardException {
-        List<String> primaryKeys = stmt.tableContext().primaryKeysIncludingDefault();
-        if (node instanceof BinaryRelationalOperatorNode && ((BinaryRelationalOperatorNode)node).getOperatorType() == XContentGenerator.SQLOperatorTypes.EQUALS) {
+    private Object extractPrimaryKeyValue(ParsedStatement stmt,
+                                          ValueNode node) throws StandardException {
+        Object value = null;
+        if (node.getNodeType() == NodeTypes.BINARY_EQUALS_OPERATOR_NODE) {
+            List<String> primaryKeys = stmt.tableContext().primaryKeysIncludingDefault();
             ValueNode leftOperand = ((BinaryRelationalOperatorNode)node).getLeftOperand();
             ValueNode rightOperand = ((BinaryRelationalOperatorNode)node).getRightOperand();
-            Object value = null;
             if (leftOperand instanceof ColumnReference) {
                 if (stmt.tableContext().isRouting(leftOperand.getColumnName()) &&
                         primaryKeys.contains(leftOperand.getColumnName())) {
@@ -81,13 +93,40 @@ public class QueryPlanner {
                             rightOperand.getColumnName(), leftOperand);
                 }
             }
-            if (value != null) {
-                stmt.setPlannerResult(PRIMARY_KEY_VALUE, value.toString());
-                return true;
+        }
+
+        return value;
+    }
+
+    /**
+     * If a primary key expression is found inside one or more {@link AndNode},
+     * return the constant value so it can be used e.g. for routing.
+     *
+     * @param stmt
+     * @param node
+     * @return
+     * @throws StandardException
+     */
+    private Object extractRoutingValue(ParsedStatement stmt,
+                                       ValueNode node) throws StandardException {
+        Object value = null;
+        if (node.getNodeType() == NodeTypes.AND_NODE) {
+            AndNode andNode = (AndNode)node;
+            if (andNode.getLeftOperand().getNodeType() == NodeTypes.BINARY_EQUALS_OPERATOR_NODE) {
+                value = extractPrimaryKeyValue(stmt, andNode.getLeftOperand());
+            } else if (andNode.getLeftOperand().getNodeType() == NodeTypes.AND_NODE) {
+                value = extractRoutingValue(stmt, andNode.getLeftOperand());
+            }
+            if (value == null) {
+                if (andNode.getRightOperand().getNodeType() == NodeTypes.BINARY_EQUALS_OPERATOR_NODE) {
+                    value = extractPrimaryKeyValue(stmt, andNode.getRightOperand());
+                } else if (andNode.getRightOperand().getNodeType() == NodeTypes.AND_NODE) {
+                    value = extractRoutingValue(stmt, andNode.getRightOperand());
+                }
             }
         }
 
-        return false;
+        return value;
     }
 
 }
