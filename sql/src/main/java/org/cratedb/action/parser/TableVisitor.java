fa925bff3359c788d26af5061d0d8cd0c3685b53
Matthias Wahl <wahl@crate-technology.com>
2013-10-16 14:41:11 +0200

    tiny progress on tablevisitor

--- sql/src/main/java/org/cratedb/action/parser/TableVisitor.java
+++ sql/src/main/java/org/cratedb/action/parser/TableVisitor.java
@@ -1,29 +1,39 @@
 package org.cratedb.action.parser;
 
+import com.google.common.collect.ImmutableMap;
+import com.sun.org.apache.xalan.internal.xsltc.compiler.util.NodeType;
 import org.cratedb.action.sql.ParsedStatement;
 import org.cratedb.sql.SQLParseException;
 import org.cratedb.sql.parser.StandardException;
 import org.cratedb.sql.parser.parser.*;
+import org.cratedb.sql.parser.types.TypeId;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.common.xcontent.XContentFactory;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.google.common.collect.Maps.immutableEnumMap;
+import static com.google.common.collect.Maps.newHashMap;
 
 public class TableVisitor extends XContentVisitor {
 
     private boolean stopTraversal;
-
-    private XContentBuilder builder;
+    private XContentBuilder mappingBuilder;
+    private Map<String, Object> indexSettings = newHashMap();
+    private Map<String, Object> mappingProperties = newHashMap();
+    private Map<String, Object> mappingMeta = newHashMap();
 
     @Override
     public XContentBuilder getXContentBuilder() throws StandardException {
-        return builder;
+        return mappingBuilder;
     }
 
     public TableVisitor(ParsedStatement stmt) throws StandardException {
         super(stmt);
         try {
-            builder = XContentFactory.jsonBuilder().startObject();
+            mappingBuilder = XContentFactory.jsonBuilder().startObject();
         } catch (IOException ex) {
             throw new StandardException(ex);
         }
@@ -47,22 +57,74 @@ public class TableVisitor extends XContentVisitor {
         if (node.isWithData()) {
             throw new SQLParseException("Create Table With Data is not Supported.");
         }
+        if (node.getQueryExpression() != null) {
+            throw new SQLParseException("Create Table from Query is not Supported.");
+        }
         // TODO handle existence checks (IF NOT EXISTS, IF EXISTS ...)
         String tableName = node.getObjectName().getTableName();
+        stmt.addIndex(tableName);
+
+        // TODO: get from CreateTableNode
+        indexSettings.put("number_of_replicas", 0);
+        indexSettings.put("number_of_shards", 5);
+
         try {
-            // build
-            builder.startObject();
+            // build mapping
+
             for (TableElementNode tableElement : node.getTableElementList()) {
-                visit(tableElement);
+                switch(tableElement.getNodeType()) {
+                    case NodeTypes.COLUMN_DEFINITION_NODE:
+                        visit((ColumnDefinitionNode) tableElement);
+                        break;
+                    case NodeTypes.CONSTRAINT_DEFINITION_NODE:
+                        visit((ConstraintDefinitionNode)tableElement);
+                }
             }
-            builder.endObject();
-            // TODO: set to CreateIndexRequest
-            builder.flush();
+            mappingBuilder.startObject("mappings").startObject("default");
+            mappingBuilder.field("_meta", mappingMeta);
+            mappingBuilder.field("properties", mappingProperties);
+            mappingBuilder.endObject().endObject();
         } catch (IOException e) {
             throw new StandardException(e);
         }
         return node;
     }
+
+    public Visitable visit(ColumnDefinitionNode node) {
+
+        String columnName = node.getColumnName();
+        mappingProperties.put(columnName, newHashMap());
+        return node;
+    }
+
+
+    public Visitable visit(ConstraintDefinitionNode node) {
+        switch(node.getConstraintType()) {
+            case PRIMARY_KEY:
+                String[] pkColumnNames = node.getColumnList().getColumnNames();
+                if (pkColumnNames.length > 1) {
+                    throw new SQLParseException("Multiple Primary Key Columns not Supported.");
+                }
+                mappingMeta.put("primary_keys", pkColumnNames[0]);
+                break;
+            // TODO: handle INDEX-DEFINITION
+            default:
+                throw new SQLParseException("Unsupported Constraint");
+        }
+        return node;
+    }
+
+    String getTypeName(ColumnDefinitionNode node) {
+        TypeId typeId = node.getType().getTypeId();
+        if (typeId.isBooleanTypeId()) {
+            return "boolean";
+        } else if(typeId.isIntegerTypeId()) {
+            // TODO: LONG, SHORT, BYTE ...
+            return "integer";
+        }
+
+    }
+
 
     @Override
     public boolean visitChildrenFirst(Visitable node) {
