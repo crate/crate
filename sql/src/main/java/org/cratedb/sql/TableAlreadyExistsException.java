4a99e503f367f6a62e69134ddae881fc36a11232
Sebastian Utz <su@rtme.net>
2013-10-17 08:55:06 +0200

    fixup, changed error code, removed idea template

--- sql/src/main/java/org/cratedb/sql/TableAlreadyExistsException.java
+++ sql/src/main/java/org/cratedb/sql/TableAlreadyExistsException.java
@@ -2,13 +2,6 @@ package org.cratedb.sql;
 
 import org.elasticsearch.rest.RestStatus;
 
-/**
- * Created with IntelliJ IDEA.
- * User: max
- * Date: 10/17/13
- * Time: 8:38 AM
- * To change this template use File | Settings | File Templates.
- */
 public class TableAlreadyExistsException extends CrateException {
 
     public TableAlreadyExistsException(String msg, Throwable e) {
@@ -17,7 +10,7 @@ public class TableAlreadyExistsException extends CrateException {
 
     @Override
     public int errorCode() {
-        return 4090;
+        return 4093;
     }
 
     @Override
