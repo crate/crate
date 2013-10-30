24e86c401ec85fb11c59ce1a135c827549c3b364
Mathias Fussenegger <f.mathias@zignar.net>
2013-10-30 17:28:08 +0100

    create correct lucene query for not equals clauses

--- sql/src/test/java/org/cratedb/action/parser/LuceneQueryVisitorTest.java
+++ sql/src/test/java/org/cratedb/action/parser/LuceneQueryVisitorTest.java
@@ -155,12 +155,11 @@ public class LuceneQueryVisitorTest {
     public void testNotEqual() throws Exception {
         assertEquals(
             "BooleanQuery/0:\n" +
+            "  MUST_NOT\n" +
+            "  TermQuery: table_name:1\n" +
             "  MUST\n" +
-            "  *:*  MUST\n" +
-            "  BooleanQuery/0:\n" +
-            "    MUST_NOT\n" +
-            "    TermQuery: x:1\n",
-            queryTree("select c from information_schema.tables where x != 1")
+            "  *:*",
+            queryTree("select table_name from information_schema.tables where table_name != 1")
         );
     }
 
