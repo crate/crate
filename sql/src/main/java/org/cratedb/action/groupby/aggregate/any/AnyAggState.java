1fb63518d65af8e2d5dfd9a6e558b0f97e2d670b
Matthias Wahl <wahl@crate-technology.com>
2013-12-02 16:06:16 +0100

    concrete serialization of aggstates by using anonym implementations of abstract AggState classes

--- sql/src/main/java/org/cratedb/action/groupby/aggregate/any/AnyAggState.java
+++ sql/src/main/java/org/cratedb/action/groupby/aggregate/any/AnyAggState.java
@@ -1,23 +1,17 @@
 package org.cratedb.action.groupby.aggregate.any;
 
 import org.cratedb.action.groupby.aggregate.AggState;
-import org.elasticsearch.common.io.stream.StreamInput;
-import org.elasticsearch.common.io.stream.StreamOutput;
 
-import java.io.IOException;
+public abstract class AnyAggState<T> extends AggState<AnyAggState<T>>{
 
-public class AnyAggState<T> extends AggState<AnyAggState<T>>{
-
-    private T value = null;
+    public T value = null;
 
     @Override
     public Object value() {
         return value;
     }
 
-    public void add(T otherValue) {
-        this.value = otherValue;
-    }
+    public abstract void add(Object otherValue);
 
     @Override
     public void reduce(AnyAggState<T> other) {
@@ -31,15 +25,5 @@ public class AnyAggState<T> extends AggState<AnyAggState<T>>{
         if (o.value == null) return 1;
 
         return 0; // any two object that are not null are considered equal
-    }
-
-    @Override
-    public void readFrom(StreamInput in) throws IOException {
-        value = (T)in.readGenericValue();
-    }
-
-    @Override
-    public void writeTo(StreamOutput out) throws IOException {
-        out.writeGenericValue(value);
     }
 }
