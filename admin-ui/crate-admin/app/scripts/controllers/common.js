0252f5f337d30c6987020753f74f1b17d1dc1ff5
Sebastian Utz <su@rtme.net>
2013-12-17 20:03:08 +0100

    admin-ui: implemented general overview page

--- admin-ui/crate-admin/app/scripts/controllers/common.js
+++ admin-ui/crate-admin/app/scripts/controllers/common.js
@@ -2,31 +2,41 @@
 
 angular.module('common', ['stats'])
   .controller('StatusBarController', function ($scope, $log, $location, ClusterState) {
-   $scope.$watch( function () { return ClusterState.data; }, function (data) {
-     $scope.cluster_state = data.status;
-     $scope.cluster_name = data.name;
-     $scope.cluster_color_label = data.color_label;
-     $scope.load1 = data.load[0] == '-.-' ? data.load[0] : data.load[0].toFixed(2);
-     $scope.load5 = data.load[1] == '-.-' ? data.load[1] : data.load[1].toFixed(2);
-     $scope.load15 = data.load[2] == '-.-' ? data.load[2] : data.load[2].toFixed(2);
+    var colorMap = {green: 'label-success',
+                    yellow: 'label-warning',
+                    red: 'label-danger',
+                    '--': 'label-default'};
 
-   }, true);
+    $scope.cluster_color_label = 'label-default';
 
-   var prefix = $location.search().prefix || '';
-   $scope.docs_url = prefix + "/_plugin/docs";
+
+    $scope.$watch( function () { return ClusterState.data; }, function (data) {
+      $scope.cluster_state = data.status;
+      $scope.cluster_name = data.name;
+      $scope.cluster_color_label = colorMap[data.status];
+      $scope.load1 = data.load[0] == '-.-' ? data.load[0] : data.load[0].toFixed(2);
+      $scope.load5 = data.load[1] == '-.-' ? data.load[1] : data.load[1].toFixed(2);
+      $scope.load15 = data.load[2] == '-.-' ? data.load[2] : data.load[2].toFixed(2);
+    }, true);
+
+    var prefix = $location.search().prefix || '';
+    $scope.docs_url = prefix + "/_plugin/docs";
 
   })
   .controller('NavigationController', function ($scope, $location, ClusterState) {
+    var colorMap = {green: '',
+                    yellow: 'label-warning',
+                    red: 'label-danger',
+                    '--': ''};
 
     $scope.$watch( function () { return ClusterState.data; }, function (data) {
-     $scope.cluster_color_label_bar = data.color_label;
-     if (data.color_label == 'label-success' || data.color_label == 'label-default') {
-       $scope.cluster_color_label_bar = '';
-     }
+      $scope.cluster_color_label_bar = colorMap[data.status];
     }, true);
 
 
     $scope.isActive = function (viewLocation) {
         return viewLocation === $location.path();
     };
+
+    $scope.params = $location.search().prefix ? '?prefix='+$location.search().prefix : '';
   });
