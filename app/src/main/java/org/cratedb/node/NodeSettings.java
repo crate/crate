7a215a1c8233381c2f7906f90a19e784578d42e9
Mathias Fussenegger <f.mathias@zignar.net>
2013-12-18 22:02:39 +0100

    support crate.yml by default

--- app/src/main/java/org/cratedb/node/NodeSettings.java
+++ app/src/main/java/org/cratedb/node/NodeSettings.java
@@ -3,6 +3,7 @@ package org.cratedb.node;
 import org.elasticsearch.cluster.ClusterName;
 import org.elasticsearch.common.settings.ImmutableSettings;
 import org.elasticsearch.common.util.concurrent.EsExecutors;
+import org.elasticsearch.env.Environment;
 
 public class NodeSettings {
 
@@ -11,8 +12,17 @@ public class NodeSettings {
      *
      * @return
      */
-    public static void applyDefaultSettings(ImmutableSettings.Builder
-                                                         settingsBuilder) {
+    public static void applyDefaultSettings(ImmutableSettings.Builder settingsBuilder) {
+
+        // read also from crate.yml by default if no other config path has been set
+        // if there is also a elasticsearch.yml file this file will be read first and the settings in crate.yml
+        // will overwrite them.
+        Environment environment = new Environment(settingsBuilder.build());
+        if (System.getProperty("es.config") == null && System.getProperty("elasticsearch.config") == null) {
+            // no explicit config path set
+            settingsBuilder.loadFromUrl(environment.resolveConfig("crate.yml"));
+        }
+
         // Forbid DELETE on '/' aka. deleting all indices
         settingsBuilder.put("action.disable_delete_all_indices", true);
 
@@ -25,5 +35,4 @@ public class NodeSettings {
         int halfProcMaxAt10 = Math.min(((availableProcessors + 1) / 2), 10);
         settingsBuilder.put("threadpool.generic.size", halfProcMaxAt10);
     }
-
 }
