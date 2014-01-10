f997fa1256d2ee2bb4569da4fddb0405e0af50b7
Matthias Wahl <matthias@crate.io>
2014-01-10 18:58:06 +0100

    udc module, tests needed

--- udc/src/main/java/io/crate/udc/plugin/UDCPlugin.java
+++ udc/src/main/java/io/crate/udc/plugin/UDCPlugin.java
@@ -1,9 +1,7 @@
 package io.crate.udc.plugin;
 
-import io.crate.udc.module.UDCModule;
 import io.crate.udc.service.UDCService;
 import org.elasticsearch.common.component.LifecycleComponent;
-import org.elasticsearch.common.inject.Module;
 import org.elasticsearch.common.settings.ImmutableSettings;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.unit.TimeValue;
@@ -26,7 +24,7 @@ public class UDCPlugin extends AbstractPlugin {
     public static final TimeValue INTERVAL_DEFAULT_SETTING = new TimeValue(24, TimeUnit.HOURS);
 
     public static final String URL_SETTING_NAME = "udc.url";
-    public static final String URL_DEFAULT_SETTING = "https://udc.crate.io/ping";
+    public static final String URL_DEFAULT_SETTING = "https://udc.crate.io/";
 
     private final Settings settings;
 
@@ -54,16 +52,6 @@ public class UDCPlugin extends AbstractPlugin {
             return services;
         }
         return super.services();
-    }
-
-    @Override
-    public Collection<Class<? extends Module>> modules() {
-        Collection<Class<? extends Module>> modules = newArrayList();
-        if (!settings.getAsBoolean("node.client", false)
-                && settings.getAsBoolean(ENABLED_SETTING_NAME, ENABLED_DEFAULT_SETTING)) {
-            modules.add(UDCModule.class);
-        }
-        return modules;
     }
 
     @Override
--- udc/src/main/java/io/crate/udc/service/UDCService.java
+++ udc/src/main/java/io/crate/udc/service/UDCService.java
@@ -1,32 +1,41 @@
 package io.crate.udc.service;
 
-import io.crate.udc.ping.Pinger;
+import io.crate.udc.ping.PingTask;
 import io.crate.udc.plugin.UDCPlugin;
 import org.elasticsearch.ElasticSearchException;
+import org.elasticsearch.cluster.ClusterService;
 import org.elasticsearch.common.component.AbstractLifecycleComponent;
 import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.unit.TimeValue;
+import org.elasticsearch.http.HttpServerTransport;
 
 import java.util.Timer;
 
 public class UDCService extends AbstractLifecycleComponent<UDCService> {
 
     private Timer timer;
-    private final Pinger pinger;
+
+    private final ClusterService clusterService;
+    private final HttpServerTransport httpServerTransport;
 
     @Inject
-    public UDCService(Settings settings, Pinger pinger) {
+    public UDCService(Settings settings, ClusterService clusterService, HttpServerTransport httpServerTransport) {
         super(settings);
-        this.pinger = pinger;
+        this.clusterService = clusterService;
+        this.httpServerTransport = httpServerTransport;
     }
 
     @Override
     protected void doStart() throws ElasticSearchException {
-        timer = new Timer("crate-udc");
+        String url = settings.get(UDCPlugin.URL_SETTING_NAME, UDCPlugin.URL_DEFAULT_SETTING);
         TimeValue initialDelay = settings.getAsTime(UDCPlugin.INITIAL_DELAY_SETTING_NAME, UDCPlugin.INITIAL_DELAY_DEFAULT_SETTING);
         TimeValue interval = settings.getAsTime(UDCPlugin.INTERVAL_SETTING_NAME, UDCPlugin.INTERVAL_DEFAULT_SETTING);
-        timer.scheduleAtFixedRate(this.pinger, initialDelay.millis(), interval.millis());
+
+        PingTask pingTask = new PingTask(this.clusterService, this.httpServerTransport, url);
+
+        timer = new Timer("crate-udc");
+        timer.scheduleAtFixedRate(pingTask, initialDelay.millis(), interval.millis());
     }
 
     @Override
@@ -36,6 +45,6 @@ public class UDCService extends AbstractLifecycleComponent<UDCService> {
 
     @Override
     protected void doClose() throws ElasticSearchException {
-
+        timer.cancel(); // safety net, in case of unlikely weirdness
     }
 }
