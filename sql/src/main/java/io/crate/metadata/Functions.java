9b024474c197f9a60f74251efa8d754f28beb2e7
dobe <bernddorn@gmail.com>
2014-01-13 22:02:29 +0100

    cleanup

--- sql/src/main/java/io/crate/metadata/Functions.java
+++ sql/src/main/java/io/crate/metadata/Functions.java
@@ -1,7 +1,5 @@
 package io.crate.metadata;
 
-import org.cratedb.DataType;
-
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -9,11 +7,11 @@ public class Functions {
 
     private static Map<FunctionIdent, FunctionImplementation> functionImplemnetations = new ConcurrentHashMap<>();
 
-    public static void registerImplementation(FunctionImplementation impl){
+    public static void registerImplementation(FunctionImplementation impl) {
         functionImplemnetations.put(impl.info().ident(), impl);
     }
 
-    public static FunctionImplementation get(FunctionIdent ident){
+    public static FunctionImplementation get(FunctionIdent ident) {
         return functionImplemnetations.get(ident);
     }
 
