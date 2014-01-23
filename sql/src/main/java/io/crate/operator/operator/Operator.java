787a41c07aff13351aae7c1201c51656a9589999
Mathias Fussenegger <f.mathias@zignar.net>
2014-01-23 14:14:45 +0100

    fixup! fixup! add EvaluatingVisitor

--- sql/src/main/java/io/crate/operator/operator/Operator.java
+++ sql/src/main/java/io/crate/operator/operator/Operator.java
@@ -1,8 +1,6 @@
 package io.crate.operator.operator;
 
 import io.crate.metadata.FunctionImplementation;
-import io.crate.operator.Input;
-import io.crate.planner.symbol.BooleanLiteral;
 import io.crate.planner.symbol.Function;
 
 public interface Operator extends FunctionImplementation<Function> {
