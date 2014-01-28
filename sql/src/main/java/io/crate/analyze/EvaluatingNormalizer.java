2df45387f4cb2a31c8a7bddad6aa70b9d955df93
Mathias Fussenegger <f.mathias@zignar.net>
2014-01-28 16:47:36 +0100

    extend the function tree normalizer to also swap literal/reference symbols

--- sql/src/main/java/io/crate/analyze/EvaluatingNormalizer.java
+++ sql/src/main/java/io/crate/analyze/EvaluatingNormalizer.java
@@ -1,21 +1,47 @@
 package io.crate.analyze;
 
-import io.crate.metadata.FunctionImplementation;
-import io.crate.metadata.Functions;
-import io.crate.metadata.ReferenceResolver;
+import com.google.common.collect.ImmutableMap;
+import io.crate.metadata.*;
 import io.crate.operator.Input;
+import io.crate.operator.operator.GtOperator;
+import io.crate.operator.operator.GteOperator;
+import io.crate.operator.operator.LtOperator;
+import io.crate.operator.operator.LteOperator;
 import io.crate.planner.RowGranularity;
 import io.crate.planner.symbol.*;
 import org.elasticsearch.common.logging.ESLogger;
 import org.elasticsearch.common.logging.Loggers;
 
+import java.util.Arrays;
+import java.util.Map;
 
+
+/**
+ * the normalizer is responsible for simplifying function trees
+ *
+ * E.g.:
+ *  The query
+ *
+ *      and(true, eq(column_ref, 'someliteral'))
+ *
+ *  will be changed to
+ *
+ *      eq(column_ref, 'someliteral')
+ *
+ * in addition any yoda statements like eq(1, column_ref) will be changed to eq(column_ref, 1)
+ */
 public class EvaluatingNormalizer extends SymbolVisitor<Void, Symbol> {
 
     private final ESLogger logger = Loggers.getLogger(getClass());
     private final Functions functions;
     private final RowGranularity granularity;
     private final ReferenceResolver referenceResolver;
+    private final Map<String, String> swapTable = ImmutableMap.<String, String>builder()
+            .put(GtOperator.NAME, LtOperator.NAME)
+            .put(GteOperator.NAME, LteOperator.NAME)
+            .put(LtOperator.NAME, GtOperator.NAME)
+            .put(LteOperator.NAME, GteOperator.NAME)
+            .build();
 
     public EvaluatingNormalizer(
             Functions functions, RowGranularity granularity, ReferenceResolver referenceResolver) {
@@ -33,7 +59,32 @@ public class EvaluatingNormalizer extends SymbolVisitor<Void, Symbol> {
             copy.setArgument(i, symbol.accept(this, context));
             i++;
         }
+
+        // swap statements like  eq(2, name) to eq(name, 2)
+        if (copy.arguments().size() == 2) {
+            Symbol left = copy.arguments().get(0);
+            Symbol right = copy.arguments().get(1);
+
+            if (left.symbolType().isLiteral() && right.symbolType() == SymbolType.REFERENCE) {
+                copy = swapFunction(copy, left, right);
+            }
+        }
+
         return optimize(copy);
+    }
+
+    private Function swapFunction(Function function, Symbol left, Symbol right) {
+        String swappedName = swapTable.get(function.info().ident().name());
+        FunctionInfo newInfo;
+
+        if (swappedName == null) {
+            newInfo = function.info();
+        } else {
+            newInfo = functions.get(
+                    new FunctionIdent(swappedName, function.info().ident().argumentTypes())).info();
+        }
+
+        return new Function(newInfo, Arrays.asList(right, left));
     }
 
     @SuppressWarnings("unchecked")
