a219df191119fdc41cd41818c452c79da2c11eb9
dobe <bernddorn@gmail.com>
2014-02-18 08:17:53 +0100

    removed old comment

--- sql/src/main/java/io/crate/operator/aggregation/CollectExpression.java
+++ sql/src/main/java/io/crate/operator/aggregation/CollectExpression.java
@@ -28,7 +28,6 @@ public abstract class CollectExpression<ReturnType> implements Input<ReturnType>
     /**
      * An expression which gets evaluated in the collect phase
      */
-    //public abstract class CollectorExpression<ReturnType> implements Expression<ReturnType> {
 
     public void startCollect() {
     }
