203e52c637e21adbc07f03fcd9262e85e6e730b3
Lukas Ender <hello@lukasender.at>
2014-02-27 18:47:05 +0100

    implemented date_trunc

--- sql/src/main/java/io/crate/analyze/StatementAnalyzer.java
+++ sql/src/main/java/io/crate/analyze/StatementAnalyzer.java
@@ -113,8 +113,6 @@ abstract class StatementAnalyzer<T extends Analysis> extends DefaultTraversalVis
 
         Symbol value = process(node.getValue(), context);
 
-
-
         DataType valueDataType = symbolDataTypeVisitor.process(value, null);
         if (valueDataType == DataType.NULL){
             // dynamic or null values cannot be queried, in scalar use-cases (system tables)
