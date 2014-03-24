940f508dff1e7a7c722a83021825ae4459894e34
Mathias Fussenegger <f.mathias@zignar.net>
2014-03-24 10:50:07 +0100

    prepare release 0.33.0

--- core/src/main/java/io/crate/Version.java
+++ core/src/main/java/io/crate/Version.java
@@ -137,7 +137,11 @@ public class Version {
     public static final Version V_0_32_03 = new Version(V_0_32_03_ID, false,
             org.elasticsearch.Version.V_1_0_1);
 
-    public static final Version CURRENT = V_0_32_03;
+    public static final int V_0_33_00_ID = /*00*/330099;
+    public static final Version V_0_33_00 = new Version(V_0_33_00_ID, false,
+            org.elasticsearch.Version.V_1_0_1);
+
+    public static final Version CURRENT = V_0_33_00;
 
     static {
         assert CURRENT.esVersion == org.elasticsearch.Version.CURRENT : "Version must be " +
@@ -201,6 +205,8 @@ public class Version {
                 return V_0_32_02;
             case V_0_32_03_ID:
                 return V_0_32_03;
+            case V_0_33_00_ID:
+                return V_0_33_00;
             default:
                 return new Version(id, null, org.elasticsearch.Version.CURRENT);
         }
