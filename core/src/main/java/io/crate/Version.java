bc95a81416dbbc37c545c5cde1c5495e8debe4c0
dobe <bernddorn@gmail.com>
2014-04-08 23:18:12 +0200

    prepare release 0.35.3

--- core/src/main/java/io/crate/Version.java
+++ core/src/main/java/io/crate/Version.java
@@ -161,8 +161,12 @@ public class Version {
     public static final Version V_0_35_02 = new Version(V_0_35_02_ID, false,
             org.elasticsearch.Version.V_1_0_1);
 
+    public static final int V_0_35_03_ID = /*00*/350399;
+    public static final Version V_0_35_03 = new Version(V_0_35_03_ID, false,
+            org.elasticsearch.Version.V_1_0_1);
 
-    public static final Version CURRENT = V_0_35_02;
+
+    public static final Version CURRENT = V_0_35_03;
 
     static {
         assert CURRENT.esVersion == org.elasticsearch.Version.CURRENT : "Version must be " +
@@ -238,6 +242,8 @@ public class Version {
                 return V_0_35_01;
             case V_0_35_02_ID:
                 return V_0_35_02;
+            case V_0_35_03_ID:
+                return V_0_35_03;
             default:
                 return new Version(id, null, org.elasticsearch.Version.CURRENT);
         }
