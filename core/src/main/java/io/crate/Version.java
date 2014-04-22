3f32ee2dcb968323775ee25ea980a912053ca2fb
Lukas Ender <hello@lukasender.at>
2014-04-22 17:39:04 +0200

    prepare release 0.36.0

--- core/src/main/java/io/crate/Version.java
+++ core/src/main/java/io/crate/Version.java
@@ -34,7 +34,7 @@ public class Version {
     // the (internal) format of the id is there so we can easily do after/before checks on the id
 
     public static final int V_0_36_00_ID = /*00*/360099;
-    public static final Version V_0_36_00 = new Version(V_0_36_00_ID, true,
+    public static final Version V_0_36_00 = new Version(V_0_36_00_ID, false,
             org.elasticsearch.Version.V_1_1_0);
 
     public static final Version CURRENT = V_0_36_00;
