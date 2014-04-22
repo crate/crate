31febd0c77a2e9abe2af05fe18f18be7af5a6913
Lukas Ender <hello@lukasender.at>
2014-04-22 13:49:41 +0200

    prepare release 0.35.6

--- core/src/main/java/io/crate/Version.java
+++ core/src/main/java/io/crate/Version.java
@@ -173,7 +173,11 @@ public class Version {
     public static final Version V_0_35_05 = new Version(V_0_35_05_ID, false,
             org.elasticsearch.Version.V_1_0_1);
 
-    public static final Version CURRENT = V_0_35_05;
+    public static final int V_0_35_06_ID = /*00*/350699;
+    public static final Version V_0_35_06 = new Version(V_0_35_06_ID, false,
+            org.elasticsearch.Version.V_1_0_1);
+
+    public static final Version CURRENT = V_0_35_06;
 
     static {
         assert CURRENT.esVersion == org.elasticsearch.Version.CURRENT : "Version must be " +
