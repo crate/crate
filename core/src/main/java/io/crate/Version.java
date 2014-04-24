f2de5663c08f9fa10019bcc20e68e008a3a8f71f
Matthias Wahl <matthias@crate.io>
2014-04-24 14:32:20 +0200

    prepare release 0.36.2
    (cherry picked from commit 3b8bf5400ee4abe6870460d2eb136a16b99f07d6)
    
    the 0.36.1 version tag was already used but never merged
    into master.
    
    Conflicts:
    	CHANGES.txt
    	core/src/main/java/io/crate/Version.java

--- core/src/main/java/io/crate/Version.java
+++ core/src/main/java/io/crate/Version.java
@@ -33,11 +33,12 @@ public class Version {
     // AA values below 50 are beta builds, and below 99 are RC builds, with 99 indicating a release
     // the (internal) format of the id is there so we can easily do after/before checks on the id
 
-    public static final int V_0_36_01_ID = /*00*/360199;
-    public static final Version V_0_36_01 = new Version(V_0_36_01_ID, false,
+
+    public static final int V_0_36_02_ID = /*00*/360299;
+    public static final Version V_0_36_02 = new Version(V_0_36_02_ID, false,
             org.elasticsearch.Version.V_1_1_0);
 
-    public static final Version CURRENT = V_0_36_01;
+    public static final Version CURRENT = V_0_36_02;
 
     static {
         assert CURRENT.esVersion == org.elasticsearch.Version.CURRENT : "Version must be " +
