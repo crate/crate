1c552753428fe7daea4cc111f1b01026ecbb4717
Mathias Fussenegger <f.mathias@zignar.net>
2014-05-05 11:59:23 +0200

    prepare release 0.36.3

--- core/src/main/java/io/crate/Version.java
+++ core/src/main/java/io/crate/Version.java
@@ -34,7 +34,7 @@ public class Version {
     // the (internal) format of the id is there so we can easily do after/before checks on the id
 
     public static final int V_0_36_03_ID = /*00*/360399;
-    public static final Version V_0_36_03 = new Version(V_0_36_03_ID, true,
+    public static final Version V_0_36_03 = new Version(V_0_36_03_ID, false,
             org.elasticsearch.Version.V_1_1_1);
 
     public static final Version CURRENT = V_0_36_03;
