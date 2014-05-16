ebeee945b3d722cf1f299fa231261f3986640a0c
Christian Haudum <christian.haudum@gmail.com>
2014-05-16 13:57:56 +0200

    prepare release 0.37.2

--- core/src/main/java/io/crate/Version.java
+++ core/src/main/java/io/crate/Version.java
@@ -33,11 +33,11 @@ public class Version {
     // AA values below 50 are beta builds, and below 99 are RC builds, with 99 indicating a release
     // the (internal) format of the id is there so we can easily do after/before checks on the id
 
-    public static final int V_0_37_01_ID = /*00*/370199;
-    public static final Version V_0_37_01 = new Version(V_0_37_01_ID, false,
+    public static final int V_0_37_02_ID = /*00*/370299;
+    public static final Version V_0_37_02 = new Version(V_0_37_02_ID, false,
             org.elasticsearch.Version.V_1_1_1);
 
-    public static final Version CURRENT = V_0_37_01;
+    public static final Version CURRENT = V_0_37_02;
 
 
     static {
