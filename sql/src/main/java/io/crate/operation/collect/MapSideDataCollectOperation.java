cdc606b1f6ea9392d8229f8760c5711f78ca649b
Mathias Fussenegger <f.mathias@zignar.net>
2014-07-07 16:24:56 +0200

    keep stacktrace on IndexShardMissingException

--- sql/src/main/java/io/crate/operation/collect/MapSideDataCollectOperation.java
+++ sql/src/main/java/io/crate/operation/collect/MapSideDataCollectOperation.java
@@ -278,7 +278,7 @@ public class MapSideDataCollectOperation implements CollectOperation<Object[][]>
                 } catch (IndexShardMissingException e) {
                     throw new UnhandledServerException(
                             String.format("unknown shard id %d on index '%s'",
-                                    shardId, entry.getKey()));
+                                    shardId, entry.getKey()), e);
                 } catch (Exception e) {
                     logger.error("Error while getting collector", e);
                     throw new UnhandledServerException(e);
