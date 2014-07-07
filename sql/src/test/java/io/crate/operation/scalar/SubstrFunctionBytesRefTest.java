7c861803257f95a6472a2396686213aae73ac6fc
Lukas Ender <hello@lukasender.at>
2014-07-07 13:58:05 +0200

    use substring(BytesRef) in SubstrFunction

--- sql/src/test/java/io/crate/operation/scalar/SubstrFunctionBytesRefTest.java
+++ sql/src/test/java/io/crate/operation/scalar/SubstrFunctionBytesRefTest.java
@@ -42,67 +42,15 @@ public class SubstrFunctionBytesRefTest {
 
     static final String[] strings = getStrings(10_000, 100);
     static final BytesRef[] bytesRefs = toBytesRefs(strings);
-    static final int ITERATIONS = 200;
 
-
-    @Rule
-    public TestRule benchmarkRun = new BenchmarkRule();
-
-    public static BytesRef substring(BytesRef utf8, int begin, int end) {
-        int pos = utf8.offset;
-        final int limit = pos + utf8.length;
-        final byte[] bytes = utf8.bytes;
-        int posBegin = pos;
-
-        int codePointCount = 0;
-        for (; pos < limit; codePointCount++) {
-            if (codePointCount == begin) {
-                posBegin = pos;
-            }
-            if (codePointCount == end) {
-                break;
-            }
-
-            int v = bytes[pos] & 0xFF;
-            if (v <   /* 0xxx xxxx */ 0x80) {
-                pos += 1;
-                continue;
-            }
-            if (v >=  /* 110x xxxx */ 0xc0) {
-                if (v < /* 111x xxxx */ 0xe0) {
-                    pos += 2;
-                    continue;
-                }
-                if (v < /* 1111 xxxx */ 0xf0) {
-                    pos += 3;
-                    continue;
-                }
-                if (v < /* 1111 1xxx */ 0xf8) {
-                    pos += 4;
-                    continue;
-                }
-                // fallthrough, consider 5 and 6 byte sequences invalid.
-            }
-
-            // Anything not covered above is invalid UTF8.
-            throw new IllegalArgumentException();
-        }
-
-        // Check if we didn't go over the limit on the last character.
-        if (pos > limit) throw new IllegalArgumentException();
-        utf8.offset = posBegin;
-        utf8.length = pos - posBegin;
-        return utf8;
-    }
-
+    // helpers
 
     public static String getTestString(int length) {
-
 
         StringBuilder buffer = new StringBuilder();
         for (int i = 0; i < length; i++) {
             char c = (char) (RANDOM.nextInt(Character.MAX_VALUE));
-            while (!utf8Encoder.canEncode(c)){
+            while (!utf8Encoder.canEncode(c)) {
                 c = (char) (RANDOM.nextInt(Character.MAX_VALUE));
             }
             buffer.append(c);
@@ -118,16 +66,6 @@ public class SubstrFunctionBytesRefTest {
         return res;
     }
 
-    @BenchmarkOptions(benchmarkRounds = 1, warmupRounds = 0)
-    @Test
-    public void testPos() throws Exception {
-        for (int i = 0; i < strings.length; i++) {
-            assertEquals(strings[i], bytesRefs[i].utf8ToString());
-            assertEquals(strings[i].substring(10, 80), substring(bytesRefs[i].clone(), 10, 80).utf8ToString());
-        }
-    }
-
-
     private static BytesRef[] toBytesRefs(String[] strings) {
         BytesRef[] res = new BytesRef[strings.length];
         for (int i = 0; i < res.length; i++) {
@@ -136,43 +74,19 @@ public class SubstrFunctionBytesRefTest {
         return res;
     }
 
-    @BenchmarkOptions(benchmarkRounds = 2, warmupRounds = 1)
+    // end of helpers
+
+    @Rule
+    public TestRule benchmarkRun = new BenchmarkRule();
+
+    @BenchmarkOptions(benchmarkRounds = 1, warmupRounds = 0)
     @Test
-    public void testStringPerf() throws Exception {
-        String s = "";
-        for (int n = 0; n < ITERATIONS; n++) {
-            for (int i = 0; i < strings.length; i++) {
-                s = strings[i].substring(4, 26);
-            }
+    public void testPos() throws Exception {
+        for (int i = 0; i < strings.length; i++) {
+            assertEquals(strings[i], bytesRefs[i].utf8ToString());
+            assertEquals(strings[i].substring(10, 80), SubstrFunction.substring(bytesRefs[i], 10, 80).utf8ToString());
         }
-        System.out.println("string: " + s);
     }
 
-
-    @BenchmarkOptions(benchmarkRounds = 2, warmupRounds = 1)
-    @Test
-    public void testBytesRefPerf() throws Exception {
-
-        BytesRef b = new BytesRef();
-        for (int n = 0; n < ITERATIONS; n++) {
-            for (int i = 0; i < strings.length; i++) {
-                b = substring(bytesRefs[i].clone(), 4, 26);
-            }
-        }
-        System.out.println("ref: " + b.utf8ToString());
-    }
-
-    @BenchmarkOptions(benchmarkRounds = 2, warmupRounds = 1)
-    @Test
-    public void testBytesRefToStringPerf() throws Exception {
-
-        BytesRef b = new BytesRef();
-        for (int n = 0; n < ITERATIONS; n++) {
-            for (int i = 0; i < strings.length; i++) {
-                b = new BytesRef(bytesRefs[i].utf8ToString().substring(4, 26));
-            }
-        }
-        System.out.println("ref: " + b.utf8ToString());
-    }
 }
 
