02d6476be989ac7ccd4461110f32e8ed7e11d267
Mathias Fussenegger <f.mathias@zignar.net>
2014-07-11 16:25:50 +0200

    add the ceil scalar function

---
+++ sql/src/test/java/io/crate/operation/scalar/arithmetic/CeilFunctionTest.java
@@ -0,0 +1,105 @@
+/*
+ * Licensed to CRATE Technology GmbH ("Crate") under one or more contributor
+ * license agreements.  See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.  Crate licenses
+ * this file to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.  You may
+ * obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * However, if you have executed another commercial license agreement
+ * with Crate these terms will supersede the license and you may use the
+ * software solely pursuant to the terms of the relevant commercial agreement.
+ */
+
+package io.crate.operation.scalar.arithmetic;
+
+import io.crate.metadata.FunctionIdent;
+import io.crate.metadata.Functions;
+import io.crate.operation.Input;
+import io.crate.operation.scalar.ScalarFunctionModule;
+import io.crate.planner.symbol.Function;
+import io.crate.planner.symbol.Literal;
+import io.crate.planner.symbol.Reference;
+import io.crate.planner.symbol.Symbol;
+import io.crate.testing.TestingHelpers;
+import io.crate.types.DataType;
+import io.crate.types.DataTypes;
+import org.elasticsearch.common.inject.ModulesBuilder;
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.Assert.assertThat;
+
+public class CeilFunctionTest {
+
+    private Functions functions;
+
+    @Before
+    public void setUp() throws Exception {
+        functions = new ModulesBuilder().add(new ScalarFunctionModule())
+                .createInjector().getInstance(Functions.class);
+    }
+
+    private CeilFunction getFunction(DataType type) {
+        return (CeilFunction) functions.get(new FunctionIdent(CeilFunction.NAME, Arrays.asList(type)));
+    }
+
+    private Number evaluate(Number number, DataType type) {
+        return getFunction(type).evaluate((Input) Literal.newLiteral(type, number));
+    }
+
+    private Symbol normalize(Number number, DataType type) {
+        CeilFunction function = getFunction(type);
+        return function.normalizeSymbol(new Function(function.info(),
+                Arrays.<Symbol>asList(Literal.newLiteral(type, number))));
+    }
+
+    @Test
+    public void testEvaluateOnDouble() throws Exception {
+        assertThat((Long) evaluate(29.9, DataTypes.DOUBLE), is(30L));
+        assertThat(evaluate(null, DataTypes.DOUBLE), nullValue());
+    }
+
+    @Test
+    public void testEvaluateOnFloat() throws Exception {
+        assertThat((Integer) evaluate(29.9f, DataTypes.FLOAT), is(30));
+        assertThat(evaluate(null, DataTypes.FLOAT), nullValue());
+    }
+
+    @Test
+    public void testEvaluateOnIntAndLong() throws Exception {
+        assertThat((Integer) evaluate(20, DataTypes.INTEGER), is(20));
+        assertThat(evaluate(null, DataTypes.INTEGER), nullValue());
+        assertThat((Long) evaluate(20L, DataTypes.LONG), is(20L));
+        assertThat(evaluate(null, DataTypes.LONG), nullValue());
+    }
+
+    @Test
+    public void testNormalizeValueSymbol() throws Exception {
+        TestingHelpers.assertLiteralSymbol(normalize(42.9, DataTypes.DOUBLE), 43L);
+        TestingHelpers.assertLiteralSymbol(normalize(42.9f, DataTypes.FLOAT), 43);
+        TestingHelpers.assertLiteralSymbol(normalize(null, DataTypes.FLOAT), null, DataTypes.INTEGER);
+    }
+
+    @Test
+    public void testNormalizeReference() throws Exception {
+        Reference height = TestingHelpers.createReference("height", DataTypes.DOUBLE);
+        CeilFunction round = getFunction(DataTypes.DOUBLE);
+        Function function = new Function(round.info(), Arrays.<Symbol>asList(height));
+        Function normalized = (Function) round.normalizeSymbol(function);
+        assertThat(normalized, Matchers.sameInstance(function));
+    }
+}
