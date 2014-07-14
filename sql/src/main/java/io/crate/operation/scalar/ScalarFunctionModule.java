434267ab4e2287a6a86621f26be5abd7b9d3d0df
Philipp Bogensberger <philipp@crate.io>
2014-07-14 15:00:02 +0200

    added square root scalar function

--- sql/src/main/java/io/crate/operation/scalar/ScalarFunctionModule.java
+++ sql/src/main/java/io/crate/operation/scalar/ScalarFunctionModule.java
@@ -72,5 +72,6 @@ public class ScalarFunctionModule extends AbstractModule {
         RandomFunction.register(this);
         FloorFunction.register(this);
         AbsFunction.register(this);
+        SquareRootFunction.register(this);
     }
 }
