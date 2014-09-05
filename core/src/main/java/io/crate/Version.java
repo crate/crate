b1bf47b1146ef7cb875980c5544b82f5fa534dee
Christian Haudum <christian.haudum@gmail.com>
2014-09-05 15:41:27 +0200

    prepare release 0.42.1

--- core/src/main/java/io/crate/Version.java
+++ core/src/main/java/io/crate/Version.java
@@ -35,7 +35,7 @@ public class Version {
 
 
     public static final boolean SNAPSHOT = false;
-    public static final Version CURRENT = new Version(420099, SNAPSHOT, org.elasticsearch.Version.V_1_3_2);
+    public static final Version CURRENT = new Version(420199, SNAPSHOT, org.elasticsearch.Version.V_1_3_2);
 
     static {
         assert CURRENT.esVersion == org.elasticsearch.Version.CURRENT : "Version must be " +
