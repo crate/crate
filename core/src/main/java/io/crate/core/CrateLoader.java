crate/crate

commit 8bf829da714f067e9827cb64a04a0b50abed8503

load crate modules using ServiceLoader instead of es plugin mechanism
also add fatjar plugin to be able to create a single crate
jar.

The es-plugin mechanism doesn't work with a single jar file
as there can only be one es-plugin.properties file and that
can only have one "plugin" key.

In order to work around that the CrateCorePlugin is now the
only plugin loaded using the es-plugin mechanism and that
plugin is loading all other crate components (using the stock
java ServiceLoader which works using META-INF/services files
which can be merged in a single jar file)

core/src/main/java/io/crate/core/CrateLoader.java
@@ -1,0 +1,156 @@
+/*
+ * Licensed to CRATE Technology GmbH ("Crate") under one or more contributor
+ * license agreements.  See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.  Crate licenses
+ * this file to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.  You may
+ * obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * However, if you have executed another commercial license agreement
+ * with Crate these terms will supersede the license and you may use the
+ * software solely pursuant to the terms of the relevant commercial agreement.
+ */
+
+package io.crate.core;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Lists;
+import io.crate.CrateComponent;
+import org.elasticsearch.common.collect.MapBuilder;
+import org.elasticsearch.common.inject.Module;
+import org.elasticsearch.common.logging.ESLogger;
+import org.elasticsearch.common.logging.Loggers;
+import org.elasticsearch.common.settings.ImmutableSettings;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.plugins.Plugin;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.ServiceLoader;
+
+public class CrateLoader {
+
+    private static CrateLoader INSTANCE;
+
+    private final ArrayList<Plugin> plugins;
+    private final ESLogger logger = Loggers.getLogger(getClass());
+    private final ImmutableMap<Plugin, List<OnModuleReference>> onModuleReferences;
+
+    public static synchronized CrateLoader getInstance(Settings settings) {
+        if (INSTANCE == null) {
+            INSTANCE = new CrateLoader(settings);
+        }
+        return INSTANCE;
+    }
+
+    private CrateLoader(Settings settings) {
+        ServiceLoader<CrateComponent> crateComponents = ServiceLoader.load(CrateComponent.class);
+        plugins = new ArrayList<>();
+        MapBuilder<Plugin, List<OnModuleReference>> onModuleReferences = MapBuilder.newMapBuilder();
+        for (CrateComponent crateComponent : crateComponents) {
+            Plugin plugin = crateComponent.createPlugin(settings);
+            plugins.add(plugin);
+            List<OnModuleReference> list = Lists.newArrayList();
+            for (Method method : plugin.getClass().getDeclaredMethods()) {
+                if (!method.getName().equals("onModule")) {
+                    continue;
+                }
+                if (method.getParameterTypes().length == 0 || method.getParameterTypes().length > 1) {
+                    logger.warn("Plugin: {} implementing onModule with no parameters or more than one parameter", plugin.name());
+                    continue;
+                }
+                Class moduleClass = method.getParameterTypes()[0];
+                if (!Module.class.isAssignableFrom(moduleClass)) {
+                    logger.warn("Plugin: {} implementing onModule by the type is not of Module type {}", plugin.name(), moduleClass);
+                    continue;
+                }
+                method.setAccessible(true);
+                //noinspection unchecked
+                list.add(new OnModuleReference(moduleClass, method));
+            }
+            if (!list.isEmpty()) {
+                onModuleReferences.put(plugin, list);
+            }
+        }
+        this.onModuleReferences = onModuleReferences.immutableMap();
+    }
+
+    public Collection<Class<? extends Module>> modules() {
+        List<Class<? extends Module>> modules = Lists.newArrayList();
+        for (Plugin plugin : plugins) {
+            modules.addAll(plugin.modules());
+        }
+        return modules;
+    }
+
+    public Collection<Module> modules(Settings settings) {
+        List<Module> modules = Lists.newArrayList();
+        for (Plugin plugin : plugins) {
+            modules.addAll(plugin.modules(settings));
+        }
+        return modules;
+    }
+
+    public Collection<Class<? extends Module>> shardModules() {
+        List<Class<? extends Module>> modules = Lists.newArrayList();
+        for (Plugin plugin : plugins) {
+            modules.addAll(plugin.shardModules());
+        }
+        return modules;
+    }
+
+    public Collection<Module> shardModules(Settings settings) {
+        List<Module> modules = Lists.newArrayList();
+        for (Plugin plugin : plugins) {
+            modules.addAll(plugin.shardModules(settings));
+        }
+        return modules;
+    }
+
+    public void processModule(Module module) {
+        for (Plugin plugin : plugins) {
+            plugin.processModule(module);
+            // see if there are onModule references
+            List<OnModuleReference> references = onModuleReferences.get(plugin);
+            if (references != null) {
+                for (OnModuleReference reference : references) {
+                    if (reference.moduleClass.isAssignableFrom(module.getClass())) {
+                        try {
+                            reference.onModuleMethod.invoke(plugin, module);
+                        } catch (Exception e) {
+                            logger.warn("plugin {}, failed to invoke custom onModule method", e, plugin.name());
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public Settings additionalSettings() {
+        ImmutableSettings.Builder builder = ImmutableSettings.settingsBuilder();
+        for (Plugin plugin : plugins) {
+            builder.put(plugin.additionalSettings());
+        }
+        return builder.build();
+    }
+
+    static class OnModuleReference {
+        public final Class<? extends Module> moduleClass;
+        public final Method onModuleMethod;
+
+        OnModuleReference(Class<? extends Module> moduleClass, Method onModuleMethod) {
+            this.moduleClass = moduleClass;
+            this.onModuleMethod = onModuleMethod;
+        }
+    }
+}

core/src/main/java/io/crate/plugin/CrateCorePlugin.java
@@ -21,12 +21,14 @@
 
 package io.crate.plugin;
 
+import io.crate.core.CrateLoader;
 import io.crate.module.CrateCoreModule;
+import io.crate.module.CrateCoreShardModule;
+import io.crate.rest.CrateRestMainAction;
 import org.elasticsearch.common.inject.Module;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.plugins.AbstractPlugin;
 import org.elasticsearch.rest.RestModule;
-import io.crate.rest.CrateRestMainAction;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -34,9 +36,11 @@ import java.util.Collection;
 public class CrateCorePlugin extends AbstractPlugin {
 
     private final Settings settings;
+    private final CrateLoader crateLoader;
 
     public CrateCorePlugin(Settings settings) {
         this.settings = settings;
+        this.crateLoader = CrateLoader.getInstance(settings);
     }
 
     @Override
@@ -50,10 +54,24 @@ public class CrateCorePlugin extends AbstractPlugin {
     }
 
     @Override
+    public Settings additionalSettings() {
+        return crateLoader.additionalSettings();
+    }
+
+    @Override
     public Collection<Class<? extends Module>> modules() {
         Collection<Class<? extends Module>> modules = new ArrayList<>();
         if (!settings.getAsBoolean("node.client", false)) {
             modules.add(CrateCoreModule.class);
+        }
+        return modules;
+    }
+
+    @Override
+    public Collection<Class<? extends Module>> shardModules() {
+        Collection<Class<? extends Module>> modules = new ArrayList<>();
+        if (!settings.getAsBoolean("node.client", false)) {
+            modules.add(CrateCoreShardModule.class);
         }
         return modules;
     }

admin-ui/build.gradle
@@ -9,4 +9,5 @@ configurations {
 
 dependencies {
     compile project(':es')
+    compile project(':core')
 }

admin-ui/src/main/java/io/crate/plugin/AdminUICrateComponent.java
@@ -1,0 +1,34 @@
+/*
+ * Licensed to CRATE Technology GmbH ("Crate") under one or more contributor
+ * license agreements.  See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.  Crate licenses
+ * this file to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.  You may
+ * obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * However, if you have executed another commercial license agreement
+ * with Crate these terms will supersede the license and you may use the
+ * software solely pursuant to the terms of the relevant commercial agreement.
+ */
+
+package io.crate.plugin;
+
+import io.crate.CrateComponent;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.plugins.Plugin;
+
+public class AdminUICrateComponent implements CrateComponent {
+
+    @Override
+    public Plugin createPlugin(Settings settings) {
+        return new AdminUIPlugin(settings);
+    }
+}

admin-ui/src/main/resources/META-INF/services/io.crate.CrateComponent
@@ -1,0 +1,1 @@
+io.crate.plugin.AdminUICrateComponent

admin-ui/src/main/resources/es-plugin.properties
@@ -1,25 +1,0 @@
-#
-# Licensed to CRATE Technology GmbH ("Crate") under one
-# or more contributor license agreements.  See the NOTICE file
-# distributed with this work for additional information
-# regarding copyright ownership.  Crate licenses this file
-# to you under the Apache License, Version 2.0 (the
-# "License"); you may not use this file except in compliance
-# with the License.  You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an
-# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied.  See the License for the
-# specific language governing permissions and limitations
-# under the License.
-#
-# However, if you have executed another commercial license
-# agreement with Crate these terms will supersede the license
-# and you may use the software solely pursuant to the terms of
-# the relevant commercial agreement.
-#
-
-plugin=io.crate.plugin.AdminUIPlugin

app/build.gradle
@@ -1,11 +1,24 @@
+buildscript {
+    repositories {
+        jcenter()
+    }
+
+    dependencies {
+        classpath 'eu.appsatori:gradle-fatjar-plugin:0.3'
+    }
+}
+
 apply plugin:'distribution'
 apply plugin: 'java'
+apply plugin: 'eu.appsatori.fatjar'
 
 archivesBaseName = 'crate-app'
 
 configurations {
     all*.exclude group: 'org.elasticsearch'
 }
+
+
 
 task release(dependsOn: 'distTar') << {
     logger.info("released version: " + getVersion.version);
@@ -15,7 +28,8 @@ task nightly(dependsOn: 'distTar') << {
     logger.info("nightly version: " + getVersion.version);
 }
 
-task getVersion ( dependsOn: 'classes') << {
+
+task getVersion (dependsOn: 'classes') << {
     def stdout = new ByteArrayOutputStream()
     javaexec {
         classpath = sourceSets.main.runtimeClasspath
@@ -38,8 +52,20 @@ task getVersion ( dependsOn: 'classes') << {
 
     logger.quiet("gitTag: " + gitTag)
     logger.quiet("version: " + version)
+}
 
-
+fatJar {
+    dependsOn getVersion
+    doFirst {
+        manifest.attributes 'Implementation-Version': getVersion.version
+    }
+    manifest {
+        attributes 'Main-Class': 'io.crate.bootstrap.CrateF',
+        'Implementation-Title': 'Crate',
+        'Built-Date': new Date(),
+        'Built-JDK': System.getProperty('java.version'),
+        'Built-Gradle': gradle.gradleVersion
+    }
 }
 
 task dist {

blob/src/main/java/io/crate/plugin/BlobCrateComponent.java
@@ -1,0 +1,33 @@
+/*
+ * Licensed to CRATE Technology GmbH ("Crate") under one or more contributor
+ * license agreements.  See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.  Crate licenses
+ * this file to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.  You may
+ * obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * However, if you have executed another commercial license agreement
+ * with Crate these terms will supersede the license and you may use the
+ * software solely pursuant to the terms of the relevant commercial agreement.
+ */
+
+package io.crate.plugin;
+
+import io.crate.CrateComponent;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.plugins.Plugin;
+
+public class BlobCrateComponent implements CrateComponent {
+    @Override
+    public Plugin createPlugin(Settings settings) {
+        return new BlobPlugin(settings);
+    }
+}

commit c56a05fa6354290b9521f85ca47e5093c0c3a199

correctly load services and indexModules in CrateLoader
(8bf829da714f067e9827cb64a04a0b50abed8503 broke blob support
as it didn't load the BlobService anymore)

core/src/main/java/io/crate/core/CrateLoader.java
@@ -25,6 +25,7 @@ import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Lists;
 import io.crate.CrateComponent;
 import org.elasticsearch.common.collect.MapBuilder;
+import org.elasticsearch.common.component.LifecycleComponent;
 import org.elasticsearch.common.inject.Module;
 import org.elasticsearch.common.logging.ESLogger;
 import org.elasticsearch.common.logging.Loggers;
@@ -89,6 +90,22 @@ public class CrateLoader {
         List<Class<? extends Module>> modules = Lists.newArrayList();
         for (Plugin plugin : plugins) {
             modules.addAll(plugin.modules());
+        }
+        return modules;
+    }
+
+    public Collection<Class<? extends LifecycleComponent>> services() {
+        List<Class<? extends LifecycleComponent>> services = Lists.newArrayList();
+        for (Plugin plugin : plugins) {
+            services.addAll(plugin.services());
+        }
+        return services;
+    }
+
+    public Collection<Class<? extends Module>> indexModules() {
+        Collection<Class<? extends Module>> modules = new ArrayList<>();
+        for (Plugin plugin : plugins) {
+            modules.addAll(plugin.indexModules());
         }
         return modules;
     }

core/src/main/java/io/crate/plugin/CrateCorePlugin.java
@@ -25,6 +25,7 @@ import io.crate.core.CrateLoader;
 import io.crate.module.CrateCoreModule;
 import io.crate.module.CrateCoreShardModule;
 import io.crate.rest.CrateRestMainAction;
+import org.elasticsearch.common.component.LifecycleComponent;
 import org.elasticsearch.common.inject.Module;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.plugins.AbstractPlugin;
@@ -56,6 +57,16 @@ public class CrateCorePlugin extends AbstractPlugin {
     @Override
     public Settings additionalSettings() {
         return crateLoader.additionalSettings();
+    }
+
+    @Override
+    public Collection<Class<? extends LifecycleComponent>> services() {
+        return crateLoader.services();
+    }
+
+    @Override
+    public Collection<Class<? extends Module>> indexModules() {
+        return crateLoader.indexModules();
     }
 
     @Override