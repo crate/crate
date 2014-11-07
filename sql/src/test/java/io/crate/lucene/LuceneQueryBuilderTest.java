crate/crate

commit 8ead30b6816e96b50f3040ee65908b12ea577081

always use source lookup in genericFunctionQuery
-> where substr(analyzed_text, 1,1) = 'x'

threw GroupByOnArrayUnsupported exception, now it works
correctly using source lookup

sql/src/main/java/io/crate/lucene/LuceneQueryBuilder.java
@@ -21,6 +21,8 @@
 
 package io.crate.lucene;
 
+import com.google.common.base.Predicates;
+import com.google.common.base.Throwables;
 import com.google.common.collect.ImmutableMap;
 import com.spatial4j.core.context.jts.JtsSpatialContext;
 import com.spatial4j.core.shape.Rectangle;
@@ -30,9 +32,11 @@ import com.vividsolutions.jts.geom.Geometry;
 import io.crate.analyze.WhereClause;
 import io.crate.lucene.match.MatchQueryBuilder;
 import io.crate.lucene.match.MultiMatchQueryBuilder;
+import io.crate.metadata.DocReferenceConverter;
 import io.crate.metadata.Functions;
 import io.crate.operation.Input;
 import io.crate.operation.collect.CollectInputSymbolVisitor;
+import io.crate.operation.collect.LuceneDocCollector;
 import io.crate.operation.operator.*;
 import io.crate.operation.operator.any.*;
 import io.crate.operation.predicate.IsNullPredicate;
@@ -45,6 +49,7 @@ import io.crate.operation.scalar.geo.DistanceFunction;
 import io.crate.operation.scalar.geo.WithinFunction;
 import io.crate.planner.symbol.*;
 import io.crate.types.DataTypes;
+import org.apache.lucene.index.AtomicReader;
 import org.apache.lucene.index.AtomicReaderContext;
 import org.apache.lucene.index.Term;
 import org.apache.lucene.search.*;
@@ -736,6 +741,11 @@ public class LuceneQueryBuilder {
             if (function.valueType() != DataTypes.BOOLEAN) {
                 raiseUnsupported(function);
             }
+            // avoid field-cache
+            // reason1: analyzed columns or columns with index off wouldn't work
+            //   substr(n, 1, 1) in the case of n => analyzed would throw an error because n would be an array
+            // reason2: would have to load each value into the field cache
+            DocReferenceConverter.convertIf(function, Predicates.<Reference>alwaysTrue());
 
             final CollectInputSymbolVisitor.Context ctx = inputSymbolVisitor.process(function);
             assert ctx.topLevelInputs().size() == 1;
@@ -743,8 +753,10 @@ public class LuceneQueryBuilder {
             final Input<Boolean> condition = (Input<Boolean>) ctx.topLevelInputs().get(0);
             @SuppressWarnings("unchecked")
             final List<LuceneCollectorExpression> expressions = ctx.docLevelExpressions();
-            CollectorContext collectorContext = new CollectorContext();
+            final CollectorContext collectorContext = new CollectorContext();
             collectorContext.searchContext(searchContext);
+            collectorContext.visitor(new LuceneDocCollector.CollectorFieldsVisitor(expressions.size()));
+
             for (LuceneCollectorExpression expression : expressions) {
                 expression.startCollect(collectorContext);
             }
@@ -756,6 +768,8 @@ public class LuceneQueryBuilder {
                     }
                     return BitsFilteredDocIdSet.wrap(
                             new FunctionDocSet(
+                                    context.reader(),
+                                    collectorContext.visitor(),
                                     condition,
                                     expressions,
                                     context.reader().maxDoc(),
@@ -770,20 +784,37 @@ public class LuceneQueryBuilder {
 
         static class FunctionDocSet extends MatchDocIdSet {
 
+            private final AtomicReader reader;
+            private final LuceneDocCollector.CollectorFieldsVisitor fieldsVisitor;
             private final Input<Boolean> condition;
             private final List<LuceneCollectorExpression> expressions;
+            private final boolean fieldsVisitorEnabled;
 
-            protected FunctionDocSet(Input<Boolean> condition,
+            protected FunctionDocSet(AtomicReader reader,
+                                     @Nullable LuceneDocCollector.CollectorFieldsVisitor fieldsVisitor,
+                                     Input<Boolean> condition,
                                      List<LuceneCollectorExpression> expressions,
                                      int maxDoc,
                                      @Nullable Bits acceptDocs) {
                 super(maxDoc, acceptDocs);
+                this.reader = reader;
+                this.fieldsVisitor = fieldsVisitor;
+                //noinspection SimplifiableConditionalExpression
+                this.fieldsVisitorEnabled = fieldsVisitor == null ? false : fieldsVisitor.required();
                 this.condition = condition;
                 this.expressions = expressions;
             }
 
             @Override
             protected boolean matchDoc(int doc) {
+                if (fieldsVisitorEnabled) {
+                    fieldsVisitor.reset();
+                    try {
+                        reader.document(doc, fieldsVisitor);
+                    } catch (IOException e) {
+                        throw Throwables.propagate(e);
+                    }
+                }
                 for (LuceneCollectorExpression expression : expressions) {
                     expression.setNextDocId(doc);
                 }

core/src/main/java/io/crate/types/DataTypes.java
@@ -28,11 +28,15 @@ import io.crate.TimestampFormat;
 import org.apache.lucene.util.BytesRef;
 import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.io.stream.StreamOutput;
+import org.elasticsearch.common.logging.ESLogger;
+import org.elasticsearch.common.logging.Loggers;
 
 import java.io.IOException;
 import java.util.*;
 
 public class DataTypes {
+
+    private final static ESLogger logger = Loggers.getLogger(DataTypes.class);
 
     public final static UndefinedType UNDEFINED = UndefinedType.INSTANCE;
     public final static NotSupportedType NOT_SUPPORTED = NotSupportedType.INSTANCE;
@@ -147,9 +151,14 @@ public class DataTypes {
 
     public static DataType fromStream(StreamInput in) throws IOException {
         int i = in.readVInt();
-        DataType type = typeRegistry.get(i).create();
-        type.readFrom(in);
-        return type;
+        try {
+            DataType type = typeRegistry.get(i).create();
+            type.readFrom(in);
+            return type;
+        } catch (NullPointerException e) {
+            logger.error(String.format(Locale.ENGLISH, "%d is missing in typeRegistry", i), e);
+            throw e;
+        }
     }
 
     public static void toStream(DataType type, StreamOutput out) throws IOException {

core/src/main/java/io/crate/types/DoubleType.java
@@ -55,14 +55,14 @@ public class DoubleType extends DataType<Double> implements Streamer<Double>, Da
         if (value == null) {
             return null;
         }
+        if (value instanceof Double) {
+            return (Double) value;
+        }
         if (value instanceof String) {
             return Double.valueOf((String)value);
         }
         if (value instanceof BytesRef) {
             return Double.valueOf(((BytesRef)value).utf8ToString());
-        }
-        if (value instanceof Double) {
-            return (Double) value;
         }
         return ((Number)value).doubleValue();
     }

core/src/main/java/io/crate/types/FloatType.java
@@ -55,6 +55,9 @@ public class FloatType extends DataType<Float> implements Streamer<Float>, DataT
         if (value == null) {
             return null;
         }
+        if (value instanceof Float) {
+            return (Float) value;
+        }
         if (value instanceof String) {
             return Float.parseFloat((String)value);
         }

core/src/main/java/io/crate/types/IntegerType.java
@@ -55,6 +55,9 @@ public class IntegerType extends DataType<Integer> implements Streamer<Integer>,
         if (value == null) {
             return null;
         }
+        if (value instanceof Integer) {
+            return (Integer) value;
+        }
         if (value instanceof String) {
             return Integer.parseInt((String)value);
         }

core/src/main/java/io/crate/types/LongType.java
@@ -53,6 +53,9 @@ public class LongType extends DataType<Long> implements Streamer<Long>, DataType
         if (value == null) {
             return null;
         }
+        if (value instanceof Long) {
+            return (Long) value;
+        }
         if (value instanceof String) {
             return Long.valueOf((String)value);
         }

core/src/main/java/io/crate/types/ShortType.java
@@ -55,6 +55,9 @@ public class ShortType extends DataType<Short> implements DataTypeFactory, Strea
         if (value == null) {
             return null;
         }
+        if (value instanceof Short) {
+            return (Short) value;
+        }
         if (value instanceof String) {
             return Short.valueOf((String)value);
         }

sql/src/main/java/io/crate/analyze/DataStatementAnalyzer.java
@@ -185,7 +185,7 @@ abstract class DataStatementAnalyzer<T extends AbstractDataAnalysis> extends Abs
 
         return context.allocateFunction(
                 NotPredicate.INFO,
-                ImmutableList.<Symbol>of(context.allocateFunction(isNullInfo, ImmutableList.of(argument))));
+                Arrays.<Symbol>asList(context.allocateFunction(isNullInfo, Arrays.asList(argument))));
     }
 
     @Override
@@ -458,7 +458,7 @@ abstract class DataStatementAnalyzer<T extends AbstractDataAnalysis> extends Abs
                 new FunctionIdent(io.crate.operation.predicate.IsNullPredicate.NAME,
                         ImmutableList.of(DataTypeVisitor.fromSymbol((value))));
         FunctionInfo functionInfo = context.getFunctionInfo(functionIdent);
-        return context.allocateFunction(functionInfo, ImmutableList.of(value));
+        return context.allocateFunction(functionInfo, Arrays.asList(value));
     }
 
     @Override
@@ -885,7 +885,7 @@ abstract class DataStatementAnalyzer<T extends AbstractDataAnalysis> extends Abs
                 left = DataTypeSymbol.toDataTypeSymbol(left, leftType);
                 if (rightType.isConvertableTo(leftType)) {
                     FunctionInfo functionInfo = CastFunctionResolver.functionInfo(rightType, leftType);
-                    ImmutableList<Symbol> arguments = ImmutableList.of(right);
+                    List<Symbol> arguments = Arrays.asList(right);
                     right = new Function(functionInfo, arguments);
                 } else {
                     throw new IllegalArgumentException(SymbolFormatter.format(

commit 0ff662f953554beee20f7d16060e43132f3e6d1c

build booleanQuery with TermQueries for "where X in (...)"
the commit 61f0e8c773449b26866f179feedba54665f32cc2 together
with 8ead30b6816e96b50f3040ee65908b12ea577081 caused a
performance regression for WHERE x IN as it used table scan
/ source lookups instead of utilizing the lucene index.

sql/src/main/java/io/crate/lucene/LuceneQueryBuilder.java
@@ -48,6 +48,7 @@ import io.crate.operation.reference.doc.lucene.LuceneDocLevelReferenceResolver;
 import io.crate.operation.scalar.geo.DistanceFunction;
 import io.crate.operation.scalar.geo.WithinFunction;
 import io.crate.planner.symbol.*;
+import io.crate.types.CollectionType;
 import io.crate.types.DataTypes;
 import org.apache.lucene.index.AtomicReader;
 import org.apache.lucene.index.AtomicReaderContext;
@@ -266,6 +267,27 @@ public class LuceneQueryBuilder {
                 String columnName = tuple.v1().info().ident().columnIdent().fqn();
                 QueryBuilderHelper builder = QueryBuilderHelper.forType(tuple.v1().valueType());
                 return builder.like(columnName, tuple.v2().value());
+            }
+        }
+
+        class InQuery extends CmpQuery {
+
+            @Override
+            public Query apply(Function input, Context context) throws IOException {
+                Tuple<Reference, Literal> tuple = prepare(input);
+                if (tuple == null) {
+                    return null;
+                }
+                String field = tuple.v1().info().ident().columnIdent().fqn();
+                Literal literal = tuple.v2();
+                CollectionType dataType = ((CollectionType) literal.valueType());
+                QueryBuilderHelper builder = QueryBuilderHelper.forType(dataType.innerType());
+                BooleanQuery booleanQuery = new BooleanQuery();
+                Set values = (Set) literal.value();
+                for (Object value : values) {
+                    booleanQuery.add(builder.eq(field, value), BooleanClause.Occur.SHOULD);
+                }
+                return booleanQuery;
             }
         }
 
@@ -648,6 +670,7 @@ public class LuceneQueryBuilder {
                         .put(GteOperator.NAME, gteQuery)
                         .put(GtOperator.NAME, gtQuery)
                         .put(LikeOperator.NAME, likeQuery)
+                        .put(InOperator.NAME, new InQuery())
                         .put(NotPredicate.NAME, new NotQuery())
                         .put(IsNullPredicate.NAME, new IsNullQuery())
                         .put(MatchPredicate.NAME, new ToMatchQuery())

sql/src/test/java/io/crate/lucene/LuceneQueryBuilderTest.java
@@ -21,19 +21,20 @@
 
 package io.crate.lucene;
 
+import com.google.common.collect.Sets;
 import io.crate.analyze.WhereClause;
 import io.crate.metadata.FunctionIdent;
 import io.crate.metadata.FunctionInfo;
 import io.crate.metadata.Functions;
 import io.crate.operation.operator.EqOperator;
+import io.crate.operation.operator.InOperator;
 import io.crate.operation.operator.OperatorModule;
-import io.crate.planner.symbol.DataTypeSymbol;
-import io.crate.planner.symbol.Function;
-import io.crate.planner.symbol.Reference;
-import io.crate.planner.symbol.Symbol;
+import io.crate.planner.symbol.*;
+import io.crate.types.DataType;
 import io.crate.types.DataTypes;
-import org.apache.lucene.search.FilteredQuery;
-import org.apache.lucene.search.Query;
+import io.crate.types.SetType;
+import org.apache.lucene.search.*;
+import org.apache.lucene.util.BytesRef;
 import org.elasticsearch.common.inject.ModulesBuilder;
 import org.elasticsearch.index.cache.IndexCache;
 import org.elasticsearch.search.internal.SearchContext;
@@ -43,6 +44,7 @@ import org.mockito.Answers;
 
 import java.util.Arrays;
 
+import static io.crate.testing.TestingHelpers.createFunction;
 import static io.crate.testing.TestingHelpers.createReference;
 import static org.hamcrest.Matchers.instanceOf;
 import static org.junit.Assert.assertThat;
@@ -67,6 +69,24 @@ public class LuceneQueryBuilderTest {
         Query query = convert(eq(foo, foo));
         assertThat(query, instanceOf(FilteredQuery.class));
     }
+
+
+    @Test
+    public void testWhereRefInSetLiteralIsConvertedToBooleanQuery() throws Exception {
+        DataType dataType = new SetType(DataTypes.STRING);
+        Reference foo = createReference("foo", DataTypes.STRING);
+        WhereClause whereClause = new WhereClause(
+                createFunction(InOperator.NAME, DataTypes.BOOLEAN,
+                        foo,
+                        Literal.newLiteral(dataType, Sets.newHashSet(new BytesRef("foo"), new BytesRef("bar")))
+                ));
+        Query query = convert(whereClause);
+        assertThat(query, instanceOf(BooleanQuery.class));
+        for (BooleanClause booleanClause : (BooleanQuery) query) {
+            assertThat(booleanClause.getQuery(), instanceOf(TermQuery.class));
+        }
+    }
+
 
     private Query convert(WhereClause eq) {
         return builder.convert(eq).query;