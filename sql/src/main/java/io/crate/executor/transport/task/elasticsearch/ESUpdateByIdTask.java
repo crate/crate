6dc544de9a7a9625a7b629931f1da7d71c4dfcab
Philipp Bogensberger <philipp@crate.io>
2015-01-16 13:53:28 +0100

    Fix: Return zero row count instead of an exception if update by id doesn't match any document

--- sql/src/main/java/io/crate/executor/transport/task/elasticsearch/ESUpdateByIdTask.java
+++ sql/src/main/java/io/crate/executor/transport/task/elasticsearch/ESUpdateByIdTask.java
@@ -31,6 +31,7 @@ import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.action.update.TransportUpdateAction;
 import org.elasticsearch.action.update.UpdateRequest;
 import org.elasticsearch.action.update.UpdateResponse;
+import org.elasticsearch.index.engine.DocumentMissingException;
 import org.elasticsearch.index.engine.VersionConflictEngineException;
 
 public class ESUpdateByIdTask extends AsyncChainedTask {
@@ -59,7 +60,8 @@ public class ESUpdateByIdTask extends AsyncChainedTask {
         @Override
         public void onFailure(Throwable e) {
             e = Exceptions.unwrap(e);
-            if (e instanceof VersionConflictEngineException) {
+            if (e instanceof VersionConflictEngineException
+                    || e instanceof DocumentMissingException) {
                 future.set(TaskResult.ZERO);
             } else {
                 future.setException(e);
