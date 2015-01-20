e3cdc88f7fffbf3c61f9830916ea7f415036db00
Philipp Bogensberger <philipp@crate.io>
2015-01-20 21:03:34 +0100

    added integrationtests for cross joins

--- sql/src/test/java/io/crate/integrationtests/CrossJoinIntegrationTest.java
+++ sql/src/test/java/io/crate/integrationtests/CrossJoinIntegrationTest.java
@@ -21,9 +21,13 @@
 
 package io.crate.integrationtests;
 
+import io.crate.action.sql.SQLActionException;
 import io.crate.test.integration.CrateIntegrationTest;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
+import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.core.Is.is;
 
 @CrateIntegrationTest.ClusterScope(scope = CrateIntegrationTest.Scope.GLOBAL)
@@ -35,11 +39,103 @@ public class CrossJoinIntegrationTest extends SQLTransportIntegrationTest {
 
     private Setup setup = new Setup(sqlExecutor);
 
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
     @Test
     public void testSelectSubscript() throws Exception {
         setup.setUpCharacters();
         setup.setUpLocations();
         execute("select female, race['name'] from characters, locations");
+        assertThat(response.rowCount(), is(52L));
+    }
+
+    @Test
+    public void testSimpleThreeTableJoin() throws Exception {
+        setup.setUpBooks();
+        setup.setUpCharacters();
+        setup.setUpLocations();
+        execute("select title, characters.name, locations.name from books, characters, locations");
+        assertThat(response.rowCount(), is(156L));
+    }
+
+    @Test
+    public void testCrossJoinWithFunction() throws Exception {
+        setup.setUpEmployees();
+        setup.setUpBooks();
+        execute("select income * 2 from employees, books limit 1");
+    }
+
+    @Test
+    public void testAmbiguousAlias() throws Exception {
+        expectedException.expect(SQLActionException.class);
+        expectedException.expectMessage("The table or alias 'a' is specified more than once");
+        setup.setUpBooks();
+        execute("select a.title, a.title from books as a CROSS JOIN books as a");
+    }
+
+    @Test
+    public void testAmbiguousTable() throws Exception {
+        expectedException.expect(SQLActionException.class);
+        expectedException.expectMessage("The table or alias 'books' is specified more than once");
+        setup.setUpBooks();
+        execute("select title, title from books, books, books");
+    }
+
+    @Test
+    public void testColumnOrder() throws Exception {
+        setup.setUpBooks();
+        setup.setUpEmployees();
+        setup.setUpLocations();
+        execute("select title, locations.date, age from employees, books, locations");
+        assertArrayEquals(new String[]{"title", "locations.date", "age"}, response.cols());
+        Object[] row = response.rows()[0];
+        assertThat(row[0], is(instanceOf(String.class)));
+        assertThat(row[1], is(instanceOf(Long.class)));
+        assertThat(row[2], is(instanceOf(Integer.class)));
+    }
+
+    @Test
+    public void testJoinWithEmptyTable() throws Exception {
+        setup.setUpBooks();
+        execute("create table empty (id int primary key) clustered into 1 shards with (number_of_replicas=0)");
+        ensureGreen();
+
+        execute("select title, author from books CROSS JOIN empty");
+        assertThat(response.rowCount(), is(0L));
+    }
+
+    @Test
+    public void testJoinTwoEmptyTables() throws Exception {
+        execute("create table empty1 (id int primary key) clustered into 1 shards with (number_of_replicas=0)");
+        execute("create table empty2 (id int primary key) clustered into 1 shards with (number_of_replicas=0)");
+        ensureGreen();
+
+        execute("select * from empty1 CROSS JOIN empty2");
+        assertThat(response.rowCount(), is(0L));
+    }
+
+    @Test
+    public void testJoinOffset() throws Exception {
+        setup.setUpCharacters();
+        setup.setUpLocations();
+        execute("select * from locations, characters offset 50");
+        assertThat(response.rowCount(), is(2L));
+    }
+
+    @Test
+    public void testJoinHighLimit() throws Exception {
+        setup.setUpCharacters();
+        setup.setUpLocations();
+        execute("select * from locations, characters limit 300");
+        assertThat(response.rowCount(), is(52L));
+    }
+
+    @Test
+    public void testJoinHighLimitAndOffset() throws Exception {
+        setup.setUpCharacters();
+        setup.setUpLocations();
+        execute("select * from locations, characters limit 300 offset 200");
         assertThat(response.rowCount(), is(52L));
     }
 }
