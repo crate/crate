b4a02aebdba48f8bf05c68e9673f4bf840608d66
dobe <bernddorn@gmail.com>
2015-01-23 23:59:02 +0100

    removed requirement for context in  aggregationsearcher

--- sql/src/main/java/io/crate/analyze/relations/RelationAnalyzer.java
+++ sql/src/main/java/io/crate/analyze/relations/RelationAnalyzer.java
@@ -139,39 +139,33 @@ public class RelationAnalyzer extends DefaultTraversalVisitor<AnalyzedRelation, 
     }
 
     private boolean isAggregate(Symbol s) {
-        if (s.symbolType() == SymbolType.FUNCTION) {
-            if (((Function) s).info().type() == FunctionInfo.Type.AGGREGATE) {
-                return true;
-            }
-            AggregationSearcherContext searcherContext = new AggregationSearcherContext();
-            AGGREGATION_SEARCHER.process(s, searcherContext);
-            return searcherContext.found;
-        }
-        return false;
+        return AGGREGATION_SEARCHER.process(s, null);
     }
 
-    static class AggregationSearcherContext {
-        boolean found = false;
-    }
-
-    static class AggregationSearcher extends SymbolVisitor<AggregationSearcherContext, Void> {
+    static class AggregationSearcher extends SymbolVisitor<Void, Boolean> {
 
         @Override
-        public Void visitFunction(Function symbol, AggregationSearcherContext context) {
+        protected Boolean visitSymbol(Symbol symbol, Void context) {
+            return false;
+        }
+
+        @Override
+        public Boolean visitFunction(Function symbol, Void context) {
             if (symbol.info().type() == FunctionInfo.Type.AGGREGATE) {
-                context.found = true;
+                return true;
             } else {
                 for (Symbol argument : symbol.arguments()) {
-                    process(argument, context);
+                    if (process(argument, context)){
+                        return true;
+                    }
                 }
             }
-            return null;
+            return false;
         }
 
         @Override
-        public Void visitAggregation(Aggregation symbol, AggregationSearcherContext context) {
-            context.found = true;
-            return null;
+        public Boolean visitAggregation(Aggregation symbol, Void context) {
+            return true;
         }
     }
 
