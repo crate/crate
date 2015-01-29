ca542a2b1cc4ea7d5b9d616b8037b500685a81bb
Senad Biser <senchi@genijalno.ba>
2015-01-29 16:51:31 +0100

    skip testAritmeticFunctions cpu load assertion on windows

--- sql/src/test/java/io/crate/integrationtests/TransportSQLActionClassLifecycleTest.java
+++ sql/src/test/java/io/crate/integrationtests/TransportSQLActionClassLifecycleTest.java
@@ -30,6 +30,7 @@ import io.crate.metadata.settings.CrateSettings;
 import io.crate.test.integration.ClassLifecycleIntegrationTest;
 import io.crate.testing.SQLTransportExecutor;
 import io.crate.testing.TestingHelpers;
+import org.elasticsearch.common.os.OsUtils;
 import org.hamcrest.Matchers;
 import org.junit.*;
 import org.junit.rules.ExpectedException;
@@ -455,8 +456,10 @@ public class TransportSQLActionClassLifecycleTest extends ClassLifecycleIntegrat
         response = executor.exec("select ? + 2 from sys.cluster", 1);
         assertThat((Long)response.rows()[0][0], is(3L));
 
-        response = executor.exec("select load['1'] + load['5'], load['1'], load['5'] from sys.nodes limit 1");
-        assertEquals(response.rows()[0][0], (Double) response.rows()[0][1] + (Double) response.rows()[0][2]);
+        if(!OsUtils.WINDOWS) {
+            response = executor.exec("select load['1'] + load['5'], load['1'], load['5'] from sys.nodes limit 1");
+            assertEquals(response.rows()[0][0], (Double) response.rows()[0][1] + (Double) response.rows()[0][2]);
+        }
     }
 
     @Test
