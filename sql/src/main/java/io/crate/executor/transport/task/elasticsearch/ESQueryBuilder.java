013020bac7c3b824a90ac72f340b9e0e7048eb57
Eugene Pustovit <eugene.pustovit@gmail.com>
2015-02-03 15:34:18 +0100

    Added test for exception on count query for non-existant column

--- sql/src/main/java/io/crate/executor/transport/task/elasticsearch/ESQueryBuilder.java
+++ sql/src/main/java/io/crate/executor/transport/task/elasticsearch/ESQueryBuilder.java
@@ -118,11 +118,8 @@ public class ESQueryBuilder {
      */
 
     private void whereClause(Context context, WhereClause whereClause) throws IOException {
-        context.builder.startObject(Fields.QUERY);
-        if (whereClause.noMatch()) {
-            // should not happen
-            throw new IllegalStateException("A where clause with no match should not result in an ES query");
-        } else if (whereClause.hasQuery()) {
+        assert !whereClause.noMatch() : "A where clause with no match should not result in an ES query";
+        if (whereClause.hasQuery()) {
             visitor.process(whereClause.query(), context);
         } else {
             context.builder.field("match_all", Collections.emptyMap());
