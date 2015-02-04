e0f88707b8f57bef751c6c22dd83a9ebb1f20960
Senad Biser <sbiser@gmail.com>
2015-02-04 18:09:35 +0100

    - close reference to file header in DigestBlob on commit

--- blob/src/test/java/io/crate/DigestBlobTests.java
+++ blob/src/test/java/io/crate/DigestBlobTests.java
@@ -115,8 +115,10 @@ public class DigestBlobTests {
         stream.read(buffer, 0, 15);
         assertEquals("ABCDEFGHIJKLMNO", new BytesArray(buffer).toUtf8().trim());
         File file = digestBlob.commit();
+        stream.close();
         assertTrue(file.exists());
-        file.delete();
+        // just in case any references to file left
+        assertTrue(file.delete());
     }
 
     @Test
@@ -139,9 +141,11 @@ public class DigestBlobTests {
         FileInputStream stream = new FileInputStream(digestBlob.file());
         stream.read(buffer, 0, 15);
         assertEquals("ABCDEFGHIJKLMNO", new BytesArray(buffer).toUtf8().trim());
+        stream.close();
         File file = digestBlob.commit();
         assertTrue(file.exists());
-        file.delete();
+        // just in case any references to file left
+        assertTrue(file.delete());
     }
 
 }
