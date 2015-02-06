531efdb0f523f68fdf2f2f781fea2370186a24ca
Philipp Bogensberger <philipp@crate.io>
2015-02-06 17:41:25 +0100

    prepare release 0.47.0

--- core/src/main/java/io/crate/Version.java
+++ core/src/main/java/io/crate/Version.java
@@ -34,7 +34,7 @@ public class Version {
     // the (internal) format of the id is there so we can easily do after/before checks on the id
 
 
-    public static final boolean SNAPSHOT = true;
+    public static final boolean SNAPSHOT = false;
     public static final Version CURRENT = new Version(470099, SNAPSHOT, org.elasticsearch.Version.V_1_4_2);
 
     static {
