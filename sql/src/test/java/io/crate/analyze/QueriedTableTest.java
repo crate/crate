00f5bd4e74e35be5cdf4a7815b162fad5d673e8b
Mathias Fussenegger <f.mathias@zignar.net>
2015-02-11 11:39:03 +0100

    support whereClaues that contain "join conditions" in CrossJoinConsumer

--- sql/src/test/java/io/crate/analyze/QueriedTableTest.java
+++ sql/src/test/java/io/crate/analyze/QueriedTableTest.java
@@ -22,31 +22,121 @@
 package io.crate.analyze;
 
 import io.crate.analyze.relations.TableRelation;
-import io.crate.metadata.ColumnIdent;
+import io.crate.metadata.*;
 import io.crate.metadata.table.TableInfo;
+import io.crate.operation.operator.AndOperator;
+import io.crate.operation.operator.EqOperator;
+import io.crate.operation.operator.OperatorModule;
+import io.crate.operation.scalar.ScalarFunctionModule;
 import io.crate.operation.scalar.arithmetic.AddFunction;
+import io.crate.planner.RowGranularity;
 import io.crate.planner.symbol.Field;
 import io.crate.planner.symbol.Function;
 import io.crate.planner.symbol.Literal;
 import io.crate.planner.symbol.Symbol;
 import io.crate.sql.tree.QualifiedName;
 import io.crate.types.DataTypes;
+import org.elasticsearch.common.inject.Injector;
+import org.elasticsearch.common.inject.ModulesBuilder;
 import org.hamcrest.Matchers;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.List;
 
-import static io.crate.testing.TestingHelpers.createFunction;
-import static io.crate.testing.TestingHelpers.isField;
-import static io.crate.testing.TestingHelpers.isFunction;
+import static io.crate.testing.TestingHelpers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.nullValue;
+import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 
 public class QueriedTableTest {
+
+    private EvaluatingNormalizer normalizer;
+
+    @Before
+    public void setUp() throws Exception {
+        Injector injector = new ModulesBuilder()
+                .add(new ScalarFunctionModule())
+                .add(new OperatorModule())
+                .createInjector();
+        ReferenceResolver referenceResolver = new ReferenceResolver() {
+
+            @Override
+            public ReferenceImplementation getImplementation(ReferenceIdent ident) {
+                return null;
+            }
+        };
+        normalizer = new EvaluatingNormalizer(
+                injector.getInstance(Functions.class),
+                RowGranularity.CLUSTER,
+                referenceResolver
+        );
+    }
+
+    @SuppressWarnings("ConstantConditions")
+    @Test
+    public void testWhereClauseSplit() throws Exception {
+        // select t1.x from t1, t2 where  t1.x = 1 and t2.x = 3 and t1.y + t2.z = 3
+
+        /**
+         * verify that:
+         *      t1.x = 1 is fully pushed down
+         *      t2.x = 3 is also fully pushed down
+         *
+         *      t1.y + t2.z = 3 is in "remaining query"
+         *      and both t1.y, t2.z are added to the outputs of t1 / t2
+         */
+
+        TableRelation tr1 = new TableRelation(mock(TableInfo.class));
+        TableRelation tr2 = new TableRelation(mock(TableInfo.class));
+        Field t1x = new Field(tr1, new ColumnIdent("x"), DataTypes.INTEGER);
+        Field t1y = new Field(tr1, new ColumnIdent("y"), DataTypes.INTEGER);
+        Field t2x = new Field(tr2, new ColumnIdent("x"), DataTypes.INTEGER);
+        Field t2z = new Field(tr2, new ColumnIdent("z"), DataTypes.INTEGER);
+
+        Function t1xEq1 = createFunction(EqOperator.NAME, DataTypes.BOOLEAN, t1x, Literal.newLiteral(1));
+        Function t2xEq3 = createFunction(EqOperator.NAME, DataTypes.BOOLEAN, t2x, Literal.newLiteral(3));
+        Function t1yPlusT2z = createFunction(AddFunction.NAME, DataTypes.INTEGER, t1y, t2z);
+        Function t1yPlusT2zEq3 = createFunction(EqOperator.NAME, DataTypes.BOOLEAN, t1yPlusT2z, Literal.newLiteral(3));
+
+        Function t2xEq3AndT1yPlusT2zEq3 = createFunction(AndOperator.NAME, DataTypes.BOOLEAN, t2xEq3, t1yPlusT2zEq3);
+        Function topAnd = createFunction(AndOperator.NAME, DataTypes.BOOLEAN, t1xEq1, t2xEq3AndT1yPlusT2zEq3);
+
+        WhereClause whereClause = new WhereClause(topAnd);
+
+        QuerySpec querySpec = new QuerySpec()
+                .outputs(Arrays.<Symbol>asList(t1x))
+                .where(whereClause);
+
+        QueriedTable qt1 = QueriedTable.newSubRelation(new QualifiedName("t1"), tr1, querySpec);
+        QueriedTable qt2 = QueriedTable.newSubRelation(new QualifiedName("t2"), tr2, querySpec);
+
+        assertThat(qt1.querySpec().outputs().size(), is(2));
+        assertThat(qt1.querySpec().outputs().get(0), isField("x"));
+        assertThat(qt1.querySpec().outputs().get(1), isField("y"));
+        assertThat(qt1.querySpec().where().query(), equalTo((Symbol) t1xEq1));
+
+        assertThat(qt2.querySpec().outputs().size(), is(1));
+        assertThat(qt2.querySpec().outputs().get(0), isField("z"));
+        assertThat(qt2.querySpec().where().query(), equalTo((Symbol) t2xEq3));
+
+        Function remainingQuery = (Function) normalizer.normalize(querySpec.where().query());
+        assertThat(remainingQuery, isFunction(EqOperator.NAME));
+        assertThat(remainingQuery.arguments().get(0), isFunction(AddFunction.NAME));
+        assertThat(remainingQuery.arguments().get(1), isLiteral(3, DataTypes.INTEGER));
+
+        Function addFunction = (Function) remainingQuery.arguments().get(0);
+        Symbol firstArg = addFunction.arguments().get(0);
+        // must be a re-written field with qt1 as relation
+        assertThat(firstArg, isField("y"));
+        assertTrue(((Field) firstArg).relation() == qt1);
+
+        Symbol secondArg = addFunction.arguments().get(1);
+        assertThat(secondArg, isField("z"));
+        assertTrue(((Field) secondArg).relation() == qt2);
+    }
 
     @Test
     public void testNewSubRelationOutputsOnly() throws Exception {
