09d3b9d3430fbef16368ead573fd32830b622c04
Mathias Fussenegger <f.mathias@zignar.net>
2015-02-25 14:39:30 +0100

    add BucketMerger which can be used to merge buckets

---
+++ core/src/main/java/io/crate/core/collections/CollectionBucket.java
@@ -0,0 +1,74 @@
+/*
+ * Licensed to CRATE Technology GmbH ("Crate") under one or more contributor
+ * license agreements.  See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.  Crate licenses
+ * this file to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.  You may
+ * obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * However, if you have executed another commercial license agreement
+ * with Crate these terms will supersede the license and you may use the
+ * software solely pursuant to the terms of the relevant commercial agreement.
+ */
+
+package io.crate.core.collections;
+
+import com.google.common.base.Function;
+import com.google.common.collect.Iterators;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
+
+
+public class CollectionBucket implements Bucket {
+
+    private final Collection<Object[]> rows;
+
+    public CollectionBucket(Collection<Object[]> rows) {
+        this.rows = rows;
+    }
+
+    @Override
+    public int size() {
+        return rows.size();
+    }
+
+    @Override
+    public Iterator<Row> iterator() {
+        return Iterators.transform(rows.iterator(), new Function<Object[], Row>() {
+            Object[] current;
+
+            final Row row = new Row() {
+                @Override
+                public int size() {
+                    return current.length;
+                }
+
+                @Override
+                public Object get(int index) {
+                    return current[index];
+                }
+
+                @Override
+                public String toString() {
+                    return Arrays.toString(current);
+                }
+            };
+
+            @Override
+            public Row apply(Object[] input) {
+                current = input;
+                return row;
+            }
+        });
+    }
+}
--- core/src/main/java/io/crate/core/collections/RowN.java
+++ core/src/main/java/io/crate/core/collections/RowN.java
@@ -19,31 +19,30 @@
  * software solely pursuant to the terms of the relevant commercial agreement.
  */
 
-package io.crate.operation.projectors.sorting;
+package io.crate.core.collections;
 
-import org.apache.lucene.util.PriorityQueue;
+import java.util.Arrays;
 
-import java.util.Comparator;
+public class RowN implements Row {
 
-public class RowPriorityQueue<T> extends PriorityQueue<T> {
+    private final Object[] cells;
 
-    private final Comparator[] comparators;
-
-    public RowPriorityQueue(int maxSize, Comparator[] comparators) {
-        super(maxSize);
-        this.comparators = comparators;
+    public RowN(Object... cells) {
+        this.cells = cells;
     }
 
     @Override
-    public boolean lessThan(T a, T b) {
-        for (Comparator c : comparators) {
-            //noinspection unchecked
-            int compared = c.compare(a, b);
+    public int size() {
+        return cells.length;
+    }
 
-            if (compared < 0) return true;
-            if (compared == 0) continue;
-            if (compared > 0) return false;
-        }
-        return false;
+    @Override
+    public Object get(int index) {
+        return cells[index];
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(cells);
     }
 }
