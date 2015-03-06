0a5b6d560073f9b82184f530703fc092d4247692
Matthias Wahl <matthias@crate.io>
2015-03-06 15:16:15 +0100

    cleanup client jar

--- client/src/test/java/io/crate/client/CrateClientTest.java
+++ client/src/test/java/io/crate/client/CrateClientTest.java
@@ -37,7 +37,6 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.Arrays;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicReference;
@@ -87,12 +86,6 @@ public class CrateClientTest extends CrateIntegrationTest {
         assertEquals("1", r.rows()[0][0]);
 
         assertThat(r.columnTypes(), is(new DataType[0]));
-
-        System.out.println(Arrays.toString(r.cols()));
-        for (Object[] row: r.rows()){
-            System.out.println(Arrays.toString(row));
-        }
-
     }
 
     @Test
@@ -158,12 +151,6 @@ public class CrateClientTest extends CrateIntegrationTest {
         assertEquals("1", r.rows()[0][0]);
 
         assertThat(r.columnTypes()[0], instanceOf(StringType.class));
-
-        System.out.println(Arrays.toString(r.cols()));
-        for (Object[] row: r.rows()){
-            System.out.println(Arrays.toString(row));
-        }
-
     }
 
     @Test
