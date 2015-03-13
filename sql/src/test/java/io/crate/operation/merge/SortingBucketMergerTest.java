6b0bc05fa5427d42b715a8d6d5d65f5ac501aec2
Matthias Wahl <matthias@crate.io>
2015-03-13 12:06:25 +0100

    unify bucketmerger implementations

--- sql/src/test/java/io/crate/operation/merge/SortingBucketMergerTest.java
+++ sql/src/test/java/io/crate/operation/merge/SortingBucketMergerTest.java
@@ -22,8 +22,13 @@
 package io.crate.operation.merge;
 
 import com.google.common.base.Joiner;
+import com.google.common.collect.Iterators;
 import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.SettableFuture;
+import io.crate.core.collections.ArrayBucket;
 import io.crate.core.collections.Bucket;
+import io.crate.core.collections.BucketPage;
+import io.crate.operation.PageConsumeListener;
 import io.crate.operation.projectors.CollectingProjector;
 import io.crate.test.integration.CrateUnitTest;
 import io.crate.testing.TestingHelpers;
@@ -31,28 +36,44 @@ import org.junit.Test;
 
 import javax.annotation.Nullable;
 import java.util.Arrays;
-import java.util.List;
+import java.util.Iterator;
 import java.util.concurrent.ExecutionException;
 
-import static io.crate.testing.TestingHelpers.createBucketFutures;
+import static io.crate.testing.TestingHelpers.createPage;
 import static org.hamcrest.Matchers.is;
 
 
 @SuppressWarnings("unchecked")
 public class SortingBucketMergerTest extends CrateUnitTest {
 
-    private Bucket mergeWith(int buckets, @Nullable Boolean nullsFirst, List<ListenableFuture<Bucket>> ... pages)
+    private Bucket mergeWith(int buckets, @Nullable Boolean nullsFirst, BucketPage... pages)
             throws ExecutionException, InterruptedException {
 
         CollectingProjector collectingProjector = new CollectingProjector();
-        SortingBucketMerger merger = new SortingBucketMerger(
+        final SortingBucketMerger merger = new SortingBucketMerger(
                 buckets, new int[] { 0 }, new boolean[] { false }, new Boolean[] { nullsFirst });
         merger.downstream(collectingProjector);
 
-        for (List<ListenableFuture<Bucket>> page : pages) {
-            merger.merge(page);
+        final Iterator<BucketPage> pageIter = Iterators.forArray(pages);
+        if (pageIter.hasNext()) {
+            merger.nextPage(pageIter.next(), new PageConsumeListener() {
+                @Override
+                public void needMore() {
+                    if (pageIter.hasNext()) {
+                        merger.nextPage(pageIter.next(), this);
+                    } else {
+                        merger.finish();
+                    }
+                }
+
+                @Override
+                public void finish() {
+                    merger.finish();
+                }
+            });
+        } else {
+            merger.finish();
         }
-        merger.finish();
         return collectingProjector.result().get();
     }
 
@@ -62,14 +83,14 @@ public class SortingBucketMergerTest extends CrateUnitTest {
 
     @Test
     public void testRowsWithMultipleColumns() throws Exception {
-        List<ListenableFuture<Bucket>> page1 = createBucketFutures(
+        BucketPage page1 = createPage(
                 Arrays.asList(
-                        new Object[] { "A", 1 },
-                        new Object[] { "B", 1 }
+                        new Object[]{"A", 1},
+                        new Object[]{"B", 1}
                 ),
                 Arrays.asList(
-                        new Object[] { "A", 2 },
-                        new Object[] { "B", 2 }
+                        new Object[]{"A", 2},
+                        new Object[]{"B", 2}
                 )
         );
         Bucket bucket = mergeWith(2, null, page1);
@@ -78,7 +99,7 @@ public class SortingBucketMergerTest extends CrateUnitTest {
 
     @Test
     public void testNullsFirst() throws Exception {
-        List<ListenableFuture<Bucket>> page1 = createBucketFutures(
+        BucketPage page1 = createPage(
                 Arrays.asList(
                         new Object[]{null, 1},
                         new Object[]{"A", 1},
@@ -90,7 +111,7 @@ public class SortingBucketMergerTest extends CrateUnitTest {
                         new Object[]{"B", 2}
                 )
         );
-        List<ListenableFuture<Bucket>> page2 = createBucketFutures(
+        BucketPage page2 = createPage(
                 Arrays.<Object[]>asList(
                         new Object[]{"C", 3}
                 ),
@@ -100,12 +121,37 @@ public class SortingBucketMergerTest extends CrateUnitTest {
         );
         Bucket bucket = mergeWith(2, false, page1, page2);
         assertRows(bucket, "NULL| 1", "NULL| 2", "A| 1", "A| 2", "B| 1", "B| 2", "C| 3", "D| 3");
+    }
 
+    @Test
+    public void testNullsLast() throws Exception {
+        BucketPage page1 = createPage(
+                Arrays.asList(
+                        new Object[]{"A", 1},
+                        new Object[]{"B", 1}
+                ),
+                Arrays.asList(
+                        new Object[]{"A", 2},
+                        new Object[]{"B", 2}
+                )
+        );
+        BucketPage page2 = createPage(
+                Arrays.asList(
+                        new Object[]{"C", 3},
+                        new Object[]{null, 1}
+                ),
+                Arrays.asList(
+                        new Object[]{"D", 3},
+                        new Object[]{null, 2}
+                )
+        );
+        Bucket bucket = mergeWith(2, false, page1, page2);
+        assertRows(bucket, "A| 1", "A| 2", "B| 1", "B| 2", "C| 3", "D| 3", "NULL| 1", "NULL| 2");
     }
 
     @Test
     public void testMergeWith3Buckets() throws Exception {
-        List<ListenableFuture<Bucket>> page1 = createBucketFutures(
+        BucketPage page1 = createPage(
                 Arrays.asList(
                         new Object[]{"B"},
                         new Object[]{"B"}
@@ -127,24 +173,24 @@ public class SortingBucketMergerTest extends CrateUnitTest {
     @Test
     public void testMerge() throws Exception {
         // page1
-        List<ListenableFuture<Bucket>> p1Buckets = createBucketFutures(Arrays.asList(
-                        new Object[] { "A" },
-                        new Object[] { "A" },
-                        new Object[] { "B" },
-                        new Object[] { "C" }),
+        BucketPage p1Buckets = createPage(Arrays.asList(
+                        new Object[]{"A"},
+                        new Object[]{"A"},
+                        new Object[]{"B"},
+                        new Object[]{"C"}),
                 Arrays.asList(
-                        new Object[] { "B" },
-                        new Object[] { "B" })
+                        new Object[]{"B"},
+                        new Object[]{"B"})
         );
-        List<ListenableFuture<Bucket>> p2Buckets = createBucketFutures(
+        BucketPage p2Buckets = createPage(
                 Arrays.asList(
-                        new Object[] { "C" },
-                        new Object[] { "C" },
-                        new Object[] { "D" }),
+                        new Object[]{"C"},
+                        new Object[]{"C"},
+                        new Object[]{"D"}),
                 Arrays.asList(
-                        new Object[] { "B" },
-                        new Object[] { "B" },
-                        new Object[] { "D" }));
+                        new Object[]{"B"},
+                        new Object[]{"B"},
+                        new Object[]{"D"}));
 
         Bucket bucket = mergeWith(2, null, p1Buckets, p2Buckets);
         assertRows(bucket, "A", "A", "B", "B", "B", "B", "B", "C", "C", "C", "D", "D");
@@ -154,7 +200,7 @@ public class SortingBucketMergerTest extends CrateUnitTest {
     public void testBucketFromP1CantBeConsumedUntilPage3() throws Exception {
         // this tests that the iterator chaining works correctly
 
-        List<ListenableFuture<Bucket>> p1 = createBucketFutures(
+        BucketPage p1 = createPage(
                 Arrays.asList(
                         new Object[]{"X"},
                         new Object[]{"X"},
@@ -165,7 +211,7 @@ public class SortingBucketMergerTest extends CrateUnitTest {
                         new Object[]{"A"}
                 )
         );
-        List<ListenableFuture<Bucket>> p2 = createBucketFutures(
+        BucketPage p2 = createPage(
                 Arrays.asList(
                         new Object[]{"Y"},
                         new Object[]{"Y"},
@@ -176,7 +222,7 @@ public class SortingBucketMergerTest extends CrateUnitTest {
                         new Object[]{"B"}
                 )
         );
-        List<ListenableFuture<Bucket>> p3 = createBucketFutures(
+        BucketPage p3 = createPage(
                 Arrays.asList(
                         new Object[]{"Z"},
                         new Object[]{"Z"},
@@ -193,7 +239,7 @@ public class SortingBucketMergerTest extends CrateUnitTest {
 
     @Test
     public void testAllBucketsAreEmpty() throws Exception {
-        List<ListenableFuture<Bucket>> p1Buckets = createBucketFutures(
+        BucketPage p1Buckets = createPage(
                 Arrays.<Object[]>asList(),
                 Arrays.<Object[]>asList()
         );
@@ -203,16 +249,16 @@ public class SortingBucketMergerTest extends CrateUnitTest {
 
     @Test
     public void testWithOnlyOneBucket() throws Exception {
-        List<ListenableFuture<Bucket>> p1Buckets = createBucketFutures(
+        BucketPage p1Buckets = createPage(
                 Arrays.asList(
-                        new Object[] { "A" },
-                        new Object[] { "B" },
-                        new Object[] { "C" }));
-        List<ListenableFuture<Bucket>> p2Buckets = createBucketFutures(
+                        new Object[]{"A"},
+                        new Object[]{"B"},
+                        new Object[]{"C"}));
+        BucketPage p2Buckets = createPage(
                 Arrays.asList(
-                        new Object[] { "C" },
-                        new Object[] { "C" },
-                        new Object[] { "D" }));
+                        new Object[]{"C"},
+                        new Object[]{"C"},
+                        new Object[]{"D"}));
 
         Bucket bucket = mergeWith(1, null, p1Buckets, p2Buckets);
         assertThat(bucket.size(), is(6));
@@ -222,15 +268,70 @@ public class SortingBucketMergerTest extends CrateUnitTest {
 
     @Test
     public void testWithTwoBucketsButOneIsEmpty() throws Exception {
-        List<ListenableFuture<Bucket>> buckets = createBucketFutures(
+        BucketPage buckets = createPage(
                 Arrays.asList(
-                        new Object[] { "A" },
-                        new Object[] { "B" },
-                        new Object[] { "C" }),
+                        new Object[]{"A"},
+                        new Object[]{"B"},
+                        new Object[]{"C"}),
                 Arrays.<Object[]>asList());
 
         Bucket bucket = mergeWith(2, null, buckets);
         assertThat(bucket.size(), is(3));
         assertRows(bucket, "A", "B", "C");
+    }
+
+    @Test
+    public void testThreaded() throws Exception {
+        final SettableFuture<Bucket> bucketFuture1 = SettableFuture.create();
+        final SettableFuture<Bucket> bucketFuture2 = SettableFuture.create();
+        final BucketPage page1 = new BucketPage(Arrays.<ListenableFuture<Bucket>>asList(bucketFuture1, bucketFuture2));
+
+        final SettableFuture<Bucket> bucketFuture3 = SettableFuture.create();
+        final SettableFuture<Bucket> bucketFuture4 = SettableFuture.create();
+        final BucketPage page2 = new BucketPage(Arrays.<ListenableFuture<Bucket>>asList(bucketFuture3, bucketFuture4));
+
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                bucketFuture1.set(new ArrayBucket(new Object[][]{{ "A" }, { "B" }}));
+                try {
+                    Thread.sleep(randomIntBetween(10, 100));
+                } catch (InterruptedException e) {
+                    bucketFuture3.setException(e);
+                }
+                bucketFuture3.set(new ArrayBucket(new Object[][]{{ "D" }, { "D" }}));
+            }
+        });
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                bucketFuture2.set(new ArrayBucket(new Object[][] {{ "C" }, { "D" }}));
+                try {
+                    Thread.sleep(randomIntBetween(10, 100));
+                } catch (InterruptedException e) {
+                    bucketFuture4.setException(e);
+                }
+                bucketFuture4.set(new ArrayBucket(new Object[][]{{ "D" }, { "E" }}));
+            }
+        });
+        final SettableFuture<Bucket> future = SettableFuture.create();
+        Thread mergeThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    future.set(mergeWith(2, null, page1, page2));
+                } catch (ExecutionException | InterruptedException e) {
+                    fail(e.getMessage());
+                }
+            }
+        });
+        mergeThread.start();
+        thread1.start();
+        thread2.start();
+
+        assertRows(future.get(), "A", "B", "C", "D", "D", "D", "D", "E");
+        thread1.join();
+        thread2.join();
+        mergeThread.join();
     }
 }
