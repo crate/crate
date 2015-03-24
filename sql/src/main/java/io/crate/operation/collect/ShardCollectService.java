e388175f886f1dfde518ff2a6f4c97f4be53b382
Philipp Bogensberger <philipp@crate.io>
2015-03-24 15:16:52 +0100

    LuceneDocCollector: emit OrderedRowDelegate if orderBy contains values which are not selected

--- sql/src/main/java/io/crate/operation/collect/ShardCollectService.java
+++ sql/src/main/java/io/crate/operation/collect/ShardCollectService.java
@@ -232,6 +232,7 @@ public class ShardCollectService {
                             return new LuceneDocCollector(
                                     docCtx.topLevelInputs(),
                                     docCtx.docLevelExpressions(),
+                                    docCtx.orderByOnlyExpressions(),
                                     collectNode,
                                     functions,
                                     downstream,
