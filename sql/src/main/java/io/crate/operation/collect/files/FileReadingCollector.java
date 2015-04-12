c8c7227b227a1294e0d61639dc4892d7b010d28a
Eugene Pustovit <eugene.pustovit@gmail.com>
2015-04-12 22:21:46 +0300

    Added exception on unsupported uri schema.

--- sql/src/main/java/io/crate/operation/collect/files/FileReadingCollector.java
+++ sql/src/main/java/io/crate/operation/collect/files/FileReadingCollector.java
@@ -96,6 +96,9 @@ public class FileReadingCollector implements CrateCollector {
             if (this.fileUri.getScheme().equals("file") && !this.fileUri.getSchemeSpecificPart().startsWith("///")) {
                 throw new IllegalArgumentException("Invalid fileURI");
             }
+            if (!this.fileUri.getScheme().equals("file") && !this.fileUri.getScheme().equals("s3") ) {
+                throw new IllegalArgumentException("URI scheme is not supported");
+            }
         }
         this.downstream = downstream.registerUpstream(this);
         this.compressed = compression != null && compression.equalsIgnoreCase("gzip");
