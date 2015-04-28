25fdaec407e596710922776c9996e0bdea1097a2
Mathias Fussenegger <f.mathias@zignar.net>
2015-04-28 11:22:24 +0200

    add CountOperation

---
+++ sql/src/main/java/io/crate/operation/count/InternalCountOperation.java
@@ -0,0 +1,123 @@
+/*
+ * Licensed to CRATE Technology GmbH ("Crate") under one or more contributor
+ * license agreements.  See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.  Crate licenses
+ * this file to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.  You may
+ * obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * However, if you have executed another commercial license agreement
+ * with Crate these terms will supersede the license and you may use the
+ * software solely pursuant to the terms of the relevant commercial agreement.
+ */
+
+package io.crate.operation.count;
+
+import io.crate.analyze.WhereClause;
+import io.crate.lucene.LuceneQueryBuilder;
+import org.elasticsearch.cache.recycler.CacheRecycler;
+import org.elasticsearch.cache.recycler.PageCacheRecycler;
+import org.elasticsearch.cluster.ClusterService;
+import org.elasticsearch.common.Strings;
+import org.elasticsearch.common.inject.Inject;
+import org.elasticsearch.common.inject.Singleton;
+import org.elasticsearch.common.lucene.Lucene;
+import org.elasticsearch.common.util.BigArrays;
+import org.elasticsearch.index.service.IndexService;
+import org.elasticsearch.index.shard.service.IndexShard;
+import org.elasticsearch.indices.IndicesService;
+import org.elasticsearch.script.ScriptService;
+import org.elasticsearch.search.SearchShardTarget;
+import org.elasticsearch.search.internal.DefaultSearchContext;
+import org.elasticsearch.search.internal.SearchContext;
+import org.elasticsearch.search.internal.ShardSearchLocalRequest;
+import org.elasticsearch.threadpool.ThreadPool;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.Set;
+
+@Singleton
+public class InternalCountOperation implements CountOperation {
+
+    private final ClusterService clusterService;
+    private final ScriptService scriptService;
+    private final CacheRecycler cacheRecycler;
+    private final PageCacheRecycler pageCacheRecycler;
+    private final LuceneQueryBuilder queryBuilder;
+    private final BigArrays bigArrays;
+    private final ThreadPool threadPool;
+    private final IndicesService indicesService;
+
+    @Inject
+    public InternalCountOperation(ClusterService clusterService,
+                                  ScriptService scriptService,
+                                  CacheRecycler cacheRecycler,
+                                  PageCacheRecycler pageCacheRecycler,
+                                  LuceneQueryBuilder queryBuilder,
+                                  BigArrays bigArrays,
+                                  ThreadPool threadPool,
+                                  IndicesService indicesService) {
+        this.clusterService = clusterService;
+        this.scriptService = scriptService;
+        this.cacheRecycler = cacheRecycler;
+        this.pageCacheRecycler = pageCacheRecycler;
+        this.queryBuilder = queryBuilder;
+        this.bigArrays = bigArrays;
+        this.threadPool = threadPool;
+        this.indicesService = indicesService;
+    }
+
+    @Override
+    public long count(Map<String, Set<Integer>> indexShardMap, WhereClause whereClause) throws IOException {
+        long count = 0L;
+        for (Map.Entry<String, Set<Integer>> entry : indexShardMap.entrySet()) {
+            String index = entry.getKey();
+            for (Integer shardId : entry.getValue()) {
+                count += count(index, shardId, whereClause);
+            }
+        }
+        return count;
+    }
+
+    @Override
+    public long count(String index, int shardId, WhereClause whereClause) throws IOException {
+        IndexService indexService = indicesService.indexServiceSafe(index);
+        IndexShard indexShard = indexService.shardSafe(shardId);
+
+        SearchShardTarget shardTarget = new SearchShardTarget(clusterService.localNode().id(), index, shardId);
+        SearchContext context = new DefaultSearchContext(0,
+                new ShardSearchLocalRequest(
+                        Strings.EMPTY_ARRAY,
+                        System.currentTimeMillis(),
+                        null
+                ),
+                shardTarget,
+                indexShard.acquireSearcher("count-operation"),
+                indexService,
+                indexShard,
+                scriptService,
+                cacheRecycler,
+                pageCacheRecycler,
+                bigArrays,
+                threadPool.estimatedTimeInMillisCounter()
+        );
+        SearchContext.setCurrent(context);
+
+        try {
+            LuceneQueryBuilder.Context queryCtx = queryBuilder.convert(whereClause, context, indexService.cache());
+            return Lucene.count(context.searcher(), queryCtx.query());
+        } finally {
+            context.close();
+            SearchContext.removeCurrent();
+        }
+    }
+}
---
+++ sql/src/test/java/io/crate/operation/count/InternalCountOperationTest.java
@@ -0,0 +1,55 @@
+/*
+ * Licensed to CRATE Technology GmbH ("Crate") under one or more contributor
+ * license agreements.  See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.  Crate licenses
+ * this file to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.  You may
+ * obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * However, if you have executed another commercial license agreement
+ * with Crate these terms will supersede the license and you may use the
+ * software solely pursuant to the terms of the relevant commercial agreement.
+ */
+
+package io.crate.operation.count;
+
+import io.crate.analyze.WhereClause;
+import io.crate.integrationtests.SQLTransportIntegrationTest;
+import io.crate.operation.operator.EqOperator;
+import io.crate.planner.symbol.Literal;
+import io.crate.test.integration.CrateIntegrationTest;
+import io.crate.testing.TestingHelpers;
+import io.crate.types.DataTypes;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+
+@CrateIntegrationTest.ClusterScope(scope = CrateIntegrationTest.Scope.SUITE, numNodes = 1)
+public class InternalCountOperationTest extends SQLTransportIntegrationTest {
+
+    @Test
+    public void testCount() throws Exception {
+        execute("create table t (name string) clustered into 1 shards with (number_of_replicas = 0)");
+        ensureYellow();
+        execute("insert into t (name) values ('Marvin'), ('Arthur'), ('Trillian')");
+        execute("refresh table t");
+
+        CountOperation countOperation = cluster().getInstance(CountOperation.class);
+
+        assertThat(countOperation.count("t", 0, WhereClause.MATCH_ALL), is(3L));
+
+        WhereClause whereClause = TestingHelpers.whereClause(
+                EqOperator.NAME,
+                TestingHelpers.createReference("name", DataTypes.STRING),
+                Literal.newLiteral("Marvin"));
+        assertThat(countOperation.count("t", 0, whereClause), is(1L));
+    }
+}
