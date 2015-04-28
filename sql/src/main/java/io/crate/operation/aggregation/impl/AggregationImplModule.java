e3fbeb61e4e695ea609ee2a05013b964154c0787
Sebastian Utz <su@rtme.net>
2015-04-28 09:46:16 +0200

    bind functions after registration
    this way plugins can also register own functions

--- sql/src/main/java/io/crate/operation/aggregation/impl/AggregationImplModule.java
+++ sql/src/main/java/io/crate/operation/aggregation/impl/AggregationImplModule.java
@@ -28,24 +28,26 @@ import io.crate.operation.aggregation.AggregationFunction;
 import org.elasticsearch.common.inject.AbstractModule;
 import org.elasticsearch.common.inject.multibindings.MapBinder;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class AggregationImplModule extends AbstractModule {
 
+    private Map<FunctionIdent, FunctionImplementation> functions = new HashMap<>();
+    private Map<String, DynamicFunctionResolver> resolver = new HashMap<>();
     private MapBinder<FunctionIdent, FunctionImplementation> functionBinder;
     private MapBinder<String, DynamicFunctionResolver> resolverBinder;
 
     public void register(AggregationFunction impl) {
-        functionBinder.addBinding(impl.info().ident()).toInstance(impl);
+        functions.put(impl.info().ident(), impl);
     }
 
     public void register(String name, DynamicFunctionResolver dynamicFunctionResolver) {
-        resolverBinder.addBinding(name).toInstance(dynamicFunctionResolver);
+        resolver.put(name, dynamicFunctionResolver);
     }
 
     @Override
     protected void configure() {
-        functionBinder = MapBinder.newMapBinder(binder(), FunctionIdent.class, FunctionImplementation.class);
-        resolverBinder = MapBinder.newMapBinder(binder(), String.class, DynamicFunctionResolver.class);
-
         AverageAggregation.register(this);
         MinimumAggregation.register(this);
         MaximumAggregation.register(this);
@@ -57,5 +59,22 @@ public class AggregationImplModule extends AbstractModule {
         VarianceAggregation.register(this);
         GeometricMeanAggregation.register(this);
         StandardDeviationAggregation.register(this);
+
+        // bind all registered functions and resolver
+        // by doing it here instead of the register functions, plugins can also use the
+        // register functions in their onModule(...) hooks
+        functionBinder = MapBinder.newMapBinder(binder(), FunctionIdent.class, FunctionImplementation.class);
+        resolverBinder = MapBinder.newMapBinder(binder(), String.class, DynamicFunctionResolver.class);
+        for (Map.Entry<FunctionIdent, FunctionImplementation> entry : functions.entrySet()) {
+            functionBinder.addBinding(entry.getKey()).toInstance(entry.getValue());
+
+        }
+        for (Map.Entry<String, DynamicFunctionResolver> entry : resolver.entrySet()) {
+            resolverBinder.addBinding(entry.getKey()).toInstance(entry.getValue());
+        }
+
+        // clear registration maps
+        functions = null;
+        resolver = null;
     }
 }
