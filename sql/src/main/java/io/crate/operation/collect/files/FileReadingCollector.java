875ff83ab0a0c4b77a309c1a34f998b76d22e323
Mathias Fussenegger <f.mathias@zignar.net>
2015-05-12 12:08:36 +0200

    add a retry logic on SocketTimeoutException in the FileReadingCollector

--- sql/src/main/java/io/crate/operation/collect/files/FileReadingCollector.java
+++ sql/src/main/java/io/crate/operation/collect/files/FileReadingCollector.java
@@ -38,6 +38,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.SocketTimeoutException;
 import java.net.URI;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Paths;
@@ -50,6 +51,7 @@ import java.util.zip.GZIPInputStream;
 
 public class FileReadingCollector implements CrateCollector {
 
+    public static final int MAX_SOCKET_TIMEOUT_RETRIES = 5;
     private final Map<String, FileInputFactory> fileInputFactoryMap;
     private final URI fileUri;
     private final Predicate<URI> globPredicate;
@@ -173,26 +175,7 @@ public class FileReadingCollector implements CrateCollector {
         try {
             uris = getUris(fileInput, uriPredicate);
             for (URI uri : uris) {
-                InputStream inputStream = fileInput.getStream(uri);
-                if (inputStream == null) {
-                    continue;
-                }
-                BufferedReader reader;
-                reader = createReader(inputStream);
-
-                try {
-                    while ((line = reader.readLine()) != null) {
-                        if (line.length() == 0) { // skip empty lines
-                            continue;
-                        }
-                        collectorContext.lineContext().rawSource(line.getBytes(StandardCharsets.UTF_8));
-                        if (!downstream.setNextRow(row)) {
-                            break;
-                        }
-                    }
-                } finally {
-                    reader.close();
-                }
+                readLines(fileInput, collectorContext, uri, 0, 0);
             }
 
         } catch (IOException e) {
@@ -200,6 +183,37 @@ public class FileReadingCollector implements CrateCollector {
             return;
         }
         downstream.finish();
+    }
+
+    private void readLines(FileInput fileInput, CollectorContext collectorContext, URI uri, long startLine, int retry) throws IOException {
+        InputStream inputStream = fileInput.getStream(uri);
+        if (inputStream == null) {
+            return;
+        }
+
+        String line;
+        long linesRead = 0L;
+        try (BufferedReader reader = createReader(inputStream)) {
+            while ((line = reader.readLine()) != null) {
+                linesRead++;
+                if (linesRead < startLine) {
+                    continue;
+                }
+                if (line.length() == 0) { // skip empty lines
+                    continue;
+                }
+                collectorContext.lineContext().rawSource(line.getBytes(StandardCharsets.UTF_8));
+                if (!downstream.setNextRow(row)) {
+                    break;
+                }
+            }
+        } catch (SocketTimeoutException e) {
+            if (retry > MAX_SOCKET_TIMEOUT_RETRIES) {
+                throw e;
+            } else {
+                readLines(fileInput, collectorContext, uri, linesRead + 1, retry + 1);
+            }
+        }
     }
 
     private BufferedReader createReader(InputStream inputStream) throws IOException {
