5c4a4f48d9893fab69ad1356fdbea67d087a5dff
Christian Haudum <christian.haudum@gmail.com>
2015-05-19 13:44:47 +0200

    added dns-discovery module

---
+++ dns-discovery/src/main/java/org/elasticsearch/discovery/srv/SrvDiscovery.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2015 Grant Rodgers
+ *
+ *     Permission is hereby granted, free of charge, to any person obtaining
+ *     a copy of this software and associated documentation files (the "Software"),
+ *     to deal in the Software without restriction, including without limitation
+ *     the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ *     and/or sell copies of the Software, and to permit persons to whom the Software
+ *     is furnished to do so, subject to the following conditions:
+ *
+ *     The above copyright notice and this permission notice shall be included in
+ *     all copies or substantial portions of the Software.
+ *
+ *     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ *     EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ *     OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ *     IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ *     CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ *     TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+ *     OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package org.elasticsearch.discovery.srv;
+
+import org.elasticsearch.cluster.ClusterName;
+import org.elasticsearch.cluster.ClusterService;
+import org.elasticsearch.cluster.node.DiscoveryNodeService;
+import org.elasticsearch.cluster.settings.DynamicSettings;
+import org.elasticsearch.common.inject.Inject;
+import org.elasticsearch.common.inject.Singleton;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.discovery.DiscoverySettings;
+import org.elasticsearch.discovery.zen.ZenDiscovery;
+import org.elasticsearch.discovery.zen.elect.ElectMasterService;
+import org.elasticsearch.discovery.zen.ping.ZenPingService;
+import org.elasticsearch.node.settings.NodeSettingsService;
+import org.elasticsearch.threadpool.ThreadPool;
+import org.elasticsearch.transport.TransportService;
+
+
+@Singleton
+public class SrvDiscovery extends ZenDiscovery {
+
+    @Inject
+    public SrvDiscovery(Settings settings, ClusterName clusterName, ThreadPool threadPool, TransportService transportService,
+                        ClusterService clusterService, NodeSettingsService nodeSettingsService, ZenPingService pingService,
+                        DiscoveryNodeService discoveryNodeService,DiscoverySettings discoverySettings,
+                        ElectMasterService electMasterService, DynamicSettings dynamicSettings) {
+        super(settings, clusterName, threadPool, transportService, clusterService, nodeSettingsService,
+                discoveryNodeService, pingService, electMasterService, discoverySettings, dynamicSettings);
+    }
+}
