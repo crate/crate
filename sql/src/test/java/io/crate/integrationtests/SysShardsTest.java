71d0b92502d11acc9517bf8a6445120b1c8a54b9
Mathias Fussenegger <f.mathias@zignar.net>
2015-05-28 16:54:36 +0200

    testing: fix flaky testSelectNodeSysExpressionWithUnassignedShards

--- sql/src/test/java/io/crate/integrationtests/SysShardsTest.java
+++ sql/src/test/java/io/crate/integrationtests/SysShardsTest.java
@@ -308,15 +308,15 @@ public class SysShardsTest extends ClassLifecycleIntegrationTest {
     @Test
     public void testSelectNodeSysExpressionWithUnassignedShards() throws Exception {
         try {
-            transportExecutor.exec(
-                    "create table users (id integer primary key, name string) with (number_of_replicas=2)");
+            transportExecutor.exec("create table users (id integer primary key, name string) " +
+                    "clustered into 5 shards with (number_of_replicas=2)");
             transportExecutor.ensureYellowOrGreen();
             SQLResponse response = transportExecutor.exec(
                     "select _node['name'], id from sys.shards where table_name = 'users' order by _node['name'] nulls last"
             );
             String nodeName = response.rows()[0][0].toString();
             assertEquals("shared0", nodeName);
-            assertThat(response.rows()[8][0], is(nullValue()));
+            assertThat(response.rows()[12][0], is(nullValue()));
         } finally {
             transportExecutor.exec("drop table users");
         }
