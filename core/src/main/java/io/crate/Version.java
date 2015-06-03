a998ac34031153b599c39d08f650a03193f0733b
Sebastian Utz <su@rtme.net>
2015-06-03 15:47:54 +0200

    prepare release 0.49.2

--- core/src/main/java/io/crate/Version.java
+++ core/src/main/java/io/crate/Version.java
@@ -35,7 +35,7 @@ public class Version {
 
 
     public static final boolean SNAPSHOT = false;
-    public static final Version CURRENT = new Version(490199, SNAPSHOT, org.elasticsearch.Version.V_1_4_4);
+    public static final Version CURRENT = new Version(490299, SNAPSHOT, org.elasticsearch.Version.V_1_4_4);
 
     static {
         // safe-guard that we don't release a version with DEBUG_MODE set to true
