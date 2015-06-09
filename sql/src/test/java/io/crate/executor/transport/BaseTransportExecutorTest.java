8f3fc992371c47dedffb4c7cb9e512e8663b1de8
Sebastian Utz <su@rtme.net>
2015-06-10 00:39:51 +0200

    implement nested-loop operation

--- sql/src/test/java/io/crate/executor/transport/BaseTransportExecutorTest.java
+++ sql/src/test/java/io/crate/executor/transport/BaseTransportExecutorTest.java
@@ -40,6 +40,7 @@ import io.crate.planner.symbol.Reference;
 import io.crate.planner.symbol.Symbol;
 import io.crate.testing.TestingHelpers;
 import io.crate.types.DataTypes;
+import org.elasticsearch.cluster.ClusterService;
 import org.junit.After;
 import org.junit.Before;
 
@@ -58,6 +59,7 @@ public class BaseTransportExecutorTest extends SQLTransportIntegrationTest {
 
     TransportExecutor executor;
     DocSchemaInfo docSchemaInfo;
+    ClusterService clusterService;
 
     TableIdent charactersIdent = new TableIdent(null, "characters");
     TableIdent booksIdent = new TableIdent(null, "books");
@@ -90,8 +92,11 @@ public class BaseTransportExecutorTest extends SQLTransportIntegrationTest {
 
     @Before
     public void transportSetUp() {
-        executor = internalCluster().getInstance(TransportExecutor.class);
-        docSchemaInfo = internalCluster().getInstance(DocSchemaInfo.class);
+        String[] nodeNames = internalCluster().getNodeNames();
+        String handlerNodeName = nodeNames[randomIntBetween(0, nodeNames.length-1)];
+        executor = internalCluster().getInstance(TransportExecutor.class, handlerNodeName);
+        docSchemaInfo = internalCluster().getInstance(DocSchemaInfo.class, handlerNodeName);
+        clusterService = internalCluster().getInstance(ClusterService.class, handlerNodeName);
     }
 
     @After
