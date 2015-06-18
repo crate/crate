cf66dedb1db2e51de3543902b2937385cbf7ec1f
Philipp Bogensberger <philipp@crate.io>
2015-06-18 15:24:17 +0200

    Fix: Fixed a bug where some searchers were not properly closed

--- sql/src/main/java/io/crate/operation/projectors/FetchProjector.java
+++ sql/src/main/java/io/crate/operation/projectors/FetchProjector.java
@@ -261,7 +261,7 @@ public class FetchProjector implements Projector, RowDownstreamHandle {
         request.executionNodeId(executionNodeId);
         request.toFetchReferences(toFetchReferences);
         request.jobSearchContextDocIds(nodeBucket.docIds());
-        if (bulkSize > NO_BULK_REQUESTS) {
+        if (bulkSize > NO_BULK_REQUESTS && remainingUpstreams.get() > 0) {
             request.closeContext(false);
         }
         transportFetchNodeAction.execute(nodeBucket.nodeId, request, new ActionListener<NodeFetchResponse>() {
