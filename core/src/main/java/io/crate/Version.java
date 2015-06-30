ebfbc3fd13c68d5b9f6a25fa4292a9a6aefb1174
Christian Haudum <christian.haudum@gmail.com>
2015-06-30 17:15:59 +0200

    prepare release 0.49.4

--- core/src/main/java/io/crate/Version.java
+++ core/src/main/java/io/crate/Version.java
@@ -35,7 +35,7 @@ public class Version {
 
 
     public static final boolean SNAPSHOT = false;
-    public static final Version CURRENT = new Version(490399, SNAPSHOT, org.elasticsearch.Version.V_1_4_4);
+    public static final Version CURRENT = new Version(490499, SNAPSHOT, org.elasticsearch.Version.V_1_4_4);
 
     static {
         // safe-guard that we don't release a version with DEBUG_MODE set to true
