4593c58d0d6fe6c6a3b0f3792fe02cdaaf64cfe4
Philipp Bogensberger <philipp@crate.io>
2015-07-06 15:49:27 +0200

    added NestedLoop execution Context

--- sql/src/main/java/io/crate/action/job/ContextPreparer.java
+++ sql/src/main/java/io/crate/action/job/ContextPreparer.java
@@ -45,6 +45,7 @@ import io.crate.operation.collect.JobCollectContext;
 import io.crate.operation.collect.MapSideDataCollectOperation;
 import io.crate.operation.count.CountOperation;
 import io.crate.operation.fetch.FetchContext;
+import io.crate.operation.join.NestedLoopOperation;
 import io.crate.operation.projectors.FlatProjectorChain;
 import io.crate.operation.projectors.RowDownstreamFactory;
 import io.crate.operation.projectors.RowReceiver;
@@ -57,6 +58,7 @@ import io.crate.planner.node.StreamerVisitor;
 import io.crate.planner.node.dql.CollectPhase;
 import io.crate.planner.node.dql.CountPhase;
 import io.crate.planner.node.dql.MergePhase;
+import io.crate.planner.node.dql.join.NestedLoopPhase;
 import io.crate.planner.node.fetch.FetchPhase;
 import io.crate.types.DataTypes;
 import org.elasticsearch.cluster.ClusterService;
@@ -66,6 +68,7 @@ import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.common.inject.Singleton;
 import org.elasticsearch.common.logging.ESLogger;
 import org.elasticsearch.common.logging.Loggers;
+import org.elasticsearch.threadpool.ThreadPool;
 
 import javax.annotation.Nullable;
 import java.util.*;
@@ -79,6 +82,7 @@ public class ContextPreparer {
     private final MapSideDataCollectOperation collectOperation;
     private ClusterService clusterService;
     private CountOperation countOperation;
+    private final ThreadPool threadPool;
     private final CircuitBreaker circuitBreaker;
     private final PageDownstreamFactory pageDownstreamFactory;
     private final RowDownstreamFactory rowDownstreamFactory;
@@ -89,11 +93,13 @@ public class ContextPreparer {
                            ClusterService clusterService,
                            CrateCircuitBreakerService breakerService,
                            CountOperation countOperation,
+                           ThreadPool threadPool,
                            PageDownstreamFactory pageDownstreamFactory,
                            RowDownstreamFactory rowDownstreamFactory) {
         this.collectOperation = collectOperation;
         this.clusterService = clusterService;
         this.countOperation = countOperation;
+        this.threadPool = threadPool;
         circuitBreaker = breakerService.getBreaker(CrateCircuitBreakerService.QUERY_BREAKER);
         this.pageDownstreamFactory = pageDownstreamFactory;
         this.rowDownstreamFactory = rowDownstreamFactory;
@@ -371,6 +377,7 @@ public class ContextPreparer {
                     pageDownstreamFactory.createMergeNodePageDownstream(
                             phase,
                             rowReceiver,
+                            false,
                             ramAccountingContext,
                             // no separate executor because TransportDistributedResultAction already runs in a threadPool
                             Optional.<Executor>absent());
@@ -436,6 +443,83 @@ public class ContextPreparer {
                     localNodeId,
                     context.sharedShardContexts,
                     routings);
+        }
+
+        @Override
+        public ExecutionSubContext visitNestedLoopPhase(NestedLoopPhase phase, PreparerContext context) {
+            RamAccountingContext ramAccountingContext = RamAccountingContext.forExecutionPhase(circuitBreaker, phase);
+            RowReceiver downstreamRowReceiver = context.getRowReceiver(phase, Paging.PAGE_SIZE);
+            if (downstreamRowReceiver == null) {
+                context.executionPhasesToProcess.add(phase);
+                return null;
+            }
+
+            FlatProjectorChain flatProjectorChain;
+            if (!phase.projections().isEmpty()) {
+                flatProjectorChain = FlatProjectorChain.withAttachedDownstream(
+                        pageDownstreamFactory.projectorFactory(),
+                        ramAccountingContext,
+                        phase.projections(),
+                        downstreamRowReceiver,
+                        phase.jobId()
+                );
+            } else {
+                flatProjectorChain = FlatProjectorChain.withReceivers(Collections.singletonList(downstreamRowReceiver));
+            }
+
+            NestedLoopOperation nestedLoopOperation = new NestedLoopOperation(flatProjectorChain.firstProjector());
+            return new NestedLoopContext(
+                    phase,
+                    flatProjectorChain,
+                    nestedLoopOperation,
+                    pageDownstreamContextForNestedLoop(
+                            phase.executionPhaseId(),
+                            context,
+                            (byte) 0,
+                            phase.leftMergePhase(),
+                            nestedLoopOperation.leftRowReceiver(),
+                            ramAccountingContext),
+                    pageDownstreamContextForNestedLoop(
+                            phase.executionPhaseId(),
+                            context,
+                            (byte) 1,
+                            phase.rightMergePhase(),
+                            nestedLoopOperation.rightRowReceiver(),
+                            ramAccountingContext
+                    )
+            );
+        }
+
+        @Nullable
+        private PageDownstreamContext pageDownstreamContextForNestedLoop(int nlPhaseId,
+                                                                         PreparerContext ctx,
+                                                                         byte inputId,
+                                                                         @Nullable MergePhase mergePhase,
+                                                                         RowReceiver rowReceiver,
+                                                                         RamAccountingContext ramAccountingContext) {
+            boolean upstreamOnSameNode = ctx.getPhaseHasSameNodeUpstream(nlPhaseId, inputId);
+            if (upstreamOnSameNode) {
+                assert mergePhase == null : "if upstream is on same node the phase must be null";
+                ctx.registerRowReceiver(nlPhaseId, inputId, rowReceiver);
+                return null;
+            }
+            assert mergePhase != null : "if upstream isn't on the same node, there must be a mergePhase";
+            Tuple<PageDownstream, FlatProjectorChain> pageDownstreamWithChain = pageDownstreamFactory.createMergeNodePageDownstream(
+                    mergePhase,
+                    rowReceiver,
+                    true,
+                    ramAccountingContext,
+                    Optional.of(threadPool.executor(ThreadPool.Names.SEARCH))
+            );
+            return new PageDownstreamContext(
+                    mergePhase.executionPhaseId(),
+                    mergePhase.name(),
+                    pageDownstreamWithChain.v1(),
+                    StreamerVisitor.streamerFromOutputs(mergePhase),
+                    ramAccountingContext,
+                    mergePhase.numUpstreams(),
+                    pageDownstreamWithChain.v2()
+            );
         }
     }
 }
---
+++ sql/src/main/java/io/crate/jobs/NestedLoopContext.java
@@ -0,0 +1,186 @@
+/*
+ * Licensed to CRATE Technology GmbH ("Crate") under one or more contributor
+ * license agreements.  See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.  Crate licenses
+ * this file to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.  You may
+ * obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * However, if you have executed another commercial license agreement
+ * with Crate these terms will supersede the license and you may use the
+ * software solely pursuant to the terms of the relevant commercial agreement.
+ */
+
+package io.crate.jobs;
+
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
+import io.crate.operation.join.NestedLoopOperation;
+import io.crate.operation.projectors.FlatProjectorChain;
+import io.crate.operation.projectors.ListenableRowReceiver;
+import io.crate.planner.node.dql.join.NestedLoopPhase;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class NestedLoopContext extends AbstractExecutionSubContext implements DownstreamExecutionSubContext, ExecutionState {
+
+    private final AtomicInteger activeSubContexts = new AtomicInteger(0);
+    private final NestedLoopPhase nestedLoopPhase;
+    private final FlatProjectorChain flatProjectorChain;
+
+    @Nullable
+    private final PageDownstreamContext leftPageDownstreamContext;
+    @Nullable
+    private final PageDownstreamContext rightPageDownstreamContext;
+    private final ListenableRowReceiver leftRowReceiver;
+    private final ListenableRowReceiver rightRowReceiver;
+
+    private volatile boolean isKilled;
+    private final AtomicReference<Throwable> failure = new AtomicReference<>(null);
+
+    public NestedLoopContext(NestedLoopPhase phase,
+                             FlatProjectorChain flatProjectorChain,
+                             NestedLoopOperation nestedLoopOperation,
+                             @Nullable PageDownstreamContext leftPageDownstreamContext,
+                             @Nullable PageDownstreamContext rightPageDownstreamContext) {
+        super(phase.executionPhaseId());
+
+        nestedLoopPhase = phase;
+        this.flatProjectorChain = flatProjectorChain;
+        this.leftPageDownstreamContext = leftPageDownstreamContext;
+        this.rightPageDownstreamContext = rightPageDownstreamContext;
+
+        leftRowReceiver = nestedLoopOperation.leftRowReceiver();
+        rightRowReceiver = nestedLoopOperation.rightRowReceiver();
+
+        if (leftPageDownstreamContext == null) {
+            Futures.addCallback(leftRowReceiver.finishFuture(), new RemoveContextCallback());
+        } else {
+            leftPageDownstreamContext.future.addCallback(new RemoveContextCallback());
+        }
+
+        if (rightPageDownstreamContext == null) {
+            Futures.addCallback(rightRowReceiver.finishFuture(), new RemoveContextCallback());
+        } else {
+            rightPageDownstreamContext.future.addCallback(new RemoveContextCallback());
+        }
+    }
+
+    @Override
+    public String name() {
+        return nestedLoopPhase.name();
+    }
+
+    @Override
+    public int id() {
+        return nestedLoopPhase.executionPhaseId();
+    }
+
+    @Override
+    public void keepAliveListener(KeepAliveListener listener) {
+    }
+
+    @Override
+    protected void innerPrepare() {
+        flatProjectorChain.startProjections(this);
+    }
+
+    @Override
+    protected void innerStart() {
+        if (leftPageDownstreamContext != null) {
+            leftPageDownstreamContext.start();
+        }
+        if (rightPageDownstreamContext != null) {
+            rightPageDownstreamContext.start();
+        }
+    }
+
+    @Override
+    protected void innerClose(@Nullable Throwable t) {
+        closeSubContext(t, leftPageDownstreamContext, leftRowReceiver);
+        closeSubContext(t, rightPageDownstreamContext, rightRowReceiver);
+    }
+
+    private static void closeSubContext(@Nullable Throwable t, @Nullable PageDownstreamContext subContext, ListenableRowReceiver rowReceiver) {
+        if (subContext == null) {
+            finishOrFail(t, rowReceiver);
+        } else {
+            subContext.close(t);
+        }
+    }
+
+    private static void finishOrFail(@Nullable Throwable t, ListenableRowReceiver rowReceiver) {
+        if (t == null) {
+            rowReceiver.finish();
+        } else {
+            rowReceiver.fail(t);
+        }
+    }
+
+    @Override
+    protected void innerKill(@Nullable Throwable t) {
+        isKilled = true;
+        killSubContext(t, leftPageDownstreamContext, leftRowReceiver);
+        killSubContext(t, rightPageDownstreamContext, rightRowReceiver);
+    }
+
+    private static void killSubContext(Throwable t, @Nullable PageDownstreamContext subContext, ListenableRowReceiver rowReceiver) {
+        if (subContext == null) {
+            rowReceiver.fail(t);
+        } else {
+            subContext.kill(t);
+        }
+    }
+
+    @Override
+    public boolean isKilled() {
+        return isKilled;
+    }
+
+    @Override
+    public PageDownstreamContext pageDownstreamContext(byte inputId) {
+        assert inputId < 2 : "Only 0 and 1 inputId's supported";
+        if (inputId == 0) {
+            return leftPageDownstreamContext;
+        }
+        return rightPageDownstreamContext;
+    }
+
+    private class RemoveContextCallback implements FutureCallback<Object> {
+
+        public RemoveContextCallback() {
+            activeSubContexts.incrementAndGet();
+        }
+
+        @Override
+        public void onSuccess(@Nullable Object result) {
+            countdown();
+        }
+
+        private void countdown() {
+            if (activeSubContexts.decrementAndGet() == 0) {
+                Throwable t = failure.get();
+                if (future.firstClose()) {
+                    future.close(t);
+                }
+            }
+        }
+
+        @Override
+        public void onFailure(@Nonnull Throwable t) {
+            failure.set(t);
+            countdown();
+        }
+    }
+}
