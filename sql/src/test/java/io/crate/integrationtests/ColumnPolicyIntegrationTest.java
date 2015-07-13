202716e1a23ac7ba28fb81c41acb3a3a97360141
mikethebeer <michael.beer@crate.io>
2015-07-13 09:00:31 +0200

    fixed flaky test at sql dynamic mapping

--- sql/src/test/java/io/crate/integrationtests/ColumnPolicyIntegrationTest.java
+++ sql/src/test/java/io/crate/integrationtests/ColumnPolicyIntegrationTest.java
@@ -233,7 +233,7 @@ public class ColumnPolicyIntegrationTest extends SQLTransportIntegrationTest {
         execute("insert into dynamic_table (my_object) values ({a=['a','b']}),({b=['a']})");
         execute("refresh table dynamic_table");
 
-        waitForMappingUpdateOnAll("dynamic_table", "my_object.a");
+        waitForMappingUpdateOnAll("dynamic_table", "my_object.a", "my_object.b");
         Map<String, Object> sourceMap = getSourceMap("dynamic_table");
         assertThat(String.valueOf(nestedValue(sourceMap, "properties.my_object.properties.a.type")), is("array"));
         assertThat(String.valueOf(nestedValue(sourceMap, "properties.my_object.properties.a.inner.type")), is("string"));
