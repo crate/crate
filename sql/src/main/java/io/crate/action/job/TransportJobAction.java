012f906c584ac80b22d37877b9e03febd202091d
Mathias Fussenegger <f.mathias@zignar.net>
2015-07-28 13:34:51 +0200

    use SAME executor in TransportJobAction
    Collect operations run in separate threads already and the
    context preparation is fairly cheap. Using Same instead of
    Search it is guaranteed that the Context creation always
    works and doesn't abort with a EsRejectedExecutionException.
    
    using same:
    
        ❯ wrk -c 500 -d30s -t4 http://localhost:4200/_sql -s select.lua
        Running 30s test @ http://localhost:4200/_sql
        4 threads and 500 connections
        Thread Stats   Avg      Stdev     Max   +/- Stdev
            Latency    35.19ms   17.59ms 477.37ms   78.48%
            Req/Sec     3.59k   613.58     5.33k    72.36%
        415313 requests in 30.00s, 71.30MB read
        Socket errors: connect 0, read 0, write 0, timeout 123
        Requests/sec:  13843.50
        Transfer/sec:      2.38MB
    
        ❯ wrk -c 500 -d30s -t4 http://localhost:4200/_sql -s select.lua
        Running 30s test @ http://localhost:4200/_sql
        4 threads and 500 connections
        Thread Stats   Avg      Stdev     Max   +/- Stdev
            Latency    35.36ms   15.14ms 490.24ms   72.98%
            Req/Sec     3.58k   496.57     6.94k    76.65%
        421952 requests in 30.00s, 72.44MB read
        Requests/sec:  14064.05
        Transfer/sec:      2.41MB
    
        ❯ wrk -c 500 -d30s -t4 http://localhost:4200/_sql -s select.lua
        Running 30s test @ http://localhost:4200/_sql
        4 threads and 500 connections
        Thread Stats   Avg      Stdev     Max   +/- Stdev
            Latency    36.49ms   21.45ms   1.70s    84.19%
            Req/Sec     3.57k   832.36    10.31k    79.86%
        414406 requests in 30.00s, 71.14MB read
        Socket errors: connect 0, read 23, write 0, timeout 23
        Requests/sec:  13812.04
        Transfer/sec:      2.37MB
    
    using search:
    
        ❯ wrk -c 500 -d30s -t4 http://localhost:4200/_sql -s select.lua
        Running 30s test @ http://localhost:4200/_sql
        4 threads and 500 connections
        Thread Stats   Avg      Stdev     Max   +/- Stdev
            Latency    40.37ms   35.45ms   1.69s    91.88%
            Req/Sec     3.20k     0.96k    6.13k    67.58%
        377092 requests in 30.00s, 64.88MB read
        Socket errors: connect 0, read 0, write 0, timeout 130
        Non-2xx or 3xx responses: 17
        Requests/sec:  12569.93
        Transfer/sec:      2.16MB
    
        ❯ wrk -c 500 -d30s -t4 http://localhost:4200/_sql -s select.lua
        Running 30s test @ http://localhost:4200/_sql
        4 threads and 500 connections
        Thread Stats   Avg      Stdev     Max   +/- Stdev
            Latency    32.67ms   23.15ms 979.39ms   83.91%
            Req/Sec     3.97k   616.80     5.50k    76.72%
        470269 requests in 30.00s, 80.86MB read
        Socket errors: connect 0, read 0, write 0, timeout 1
        Non-2xx or 3xx responses: 6
        Requests/sec:  15674.24
        Transfer/sec:      2.69MB
    
        ❯ wrk -c 500 -d30s -t4 http://localhost:4200/_sql -s select.lua
        Running 30s test @ http://localhost:4200/_sql
        4 threads and 500 connections
        Thread Stats   Avg      Stdev     Max   +/- Stdev
            Latency    33.08ms   29.24ms 464.06ms   93.44%
            Req/Sec     3.99k   607.62     7.96k    80.31%
        473806 requests in 30.00s, 81.46MB read
        Requests/sec:  15793.33
        Transfer/sec:      2.72MB

--- sql/src/main/java/io/crate/action/job/TransportJobAction.java
+++ sql/src/main/java/io/crate/action/job/TransportJobAction.java
@@ -32,7 +32,6 @@ import io.crate.executor.transport.Transports;
 import io.crate.jobs.JobContextService;
 import io.crate.jobs.JobExecutionContext;
 import io.crate.operation.NodeOperation;
-import io.crate.planner.node.ExecutionPhase;
 import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.common.inject.Singleton;
@@ -47,7 +46,7 @@ import java.util.List;
 public class TransportJobAction implements NodeAction<JobRequest, JobResponse> {
 
     public static final String ACTION_NAME = "crate/sql/job";
-    private static final String EXECUTOR = ThreadPool.Names.SEARCH;
+    private static final String EXECUTOR = ThreadPool.Names.SAME;
 
     private final Transports transports;
     private final JobContextService jobContextService;
