10635dea55acd4bac0d4545472fd983352d4d543
dobe <bernddorn@gmail.com>
2015-08-20 13:51:51 +0200

    fixed stresstests

--- stresstest/src/test/java/io/crate/benchmark/LuceneDocCollectorBenchmark.java
+++ stresstest/src/test/java/io/crate/benchmark/LuceneDocCollectorBenchmark.java
@@ -282,7 +282,7 @@ public class LuceneDocCollectorBenchmark extends BenchmarkBase {
         topNProjector.startProjection(jobCollectContext);
         LuceneDocCollector docCollector = createDocCollector(null, null, topNProjector, ImmutableList.of((Symbol) reference));
         docCollector.doCollect();
-        topNProjector.doFinish();
+        topNProjector.finish();
         collectingProjector.finish();
     }
 
