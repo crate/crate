crate/crate

commit babb0cee5458c19e78e228224fd592ba66dd4d7d

avoid creating new BytesStreamOutput objects in StreamBucket

sql/src/main/java/io/crate/executor/transport/StreamBucket.java
@@ -89,7 +89,7 @@ public class StreamBucket implements Bucket, Streamable {
         }
 
         public void reset() {
-            out = new BytesStreamOutput(INITIAL_PAGE_SIZE);
+            out.reset();
             size = 0;
         }
     }

commit 46eb178ff15f2dc9c0b50a360aa6ea121627ecd4

copy bytes in StreamBucket.Builder build()
babb0cee5458c19e78e228224fd592ba66dd4d7d broke the
StreamBucket.

If the StreamBucket is sent to the same node
writeTo/readFrom aren't executed so the bytes weren't
copied.

And `BytesStreamOutput.reset` resizes the bytesArray if it
grew larger than 1024 which would also result in failures.

sql/src/main/java/io/crate/executor/transport/StreamBucket.java
@@ -89,7 +89,7 @@ public class StreamBucket implements Bucket, Streamable {
         }
 
         public void reset() {
-            out.reset();
+            out = new BytesStreamOutput(size); // next bucket is probably going to have the same size
             size = 0;
         }
     }