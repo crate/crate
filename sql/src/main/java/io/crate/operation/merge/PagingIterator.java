0b25ab1777ee82cc90ecc2a333f53a5783980bc8
Matthias Wahl <matthias@crate.io>
2015-09-08 14:42:04 +0200

    support (cached) repeat in IteratorPageDownstream / PagingIterators

--- sql/src/main/java/io/crate/operation/merge/PagingIterator.java
+++ sql/src/main/java/io/crate/operation/merge/PagingIterator.java
@@ -38,4 +38,10 @@ public interface PagingIterator<T> extends Iterator<T> {
     void finish();
 
     int exhaustedIterable();
+
+    /**
+     * create an iterator to repeat the previous iteration
+     * @return an iterator that will iterate through the already emitted items and emit them again in the same order as before
+     */
+    Iterator<T> repeat();
 }
