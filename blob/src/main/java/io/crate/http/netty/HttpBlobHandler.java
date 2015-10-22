b7d4f9e3e5e4a335888f7c88b10053fa3619429d
Christian Haudum <christian.haudum@gmail.com>
2015-10-22 12:13:24 +0200

    close RandomAccessFile channel when exception occurs

--- blob/src/main/java/io/crate/http/netty/HttpBlobHandler.java
+++ blob/src/main/java/io/crate/http/netty/HttpBlobHandler.java
@@ -303,7 +303,7 @@ public class HttpBlobHandler extends SimpleChannelUpstreamHandler implements
 
     private void partialContentResponse(String range, HttpRequest request, String index, final String digest)
         throws  IOException {
-        assert(range != null);
+        assert range != null : "Getting partial response but no byte-range is not present.";
         Matcher matcher = contentRangePattern.matcher(range);
         if (!matcher.matches()) {
             logger.warn("Invalid byte-range: {}; returning full content", range);
@@ -316,46 +316,66 @@ public class HttpBlobHandler extends SimpleChannelUpstreamHandler implements
         long start;
         long end;
         try {
-            start = Long.parseLong(matcher.group(1));
-            if (start > raf.length()) {
-                logger.warn("416 Requested Range not satisfiable");
-                simpleResponse(HttpResponseStatus.REQUESTED_RANGE_NOT_SATISFIABLE, null);
-                raf.close();
-                return;
+            try {
+                start = Long.parseLong(matcher.group(1));
+                if (start > raf.length()) {
+                    logger.warn("416 Requested Range not satisfiable");
+                    simpleResponse(HttpResponseStatus.REQUESTED_RANGE_NOT_SATISFIABLE, null);
+                    raf.close();
+                    return;
+                }
+                end = raf.length() - 1;
+                if (!matcher.group(2).equals("")) {
+                    end = Long.parseLong(matcher.group(2));
+                }
+            } catch (NumberFormatException ex) {
+                logger.error("Couldn't parse Range Header", ex);
+                start = 0;
+                end = raf.length();
             }
-            end = raf.length() - 1;
-            if (!matcher.group(2).equals("")) {
-                end = Long.parseLong(matcher.group(2));
+
+            HttpResponse response = new DefaultHttpResponse(HTTP_1_1, PARTIAL_CONTENT);
+            HttpHeaders.setContentLength(response, end - start + 1);
+            response.headers().set(CONTENT_RANGE, "bytes " + start + "-" + end + "/" + raf.length());
+            setDefaultGetHeaders(response);
+
+            ctx.getChannel().write(response);
+            ChannelFuture writeFuture = transferFile(digest, raf, start, end - start + 1);
+            if (!HttpHeaders.isKeepAlive(request)) {
+                writeFuture.addListener(ChannelFutureListener.CLOSE);
             }
-        } catch (NumberFormatException ex) {
-            logger.error("Couldn't parse Range Header", ex);
-            start = 0;
-            end = raf.length();
-        }
-
-        HttpResponse response = new DefaultHttpResponse(HTTP_1_1, PARTIAL_CONTENT);
-        HttpHeaders.setContentLength(response, end - start + 1);
-        response.headers().set(CONTENT_RANGE, "bytes " + start + "-" + end + "/" + raf.length());
-        setDefaultGetHeaders(response);
-
-        ctx.getChannel().write(response);
-        ChannelFuture writeFuture = transferFile(digest, raf, start, end - start + 1);
-        if (!HttpHeaders.isKeepAlive(request)) {
-            writeFuture.addListener(ChannelFutureListener.CLOSE);
+        } catch (Throwable t) {
+            /**
+             * Make sure RandomAccessFile is closed when exception is raised.
+             * In case of success, the ChannelFutureListener in "transferFile" will take care
+             * that the resources are released.
+             */
+            raf.close();
+            throw t;
         }
     }
 
     private void fullContentResponse(HttpRequest request, String index, final String digest) throws  IOException {
         BlobShard blobShard = localBlobShard(index, digest);
-        final RandomAccessFile raf = blobShard.blobContainer().getRandomAccessFile(digest);
         HttpResponse response = new DefaultHttpResponse(HTTP_1_1, OK);
-        HttpHeaders.setContentLength(response, raf.length());
-        setDefaultGetHeaders(response);
-        logger.trace("HttpResponse: {}", response);
-        ctx.getChannel().write(response);
-        ChannelFuture writeFuture = transferFile(digest, raf, 0, raf.length());
-        if (!HttpHeaders.isKeepAlive(request)) {
-            writeFuture.addListener(ChannelFutureListener.CLOSE);
+        final RandomAccessFile raf = blobShard.blobContainer().getRandomAccessFile(digest);
+        try {
+            HttpHeaders.setContentLength(response, raf.length());
+            setDefaultGetHeaders(response);
+            logger.trace("HttpResponse: {}", response);
+            ctx.getChannel().write(response);
+            ChannelFuture writeFuture = transferFile(digest, raf, 0, raf.length());
+            if (!HttpHeaders.isKeepAlive(request)) {
+                writeFuture.addListener(ChannelFutureListener.CLOSE);
+            }
+        } catch (Throwable t) {
+            /**
+             * Make sure RandomAccessFile is closed when exception is raised.
+             * In case of success, the ChannelFutureListener in "transferFile" will take care
+             * that the resources are released.
+             */
+            raf.close();
+            throw t;
         }
     }
 
