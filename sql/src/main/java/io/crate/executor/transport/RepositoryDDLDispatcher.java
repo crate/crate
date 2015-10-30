718f4b5bebc7ae50a4c298c551271ac3bfcd6939
Mathias Fussenegger <f.mathias@zignar.net>
2015-10-30 13:42:29 +0100

    include more details in the message of create repository errors

--- sql/src/main/java/io/crate/executor/transport/RepositoryDDLDispatcher.java
+++ sql/src/main/java/io/crate/executor/transport/RepositoryDDLDispatcher.java
@@ -26,15 +26,18 @@ import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.SettableFuture;
 import io.crate.analyze.CreateRepositoryAnalyzedStatement;
 import io.crate.analyze.DropRepositoryAnalyzedStatement;
+import io.crate.exceptions.Exceptions;
 import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.action.admin.cluster.repositories.delete.DeleteRepositoryRequest;
 import org.elasticsearch.action.admin.cluster.repositories.delete.DeleteRepositoryResponse;
 import org.elasticsearch.action.admin.cluster.repositories.put.PutRepositoryRequest;
 import org.elasticsearch.action.admin.cluster.repositories.put.PutRepositoryResponse;
+import org.elasticsearch.common.inject.CreationException;
 import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.common.inject.Singleton;
 import org.elasticsearch.common.logging.ESLogger;
 import org.elasticsearch.common.logging.Loggers;
+import org.elasticsearch.repositories.RepositoryException;
 
 @Singleton
 public class RepositoryDDLDispatcher {
@@ -78,17 +81,45 @@ public class RepositoryDDLDispatcher {
         PutRepositoryRequest request = new PutRepositoryRequest(statement.repositoryName());
         request.type(statement.repositoryType());
         request.settings(statement.settings());
-        transportActionProvider.transportPutRepositoryAction().execute(request, new ActionListener<PutRepositoryResponse>() {
-            @Override
-            public void onResponse(PutRepositoryResponse putRepositoryResponse) {
-                result.set(1L);
-            }
+        transportActionProvider.transportPutRepositoryAction().execute(request, new PutRepositoryResponseActionListener(result));
+        return result;
+    }
 
-            @Override
-            public void onFailure(Throwable e) {
+    static class PutRepositoryResponseActionListener implements ActionListener<PutRepositoryResponse> {
+        private final SettableFuture<Long> result;
+
+        public PutRepositoryResponseActionListener(SettableFuture<Long> result) {
+            this.result = result;
+        }
+
+        @Override
+        public void onResponse(PutRepositoryResponse putRepositoryResponse) {
+            result.set(1L);
+        }
+
+        @Override
+        public void onFailure(Throwable e) {
+            e = Exceptions.unwrap(e);
+            Throwable cause = e.getCause();
+
+            /**
+             * usually the exception looks like:
+             *      RepositoryException
+             *          cause: CreationException (from guice)
+             *                      cause: RepositoryException (with a message that includes the real failure reason
+             *
+             * results in something like: [foo] failed to create repository: [foo] missing location
+             * instead of just: [foo] failed to create repository
+             */
+            if (e instanceof RepositoryException && cause != null) {
+                String msg = e.getMessage();
+                if (cause instanceof CreationException && cause.getCause() != null) {
+                    msg += ": " + cause.getCause().getMessage();
+                }
+                result.setException(new RepositoryException("", msg, e));
+            } else {
                 result.setException(e);
             }
-        });
-        return result;
+        }
     }
 }
