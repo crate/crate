ef3d92707f2f77e2e91f1d21e8a66b06c5229d68
Mathias Fussenegger <f.mathias@zignar.net>
2016-01-11 17:59:44 +0100

    adapt blob module

--- blob/src/main/java/io/crate/blob/v2/BlobIndex.java
+++ blob/src/main/java/io/crate/blob/v2/BlobIndex.java
@@ -22,12 +22,12 @@
 package io.crate.blob.v2;
 
 import org.elasticsearch.cluster.ClusterService;
-import org.elasticsearch.cluster.routing.ShardIterator;
 import org.elasticsearch.cluster.routing.OperationRouting;
+import org.elasticsearch.cluster.routing.ShardIterator;
 import org.elasticsearch.common.inject.Inject;
-import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.index.AbstractIndexComponent;
 import org.elasticsearch.index.Index;
+import org.elasticsearch.index.settings.IndexSettingsService;
 import org.elasticsearch.index.shard.ShardId;
 
 public class BlobIndex extends AbstractIndexComponent {
@@ -37,10 +37,9 @@ public class BlobIndex extends AbstractIndexComponent {
     private final ClusterService clusterService;
 
     @Inject
-    BlobIndex(Index index, Settings indexSettings,
-            OperationRouting operationRouting,
-            ClusterService clusterService) {
-        super(index, indexSettings);
+    BlobIndex(Index index, IndexSettingsService indexSettingsService,
+              OperationRouting operationRouting, ClusterService clusterService) {
+        super(index, indexSettingsService.getSettings());
         this.operationRouting = operationRouting;
         this.clusterService = clusterService;
     }
