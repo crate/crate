a9058c11284d18a04e41e7f0ef06ae73d92b0f05
Mathias Fussenegger <f.mathias@zignar.net>
2016-01-24 20:15:21 +0100

    use assertNormalize in FloorFunctionTest

--- sql/src/test/java/io/crate/operation/scalar/arithmetic/FloorFunctionTest.java
+++ sql/src/test/java/io/crate/operation/scalar/arithmetic/FloorFunctionTest.java
@@ -21,75 +21,39 @@
 
 package io.crate.operation.scalar.arithmetic;
 
-import io.crate.analyze.symbol.Function;
-import io.crate.analyze.symbol.Literal;
-import io.crate.analyze.symbol.Reference;
-import io.crate.analyze.symbol.Symbol;
-import io.crate.metadata.FunctionIdent;
-import io.crate.operation.Input;
 import io.crate.operation.scalar.AbstractScalarFunctionsTest;
-import io.crate.testing.TestingHelpers;
-import io.crate.types.DataType;
 import io.crate.types.DataTypes;
-import org.hamcrest.Matchers;
 import org.junit.Test;
 
-import java.util.Arrays;
-
+import static io.crate.testing.TestingHelpers.isFunction;
 import static io.crate.testing.TestingHelpers.isLiteral;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.nullValue;
 
 public class FloorFunctionTest extends AbstractScalarFunctionsTest {
 
-    private FloorFunction getFunction(DataType type) {
-        return (FloorFunction) functions.get(new FunctionIdent(FloorFunction.NAME, Arrays.asList(type)));
-    }
-
-    private Number evaluate(Number number, DataType type) {
-        return getFunction(type).evaluate((Input) Literal.newLiteral(type, number));
-    }
-
-    private Symbol normalize(Number number, DataType type) {
-        FloorFunction function = getFunction(type);
-        return function.normalizeSymbol(new Function(function.info(),
-                Arrays.<Symbol>asList(Literal.newLiteral(type, number))));
-    }
-
     @Test
     public void testEvaluateOnDouble() throws Exception {
-        assertThat((Long) evaluate(29.9, DataTypes.DOUBLE), is(29L));
-        assertThat(evaluate(null, DataTypes.DOUBLE), nullValue());
+        assertNormalize("floor(29.9)", isLiteral(29L));
+        assertNormalize("floor(cast(null as double))", isLiteral(null, DataTypes.LONG));
     }
 
     @Test
     public void testEvaluateOnFloat() throws Exception {
-        assertThat((Integer) evaluate(29.9f, DataTypes.FLOAT), is(29));
-        assertThat(evaluate(null, DataTypes.FLOAT), nullValue());
+        assertNormalize("floor(cast(29.9 as float))", isLiteral(29));
+        assertNormalize("floor(cast(null as float))", isLiteral(null, DataTypes.INTEGER));
     }
 
     @Test
     public void testEvaluateOnIntAndLong() throws Exception {
-        assertThat((Integer) evaluate(20, DataTypes.INTEGER), is(20));
-        assertThat(evaluate(null, DataTypes.INTEGER), nullValue());
-        assertThat((Long) evaluate(20L, DataTypes.LONG), is(20L));
-        assertThat(evaluate(null, DataTypes.LONG), nullValue());
-    }
+        assertNormalize("floor(cast(20 as integer))", isLiteral(20));
+        assertNormalize("floor(cast(null as integer))", isLiteral(null, DataTypes.INTEGER));
+        assertNormalize("floor(42.9)", isLiteral(42L));
 
-    @Test
-    public void testNormalizeValueSymbol() throws Exception {
-        assertThat(normalize(42.9, DataTypes.DOUBLE), isLiteral(42L));
-        assertThat(normalize(42.9f, DataTypes.FLOAT), isLiteral(42));
-        assertThat(normalize(null, DataTypes.FLOAT), isLiteral(null, DataTypes.INTEGER));
+        assertNormalize("floor(20)", isLiteral(20L));
+        assertNormalize("floor(cast(null as long))", isLiteral(null, DataTypes.LONG));
     }
 
     @Test
     public void testNormalizeReference() throws Exception {
-        Reference height = TestingHelpers.createReference("height", DataTypes.DOUBLE);
-        FloorFunction floor = getFunction(DataTypes.DOUBLE);
-        Function function = new Function(floor.info(), Arrays.<Symbol>asList(height));
-        Function normalized = (Function) floor.normalizeSymbol(function);
-        assertThat(normalized, Matchers.sameInstance(function));
+        assertNormalize("floor(age)", isFunction(FloorFunction.NAME));
     }
-
 }
