c42c672e69fc10c0792f9fceefb35451151b3a2c
Mathias Fussenegger <f.mathias@zignar.net>
2016-04-13 11:10:00 +0200

    bind ContextPreparer and LuceneQueryBuilder as asEagerSingleton
    There was some kind of race condition which could cause the
    LuceneQueryBuilder to be bound in the shardInjector instead of the
    nodeInjector.
    
    Which would then cause guice injection errors once the
    LuceneQueryBuilder was accessed on a node level.

--- sql/src/main/java/io/crate/executor/transport/TransportExecutorModule.java
+++ sql/src/main/java/io/crate/executor/transport/TransportExecutorModule.java
@@ -21,11 +21,13 @@
 
 package io.crate.executor.transport;
 
+import io.crate.action.job.ContextPreparer;
 import io.crate.action.job.TransportJobAction;
 import io.crate.action.job.TransportKeepAliveAction;
 import io.crate.executor.Executor;
 import io.crate.executor.transport.distributed.TransportDistributedResultAction;
 import io.crate.executor.transport.kill.TransportKillAllNodeAction;
+import io.crate.lucene.LuceneQueryBuilder;
 import org.elasticsearch.common.inject.AbstractModule;
 
 public class TransportExecutorModule extends AbstractModule {
@@ -34,6 +36,8 @@ public class TransportExecutorModule extends AbstractModule {
     protected void configure() {
         bind(TransportActionProvider.class).asEagerSingleton();
         bind(Executor.class).to(TransportExecutor.class).asEagerSingleton();
+        bind(ContextPreparer.class).asEagerSingleton();
+        bind(LuceneQueryBuilder.class).asEagerSingleton();
 
         bind(TransportJobAction.class).asEagerSingleton();
         bind(TransportKeepAliveAction.class).asEagerSingleton();
