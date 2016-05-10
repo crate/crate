56194417580a87860bca7743fd2e313d9a32bb4b
Mathias Fussenegger <f.mathias@zignar.net>
2016-05-10 13:48:03 +0200

    treat sphinx warnings as error and run it on travis

--- blackbox/docs/conf.py
+++ blackbox/docs/conf.py
@@ -1,3 +1,7 @@
 from crate.theme.rtd.conf.crate_server import *
 
 exclude_patterns = ['out/**', 'clients/**', 'tmp/**', 'eggs/**', 'requirements.txt']
+
+# crate.theme sets html_favicon to favicon.png which causes a warning because it should be a .ico
+# and in addition there is no favicon.png in this project so it can't find the file
+html_favicon = None
