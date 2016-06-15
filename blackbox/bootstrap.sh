6afe00abd11c5760347014381ddac7fd227afcec
Mathias Fussenegger <f.mathias@zignar.net>
2016-06-15 22:04:33 +0200

    Replace buildout with virtualenv/pip install
    This should speed up the doctests from a clean checkout a bit.
    Pip can utilize wheels which is faster than how buildout installs
    packages.
    
    Before:
    
        195.64s user 4.90s system 140% cpu 2:22.88 total
    
    With this change:
    
        134.22s user 3.65s system 235% cpu 58.516 total

--- blackbox/bootstrap.sh
+++ blackbox/bootstrap.sh
@@ -2,7 +2,9 @@
 
 # travis and jenkins have python3.4 installed
 if hash python3.4 2> /dev/null; then
-    python3.4 -m venv .venv
+    python3.4 -m venv .venv --without-pip
+    wget https://bootstrap.pypa.io/get-pip.py
+    .venv/bin/python get-pip.py
 elif hash python3 2> /dev/null; then
     # fallback for dev machines, this should be >= 3.3
     python3 -m venv .venv
@@ -11,4 +13,4 @@ else
     exit 1
 fi
 
-.venv/bin/python bootstrap.py
+.venv/bin/python -m pip install -r requirements.txt
