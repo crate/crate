c7c4def66b86ff64c5844a3374e178a3b96253aa
Sebastian Utz <su@rtme.net>
2016-06-28 11:07:49 +0200

    implements numeric PG types

---
+++ sql/src/test/java/io/crate/protocols/postgres/types/PGTypesTest.java
@@ -0,0 +1,56 @@
+/*
+ * Licensed to Crate under one or more contributor license agreements.
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.  Crate licenses this file
+ * to you under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.  You may
+ * obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.  See the License for the specific language governing
+ * permissions and limitations under the License.
+ *
+ * However, if you have executed another commercial license agreement
+ * with Crate these terms will supersede the license and you may use the
+ * software solely pursuant to the terms of the relevant commercial
+ * agreement.
+ */
+
+package io.crate.protocols.postgres.types;
+
+import io.crate.types.*;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.instanceOf;
+
+public class PGTypesTest {
+
+    @Test
+    public void testCrate2PGType() throws Exception {
+        assertThat(PGTypes.CRATE_TO_PG_TYPES.get(DataTypes.STRING), instanceOf(VarCharType.class));
+        assertThat(PGTypes.CRATE_TO_PG_TYPES.get(DataTypes.OBJECT), instanceOf(JsonType.class));
+        assertThat(PGTypes.CRATE_TO_PG_TYPES.get(DataTypes.BOOLEAN), instanceOf(BooleanType.class));
+        assertThat(PGTypes.CRATE_TO_PG_TYPES.get(DataTypes.SHORT), instanceOf(SmallIntType.class));
+        assertThat(PGTypes.CRATE_TO_PG_TYPES.get(DataTypes.INTEGER), instanceOf(IntegerType.class));
+        assertThat(PGTypes.CRATE_TO_PG_TYPES.get(DataTypes.LONG), instanceOf(BigIntType.class));
+        assertThat(PGTypes.CRATE_TO_PG_TYPES.get(DataTypes.FLOAT), instanceOf(RealType.class));
+        assertThat(PGTypes.CRATE_TO_PG_TYPES.get(DataTypes.DOUBLE), instanceOf(DoubleType.class));
+    }
+
+    @Test
+    public void testPG2CrateType() throws Exception {
+        assertThat(PGTypes.fromOID(VarCharType.OID), instanceOf(StringType.class));
+        assertThat(PGTypes.fromOID(JsonType.OID), instanceOf(ObjectType.class));
+        assertThat(PGTypes.fromOID(BooleanType.OID), instanceOf(io.crate.types.BooleanType.class));
+        assertThat(PGTypes.fromOID(SmallIntType.OID), instanceOf(ShortType.class));
+        assertThat(PGTypes.fromOID(IntegerType.OID), instanceOf(io.crate.types.IntegerType.class));
+        assertThat(PGTypes.fromOID(BigIntType.OID), instanceOf(LongType.class));
+        assertThat(PGTypes.fromOID(RealType.OID), instanceOf(FloatType.class));
+        assertThat(PGTypes.fromOID(DoubleType.OID), instanceOf(io.crate.types.DoubleType.class));
+    }
+}
