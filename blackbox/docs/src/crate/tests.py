709b613aa1d1569eb5fc9254b98bee5c1c3f3ee6
Marios Trivyzas <marios@crate.io>
2016-07-03 18:28:43 +0200

    Added documentation for least/greatest conditional functions

--- blackbox/docs/src/crate/tests.py
+++ blackbox/docs/src/crate/tests.py
@@ -416,6 +416,7 @@ def test_suite():
                'sql/partitioned_tables.txt',
                'sql/aggregation.txt',
                'sql/arithmetic.txt',
+               'sql/conditional.txt',
                'sql/scalar.txt',
                'sql/table_functions.txt',
                'sql/system.txt',
