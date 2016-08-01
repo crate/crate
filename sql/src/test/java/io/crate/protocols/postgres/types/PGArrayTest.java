0ab3c31ce8ed9b198f36ebd58ad1a13eeadbca96
Marios Trivyzas <marios@crate.io>
2016-08-01 11:24:44 +0200

    Implement n-dimension arrays

--- sql/src/test/java/io/crate/protocols/postgres/types/PGArrayTest.java
+++ sql/src/test/java/io/crate/protocols/postgres/types/PGArrayTest.java
@@ -23,6 +23,7 @@
 package io.crate.protocols.postgres.types;
 
 import org.hamcrest.Matchers;
+import org.hamcrest.core.Is;
 import org.junit.Test;
 
 import java.nio.charset.StandardCharsets;
@@ -38,10 +39,15 @@ public class PGArrayTest {
 
     @Test
     public void testEncodeUTF8Text() throws Exception {
+        // 1-dimension array
         byte[] bytes = pgArray.encodeAsUTF8Text(new Object[] { 10, 20 });
         String s = new String(bytes, StandardCharsets.UTF_8);
-
         assertThat(s, is("{\"10\",\"20\"}"));
+
+        // 3-dimension array
+        bytes = pgArray.encodeAsUTF8Text(new Object[][][] {{{1, 2}, {3, 4}}, {{5, 6}, {7}}});
+        s = new String(bytes, StandardCharsets.UTF_8);
+        assertThat(s, is("{{{\"1\",\"2\"},{\"3\",\"4\"}},{{\"5\",\"6\"},{\"7\"}}}"));
     }
 
     @Test
@@ -57,32 +63,87 @@ public class PGArrayTest {
     @SuppressWarnings("unchecked")
     @Test
     public void testJsonArrayEncodeDecode() throws Exception {
+        // 1-dimension array
+        // Decode
         String s = "{\"{\"names\":[\"Arthur\",\"Trillian\"]}\",\"{\"names\":[\"Ford\",\"Slarti\"]}\"}";
         Object[] values = (Object[]) PGArray.JSON_ARRAY.decodeUTF8Text(s.getBytes(StandardCharsets.UTF_8));
 
-        List<String> firstNames = (List<String>) ((Map) values[0]).get("names");
-        assertThat(firstNames, Matchers.contains("Arthur", "Trillian"));
+        List<String> names = (List<String>) ((Map) values[0]).get("names");
+        assertThat(names, Matchers.contains("Arthur", "Trillian"));
 
-        List<String> secondNames = (List<String>) ((Map) values[1]).get("names");
-        assertThat(secondNames, Matchers.contains("Ford", "Slarti"));
+        names = (List<String>) ((Map) values[1]).get("names");
+        assertThat(names, Matchers.contains("Ford", "Slarti"));
+
+        // Encode
+        byte[] bytes = PGArray.JSON_ARRAY.encodeAsUTF8Text(values);
+        s = new String(bytes, StandardCharsets.UTF_8);
+        assertThat(s, is(s));
+
+        // 3-dimension array
+        // Decode
+        s = "{{{\"{\"names\":[\"A\",\"B\"]}\",\"{\"names\":[\"C\",\"D\"]}\"}," +
+              "{\"{\"names\":[\"E\",\"F\"]}\",\"{\"names\":[\"G\",\"H\"]}\"}}," +
+             "{{\"{\"names\":[\"I\",\"J\"]}\",\"{\"names\":[\"K\",\"L\"]}\"}," +
+              "{\"{\"names\":[\"M\",\"N\"]}\",\"{\"names\":[\"O\",\"P\"]}\"}}}";
+        values = (Object[]) PGArray.JSON_ARRAY.decodeUTF8Text(s.getBytes(StandardCharsets.UTF_8));
+
+        names = (List<String>) ((Map) ((Object[])((Object[])values[0])[0])[0]).get("names");
+        assertThat(names, Matchers.contains("A", "B"));
+        names = (List<String>) ((Map) ((Object[])((Object[])values[0])[0])[1]).get("names");
+        assertThat(names, Matchers.contains("C", "D"));
+
+        names = (List<String>) ((Map) ((Object[])((Object[])values[0])[1])[0]).get("names");
+        assertThat(names, Matchers.contains("E", "F"));
+        names = (List<String>) ((Map) ((Object[])((Object[])values[0])[1])[1]).get("names");
+        assertThat(names, Matchers.contains("G", "H"));
+
+        names = (List<String>) ((Map) ((Object[])((Object[])values[1])[0])[0]).get("names");
+        assertThat(names, Matchers.contains("I", "J"));
+        names = (List<String>) ((Map) ((Object[])((Object[])values[1])[0])[1]).get("names");
+        assertThat(names, Matchers.contains("K", "L"));
+
+        names = (List<String>) ((Map) ((Object[])((Object[])values[1])[1])[0]).get("names");
+        assertThat(names, Matchers.contains("M", "N"));
+        names = (List<String>) ((Map) ((Object[])((Object[])values[1])[1])[1]).get("names");
+        assertThat(names, Matchers.contains("O", "P"));
+
+        // Encode
+        bytes = PGArray.JSON_ARRAY.encodeAsUTF8Text(values);
+        s = new String(bytes, StandardCharsets.UTF_8);
+        assertThat(s, is(s));
     }
 
     @Test
     @SuppressWarnings("unchecked")
     public void testDecodeEncodeEscapedJson() throws Exception {
+        // Decode
         String s = "{\"{\\\"names\\\":[\\\"Arthur\\\",\\\"Trillian\\\"]}\",\"{\\\"names\\\":[\\\"Ford\\\",\\\"Slarti\\\"]}\"}";
         Object[] values = (Object[]) PGArray.JSON_ARRAY.decodeUTF8Text(s.getBytes(StandardCharsets.UTF_8));
 
-        List<String> firstNames = (List<String>) ((Map) values[0]).get("names");
-        assertThat(firstNames, Matchers.contains("Arthur", "Trillian"));
+        List<String> names = (List<String>) ((Map) values[0]).get("names");
+        assertThat(names, Matchers.contains("Arthur", "Trillian"));
 
-        List<String> secondNames = (List<String>) ((Map) values[1]).get("names");
-        assertThat(secondNames, Matchers.contains("Ford", "Slarti"));
+        names = (List<String>) ((Map) values[1]).get("names");
+        assertThat(names, Matchers.contains("Ford", "Slarti"));
+
+        // Encode
+        byte[] bytes = PGArray.JSON_ARRAY.encodeAsUTF8Text(values);
+        s = new String(bytes, StandardCharsets.UTF_8);
+        assertThat(s, is("{\"{\"names\":[\"Arthur\",\"Trillian\"]}\",\"{\"names\":[\"Ford\",\"Slarti\"]}\"}"));
     }
 
     @Test
     public void testDecodeUTF8Text() throws Exception {
+        // 1-dimension array
         Object o = pgArray.decodeUTF8Text("{\"10\",\"20\"}".getBytes(StandardCharsets.UTF_8));
-        assertThat((Object[]) o, is(new Object[] { 10, 20}));
+        assertThat((Object[]) o, is(new Object[] {10, 20}));
+
+        // 2-dimension array
+        o = pgArray.decodeUTF8Text("{{\"1\",NULL,\"2\"},{NULL,\"3\",\"4\"}}".getBytes(StandardCharsets.UTF_8));
+        assertThat(((Object[]) o), Is.<Object[]>is(new Object[][] {{1, null, 2}, {null, 3, 4}}));
+
+        // 3-dimension array
+        o = pgArray.decodeUTF8Text("{{{\"1\",NULL,\"2\"},{NULL,\"3\",\"4\"}},{{\"5\",NULL,\"6\"},{\"7\"}}".getBytes(StandardCharsets.UTF_8));
+        assertThat(((Object[]) o), Is.<Object[]>is(new Object[][][] {{{1, null, 2}, {null, 3, 4}}, {{5, null, 6}, {7}}}));
     }
 }
