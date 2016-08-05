f3ac9f24826dcfc151304d70d435b6eb1feb2ef8
Philipp Bogensberger <philipp@crate.io>
2016-08-05 11:06:51 +0200

    prepare release 0.55.3

--- core/src/main/java/io/crate/Version.java
+++ core/src/main/java/io/crate/Version.java
@@ -35,7 +35,7 @@ public class Version {
 
 
     public static final boolean SNAPSHOT = false;
-    public static final Version CURRENT = new Version(550299, SNAPSHOT, org.elasticsearch.Version.V_2_3_4);
+    public static final Version CURRENT = new Version(550399, SNAPSHOT, org.elasticsearch.Version.V_2_3_4);
 
     static {
         // safe-guard that we don't release a version with DEBUG_MODE set to true
