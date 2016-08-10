dab4ae861d9d9e588c9878e933443ea0b7eb4210
Michael Beer <michael@crate.io>
2016-08-10 11:33:00 +0200

    added supported features table to doc

--- blackbox/docs/conf.py
+++ blackbox/docs/conf.py
@@ -2,6 +2,7 @@ from crate.theme.rtd.conf.crate_server import *
 
 exclude_patterns = ['out/**', 'clients/**', 'tmp/**', 'eggs/**', 'requirements.txt']
 
+extensions = ['crate.sphinx.csv']
 # crate.theme sets html_favicon to favicon.png which causes a warning because it should be a .ico
 # and in addition there is no favicon.png in this project so it can't find the file
 html_favicon = None
