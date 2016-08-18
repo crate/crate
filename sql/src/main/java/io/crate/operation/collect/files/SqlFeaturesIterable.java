38cd85b6175e4eba16f62d149e309cdb39f19eac
Michael Beer <michael@crate.io>
2016-08-18 18:04:47 +0200

    setup filesystem implicitly to load ressource out of JAR file

--- sql/src/main/java/io/crate/operation/collect/files/SqlFeaturesIterable.java
+++ sql/src/main/java/io/crate/operation/collect/files/SqlFeaturesIterable.java
@@ -25,25 +25,15 @@ package io.crate.operation.collect.files;
 import com.google.common.base.Splitter;
 import org.elasticsearch.ResourceNotFoundException;
 
-import javax.annotation.Nullable;
 import java.io.*;
-import java.net.URISyntaxException;
-import java.net.URL;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 public class SqlFeaturesIterable implements Iterable<SqlFeatureContext> {
 
     private static SqlFeaturesIterable instance = null;
     private static List<SqlFeatureContext> featuresList;
     private static final Splitter TAB_SPLITTER = Splitter.on("\t");
-    private static final URL SQL_FEATURES = SqlFeaturesIterable.class.getResource("/sql_features.tsv");
-    private BufferedReader reader;
 
     public static SqlFeaturesIterable getInstance() {
         if (instance == null) {
@@ -53,58 +43,31 @@ public class SqlFeaturesIterable implements Iterable<SqlFeatureContext> {
     }
 
     private SqlFeaturesIterable() {
-        if (SQL_FEATURES == null) {
-            throw new ResourceNotFoundException("sql_features.tsv file not found");
-        }
         loadContextList();
     }
 
     private void loadContextList() {
-        featuresList = new ArrayList<>();
-        Path features = null;
-        try {
-            features = Paths.get(SQL_FEATURES.toURI());
-        } catch (URISyntaxException e) {
-        }
-        reader = getReader(features);
-        SqlFeatureContext ctx;
-        String next;
-        while ((next = nextLine()) != null) {
-            List<String> parts = TAB_SPLITTER.splitToList(next);
-            ctx = SqlFeatureContext.builder()
-                    .featureId(parts.get(0))
-                    .featureName(parts.get(1))
-                    .subFeatureId(parts.get(2))
-                    .subFeatureName(parts.get(3))
-                    .isSupported(parts.get(4).equals("YES"))
-                    .isVerifiedBy(parts.get(5).isEmpty() ? null : parts.get(5))
-                    .comments(parts.get(6).isEmpty() ? null : parts.get(6))
-                    .build();
-            featuresList.add(ctx);
-        }
-        close();
-    }
-
-    private BufferedReader getReader(Path features) {
-        try {
-            return Files.newBufferedReader(features, StandardCharsets.UTF_8);
-        } catch (IOException e) {
-            return null;
-        }
-    }
-
-    @Nullable
-    private String nextLine() {
-        try {
-            return reader.readLine();
-        } catch (IOException e) {
-            return null;
-        }
-    }
-
-    private void close() {
-        try {
-            reader.close();
+        try (InputStream sqlFeatures = SqlFeaturesIterable.class.getResourceAsStream("/sql_features.tsv");
+             BufferedReader reader = new BufferedReader(new InputStreamReader(sqlFeatures, StandardCharsets.UTF_8))) {
+            if (sqlFeatures == null) {
+                throw new ResourceNotFoundException("sql_features.tsv file not found");
+            }
+            featuresList = new ArrayList<>();
+            SqlFeatureContext ctx;
+            String next;
+            while ((next = reader.readLine()) != null) {
+                List<String> parts = TAB_SPLITTER.splitToList(next);
+                ctx = SqlFeatureContext.builder()
+                        .featureId(parts.get(0))
+                        .featureName(parts.get(1))
+                        .subFeatureId(parts.get(2))
+                        .subFeatureName(parts.get(3))
+                        .isSupported(parts.get(4).equals("YES"))
+                        .isVerifiedBy(parts.get(5).isEmpty() ? null : parts.get(5))
+                        .comments(parts.get(6).isEmpty() ? null : parts.get(6))
+                        .build();
+                featuresList.add(ctx);
+            }
         } catch (IOException e) {
         }
     }
