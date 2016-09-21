a6d7ab59e3ba3953e6a2a5b3b1faab1ee01006a1
Marios Trivyzas <marios@crate.io>
2016-09-21 11:04:22 +0200

    Implemented Analysis for UNION

--- sql/src/main/java/io/crate/analyze/Analyzer.java
+++ sql/src/main/java/io/crate/analyze/Analyzer.java
@@ -128,7 +128,7 @@ public class Analyzer {
 
         @Override
         protected AnalyzedStatement visitQuery(Query node, Analysis analysis) {
-            AnalyzedRelation relation = relationAnalyzer.analyze(node.getQueryBody(), analysis);
+            AnalyzedRelation relation = relationAnalyzer.analyze(node, analysis);
             analysis.rootRelation(relation);
             return new SelectAnalyzedStatement((QueriedRelation) relation);
         }
