78d7cdf932c4db00a7da4224fa1c993571a04be7
Mathias Fussenegger <f.mathias@zignar.net>
2016-10-14 21:43:20 +0200

    Clean up array_unique & clarify cast behaviour

--- sql/src/test/java/io/crate/operation/scalar/ArrayUniqueFunctionTest.java
+++ sql/src/test/java/io/crate/operation/scalar/ArrayUniqueFunctionTest.java
@@ -22,201 +22,101 @@
 
 package io.crate.operation.scalar;
 
-import io.crate.analyze.symbol.Function;
 import io.crate.analyze.symbol.Literal;
-import io.crate.analyze.symbol.Symbol;
-import io.crate.metadata.FunctionIdent;
-import io.crate.metadata.Scalar;
-import io.crate.metadata.TransactionContext;
-import io.crate.operation.Input;
-import io.crate.testing.TestingHelpers;
 import io.crate.types.ArrayType;
-import io.crate.types.DataType;
 import io.crate.types.DataTypes;
 import org.apache.lucene.util.BytesRef;
-import org.hamcrest.Matchers;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
+import static io.crate.testing.SymbolMatchers.isFunction;
 import static io.crate.testing.SymbolMatchers.isLiteral;
-import static org.hamcrest.Matchers.is;
 
 public class ArrayUniqueFunctionTest extends AbstractScalarFunctionsTest {
 
-    private static final ArrayType arrayOfIntegerType = new ArrayType(DataTypes.INTEGER);
-    private static final ArrayType arrayOfLongType = new ArrayType(DataTypes.LONG);
-    private static final ArrayType arrayOfStringType = new ArrayType(DataTypes.STRING);
-    private static final ArrayType arrayOfBooleanType = new ArrayType(DataTypes.BOOLEAN);
-    private static final ArrayType arrayOfIpType = new ArrayType(DataTypes.IP);
-    private static final ArrayType arrayOfUndefinedType = new ArrayType(DataTypes.UNDEFINED);
-
-    private ArrayUniqueFunction getFunction(ArrayType... args) {
-        List<DataType> argumentTypes = new ArrayList<>(args.length);
-        for (int i = 0; i < args.length; i++) {
-            argumentTypes.add(args[i]);
-        }
-        ArrayUniqueFunction function = ((ArrayUniqueFunction) functions.get(new FunctionIdent(ArrayUniqueFunction.NAME, argumentTypes)));
-        return function;
-    }
-
-    private void assertEval(Object[] expected, Literal... args) {
-        List<DataType> argumentTypes = new ArrayList<>(args.length);
-        Input[] inputs = new Input[args.length];
-        for (int i = 0; i < args.length; i++) {
-            inputs[i] = args[i];
-            argumentTypes.add(args[i].valueType());
-        }
-        Scalar scalar = ((Scalar) functions.get(new FunctionIdent(ArrayUniqueFunction.NAME, argumentTypes)));
-        Object[] evaluate = (Object[]) scalar.evaluate(inputs);
-        assertThat(evaluate, is(expected));
-    }
-
     @Test
     public void testNormalizeWithValueSymbols() throws Exception {
-        ArrayUniqueFunction function = getFunction(arrayOfIntegerType, arrayOfIntegerType);
-
-        Symbol symbol = function.normalizeSymbol(new Function(function.info(), Arrays.<Symbol>asList(
-            Literal.of(new Integer[]{10, 20}, arrayOfIntegerType),
-            Literal.of(new Integer[]{10, 30}, arrayOfIntegerType)
-        )), new TransactionContext());
-
-        assertThat(symbol, isLiteral(new Integer[]{10, 20, 30}, arrayOfIntegerType));
+        assertNormalize("array_unique([10, 20], [10, 30])", isLiteral(new Long[] {10L, 20L, 30L}));
     }
 
     @Test
     public void testNormalizeWithRefs() throws Exception {
-        ArrayUniqueFunction function = getFunction(arrayOfIntegerType, arrayOfIntegerType);
-
-        Function functionSymbol = new Function(function.info(), Arrays.<Symbol>asList(
-            TestingHelpers.createReference("foo", arrayOfIntegerType),
-            Literal.of(new Integer[]{10, 30}, arrayOfIntegerType)
-        ));
-        Function symbol = (Function) function.normalizeSymbol(functionSymbol, new TransactionContext());
-        assertThat(symbol, Matchers.sameInstance(functionSymbol));
+        assertNormalize("array_unique(long_array, [10, 30])", isFunction("array_unique"));
     }
 
     @Test
     public void testNullArguments() throws Exception {
-        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expect(UnsupportedOperationException.class);
         expectedException.expectMessage("Argument 2 of the array_unique function cannot be converted to array");
-        assertEval(null, Literal.of(new Object[]{1}, arrayOfIntegerType), Literal.NULL);
+        assertEvaluate("array_unique([1], null)", null);
     }
 
     @Test
-    public void testNullArgumentsWithoutCheckingForArrays() throws Exception {
-        ArrayUniqueFunction function = getFunction(arrayOfIntegerType, arrayOfIntegerType);
-
-        Input[] inputs = new Input[]{
-            null,
-            Literal.NULL
-        };
-
-        Object[] expected = new Object[]{};
-        Object[] evaluate = function.evaluate(inputs);
-        assertThat(evaluate, is(expected));
+    public void testRefWithNullValue() throws Exception {
+        assertEvaluate("array_unique([1], long_array)",
+            new Long[] {1L},
+            Literal.of(new ArrayType(DataTypes.LONG), null));
     }
 
     @Test
     public void testZeroArguments() throws Exception {
-        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expect(UnsupportedOperationException.class);
         expectedException.expectMessage("array_unique function requires one or two arguments");
-        assertEval(null);
+        assertEvaluate("array_unique()", null);
     }
 
     @Test
     public void testOneArgument() throws Exception {
-        assertEval(
-            new Object[]{new BytesRef("foo"), new BytesRef("bar"), new BytesRef("baz")},
-            Literal.of(new Object[]{"foo", "bar", "baz", "baz"}, arrayOfStringType));
+        assertEvaluate("array_unique(['foo', 'bar', 'baz', 'baz'])",
+            new BytesRef[]{
+                new BytesRef("foo"),
+                new BytesRef("bar"),
+                new BytesRef("baz")
+            }
+        );
     }
 
     @Test
     public void testDifferentButConvertableInnerTypes() throws Exception {
-        assertEval(
-            new Object[]{1},
-            Literal.of(new Object[]{1}, arrayOfIntegerType),
-            Literal.of(new Object[]{1L}, arrayOfLongType));
+        assertEvaluate("array_unique([10, 20], [10.1, 20.0])", new Long[] {10L, 20L });
     }
 
     @Test
     public void testConvertNonNumericStringToNumber() throws Exception {
         expectedException.expect(NumberFormatException.class);
-        assertEval(
-            null,
-            Literal.of(new Object[]{1}, arrayOfIntegerType),
-            Literal.of(new Object[]{"foo", "bar"}, arrayOfStringType));
+        assertEvaluate("array_unique([10, 20], ['foo', 'bar'])", null);
     }
 
     @Test
     public void testDifferentUnconvertableInnerTypes() throws Exception {
-        expectedException.expect(IllegalArgumentException.class);
-        expectedException.expectMessage("Second argument's inner type (ip) of the array_unique function cannot be converted to the first argument's inner type (boolean)");
-        assertEval(
-            null,
-            Literal.of(new Object[]{true}, arrayOfBooleanType),
-            Literal.of(new Object[]{new BytesRef("127.0.0.1")}, arrayOfIpType));
-
+        expectedException.expect(UnsupportedOperationException.class);
+        expectedException.expectMessage("Second argument's inner type (boolean) of the array_unique function " +
+                                        "cannot be converted to the first argument's inner type (geo_point)");
+        assertEvaluate("array_unique([geopoint], [true])", null);
     }
 
     @Test
     public void testNullElements() throws Exception {
-        assertEval(
-            new Object[]{1, null, 3, 2},
-            Literal.of(new Object[]{1, null, 3}, arrayOfIntegerType),
-            Literal.of(new Object[]{null, 2, 3}, arrayOfIntegerType));
+        assertEvaluate("array_unique([1, null, 3], [null, 2, 3])", new Long[] { 1L, null, 3L, 2L });
     }
 
     @Test
-    public void testTwoIntegerArguments() throws Exception {
-        assertEval(
-            new Object[]{1, 2, 3},
-            Literal.of(new Object[]{1, 2}, arrayOfIntegerType),
-            Literal.of(new Object[]{2, 3}, arrayOfIntegerType));
+    public void testEmptyArrayAndLongArray() throws Exception {
+        assertEvaluate("array_unique([], [111, 222, 333])", new Long[] {111L, 222L, 333L});
     }
-
-    @Test
-    public void testTwoLongArguments() throws Exception {
-        assertEval(
-            new Object[]{44L, 55L, 66L},
-            Literal.of(new Object[]{44L, 55L}, arrayOfLongType),
-            Literal.of(new Object[]{55L, 66L}, arrayOfLongType));
-    }
-
-    @Test
-    public void testTwoStringArguments() throws Exception {
-        assertEval(
-            new Object[]{new BytesRef("foo"), new BytesRef("bar"), new BytesRef("baz")},
-            Literal.of(new Object[]{"foo", "bar"}, arrayOfStringType),
-            Literal.of(new Object[]{"bar", "baz"}, arrayOfStringType));
-    }
-
-    @Test
-    public void testEmptyArrayAndIntegerArray() throws Exception {
-        assertEval(
-            new Object[]{111, 222, 333},
-            Literal.of(new Object[]{}, arrayOfUndefinedType),
-            Literal.of(new Object[]{111, 222, 333}, arrayOfIntegerType));
-    }
-
 
     @Test
     public void testEmptyArrays() throws Exception {
-        expectedException.expect(IllegalArgumentException.class);
-        expectedException.expectMessage("One of the arguments of the array_unique function can be of undefined inner type, but not both");
-        assertEval(new Object[]{},
-            Literal.of(new Object[]{}, arrayOfUndefinedType),
-            Literal.of(new Object[]{}, arrayOfUndefinedType));
+        expectedException.expect(UnsupportedOperationException.class);
+        expectedException.expectMessage("One of the arguments of the array_unique function can " +
+                                        "be of undefined inner type, but not both");
+        assertEvaluate("array_unique([], [])", null);
     }
 
     @Test
     public void testEmptyArray() throws Exception {
-        expectedException.expect(IllegalArgumentException.class);
-        expectedException.expectMessage("When used with only one argument, the inner type of the array argument cannot be undefined");
-        assertEval(
-            null,
-            Literal.of(new Object[]{}, arrayOfUndefinedType));
+        expectedException.expect(UnsupportedOperationException.class);
+        expectedException.expectMessage(
+            "When used with only one argument, the inner type of the array argument cannot be undefined");
+        assertEvaluate("array_unique([])", null);
     }
 }
