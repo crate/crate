709932a1d2bfa3115c2358d76d8356ff1dd40dbf
Mathias Fussenegger <f.mathias@zignar.net>
2016-11-16 13:34:51 +0100

    Fix collectPhase normalize to not lose orderBy information
    Fixes a bug which was exposed due to the fact that using scalar
    subqueries can cause the collectPhase to be normalized again on the
    map-side.

--- sql/src/main/java/io/crate/planner/node/dql/RoutedCollectPhase.java
+++ sql/src/main/java/io/crate/planner/node/dql/RoutedCollectPhase.java
@@ -291,9 +291,14 @@ public class RoutedCollectPhase extends AbstractProjectionsPhase implements Coll
         List<Symbol> newToCollect = normalizer.normalize(toCollect(), transactionContext);
         boolean changed = newToCollect != toCollect();
         WhereClause newWhereClause = whereClause().normalize(normalizer, transactionContext);
-        if (newWhereClause != whereClause()) {
-            changed = changed || newWhereClause != whereClause();
+        OrderBy orderBy = this.orderBy;
+        if (orderBy != null) {
+            List<Symbol> orderBySymbols = orderBy.orderBySymbols();
+            List<Symbol> normalizedOrderBy = normalizer.normalize(orderBySymbols, transactionContext);
+            changed = changed || orderBySymbols != normalizedOrderBy;
+            orderBy = new OrderBy(normalizedOrderBy, this.orderBy.reverseFlags(), this.orderBy.nullsFirst());
         }
+        changed = changed || newWhereClause != whereClause();
         if (changed) {
             result = new RoutedCollectPhase(
                 jobId(),
@@ -306,6 +311,7 @@ public class RoutedCollectPhase extends AbstractProjectionsPhase implements Coll
                 newWhereClause,
                 distributionInfo
             );
+            result.orderBy(orderBy);
         }
         return result;
     }
