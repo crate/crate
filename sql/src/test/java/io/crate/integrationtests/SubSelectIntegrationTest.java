crate/crate

commit c5e6798e4646587a5b33aa1afba581b20a2a1823

Move scalar subquery planning into ConsumingPlanner
Scalar subqueries were only planned on the root relation. With the
upcoming "subquery in FROM" support it is necessary to do the
scalar subquery planning for each sub relation as well.

sql/src/main/java/io/crate/planner/SelectStatementPlanner.java
@@ -42,7 +42,6 @@ import io.crate.planner.node.dql.QueryThenFetch;
 import io.crate.planner.node.fetch.FetchPhase;
 import io.crate.planner.node.fetch.FetchSource;
 import io.crate.planner.projection.FetchProjection;
-import io.crate.sql.tree.QualifiedName;
 
 import java.util.ArrayList;
 import java.util.Map;
@@ -79,7 +78,11 @@ class SelectStatementPlanner {
         }
 
         private Plan invokeConsumingPlanner(AnalyzedRelation relation, Planner.Context context) {
-            return Merge.mergeToHandler(consumingPlanner.plan(relation, context), context);
+            Plan plan = consumingPlanner.plan(relation, context);
+            if (plan == null) {
+                throw new UnsupportedOperationException("Cannot create plan for: " + relation);
+            }
+            return Merge.mergeToHandler(plan, context);
         }
 
         @Override
@@ -89,25 +92,20 @@ class SelectStatementPlanner {
 
         @Override
         public Plan visitQueriedTable(QueriedTable table, Planner.Context context) {
-            SubqueryPlanner subqueryPlanner = new SubqueryPlanner(context);
-            QuerySpec querySpec = table.querySpec();
-            context.applySoftLimit(querySpec);
-            Map<Plan, SelectSymbol> subQueries = subqueryPlanner.planSubQueries(querySpec);
-            Plan plan = super.visitQueriedTable(table, context);
-            return MultiPhasePlan.createIfNeeded(plan, subQueries);
+            context.applySoftLimit(table.querySpec());
+            return super.visitQueriedTable(table, context);
         }
 
         @Override
         public Plan visitQueriedDocTable(QueriedDocTable table, Planner.Context context) {
             QuerySpec querySpec = table.querySpec();
             context.applySoftLimit(querySpec);
-            SubqueryPlanner subqueryPlanner = new SubqueryPlanner(context);
-            Map<Plan, SelectSymbol> subQueries = subqueryPlanner.planSubQueries(querySpec);
             if (querySpec.hasAggregates() || querySpec.groupBy().isPresent()) {
-                Plan subPlan = invokeConsumingPlanner(table, context);
-                return MultiPhasePlan.createIfNeeded(subPlan, subQueries);
+                return invokeConsumingPlanner(table, context);
             }
             if (querySpec.where().docKeys().isPresent() && !table.tableRelation().tableInfo().isAlias()) {
+                SubqueryPlanner subqueryPlanner = new SubqueryPlanner(context);
+                Map<Plan, SelectSymbol> subQueries = subqueryPlanner.planSubQueries(table.querySpec());
                 return MultiPhasePlan.createIfNeeded(ESGetStatementPlanner.convert(table, context), subQueries);
             }
             if (querySpec.where().hasVersions()) {
@@ -122,7 +120,7 @@ class SelectStatementPlanner {
             FetchPushDown fetchPushDown = new FetchPushDown(querySpec, table.tableRelation());
             QueriedDocTable subRelation = fetchPushDown.pushDown();
             if (subRelation == null) {
-                return MultiPhasePlan.createIfNeeded(invokeConsumingPlanner(table, context), subQueries);
+                return invokeConsumingPlanner(table, context);
             }
             Plan plannedSubQuery = subPlan(subRelation, context);
             assert plannedSubQuery != null : "consumingPlanner should have created a subPlan";
@@ -141,8 +139,7 @@ class SelectStatementPlanner {
                 table, querySpec, fetchPushDown, readerAllocations, fetchPhase, context.fetchSize());
             plannedSubQuery.addProjection(fp, null, null, fp.outputs().size(), null);
 
-            QueryThenFetch queryThenFetch = new QueryThenFetch(plannedSubQuery, fetchPhase);
-            return MultiPhasePlan.createIfNeeded(queryThenFetch, subQueries);
+            return new QueryThenFetch(plannedSubQuery, fetchPhase);
         }
 
         @Override
@@ -152,13 +149,8 @@ class SelectStatementPlanner {
             if (querySpec.where().noMatch() && !querySpec.hasAggregates()) {
                 return new NoopPlan(context.jobId());
             }
-            SubqueryPlanner subqueryPlanner = new SubqueryPlanner(context);
-            Map<Plan, SelectSymbol> subQueries = subqueryPlanner.planSubQueries(querySpec);
-            for (Map.Entry<QualifiedName, RelationSource> entry : mss.sources().entrySet()) {
-                subQueries.putAll(subqueryPlanner.planSubQueries(entry.getValue().querySpec()));
-            }
             if (mss.canBeFetched().isEmpty()) {
-                return MultiPhasePlan.createIfNeeded(invokeConsumingPlanner(mss, context), subQueries);
+                return invokeConsumingPlanner(mss, context);
             }
             MultiSourceFetchPushDown pd = MultiSourceFetchPushDown.pushDown(mss);
             Plan plannedSubQuery = invokeConsumingPlanner(mss, context);
@@ -187,8 +179,7 @@ class SelectStatementPlanner {
                 readerAllocations.indicesToIdents());
 
             plannedSubQuery.addProjection(fp, null, null, fp.outputs().size(), null);
-            QueryThenFetch queryThenFetch = new QueryThenFetch(plannedSubQuery, fetchPhase);
-            return MultiPhasePlan.createIfNeeded(Merge.mergeToHandler(queryThenFetch, context), subQueries);
+            return new QueryThenFetch(plannedSubQuery, fetchPhase);
         }
 
         @Override

sql/src/main/java/io/crate/executor/transport/SingleRowSingleValueRowReceiver.java
@@ -22,22 +22,20 @@
 
 package io.crate.executor.transport;
 
-import com.google.common.util.concurrent.ListenableFuture;
-import com.google.common.util.concurrent.SettableFuture;
 import io.crate.analyze.symbol.SelectSymbol;
-import io.crate.concurrent.CompletionListenable;
 import io.crate.core.collections.Row;
 import io.crate.operation.projectors.*;
 import io.crate.planner.Plan;
 
 import java.util.Set;
+import java.util.concurrent.CompletableFuture;
 
 /**
  * RowReceiver expects to receive only one row and triggers a future with the value once completed
  */
-class SingleRowSingleValueRowReceiver implements RowReceiver, CompletionListenable {
+class SingleRowSingleValueRowReceiver implements RowReceiver {
 
-    private final SettableFuture<Object> completionFuture = SettableFuture.create();
+    private final CompletableFuture<Object> completionFuture = new CompletableFuture<>();
     private final static Object SENTINEL = new Object();
     private final SubSelectSymbolReplacer replacer;
     private Object value = SENTINEL;
@@ -66,20 +64,20 @@ class SingleRowSingleValueRowReceiver implements RowReceiver, CompletionListenab
             Object value = this.value == SENTINEL ? null : this.value;
             replacer.onSuccess(value);
         } catch (Throwable e) {
-            completionFuture.setException(e);
+            completionFuture.completeExceptionally(e);
             return;
         }
-        completionFuture.set(value);
+        completionFuture.complete(value);
     }
 
     @Override
     public void fail(Throwable throwable) {
-        completionFuture.setException(throwable);
+        completionFuture.completeExceptionally(throwable);
     }
 
     @Override
     public void kill(Throwable throwable) {
-        completionFuture.setException(throwable);
+        completionFuture.completeExceptionally(throwable);
     }
 
     @Override
@@ -87,8 +85,7 @@ class SingleRowSingleValueRowReceiver implements RowReceiver, CompletionListenab
         return Requirements.NO_REQUIREMENTS;
     }
 
-    @Override
-    public ListenableFuture<?> completionFuture() {
+    public CompletableFuture<?> completionFuture() {
         return completionFuture;
     }
 }

sql/src/main/java/io/crate/executor/transport/SubSelectSymbolReplacer.java
@@ -32,6 +32,7 @@ import io.crate.collections.Lists2;
 import io.crate.metadata.ReplaceMode;
 import io.crate.metadata.ReplacingSymbolVisitor;
 import io.crate.planner.Merge;
+import io.crate.planner.MultiPhasePlan;
 import io.crate.planner.Plan;
 import io.crate.planner.PlanVisitor;
 import io.crate.planner.node.dql.*;
@@ -78,6 +79,12 @@ class SubSelectSymbolReplacer implements FutureCallback<Object> {
         @Override
         protected Void visitPlan(Plan plan, SymbolReplacer context) {
             throw new UnsupportedOperationException("Subselect not supported in " + plan);
+        }
+
+        @Override
+        public Void visitMultiPhasePlan(MultiPhasePlan multiPhasePlan, SymbolReplacer replacer) {
+            process(multiPhasePlan.rootPlan(), replacer);
+            return null;
         }
 
         @Override

sql/src/main/java/io/crate/executor/transport/TransportExecutor.java
@@ -22,9 +22,7 @@
 
 package io.crate.executor.transport;
 
-import com.google.common.base.Supplier;
 import com.google.common.collect.ImmutableList;
-import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
 import io.crate.action.job.ContextPreparer;
 import io.crate.action.sql.DDLStatementDispatcher;
@@ -77,6 +75,7 @@ import org.elasticsearch.threadpool.ThreadPool;
 
 import javax.annotation.Nullable;
 import java.util.*;
+import java.util.concurrent.CompletableFuture;
 
 @Singleton
 public class TransportExecutor implements Executor {
@@ -97,6 +96,7 @@ public class TransportExecutor implements Executor {
     private final BulkRetryCoordinatorPool bulkRetryCoordinatorPool;
 
     private final ProjectionToProjectorVisitor globalProjectionToProjectionVisitor;
+    private final MultiPhaseExecutor multiPhaseExecutor = new MultiPhaseExecutor();
 
     private final static BulkNodeOperationTreeGenerator BULK_NODE_OPERATION_VISITOR = new BulkNodeOperationTreeGenerator();
 
@@ -141,7 +141,10 @@ public class TransportExecutor implements Executor {
 
     @Override
     public void execute(Plan plan, RowReceiver rowReceiver, Row parameters) {
-        plan2TaskVisitor.process(plan, null).execute(rowReceiver, parameters);
+        CompletableFuture<Plan> planFuture = multiPhaseExecutor.process(plan, null);
+        planFuture
+            .thenAccept(p -> plan2TaskVisitor.process(p, null).execute(rowReceiver, parameters))
+            .exceptionally(t -> { rowReceiver.fail(t); return null; });
     }
 
     @Override
@@ -260,53 +263,75 @@ public class TransportExecutor implements Executor {
 
         @Override
         public Task visitMultiPhasePlan(MultiPhasePlan multiPhasePlan, final Void context) {
-            /*
-             * This triggers a sequential execution of a multiPhasePlan:
-             * First the dependencies (concurrently)
-             * Then via a DelayedTask the root plan.
-             *
-             * The rootTask and each dependency will be executed as separate job.
-             *
-             * This can be recursive.
-             * E.g.
-             *
-             * MultiPhaseTask
-             *      rootPlan: QueryThenFetch
-             *      deps: [
-             *          MultiPhasePlan
-             *              rootPlan: QueryThenFetch
-             *              deps: [CollectAndMerge, CollectAndMerge]
-             *      ]
-             *
-             * Note that a MultiPhaseTask always has to be the parent. Something like:
-             *
-             *      QueryThenFetch
-             *          subPlan: MultiPhaseTask
-             *
-             * Wouldn't work because QueryThenFetch would be handled by the NodeOperationTreeGenerator
-             */
+            throw new UnsupportedOperationException("MultiPhasePlan should have been processed by MultiPhaseExecutor");
+        }
+    }
+
+    /**
+     * Executor that triggers the execution of MultiPhasePlans.
+     * E.g.:
+     *
+     * processing a Plan that looks as follows:
+     * <pre>
+     *     QTF
+     *      |
+     *      +-- MultiPhasePlan
+     *              root: Collect3
+     *              deps: [Collect1, Collect2]
+     * </pre>
+     *
+     * Executions will be triggered for collect1 and collect2, and if those are completed, Collect3 will be returned
+     * as future which encapsulated the execution
+     */
+    private class MultiPhaseExecutor extends PlanVisitor<Void, CompletableFuture<Plan>> {
+
+        @Override
+        protected CompletableFuture<Plan> visitPlan(Plan plan, Void context) {
+            return CompletableFuture.completedFuture(plan);
+        }
+
+        @Override
+        public CompletableFuture<Plan> visitMerge(Merge merge, Void context) {
+            return process(merge.subPlan(), context).thenApply(p -> merge);
+        }
+
+        @Override
+        public CompletableFuture<Plan> visitNestedLoop(NestedLoop plan, Void context) {
+            CompletableFuture<Plan> fLeft = process(plan.left(), context);
+            CompletableFuture<Plan> fRight = process(plan.right(), context);
+            return CompletableFuture.allOf(fLeft, fRight).thenApply(x -> plan);
+        }
+
+        @Override
+        public CompletableFuture<Plan> visitQueryThenFetch(QueryThenFetch qtf, Void context) {
+            return process(qtf.subPlan(), context).thenApply(x -> qtf);
+        }
+
+        @Override
+        public CompletableFuture<Plan> visitMultiPhasePlan(MultiPhasePlan multiPhasePlan, Void context) {
             Map<Plan, SelectSymbol> dependencies = multiPhasePlan.dependencies();
-            List<ListenableFuture<?>> futures = new ArrayList<>();
-            final Plan rootPlan = multiPhasePlan.rootPlan();
-            for (final Map.Entry<Plan, SelectSymbol> entry : dependencies.entrySet()) {
-                Plan dependency = entry.getKey();
-                Task task = process(dependency, context);
-                SingleRowSingleValueRowReceiver singleRowSingleValueRowReceiver = new SingleRowSingleValueRowReceiver(
-                    rootPlan, entry.getValue());
-                futures.add(singleRowSingleValueRowReceiver.completionFuture());
-                task.execute(singleRowSingleValueRowReceiver, Row.EMPTY);
+            List<CompletableFuture<?>> dependencyFutures = new ArrayList<>();
+            Plan rootPlan = multiPhasePlan.rootPlan();
+            for (Map.Entry<Plan, SelectSymbol> entry : dependencies.entrySet()) {
+                Plan plan = entry.getKey();
+                SingleRowSingleValueRowReceiver singleRowSingleValueRowReceiver =
+                    new SingleRowSingleValueRowReceiver(rootPlan, entry.getValue());
+
+                CompletableFuture<Plan> planFuture = process(plan, context);
+                planFuture.whenComplete((p, e) -> {
+                    if (e == null) {
+                        // must use plan2TaskVisitor instead of calling execute
+                        // to avoid triggering MultiPhasePlans inside p again (they're already processed).
+                        // since plan's are not mutated to remove them they're still part of the plan tree
+                        plan2TaskVisitor.process(p, null).execute(singleRowSingleValueRowReceiver, Row.EMPTY);
+                    } else {
+                        singleRowSingleValueRowReceiver.fail(e);
+                    }
+                });
+                dependencyFutures.add(singleRowSingleValueRowReceiver.completionFuture());
             }
-            // Creation of the rootTask is delayed until the DelayedTask actually wants to use it.
-            // This is done because some Tasks might access the Symbols in the constructor and they might not be able
-            // to handle SelectSymbols.
-            // This ensures the Symbols are replaced once accessed
-            Supplier<Task> rootTaskSupplier = new Supplier<Task>() {
-                @Override
-                public Task get() {
-                    return process(rootPlan, context);
-                }
-            };
-            return new DelayedTask(Futures.allAsList(futures), rootTaskSupplier);
+            CompletableFuture[] cfs = dependencyFutures.toArray(new CompletableFuture[0]);
+            return CompletableFuture.allOf(cfs).thenCompose(x -> process(rootPlan, context));
         }
     }
 
@@ -528,6 +553,15 @@ public class TransportExecutor implements Executor {
         public Void visitQueryThenFetch(QueryThenFetch node, NodeOperationTreeContext context) {
             process(node.subPlan(), context);
             context.addContextPhase(node.fetchPhase());
+            return null;
+        }
+
+        @Override
+        public Void visitMultiPhasePlan(MultiPhasePlan multiPhasePlan, NodeOperationTreeContext context) {
+            // MultiPhasePlan's should be executed by the MultiPhaseExecutor, but it doesn't remove
+            // them from the tree in order to avoid re-creating plans with the MultiPhasePlan removed,
+            // so here it's fine to just skip over the multiPhasePlan because it has already been executed
+            process(multiPhasePlan.rootPlan(), context);
             return null;
         }
 

sql/src/main/java/io/crate/planner/MultiPhasePlan.java
@@ -48,7 +48,7 @@ public class MultiPhasePlan implements Plan {
     private final Plan rootPlan;
     private final Map<Plan, SelectSymbol> dependencies;
 
-    static Plan createIfNeeded(Plan subPlan, Map<Plan, SelectSymbol> dependencies) {
+    public static Plan createIfNeeded(Plan subPlan, Map<Plan, SelectSymbol> dependencies) {
         if (dependencies.isEmpty()) {
             return subPlan;
         }

sql/src/main/java/io/crate/planner/SubqueryPlanner.java
@@ -34,13 +34,13 @@ import org.elasticsearch.common.util.Consumer;
 import java.util.HashMap;
 import java.util.Map;
 
-class SubqueryPlanner {
+public class SubqueryPlanner {
 
     private final Planner.Context plannerContext;
     private final Visitor visitor;
     private final Map<Plan, SelectSymbol> subQueries = new HashMap<>();
 
-    SubqueryPlanner(Planner.Context plannerContext) {
+    public SubqueryPlanner(Planner.Context plannerContext) {
         this.plannerContext = plannerContext;
         this.visitor = new Visitor();
     }
@@ -52,7 +52,7 @@ class SubqueryPlanner {
         subQueries.put(subPlan, selectSymbol);
     }
 
-    Map<Plan, SelectSymbol> planSubQueries(QuerySpec querySpec) {
+    public Map<Plan, SelectSymbol> planSubQueries(QuerySpec querySpec) {
         querySpec.visitSymbols(visitor);
         return subQueries;
     }

sql/src/main/java/io/crate/planner/consumer/ConsumingPlanner.java
@@ -21,18 +21,20 @@
 
 package io.crate.planner.consumer;
 
+import io.crate.analyze.QuerySpec;
 import io.crate.analyze.Rewriter;
 import io.crate.analyze.relations.AnalyzedRelation;
+import io.crate.analyze.relations.QueriedRelation;
+import io.crate.analyze.symbol.SelectSymbol;
 import io.crate.exceptions.ValidationException;
 import io.crate.metadata.Functions;
-import io.crate.planner.Plan;
-import io.crate.planner.Planner;
-import io.crate.planner.TableStatsService;
+import io.crate.planner.*;
 import org.elasticsearch.cluster.ClusterService;
 
 import javax.annotation.Nullable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class ConsumingPlanner {
 
@@ -63,6 +65,12 @@ public class ConsumingPlanner {
         for (Consumer consumer : consumers) {
             Plan plan = consumer.consume(relation, consumerContext);
             if (plan != null) {
+                if (relation instanceof QueriedRelation) {
+                    QuerySpec qs = ((QueriedRelation) relation).querySpec();
+                    SubqueryPlanner subqueryPlanner = new SubqueryPlanner(consumerContext.plannerContext());
+                    Map<Plan, SelectSymbol> subQueries = subqueryPlanner.planSubQueries(qs);
+                    return MultiPhasePlan.createIfNeeded(plan, subQueries);
+                }
                 return plan;
             }
         }

sql/src/test/java/io/crate/planner/SingleRowSubselectPlannerTest.java
@@ -40,10 +40,11 @@ public class SingleRowSubselectPlannerTest extends CrateUnitTest {
 
     @Test
     public void testPlanSimpleSelectWithSingleRowSubSelectInWhereClause() throws Exception {
-        MultiPhasePlan plan = e.plan("select x from t1 where a = (select b from t2)");
-        assertThat(plan.rootPlan(), instanceOf(QueryThenFetch.class));
-        assertThat(((QueryThenFetch) plan.rootPlan()).subPlan(), instanceOf(Collect.class));
-        assertThat(plan.dependencies().keySet(), contains(instanceOf(QueryThenFetch.class)));
+        QueryThenFetch qtf = e.plan("select x from t1 where a = (select b from t2)");
+        assertThat(qtf.subPlan(), instanceOf(MultiPhasePlan.class));
+
+        MultiPhasePlan multiPhasePlan = (MultiPhasePlan) qtf.subPlan();
+        assertThat(multiPhasePlan.dependencies().keySet(), contains(instanceOf(QueryThenFetch.class)));
     }
 
     @Test
@@ -70,9 +71,8 @@ public class SingleRowSubselectPlannerTest extends CrateUnitTest {
 
     @Test
     public void testSingleRowSubSelectOfWhereInJoin() throws Exception {
-        MultiPhasePlan plan = e.plan("select * from users u1, users u2 where u1.name = (select 'Arthur')");
-        assertThat(plan.rootPlan(), instanceOf(QueryThenFetch.class));
-        assertThat(((QueryThenFetch) plan.rootPlan()).subPlan(), instanceOf(NestedLoop.class));
-        assertThat(plan.dependencies().keySet(), contains(instanceOf(Collect.class)));
+        QueryThenFetch plan = e.plan("select * from users u1, users u2 where u1.name = (select 'Arthur')");
+        assertThat(plan.subPlan(), instanceOf(NestedLoop.class));
+        assertThat(((NestedLoop) plan.subPlan()).left(), instanceOf(MultiPhasePlan.class));
     }
 }

commit 1828808c673c9b8649969b258cd36bf3439bef6a

Fix scalar subquery execution in query-then-fetch
Fixes a regression introduced by c5e6798e4646587a5b33aa1afba581b20a2a1823

sql/src/main/java/io/crate/planner/SelectStatementPlanner.java
@@ -24,7 +24,10 @@ package io.crate.planner;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
-import io.crate.analyze.*;
+import io.crate.analyze.MultiSourceSelect;
+import io.crate.analyze.QueriedTable;
+import io.crate.analyze.QuerySpec;
+import io.crate.analyze.SelectAnalyzedStatement;
 import io.crate.analyze.relations.AnalyzedRelation;
 import io.crate.analyze.relations.AnalyzedRelationVisitor;
 import io.crate.analyze.relations.QueriedDocTable;
@@ -138,7 +141,10 @@ class SelectStatementPlanner {
                 table, querySpec, fetchPushDown, readerAllocations, fetchPhase, context.fetchSize());
             plannedSubQuery.addProjection(fp, null, null, fp.outputs().size(), null);
 
-            return new QueryThenFetch(plannedSubQuery, fetchPhase);
+            QueryThenFetch qtf = new QueryThenFetch(plannedSubQuery, fetchPhase);
+            SubqueryPlanner subqueryPlanner = new SubqueryPlanner(context);
+            Map<Plan, SelectSymbol> subqueries = subqueryPlanner.planSubQueries(querySpec);
+            return MultiPhasePlan.createIfNeeded(qtf, subqueries);
         }
 
         @Override

sql/src/test/java/io/crate/integrationtests/SubSelectIntegrationTest.java
@@ -394,4 +394,17 @@ public class SubSelectIntegrationTest extends SQLTransportIntegrationTest {
         execute("select sum(x) from (select min(col1) as x from unnest([1])) as t where x = 2");
         assertThat(TestingHelpers.printedTable(response.rows()), is("NULL\n"));
     }
+
+    @Test
+    public void testSubQueryInSelectListOnDocTable() throws Exception {
+        execute("create table t (x int)");
+        ensureYellow();
+        execute("insert into t (x) values (1), (1)");
+        execute("refresh table t");
+
+        execute("select (select 2), x from t");
+        assertThat(TestingHelpers.printedTable(response.rows()),
+            is("2| 1\n" +
+               "2| 1\n"));
+    }
 }