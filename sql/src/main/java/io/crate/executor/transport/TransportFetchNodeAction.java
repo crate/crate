c2ffc601360ff9e55275d5d16b3ed8afdf00902b
Mathias Fussenegger <f.mathias@zignar.net>
2016-11-29 16:18:23 +0100

    Decouple transport / nodeFetchOperation
    Moving some more logic into NodeFetchOperation so that
    TransportFetchNodeAction is only a small wrapper doing networking.
    
    This also removes the RamAccountingContext creation because it was
    unused. (The `totalBytes()` call always returned 0 because there were
    no `addBytes()` calls)

--- sql/src/main/java/io/crate/executor/transport/TransportFetchNodeAction.java
+++ sql/src/main/java/io/crate/executor/transport/TransportFetchNodeAction.java
@@ -26,25 +26,15 @@ import com.google.common.util.concurrent.FutureCallback;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
 import io.crate.Streamer;
-import io.crate.breaker.CrateCircuitBreakerService;
-import io.crate.breaker.RamAccountingContext;
-import io.crate.exceptions.Exceptions;
-import io.crate.jobs.JobContextService;
-import io.crate.jobs.JobExecutionContext;
-import io.crate.operation.collect.StatsTables;
-import io.crate.operation.fetch.FetchContext;
 import io.crate.operation.fetch.NodeFetchOperation;
 import org.elasticsearch.action.ActionListener;
-import org.elasticsearch.common.breaker.CircuitBreaker;
 import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.common.inject.Singleton;
-import org.elasticsearch.indices.breaker.CircuitBreakerService;
 import org.elasticsearch.threadpool.ThreadPool;
 import org.elasticsearch.transport.TransportService;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
-import java.util.Locale;
 
 @Singleton
 public class TransportFetchNodeAction implements NodeAction<NodeFetchRequest, NodeFetchResponse> {
@@ -54,23 +44,14 @@ public class TransportFetchNodeAction implements NodeAction<NodeFetchRequest, No
     private static final String RESPONSE_EXECUTOR = ThreadPool.Names.SAME;
 
     private final Transports transports;
-    private final StatsTables statsTables;
     private final NodeFetchOperation nodeFetchOperation;
-    private final CircuitBreaker circuitBreaker;
-    private final JobContextService jobContextService;
 
     @Inject
     public TransportFetchNodeAction(TransportService transportService,
                                     Transports transports,
-                                    StatsTables statsTables,
-                                    CircuitBreakerService breakerService,
-                                    JobContextService jobContextService,
                                     NodeFetchOperation nodeFetchOperation) {
         this.transports = transports;
-        this.statsTables = statsTables;
         this.nodeFetchOperation = nodeFetchOperation;
-        this.circuitBreaker = breakerService.getBreaker(CrateCircuitBreakerService.QUERY);
-        this.jobContextService = jobContextService;
 
         transportService.registerRequestHandler(TRANSPORT_ACTION,
             NodeFetchRequest.class,
@@ -93,60 +74,23 @@ public class TransportFetchNodeAction implements NodeAction<NodeFetchRequest, No
 
     @Override
     public void nodeOperation(final NodeFetchRequest request,
-                              final ActionListener<NodeFetchResponse> fetchResponse) {
-
-        String ramAccountingContextId = String.format(Locale.ENGLISH, "%s: %d", request.jobId(), request.fetchPhaseId());
-        final RamAccountingContext ramAccountingContext = new RamAccountingContext(ramAccountingContextId, circuitBreaker);
-        try {
-            statsTables.operationStarted(request.fetchPhaseId(), request.jobId(), "fetch");
-
-            // nothing to fetch, just close
-            if (request.toFetch() == null && request.isCloseContext()) {
-                JobExecutionContext ctx = jobContextService.getContextOrNull(request.jobId());
-                if (ctx != null) {
-                    FetchContext fetchContext = ctx.getSubContextOrNull(request.fetchPhaseId());
-                    if (fetchContext != null) {
-                        fetchContext.close();
-                    }
-                }
-                fetchResponse.onResponse(NodeFetchResponse.EMPTY);
-                return;
+                              final ActionListener<NodeFetchResponse> responseListener) {
+        ListenableFuture<IntObjectMap<StreamBucket>> resultFuture = nodeFetchOperation.fetch(
+            request.jobId(),
+            request.fetchPhaseId(),
+            request.toFetch(),
+            request.isCloseContext()
+        );
+        Futures.addCallback(resultFuture, new FutureCallback<IntObjectMap<StreamBucket>>() {
+            @Override
+            public void onSuccess(@Nullable IntObjectMap<StreamBucket> result) {
+                responseListener.onResponse(NodeFetchResponse.forSending(result));
             }
 
-            JobExecutionContext jobExecutionContext = jobContextService.getContext(request.jobId());
-            final FetchContext fetchContext = jobExecutionContext.getSubContext(request.fetchPhaseId());
-
-            ListenableFuture<IntObjectMap<StreamBucket>> fetchedBucketFuture =
-                nodeFetchOperation.doFetch(fetchContext, request.toFetch());
-
-            Futures.addCallback(fetchedBucketFuture, new FutureCallback<IntObjectMap<StreamBucket>>() {
-                @Override
-                public void onSuccess(@Nullable IntObjectMap<StreamBucket> result) {
-                    assert result != null : "result of nodeFetchOperation.doFetch must not be null";
-                    NodeFetchResponse response = NodeFetchResponse.forSending(result);
-                    if (request.isCloseContext()) {
-                        fetchContext.close();
-                    }
-                    fetchResponse.onResponse(response);
-                    statsTables.operationFinished(request.fetchPhaseId(), request.jobId(), null,
-                        ramAccountingContext.totalBytes());
-                    ramAccountingContext.close();
-                }
-
-                @Override
-                public void onFailure(@Nonnull Throwable t) {
-                    fetchContext.kill(t);
-                    fetchResponse.onFailure(t);
-                    statsTables.operationFinished(request.fetchPhaseId(), request.jobId(), Exceptions.messageOf(t),
-                        ramAccountingContext.totalBytes());
-                    ramAccountingContext.close();
-                }
-            });
-        } catch (Throwable t) {
-            fetchResponse.onFailure(t);
-            statsTables.operationFinished(request.fetchPhaseId(), request.jobId(), Exceptions.messageOf(t),
-                ramAccountingContext.totalBytes());
-            ramAccountingContext.close();
-        }
+            @Override
+            public void onFailure(@Nonnull Throwable t) {
+                responseListener.onFailure(t);
+            }
+        });
     }
 }
