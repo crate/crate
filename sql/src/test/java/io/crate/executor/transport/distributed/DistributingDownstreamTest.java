crate/crate

commit 5b13dae38c9c7e7488ece613db1eae45976ab03d

implement kill() on PageDownstream
fail() was mis-used for kill and the exception was
not forwarded to the row-receivers if the PageDownstream
was repeating. this is true for a valid fail, but not for kill.

sql/src/main/java/io/crate/jobs/PageDownstreamContext.java
@@ -201,7 +201,9 @@ public class PageDownstreamContext extends AbstractExecutionSubContext implement
 
     @Override
     protected void innerKill(@Nonnull Throwable t) {
-        innerClose(t);
+        pageDownstream.kill(t);
+        future.bytesUsed(ramAccountingContext.totalBytes());
+        ramAccountingContext.close();
     }
 
     @Override

sql/src/main/java/io/crate/operation/PageDownstream.java
@@ -39,4 +39,9 @@ public interface PageDownstream {
     void finish();
 
     void fail(Throwable t);
+
+    /**
+     * Kill an PageDownstream and stop execution.
+     */
+    void kill(Throwable t);
 }

sql/src/main/java/io/crate/operation/merge/IteratorPageDownstream.java
@@ -184,4 +184,9 @@ public class IteratorPageDownstream implements PageDownstream, ResumeHandle, Rep
             lastListener.finish();
         }
     }
+
+    @Override
+    public void kill(Throwable t) {
+        rowReceiver.kill(t);
+    }
 }

sql/src/test/java/io/crate/jobs/PageDownstreamContextTest.java
@@ -101,6 +101,6 @@ public class PageDownstreamContextTest extends CrateUnitTest {
 
         ctx.kill(null);
         assertThat(throwable.get(), Matchers.instanceOf(InterruptedException.class));
-        verify(downstream, times(1)).fail(any(InterruptedException.class));
+        verify(downstream, times(1)).kill(any(InterruptedException.class));
     }
 }

sql/src/test/java/io/crate/operation/merge/IteratorPageDownstreamTest.java
@@ -35,12 +35,14 @@ import io.crate.test.integration.CrateUnitTest;
 import io.crate.testing.CollectingRowReceiver;
 import io.crate.testing.TestingHelpers;
 import org.elasticsearch.common.breaker.CircuitBreakingException;
+import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.common.util.concurrent.EsRejectedExecutionException;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 
 import javax.annotation.Nonnull;
+import java.util.concurrent.CancellationException;
 import java.util.concurrent.Executor;
 
 import static com.carrotsearch.randomizedtesting.RandomizedTest.$;
@@ -293,5 +295,19 @@ public class IteratorPageDownstreamTest extends CrateUnitTest {
         assertThat(rowReceiver.rows.size(), is(4));
         verify(pageConsumeListener, times(1)).needMore();
         verify(pageConsumeListener, times(0)).finish();
+    }
+
+    @Test
+    public void testKillCallsRowReceiver() throws Exception {
+        CollectingRowReceiver rowReceiver = new CollectingRowReceiver();
+        IteratorPageDownstream pageDownstream = new IteratorPageDownstream(
+            rowReceiver,
+            PassThroughPagingIterator.<Void, Row>repeatable(),
+            Optional.<Executor>absent());
+
+        pageDownstream.kill(null);
+
+        expectedException.expect(CancellationException.class);
+        rowReceiver.result(TimeValue.timeValueSeconds(5));
     }
 }

commit 7ddec91a0b7b904f293834612cf50fa24fd9c515

forward kill reason to distributing downstream
this fixes error handling issues introduced by
https://github.com/crate/crate/commit/5b13dae38c9c7e7488ece613db1eae45976ab03d

sql/src/main/java/io/crate/executor/transport/distributed/DistributedResultRequest.java
@@ -44,15 +44,15 @@ public class DistributedResultRequest extends TransportRequest {
     private boolean isLast = true;
 
     private Throwable throwable = null;
+    private boolean isKilled = false;
 
     public DistributedResultRequest() {
     }
 
-    private DistributedResultRequest(UUID jobId, byte inputId, int executionPhaseId, int bucketIdx, Streamer<?>[] streamers) {
+    private DistributedResultRequest(UUID jobId, byte inputId, int executionPhaseId, int bucketIdx) {
         this.jobId = jobId;
         this.executionPhaseId = executionPhaseId;
         this.bucketIdx = bucketIdx;
-        this.streamers = streamers;
         this.inputId = inputId;
     }
 
@@ -63,7 +63,8 @@ public class DistributedResultRequest extends TransportRequest {
                                     Streamer<?>[] streamers,
                                     Bucket rows,
                                     boolean isLast) {
-        this(jobId, inputId, executionPhaseId, bucketIdx, streamers);
+        this(jobId, inputId, executionPhaseId, bucketIdx);
+        this.streamers = streamers;
         this.rows = rows;
         this.isLast = isLast;
     }
@@ -72,10 +73,11 @@ public class DistributedResultRequest extends TransportRequest {
                                     int executionPhaseId,
                                     byte inputId,
                                     int bucketIdx,
-                                    Streamer<?>[] streamers,
-                                    Throwable throwable) {
-        this(jobId, inputId, executionPhaseId, bucketIdx, streamers);
+                                    Throwable throwable,
+                                    boolean isKilled) {
+        this(jobId, inputId, executionPhaseId, bucketIdx);
         this.throwable = throwable;
+        this.isKilled = isKilled;
     }
 
     public UUID jobId() {
@@ -122,6 +124,10 @@ public class DistributedResultRequest extends TransportRequest {
         return throwable;
     }
 
+    public boolean isKilled() {
+        return isKilled;
+    }
+
     @Override
     public void readFrom(StreamInput in) throws IOException {
         super.readFrom(in);
@@ -134,6 +140,7 @@ public class DistributedResultRequest extends TransportRequest {
         boolean failure = in.readBoolean();
         if (failure) {
             throwable = in.readThrowable();
+            isKilled = in.readBoolean();
         } else {
             StreamBucket bucket = new StreamBucket(streamers);
             bucket.readFrom(in);
@@ -155,6 +162,7 @@ public class DistributedResultRequest extends TransportRequest {
         out.writeBoolean(failure);
         if (failure) {
             out.writeThrowable(throwable);
+            out.writeBoolean(isKilled);
         } else {
             // TODO: we should not rely on another bucket in this class and instead write to the stream directly
             StreamBucket.writeBucket(out, streamers, rows);

sql/src/main/java/io/crate/executor/transport/distributed/DistributingDownstream.java
@@ -96,6 +96,7 @@ public class DistributingDownstream implements RowReceiver {
     private final Bucket[] buckets;
     private final boolean traceEnabled;
     private boolean upstreamFinished;
+    private boolean isKilled = false;
     private final AtomicReference<ResumeHandle> resumeHandleRef = new AtomicReference<>(ResumeHandle.INVALID);
     private volatile boolean stop = false;
     private final AtomicReference<Throwable> failure = new AtomicReference<>(null);
@@ -232,7 +233,7 @@ public class DistributingDownstream implements RowReceiver {
                 if (error == null) {
                     downstream.sendRequest(buckets[i], isLastRequest);
                 } else {
-                    downstream.sendRequest(error);
+                    downstream.sendRequest(error, isKilled);
                 }
             }
         }
@@ -250,7 +251,12 @@ public class DistributingDownstream implements RowReceiver {
     @Override
     public void kill(Throwable throwable) {
         stop = true;
-        // downstream will also eventually receive a kill - no need to forward it.
+        // forward kill reason to downstream if not already done, otherwise downstream may wait forever for a final result
+        if (failure.compareAndSet(null, throwable)) {
+            upstreamFinished = true;
+            isKilled = true;
+            trySendRequests();
+        }
     }
 
     @Override
@@ -267,10 +273,10 @@ public class DistributingDownstream implements RowReceiver {
             this.downstreamNodeId = downstreamNodeId;
         }
 
-        void sendRequest(Throwable t) {
+        void sendRequest(Throwable t, boolean isKilled) {
             distributedResultAction.pushResult(
                 downstreamNodeId,
-                new DistributedResultRequest(jobId, targetPhaseId, inputId, bucketIdx, streamers, t),
+                new DistributedResultRequest(jobId, targetPhaseId, inputId, bucketIdx, t, isKilled),
                 NO_OP_ACTION_LISTENER
             );
         }

sql/src/main/java/io/crate/executor/transport/distributed/TransportDistributedResultAction.java
@@ -128,7 +128,11 @@ public class TransportDistributedResultAction extends AbstractComponent implemen
                 request.isLast(),
                 new SendResponsePageResultListener(listener, request));
         } else {
-            pageBucketReceiver.failure(request.bucketIdx(), throwable);
+            if (request.isKilled()) {
+                pageBucketReceiver.killed(request.bucketIdx(), throwable);
+            } else {
+                pageBucketReceiver.failure(request.bucketIdx(), throwable);
+            }
             listener.onResponse(new DistributedResultResponse(false));
         }
     }

sql/src/main/java/io/crate/executor/transport/executionphases/InterceptingRowReceiver.java
@@ -119,7 +119,7 @@ class InterceptingRowReceiver implements RowReceiver, FutureCallback<Void> {
                 new KillJobsRequest(Collections.singletonList(jobId)), new ActionListener<KillResponse>() {
                     @Override
                     public void onResponse(KillResponse killResponse) {
-                        LOGGER.trace("Killed {} jobs before forwarding the failure", killResponse.numKilled());
+                        LOGGER.trace("Killed {} jobs before forwarding the failure={}", killResponse.numKilled(), failure);
                         rowReceiver.fail(failure);
                     }
 

sql/src/main/java/io/crate/executor/transport/kill/TransportKillNodeAction.java
@@ -87,10 +87,11 @@ abstract class TransportKillNodeAction<Request extends TransportRequest> extends
     }
 
     /**
-     * broadcasts the given kill request to all nodes in the cluster
+     * Broadcasts the given kill request to all nodes in the cluster
      */
     public void broadcast(Request request, ActionListener<KillResponse> listener) {
         DiscoveryNodes nodes = clusterService.state().nodes();
+
         listener = new MultiActionListener<>(nodes.size(), KillResponse.MERGE_FUNCTION, listener);
         DefaultTransportResponseHandler<KillResponse> responseHandler =
             new DefaultTransportResponseHandler<KillResponse>(listener) {

sql/src/main/java/io/crate/jobs/PageBucketReceiver.java
@@ -32,5 +32,7 @@ public interface PageBucketReceiver {
 
     void failure(int bucketIdx, Throwable throwable);
 
+    void killed(int bucketIdx, Throwable throwable);
+
     Streamer<?>[] streamers();
 }

sql/src/main/java/io/crate/jobs/PageDownstreamContext.java
@@ -162,6 +162,15 @@ public class PageDownstreamContext extends AbstractExecutionSubContext implement
         }
     }
 
+    @Override
+    public void killed(int bucketIdx, Throwable throwable) {
+        traceLog("method=killed", bucketIdx, throwable);
+        synchronized (lock) {
+            setExhaustedUpstreams();
+            bucketFutures.get(bucketIdx).setException(throwable);
+        }
+    }
+
     private void clearPageIfFull(int bucketIdx) {
         if (allFuturesSet.cardinality() == numBuckets) {
             traceLog("page is full, clearing it", bucketIdx);
@@ -194,14 +203,15 @@ public class PageDownstreamContext extends AbstractExecutionSubContext implement
         } else {
             pageDownstream.fail(throwable);
         }
-
-        future.bytesUsed(ramAccountingContext.totalBytes());
-        ramAccountingContext.close();
     }
 
     @Override
     protected void innerKill(@Nonnull Throwable t) {
         pageDownstream.kill(t);
+    }
+
+    @Override
+    public void cleanup() {
         future.bytesUsed(ramAccountingContext.totalBytes());
         ramAccountingContext.close();
     }

sql/src/test/java/io/crate/executor/transport/distributed/DistributingDownstreamTest.java
@@ -52,7 +52,9 @@ import java.util.UUID;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
 
+import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.core.Is.is;
 import static org.mockito.Mockito.mock;
 
@@ -187,5 +189,41 @@ public class DistributingDownstreamTest extends CrateUnitTest {
         );
         rowSender.run();
         assertThat(requestsReceived.get(), is(3));
+    }
+
+    @Test
+    public void testKillFailureIsForwarded() throws Exception {
+        Streamer[] streamers = new Streamer[]{DataTypes.INTEGER.streamer()};
+        AtomicReference<Throwable> throwableReceived = new AtomicReference<>();
+        TransportDistributedResultAction transportDistributedResultAction = new TransportDistributedResultAction(
+            mock(Transports.class),
+            mock(JobContextService.class),
+            mock(ThreadPool.class),
+            mock(TransportService.class),
+            Settings.EMPTY) {
+
+            @Override
+            public void pushResult(String node, final DistributedResultRequest request, final ActionListener<DistributedResultResponse> listener) {
+                if (request.isKilled() && request.throwable() != null) {
+                    throwableReceived.set(request.throwable());
+                }
+            }
+        };
+
+        DistributingDownstream dd = new DistributingDownstream(
+            Loggers.getLogger(DistributingDownstream.class),
+            UUID.randomUUID(),
+            new BroadcastingBucketBuilder(streamers, 2),
+            1,
+            (byte) 0,
+            0,
+            ImmutableList.of("n1", "n2"),
+            transportDistributedResultAction,
+            streamers,
+            2
+        );
+        dd.kill(new IllegalStateException("dummy"));
+
+        assertThat(throwableReceived.get(), instanceOf(IllegalStateException.class));
     }
 }

sql/src/test/java/io/crate/executor/transport/merge/DistributedResultRequestTest.java
@@ -35,8 +35,7 @@ import java.util.UUID;
 
 import static io.crate.testing.TestingHelpers.isNullRow;
 import static io.crate.testing.TestingHelpers.isRow;
-import static org.hamcrest.Matchers.contains;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
 
 public class DistributedResultRequestTest extends CrateUnitTest {
 
@@ -64,5 +63,22 @@ public class DistributedResultRequestTest extends CrateUnitTest {
         assertThat(r1.executionPhaseInputId(), is(r2.executionPhaseInputId()));
 
         assertThat(r2.rows(), contains(isRow("ab"), isNullRow(), isRow("cd")));
+    }
+
+    @Test
+    public void testStreamingOfFailure() throws Exception {
+        UUID uuid = UUID.randomUUID();
+        Throwable throwable = new IllegalStateException("dummy");
+
+        DistributedResultRequest r1 = new DistributedResultRequest(uuid, 1, (byte) 3, 1, throwable, true);
+
+        BytesStreamOutput out = new BytesStreamOutput();
+        r1.writeTo(out);
+        StreamInput in = StreamInput.wrap(out.bytes());
+        DistributedResultRequest r2 = new DistributedResultRequest();
+        r2.readFrom(in);
+
+        assertThat(r2.throwable(), instanceOf(throwable.getClass()));
+        assertThat(r2.isKilled(), is(r1.isKilled()));
     }
 }