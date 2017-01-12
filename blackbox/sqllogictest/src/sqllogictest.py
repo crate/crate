d5b025fc3b9744491ce40496d532b0a8a6604079
Philipp Bogensberger <philipp@crate.io>
2017-01-12 15:30:27 +0000

    exclude BETWEEN NULL queries in sqllogic tests as CrateDB delivers different results than postgresql or sqlite in some cases

--- blackbox/sqllogictest/src/sqllogictest.py
+++ blackbox/sqllogictest/src/sqllogictest.py
@@ -23,6 +23,7 @@ tqdm.monitor_interval = 0
 
 QUERY_WHITELIST = [re.compile(o, re.IGNORECASE) for o in [
     'CREATE INDEX.*',                        # CREATE INDEX is not supported, but raises SQLParseException
+    '.*BETWEEN.*NULL.*',
 ]]
 
 varchar_to_string = partial(re.compile('VARCHAR\(\d+\)').sub, 'STRING')
