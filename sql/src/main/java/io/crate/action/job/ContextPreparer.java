e26f244b977983aade16e80ccbd7d7f4e4209cb4
Mathias Fussenegger <f.mathias@zignar.net>
2017-01-22 01:13:52 +0100

    Use IntObjectMap for targetToSource instead of Multimap
    To avoid boxing. This brings back the
    measureCreateNodeOperationCtxPlusFindLeafs benchmark from ~300ns to
    ~200ns

--- sql/src/main/java/io/crate/action/job/ContextPreparer.java
+++ sql/src/main/java/io/crate/action/job/ContextPreparer.java
@@ -23,10 +23,9 @@ package io.crate.action.job;
 
 import com.carrotsearch.hppc.*;
 import com.carrotsearch.hppc.cursors.IntCursor;
+import com.carrotsearch.hppc.cursors.IntObjectCursor;
 import com.carrotsearch.hppc.procedures.ObjectProcedure;
 import com.google.common.base.MoreObjects;
-import com.google.common.collect.HashMultimap;
-import com.google.common.collect.Multimap;
 import io.crate.Streamer;
 import io.crate.analyze.EvaluatingNormalizer;
 import io.crate.breaker.CrateCircuitBreakerService;
@@ -231,18 +230,18 @@ public class ContextPreparer extends AbstractComponent {
      * {@link PreparerContext#opCtx#targetToSourceMap} will be used to traverse the nodeOperations
      */
     private void prepareSourceOperations(int startPhaseId, PreparerContext preparerContext) {
-        Collection<Integer> sourcePhaseIds = preparerContext.opCtx.targetToSourceMap.get(startPhaseId);
-        if (sourcePhaseIds.isEmpty()) {
+        IntContainer sourcePhaseIds = preparerContext.opCtx.targetToSourceMap.get(startPhaseId);
+        if (sourcePhaseIds == null) {
             return;
         }
-        for (Integer sourcePhaseId : sourcePhaseIds) {
-            NodeOperation nodeOperation = preparerContext.opCtx.nodeOperationByPhaseId.get(sourcePhaseId);
+        for (IntCursor sourcePhaseId : sourcePhaseIds) {
+            NodeOperation nodeOperation = preparerContext.opCtx.nodeOperationByPhaseId.get(sourcePhaseId.value);
             Boolean created = createContexts(nodeOperation.executionPhase(), preparerContext);
             assert created : "a subContext is required to be created";
             preparerContext.opCtx.builtNodeOperations.set(nodeOperation.executionPhase().phaseId());
         }
-        for (Integer sourcePhaseId : sourcePhaseIds) {
-            prepareSourceOperations(sourcePhaseId, preparerContext);
+        for (IntCursor sourcePhaseId : sourcePhaseIds) {
+            prepareSourceOperations(sourcePhaseId.value, preparerContext);
         }
     }
 
@@ -267,8 +266,8 @@ public class ContextPreparer extends AbstractComponent {
          * <p/>
          * (In the example above, NodeOp 0 might depend on the context/RowReceiver of NodeOp 1 being built first.)
          */
-        private final Multimap<Integer, Integer> targetToSourceMap;
-        private final IntObjectHashMap<NodeOperation> nodeOperationByPhaseId;
+        private final IntObjectMap<? extends IntContainer> targetToSourceMap;
+        private final IntObjectMap<NodeOperation> nodeOperationByPhaseId;
         private final BitSet builtNodeOperations;
         private final String localNodeId;
 
@@ -287,13 +286,18 @@ public class ContextPreparer extends AbstractComponent {
             return map;
         }
 
-        static Multimap<Integer, Integer> createTargetToSourceMap(Iterable<? extends NodeOperation> nodeOperations) {
-            HashMultimap<Integer, Integer> targetToSource = HashMultimap.create();
-            for (NodeOperation nodeOperation : nodeOperations) {
-                if (nodeOperation.downstreamExecutionPhaseId() == NodeOperation.NO_DOWNSTREAM) {
+        static IntObjectHashMap<? extends IntContainer> createTargetToSourceMap(Iterable<? extends NodeOperation> nodeOperations) {
+            IntObjectHashMap<IntArrayList> targetToSource = new IntObjectHashMap<>();
+            for (NodeOperation nodeOp : nodeOperations) {
+                if (nodeOp.downstreamExecutionPhaseId() == NodeOperation.NO_DOWNSTREAM) {
                     continue;
                 }
-                targetToSource.put(nodeOperation.downstreamExecutionPhaseId(), nodeOperation.executionPhase().phaseId());
+                IntArrayList sourceIds = targetToSource.get(nodeOp.downstreamExecutionPhaseId());
+                if (sourceIds == null) {
+                    sourceIds = new IntArrayList();
+                    targetToSource.put(nodeOp.downstreamExecutionPhaseId(), sourceIds);
+                }
+                sourceIds.add(nodeOp.executionPhase().phaseId());
             }
             return targetToSource;
         }
@@ -302,24 +306,43 @@ public class ContextPreparer extends AbstractComponent {
          * find all phases that don't have any downstreams.
          * <p>
          * This is usually only one phase (the handlerPhase, but there might be more in case of bulk operations)
+         * <pre>
+         *
+         *     flow:            targetToSourceMap:
+         *
+         *     0    1               2 -> [0, 1]
+         *      \  /                3 -> [2]
+         *       2
+         *       |
+         *       3
+         *
+         *     leafs = all keys in targetToSource map which have no entry in values (3 in the example above)
+         * </pre>
          */
-        private static IntCollection findLeafs(Multimap<Integer, Integer> targetToSourceMap) {
+        private static IntCollection findLeafs(IntObjectMap<? extends IntContainer> targetToSourceMap) {
             IntArrayList leafs = new IntArrayList();
-            for (Integer targetPhaseId : targetToSourceMap.keySet()) {
-                if (!targetToSourceMap.containsValue(targetPhaseId)) {
-                    leafs.add(targetPhaseId);
+            BitSet sources = new BitSet();
+            for (IntObjectCursor<? extends IntContainer> sourceIds : targetToSourceMap) {
+                for (IntCursor sourceId : sourceIds.value) {
+                    sources.set(sourceId.value);
+                }
+            }
+            for (IntCursor targetPhaseCursor : targetToSourceMap.keys()) {
+                int targetPhase = targetPhaseCursor.value;
+                if (!sources.get(targetPhase)) {
+                    leafs.add(targetPhase);
                 }
             }
             return leafs;
         }
 
         public boolean upstreamsAreOnSameNode(int phaseId) {
-            Collection<Integer> sourcePhases = targetToSourceMap.get(phaseId);
-            if (sourcePhases.isEmpty()) {
+            IntContainer sourcePhases = targetToSourceMap.get(phaseId);
+            if (sourcePhases == null) {
                 return false;
             }
-            for (Integer sourcePhase : sourcePhases) {
-                NodeOperation nodeOperation = nodeOperationByPhaseId.get(sourcePhase);
+            for (IntCursor sourcePhase : sourcePhases) {
+                NodeOperation nodeOperation = nodeOperationByPhaseId.get(sourcePhase.value);
                 if (nodeOperation == null) {
                     return false;
                 }
