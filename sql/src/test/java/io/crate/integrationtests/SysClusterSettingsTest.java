crate/crate

commit 0b5d5726524a7e67b35ff13506b4e6d256692e23

changes crate’s breaker limit settings to memory setting
memory setting appliers (validators) were already used
for that settings, so there are no behaviour changes

sql/src/main/java/io/crate/metadata/settings/MemorySetting.java
@@ -1,0 +1,75 @@
+/*
+ * Licensed to Crate under one or more contributor license agreements.
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.  Crate licenses this file
+ * to you under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.  You may
+ * obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.  See the License for the specific language governing
+ * permissions and limitations under the License.
+ *
+ * However, if you have executed another commercial license agreement
+ * with Crate these terms will supersede the license and you may use the
+ * software solely pursuant to the terms of the relevant commercial
+ * agreement.
+ */
+
+package io.crate.metadata.settings;
+
+import io.crate.types.DataType;
+import io.crate.types.DataTypes;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.unit.ByteSizeValue;
+
+import javax.annotation.Nullable;
+
+public class MemorySetting extends Setting<String, ByteSizeValue> {
+
+    private final String name;
+    private final String defaultValue;
+    private final boolean isRuntime;
+    private final Setting<?, ?> parent;
+
+    public MemorySetting(String name, String defaultValue, boolean isRuntime, @Nullable Setting<?, ?> parent) {
+        this.name = name;
+        this.defaultValue = defaultValue;
+        this.isRuntime = isRuntime;
+        this.parent = parent;
+    }
+
+    @Override
+    public String name() {
+        return name;
+    }
+
+    @Override
+    public String defaultValue() {
+        return defaultValue;
+    }
+
+    @Override
+    public ByteSizeValue extract(Settings settings) {
+        return settings.getAsMemory(name, defaultValue);
+    }
+
+    @Override
+    public boolean isRuntime() {
+        return isRuntime;
+    }
+
+    @Override
+    public DataType dataType() {
+        return DataTypes.STRING;
+    }
+
+    @Override
+    public Setting parent() {
+        return parent;
+    }
+}

sql/src/test/java/io/crate/integrationtests/SysClusterSettingsTest.java
@@ -24,6 +24,7 @@ package io.crate.integrationtests;
 import io.crate.metadata.settings.CrateSettings;
 import io.crate.testing.UseJdbc;
 import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.unit.MemorySizeValue;
 import org.elasticsearch.test.ESIntegTestCase;
 import org.junit.After;
 import org.junit.Test;
@@ -183,6 +184,9 @@ public class SysClusterSettingsTest extends SQLTransportIntegrationTest {
         Map<String, Map> breaker = stats.get(CrateSettings.STATS_BREAKER.name());
         Map<String, Map> log = breaker.get(CrateSettings.STATS_BREAKER_LOG.name());
         Map<String, Map> jobs = log.get(CrateSettings.STATS_BREAKER_LOG_JOBS.name());
-        assertThat(jobs.get(CrateSettings.STATS_BREAKER_LOG_JOBS_LIMIT.name()), is("5%"));
+        assertThat(jobs.get(CrateSettings.STATS_BREAKER_LOG_JOBS_LIMIT.name()),
+            is(MemorySizeValue.parseBytesSizeValueOrHeapRatio(  // convert default string value (percentage) to byte size string
+                CrateSettings.STATS_BREAKER_LOG_JOBS_LIMIT.defaultValue(),
+                CrateSettings.STATS_BREAKER_LOG_JOBS_LIMIT.settingName()).toString()));
     }
 }

sql/src/main/java/io/crate/metadata/settings/CrateSettings.java
@@ -28,6 +28,7 @@ import io.crate.breaker.CrateCircuitBreakerService;
 import org.elasticsearch.common.unit.ByteSizeUnit;
 import org.elasticsearch.common.unit.ByteSizeValue;
 import org.elasticsearch.common.unit.TimeValue;
+import org.elasticsearch.indices.breaker.HierarchyCircuitBreakerService;
 
 import javax.annotation.Nonnull;
 import java.util.List;
@@ -231,8 +232,8 @@ public class CrateSettings {
         }
     };
 
-    public static final StringSetting STATS_BREAKER_LOG_JOBS_LIMIT =
-        new StringSetting("limit", null, true, CrateCircuitBreakerService.DEFAULT_JOBS_LOG_CIRCUIT_BREAKER_LIMIT, STATS_BREAKER_LOG_JOBS);
+    public static final MemorySetting STATS_BREAKER_LOG_JOBS_LIMIT =
+        new MemorySetting("limit", CrateCircuitBreakerService.DEFAULT_JOBS_LOG_CIRCUIT_BREAKER_LIMIT, true, STATS_BREAKER_LOG_JOBS);
 
     public static final NestedSetting STATS_BREAKER_LOG_OPERATIONS = new NestedSetting() {
 
@@ -257,8 +258,8 @@ public class CrateSettings {
         }
     };
 
-    public static final StringSetting STATS_BREAKER_LOG_OPERATIONS_LIMIT =
-        new StringSetting("limit", null, true, CrateCircuitBreakerService.DEFAULT_OPERATIONS_LOG_CIRCUIT_BREAKER_LIMIT, STATS_BREAKER_LOG_OPERATIONS);
+    public static final MemorySetting STATS_BREAKER_LOG_OPERATIONS_LIMIT =
+        new MemorySetting("limit", CrateCircuitBreakerService.DEFAULT_OPERATIONS_LOG_CIRCUIT_BREAKER_LIMIT, true, STATS_BREAKER_LOG_OPERATIONS);
 
     public static final NestedSetting CLUSTER = new NestedSetting() {
         @Override
@@ -1137,8 +1138,8 @@ public class CrateSettings {
         }
     };
 
-    public static final StringSetting INDICES_FIELDDATA_BREAKER_LIMIT = new StringSetting(
-        "limit", null, true, "60%", INDICES_FIELDDATA_BREAKER);
+    public static final MemorySetting INDICES_FIELDDATA_BREAKER_LIMIT = new MemorySetting(
+        "limit", HierarchyCircuitBreakerService.DEFAULT_FIELDDATA_BREAKER_LIMIT, true, INDICES_FIELDDATA_BREAKER);
 
     public static final DoubleSetting INDICES_FIELDDATA_BREAKER_OVERHEAD = new DoubleSetting() {
         @Override
@@ -1212,8 +1213,8 @@ public class CrateSettings {
         }
     };
 
-    public static final StringSetting INDICES_BREAKER_QUERY_LIMIT = new StringSetting(
-        "limit", null, true, CrateCircuitBreakerService.DEFAULT_QUERY_CIRCUIT_BREAKER_LIMIT, INDICES_BREAKER_QUERY);
+    public static final MemorySetting INDICES_BREAKER_QUERY_LIMIT = new MemorySetting(
+        "limit", CrateCircuitBreakerService.DEFAULT_QUERY_CIRCUIT_BREAKER_LIMIT, true, INDICES_BREAKER_QUERY);
 
     public static final DoubleSetting INDICES_BREAKER_QUERY_OVERHEAD = new DoubleSetting() {
         @Override
@@ -1262,8 +1263,8 @@ public class CrateSettings {
         }
     };
 
-    public static final StringSetting INDICES_BREAKER_REQUEST_LIMIT = new StringSetting(
-        "limit", null, true, "40%", INDICES_BREAKER_REQUEST);
+    public static final MemorySetting INDICES_BREAKER_REQUEST_LIMIT = new MemorySetting(
+        "limit", HierarchyCircuitBreakerService.DEFAULT_REQUEST_BREAKER_LIMIT, true, INDICES_BREAKER_REQUEST);
 
     public static final DoubleSetting INDICES_BREAKER_REQUEST_OVERHEAD = new DoubleSetting() {
         @Override

sql/src/test/java/io/crate/metadata/settings/CrateSettingsTest.java
@@ -248,9 +248,8 @@ public class CrateSettingsTest extends CrateUnitTest {
 
     @Test
     public void testApplyMemoryCrateSetting() throws Exception {
-        StringSetting stringSetting = CrateSettings.INDICES_BREAKER_QUERY_LIMIT;
         SettingsAppliers.MemoryValueSettingsApplier intSettingsApplier =
-            new SettingsAppliers.MemoryValueSettingsApplier(stringSetting);
+            new SettingsAppliers.MemoryValueSettingsApplier(CrateSettings.INDICES_BREAKER_QUERY_LIMIT);
 
         Settings.Builder settings = Settings.builder();
         intSettingsApplier.apply(settings, Row.EMPTY, Literal.fromObject("70%"));

sql/src/test/java/io/crate/operation/reference/sys/TestGlobalSysExpressions.java
@@ -47,6 +47,7 @@ import org.elasticsearch.common.inject.Injector;
 import org.elasticsearch.common.inject.ModulesBuilder;
 import org.elasticsearch.common.inject.multibindings.MapBinder;
 import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.unit.MemorySizeValue;
 import org.elasticsearch.env.NodeEnvironment;
 import org.elasticsearch.monitor.os.OsService;
 import org.elasticsearch.monitor.os.OsStats;
@@ -167,17 +168,19 @@ public class TestGlobalSysExpressions extends CrateUnitTest {
         Map settings = settingsExpression.value();
 
         Map stats = (Map) settings.get(CrateSettings.STATS.name());
-        assertThat(CrateSettings.STATS_ENABLED.defaultValue(),
-            is(stats.get(CrateSettings.STATS_ENABLED.name())));
-        assertThat(CrateSettings.STATS_JOBS_LOG_SIZE.defaultValue(),
-            is(stats.get(CrateSettings.STATS_JOBS_LOG_SIZE.name())));
-        assertThat(CrateSettings.STATS_OPERATIONS_LOG_SIZE.defaultValue(),
-            is(stats.get(CrateSettings.STATS_OPERATIONS_LOG_SIZE.name())));
+        assertThat(stats.get(CrateSettings.STATS_ENABLED.name()),
+            is(CrateSettings.STATS_ENABLED.defaultValue()));
+        assertThat(stats.get(CrateSettings.STATS_JOBS_LOG_SIZE.name()),
+            is(CrateSettings.STATS_JOBS_LOG_SIZE.defaultValue()));
+        assertThat(stats.get(CrateSettings.STATS_OPERATIONS_LOG_SIZE.name()),
+            is(CrateSettings.STATS_OPERATIONS_LOG_SIZE.defaultValue()));
         Map statsBreakerLog = (Map) ((Map) stats.get(CrateSettings.STATS_BREAKER.name()))
             .get(CrateSettings.STATS_BREAKER_LOG.name());
         Map statsBreakerLogJobs = (Map) statsBreakerLog.get(CrateSettings.STATS_BREAKER_LOG_JOBS.name());
-        assertThat(CrateSettings.STATS_BREAKER_LOG_JOBS_LIMIT.defaultValue(),
-            is(statsBreakerLogJobs.get(CrateSettings.STATS_BREAKER_LOG_JOBS_LIMIT.name())));
+        assertThat(statsBreakerLogJobs.get(CrateSettings.STATS_BREAKER_LOG_JOBS_LIMIT.name()),
+            is(MemorySizeValue.parseBytesSizeValueOrHeapRatio(  // convert default string value (percentage) to byte size string
+                CrateSettings.STATS_BREAKER_LOG_JOBS_LIMIT.defaultValue(),
+                CrateSettings.STATS_BREAKER_LOG_JOBS_LIMIT.settingName()).toString()));
 
         Map cluster = (Map) settings.get(CrateSettings.CLUSTER.name());
         Map gracefulStop = (Map) cluster.get(CrateSettings.GRACEFUL_STOP.name());

commit a8f4161583973f4eae87ce33d6278862520ecdaf

fix bug that returned always default memory settings
this was caused by 0b5d5726524a7e67b35ff13506b4e6d256692e23

sql/src/main/java/io/crate/metadata/settings/MemorySetting.java
@@ -55,7 +55,7 @@ public class MemorySetting extends Setting<String, ByteSizeValue> {
 
     @Override
     public ByteSizeValue extract(Settings settings) {
-        return settings.getAsMemory(name, defaultValue);
+        return settings.getAsMemory(settingName(), defaultValue);
     }
 
     @Override

sql/src/test/java/io/crate/integrationtests/SysClusterSettingsTest.java
@@ -46,7 +46,7 @@ public class SysClusterSettingsTest extends SQLTransportIntegrationTest {
 
     @After
     public void resetSettings() throws Exception {
-        execute("reset global stats.operations_log_size, bulk.request_timeout");
+        execute("reset global stats.operations_log_size, bulk.request_timeout, \"indices.breaker.query.limit\"");
     }
 
     @Test
@@ -76,6 +76,15 @@ public class SysClusterSettingsTest extends SQLTransportIntegrationTest {
         assertThat((Boolean) response.rows()[0][0], is(false));
         assertThat((Integer) response.rows()[0][1], is(10_000));
         assertThat((Integer) response.rows()[0][2], is(10_000));
+
+        execute("set global transient \"indices.breaker.query.limit\" = '2mb'");
+        execute("select settings['indices'] from sys.cluster");
+        Map<String, Map> indices = (Map<String, Map>) response.rows()[0][0];
+        Map<String, Map> breaker = indices.get(CrateSettings.INDICES_BREAKER.name());
+        Map<String, Map> query = breaker.get(CrateSettings.INDICES_BREAKER_QUERY.name());
+        assertThat(query.get(CrateSettings.INDICES_BREAKER_QUERY_LIMIT.name()),
+            is(MemorySizeValue.parseBytesSizeValueOrHeapRatio("2mb",
+                CrateSettings.INDICES_BREAKER_QUERY_LIMIT.settingName()).toString()));
     }
 
     @Test