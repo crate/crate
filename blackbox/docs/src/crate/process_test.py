7d110e65458b93dae684537be589c012f43a5a97
Marios Trivyzas <marios@crate.io>
2017-01-26 15:44:07 +0100

    Fixed issues with setup of itest/sigar/sqllogictest

--- blackbox/docs/src/crate/process_test.py
+++ blackbox/docs/src/crate/process_test.py
@@ -107,7 +107,7 @@ class GracefulStopTest(unittest.TestCase):
             layer = GracefulStopCrateLayer(
                 self.node_name(i),
                 crate_path(),
-                host='_local_',
+                host='localhost',
                 port=GLOBAL_PORT_POOL.get(),
                 transport_port=GLOBAL_PORT_POOL.get(),
                 cluster_name=self.__class__.__name__)
