9f1877044d74a7eb73dce0b898b6d47e653c8bf3
Mathias Fussenegger <f.mathias@zignar.net>
2017-02-07 11:02:04 +0100

    Replace TransportActionProvider dependency with TransportNodeStatsAction

--- sql/src/main/java/io/crate/executor/transport/TransportActionProvider.java
+++ sql/src/main/java/io/crate/executor/transport/TransportActionProvider.java
@@ -74,7 +74,6 @@ public class TransportActionProvider {
     private final Provider<TransportCreateSnapshotAction> transportCreateSnapshotActionProvider;
     private final Provider<TransportDeleteSnapshotAction> transportDeleteSnapshotActionProvider;
     private final Provider<TransportRestoreSnapshotAction> transportRestoreSnapshotActionProvider;
-    private final Provider<TransportNodeStatsAction> transportStatTablesActionProvider;
 
     @Inject
     public TransportActionProvider(Provider<TransportFetchNodeAction> transportFetchNodeActionProvider,
@@ -98,8 +97,7 @@ public class TransportActionProvider {
                                    Provider<TransportKillJobsNodeAction> transportKillJobsNodeActionProvider,
                                    Provider<TransportDeleteSnapshotAction> transportDeleteSnapshotActionProvider,
                                    Provider<TransportCreateSnapshotAction> transportCreateSnapshotActionProvider,
-                                   Provider<TransportRestoreSnapshotAction> transportRestoreSnapshotActionProvider,
-                                   Provider<TransportNodeStatsAction> transportStatTablesActionProvider) {
+                                   Provider<TransportRestoreSnapshotAction> transportRestoreSnapshotActionProvider) {
         this.transportCreateIndexActionProvider = transportCreateIndexActionProvider;
         this.transportDeleteIndexActionProvider = transportDeleteIndexActionProvider;
         this.transportPutIndexTemplateActionProvider = transportPutIndexTemplateActionProvider;
@@ -122,7 +120,6 @@ public class TransportActionProvider {
         this.transportDeleteSnapshotActionProvider = transportDeleteSnapshotActionProvider;
         this.transportCreateSnapshotActionProvider = transportCreateSnapshotActionProvider;
         this.transportRestoreSnapshotActionProvider = transportRestoreSnapshotActionProvider;
-        this.transportStatTablesActionProvider = transportStatTablesActionProvider;
     }
 
     public TransportCreateIndexAction transportCreateIndexAction() {
@@ -211,9 +208,5 @@ public class TransportActionProvider {
 
     public TransportRestoreSnapshotAction transportRestoreSnapshotAction() {
         return transportRestoreSnapshotActionProvider.get();
-    }
-
-    public TransportNodeStatsAction transportStatTablesActionProvider() {
-        return transportStatTablesActionProvider.get();
     }
 }
