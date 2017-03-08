eefec6e25b2f680f6a8706d2b8b1ddeb50ec7c70
Mathias Fussenegger <f.mathias@zignar.net>
2017-03-08 13:59:42 +0100

    Add BatchIteratorProjector implementation to SysUpdateProjector

--- sql/src/main/java/io/crate/operation/projectors/SysUpdateProjector.java
+++ sql/src/main/java/io/crate/operation/projectors/SysUpdateProjector.java
@@ -22,15 +22,19 @@
 
 package io.crate.operation.projectors;
 
+import io.crate.data.BatchIteratorProjector;
+import io.crate.data.CollectingBatchIterator;
 import io.crate.data.Row;
 import io.crate.data.Row1;
 
+import javax.annotation.Nullable;
+import java.util.Collections;
 import java.util.function.Consumer;
+import java.util.stream.Collector;
 
 class SysUpdateProjector extends AbstractProjector {
 
     private final Consumer<Object> rowWriter;
-    private long rowCount = 0;
 
     SysUpdateProjector(Consumer<Object> rowWriter)  {
         this.rowWriter = rowWriter;
@@ -39,20 +43,40 @@ class SysUpdateProjector extends AbstractProjector {
     @SuppressWarnings("unchecked")
     @Override
     public Result setNextRow(Row row) {
-        Object id = row.get(0);
-        rowWriter.accept(id);
-        rowCount++;
-        return Result.CONTINUE;
+        throw new UnsupportedOperationException("SysUpdateProjector should be used as BatchIteratorProjector");
     }
 
     @Override
     public void finish(RepeatHandle repeatHandle) {
-        downstream.setNextRow(new Row1(rowCount));
-        downstream.finish(RepeatHandle.UNSUPPORTED);
+        throw new UnsupportedOperationException("SysUpdateProjector should be used as BatchIteratorProjector");
     }
 
     @Override
     public void fail(Throwable throwable) {
-        downstream.fail(throwable);
+        throw new UnsupportedOperationException("SysUpdateProjector should be used as BatchIteratorProjector");
+    }
+
+    @Nullable
+    @Override
+    public BatchIteratorProjector asProjector() {
+        return bi -> CollectingBatchIterator.newInstance(bi,
+            Collector.of(
+                () -> new State(rowWriter),
+                (state, row) -> {
+                    state.rowWriter.accept(row.get(0));
+                    state.rowCount++;
+                },
+                (state1, state2) -> { throw new UnsupportedOperationException("Combine not supported"); },
+                state -> Collections.singletonList(new Row1(state.rowCount))
+            ), 1);
+    }
+
+    private static class State {
+        final Consumer<Object> rowWriter;
+        long rowCount = 0;
+
+        State(Consumer<Object> rowWriter) {
+            this.rowWriter = rowWriter;
+        }
     }
 }
