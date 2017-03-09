d5cedb05472416261f647ff0058707ad2a2b34ff
Andrei Dan <andrei.dan@crate.io>
2017-03-09 15:28:38 +0000

    Create AsyncCompositeBatchIterator and use it in ShardCollectSource instead of the MultiUpstreamRowReceiver

--- dex/src/main/java/io/crate/data/AsyncCompositeBatchIterator.java
+++ dex/src/main/java/io/crate/data/AsyncCompositeBatchIterator.java
@@ -24,33 +24,34 @@ package io.crate.data;
 
 import io.crate.concurrent.CompletableFutures;
 
-import java.util.Arrays;
-import java.util.Comparator;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
+import java.util.concurrent.Executor;
+import java.util.function.Supplier;
 
 /**
- * BatchIterator implementation that is backed by multiple other BatchIterators.
- * <p>
- * It will consume each iterator to it's end before consuming the next iterator in order to make sure that repeated
- * consumption via {@link #moveToStart()} always returns the same result.
+ * Similar to {@link CompositeBatchIterator} this is a BatchIterator implementation which is backed by multiple
+ * other BatchIterators.
+ *
+ * The difference is that this will eagerly load all sources in {@link #loadNextBatch()} using a separate executor.
+ * E.g. if the source BatchIterators are {@link CollectingBatchIterator} which can have a fairly expensive {@link #loadNextBatch()}
+ * their work can be executed concurrently.
  */
-public class CompositeBatchIterator implements BatchIterator {
+public class AsyncCompositeBatchIterator implements BatchIterator {
 
     private final BatchIterator[] iterators;
     private final CompositeColumns columns;
-    private int idx = 0;
+    private final Executor executor;
 
+    private int idx = 0;
     private boolean loading = false;
 
-    /**
-     * @param iterators underlying iterators to use; order of consumption may change if some of them are unloaded
-     *                  to prefer loaded iterators over unloaded.
-     */
-    public CompositeBatchIterator(BatchIterator... iterators) {
+    public AsyncCompositeBatchIterator(Executor executor, BatchIterator... iterators) {
         assert iterators.length > 0 : "Must have at least 1 iterator";
 
-        // prefer loaded iterators over unloaded to improve performance in case only a subset of data is consumed
-        Arrays.sort(iterators, Comparator.comparing(BatchIterator::allLoaded).reversed());
+        this.executor = executor;
         this.iterators = iterators;
         this.columns = new CompositeColumns(iterators);
     }
@@ -82,9 +83,6 @@ public class CompositeBatchIterator implements BatchIterator {
             if (iterator.moveNext()) {
                 return true;
             }
-            if (iterator.allLoaded() == false) {
-                return false;
-            }
             idx++;
             columns.updateInputs(idx);
         }
@@ -104,14 +102,35 @@ public class CompositeBatchIterator implements BatchIterator {
         if (loading) {
             return CompletableFutures.failedFuture(new IllegalStateException("BatchIterator already loading"));
         }
+        if (allLoaded()) {
+            return CompletableFutures.failedFuture(new IllegalStateException("BatchIterator already loaded"));
+        }
+
+        List<CompletableFuture<CompletableFuture<?>>> asyncInvocationFutures = new ArrayList<>();
+        loading = true;
         for (BatchIterator iterator : iterators) {
             if (iterator.allLoaded()) {
                 continue;
             }
-            loading = true;
-            return iterator.loadNextBatch().whenComplete((r, t) -> loading = false);
+            Supplier<CompletableFuture<?>> loadNextBatchFuture = () -> iterator.loadNextBatch().toCompletableFuture();
+
+            // Every "supplyAsync future" completes with the iterator::loadNextBatch() future, so in order to
+            // access the loadNextBatch future, the asyncInvocation one should be completed and the result extracted
+            CompletableFuture<CompletableFuture<?>> supplyAsyncFuture =
+                CompletableFuture.supplyAsync(loadNextBatchFuture, executor);
+            asyncInvocationFutures.add(supplyAsyncFuture);
         }
-        return CompletableFutures.failedFuture(new IllegalStateException("BatchIterator already fully loaded"));
+
+        CompletableFuture<List<CompletableFuture<?>>> loadNextBatchListFuture =
+            CompletableFutures.allAsList(asyncInvocationFutures);
+
+        return loadNextBatchListFuture.thenCompose(
+            (List<CompletableFuture<?>> loadNextBatchFutures) ->
+                // Future that'll wait for all loadNextBatch futures to complete
+                CompletableFuture.allOf(loadNextBatchFutures.toArray(new CompletableFuture[0]))
+                    // loading is complete when all loadNextBatch futures are complete
+                    .whenComplete((r, t) -> loading = false)
+        );
     }
 
     @Override
