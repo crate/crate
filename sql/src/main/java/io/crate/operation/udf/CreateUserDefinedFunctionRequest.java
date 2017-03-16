3c26579008cc26a047ae13f23d70fd00d1198aa8
Philipp Bogensberger <philipp@crate.io>
2017-03-16 18:20:28 +0700

    add create user defined function action

--- sql/src/main/java/io/crate/operation/udf/CreateUserDefinedFunctionRequest.java
+++ sql/src/main/java/io/crate/operation/udf/CreateUserDefinedFunctionRequest.java
@@ -1,30 +1,34 @@
 /*
- * Licensed to Crate under one or more contributor license agreements.
- * See the NOTICE file distributed with this work for additional
- * information regarding copyright ownership.  Crate licenses this file
- * to you under the Apache License, Version 2.0 (the "License"); you may
- * not use this file except in compliance with the License.  You may
+ * Licensed to Crate.io Inc. ("Crate.io") under one or more contributor
+ * license agreements.  See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.  Crate.io licenses
+ * this file to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.  You may
  * obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
- * implied.  See the License for the specific language governing
- * permissions and limitations under the License.
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
  *
- * However, if you have executed another commercial license agreement
- * with Crate these terms will supersede the license and you may use the
- * software solely pursuant to the terms of the relevant commercial
- * agreement.
+ * To enable or use any of the enterprise features, Crate.io must have given
+ * you permission to enable and use the Enterprise Edition of CrateDB and you
+ * must have a valid Enterprise or Subscription Agreement with Crate.io.  If
+ * you enable or use features that are part of the Enterprise Edition, you
+ * represent and warrant that you have a valid Enterprise or Subscription
+ * Agreement with Crate.io.  Your use of features of the Enterprise Edition
+ * is governed by the terms and conditions of your Enterprise or Subscription
+ * Agreement with Crate.io.
  */
 
 package io.crate.operation.udf;
 
-import org.elasticsearch.action.ActionRequestValidationException;
-import org.elasticsearch.action.ValidateActions;
+import org.elasticsearch.action.*;
 import org.elasticsearch.action.support.master.MasterNodeRequest;
+import org.elasticsearch.client.ElasticsearchClient;
 import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.io.stream.StreamOutput;
 
@@ -72,5 +76,18 @@ public class CreateUserDefinedFunctionRequest extends MasterNodeRequest<CreateUs
         super.writeTo(out);
         userDefinedFunctionMetaData.writeTo(out);
         out.writeBoolean(replace);
+    }
+
+    static class RequestBuilder extends ActionRequestBuilder<CreateUserDefinedFunctionRequest,
+        CreateUserDefinedFunctionResponse, RequestBuilder> {
+
+        RequestBuilder(ElasticsearchClient client, CreateUserDefinedFunctionAction action) {
+            super(client, action, new CreateUserDefinedFunctionRequest());
+        }
+
+        @Override
+        public void execute(ActionListener<CreateUserDefinedFunctionResponse> listener) {
+            client.execute(CreateUserDefinedFunctionAction.INSTANCE, request, listener);
+        }
     }
 }
