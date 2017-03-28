crate/crate

commit f1c0595b2dbe485a6626231f98603782a1bbff48

Rename docId to fetchId and add a FetchId class
The term `docId` was used for two different things. This renames one
usage to `fetchId` to avoid confusion.

sql/src/main/java/io/crate/operation/projectors/fetch/FetchId.java
@@ -1,0 +1,54 @@
+/*
+ * Licensed to Crate under one or more contributor license agreements.
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.  Crate licenses this file
+ * to you under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.  You may
+ * obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.  See the License for the specific language governing
+ * permissions and limitations under the License.
+ *
+ * However, if you have executed another commercial license agreement
+ * with Crate these terms will supersede the license and you may use the
+ * software solely pursuant to the terms of the relevant commercial
+ * agreement.
+ */
+
+package io.crate.operation.projectors.fetch;
+
+/**
+ * Utility class to pack/unpack a fetchId.
+ * <p>
+ *  A fetchId is a long which contains a readerId and a docId.
+ *
+ * readerIds are generated in the planner-phase. They're used to identify a shard/indexReader.
+ * docIds are part of the lucene api, where they're used to identify documents within an IndexReader.
+ * </p>
+ *
+ * <pre>
+ *      4 bytes     |  4 bytes
+ *         |             |
+ *       readerId      docId
+ * </pre>
+ */
+public final class FetchId {
+
+
+    static int decodeReaderId(long fetchId) {
+        return (int) (fetchId >> 32);
+    }
+
+    static int decodeDocId(long fetchId) {
+        return (int) fetchId;
+    }
+
+    public static long encode(int readerId, int docId) {
+        return ((long) readerId << 32) | (docId & 0xfffL);
+    }
+}

sql/src/test/java/io/crate/operation/projectors/fetch/FetchIdTest.java
@@ -1,0 +1,39 @@
+/*
+ * Licensed to Crate under one or more contributor license agreements.
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.  Crate licenses this file
+ * to you under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.  You may
+ * obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.  See the License for the specific language governing
+ * permissions and limitations under the License.
+ *
+ * However, if you have executed another commercial license agreement
+ * with Crate these terms will supersede the license and you may use the
+ * software solely pursuant to the terms of the relevant commercial
+ * agreement.
+ */
+
+package io.crate.operation.projectors.fetch;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class FetchIdTest {
+
+    @Test
+    public void testEncodeAndDecode() throws Exception {
+        long fetchId = FetchId.encode(10, 3248);
+
+        assertThat(FetchId.decodeDocId(fetchId), is(3248));
+        assertThat(FetchId.decodeReaderId(fetchId), is(10));
+    }
+}

sql/src/main/java/io/crate/analyze/fetch/FetchFieldExtractor.java
@@ -89,7 +89,7 @@ public class FetchFieldExtractor {
     private static class IsFetchableVisitor extends AnalyzedRelationVisitor<Field, Boolean> {
 
         private static final IsFetchableVisitor INSTANCE = new IsFetchableVisitor();
-        private static final Set<Path> NOT_FETCHABLE = ImmutableSet.<Path>of(DocSysColumns.SCORE, DocSysColumns.DOCID);
+        private static final Set<Path> NOT_FETCHABLE = ImmutableSet.<Path>of(DocSysColumns.SCORE, DocSysColumns.FETCHID);
 
         public static Boolean isFetchable(Field field) {
             return INSTANCE.process(field.relation(), field);

sql/src/main/java/io/crate/analyze/relations/DocTableRelation.java
@@ -41,7 +41,7 @@ import java.util.Optional;
 
 public class DocTableRelation extends AbstractTableRelation<DocTableInfo> {
 
-    private static final ImmutableSet<ColumnIdent> HIDDEN_COLUMNS = ImmutableSet.of(DocSysColumns.DOCID);
+    private static final ImmutableSet<ColumnIdent> HIDDEN_COLUMNS = ImmutableSet.of(DocSysColumns.FETCHID);
     private final static SortValidator SORT_VALIDATOR = new SortValidator();
 
     private static class SortValidator extends SymbolVisitor<DocTableRelation, Void> {

sql/src/main/java/io/crate/analyze/symbol/DefaultTraversalSymbolVisitor.java
@@ -33,7 +33,7 @@ public abstract class DefaultTraversalSymbolVisitor<C, R> extends SymbolVisitor<
 
     @Override
     public R visitFetchReference(FetchReference fetchReference, C context) {
-        process(fetchReference.docId(), context);
+        process(fetchReference.fetchId(), context);
         process(fetchReference.ref(), context);
         return super.visitFetchReference(fetchReference, context);
     }

sql/src/main/java/io/crate/analyze/symbol/FetchReference.java
@@ -31,11 +31,11 @@ import java.io.IOException;
 
 public class FetchReference extends Symbol {
 
-    private final Symbol docId;
+    private final Symbol fetchId;
     private final Reference ref;
 
-    public FetchReference(Symbol docId, Reference ref) {
-        this.docId = docId;
+    public FetchReference(Symbol fetchId, Reference ref) {
+        this.fetchId = fetchId;
         this.ref = ref;
     }
 
@@ -54,8 +54,8 @@ public class FetchReference extends Symbol {
         return ref.valueType();
     }
 
-    public Symbol docId() {
-        return docId;
+    public Symbol fetchId() {
+        return fetchId;
     }
 
     public Reference ref() {

sql/src/main/java/io/crate/analyze/symbol/SymbolVisitors.java
@@ -50,7 +50,7 @@ public class SymbolVisitors {
         @Override
         public Boolean visitFetchReference(FetchReference fetchReference, Predicate<? super Symbol> symbolPredicate) {
             return symbolPredicate.apply(fetchReference)
-                   || fetchReference.docId().accept(this, symbolPredicate)
+                   || fetchReference.fetchId().accept(this, symbolPredicate)
                    || fetchReference.ref().accept(this, symbolPredicate);
         }
 

sql/src/main/java/io/crate/analyze/symbol/Symbols.java
@@ -128,7 +128,7 @@ public class Symbols {
 
         @Override
         public Boolean visitFetchReference(FetchReference fetchReference, ColumnIdent context) {
-            if (process(fetchReference.docId(), context)) {
+            if (process(fetchReference.fetchId(), context)) {
                 return true;
             }
             return process(fetchReference.ref(), context);

commit 1ed9cd5c87d003b1fd896ef492a398d64aed5de1

Fix fetchId encoding
f1c0595b2dbe485a6626231f98603782a1bbff48 broke the encoding of fetchId,
causing queries to return wrong values.

sql/src/main/java/io/crate/operation/projectors/fetch/FetchId.java
@@ -49,6 +49,6 @@ public final class FetchId {
     }
 
     public static long encode(int readerId, int docId) {
-        return ((long) readerId << 32) | (docId & 0xfffL);
+        return ((long) readerId << 32) | (docId & 0xffffffffL);
     }
 }

sql/src/test/java/io/crate/operation/projectors/fetch/FetchIdTest.java
@@ -22,18 +22,25 @@
 
 package io.crate.operation.projectors.fetch;
 
+import io.crate.test.integration.CrateUnitTest;
 import org.junit.Test;
 
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
+import java.util.stream.LongStream;
 
-public class FetchIdTest {
+import static org.hamcrest.core.Is.is;
+
+public class FetchIdTest extends CrateUnitTest {
 
     @Test
     public void testEncodeAndDecode() throws Exception {
-        long fetchId = FetchId.encode(10, 3248);
+        LongStream longs = random().longs(50, 0, Long.MAX_VALUE);
+        longs.forEach(this::assertEncodeDecodeMatches);
+    }
 
-        assertThat(FetchId.decodeDocId(fetchId), is(3248));
-        assertThat(FetchId.decodeReaderId(fetchId), is(10));
+    private void assertEncodeDecodeMatches(long val) {
+        int docId = FetchId.decodeDocId(val);
+        int readerId = FetchId.decodeReaderId(val);
+
+        assertThat(FetchId.encode(readerId, docId), is(val));
     }
 }