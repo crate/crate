0858e55a8bf56bf32d1851f2dfab1ef2acba0a64
Mathias Fussenegger <f.mathias@zignar.net>
2017-05-18 16:47:10 +0200

    Prevent `KILL` or `KILL ALL` from killing the whole node
    This fixes a nasty race condition which lead to the termination of a
    node.
    
    Since ES5 there is an uncaught exception handler which terminates the
    node if an uncaught exception is encountered which is not a
    RuntimeException. (That's a good thing)
    
    With the BatchIterator introduction we've added consumers which had a
    bug: They didn't handle exceptions on `batchIterator.allLoaded` calls -
    these were simply uncaught and could lead to a query getting stuck.
    
    Now the only cases where `allLoaded` thows an exception is due to a
    kill: Either internal or user provoked with the `KILL (ALL)` statement.

--- sql/src/main/java/io/crate/action/sql/BatchConsumerToResultReceiver.java
+++ sql/src/main/java/io/crate/action/sql/BatchConsumerToResultReceiver.java
@@ -56,6 +56,7 @@ public class BatchConsumerToResultReceiver implements BatchConsumer {
 
     private void consumeIt(BatchIterator iterator) {
         Row row = RowBridging.toRow(iterator.rowData());
+        boolean allLoaded;
         try {
             while (iterator.moveNext()) {
                 rowCount++;
@@ -67,12 +68,13 @@ public class BatchConsumerToResultReceiver implements BatchConsumer {
                     return; // resumed via postgres protocol, close is done later
                 }
             }
+            allLoaded = iterator.allLoaded();
         } catch (Throwable t) {
             iterator.close();
             resultReceiver.fail(t);
             return;
         }
-        if (iterator.allLoaded()) {
+        if (allLoaded) {
             iterator.close();
             resultReceiver.allFinished(false);
         } else {
