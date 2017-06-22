1099c1fa5324817a3a4888deddb69f4df2f55add
Andrei Dan <andrei@crate.io>
2017-06-22 13:54:39 +0100

    Analyzer support for `DENY` privileges statement.

--- sql-parser/src/main/java/io/crate/sql/tree/RevokePrivilege.java
+++ sql-parser/src/main/java/io/crate/sql/tree/RevokePrivilege.java
@@ -1,57 +1,20 @@
 package io.crate.sql.tree;
 
-import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
 
-public class RevokePrivilege extends Statement {
-
-    private final List<String> userNames;
-    private final List<String> privilegeTypes;
-    private final boolean all;
+public class RevokePrivilege extends PrivilegeStatement {
 
     public RevokePrivilege(List<String> userNames) {
-        this.userNames = userNames;
-        privilegeTypes = Collections.emptyList();
-        all = true;
+        super(userNames);
     }
 
     public RevokePrivilege(List<String> userNames, List<String> privilegeTypes) {
-        this.privilegeTypes = privilegeTypes;
-        this.userNames = userNames;
-        all = false;
-    }
-
-    public List<String> privileges() {
-        return privilegeTypes;
-    }
-
-    public List<String> userNames() {
-        return userNames;
-    }
-
-    public boolean all() {
-        return all;
+        super(userNames, privilegeTypes);
     }
 
     @Override
     public <R, C> R accept(AstVisitor<R, C> visitor, C context) {
         return visitor.visitRevokePrivilege(this, context);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        RevokePrivilege that = (RevokePrivilege) o;
-        return all == that.all &&
-               Objects.equals(userNames, that.userNames) &&
-               Objects.equals(privilegeTypes, that.privilegeTypes);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(userNames, privilegeTypes, all);
     }
 
     @Override
