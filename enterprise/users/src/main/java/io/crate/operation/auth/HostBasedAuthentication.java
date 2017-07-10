crate/crate

commit c6cdb244c315ca555817a0cd5c64626de07de0de

Re-work user functionality loading mechanism
This replaces the (ES) UsersPlugin with a UsersExtension class that is
loaded using a ServiceLoader from within the SQLPlugin.

This solves a ClassCastException that occured due to the usage of two
independent URLClassLoader instances. (One from the PluginService, one
from the EnterpriseLoader). This resulted in the loss of custom metadata
since the ES52 upgrade on node start, as the metadata couldn't be read
anymore.

enterprise/users/src/main/java/io/crate/operation/auth/HostBasedAuthentication.java
@@ -22,6 +22,7 @@ import com.google.common.annotations.VisibleForTesting;
 import com.google.common.collect.ImmutableMap;
 import io.crate.operation.user.UserLookup;
 import io.crate.protocols.postgres.ConnectionProperties;
+import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.common.network.Cidrs;
 import org.elasticsearch.common.network.InetAddresses;
 import org.elasticsearch.common.settings.Settings;
@@ -73,9 +74,10 @@ public class HostBasedAuthentication implements Authentication {
 
     private final Map<String, Supplier<AuthenticationMethod>> authMethodRegistry = new HashMap<>();
 
+    @Inject
     public HostBasedAuthentication(Settings settings, UserLookup userLookup) {
-        enabled = AuthenticationProvider.AUTH_HOST_BASED_ENABLED_SETTING.setting().get(settings);
-        hbaConf = convertHbaSettingsToHbaConf(AuthenticationProvider.AUTH_HOST_BASED_CONFIG_SETTING.setting().get(settings));
+        enabled = AuthSettings.AUTH_HOST_BASED_ENABLED_SETTING.setting().get(settings);
+        hbaConf = convertHbaSettingsToHbaConf(AuthSettings.AUTH_HOST_BASED_CONFIG_SETTING.setting().get(settings));
         authMethodRegistry.put(TrustAuthentication.NAME, () -> new TrustAuthentication(userLookup));
         authMethodRegistry.put(ClientCertAuth.NAME, () -> new ClientCertAuth(userLookup));
     }

enterprise/users/src/main/java/io/crate/plugin/AuthenticationHttpAuthHandlerRegistry.java
@@ -20,34 +20,28 @@
  * agreement.
  */
 
-package io.crate.operation.auth;
+package io.crate.plugin;
 
-import io.crate.plugin.EnterpriseLoader;
-import io.crate.settings.SharedSettings;
-import org.elasticsearch.common.Nullable;
-import org.elasticsearch.common.io.PathUtils;
+import io.crate.operation.auth.Authentication;
+import io.crate.protocols.http.HttpAuthUpstreamHandler;
+import org.elasticsearch.common.inject.Inject;
+import org.elasticsearch.common.inject.Singleton;
 import org.elasticsearch.common.settings.Settings;
 
-import java.io.IOException;
-import java.nio.file.Path;
+@Singleton
+public class AuthenticationHttpAuthHandlerRegistry {
 
-import static org.elasticsearch.common.Strings.cleanPath;
-import static org.elasticsearch.env.Environment.PATH_HOME_SETTING;
-
-public final class UserServiceFactoryLoader {
-
-    @Nullable
-    public static UserServiceFactory load(Settings settings) {
-        if (!SharedSettings.ENTERPRISE_LICENSE_SETTING.setting().get(settings)) {
-            return null;
-        }
-        Path crateHomeDir = PathUtils.get(cleanPath(PATH_HOME_SETTING.get(settings)));
-        Path esPlugins = crateHomeDir.resolve("lib").resolve("enterprise").resolve("es-plugins");
-
-        try {
-            return EnterpriseLoader.loadSingle(esPlugins, UserServiceFactory.class);
-        } catch (IOException e) {
-            return null;
+    @Inject
+    public AuthenticationHttpAuthHandlerRegistry(Settings settings,
+                                                 PipelineRegistry pipelineRegistry,
+                                                 Authentication authentication) {
+        if (authentication.enabled()) {
+            PipelineRegistry.ChannelPipelineItem pipelineItem = new PipelineRegistry.ChannelPipelineItem(
+                "blob_handler",
+                "auth_handler",
+                () -> new HttpAuthUpstreamHandler(settings, authentication)
+            );
+            pipelineRegistry.addBefore(pipelineItem);
         }
     }
 }

enterprise/users/src/main/java/io/crate/plugin/AuthenticationModule.java
@@ -22,29 +22,15 @@
 
 package io.crate.plugin;
 
+import io.crate.operation.auth.Authentication;
+import io.crate.operation.auth.HostBasedAuthentication;
+import org.elasticsearch.common.inject.AbstractModule;
 
-import org.elasticsearch.common.Nullable;
+public class AuthenticationModule extends AbstractModule {
 
-import java.util.Iterator;
-import java.util.ServiceConfigurationError;
-import java.util.ServiceLoader;
-
-/**
- * Utility class to load classes dynamically using a serviceLoader.
- * (Classes are discovered using META-INF/services files)
- */
-public final class EnterpriseLoader {
-
-    @Nullable
-    public static <T> T loadSingle(Class<T> clazz) {
-        Iterator<T> it = ServiceLoader.load(clazz).iterator();
-        T instance = null;
-        while (it.hasNext()) {
-            if (instance != null) {
-                throw new ServiceConfigurationError(clazz.getSimpleName() + " found twice");
-            }
-            instance = it.next();
-        }
-        return instance;
+    @Override
+    protected void configure() {
+        bind(Authentication.class).to(HostBasedAuthentication.class);
+        bind(AuthenticationHttpAuthHandlerRegistry.class).asEagerSingleton();
     }
 }

enterprise/users/src/main/java/io/crate/plugin/EnterpriseUsersExtension.java
@@ -21,28 +21,20 @@ package io.crate.plugin;
 import io.crate.metadata.UsersMetaData;
 import io.crate.metadata.UsersPrivilegesMetaData;
 import io.crate.scalar.UsersScalarFunctionModule;
-import io.crate.settings.SharedSettings;
+import io.crate.user.UserExtension;
 import org.elasticsearch.cluster.NamedDiff;
 import org.elasticsearch.cluster.metadata.MetaData;
 import org.elasticsearch.common.ParseField;
 import org.elasticsearch.common.inject.Module;
 import org.elasticsearch.common.io.stream.NamedWriteableRegistry;
-import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.xcontent.NamedXContentRegistry;
-import org.elasticsearch.plugins.Plugin;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 
-public class UsersPlugin extends Plugin {
-
-    private final Settings settings;
-
-    public UsersPlugin(Settings settings) {
-        this.settings = settings;
-    }
+public class EnterpriseUsersExtension implements UserExtension {
 
     @Override
     public List<NamedWriteableRegistry.Entry> getNamedWriteables() {
@@ -75,12 +67,12 @@ public class UsersPlugin extends Plugin {
     public List<NamedXContentRegistry.Entry> getNamedXContent() {
         List<NamedXContentRegistry.Entry> entries = new ArrayList<>(2);
         entries.add(new NamedXContentRegistry.Entry(
-            UsersMetaData.class,
+            MetaData.Custom.class,
             new ParseField(UsersMetaData.TYPE),
             UsersMetaData::fromXContent
         ));
         entries.add(new NamedXContentRegistry.Entry(
-            UsersPrivilegesMetaData.class,
+            MetaData.Custom.class,
             new ParseField(UsersPrivilegesMetaData.TYPE),
             UsersPrivilegesMetaData::fromXContent
         ));
@@ -88,11 +80,10 @@ public class UsersPlugin extends Plugin {
     }
 
     @Override
-    public Collection<Module> createGuiceModules() {
-        if (SharedSettings.ENTERPRISE_LICENSE_SETTING.setting().get(settings)) {
-            return Collections.singletonList(new UsersScalarFunctionModule());
-        } else {
-            return Collections.emptyList();
-        }
+    public Collection<Module> getModules() {
+        return Arrays.asList(
+            new UserManagementModule(),
+            new AuthenticationModule(),
+            new UsersScalarFunctionModule());
     }
 }

sql/src/main/java/io/crate/operation/auth/AlwaysOKAuthentication.java
@@ -20,31 +20,32 @@
  * agreement.
  */
 
-package io.crate.plugin;
+package io.crate.operation.auth;
 
+import io.crate.operation.user.User;
+import io.crate.protocols.postgres.ConnectionProperties;
 
-import org.elasticsearch.common.Nullable;
+public class AlwaysOKAuthentication implements Authentication {
 
-import java.util.Iterator;
-import java.util.ServiceConfigurationError;
-import java.util.ServiceLoader;
-
-/**
- * Utility class to load classes dynamically using a serviceLoader.
- * (Classes are discovered using META-INF/services files)
- */
-public final class EnterpriseLoader {
-
-    @Nullable
-    public static <T> T loadSingle(Class<T> clazz) {
-        Iterator<T> it = ServiceLoader.load(clazz).iterator();
-        T instance = null;
-        while (it.hasNext()) {
-            if (instance != null) {
-                throw new ServiceConfigurationError(clazz.getSimpleName() + " found twice");
-            }
-            instance = it.next();
+    private final AuthenticationMethod alwaysOk = new AuthenticationMethod() {
+        @Override
+        public User authenticate(String userName, ConnectionProperties connectionProperties) {
+            return null;
         }
-        return instance;
+
+        @Override
+        public String name() {
+            return "alwaysOk";
+        }
+    };
+
+    @Override
+    public boolean enabled() {
+        return true;
+    }
+
+    @Override
+    public AuthenticationMethod resolveAuthenticationType(String user, ConnectionProperties connectionProperties) {
+        return alwaysOk;
     }
 }

sql/src/main/java/io/crate/plugin/SQLPlugin.java
@@ -42,7 +42,7 @@ import io.crate.metadata.settings.CrateSettings;
 import io.crate.metadata.sys.MetaDataSysModule;
 import io.crate.monitor.MonitorModule;
 import io.crate.operation.aggregation.impl.AggregationImplModule;
-import io.crate.operation.auth.AuthenticationProvider;
+import io.crate.operation.auth.AuthSettings;
 import io.crate.operation.collect.CollectOperationModule;
 import io.crate.operation.collect.files.FileCollectModule;
 import io.crate.operation.operator.OperatorModule;
@@ -58,6 +58,8 @@ import io.crate.protocols.postgres.PostgresNetty;
 import io.crate.protocols.ssl.SslConfigSettings;
 import io.crate.rest.action.RestSQLAction;
 import io.crate.settings.CrateSetting;
+import io.crate.user.UserFallbackModule;
+import io.crate.user.UserExtension;
 import org.elasticsearch.action.bulk.BulkModule;
 import org.elasticsearch.cluster.NamedDiff;
 import org.elasticsearch.cluster.metadata.MetaData;
@@ -86,14 +88,21 @@ import java.util.List;
 import java.util.Map;
 
 import static com.google.common.collect.Lists.newArrayList;
+import static io.crate.settings.SharedSettings.ENTERPRISE_LICENSE_SETTING;
 
 public class SQLPlugin extends Plugin implements ActionPlugin, MapperPlugin, ClusterPlugin {
 
     private final Settings settings;
+    private final UserExtension userExtension;
 
     @SuppressWarnings("WeakerAccess") // must be public for pluginLoader
     public SQLPlugin(Settings settings) {
         this.settings = settings;
+        if (ENTERPRISE_LICENSE_SETTING.setting().get(settings)) {
+            userExtension = EnterpriseLoader.loadSingle(UserExtension.class);
+        } else {
+            userExtension = null;
+        }
     }
 
     @Override
@@ -118,9 +127,9 @@ public class SQLPlugin extends Plugin implements ActionPlugin, MapperPlugin, Clu
         settings.add(PostgresNetty.PSQL_PORT_SETTING.setting());
 
         // Authentication settings are node settings
-        settings.add(AuthenticationProvider.AUTH_HOST_BASED_ENABLED_SETTING.setting());
-        settings.add(AuthenticationProvider.AUTH_HOST_BASED_CONFIG_SETTING.setting());
-        settings.add(AuthenticationProvider.AUTH_TRUST_HTTP_DEFAULT_HEADER.setting());
+        settings.add(AuthSettings.AUTH_HOST_BASED_ENABLED_SETTING.setting());
+        settings.add(AuthSettings.AUTH_HOST_BASED_CONFIG_SETTING.setting());
+        settings.add(AuthSettings.AUTH_TRUST_HTTP_DEFAULT_HEADER.setting());
 
         // Settings for SSL (available only in the Enterprise version)
         settings.add(SslConfigSettings.SSL_HTTP_ENABLED.setting());
@@ -177,6 +186,11 @@ public class SQLPlugin extends Plugin implements ActionPlugin, MapperPlugin, Clu
         modules.add(new SysChecksModule());
         modules.add(new SysNodeChecksModule());
         modules.add(new RepositorySettingsModule());
+        if (userExtension != null) {
+            modules.addAll(userExtension.getModules());
+        } else {
+            modules.add(new UserFallbackModule());
+        }
         return modules;
     }
 
@@ -208,6 +222,9 @@ public class SQLPlugin extends Plugin implements ActionPlugin, MapperPlugin, Clu
             UserDefinedFunctionsMetaData.TYPE,
             UserDefinedFunctionsMetaData::readDiffFrom
         ));
+        if (userExtension != null) {
+            entries.addAll(userExtension.getNamedWriteables());
+        }
         return entries;
     }
 
@@ -219,6 +236,9 @@ public class SQLPlugin extends Plugin implements ActionPlugin, MapperPlugin, Clu
             new ParseField(UserDefinedFunctionsMetaData.TYPE),
             UserDefinedFunctionsMetaData::fromXContent
         ));
+        if (userExtension != null) {
+            entries.addAll(userExtension.getNamedXContent());
+        }
         return entries;
     }
 }

sql/src/main/java/io/crate/user/UserExtension.java
@@ -20,31 +20,19 @@
  * agreement.
  */
 
-package io.crate.plugin;
+package io.crate.user;
 
+import org.elasticsearch.common.inject.Module;
+import org.elasticsearch.common.io.stream.NamedWriteableRegistry;
+import org.elasticsearch.common.xcontent.NamedXContentRegistry;
 
-import org.elasticsearch.common.Nullable;
+import java.util.Collection;
+import java.util.List;
 
-import java.util.Iterator;
-import java.util.ServiceConfigurationError;
-import java.util.ServiceLoader;
+public interface UserExtension {
 
-/**
- * Utility class to load classes dynamically using a serviceLoader.
- * (Classes are discovered using META-INF/services files)
- */
-public final class EnterpriseLoader {
+    List<NamedWriteableRegistry.Entry> getNamedWriteables();
+    List<NamedXContentRegistry.Entry> getNamedXContent();
 
-    @Nullable
-    public static <T> T loadSingle(Class<T> clazz) {
-        Iterator<T> it = ServiceLoader.load(clazz).iterator();
-        T instance = null;
-        while (it.hasNext()) {
-            if (instance != null) {
-                throw new ServiceConfigurationError(clazz.getSimpleName() + " found twice");
-            }
-            instance = it.next();
-        }
-        return instance;
-    }
+    Collection<Module> getModules();
 }

enterprise/users/src/test/java/io/crate/operation/auth/HostBasedAuthenticationTest.java
@@ -80,7 +80,7 @@ public class HostBasedAuthenticationTest extends CrateUnitTest {
     @Before
     private void setUpTest() throws Exception {
         Settings settings = Settings.builder()
-            .put(AuthenticationProvider.AUTH_HOST_BASED_ENABLED_SETTING.getKey(), true)
+            .put(AuthSettings.AUTH_HOST_BASED_ENABLED_SETTING.getKey(), true)
             .build();
         authService = new HostBasedAuthentication(settings, null);
 
@@ -110,7 +110,7 @@ public class HostBasedAuthenticationTest extends CrateUnitTest {
         assertFalse(authService.enabled());
 
         Settings settings = Settings.builder()
-            .put(AuthenticationProvider.AUTH_HOST_BASED_ENABLED_SETTING.getKey(), true)
+            .put(AuthSettings.AUTH_HOST_BASED_ENABLED_SETTING.getKey(), true)
             .build();
         authService = new HostBasedAuthentication(settings, null);
         assertTrue(authService.enabled());

commit 7dcb788076e557f28cca8f6ebcc6d88d65456d61

Don't bind HBA if it's disabled
Follow up to c6cdb244c315ca555817a0cd5c64626de07de0de which broke
sqllogic tests (among others), because for postgres HBA was always
enabled.

enterprise/users/src/main/java/io/crate/operation/auth/HostBasedAuthentication.java
@@ -70,13 +70,11 @@ public class HostBasedAuthentication implements Authentication {
      }
      */
     private Map<String, Map<String, String>> hbaConf;
-    private boolean enabled;
 
     private final Map<String, Supplier<AuthenticationMethod>> authMethodRegistry = new HashMap<>();
 
     @Inject
     public HostBasedAuthentication(Settings settings, UserLookup userLookup) {
-        enabled = AuthSettings.AUTH_HOST_BASED_ENABLED_SETTING.setting().get(settings);
         hbaConf = convertHbaSettingsToHbaConf(AuthSettings.AUTH_HOST_BASED_CONFIG_SETTING.setting().get(settings));
         authMethodRegistry.put(TrustAuthentication.NAME, () -> new TrustAuthentication(userLookup));
         authMethodRegistry.put(ClientCertAuth.NAME, () -> new ClientCertAuth(userLookup));
@@ -96,11 +94,6 @@ public class HostBasedAuthentication implements Authentication {
             hostBasedConf.put(entry.getKey(), entry.getValue().getAsMap());
         }
         return hostBasedConf.build();
-    }
-
-    @Override
-    public boolean enabled() {
-        return enabled;
     }
 
     @Override

enterprise/users/src/main/java/io/crate/plugin/AuthenticationHttpAuthHandlerRegistry.java
@@ -28,6 +28,9 @@ import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.common.inject.Singleton;
 import org.elasticsearch.common.settings.Settings;
 
+/**
+ * Class that if instantiated causes the {@link HttpAuthUpstreamHandler} to be registered in the {@link PipelineRegistry}
+ */
 @Singleton
 public class AuthenticationHttpAuthHandlerRegistry {
 
@@ -35,13 +38,11 @@ public class AuthenticationHttpAuthHandlerRegistry {
     public AuthenticationHttpAuthHandlerRegistry(Settings settings,
                                                  PipelineRegistry pipelineRegistry,
                                                  Authentication authentication) {
-        if (authentication.enabled()) {
-            PipelineRegistry.ChannelPipelineItem pipelineItem = new PipelineRegistry.ChannelPipelineItem(
-                "blob_handler",
-                "auth_handler",
-                () -> new HttpAuthUpstreamHandler(settings, authentication)
-            );
-            pipelineRegistry.addBefore(pipelineItem);
-        }
+        PipelineRegistry.ChannelPipelineItem pipelineItem = new PipelineRegistry.ChannelPipelineItem(
+            "blob_handler",
+            "auth_handler",
+            () -> new HttpAuthUpstreamHandler(settings, authentication)
+        );
+        pipelineRegistry.addBefore(pipelineItem);
     }
 }

enterprise/users/src/main/java/io/crate/plugin/AuthenticationModule.java
@@ -22,15 +22,28 @@
 
 package io.crate.plugin;
 
+import io.crate.operation.auth.AlwaysOKAuthentication;
+import io.crate.operation.auth.AuthSettings;
 import io.crate.operation.auth.Authentication;
 import io.crate.operation.auth.HostBasedAuthentication;
 import org.elasticsearch.common.inject.AbstractModule;
+import org.elasticsearch.common.settings.Settings;
 
 public class AuthenticationModule extends AbstractModule {
 
+    private final Settings settings;
+
+    public AuthenticationModule(Settings settings) {
+        this.settings = settings;
+    }
+
     @Override
     protected void configure() {
-        bind(Authentication.class).to(HostBasedAuthentication.class);
-        bind(AuthenticationHttpAuthHandlerRegistry.class).asEagerSingleton();
+        if (AuthSettings.AUTH_HOST_BASED_ENABLED_SETTING.setting().get(settings)) {
+            bind(Authentication.class).to(HostBasedAuthentication.class);
+            bind(AuthenticationHttpAuthHandlerRegistry.class).asEagerSingleton();
+        } else {
+            bind(Authentication.class).to(AlwaysOKAuthentication.class);
+        }
     }
 }

enterprise/users/src/main/java/io/crate/plugin/EnterpriseUsersExtension.java
@@ -27,6 +27,7 @@ import org.elasticsearch.cluster.metadata.MetaData;
 import org.elasticsearch.common.ParseField;
 import org.elasticsearch.common.inject.Module;
 import org.elasticsearch.common.io.stream.NamedWriteableRegistry;
+import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.xcontent.NamedXContentRegistry;
 
 import java.util.ArrayList;
@@ -80,10 +81,10 @@ public class EnterpriseUsersExtension implements UserExtension {
     }
 
     @Override
-    public Collection<Module> getModules() {
+    public Collection<Module> getModules(Settings settings) {
         return Arrays.asList(
             new UserManagementModule(),
-            new AuthenticationModule(),
+            new AuthenticationModule(settings),
             new UsersScalarFunctionModule());
     }
 }

sql/src/main/java/io/crate/operation/auth/AlwaysOKAuthentication.java
@@ -40,11 +40,6 @@ public class AlwaysOKAuthentication implements Authentication {
     };
 
     @Override
-    public boolean enabled() {
-        return true;
-    }
-
-    @Override
     public AuthenticationMethod resolveAuthenticationType(String user, ConnectionProperties connectionProperties) {
         return alwaysOk;
     }

sql/src/main/java/io/crate/operation/auth/Authentication.java
@@ -28,8 +28,6 @@ import javax.annotation.Nullable;
 
 public interface Authentication {
 
-    boolean enabled();
-
     @Nullable
     AuthenticationMethod resolveAuthenticationType(String user, ConnectionProperties connectionProperties);
 }

sql/src/main/java/io/crate/plugin/SQLPlugin.java
@@ -58,8 +58,8 @@ import io.crate.protocols.postgres.PostgresNetty;
 import io.crate.protocols.ssl.SslConfigSettings;
 import io.crate.rest.action.RestSQLAction;
 import io.crate.settings.CrateSetting;
-import io.crate.user.UserFallbackModule;
 import io.crate.user.UserExtension;
+import io.crate.user.UserFallbackModule;
 import org.elasticsearch.action.bulk.BulkModule;
 import org.elasticsearch.cluster.NamedDiff;
 import org.elasticsearch.cluster.metadata.MetaData;
@@ -187,7 +187,7 @@ public class SQLPlugin extends Plugin implements ActionPlugin, MapperPlugin, Clu
         modules.add(new SysNodeChecksModule());
         modules.add(new RepositorySettingsModule());
         if (userExtension != null) {
-            modules.addAll(userExtension.getModules());
+            modules.addAll(userExtension.getModules(settings));
         } else {
             modules.add(new UserFallbackModule());
         }

sql/src/main/java/io/crate/user/UserExtension.java
@@ -24,6 +24,7 @@ package io.crate.user;
 
 import org.elasticsearch.common.inject.Module;
 import org.elasticsearch.common.io.stream.NamedWriteableRegistry;
+import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.xcontent.NamedXContentRegistry;
 
 import java.util.Collection;
@@ -34,5 +35,5 @@ public interface UserExtension {
     List<NamedWriteableRegistry.Entry> getNamedWriteables();
     List<NamedXContentRegistry.Entry> getNamedXContent();
 
-    Collection<Module> getModules();
+    Collection<Module> getModules(Settings settings);
 }

enterprise/users/src/test/java/io/crate/operation/auth/HostBasedAuthenticationTest.java
@@ -105,18 +105,6 @@ public class HostBasedAuthenticationTest extends CrateUnitTest {
     }
 
     @Test
-    public void testEnableAuthentication() throws Exception {
-        Authentication authService = new HostBasedAuthentication(Settings.EMPTY, null);
-        assertFalse(authService.enabled());
-
-        Settings settings = Settings.builder()
-            .put(AuthSettings.AUTH_HOST_BASED_ENABLED_SETTING.getKey(), true)
-            .build();
-        authService = new HostBasedAuthentication(settings, null);
-        assertTrue(authService.enabled());
-    }
-
-    @Test
     public void testMissingUserOrAddress() throws Exception {
         authService.updateHbaConfig(Collections.emptyMap());
         AuthenticationMethod method;

sql/src/test/java/io/crate/protocols/postgres/PostgresWireProtocolTest.java
@@ -176,11 +176,6 @@ public class PostgresWireProtocolTest extends CrateDummyClusterServiceUnitTest {
         }
 
         @Override
-        public boolean enabled() {
-            return true;
-        }
-
-        @Override
         public AuthenticationMethod resolveAuthenticationType(String user, ConnectionProperties connectionProperties) {
             return new AuthenticationMethod() {
                 @Nullable