19caa9c75c5c24fc7fbd82422f7d6dc756f08279
Sebastian Utz <su@rtme.net>
2017-07-17 22:05:26 +0200

    improves privileges integration tests
    tests are supporting repeated calls now by dropping the created user,
    simplified all tests by always using the same test user

--- enterprise/users/src/test/java/io/crate/integrationtests/PrivilegesIntegrationTest.java
+++ enterprise/users/src/test/java/io/crate/integrationtests/PrivilegesIntegrationTest.java
@@ -18,7 +18,6 @@
 
 package io.crate.integrationtests;
 
-import io.crate.action.sql.Option;
 import io.crate.action.sql.SQLActionException;
 import io.crate.action.sql.SQLOperations;
 import io.crate.operation.udf.UserDefinedFunctionService;
@@ -26,10 +25,10 @@ import io.crate.operation.user.User;
 import io.crate.operation.user.UserManager;
 import io.crate.operation.user.UserManagerService;
 import io.crate.testing.SQLResponse;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import static io.crate.testing.SQLTransportExecutor.DEFAULT_SOFT_LIMIT;
 import static io.crate.testing.TestingHelpers.printedTable;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.notNullValue;
@@ -37,78 +36,88 @@ import static org.hamcrest.core.Is.is;
 
 public class PrivilegesIntegrationTest extends BaseUsersIntegrationTest {
 
-    private void assertPrivilegeIsGranted(String privilege, String userName) throws Exception {
+    private static final String TEST_USERNAME = "privileges_test_user";
+
+    private final UserDefinedFunctionsIntegrationTest.DummyLang dummyLang = new UserDefinedFunctionsIntegrationTest.DummyLang();
+    private SQLOperations sqlOperations;
+    private UserManager userManager;
+
+    private void assertPrivilegeIsGranted(String privilege) throws Exception {
         SQLResponse response = executeAsSuperuser("select count(*) from sys.privileges where grantee = ? and type = ?",
-            new Object[]{userName, privilege});
+            new Object[]{TEST_USERNAME, privilege});
         assertThat(response.rows()[0][0], is(1L));
     }
 
-    private void assertPrivilegeIsRevoked(String privilege, String userName) throws Exception {
+    private void assertPrivilegeIsRevoked(String privilege) throws Exception {
         SQLResponse response = executeAsSuperuser("select count(*) from sys.privileges where grantee = ? and type = ?",
-            new Object[]{userName, privilege});
+            new Object[]{TEST_USERNAME, privilege});
         assertThat(response.rows()[0][0], is(0L));
     }
 
-    private final UserDefinedFunctionsIntegrationTest.DummyLang dummyLang = new UserDefinedFunctionsIntegrationTest.DummyLang();
+    private SQLOperations.Session testUserSession() {
+        User user = userManager.findUser(TEST_USERNAME);
+        assertThat(user, notNullValue());
+        return sqlOperations.createSession(null, user);
+    }
 
     @Before
-    public void setUpUDF() {
+    public void setUp() throws Exception {
+        super.setUp();
         Iterable<UserDefinedFunctionService> udfServices = internalCluster().getInstances(UserDefinedFunctionService.class);
         for (UserDefinedFunctionService udfService : udfServices) {
             udfService.registerLanguage(dummyLang);
         }
+        userManager = internalCluster().getInstance(UserManager.class);
+        sqlOperations = internalCluster().getInstance(SQLOperations.class, null);
+        executeAsSuperuser("create user " + TEST_USERNAME);
+    }
+
+    @After
+    public void dropUsers() {
+        executeAsSuperuser("drop user " + TEST_USERNAME);
     }
 
     @Test
     public void testNormalUserGrantsPrivilegeThrowsException() throws Exception {
-        executeAsSuperuser("create user joey");
-
         expectedException.expect(SQLActionException.class);
         expectedException.expectMessage("UnauthorizedException: User \"normal\" is not authorized to execute statement");
-        executeAsNormalUser("grant DQL to joey");
+        executeAsNormalUser("grant DQL to " + TEST_USERNAME);
     }
 
     @Test
     public void testNewUserHasNoPrivilegesByDefault() throws Exception {
-        executeAsSuperuser("create user ford");
-        executeAsSuperuser("select * from sys.privileges where grantee = ?", new Object[]{"ford"});
+        executeAsSuperuser("select * from sys.privileges where grantee = ?", new Object[]{TEST_USERNAME});
         assertThat(response.rowCount(), is(0L));
     }
 
     @Test
     public void testSuperUserGrantsPrivilege() throws Exception {
-        executeAsSuperuser("create user chandler");
-
-        executeAsSuperuser("grant DQL to chandler");
-        assertPrivilegeIsGranted("DQL", "chandler");
+        executeAsSuperuser("grant DQL to " + TEST_USERNAME);
+        assertPrivilegeIsGranted("DQL");
     }
 
     @Test
     public void testGrantRevokeALLPrivileges() throws Exception {
-        executeAsSuperuser("create user phoebe");
+        executeAsSuperuser("grant ALL to " + TEST_USERNAME);
+        assertPrivilegeIsGranted("DQL");
+        assertPrivilegeIsGranted("DML");
+        assertPrivilegeIsGranted("DDL");
 
-        executeAsSuperuser("grant ALL to phoebe");
-        assertPrivilegeIsGranted("DQL", "phoebe");
-        assertPrivilegeIsGranted("DML", "phoebe");
-        assertPrivilegeIsGranted("DDL", "phoebe");
-
-        executeAsSuperuser("revoke ALL from phoebe");
-        assertPrivilegeIsRevoked("DQL", "phoebe");
-        assertPrivilegeIsRevoked("DML", "phoebe");
-        assertPrivilegeIsRevoked("DDL", "phoebe");
+        executeAsSuperuser("revoke ALL from " + TEST_USERNAME);
+        assertPrivilegeIsRevoked("DQL");
+        assertPrivilegeIsRevoked("DML");
+        assertPrivilegeIsRevoked("DDL");
     }
 
     @Test
     public void testSuperUserRevokesPrivilege() throws Exception {
-        executeAsSuperuser("create user ross");
-
-        executeAsSuperuser("grant DQL to ross");
+        executeAsSuperuser("grant DQL to " + TEST_USERNAME);
         assertThat(response.rowCount(), is(1L));
 
-        executeAsSuperuser("revoke DQL from ross");
+        executeAsSuperuser("revoke DQL from " + TEST_USERNAME);
         assertThat(response.rowCount(), is(1L));
 
-        assertPrivilegeIsRevoked("DQL", "ross");
+        assertPrivilegeIsRevoked("DQL");
     }
 
     @Test
@@ -136,7 +145,6 @@ public class PrivilegesIntegrationTest extends BaseUsersIntegrationTest {
 
     @Test
     public void testQuerySysShardsReturnsOnlyRowsRegardingTablesUserHasAccessOn() throws Exception {
-        executeAsSuperuser("create user normal");
         execute("create table t1 (x int) partitioned by (x) clustered into 1 shards with (number_of_replicas = 0)");
         execute("insert into t1 values (1)");
         execute("insert into t1 values (2)");
@@ -144,12 +152,11 @@ public class PrivilegesIntegrationTest extends BaseUsersIntegrationTest {
         execute("create table doc.t2 (x int) clustered into 1 shards with (number_of_replicas = 0)");
         execute("create table t3 (x int) clustered into 1 shards with (number_of_replicas = 0)");
 
-        executeAsSuperuser("grant dql on table t1 to normal");
-        executeAsSuperuser("grant dml on table t2 to normal");
-        executeAsSuperuser("grant dql on table sys.shards to normal");
+        executeAsSuperuser("grant dql on table t1 to " + TEST_USERNAME);
+        executeAsSuperuser("grant dml on table t2 to " + TEST_USERNAME);
+        executeAsSuperuser("grant dql on table sys.shards to " + TEST_USERNAME);
 
-        SQLOperations.Session normalUserSession = getSessionFor("normal");
-        execute("select table_name from sys.shards order by table_name", null, normalUserSession);
+        execute("select table_name from sys.shards order by table_name", null, testUserSession());
         assertThat(response.rowCount(), is(4L));
         assertThat(response.rows()[0][0], is("t1"));
         assertThat(response.rows()[1][0], is("t1"));
@@ -159,8 +166,6 @@ public class PrivilegesIntegrationTest extends BaseUsersIntegrationTest {
 
     @Test
     public void testQueryInformationSchemaShowsOnlyRowsRegardingTablesUserHasAccessOn() throws Exception {
-        executeAsSuperuser("create user information_schema_test_user");
-
         execute("create table t1 (x int) partitioned by (x) clustered into 1 shards with (number_of_replicas = 0)");
         execute("insert into t1 values (1)");
         execute("insert into t1 values (2)");
@@ -172,79 +177,63 @@ public class PrivilegesIntegrationTest extends BaseUsersIntegrationTest {
         execute("create function other_func(long)" +
                 " returns string language dummy_lang as 'function foo(x) { return \"1\"; }'");
 
-        executeAsSuperuser("grant dql on table t1 to information_schema_test_user");
-        executeAsSuperuser("grant dml on table my_schema.t2 to information_schema_test_user");
-        executeAsSuperuser("grant dql on schema my_schema to information_schema_test_user");
+        executeAsSuperuser("grant dql on table t1 to " + TEST_USERNAME);
+        executeAsSuperuser("grant dml on table my_schema.t2 to " + TEST_USERNAME);
+        executeAsSuperuser("grant dql on schema my_schema to " + TEST_USERNAME);
 
-        SQLOperations.Session normalUserSession = getSessionFor("information_schema_test_user");
-        execute("select schema_name from information_schema.schemata order by schema_name", null, normalUserSession);
+        execute("select schema_name from information_schema.schemata order by schema_name", null, testUserSession());
         assertThat(printedTable(response.rows()), is("my_schema\n"));
-        execute("select table_name from information_schema.tables order by table_name", null, normalUserSession);
+        execute("select table_name from information_schema.tables order by table_name", null, testUserSession());
         assertThat(printedTable(response.rows()), is("t1\n" +
                                                      "t2\n"));
-        execute("select table_name from information_schema.table_partitions order by table_name", null, normalUserSession);
+        execute("select table_name from information_schema.table_partitions order by table_name", null, testUserSession());
         assertThat(printedTable(response.rows()), is("t1\n" +
                                                      "t1\n"));
-        execute("select table_name from information_schema.columns order by table_name", null, normalUserSession);
+        execute("select table_name from information_schema.columns order by table_name", null, testUserSession());
         assertThat(printedTable(response.rows()), is("t1\n" +
                                                      "t2\n"));
-        execute("select table_name from information_schema.table_constraints order by table_name", null, normalUserSession);
+        execute("select table_name from information_schema.table_constraints order by table_name", null, testUserSession());
         assertThat(printedTable(response.rows()), is("t2\n"));
-        execute("select routine_schema from information_schema.routines order by routine_schema", null, normalUserSession);
+        execute("select routine_schema from information_schema.routines order by routine_schema", null, testUserSession());
         assertThat(printedTable(response.rows()), is("my_schema\n"));
     }
 
     @Test
     public void testRenameTableTransfersPrivilegesToNewTable() {
-        executeAsSuperuser("create user gunther");
         execute("create table doc.t1 (x int) clustered into 1 shards with (number_of_replicas = 0)");
-        executeAsSuperuser("grant dql on table t1 to gunther");
+        executeAsSuperuser("grant dql on table t1 to "+ TEST_USERNAME);
 
         executeAsSuperuser("alter table doc.t1 rename to t1_renamed");
         ensureYellow();
 
-        SQLOperations.Session normalUserSession = getSessionFor("gunther");
-        execute("select * from t1_renamed", null, normalUserSession);
+        execute("select * from t1_renamed", null, testUserSession());
         assertThat(response.rowCount(), is(0L));
     }
 
     @Test
     public void testRenamePartitionedTableTransfersPrivilegesToNewTable() {
-        executeAsSuperuser("create user rachel");
         executeAsSuperuser("create table t1 (x int) partitioned by (x) clustered into 1 shards with (number_of_replicas = 0)");
         executeAsSuperuser("insert into t1 values (1)");
-        executeAsSuperuser("grant dql on table t1 to rachel");
+        executeAsSuperuser("grant dql on table t1 to " + TEST_USERNAME);
 
         executeAsSuperuser("alter table doc.t1 rename to t1_renamed");
         ensureYellow();
 
-        SQLOperations.Session normalUserSession = getSessionFor("rachel");
-        execute("select * from t1_renamed", null, normalUserSession);
+        execute("select * from t1_renamed", null, testUserSession());
         assertThat(response.rowCount(), is(1L));
     }
 
     @Test
     public void testDropTableRemovesPrivileges() {
-        executeAsSuperuser("create user janice");
         executeAsSuperuser("create table doc.t1 (x int) clustered into 1 shards with (number_of_replicas = 0)");
-        executeAsSuperuser("grant dql on table t1 to janice");
+        executeAsSuperuser("grant dql on table t1 to "+ TEST_USERNAME);
 
         executeAsSuperuser("drop table t1");
         ensureYellow();
 
         executeAsSuperuser("create table doc.t1 (x int) clustered into 1 shards with (number_of_replicas = 0)");
-        SQLOperations.Session normalUserSession = getSessionFor("janice");
         expectedException.expect(SQLActionException.class);
-        expectedException.expectMessage(containsString("Missing 'DQL' privilege for user 'janice'"));
-        execute("select * from t1", null, normalUserSession);
-    }
-
-    private SQLOperations.Session getSessionFor(String userName) {
-        UserManager userManager = internalCluster().getInstance(UserManager.class);
-        User user = userManager.findUser(userName);
-        assertThat(user, notNullValue());
-
-        SQLOperations sqlOperations = internalCluster().getInstance(SQLOperations.class, null);
-        return sqlOperations.createSession(null, user, Option.NONE, DEFAULT_SOFT_LIMIT);
+        expectedException.expectMessage(containsString("Missing 'DQL' privilege for user '"+ TEST_USERNAME + "'"));
+        execute("select * from t1", null, testUserSession());
     }
 }
