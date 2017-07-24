crate/crate

commit 14edac028e2c11f49e51b23f1fa3d8c318a5b928

add support for group by with scalar

sql/src/main/java/io/crate/analyze/relations/RelationAnalyzer.java
@@ -343,7 +343,7 @@ public class RelationAnalyzer extends DefaultTraversalVisitor<AnalyzedRelation, 
     private static List<Symbol> rewriteGlobalDistinct(List<Symbol> outputSymbols) {
         List<Symbol> groupBy = new ArrayList<>(outputSymbols.size());
         for (Symbol symbol : outputSymbols) {
-            if (!Aggregations.containsAggregation(symbol)) {
+            if (Aggregations.containsAggregationOrscalar(symbol) == false) {
                 GroupBySymbolValidator.validate(symbol);
                 groupBy.add(symbol);
             }
@@ -357,7 +357,7 @@ public class RelationAnalyzer extends DefaultTraversalVisitor<AnalyzedRelation, 
         for (int i = 0; i < outputSymbols.size(); i++) {
             Symbol output = outputSymbols.get(i);
             if (groupBy == null || !groupBy.contains(output)) {
-                if (!Aggregations.containsAggregation(output)) {
+                if (!Aggregations.containsAggregationOrscalar(output) || !Aggregations.matchGroupBySymbol(output, groupBy)) {
                     throw new IllegalArgumentException(
                         String.format(Locale.ENGLISH, "column '%s' must appear in the GROUP BY clause " +
                                       "or be used in an aggregation function", outputNames.get(i).outputName()));

sql/src/main/java/io/crate/analyze/symbol/Aggregations.java
@@ -23,16 +23,59 @@
 package io.crate.analyze.symbol;
 
 import io.crate.metadata.FunctionInfo;
+import io.crate.operation.scalar.arithmetic.ArrayFunction;
+
+import java.util.List;
 
 public final class Aggregations {
 
     private final static AggregationSearcher AGGREGATION_SEARCHER = new AggregationSearcher();
+    private final static AggregationOrScalarSearcher AGGREGATION_OR_SCALAR_SEARCHER = new AggregationOrScalarSearcher();
+    private final static GroupBySymbolMatcher GROUP_BY_MATCHER = new GroupBySymbolMatcher();
 
     /**
      * @return true if the symbol is an aggregation or function which contains an aggregation.
      */
     public static boolean containsAggregation(Symbol s) {
         return AGGREGATION_SEARCHER.process(s, null);
+    }
+
+    /**
+     * @return true if the symbol is an aggregation or function which contains an aggregation or a scalar.
+     */
+    public static boolean containsAggregationOrscalar(Symbol s) {
+        return AGGREGATION_OR_SCALAR_SEARCHER.process(s, null);
+    }
+
+    /**
+     * @return true if the symbol is found in the group by, if the symbol is a scalar function all column arguments must be found in the group by.
+     */
+    public static boolean matchGroupBySymbol(Symbol s, List<Symbol> groupBy) {
+        return GROUP_BY_MATCHER.process(s, groupBy);
+    }
+
+    private static class AggregationOrScalarSearcher extends SymbolVisitor<Void, Boolean> {
+
+        @Override
+        protected Boolean visitSymbol(Symbol symbol, Void context) {
+            return false;
+        }
+
+        @Override
+        public Boolean visitFunction(Function symbol, Void context) {
+            if (symbol.info().type() == FunctionInfo.Type.AGGREGATE || symbol.info().type() == FunctionInfo.Type.SCALAR) {
+                if (ArrayFunction.NAME.equals(symbol.info().ident().name()) && Symbols.allLiterals(symbol)) {
+                    return false;
+                }
+                return true;
+            }
+            return false;
+        }
+
+        @Override
+        public Boolean visitAggregation(Aggregation symbol, Void context) {
+            return true;
+        }
     }
 
     private static class AggregationSearcher extends SymbolVisitor<Void, Boolean> {
@@ -58,6 +101,39 @@ public final class Aggregations {
 
         @Override
         public Boolean visitAggregation(Aggregation symbol, Void context) {
+            return true;
+        }
+    }
+
+    private static class GroupBySymbolMatcher extends SymbolVisitor<List<Symbol>, Boolean> {
+
+        @Override
+        protected Boolean visitSymbol(Symbol symbol, List<Symbol> context) {
+            return context.contains(symbol);
+        }
+
+        @Override
+        public Boolean visitLiteral(Literal symbol, List<Symbol> context) {
+            return true;
+        }
+
+        @Override
+        public Boolean visitFunction(Function symbol, List<Symbol> context) {
+            if (context.contains(symbol)) {
+                return true;
+            }
+
+            boolean isSymbolContained = true;
+            if (symbol.info().type() == FunctionInfo.Type.SCALAR) {
+                for (int i = 0; i < symbol.arguments().size(); i++) {
+                    isSymbolContained = isSymbolContained && this.process(symbol.arguments().get(i), context);
+                }
+            }
+            return isSymbolContained;
+        }
+
+        @Override
+        public Boolean visitAggregation(Aggregation symbol, List<Symbol> context) {
             return true;
         }
     }

sql/src/main/java/io/crate/analyze/symbol/Symbols.java
@@ -46,6 +46,7 @@ import java.util.function.UnaryOperator;
 public class Symbols {
 
     private static final HasColumnVisitor HAS_COLUMN_VISITOR = new HasColumnVisitor();
+    private static final AllLiteralsMatcher ALL_LITERALS_MATCHER = new AllLiteralsMatcher();
     private static final ReplacingSymbolVisitor<Void> DEEP_COPY_VISITOR = new ReplacingSymbolVisitor<>(ReplaceMode.COPY);
 
     public static final Predicate<Symbol> IS_COLUMN = s -> s instanceof Field || s instanceof Reference;
@@ -87,6 +88,11 @@ public class Symbols {
             }
         }
         return false;
+    }
+
+    public static boolean allLiterals(Symbol symbol) {
+        assert symbol != null : "symbol must not be null";
+        return ALL_LITERALS_MATCHER.process(symbol, null);
     }
 
     public static void toStream(Collection<? extends Symbol> symbols, StreamOutput out) throws IOException {
@@ -148,4 +154,26 @@ public class Symbols {
             return context.equals(symbol.ident().columnIdent());
         }
     }
+
+    /**
+     * Returns true if all symbols in an expression are literals and false otherwise.
+     */
+    private static class AllLiteralsMatcher extends SymbolVisitor<Void, Boolean> {
+
+        @Override
+        public Boolean visitFunction(Function function, Void context) {
+            return function.arguments().stream().allMatch(arg -> process(arg, null));
+        }
+
+        @Override
+        public Boolean visitLiteral(Literal symbol, Void context) {
+            return true;
+        }
+
+        @Override
+        public Boolean visitSymbol(Symbol symbol, Void context) {
+            return false;
+        }
+    }
+
 }

sql/src/main/java/io/crate/analyze/validator/GroupBySymbolValidator.java
@@ -22,10 +22,10 @@
 package io.crate.analyze.validator;
 
 import io.crate.analyze.symbol.Function;
-import io.crate.analyze.symbol.Literal;
 import io.crate.analyze.symbol.MatchPredicate;
 import io.crate.analyze.symbol.Symbol;
 import io.crate.analyze.symbol.SymbolVisitor;
+import io.crate.analyze.symbol.Symbols;
 import io.crate.analyze.symbol.format.SymbolPrinter;
 import io.crate.types.DataTypes;
 
@@ -35,7 +35,6 @@ import java.util.Locale;
 public class GroupBySymbolValidator {
 
     private final static InnerValidator INNER_VALIDATOR = new InnerValidator();
-    private final static LiteralVisitor LITERAL_VISITOR = new LiteralVisitor();
 
     public static void validate(Symbol symbol) throws IllegalArgumentException, UnsupportedOperationException {
         INNER_VALIDATOR.process(symbol, null);
@@ -57,7 +56,7 @@ public class GroupBySymbolValidator {
             switch (function.info().type()) {
                 case SCALAR:
                     // If is literal no further datatype validation is required
-                    if (LITERAL_VISITOR.process(function, null)) {
+                    if (Symbols.allLiterals(function)) {
                         return null;
                     }
 
@@ -87,7 +86,7 @@ public class GroupBySymbolValidator {
         @Override
         protected Void visitSymbol(Symbol symbol, @Nullable Function parentScalar) {
             // If is literal no further datatype validation is required
-            if (LITERAL_VISITOR.process(symbol, null)) {
+            if (Symbols.allLiterals(symbol)) {
                 return null;
             }
 
@@ -100,28 +99,4 @@ public class GroupBySymbolValidator {
         }
     }
 
-    /**
-     * Returns true if all symbols in an expression are literals and false otherwise.
-     */
-    private static class LiteralVisitor extends SymbolVisitor<Void, Boolean> {
-
-        @Override
-        public Boolean visitFunction(Function function, Void context) {
-            boolean result = true;
-            for (Symbol arg: function.arguments()) {
-                result &= process(arg, null);
-            }
-            return result;
-        }
-
-        @Override
-        public Boolean visitLiteral(Literal symbol, Void context) {
-            return true;
-        }
-
-        @Override
-        public Boolean visitSymbol(Symbol symbol, Void context) {
-            return false;
-        }
-    }
 }

sql/src/test/java/io/crate/analyze/relations/GroupByScalarAnalyzerTest.java
@@ -1,0 +1,45 @@
+package io.crate.analyze.relations;
+
+import io.crate.analyze.SelectAnalyzedStatement;
+import io.crate.test.integration.CrateDummyClusterServiceUnitTest;
+import io.crate.testing.SQLExecutor;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+
+
+public class GroupByScalarAnalyzerTest extends CrateDummyClusterServiceUnitTest {
+
+    private SQLExecutor executor;
+
+    @Before
+    public void prepare() {
+        executor = SQLExecutor.builder(clusterService).enableDefaultTables().build();
+    }
+
+    @Test
+    public void testScalarFunctionArgumentsNotAllInGroupByThrowsException() throws Exception {
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage("column '(id * other_id)' must appear in the GROUP BY clause or be used in an aggregation function");
+        executor.analyze("select id * other_id from users group by id");
+    }
+
+    @Test
+    public void testValidGroupByWithScalarAndMultipleColumns() throws Exception {
+        SelectAnalyzedStatement statement = executor.analyze("select id * other_id from users group by id, other_id");
+        assertThat(statement.relation().fields().get(0).path().outputName(), is("(id * other_id)"));
+    }
+
+    @Test
+    public void testValidGroupByWithScalar() throws Exception {
+        SelectAnalyzedStatement statement = executor.analyze("select id * 2 from users group by id");
+        assertThat(statement.relation().fields().get(0).path().outputName(), is("(id * 2)"));
+    }
+
+    @Test
+    public void testValidGroupByWithMultipleScalarFunctions() throws Exception {
+        SelectAnalyzedStatement statement = executor.analyze("select abs(id * 2) from users group by id");
+        assertThat(statement.relation().fields().get(0).path().outputName(), is("abs((id * 2))"));
+    }
+}

sql/src/test/java/io/crate/planner/GroupByScalarPlannerTest.java
@@ -1,0 +1,115 @@
+package io.crate.planner;
+
+
+import com.google.common.collect.Iterables;
+import io.crate.analyze.symbol.Function;
+import io.crate.analyze.symbol.InputColumn;
+import io.crate.metadata.RowGranularity;
+import io.crate.operation.aggregation.impl.CountAggregation;
+import io.crate.operation.scalar.arithmetic.ArithmeticFunctions;
+import io.crate.planner.node.dql.Collect;
+import io.crate.planner.node.dql.MergePhase;
+import io.crate.planner.node.dql.RoutedCollectPhase;
+import io.crate.planner.projection.AggregationProjection;
+import io.crate.planner.projection.EvalProjection;
+import io.crate.planner.projection.GroupProjection;
+import io.crate.planner.projection.OrderedTopNProjection;
+import io.crate.planner.projection.TopNProjection;
+import io.crate.test.integration.CrateDummyClusterServiceUnitTest;
+import io.crate.testing.SQLExecutor;
+import io.crate.types.DataType;
+import io.crate.types.DataTypes;
+import org.junit.Before;
+import org.junit.Test;
+
+import static io.crate.testing.SymbolMatchers.isFunction;
+import static io.crate.testing.SymbolMatchers.isLiteral;
+import static io.crate.testing.SymbolMatchers.isReference;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.nullValue;
+
+public class GroupByScalarPlannerTest extends CrateDummyClusterServiceUnitTest {
+
+    private SQLExecutor e;
+
+    @Before
+    public void prepare() {
+        e = SQLExecutor.builder(clusterService)
+            .enableDefaultTables()
+            .build();
+    }
+
+    @Test
+    public void testGroupByWithScalarPlan() throws Exception {
+        Merge merge = e.plan("select id + 1 from users group by id");
+        Collect collect = (Collect) merge.subPlan();
+        RoutedCollectPhase collectPhase = ((RoutedCollectPhase) collect.collectPhase());
+
+        assertEquals(DataTypes.LONG, collectPhase.outputTypes().get(0));
+        assertThat(collectPhase.maxRowGranularity(), is(RowGranularity.DOC));
+        assertThat(collectPhase.projections().size(), is(2));
+        assertThat(collectPhase.projections().get(0), instanceOf(GroupProjection.class));
+        assertThat(collectPhase.projections().get(0).requiredGranularity(), is(RowGranularity.SHARD));
+        assertThat(collectPhase.projections().get(1), instanceOf(EvalProjection.class));
+        assertThat(collectPhase.projections().get(1).outputs().get(0), instanceOf(Function.class));
+        assertThat(collectPhase.toCollect(), contains(isFunction("add"), isReference("id", DataTypes.LONG)));
+
+        GroupProjection groupProjection = (GroupProjection) collectPhase.projections().get(0);
+        assertThat(groupProjection.keys().get(0).valueType(), is(DataTypes.LONG));
+
+
+        MergePhase mergePhase = merge.mergePhase();
+
+        assertEquals(DataTypes.LONG, Iterables.get(mergePhase.inputTypes(), 0));
+        assertEquals(DataTypes.LONG, mergePhase.outputTypes().get(0));
+    }
+
+    @Test
+    public void testGroupByWithMultipleScalarPlan() throws Exception {
+        Merge merge = e.plan("select abs(id + 1) from users group by id");
+        Collect collect = (Collect) merge.subPlan();
+        RoutedCollectPhase collectPhase = ((RoutedCollectPhase) collect.collectPhase());
+
+        assertEquals(DataTypes.LONG, collectPhase.outputTypes().get(0));
+        assertThat(collectPhase.maxRowGranularity(), is(RowGranularity.DOC));
+        assertThat(collectPhase.projections().size(), is(2));
+        assertThat(collectPhase.projections().get(0), instanceOf(GroupProjection.class));
+        assertThat(collectPhase.projections().get(0).requiredGranularity(), is(RowGranularity.SHARD));
+        assertThat(collectPhase.projections().get(1), instanceOf(EvalProjection.class));
+        assertThat(collectPhase.projections().get(1).outputs().get(0), instanceOf(Function.class));
+        assertThat(collectPhase.toCollect(), contains(isFunction("abs"), isReference("id", DataTypes.LONG)));
+
+        GroupProjection groupProjection = (GroupProjection) collectPhase.projections().get(0);
+        assertThat(groupProjection.keys().get(0).valueType(), is(DataTypes.LONG));
+
+        MergePhase mergePhase = merge.mergePhase();
+
+        assertEquals(DataTypes.LONG, Iterables.get(mergePhase.inputTypes(), 0));
+        assertEquals(DataTypes.LONG, mergePhase.outputTypes().get(0));
+    }
+
+    @Test
+    public void testGroupByScalarWithMultipleColumnArgumentsPlan() throws Exception {
+        Merge merge = e.plan("select abs(id + other_id) from users group by id, other_id");
+        Merge subplan = (Merge) merge.subPlan();
+        Collect collect = (Collect) subplan.subPlan();
+        RoutedCollectPhase collectPhase = ((RoutedCollectPhase) collect.collectPhase());
+        assertThat(collectPhase.projections().size(), is(1));
+        assertThat(collectPhase.projections().get(0), instanceOf(GroupProjection.class));
+        assertThat(collectPhase.projections().get(0).requiredGranularity(), is(RowGranularity.SHARD));
+        assertThat(collectPhase.toCollect(), contains(isFunction("abs"), isReference("id", DataTypes.LONG), isReference("other_id", DataTypes.LONG)));
+
+        GroupProjection groupProjection = (GroupProjection) collectPhase.projections().get(0);
+        assertThat(groupProjection.keys().size(), is(2));
+        assertThat(groupProjection.keys().get(0).valueType(), is(DataTypes.LONG));
+        assertThat(groupProjection.keys().get(1).valueType(), is(DataTypes.LONG));
+
+        MergePhase mergePhase = subplan.mergePhase();
+        assertThat(mergePhase.projections().size(), is(2));
+        assertThat(mergePhase.projections().get(0), instanceOf(GroupProjection.class));
+        assertThat(mergePhase.projections().get(1), instanceOf(EvalProjection.class));
+    }
+}

commit 3bf7d7bb223a078c3d0174f35ec88d33fa1d89b2

Fixed a regression on DISTINCT with scalar functions.
Caused by 14edac028e2c11f49e51b23f1fa3d8c318a5b928.

sql/src/main/java/io/crate/analyze/relations/RelationAnalyzer.java
@@ -343,7 +343,7 @@ public class RelationAnalyzer extends DefaultTraversalVisitor<AnalyzedRelation, 
     private static List<Symbol> rewriteGlobalDistinct(List<Symbol> outputSymbols) {
         List<Symbol> groupBy = new ArrayList<>(outputSymbols.size());
         for (Symbol symbol : outputSymbols) {
-            if (Aggregations.containsAggregationOrscalar(symbol) == false) {
+            if (Aggregations.containsAggregation(symbol) == false) {
                 GroupBySymbolValidator.validate(symbol);
                 groupBy.add(symbol);
             }

sql/src/test/java/io/crate/analyze/SelectStatementAnalyzerTest.java
@@ -609,6 +609,13 @@ public class SelectStatementAnalyzerTest extends CrateDummyClusterServiceUnitTes
     }
 
     @Test
+    public void testSelectDistinctWithFunction() {
+        SelectAnalyzedStatement distinctAnalysis = analyze("select distinct id + 1 from users");
+        assertThat(distinctAnalysis.relation().querySpec().groupBy().get(), isSQL("add(doc.users.id, 1)"));
+        assertThat(distinctAnalysis.relation().querySpec().outputs(), isSQL("add(doc.users.id, 1)"));
+    }
+
+    @Test
     public void testSelectDistinctWithGroupBySameFieldsSameOrder() {
         SelectAnalyzedStatement distinctAnalysis = analyze("select distinct id, name from users group by id, name");
         SelectAnalyzedStatement groupByAnalysis = analyze("select id, name from users group by id, name");