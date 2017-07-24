9f5ae077833f7a0687290f0c1ab081365c895d19
Elina Zarisheva <e.zarisheva@binaere-bauten.de>
2017-07-24 22:05:18 +0200

    add missing tests

--- sql/src/test/java/io/crate/operation/scalar/arithmetic/ScaleFunctionTest.java
+++ sql/src/test/java/io/crate/operation/scalar/arithmetic/ScaleFunctionTest.java
@@ -11,12 +11,14 @@ public class ScaleFunctionTest extends AbstractScalarFunctionsTest {
     public void testScale() throws Exception {
         assertEvaluate("scale(cast(34 as integer))", 0);
         assertEvaluate("scale(cast(-34 as integer))", 0);
-
         assertEvaluate("scale(34)", 0);
         assertEvaluate("scale(34.2)", 1);
         assertEvaluate("scale(cast(34 as float))", 0);
         assertEvaluate("scale(cast(34.2 as float))", 1);
         assertEvaluate("scale(cast(34.2098 as double))", 4);
+        assertEvaluate("scale(cast(34 as long))", 0);
+        assertEvaluate("scale(cast(34 as short))", 0);
+        assertEvaluate("scale(cast(34 as byte))", 0);
 
         assertEvaluate("scale(null)", null);
 
