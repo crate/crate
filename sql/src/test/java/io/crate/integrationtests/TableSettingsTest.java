9573105d07cfdda36135f7e872d5467d353c4590
m-kharbat <meriam.kharbat@gmail.com>
2017-08-03 10:46:26 +0200

    implement information_schema.ingestion_rules table

--- sql/src/test/java/io/crate/integrationtests/TableSettingsTest.java
+++ sql/src/test/java/io/crate/integrationtests/TableSettingsTest.java
@@ -88,7 +88,7 @@ public class TableSettingsTest extends SQLTransportIntegrationTest {
     public void testFilterOnNull() throws Exception {
         execute("select * from information_schema.tables " +
                 "where settings IS NULL");
-        assertEquals(22L, response.rowCount());
+        assertEquals(23L, response.rowCount());
         execute("select * from information_schema.tables " +
                 "where table_name = 'settings_table' and settings['warmer']['enabled'] IS NULL");
         assertEquals(0, response.rowCount());
