5ad2055b820b1671bba7f8d18b5c276bdefdf1a8
Sebastian Utz <su@rtme.net>
2017-08-08 11:10:31 +0200

    rebuild `ingestion_rules` iterables on cluster state change
    also rebuild `routines` iterable on cluster state change

--- sql/src/main/java/io/crate/metadata/IngestionRuleInfos.java
+++ sql/src/main/java/io/crate/metadata/IngestionRuleInfos.java
@@ -3,7 +3,6 @@ package io.crate.metadata;
 
 import io.crate.metadata.rule.ingest.IngestRulesMetaData;
 import org.elasticsearch.cluster.metadata.MetaData;
-import org.elasticsearch.cluster.service.ClusterService;
 
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -13,21 +12,20 @@ import static java.util.Collections.emptyIterator;
 
 public class IngestionRuleInfos implements Iterable<IngestionRuleInfo> {
 
-    private final ClusterService clusterService;
+    private final MetaData metaData;
 
-    public IngestionRuleInfos(ClusterService clusterService) {
-        this.clusterService = clusterService;
+    public IngestionRuleInfos(MetaData metaData) {
+        this.metaData = metaData;
     }
 
     @Override
     public Iterator<IngestionRuleInfo> iterator() {
-        MetaData metaData = clusterService.state().getMetaData();
         IngestRulesMetaData ingestRulesMetaData = metaData.custom(IngestRulesMetaData.TYPE);
         if (ingestRulesMetaData == null || ingestRulesMetaData.getIngestRules() == null) {
             return emptyIterator();
         }
         List<IngestionRuleInfo> ingestionRuleInfoList = new ArrayList<>();
-        
+
         ingestRulesMetaData.getIngestRules().forEach((key, value) -> value.forEach(ingestRule ->
             ingestionRuleInfoList.add(new IngestionRuleInfo(
                 ingestRule.getName(),
