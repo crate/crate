37495452b462480907e4390ba58d4d914bb8aca7
Sebastian Utz <su@rtme.net>
2017-08-17 17:43:13 +0200

    migrated the POC mqtt plugin code into crate
    also adjusted code and tests to work with the core
    (e.g. usage of netty4 instead of netty3)

---
+++ enterprise/mqtt/src/test/java/io/crate/mqtt/protocol/CrateMqttProcessorTest.java
@@ -0,0 +1,101 @@
+/*
+ * This file is part of a module with proprietary Enterprise Features.
+ *
+ * Licensed to Crate.io Inc. ("Crate.io") under one or more contributor
+ * license agreements.  See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.
+ *
+ * Unauthorized copying of this file, via any medium is strictly prohibited.
+ *
+ * To use this file, Crate.io must have given you permission to enable and
+ * use such Enterprise Features and you must have a valid Enterprise or
+ * Subscription Agreement with Crate.io.  If you enable or use the Enterprise
+ * Features, you represent and warrant that you have a valid Enterprise or
+ * Subscription Agreement with Crate.io.  Your use of the Enterprise Features
+ * if governed by the terms and conditions of your Enterprise or Subscription
+ * Agreement with Crate.io.
+ */
+
+package io.crate.mqtt.protocol;
+
+import io.crate.mqtt.operations.CrateIngestService;
+import io.netty.channel.embedded.EmbeddedChannel;
+import io.netty.handler.codec.mqtt.*;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+
+
+public class CrateMqttProcessorTest {
+
+    private final CrateMqttProcessor processor = new CrateMqttProcessor(mock(CrateIngestService.class));
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+    private static MqttMessage connectMessage(String clientId, boolean isCleanSession) {
+        return CrateMqttMessageBuilders.connect()
+                .protocolVersion(MqttVersion.MQTT_3_1_1)
+                .clientId(clientId)
+                .cleanSession(isCleanSession)
+                .build();
+    }
+
+    @Test
+    public void testConnectWithWrongMqttVersion() throws Exception {
+        EmbeddedChannel ch = new EmbeddedChannel();
+
+        MqttFixedHeader fixedHeader = new MqttFixedHeader(
+                MqttMessageType.CONNECT, false, MqttQoS.AT_LEAST_ONCE, false, 0);
+        MqttConnectVariableHeader variableHeader = new MqttConnectVariableHeader(
+                "connect", (byte) 1, false, false, false, (byte) 1, false, false, 60);
+        MqttConnectPayload payload = new MqttConnectPayload("mqttClient", null, null, null, null);
+        processor.handleConnect(ch,
+                (MqttConnectMessage) MqttMessageFactory.newMessage(fixedHeader, variableHeader, payload));
+
+        MqttConnAckMessage response = ch.readOutbound();
+        assertThat(response.variableHeader().connectReturnCode(),
+                is(MqttConnectReturnCode.CONNECTION_REFUSED_UNACCEPTABLE_PROTOCOL_VERSION));
+        assertFalse(response.variableHeader().isSessionPresent());
+    }
+
+    @Test
+    public void testConnectWithoutClientId() throws Exception {
+        EmbeddedChannel ch = new EmbeddedChannel();
+
+        // clientId may be null if the session is clean
+        MqttMessage msg = connectMessage(null, true);
+        processor.handleConnect(ch, (MqttConnectMessage) msg);
+
+        MqttConnAckMessage response = ch.readOutbound();
+        assertThat(response.variableHeader().connectReturnCode(),
+                is(MqttConnectReturnCode.CONNECTION_ACCEPTED));
+        assertTrue(response.variableHeader().isSessionPresent());
+
+        // clientID must not be null if the session is not clean
+        msg = connectMessage(null, false);
+        processor.handleConnect(ch, (MqttConnectMessage) msg);
+
+        response = ch.readOutbound();
+        assertThat(response.variableHeader().connectReturnCode(),
+                is(MqttConnectReturnCode.CONNECTION_REFUSED_IDENTIFIER_REJECTED));
+        assertFalse(response.variableHeader().isSessionPresent());
+    }
+
+    @Test
+    public void testConnectAck() throws Exception {
+        EmbeddedChannel ch = new EmbeddedChannel();
+
+        MqttMessage msg = connectMessage("client", true);
+        processor.handleConnect(ch, (MqttConnectMessage) msg);
+
+        MqttConnAckMessage response = ch.readOutbound();
+        assertThat(response.variableHeader().connectReturnCode(),
+                is(MqttConnectReturnCode.CONNECTION_ACCEPTED));
+        assertTrue(response.variableHeader().isSessionPresent());
+    }
+}
