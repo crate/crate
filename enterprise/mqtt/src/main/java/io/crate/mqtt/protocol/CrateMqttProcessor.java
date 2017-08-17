8464dd3814039076c6c653ffa2de88169d62917b
Sebastian Utz <su@rtme.net>
2017-08-17 17:43:13 +0200

    migrated the POC mqtt plugin code into crate
    also adjusted code and tests to work with the core
    (e.g. usage of netty4 instead of netty3)

---
+++ enterprise/mqtt/src/main/java/io/crate/mqtt/protocol/CrateMqttProcessor.java
@@ -0,0 +1,165 @@
+/*
+ * This file is part of a module with proprietary Enterprise Features.
+ *
+ * Licensed to Crate.io Inc. ("Crate.io") under one or more contributor
+ * license agreements.  See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.
+ *
+ * Unauthorized copying of this file, via any medium is strictly prohibited.
+ *
+ * To use this file, Crate.io must have given you permission to enable and
+ * use such Enterprise Features and you must have a valid Enterprise or
+ * Subscription Agreement with Crate.io.  If you enable or use the Enterprise
+ * Features, you represent and warrant that you have a valid Enterprise or
+ * Subscription Agreement with Crate.io.  Your use of the Enterprise Features
+ * if governed by the terms and conditions of your Enterprise or Subscription
+ * Agreement with Crate.io.
+ */
+
+package io.crate.mqtt.protocol;
+
+import io.crate.mqtt.operations.CrateIngestService;
+import io.moquette.server.netty.NettyUtils;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.handler.codec.mqtt.*;
+import io.netty.handler.timeout.IdleStateHandler;
+import org.apache.logging.log4j.Logger;
+import org.elasticsearch.common.logging.Loggers;
+import org.elasticsearch.index.engine.VersionConflictEngineException;
+
+import java.util.Locale;
+import java.util.UUID;
+import java.util.function.BiConsumer;
+
+
+public class CrateMqttProcessor {
+
+    private static final Logger LOGGER = Loggers.getLogger(CrateMqttProcessor.class);
+    private final CrateIngestService ingestService;
+
+    public CrateMqttProcessor(CrateIngestService ingestService) {
+        this.ingestService = ingestService;
+    }
+
+    public void handleConnect(Channel channel, MqttConnectMessage msg) {
+        String clientId = msg.payload().clientIdentifier();
+        LOGGER.info("CONNECT for client <{}>", clientId);
+
+        if (!(msg.variableHeader().version() == MqttVersion.MQTT_3_1.protocolLevel()
+                || msg.variableHeader().version() == MqttVersion.MQTT_3_1_1.protocolLevel())) {
+            sendErrorResponse(channel, MqttConnectReturnCode.CONNECTION_REFUSED_UNACCEPTABLE_PROTOCOL_VERSION)
+                    .addListener(f -> LOGGER.warn("CONNECT sent UNACCEPTABLE_PROTOCOL_VERSION"));
+            return;
+        }
+
+        /*
+         If you don’t need a state to be hold by the broker,
+         in MQTT 3.1.1 (current standard) it is also possible to send an empty ClientId,
+         which results in a connection without any state.
+         A condition is that clean session is true, otherwise the connection will be rejected.
+         http://www.hivemq.com/blog/mqtt-essentials-part-3-client-broker-connection-establishment
+         */
+        if (clientId == null || clientId.length() == 0) {
+            if (!msg.variableHeader().isCleanSession()) {
+                sendErrorResponse(channel, MqttConnectReturnCode.CONNECTION_REFUSED_IDENTIFIER_REJECTED)
+                        .addListener(cf -> LOGGER.warn("CONNECT sent IDENTIFIER_REJECTED"));
+                return;
+            }
+            clientId = UUID.randomUUID().toString();
+            LOGGER.info("Client connected with server generated identifier: {}", clientId);
+        }
+        NettyUtils.clientID(channel, clientId);
+
+        int keepAlive = msg.variableHeader().keepAliveTimeSeconds();
+        if (keepAlive > 0) {
+            if (LOGGER.isDebugEnabled()) {
+                LOGGER.debug("Client connected with keepAlive of {} s", keepAlive);
+            }
+            NettyUtils.keepAlive(channel, keepAlive);
+
+            if (channel.pipeline().names().contains("idleStateHandler")) {
+                channel.pipeline().remove("idleStateHandler");
+            }
+            channel.pipeline().addFirst("idleStateHandler", new IdleStateHandler(0, 0, Math.round(keepAlive * 1.5f)));
+        }
+
+        channel.writeAndFlush(CrateMqttMessageBuilders.connAck()
+                .returnCode(MqttConnectReturnCode.CONNECTION_ACCEPTED)
+                .sessionPresent(true)
+                .build())
+                .addListener(cf -> LOGGER.info("CONNECT sent CONNECTION_ACCEPTED"));
+    }
+
+    private ChannelFuture sendErrorResponse(Channel channel, MqttConnectReturnCode returnCode) {
+
+        return channel.writeAndFlush(CrateMqttMessageBuilders.connAck()
+                .returnCode(returnCode)
+                .sessionPresent(false)
+                .build())
+                .addListener(ChannelFutureListener.CLOSE);
+    }
+
+    public void handlePublish(Channel channel, MqttPublishMessage msg) {
+        final MqttQoS qos = msg.fixedHeader().qosLevel();
+        String clientId = NettyUtils.clientID(channel);
+        switch (qos) {
+            case AT_LEAST_ONCE:
+                SendPubAckCallback ackCallback = new SendPubAckCallback(
+                        channel, msg.variableHeader().packetId(), msg.fixedHeader().isDup());
+                ingestService.doInsert(clientId, msg, ackCallback);
+                break;
+            default:
+                String message = String.format(Locale.ENGLISH, "QoS %s (%s) is not supported.",
+                        qos.value(), qos.name());
+                throw new UnsupportedOperationException(message);
+        }
+    }
+
+    public void handleDisconnect(Channel channel) throws InterruptedException {
+        channel.flush()
+                .close()
+                .addListener(cf -> LOGGER.debug("Client closed connection by sending DISCONNECT"));
+        ingestService.close();
+    }
+
+    public void handlePingReq(Channel channel) {
+        channel.writeAndFlush(CrateMqttMessageBuilders.pingResp()
+            .qos(MqttQoS.AT_LEAST_ONCE)
+            .build())
+            .addListener(cf -> LOGGER.info("PINGRESP sent"));
+    }
+
+    private static class SendPubAckCallback implements BiConsumer<Object, Throwable> {
+
+        private final Channel channel;
+        private final Integer packetId;
+        private final boolean isDupFlag;
+
+        private SendPubAckCallback(Channel channel, Integer packetId, boolean isDupFlag) {
+            this.channel = channel;
+            this.packetId = packetId;
+            this.isDupFlag = isDupFlag;
+        }
+
+        @Override
+        public void accept(Object result, Throwable t) {
+            // If the DUP flag is set by the client a version conflict is ignored and a PubAck is returned.
+            if (t == null || (t.getClass() == VersionConflictEngineException.class && isDupFlag)) {
+                sendPubAck(channel, packetId, isDupFlag);
+            } else {
+                LOGGER.info("Failed to insert MQTT message into CrateDB: {}", t.getMessage());
+            }
+        }
+
+        private static void sendPubAck(Channel channel, int packetId, boolean isDupFlag) {
+            channel.writeAndFlush(CrateMqttMessageBuilders.pubAck()
+                    .qos(MqttQoS.AT_LEAST_ONCE)
+                    .isDup(isDupFlag)
+                    .packetId(packetId)
+                    .build())
+                    .addListener(cf -> LOGGER.info("PUBACK sent"));
+        }
+    }
+}
---
+++ enterprise/mqtt/src/test/java/io/crate/mqtt/operations/MqttPublishIntegrationTest.java
@@ -0,0 +1,146 @@
+package io.crate.mqtt.operations;
+
+import io.crate.mqtt.protocol.CrateMqttMessageBuilders;
+import io.netty.buffer.Unpooled;
+import io.netty.handler.codec.mqtt.MqttConnAckMessage;
+import io.netty.handler.codec.mqtt.MqttConnectReturnCode;
+import io.netty.handler.codec.mqtt.MqttMessage;
+import io.netty.handler.codec.mqtt.MqttMessageBuilders;
+import io.netty.handler.codec.mqtt.MqttPubAckMessage;
+import io.netty.handler.codec.mqtt.MqttPublishMessage;
+import io.netty.handler.codec.mqtt.MqttQoS;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+
+
+public class MqttPublishIntegrationTest extends MqttIntegrationTest {
+
+    private static final AtomicInteger messageId = new AtomicInteger(0);
+
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+
+        execute("CREATE TABLE IF NOT EXISTS mqtt.raw (\n" +
+                "  client_id STRING,\n" +
+                "  packet_id INTEGER,\n" +
+                "  topic STRING,\n" +
+                "  ts TIMESTAMP,\n" +
+                "  payload OBJECT(IGNORED),\n" +
+                "  PRIMARY KEY (\"client_id\", \"packet_id\")\n" +
+                ") WITH (column_policy = 'strict', number_of_replicas = '0')");
+
+        // MQTT CONNECT
+        MQTT_CLIENT.sendMessage(CrateMqttMessageBuilders.connect()
+                .clientId(MQTT_CLIENT.clientId())
+                .build());
+        MqttConnAckMessage response = (MqttConnAckMessage) MQTT_CLIENT.lastReceivedMessage();
+        assertThat(response.variableHeader().connectReturnCode(),
+                is(MqttConnectReturnCode.CONNECTION_ACCEPTED));
+    }
+
+
+    private static MqttPublishMessage publishMessage(String payload) {
+        return publishMessage(MqttQoS.AT_LEAST_ONCE, payload);
+    }
+
+    private static MqttPublishMessage publishMessage(MqttQoS level, String payload) {
+        return CrateMqttMessageBuilders.publish()
+                .topicName("t1")
+                .qos(level)
+                .payload(Unpooled.copiedBuffer(payload.getBytes()))
+                .messageId(messageId.incrementAndGet())
+                .build();
+    }
+
+    @Test
+    public void testPublishUnsupportedQoS() throws Exception {
+        MqttPublishMessage mqttPublishMessage = CrateMqttMessageBuilders.publish()
+                .qos(MqttQoS.AT_LEAST_ONCE)
+                .topicName("t1")
+                .messageId(messageId.incrementAndGet())
+                .retained(false)
+                .payload(Unpooled.copiedBuffer("Hello World".getBytes()))
+                .build();
+        MQTT_CLIENT.sendMessage(mqttPublishMessage);
+
+        expectedException.expectMessage("MQTT client did not receive message in time");
+        MQTT_CLIENT.lastReceivedMessage();
+    }
+
+    @Test
+    public void testPublishQoS1WithNonJsonPayload() throws Exception {
+        MqttMessage message = publishMessage("hello world");
+        MQTT_CLIENT.sendMessage(message);
+
+        expectedException.expectMessage("MQTT client did not receive message in time");
+        MQTT_CLIENT.lastReceivedMessage();
+    }
+
+    @Test
+    public void testPublishQos1WithoutDUPFlag() throws Exception {
+        int mId = messageId.incrementAndGet();
+        // isDup = false
+        MqttMessageBuilders.PublishBuilder message = CrateMqttMessageBuilders.publish()
+                .topicName("t1")
+                .qos(MqttQoS.AT_LEAST_ONCE)
+                .payload(Unpooled.copiedBuffer("{\"ts\":1498797237000}".getBytes()))
+                .messageId(mId);
+
+        MQTT_CLIENT.sendMessage(message.build());
+        MqttPubAckMessage response = (MqttPubAckMessage) MQTT_CLIENT.lastReceivedMessage();
+        assertThat(response.variableHeader().messageId(), is(mId));
+
+        // resend same message with isDup = false
+        MQTT_CLIENT.sendMessage(message.build());
+        expectedException.expectMessage("MQTT client did not receive message in time");
+        MQTT_CLIENT.lastReceivedMessage();
+    }
+
+    @Test
+    public void testPublishQos1WithDUPFlag() throws Exception {
+        // isDup = false
+        MqttPublishMessage message = publishMessage(MqttQoS.AT_LEAST_ONCE, "{\"ts\":1498797237000}");
+        MQTT_CLIENT.sendMessage(message);
+        MqttPubAckMessage response = (MqttPubAckMessage) MQTT_CLIENT.lastReceivedMessage();
+        assertThat(response.variableHeader().messageId(), is(message.variableHeader().packetId()));
+
+        // isDup = true
+        message = publishMessage(MqttQoS.AT_LEAST_ONCE, "{\"ts\":1498797237000}");
+        MQTT_CLIENT.sendMessage(message);
+        response = (MqttPubAckMessage) MQTT_CLIENT.lastReceivedMessage();
+        assertThat(response.variableHeader().messageId(), is(message.variableHeader().packetId()));
+    }
+
+    @Test
+    public void testPublishQoS1WithJsonPayload() throws Exception {
+        MqttPublishMessage message = publishMessage("{}");
+        MQTT_CLIENT.sendMessage(message);
+        MqttPubAckMessage response = (MqttPubAckMessage) MQTT_CLIENT.lastReceivedMessage();
+        assertThat(response.variableHeader().messageId(), is(message.variableHeader().packetId()));
+    }
+
+    @Test
+    public void testMessageInserted() throws Exception {
+        MqttPublishMessage message = publishMessage("{\"ts\":1498797237000}");
+        MQTT_CLIENT.sendMessage(message);
+        MqttPubAckMessage mqttResponse = (MqttPubAckMessage) MQTT_CLIENT.lastReceivedMessage();
+        assertThat(mqttResponse.variableHeader().messageId(), is(message.variableHeader().packetId()));
+
+        execute("SELECT client_id, packet_id, topic, payload, ts " +
+                "FROM mqtt.raw " +
+                "WHERE client_id = ? AND packet_id = ?",
+            new Object[]{MQTT_CLIENT.clientId(), message.variableHeader().packetId()});
+        assertThat(response.rowCount(), is(1L));
+        assertThat(response.rows()[0][0], is(MQTT_CLIENT.clientId()));
+        assertThat(response.rows()[0][1], is(message.variableHeader().packetId()));
+        assertThat(response.rows()[0][2], is(message.variableHeader().topicName()));
+        assertThat(response.rows()[0][3], notNullValue());
+        assertThat(response.rows()[0][4], notNullValue());
+    }
+}
