19b947872335d7018fd883d9e6bccafbb45ece66
Andrei Dan <andrei@crate.io>
2017-08-23 16:31:43 +0100

    Create IngestRules with the where condition parameters substituted.

--- sql/src/main/java/io/crate/analyze/CreateIngestionRuleAnalyzer.java
+++ sql/src/main/java/io/crate/analyze/CreateIngestionRuleAnalyzer.java
@@ -1,28 +1,55 @@
 package io.crate.analyze;
 
 
+import io.crate.analyze.symbol.Symbol;
 import io.crate.metadata.Schemas;
 import io.crate.metadata.TableIdent;
+import io.crate.sql.ExpressionFormatter;
 import io.crate.sql.tree.CreateIngestRule;
-import io.crate.sql.tree.QualifiedName;
+import io.crate.sql.tree.ParameterExpression;
 
 
 public class CreateIngestionRuleAnalyzer {
 
+    private static class ExpressionParameterSubstitutionFormatter extends ExpressionFormatter.Formatter {
+
+        private final Analysis analysis;
+
+        ExpressionParameterSubstitutionFormatter(Analysis context) {
+            this.analysis = context;
+        }
+
+        @Override
+        public String visitParameterExpression(ParameterExpression node, Void context) {
+            String formattedExpression = null;
+            Symbol symbol = analysis.parameterContext().apply(node);
+            if (symbol != null) {
+                formattedExpression = symbol.representation();
+            }
+            return formattedExpression;
+        }
+    }
+
     private final Schemas schemas;
 
-    public CreateIngestionRuleAnalyzer(Schemas schemas) {
+    CreateIngestionRuleAnalyzer(Schemas schemas) {
         this.schemas = schemas;
     }
 
-    CreateIngestionRuleAnalysedStatement analyze(CreateIngestRule node) {
+    CreateIngestionRuleAnalysedStatement analyze(CreateIngestRule node, Analysis context) {
         TableIdent tableIdent = TableIdent.of(node.targetTable(), Schemas.DEFAULT_SCHEMA_NAME);
         validateTableName(tableIdent);
+
+        String whereCondition = "";
+        if (node.where().isPresent()) {
+            whereCondition = ExpressionFormatter.formatStandaloneExpression(node.where().get(),
+                new ExpressionParameterSubstitutionFormatter(context));
+        }
 
         return new CreateIngestionRuleAnalysedStatement(node.ruleName(),
             node.sourceIdent(),
             tableIdent,
-            node.where().isPresent() ? node.where().get().toString() : "");
+            whereCondition);
     }
 
     private void validateTableName(TableIdent tableIdent) {
