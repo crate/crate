1f198e903b77e580a94707cbab4d097914625623
Mathias Fussenegger <f.mathias@zignar.net>
2017-09-18 13:52:30 +0200

    Add LogicalPlan/LogicalPlanner for aggregates on base user tables
    This adds a new `LogicalPlan` and `LogicalPlanner` which should
    eventually replace the `ConsumingPlanner`.
    
    See the class documentation for a description of the `LogicalPlan` and
    `LogicalPlanner`.
    
    The goal here is to reduce the complexity that we currently have in the
    `ConsumingPlanner` due to a lot of repeating logic. Almost all consumers
    currently deal with limit and order by on their own. The consumers are
    not really composable.
    
    Although this commit already introduces a lot of operators which will be
    useable for all types of queries, it is only enabled for aggregations on
    base user table because functionality required for other queriees is
    still missing or incomplete.

---
+++ sql/src/main/java/io/crate/planner/operators/FetchOrEval.java
@@ -0,0 +1,82 @@
+/*
+ * Licensed to Crate under one or more contributor license agreements.
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.  Crate licenses this file
+ * to you under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.  You may
+ * obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.  See the License for the specific language governing
+ * permissions and limitations under the License.
+ *
+ * However, if you have executed another commercial license agreement
+ * with Crate these terms will supersede the license and you may use the
+ * software solely pursuant to the terms of the relevant commercial
+ * agreement.
+ */
+
+package io.crate.planner.operators;
+
+import io.crate.analyze.OrderBy;
+import io.crate.analyze.symbol.Symbol;
+import io.crate.planner.Plan;
+import io.crate.planner.Planner;
+import io.crate.planner.projection.EvalProjection;
+import io.crate.planner.projection.builder.InputColumns;
+import io.crate.planner.projection.builder.ProjectionBuilder;
+
+import javax.annotation.Nullable;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+
+class FetchOrEval implements LogicalPlan {
+
+    final LogicalPlan source;
+    private final Set<Symbol> usedColumns;
+    final List<Symbol> outputs;
+
+    static LogicalPlan.Builder create(LogicalPlan.Builder source, List<Symbol> outputs) {
+        return usedColumns -> new FetchOrEval(source.build(Collections.emptySet()), usedColumns, outputs);
+    }
+
+    private FetchOrEval(LogicalPlan source, Set<Symbol> usedColumns, List<Symbol> outputs) {
+        this.source = source;
+        this.usedColumns = usedColumns;
+        this.outputs = outputs;
+    }
+
+    @Override
+    public Plan build(Planner.Context plannerContext,
+                      ProjectionBuilder projectionBuilder,
+                      int limit,
+                      int offset,
+                      @Nullable OrderBy order) {
+
+        Plan plan = source.build(plannerContext, projectionBuilder, limit, offset, order);
+        if (!source.outputs().equals(outputs)) {
+            InputColumns.Context ctx = new InputColumns.Context(source.outputs());
+            plan.addProjection(new EvalProjection(InputColumns.create(outputs, ctx)));
+        }
+        return plan;
+    }
+
+    @Override
+    public LogicalPlan tryCollapse() {
+        LogicalPlan collapsed = source.tryCollapse();
+        if (collapsed == this) {
+            return this;
+        }
+        return new FetchOrEval(collapsed, usedColumns, outputs);
+    }
+
+    @Override
+    public List<Symbol> outputs() {
+        return outputs;
+    }
+}
---
+++ sql/src/main/java/io/crate/planner/operators/Order.java
@@ -0,0 +1,105 @@
+/*
+ * Licensed to Crate under one or more contributor license agreements.
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.  Crate licenses this file
+ * to you under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.  You may
+ * obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.  See the License for the specific language governing
+ * permissions and limitations under the License.
+ *
+ * However, if you have executed another commercial license agreement
+ * with Crate these terms will supersede the license and you may use the
+ * software solely pursuant to the terms of the relevant commercial
+ * agreement.
+ */
+
+package io.crate.planner.operators;
+
+import io.crate.analyze.OrderBy;
+import io.crate.analyze.symbol.InputColumn;
+import io.crate.analyze.symbol.Symbol;
+import io.crate.planner.Plan;
+import io.crate.planner.Planner;
+import io.crate.planner.PositionalOrderBy;
+import io.crate.planner.ResultDescription;
+import io.crate.planner.projection.OrderedTopNProjection;
+import io.crate.planner.projection.builder.InputColumns;
+import io.crate.planner.projection.builder.ProjectionBuilder;
+
+import javax.annotation.Nullable;
+import java.util.List;
+import java.util.Set;
+
+import static io.crate.planner.operators.LogicalPlanner.NO_LIMIT;
+import static io.crate.planner.operators.LogicalPlanner.extractColumns;
+
+class Order implements LogicalPlan {
+
+    final LogicalPlan source;
+    final OrderBy orderBy;
+
+    static LogicalPlan.Builder create(LogicalPlan.Builder source, @Nullable OrderBy orderBy) {
+        if (orderBy == null) {
+            return source;
+        }
+        Set<Symbol> columnsInOrderBy = extractColumns(orderBy.orderBySymbols());
+        return usedColumns -> {
+            columnsInOrderBy.addAll(usedColumns);
+            return new Order(source.build(columnsInOrderBy), orderBy);
+        };
+    }
+
+    private Order(LogicalPlan source, OrderBy orderBy) {
+        this.source = source;
+        this.orderBy = orderBy;
+    }
+
+    @Override
+    public Plan build(Planner.Context plannerContext,
+                      ProjectionBuilder projectionBuilder,
+                      int limit,
+                      int offset,
+                      OrderBy order) {
+        Plan plan = source.build(plannerContext, projectionBuilder, limit, offset, orderBy);
+        ResultDescription resultDescription = plan.resultDescription();
+        if (resultDescription.orderBy() == null) {
+            InputColumns.Context ctx = new InputColumns.Context(source.outputs());
+            OrderedTopNProjection topNProjection = new OrderedTopNProjection(
+                limit,
+                offset,
+                InputColumn.fromSymbols(source.outputs()),
+                InputColumns.create(orderBy.orderBySymbols(), ctx),
+                orderBy.reverseFlags(),
+                orderBy.nullsFirst()
+            );
+            plan.addProjection(
+                topNProjection,
+                NO_LIMIT,
+                0,
+                PositionalOrderBy.of(orderBy, source.outputs())
+            );
+        }
+        return plan;
+    }
+
+    @Override
+    public LogicalPlan tryCollapse() {
+        LogicalPlan collapsed = source.tryCollapse();
+        if (collapsed == source) {
+            return this;
+        }
+        return new Order(collapsed, orderBy);
+    }
+
+    @Override
+    public List<Symbol> outputs() {
+        return source.outputs();
+    }
+}
