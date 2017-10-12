74317f6678f218a95a7a7a45ddff257e29c5f447
Marios Trivyzas <marios@crate.io>
2017-10-12 19:41:54 +0200

    Upgraded ES to v5.6.3

--- core/src/main/java/io/crate/Version.java
+++ core/src/main/java/io/crate/Version.java
@@ -41,7 +41,7 @@ public class Version {
 
 
     public static final boolean SNAPSHOT = true;
-    public static final Version CURRENT = new Version(2030099, SNAPSHOT, org.elasticsearch.Version.V_5_6_2_UNRELEASED);
+    public static final Version CURRENT = new Version(2030099, SNAPSHOT, org.elasticsearch.Version.V_5_6_3_UNRELEASED);
 
     static {
         // safe-guard that we don't release a version with DEBUG_MODE set to true
