70629880437136f06f58821db262daff97ad4ea1
Autophagy <mika@crate.io>
2017-10-12 13:17:52 +0200

    Prepare release 2.1.8

--- core/src/main/java/io/crate/Version.java
+++ core/src/main/java/io/crate/Version.java
@@ -41,7 +41,7 @@ public class Version {
 
 
     public static final boolean SNAPSHOT = false;
-    public static final Version CURRENT = new Version(2010799, SNAPSHOT, org.elasticsearch.Version.V_5_4_3_UNRELEASED);
+    public static final Version CURRENT = new Version(2010899, SNAPSHOT, org.elasticsearch.Version.V_5_4_3_UNRELEASED);
 
     static {
         // safe-guard that we don't release a version with DEBUG_MODE set to true
