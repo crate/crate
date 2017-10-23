crate/crate

commit 12f4258f51bb274d18e9231f9e6c98253c7e9413

Decouple sharding/bulk req creation from upsert executor
This extracts the bulk-sizing logic from the
BatchIteratorBackpressureExecutor, separating the logic a bit.

There is now a new `partitionBySize` function that can be used to group
elements of a BatchIterator.

The new (logical) flow is as follows:

    BatchIterator<Row>
        | // partitionBySize(bulkSize, groupFunc)
    BatchIterator<ShardedRequests>
        |
    BatchIteratorBackpressureExecutor
        |-> ShardingUpsertExecutor.execute

sql/src/main/java/io/crate/operation/projectors/BatchIteratorBackpressureExecutor.java
@@ -23,94 +23,117 @@
 package io.crate.operation.projectors;
 
 import io.crate.data.BatchIterator;
-import io.crate.data.Row;
 import org.elasticsearch.action.bulk.BackoffPolicy;
 import org.elasticsearch.common.unit.TimeValue;
 
+import javax.annotation.Nullable;
 import java.util.Iterator;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.BiConsumer;
-import java.util.function.BooleanSupplier;
-import java.util.function.Consumer;
-import java.util.function.Supplier;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 /**
- * Consumes a BatchIterator in bulks based on the configured {@link #bulkSize}. When a bulk is complete, it checks
- * whether executing the bulk is possible (based on the provided {@link #pauseConsumption}) in which case it runs
- * the {@link #executeFunction}. Otherwise, it stops consuming the iterator and schedules retrying the execution of the
- * bulk for later.
- * Iterator consumption is resumed once the parked/scheduled bulk is executed successfully.
+ * Consumes a BatchIterator, concurrently invoking {@link #execute} on
+ * each item until {@link #pauseConsumption} returns true.
  *
- * @param <R> the type of the result the {@link #executeFunction} future returns.
+ * The future returned on {@link #consumeIteratorAndExecute()} completes
+ * once all items in the BatchIterator have been processed.
+ *
+ * If {@link #pauseConsumption} returns true it will pause for a while ({@link #throttleDelay})
+ * and afterwards resume consumption.
  */
-public class BatchIteratorBackpressureExecutor<R> {
+public class BatchIteratorBackpressureExecutor<T, R> {
 
-    private final BatchIterator<Row> batchIterator;
-    private final Supplier<CompletableFuture<R>> executeFunction;
-    private final Consumer<Row> onRowConsumer;
+    private final BatchIterator<T> batchIterator;
+    private final Function<T, CompletableFuture<R>> execute;
     private final ScheduledExecutorService scheduler;
     private final Iterator<TimeValue> throttleDelay;
-    private final BooleanSupplier pauseConsumption;
-    private final BiConsumer<Object, Throwable> continueConsumptionOrFinish;
-    private final int bulkSize;
+    private final BinaryOperator<R> combiner;
+    private final Predicate<T> pauseConsumption;
+    private final BiConsumer<R, Throwable> continueConsumptionOrFinish;
     private final AtomicInteger inFlightExecutions = new AtomicInteger(0);
-    private final CompletableFuture<Void> resultFuture = new CompletableFuture<>();
+    private final CompletableFuture<R> resultFuture = new CompletableFuture<>();
     private final Semaphore semaphore = new Semaphore(1);
 
-    private int indexInBulk = 0;
+    private final AtomicReference<R> resultRef;
+    private final AtomicReference<Throwable> failureRef = new AtomicReference<>(null);
     private volatile boolean consumptionFinished = false;
 
-    public BatchIteratorBackpressureExecutor(BatchIterator<Row> batchIterator,
-                                             ScheduledExecutorService scheduler,
-                                             Consumer<Row> onRowConsumer,
-                                             Supplier<CompletableFuture<R>> executeFunction,
-                                             BooleanSupplier pauseConsumption,
-                                             int bulkSize,
+    /**
+     * @param batchIterator provides the items for {@code execute}
+     * @param execute async function which is called for each item from the batchIterator
+     * @param combiner used to combine partial results returned by the {@code execute} function
+     * @param identity default value (this is the result, if the batchIterator contains no items)
+     * @param pauseConsumption predicate used to check if the consumption should be paused
+     */
+    public BatchIteratorBackpressureExecutor(ScheduledExecutorService scheduler,
+                                             BatchIterator<T> batchIterator,
+                                             Function<T, CompletableFuture<R>> execute,
+                                             BinaryOperator<R> combiner,
+                                             R identity,
+                                             Predicate<T> pauseConsumption,
                                              BackoffPolicy backoffPolicy) {
         this.batchIterator = batchIterator;
         this.scheduler = scheduler;
-        this.onRowConsumer = onRowConsumer;
-        this.executeFunction = executeFunction;
+        this.execute = execute;
+        this.combiner = combiner;
         this.pauseConsumption = pauseConsumption;
-        this.bulkSize = bulkSize;
         this.throttleDelay = backoffPolicy.iterator();
+        this.resultRef = new AtomicReference<>(identity);
         this.continueConsumptionOrFinish = this::continueConsumptionOrFinish;
     }
 
-    public CompletableFuture<Void> consumeIteratorAndExecute() {
+    public CompletableFuture<R> consumeIteratorAndExecute() {
         consumeIterator();
         return resultFuture;
     }
 
-    private void continueConsumptionOrFinish(Object ignored, Throwable failure) {
+    private void continueConsumptionOrFinish(@Nullable R result, Throwable failure) {
         int inFlight = inFlightExecutions.decrementAndGet();
         assert inFlight >= 0 : "Number of in-flight executions must not be negative";
 
         if (consumptionFinished) {
+            R finalResult = maybeUpdateResult(result);
             if (inFlight == 0) {
-                setResult(failure);
+                setResult(finalResult, failure == null ? failureRef.get() : failure);
             }
             // else: waiting for other async-operations to finish
         } else {
+            if (failure != null) {
+                failureRef.set(failure);
+            }
+            if (result != null) {
+                resultRef.accumulateAndGet(result, combiner);
+            }
             consumeIterator();
         }
     }
 
-    private void setResult(Throwable failure) {
+    private R maybeUpdateResult(@Nullable R result) {
+        if (result == null) {
+            return resultRef.get();
+        }
+        return resultRef.accumulateAndGet(result, combiner);
+    }
+
+    private void setResult(R finalResult, Throwable failure) {
         batchIterator.close();
         if (failure == null) {
-            resultFuture.complete(null);
+            resultFuture.complete(finalResult);
         } else {
             resultFuture.completeExceptionally(failure);
         }
     }
 
     /**
-     * Consumes the rows from the BatchIterator and invokes {@link #executeFunction} every {@link #bulkSize} rows.
+     * Consumes the rows from the BatchIterator and invokes {@link #execute} on each row.
      * This loop continues until either:
      *
      *  - The BatchIterator has been fully consumed.
@@ -127,31 +150,27 @@ public class BatchIteratorBackpressureExecutor<R> {
         }
         try {
             while (batchIterator.moveNext()) {
-                indexInBulk++;
-                onRowConsumer.accept(batchIterator.currentElement());
-
-                if (indexInBulk == bulkSize) {
-                    if (pauseConsumption.getAsBoolean()) {
-                        // release semaphore inside resumeConsumption: after throttle delay has passed
-                        // to make sure callbacks of previously triggered async operations don't resume consumption
-                        scheduler.schedule(this::resumeConsumption, throttleDelay.next().getMillis(), TimeUnit.MILLISECONDS);
-                        return;
-                    }
-                    executeBatch();
+                T item = batchIterator.currentElement();
+                if (pauseConsumption.test(item)) {
+                    // release semaphore inside resumeConsumption: after throttle delay has passed
+                    // to make sure callbacks of previously triggered async operations don't resume consumption
+                    scheduler.schedule(this::resumeConsumption, throttleDelay.next().getMillis(), TimeUnit.MILLISECONDS);
+                    return;
                 }
+                execute(item);
             }
 
             inFlightExecutions.incrementAndGet();
             if (batchIterator.allLoaded()) {
                 semaphore.release();
                 consumptionFinished = true;
-                executeFunction.get().whenComplete(continueConsumptionOrFinish);
+                continueConsumptionOrFinish.accept(null, null);
             } else {
                 batchIterator.loadNextBatch()
                     // consumption can only be continued after loadNextBatch completes; so keep permit until then.
                     .whenComplete((r, f) -> {
                         semaphore.release();
-                        continueConsumptionOrFinish.accept(r, f);
+                        continueConsumptionOrFinish.accept(null, f);
                     });
             }
         } catch (Throwable t) {
@@ -161,20 +180,20 @@ public class BatchIteratorBackpressureExecutor<R> {
         }
     }
 
-    private void executeBatch() {
-        indexInBulk = 0;
+    private void execute(T item) {
         inFlightExecutions.incrementAndGet();
-        executeFunction.get().whenComplete(continueConsumptionOrFinish);
+        execute.apply(item).whenComplete(continueConsumptionOrFinish);
     }
 
     private void resumeConsumption() {
-        if (pauseConsumption.getAsBoolean()) {
+        T item = batchIterator.currentElement();
+        if (pauseConsumption.test(item)) {
             scheduler.schedule(this::resumeConsumption, throttleDelay.next().getMillis(), TimeUnit.MILLISECONDS);
             return;
         }
         // Suspend happened once a batch was ready, so execute it now.
         // consumeIterator would otherwise move past the indexInBulk == bulkSize check and end up building a huge batch
-        executeBatch();
+        execute(item);
         semaphore.release();
 
         // In case executeBatch takes some time to finish - also immediately resume consumption.

dex/src/main/java/io/crate/data/BatchIterators.java
@@ -24,6 +24,7 @@ package io.crate.data;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.function.BiConsumer;
+import java.util.function.Supplier;
 import java.util.stream.Collector;
 
 public class BatchIterators {
@@ -80,5 +81,57 @@ public class BatchIterators {
             });
         }
         return resultFuture;
+    }
+
+    /**
+     * Partition the items of a BatchIterator into blocks of {@code size}.
+     *
+     * Example:
+     * <pre>
+     *     inputBi: [1, 2, 3, 4, 5]
+     *
+     *     partition(inputBi, 2, ArrayList::new, List::add) -> [[1, 2], [3, 4], [5]]
+     * </pre>
+     * @param supplier Used to create the state per partition
+     * @param accumulator Used to add items to the partitions state
+     * @param <T> input item type
+     * @param <A> output item type
+     */
+    public static <T, A> BatchIterator<A> partition(BatchIterator<T> bi,
+                                                    int size,
+                                                    Supplier<A> supplier,
+                                                    BiConsumer<A, T> accumulator) {
+        return new MappedForwardingBatchIterator<T, A>() {
+
+            private A element = null;
+            private A state = supplier.get();
+            private int idx = 0;
+
+            @Override
+            protected BatchIterator<T> delegate() {
+                return bi;
+            }
+
+            @Override
+            public boolean moveNext() {
+                while (idx < size && bi.moveNext()) {
+                    accumulator.accept(state, bi.currentElement());
+                    idx++;
+                }
+                if (idx == size || (idx > 0 && bi.allLoaded())) {
+                    element = state;
+                    state = supplier.get();
+                    idx = 0;
+                    return true;
+                }
+                element = null;
+                return false;
+            }
+
+            @Override
+            public A currentElement() {
+                return element;
+            }
+        };
     }
 }

dex/src/main/java/io/crate/data/MappedForwardingBatchIterator.java
@@ -22,13 +22,43 @@
 
 package io.crate.data;
 
+import javax.annotation.Nonnull;
+import java.util.concurrent.CompletionStage;
+
 /**
  * Base class for BatchIterator implementations which mostly forward to another BatchIterator.
  */
-public abstract class ForwardingBatchIterator<T> extends MappedForwardingBatchIterator<T, T> {
+public abstract class MappedForwardingBatchIterator<I, O> implements BatchIterator<O> {
+
+    protected abstract BatchIterator<I> delegate();
 
     @Override
-    public T currentElement() {
-        return delegate().currentElement();
+    public void moveToStart() {
+        delegate().moveToStart();
+    }
+
+    @Override
+    public boolean moveNext() {
+        return delegate().moveNext();
+    }
+
+    @Override
+    public void close() {
+        delegate().close();
+    }
+
+    @Override
+    public CompletionStage<?> loadNextBatch() {
+        return delegate().loadNextBatch();
+    }
+
+    @Override
+    public boolean allLoaded() {
+        return delegate().allLoaded();
+    }
+
+    @Override
+    public void kill(@Nonnull Throwable throwable) {
+        delegate().kill(throwable);
     }
 }

dex/src/test/java/io/crate/data/BatchIteratorsTest.java
@@ -22,11 +22,18 @@
 
 package io.crate.data;
 
+import io.crate.testing.BatchSimulatingIterator;
 import io.crate.testing.FailingBatchIterator;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
@@ -38,5 +45,37 @@ public class BatchIteratorsTest {
         CompletableFuture<Long> future = BatchIterators.collect(
             FailingBatchIterator.failOnAllLoaded(), Collectors.counting());
         assertThat(future.isCompletedExceptionally(), is(true));
+    }
+
+
+    @Test
+    public void testBatchBySize() throws Exception {
+        BatchIterator<Integer> batchIterator = InMemoryBatchIterator.of(() -> IntStream.range(0, 5).iterator(), null);
+        BatchIterator<List<Integer>> batchedIt = BatchIterators.partition(batchIterator, 2, ArrayList::new, List::add);
+
+        assertThat(batchedIt.moveNext(), is(true));
+        assertThat(batchedIt.currentElement(), is(Arrays.asList(0, 1)));
+        assertThat(batchedIt.moveNext(), is(true));
+        assertThat(batchedIt.currentElement(), is(Arrays.asList(2, 3)));
+        assertThat(batchedIt.moveNext(), is(true));
+        assertThat(batchedIt.currentElement(), is(Collections.singletonList(4)));
+    }
+
+    @Test
+    public void testBatchBySizeWithBatchedSource() throws Exception {
+        BatchIterator<Integer> batchIterator = new BatchSimulatingIterator<>(
+            InMemoryBatchIterator.of(() -> IntStream.range(0, 5).iterator(), null),
+            3,
+            2,
+            null
+        );
+        BatchIterator<List<Integer>> batchedIt = BatchIterators.partition(batchIterator, 2, ArrayList::new, List::add);
+
+        CompletableFuture<List<List<Integer>>> future = BatchIterators.collect(batchedIt, Collectors.toList());
+        assertThat(future.get(10, TimeUnit.SECONDS), is(Arrays.asList(
+            Arrays.asList(0, 1),
+            Arrays.asList(2, 3),
+            Collections.singletonList(4)
+        )));
     }
 }

sql/src/main/java/io/crate/executor/transport/ShardResponse.java
@@ -196,4 +196,10 @@ public class ShardResponse extends ReplicationResponse implements WriteResponse 
             }
         }
     }
+
+    public int successRowCount() {
+        BitSet bitSet = new BitSet();
+        markResponseItemsAndFailures(this, bitSet);
+        return bitSet.cardinality();
+    }
 }

sql/src/main/java/io/crate/executor/transport/task/UpsertByIdTask.java
@@ -34,7 +34,7 @@ import io.crate.executor.transport.ShardResponse;
 import io.crate.executor.transport.ShardUpsertRequest;
 import io.crate.metadata.IndexParts;
 import io.crate.operation.projectors.RetryListener;
-import io.crate.operation.projectors.ShardingUpsertExecutor;
+import io.crate.operation.projectors.sharding.ShardingUpsertExecutor;
 import io.crate.planner.node.dml.UpsertById;
 import org.apache.logging.log4j.Logger;
 import org.elasticsearch.action.ActionListener;

sql/src/main/java/io/crate/executor/transport/task/elasticsearch/ESDeleteTask.java
@@ -31,7 +31,7 @@ import io.crate.executor.JobTask;
 import io.crate.executor.transport.ShardDeleteRequest;
 import io.crate.executor.transport.ShardResponse;
 import io.crate.executor.transport.TransportShardDeleteAction;
-import io.crate.operation.projectors.ShardingUpsertExecutor;
+import io.crate.operation.projectors.sharding.ShardingUpsertExecutor;
 import io.crate.planner.node.dml.ESDelete;
 import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.cluster.service.ClusterService;

sql/src/main/java/io/crate/metadata/settings/CrateSettings.java
@@ -28,7 +28,7 @@ import io.crate.breaker.CrateCircuitBreakerService;
 import io.crate.cluster.gracefulstop.DecommissioningService;
 import io.crate.metadata.ReferenceImplementation;
 import io.crate.operation.collect.stats.JobsLogService;
-import io.crate.operation.projectors.ShardingUpsertExecutor;
+import io.crate.operation.projectors.sharding.ShardingUpsertExecutor;
 import io.crate.operation.reference.NestedObjectExpression;
 import io.crate.planner.TableStatsService;
 import io.crate.settings.CrateSetting;

commit 9c08282f0db5c3de1ca92029bf34e6d1a5c78031

Fix race condition in BatchIteratorBackpressureExecutor leading to invalid row-counts
The `inFlightExecutions` counter needs to be decremented *after* the
result has been updated. Otherwise result updates could get lost (happen
after the result future has already been set)

Fixes a regression introduced with 12f4258f51bb274d18e9231f9e6c98253c7e9413
`testIndexWriter` was flaky due to this.

sql/src/main/java/io/crate/operation/projectors/BatchIteratorBackpressureExecutor.java
@@ -96,31 +96,24 @@ public class BatchIteratorBackpressureExecutor<T, R> {
     }
 
     private void continueConsumptionOrFinish(@Nullable R result, Throwable failure) {
+        if (result != null) {
+            resultRef.accumulateAndGet(result, combiner);
+        }
+        if (failure != null) {
+            failureRef.set(failure);
+        }
+
         int inFlight = inFlightExecutions.decrementAndGet();
         assert inFlight >= 0 : "Number of in-flight executions must not be negative";
 
         if (consumptionFinished) {
-            R finalResult = maybeUpdateResult(result);
             if (inFlight == 0) {
-                setResult(finalResult, failure == null ? failureRef.get() : failure);
+                setResult(resultRef.get(), failure == null ? failureRef.get() : failure);
             }
             // else: waiting for other async-operations to finish
         } else {
-            if (failure != null) {
-                failureRef.set(failure);
-            }
-            if (result != null) {
-                resultRef.accumulateAndGet(result, combiner);
-            }
             consumeIterator();
         }
-    }
-
-    private R maybeUpdateResult(@Nullable R result) {
-        if (result == null) {
-            return resultRef.get();
-        }
-        return resultRef.accumulateAndGet(result, combiner);
     }
 
     private void setResult(R finalResult, Throwable failure) {