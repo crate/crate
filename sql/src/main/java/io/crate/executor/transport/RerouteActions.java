crate/crate

commit ae1e3e040f9aa8ce18393c9aeeac47902d2f8ca3

Added REROUTE ALLOCATE REPLICA subcommand to ALTER TABLE
This commit introduces the reroute option ``REROUTE ALLOCATE REPLICA``
that allows to force the allocation of an unassigned replica shards on a
specific node.

sql/src/main/java/io/crate/executor/transport/RerouteActions.java
@@ -1,0 +1,127 @@
+/*
+ * Licensed to Crate under one or more contributor license agreements.
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.  Crate licenses this file
+ * to you under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.  You may
+ * obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.  See the License for the specific language governing
+ * permissions and limitations under the License.
+ *
+ * However, if you have executed another commercial license agreement
+ * with Crate these terms will supersede the license and you may use the
+ * software solely pursuant to the terms of the relevant commercial
+ * agreement.
+ */
+
+package io.crate.executor.transport;
+
+import io.crate.action.FutureActionListener;
+import io.crate.analyze.AlterTableAnalyzer;
+import io.crate.analyze.AnalyzedStatementVisitor;
+import io.crate.analyze.RerouteAllocateReplicaShardAnalyzedStatement;
+import io.crate.analyze.RerouteAnalyzedStatement;
+import io.crate.analyze.RerouteMoveShardAnalyzedStatement;
+import io.crate.analyze.expressions.ExpressionToNumberVisitor;
+import io.crate.analyze.expressions.ExpressionToStringVisitor;
+import io.crate.data.Row;
+import io.crate.metadata.PartitionName;
+import io.crate.metadata.doc.DocTableInfo;
+import io.crate.metadata.table.ShardedTable;
+import org.elasticsearch.action.ActionListener;
+import org.elasticsearch.action.admin.cluster.reroute.ClusterRerouteRequest;
+import org.elasticsearch.action.admin.cluster.reroute.ClusterRerouteResponse;
+import org.elasticsearch.cluster.routing.allocation.command.AllocateReplicaAllocationCommand;
+import org.elasticsearch.cluster.routing.allocation.command.MoveAllocationCommand;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.function.BiConsumer;
+
+import static io.crate.concurrent.CompletableFutures.failedFuture;
+
+public final class RerouteActions {
+
+    private RerouteActions() {
+
+    }
+
+    static final CompletableFuture<Long> execute(
+        BiConsumer<ClusterRerouteRequest, ActionListener<ClusterRerouteResponse>> rerouteAction,
+        RerouteAnalyzedStatement stmt,
+        Row parameters) {
+
+        ClusterRerouteRequest request;
+        try {
+            request = prepareRequest(stmt, parameters);
+        } catch (Throwable t) {
+            return failedFuture(t);
+        }
+        FutureActionListener<ClusterRerouteResponse, Long> listener =
+            new FutureActionListener<>(r -> r.isAcknowledged() ? 1L : -1L);
+        rerouteAction.accept(request, listener);
+        return listener;
+    }
+
+    static ClusterRerouteRequest prepareRequest(RerouteAnalyzedStatement stmt, Row parameters) {
+        return RequestBuilder.INSTANCE.process(stmt, parameters);
+    }
+
+    static String getRerouteIndex(RerouteAnalyzedStatement statement, Row parameters) throws IllegalArgumentException {
+        ShardedTable shardedTable = statement.tableInfo();
+        if (shardedTable instanceof DocTableInfo) {
+            DocTableInfo docTableInfo = (DocTableInfo) shardedTable;
+            String indexName = docTableInfo.ident().indexName();
+            PartitionName partitionName = AlterTableAnalyzer.createPartitionName(statement.partitionProperties(),
+                docTableInfo, parameters);
+            if (partitionName != null) {
+                indexName = partitionName.asIndexName();
+            } else if (docTableInfo.isPartitioned()) {
+                throw new IllegalArgumentException("table is partitioned however no partition clause has been specified");
+            }
+
+            return indexName;
+        }
+
+        // Table is a blob table
+        assert shardedTable.concreteIndices().length == 1 : "table has to contain only 1 index name";
+        return shardedTable.concreteIndices()[0];
+    }
+
+    private static class RequestBuilder extends AnalyzedStatementVisitor<Row, ClusterRerouteRequest> {
+
+        private static final RequestBuilder INSTANCE = new RequestBuilder();
+
+        @Override
+        protected ClusterRerouteRequest visitRerouteMoveShard(RerouteMoveShardAnalyzedStatement statement,
+                                                              Row parameters) {
+            String indexName = getRerouteIndex(statement, parameters);
+            int shardId = ExpressionToNumberVisitor.convert(statement.shardId(), parameters).intValue();
+            String fromNodeId = ExpressionToStringVisitor.convert(statement.fromNodeId(), parameters);
+            String toNodeId = ExpressionToStringVisitor.convert(statement.toNodeId(), parameters);
+
+            MoveAllocationCommand command = new MoveAllocationCommand(indexName, shardId, fromNodeId, toNodeId);
+            ClusterRerouteRequest request = new ClusterRerouteRequest();
+            request.add(command);
+            return request;
+        }
+
+        @Override
+        protected ClusterRerouteRequest visitRerouteAllocateReplicaShard(RerouteAllocateReplicaShardAnalyzedStatement statement,
+                                                                         Row parameters) {
+            String indexName = getRerouteIndex(statement, parameters);
+            int shardId = ExpressionToNumberVisitor.convert(statement.shardId(), parameters).intValue();
+            String nodeId = ExpressionToStringVisitor.convert(statement.nodeId(), parameters);
+
+            AllocateReplicaAllocationCommand command = new AllocateReplicaAllocationCommand(indexName, shardId, nodeId);
+            ClusterRerouteRequest request = new ClusterRerouteRequest();
+            request.add(command);
+            return request;
+        }
+    }
+}

sql-parser/src/main/antlr/SqlBase.g4
@@ -476,6 +476,7 @@ addGeneratedColumnDefinition
 
 rerouteOption
     : MOVE SHARD shardId=parameterOrInteger FROM fromNodeId=parameterOrString TO toNodeId=parameterOrString #rerouteMoveShard
+    | ALLOCATE REPLICA SHARD shardId=parameterOrInteger ON nodeId=parameterOrString                         #rerouteAllocateReplicaShard
     ;
 
 dataType
@@ -602,7 +603,7 @@ nonReserved
     | REPOSITORY | SNAPSHOT | RESTORE | GENERATED | ALWAYS | BEGIN
     | ISOLATION | TRANSACTION | CHARACTERISTICS | LEVEL | LANGUAGE | OPEN | CLOSE | RENAME
     | PRIVILEGES | SCHEMA | INGEST | RULE
-    | REROUTE | MOVE | SHARD
+    | REROUTE | MOVE | SHARD | ALLOCATE | REPLICA
     ;
 
 SELECT: 'SELECT';
@@ -702,6 +703,8 @@ RENAME: 'RENAME';
 REROUTE: 'REROUTE';
 MOVE: 'MOVE';
 SHARD: 'SHARD';
+ALLOCATE: 'ALLOCATE';
+REPLICA: 'REPLICA';
 
 BOOLEAN: 'BOOLEAN';
 BYTE: 'BYTE';

sql-parser/src/main/java/io/crate/sql/parser/AstBuilder.java
@@ -128,6 +128,7 @@ import io.crate.sql.tree.QueryBody;
 import io.crate.sql.tree.QuerySpecification;
 import io.crate.sql.tree.RefreshStatement;
 import io.crate.sql.tree.Relation;
+import io.crate.sql.tree.RerouteAllocateReplicaShard;
 import io.crate.sql.tree.RerouteMoveShard;
 import io.crate.sql.tree.RerouteOption;
 import io.crate.sql.tree.ResetStatement;
@@ -650,6 +651,13 @@ class AstBuilder extends SqlBaseBaseVisitor<Node> {
             (Expression) visit(context.shardId),
             (Expression) visit(context.fromNodeId),
             (Expression) visit(context.toNodeId));
+    }
+
+    @Override
+    public Node visitRerouteAllocateReplicaShard(SqlBaseParser.RerouteAllocateReplicaShardContext context) {
+        return new RerouteAllocateReplicaShard(
+            (Expression) visit(context.shardId),
+            (Expression) visit(context.nodeId));
     }
 
     // Properties

sql-parser/src/main/java/io/crate/sql/tree/AstVisitor.java
@@ -481,6 +481,10 @@ public abstract class AstVisitor<R, C> {
         return visitNode(node, context);
     }
 
+    public R visitRerouteAllocateReplicaShard(RerouteAllocateReplicaShard node, C context) {
+        return visitNode(node, context);
+    }
+
     public R visitAddColumnDefinition(AddColumnDefinition node, C context) {
         return visitTableElement(node, context);
     }

sql-parser/src/main/java/io/crate/sql/tree/RerouteAllocateReplicaShard.java
@@ -1,0 +1,74 @@
+/*
+ * Licensed to CRATE Technology GmbH ("Crate") under one or more contributor
+ * license agreements.  See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.  Crate licenses
+ * this file to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.  You may
+ * obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * However, if you have executed another commercial license agreement
+ * with Crate these terms will supersede the license and you may use the
+ * software solely pursuant to the terms of the relevant commercial agreement.
+ */
+
+package io.crate.sql.tree;
+
+import com.google.common.base.MoreObjects;
+import com.google.common.base.Objects;
+
+public class RerouteAllocateReplicaShard extends RerouteOption {
+
+    private final Expression nodeId;
+    private final Expression shardId;
+
+    public RerouteAllocateReplicaShard(Expression shardId, Expression nodeId) {
+        this.shardId = shardId;
+        this.nodeId = nodeId;
+    }
+
+    public Expression nodeId() {
+        return nodeId;
+    }
+
+    public Expression shardId() {
+        return shardId;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(shardId, nodeId);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+
+        RerouteAllocateReplicaShard that = (RerouteAllocateReplicaShard) obj;
+
+        if (!shardId.equals(that.shardId)) return false;
+        if (!nodeId.equals(that.nodeId)) return false;
+
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this)
+            .add("shardId", shardId)
+            .add("nodeId", nodeId).toString();
+    }
+
+    @Override
+    public <R, C> R accept(AstVisitor<R, C> visitor, C context) {
+        return visitor.visitRerouteAllocateReplicaShard(this, context);
+    }
+}

sql-parser/src/test/java/io/crate/sql/parser/TestStatementBuilder.java
@@ -1121,6 +1121,7 @@ public class TestStatementBuilder {
     public void testAlterTableReroute() throws Exception {
         printStatement("alter table t reroute move shard 1 from 'node1' to 'node2'");
         printStatement("alter table t partition (parted_col = ?) reroute move shard ? from ? to ?");
+        printStatement("alter table t reroute allocate replica shard 1 on 'node1'");
     }
 
     @Test

sql/src/main/java/io/crate/action/sql/DDLStatementDispatcher.java
@@ -43,16 +43,19 @@ import io.crate.analyze.DropUserAnalyzedStatement;
 import io.crate.analyze.OptimizeSettings;
 import io.crate.analyze.OptimizeTableAnalyzedStatement;
 import io.crate.analyze.RefreshTableAnalyzedStatement;
+import io.crate.analyze.RerouteAllocateReplicaShardAnalyzedStatement;
 import io.crate.analyze.RerouteMoveShardAnalyzedStatement;
 import io.crate.analyze.RestoreSnapshotAnalyzedStatement;
 import io.crate.blob.v2.BlobAdminClient;
 import io.crate.data.Row;
 import io.crate.executor.transport.AlterTableOperation;
 import io.crate.executor.transport.RepositoryService;
+import io.crate.executor.transport.RerouteActions;
 import io.crate.executor.transport.SnapshotRestoreDDLDispatcher;
 import io.crate.executor.transport.TableCreator;
 import io.crate.operation.udf.UserDefinedFunctionDDLClient;
 import io.crate.operation.user.UserManager;
+import org.elasticsearch.action.admin.cluster.reroute.TransportClusterRerouteAction;
 import org.elasticsearch.action.admin.indices.forcemerge.ForceMergeRequest;
 import org.elasticsearch.action.admin.indices.forcemerge.ForceMergeResponse;
 import org.elasticsearch.action.admin.indices.forcemerge.TransportForceMergeAction;
@@ -92,6 +95,7 @@ public class DDLStatementDispatcher implements BiFunction<AnalyzedStatement, Row
     private final UserManager userManager;
 
     private final InnerVisitor innerVisitor = new InnerVisitor();
+    private final TransportClusterRerouteAction rerouteAction;
 
 
     @Inject
@@ -101,6 +105,7 @@ public class DDLStatementDispatcher implements BiFunction<AnalyzedStatement, Row
                                   RepositoryService repositoryService,
                                   SnapshotRestoreDDLDispatcher snapshotRestoreDDLDispatcher,
                                   UserDefinedFunctionDDLClient udfDDLClient,
+                                  TransportClusterRerouteAction rerouteAction,
                                   Provider<UserManager> userManagerProvider,
                                   Provider<TransportUpgradeAction> transportUpgradeActionProvider,
                                   Provider<TransportForceMergeAction> transportForceMergeActionProvider,
@@ -115,6 +120,7 @@ public class DDLStatementDispatcher implements BiFunction<AnalyzedStatement, Row
         this.transportForceMergeActionProvider = transportForceMergeActionProvider;
         this.transportRefreshActionProvider = transportRefreshActionProvider;
         this.userManager = userManagerProvider.get();
+        this.rerouteAction = rerouteAction;
     }
 
     @Override
@@ -248,7 +254,12 @@ public class DDLStatementDispatcher implements BiFunction<AnalyzedStatement, Row
 
         @Override
         protected CompletableFuture<Long> visitRerouteMoveShard(RerouteMoveShardAnalyzedStatement analysis, Row parameters) {
-            return alterTableOperation.executeRerouteMoveShard(analysis, parameters);
+            return RerouteActions.execute(rerouteAction::execute, analysis, parameters);
+        }
+
+        @Override
+        protected CompletableFuture<Long> visitRerouteAllocateReplicaShard(RerouteAllocateReplicaShardAnalyzedStatement analysis, Row parameters) {
+            return RerouteActions.execute(rerouteAction::execute, analysis, parameters);
         }
     }
 

sql/src/main/java/io/crate/analyze/AlterTableRerouteAnalyzer.java
@@ -29,6 +29,7 @@ import io.crate.metadata.table.ShardedTable;
 import io.crate.sql.tree.AlterTableReroute;
 import io.crate.sql.tree.Assignment;
 import io.crate.sql.tree.AstVisitor;
+import io.crate.sql.tree.RerouteAllocateReplicaShard;
 import io.crate.sql.tree.RerouteMoveShard;
 
 import java.util.List;
@@ -67,6 +68,12 @@ public class AlterTableRerouteAnalyzer {
         public RerouteAnalyzedStatement visitRerouteMoveShard(RerouteMoveShard node, Context context) {
             return new RerouteMoveShardAnalyzedStatement(
                 context.tableInfo, context.partitionProperties, node.shardId(),node.fromNodeId(), node.toNodeId());
+        }
+
+        @Override
+        public RerouteAnalyzedStatement visitRerouteAllocateReplicaShard(RerouteAllocateReplicaShard node, Context context) {
+            return new RerouteAllocateReplicaShardAnalyzedStatement(
+                context.tableInfo, context.partitionProperties, node.shardId(), node.nodeId());
         }
     }
 }

commit 286350a17a74ac7d934f05a0c3a2d1ffef3273f9

fix compile error introduced with ae1e3e040f9aa8ce18393c9aeeac47902d2f8ca3

sql/src/main/java/io/crate/executor/transport/RerouteActions.java
@@ -51,7 +51,7 @@ public final class RerouteActions {
 
     }
 
-    static final CompletableFuture<Long> execute(
+    public static CompletableFuture<Long> execute(
         BiConsumer<ClusterRerouteRequest, ActionListener<ClusterRerouteResponse>> rerouteAction,
         RerouteAnalyzedStatement stmt,
         Row parameters) {