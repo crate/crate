crate/crate

commit 927b22cac331095ee0bea79feb5c8ed074a8df27

Remove ReplaceMode; Merge FunctionCopyVisitor & ReplacingSymbolVisitor

sql/src/main/java/io/crate/planner/node/dql/RoutedCollectPhase.java
@@ -269,15 +269,13 @@ public class RoutedCollectPhase extends AbstractProjectionsPhase implements Coll
     public RoutedCollectPhase normalize(EvaluatingNormalizer normalizer, TransactionContext transactionContext) {
         assert whereClause() != null : "whereClause must not be null";
         RoutedCollectPhase result = this;
-        List<Symbol> newToCollect = normalizer.normalize(toCollect(), transactionContext);
+        Function<Symbol, Symbol> normalize = s -> normalizer.normalize(s, transactionContext);
+        List<Symbol> newToCollect = Lists2.copyAndReplace(toCollect, normalize);
         boolean changed = newToCollect != toCollect();
         WhereClause newWhereClause = whereClause().normalize(normalizer, transactionContext);
         OrderBy orderBy = this.orderBy;
         if (orderBy != null) {
-            List<Symbol> orderBySymbols = orderBy.orderBySymbols();
-            List<Symbol> normalizedOrderBy = normalizer.normalize(orderBySymbols, transactionContext);
-            changed = changed || orderBySymbols != normalizedOrderBy;
-            orderBy = new OrderBy(normalizedOrderBy, this.orderBy.reverseFlags(), this.orderBy.nullsFirst());
+            orderBy = orderBy.copyAndReplace(normalize);
         }
         changed = changed || newWhereClause != whereClause();
         if (changed) {

sql/src/main/java/io/crate/analyze/CopyAnalyzer.java
@@ -44,7 +44,6 @@ import io.crate.metadata.Functions;
 import io.crate.metadata.GeneratedReference;
 import io.crate.metadata.PartitionName;
 import io.crate.metadata.Reference;
-import io.crate.metadata.ReplaceMode;
 import io.crate.metadata.RowGranularity;
 import io.crate.metadata.Schemas;
 import io.crate.metadata.TableIdent;
@@ -114,7 +113,6 @@ class CopyAnalyzer {
         EvaluatingNormalizer normalizer = new EvaluatingNormalizer(
             functions,
             RowGranularity.CLUSTER,
-            ReplaceMode.COPY,
             null,
             tableRelation);
         ExpressionAnalyzer expressionAnalyzer = createExpressionAnalyzer(analysis, tableRelation);
@@ -178,7 +176,6 @@ class CopyAnalyzer {
         EvaluatingNormalizer normalizer = new EvaluatingNormalizer(
             functions,
             RowGranularity.CLUSTER,
-            ReplaceMode.COPY,
             null,
             tableRelation);
         ExpressionAnalyzer expressionAnalyzer = createExpressionAnalyzer(analysis, tableRelation);

sql/src/main/java/io/crate/analyze/DeleteAnalyzer.java
@@ -34,7 +34,6 @@ import io.crate.analyze.symbol.Symbols;
 import io.crate.analyze.where.WhereClauseAnalyzer;
 import io.crate.exceptions.UnsupportedFeatureException;
 import io.crate.metadata.Functions;
-import io.crate.metadata.ReplaceMode;
 import io.crate.metadata.RowGranularity;
 import io.crate.metadata.TransactionContext;
 import io.crate.metadata.doc.DocSysColumns;
@@ -76,7 +75,6 @@ class DeleteAnalyzer {
         EvaluatingNormalizer normalizer = new EvaluatingNormalizer(
             functions,
             RowGranularity.CLUSTER,
-            ReplaceMode.COPY,
             null,
             docTableRelation);
         DeleteAnalyzedStatement deleteAnalyzedStatement = new DeleteAnalyzedStatement(docTableRelation);

sql/src/main/java/io/crate/analyze/EvaluatingNormalizer.java
@@ -23,18 +23,16 @@ package io.crate.analyze;
 import io.crate.analyze.relations.FieldResolver;
 import io.crate.analyze.symbol.Field;
 import io.crate.analyze.symbol.Function;
+import io.crate.analyze.symbol.FunctionCopyVisitor;
 import io.crate.analyze.symbol.Literal;
 import io.crate.analyze.symbol.MatchPredicate;
 import io.crate.analyze.symbol.Symbol;
-import io.crate.analyze.symbol.SymbolVisitor;
 import io.crate.analyze.symbol.Symbols;
 import io.crate.analyze.symbol.format.SymbolFormatter;
 import io.crate.data.Input;
-import io.crate.metadata.FunctionIdent;
 import io.crate.metadata.FunctionImplementation;
 import io.crate.metadata.Functions;
 import io.crate.metadata.Reference;
-import io.crate.metadata.ReplaceMode;
 import io.crate.metadata.RowGranularity;
 import io.crate.metadata.TransactionContext;
 import io.crate.operation.reference.ReferenceResolver;
@@ -51,20 +49,13 @@ import java.util.Map;
 
 
 /**
- * the normalizer does symbol normalization and reference resolving if possible
- * <p>
- * E.g.:
- * The query
- * </p>
- * <p>
- * and(true, eq(column_ref, 'someliteral'))
- * </p>
- * <p>
- * will be changed to
- * </p>
- * <p>
- * eq(column_ref, 'someliteral')
- * </p>
+ * The normalizer does several things:
+ *
+ *  - Convert functions into a simpler form by using {@link FunctionImplementation#normalizeSymbol(Function, TransactionContext)}
+ *  - Convert {@link Field} to {@link Reference} if {@link FieldResolver} is available.
+ *  - Convert {@link MatchPredicate} to a {@link Function} if {@link FieldResolver} is available
+ *  - Convert {@link Reference} into a Literal value if {@link ReferenceResolver} is available
+ *    and {@link io.crate.metadata.ReferenceImplementation}s can be retrieved for the Reference.
  */
 public class EvaluatingNormalizer {
 
@@ -76,34 +67,28 @@ public class EvaluatingNormalizer {
     private final BaseVisitor visitor;
 
     public static EvaluatingNormalizer functionOnlyNormalizer(Functions functions) {
-        return new EvaluatingNormalizer(functions, RowGranularity.CLUSTER, ReplaceMode.COPY, null, null);
+        return new EvaluatingNormalizer(functions, RowGranularity.CLUSTER, null, null);
     }
 
     /**
      * @param functions         function resolver
      * @param granularity       the maximum row granularity the normalizer should try to normalize
-     * @param replaceMode       defines if symbols like functions can be mutated or if they have to be copied
      * @param referenceResolver reference resolver which is used to resolve paths
      * @param fieldResolver     optional field resolver to resolve fields
      */
     public EvaluatingNormalizer(Functions functions,
                                 RowGranularity granularity,
-                                ReplaceMode replaceMode,
                                 @Nullable ReferenceResolver<? extends Input<?>> referenceResolver,
                                 @Nullable FieldResolver fieldResolver) {
         this.functions = functions;
         this.granularity = granularity;
         this.referenceResolver = referenceResolver;
         this.fieldResolver = fieldResolver;
-        if (replaceMode == ReplaceMode.MUTATE) {
-            this.visitor = new InPlaceVisitor();
-        } else {
-            this.visitor = new CopyingVisitor();
-        }
+        this.visitor = new BaseVisitor();
     }
 
 
-    private abstract class BaseVisitor extends SymbolVisitor<TransactionContext, Symbol> {
+    private class BaseVisitor extends FunctionCopyVisitor<TransactionContext> {
         @Override
         public Symbol visitField(Field field, TransactionContext context) {
             if (fieldResolver != null) {
@@ -145,14 +130,6 @@ public class EvaluatingNormalizer {
             return matchPredicate;
         }
 
-
-        @SuppressWarnings("unchecked")
-        Symbol normalizeFunctionSymbol(Function function, TransactionContext context) {
-            FunctionIdent ident = function.info().ident();
-            FunctionImplementation impl = functions.getQualified(ident);
-            return impl.normalizeSymbol(function, context);
-        }
-
         @Override
         public Symbol visitReference(Reference symbol, TransactionContext context) {
             if (referenceResolver == null || symbol.granularity().ordinal() > granularity.ordinal()) {
@@ -171,51 +148,11 @@ public class EvaluatingNormalizer {
         }
 
         @Override
-        protected Symbol visitSymbol(Symbol symbol, TransactionContext context) {
-            return symbol;
-        }
-    }
-
-    private class CopyingVisitor extends BaseVisitor {
-        @Override
         public Symbol visitFunction(Function function, TransactionContext context) {
-            List<Symbol> newArgs = normalize(function.arguments(), context);
-            if (newArgs != function.arguments()) {
-                function = new Function(function.info(), newArgs);
-            }
-            return normalizeFunctionSymbol(function, context);
+            function = processAndMaybeCopy(function, context);
+            FunctionImplementation implementation = functions.getQualified(function.info().ident());
+            return implementation.normalizeSymbol(function, context);
         }
-    }
-
-    private class InPlaceVisitor extends BaseVisitor {
-        @Override
-        public Symbol visitFunction(Function function, TransactionContext context) {
-            normalizeInplace(function.arguments(), context);
-            return normalizeFunctionSymbol(function, context);
-        }
-    }
-
-    /**
-     * Normalizes all symbols of a List. Does not return a new list if no changes occur.
-     *
-     * @param symbols the list to be normalized
-     * @return a list with normalized symbols
-     */
-    public List<Symbol> normalize(List<Symbol> symbols, TransactionContext context) {
-        if (symbols.size() > 0) {
-            boolean changed = false;
-            Symbol[] newArgs = new Symbol[symbols.size()];
-            int i = 0;
-            for (Symbol symbol : symbols) {
-                Symbol newArg = normalize(symbol, context);
-                changed = changed || newArg != symbol;
-                newArgs[i++] = newArg;
-            }
-            if (changed) {
-                return Arrays.asList(newArgs);
-            }
-        }
-        return symbols;
     }
 
     /**

sql/src/main/java/io/crate/analyze/GeneratedColumnComparisonReplacer.java
@@ -26,10 +26,25 @@ import com.google.common.collect.HashMultimap;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Multimap;
-import io.crate.analyze.symbol.*;
-import io.crate.metadata.*;
+import io.crate.analyze.symbol.Function;
+import io.crate.analyze.symbol.FunctionCopyVisitor;
+import io.crate.analyze.symbol.RefReplacer;
+import io.crate.analyze.symbol.Symbol;
+import io.crate.analyze.symbol.SymbolType;
+import io.crate.analyze.symbol.SymbolVisitors;
+import io.crate.analyze.symbol.Symbols;
+import io.crate.metadata.FunctionIdent;
+import io.crate.metadata.FunctionInfo;
+import io.crate.metadata.GeneratedReference;
+import io.crate.metadata.Reference;
 import io.crate.metadata.doc.DocTableInfo;
-import io.crate.operation.operator.*;
+import io.crate.operation.operator.AndOperator;
+import io.crate.operation.operator.EqOperator;
+import io.crate.operation.operator.GtOperator;
+import io.crate.operation.operator.GteOperator;
+import io.crate.operation.operator.LtOperator;
+import io.crate.operation.operator.LteOperator;
+import io.crate.operation.operator.Operators;
 import io.crate.operation.scalar.DateTruncFunction;
 import io.crate.operation.scalar.arithmetic.CeilFunction;
 import io.crate.operation.scalar.arithmetic.FloorFunction;
@@ -38,7 +53,12 @@ import io.crate.types.DataTypes;
 import org.elasticsearch.common.inject.Singleton;
 
 import javax.annotation.Nullable;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 @Singleton
 public class GeneratedColumnComparisonReplacer {
@@ -62,7 +82,7 @@ public class GeneratedColumnComparisonReplacer {
         return COMPARISON_REPLACE_VISITOR.addComparisons(symbol, tableInfo);
     }
 
-    private static class ComparisonReplaceVisitor extends ReplacingSymbolVisitor<ComparisonReplaceVisitor.Context> {
+    private static class ComparisonReplaceVisitor extends FunctionCopyVisitor<ComparisonReplaceVisitor.Context> {
 
         static class Context {
             private final Multimap<Reference, GeneratedReference> referencedRefsToGeneratedColumn;
@@ -73,7 +93,7 @@ public class GeneratedColumnComparisonReplacer {
         }
 
         ComparisonReplaceVisitor() {
-            super(ReplaceMode.COPY);
+            super();
         }
 
         Symbol addComparisons(Symbol symbol, DocTableInfo tableInfo) {

sql/src/main/java/io/crate/analyze/HavingClause.java
@@ -22,7 +22,6 @@
 package io.crate.analyze;
 
 import io.crate.analyze.symbol.Symbol;
-import io.crate.metadata.TransactionContext;
 import io.crate.operation.operator.AndOperator;
 
 import javax.annotation.Nullable;
@@ -31,17 +30,6 @@ public class HavingClause extends QueryClause {
 
     public HavingClause(@Nullable Symbol query) {
         super(query);
-    }
-
-    public HavingClause normalize(EvaluatingNormalizer normalizer, TransactionContext transactionContext) {
-        if (noMatch || query == null) {
-            return this;
-        }
-        Symbol normalizedQuery = normalizer.normalize(query, transactionContext);
-        if (normalizedQuery == query) {
-            return this;
-        }
-        return new HavingClause(normalizedQuery);
     }
 
     public HavingClause add(Symbol otherQuery) {

sql/src/main/java/io/crate/analyze/InsertFromSubQueryAnalyzer.java
@@ -28,7 +28,11 @@ import io.crate.action.sql.SessionContext;
 import io.crate.analyze.expressions.ExpressionAnalysisContext;
 import io.crate.analyze.expressions.ExpressionAnalyzer;
 import io.crate.analyze.expressions.ValueNormalizer;
-import io.crate.analyze.relations.*;
+import io.crate.analyze.relations.DocTableRelation;
+import io.crate.analyze.relations.FieldProvider;
+import io.crate.analyze.relations.NameFieldProvider;
+import io.crate.analyze.relations.QueriedRelation;
+import io.crate.analyze.relations.RelationAnalyzer;
 import io.crate.analyze.symbol.DynamicReference;
 import io.crate.analyze.symbol.Field;
 import io.crate.analyze.symbol.InputColumn;
@@ -36,13 +40,25 @@ import io.crate.analyze.symbol.Symbol;
 import io.crate.analyze.symbol.format.SymbolFormatter;
 import io.crate.analyze.symbol.format.SymbolPrinter;
 import io.crate.exceptions.ColumnUnknownException;
-import io.crate.metadata.*;
+import io.crate.metadata.ColumnIdent;
+import io.crate.metadata.Functions;
+import io.crate.metadata.Reference;
+import io.crate.metadata.RowGranularity;
+import io.crate.metadata.Schemas;
+import io.crate.metadata.TableIdent;
+import io.crate.metadata.TransactionContext;
 import io.crate.metadata.doc.DocTableInfo;
 import io.crate.metadata.table.Operation;
 import io.crate.sql.tree.Assignment;
 import io.crate.sql.tree.InsertFromSubquery;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 
 class InsertFromSubQueryAnalyzer {
 
@@ -197,7 +213,6 @@ class InsertFromSubQueryAnalyzer {
         EvaluatingNormalizer normalizer = new EvaluatingNormalizer(
             functions,
             RowGranularity.CLUSTER,
-            ReplaceMode.COPY,
             null,
             tableRelation);
         ValueNormalizer valuesNormalizer = new ValueNormalizer();

sql/src/main/java/io/crate/analyze/InsertFromValuesAnalyzer.java
@@ -43,7 +43,6 @@ import io.crate.metadata.Functions;
 import io.crate.metadata.GeneratedReference;
 import io.crate.metadata.Reference;
 import io.crate.metadata.ReferenceToLiteralConverter;
-import io.crate.metadata.ReplaceMode;
 import io.crate.metadata.RowGranularity;
 import io.crate.metadata.Schemas;
 import io.crate.metadata.TableIdent;
@@ -146,7 +145,6 @@ class InsertFromValuesAnalyzer extends AbstractInsertAnalyzer {
         EvaluatingNormalizer normalizer = new EvaluatingNormalizer(
             functions,
             RowGranularity.CLUSTER,
-            ReplaceMode.COPY,
             null,
             tableRelation);
         analyzeColumns(statement.tableInfo(), statement.columns());

commit f942fcd09b196ced17f082d90df328da763becd8

Fix a perf regression for select.. order by limit queries
A change in 927b22cac331095ee0bea79feb5c8ed074a8df27 caused a regression
because the `nodePageSizeHint` wasn't preserved when normalizing a
`RoutedCollectPhase`. This caused the collect operation to collect way
more documents than necessary.

The problematic change is
https://github.com/crate/crate/commit/927b22cac331095ee0bea79feb5c8ed074a8df27#diff-117849d9b4075be1753c40c23995c810R273
which causes the `boolean changed = newToCollect != toCollect();` check
to always result in `changed = true`

sql/src/main/java/io/crate/planner/node/dql/RoutedCollectPhase.java
@@ -286,6 +286,7 @@ public class RoutedCollectPhase extends AbstractProjectionsPhase implements Coll
                 distributionInfo,
                 user
             );
+            result.nodePageSizeHint(nodePageSizeHint);
             result.orderBy(orderBy);
         }
         return result;

sql/src/test/java/io/crate/planner/node/RoutedCollectPhaseTest.java
@@ -112,4 +112,27 @@ public class RoutedCollectPhaseTest extends CrateUnitTest {
 
         assertThat(normalizedCollect.orderBy(), notNullValue());
     }
+
+    @Test
+    public void testNormalizePreservesNodePageSizeHint() throws Exception {
+        Symbol toInt10 = CastFunctionResolver.generateCastFunction(Literal.of(10L), DataTypes.INTEGER, false);
+        RoutedCollectPhase collect = new RoutedCollectPhase(
+            UUID.randomUUID(),
+            1,
+            "collect",
+            new Routing(Collections.emptyMap()),
+            RowGranularity.DOC,
+            Collections.singletonList(toInt10),
+            Collections.emptyList(),
+            WhereClause.MATCH_ALL,
+            DistributionInfo.DEFAULT_SAME_NODE,
+            null
+        );
+        collect.nodePageSizeHint(10);
+        EvaluatingNormalizer normalizer = EvaluatingNormalizer.functionOnlyNormalizer(getFunctions());
+        RoutedCollectPhase normalizedCollect = collect.normalize(
+            normalizer, new TransactionContext(SessionContext.create()));
+
+        assertThat(normalizedCollect.nodePageSizeHint(), is(10));
+    }
 }