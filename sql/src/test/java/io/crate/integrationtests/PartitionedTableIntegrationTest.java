crate/crate

commit c750e6bcfff1c75d0da31f2745933ecc5a7cf974

re-analyze where clause after sub-query excution
Fixes an error if a subquery is used in comparison with a routing/clustered-by column.

This fix requires to adjust how an explain plan output is generated because
a logical plan containing a sub-query symbol cannot be built into an execution plan without
executing the sub-queries. Introducing a logical plan printer which kicks in
if building an execution plan (and so using the execution plan printer) fails.

sql/src/main/java/io/crate/analyze/where/WhereClauseAnalyzer.java
@@ -27,12 +27,15 @@ import io.crate.analyze.EvaluatingNormalizer;
 import io.crate.analyze.GeneratedColumnExpander;
 import io.crate.analyze.SymbolToTrueVisitor;
 import io.crate.analyze.WhereClause;
+import io.crate.analyze.relations.AbstractTableRelation;
 import io.crate.analyze.relations.DocTableRelation;
 import io.crate.analyze.symbol.Literal;
+import io.crate.analyze.symbol.SelectSymbol;
 import io.crate.analyze.symbol.Symbol;
 import io.crate.analyze.symbol.Symbols;
 import io.crate.analyze.symbol.ValueSymbolVisitor;
 import io.crate.collections.Lists2;
+import io.crate.data.Row;
 import io.crate.execution.expression.reference.partitioned.PartitionExpression;
 import io.crate.metadata.ColumnIdent;
 import io.crate.metadata.Functions;
@@ -44,6 +47,7 @@ import io.crate.metadata.TransactionContext;
 import io.crate.metadata.doc.DocSysColumns;
 import io.crate.metadata.doc.DocTableInfo;
 import io.crate.planner.WhereClauseOptimizer;
+import io.crate.planner.operators.SubQueryAndParamBinder;
 import org.elasticsearch.common.collect.Tuple;
 
 import javax.annotation.Nullable;
@@ -56,6 +60,27 @@ import java.util.Map;
 import java.util.Set;
 
 public class WhereClauseAnalyzer {
+
+    /**
+     * Replace parameters and sub-queries with the related values and analyze the query afterwards.
+     */
+    public static WhereClause bindAndAnalyze(WhereClause where,
+                                             Row params,
+                                             Map<SelectSymbol, Object> subQueryValues,
+                                             AbstractTableRelation tableRelation,
+                                             Functions functions,
+                                             TransactionContext transactionContext) {
+        if (where.hasQuery()) {
+            Symbol query = SubQueryAndParamBinder.convert(where.query(), params, subQueryValues);
+            if (tableRelation instanceof DocTableRelation) {
+                WhereClauseAnalyzer whereClauseAnalyzer = new WhereClauseAnalyzer(functions, (DocTableRelation) tableRelation);
+                return whereClauseAnalyzer.analyze(query, transactionContext);
+            } else {
+                return new WhereClause(query);
+            }
+        }
+        return where;
+    }
 
     private final Functions functions;
     private final DocTableInfo table;

sql/src/main/java/io/crate/analyze/symbol/SelectSymbol.java
@@ -93,7 +93,7 @@ public class SelectSymbol extends Symbol {
 
     @Override
     public String representation() {
-        return "SubQuery{" + relation.getQualifiedName() + '}';
+        return "SubQuery{" + relation.getQualifiedName() + "}@" + hashCode();
     }
 
     public ResultType getResultType() {

sql/src/main/java/io/crate/planner/ExplainLeaf.java
@@ -22,7 +22,7 @@
 
 package io.crate.planner;
 
-import java.util.List;
+import java.util.Collection;
 import java.util.stream.Collectors;
 
 /**
@@ -36,7 +36,7 @@ public interface ExplainLeaf {
     String representation();
 
 
-    static String printList(List<? extends ExplainLeaf> leaves) {
+    static String printList(Collection<? extends ExplainLeaf> leaves) {
         return leaves.stream()
             .map(ExplainLeaf::representation)
             .collect(Collectors.joining(", "));

sql/src/main/java/io/crate/planner/PlanPrinter.java
@@ -23,27 +23,24 @@ package io.crate.planner;
 
 import com.google.common.collect.ImmutableMap;
 import io.crate.analyze.WhereClause;
-import io.crate.analyze.symbol.Symbol;
-import io.crate.analyze.symbol.format.SymbolPrinter;
-import io.crate.planner.distribution.DistributionInfo;
-import io.crate.execution.dsl.phases.UpstreamPhase;
-import io.crate.execution.dsl.phases.ExecutionPhase;
-import io.crate.execution.dsl.phases.ExecutionPhaseVisitor;
 import io.crate.execution.dsl.phases.AbstractProjectionsPhase;
-import io.crate.planner.node.dql.Collect;
 import io.crate.execution.dsl.phases.CollectPhase;
 import io.crate.execution.dsl.phases.CountPhase;
-import io.crate.execution.dsl.phases.MergePhase;
-import io.crate.planner.node.dql.QueryThenFetch;
-import io.crate.execution.dsl.phases.RoutedCollectPhase;
-import io.crate.planner.node.dql.join.NestedLoop;
-import io.crate.execution.dsl.phases.NestedLoopPhase;
+import io.crate.execution.dsl.phases.ExecutionPhase;
+import io.crate.execution.dsl.phases.ExecutionPhaseVisitor;
 import io.crate.execution.dsl.phases.FetchPhase;
+import io.crate.execution.dsl.phases.MergePhase;
+import io.crate.execution.dsl.phases.NestedLoopPhase;
+import io.crate.execution.dsl.phases.RoutedCollectPhase;
+import io.crate.execution.dsl.phases.UpstreamPhase;
 import io.crate.execution.dsl.projection.Projection;
+import io.crate.planner.distribution.DistributionInfo;
+import io.crate.planner.node.dql.Collect;
+import io.crate.planner.node.dql.QueryThenFetch;
+import io.crate.planner.node.dql.join.NestedLoop;
 
 import javax.annotation.Nullable;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
@@ -53,22 +50,25 @@ public class PlanPrinter {
     }
 
     public static Map<String, Object> objectMap(ExecutionPlan executionPlan) {
-        return ExecutionPlan2MapVisitor.toMap(executionPlan);
-    }
-
-    private static List<Object> refs(Collection<? extends Symbol> symbols) {
-        List<Object> refs = new ArrayList<>(symbols.size());
-        for (Symbol s : symbols) {
-            refs.add(SymbolPrinter.INSTANCE.print(s, SymbolPrinter.Style.FULL_QUALIFIED));
-        }
-        return refs;
+        return ExecutionPlan2MapVisitor.createMap(executionPlan);
     }
 
     private static class ExecutionPhase2MapVisitor extends ExecutionPhaseVisitor<Void, ImmutableMap.Builder<String, Object>> {
 
         public static final ExecutionPhase2MapVisitor INSTANCE = new ExecutionPhase2MapVisitor();
 
-        private ExecutionPhase2MapVisitor() {
+        private static ImmutableMap.Builder<String, Object> createMap(ExecutionPhase executionPhase,
+                                                                      ImmutableMap.Builder<String, Object> subMap) {
+            return ImmutableMap.<String, Object>builder()
+                .put(executionPhase.type().toString(), subMap.build());
+        }
+
+        private static ImmutableMap.Builder<String, Object> createSubMap(ExecutionPhase phase) {
+            return ImmutableMap.<String, Object>builder()
+                .put("type", "executionPhase")
+                .put("id", phase.phaseId())
+                // Converting TreeMap.keySet() to be able to stream
+                .put("executionNodes", new ArrayList<>(phase.nodeIds()));
         }
 
         static ImmutableMap.Builder<String, Object> toBuilder(ExecutionPhase executionPhase) {
@@ -86,22 +86,17 @@ public class PlanPrinter {
             return result;
         }
 
-        private static ImmutableMap.Builder<String, Object> newBuilder() {
-            return ImmutableMap.builder();
+
+        private ExecutionPhase2MapVisitor() {
         }
 
         @Override
         protected ImmutableMap.Builder<String, Object> visitExecutionPhase(ExecutionPhase phase, Void context) {
-            return newBuilder()
-                .put("phaseType", phase.type().toString())
-                .put("id", phase.phaseId())
-                // Converting TreeMap.keySet() to be able to stream
-                .put("executionNodes", new ArrayList<>(phase.nodeIds())
-                );
+            return createMap(phase, createSubMap(phase));
         }
 
         private ImmutableMap.Builder<String, Object> process(DistributionInfo info) {
-            return newBuilder()
+            return ImmutableMap.<String, Object>builder()
                 .put("distributedByColumn", info.distributeByColumn())
                 .put("type", info.distributionType().toString());
         }
@@ -119,46 +114,47 @@ public class PlanPrinter {
 
         @Override
         public ImmutableMap.Builder<String, Object> visitRoutedCollectPhase(RoutedCollectPhase phase, Void context) {
-            ImmutableMap.Builder<String, Object> builder = visitCollectPhase(phase, context);
+            ImmutableMap.Builder<String, Object> builder = upstreamPhase(phase, createSubMap(phase));
+            builder.put("toCollect", ExplainLeaf.printList(phase.toCollect()));
             builder = dqlPlanNode(phase, builder);
             builder.put("routing", phase.routing().locations());
             WhereClause whereClause = phase.whereClause();
             if (whereClause.hasQuery()) {
                 builder.put("where", whereClause.query().representation());
             }
-            return builder;
+            return createMap(phase, builder);
         }
 
         @Override
         public ImmutableMap.Builder<String, Object> visitCollectPhase(CollectPhase phase, Void context) {
-            ImmutableMap.Builder<String, Object> builder = upstreamPhase(phase, visitExecutionPhase(phase, context));
-            builder.put("toCollect", refs(phase.toCollect()));
-            return builder;
+            ImmutableMap.Builder<String, Object> builder = upstreamPhase(phase, createSubMap(phase));
+            builder.put("toCollect", ExplainLeaf.printList(phase.toCollect()));
+            return createMap(phase, builder);
         }
 
         @Override
         public ImmutableMap.Builder<String, Object> visitCountPhase(CountPhase phase, Void context) {
-            return upstreamPhase(phase, visitExecutionPhase(phase, context));
+            return createMap(phase, upstreamPhase(phase, createSubMap(phase)));
         }
 
         @Override
         public ImmutableMap.Builder<String, Object> visitFetchPhase(FetchPhase phase, Void context) {
-            return visitExecutionPhase(phase, context)
-                .put("fetchRefs", refs(phase.fetchRefs()));
+            return createMap(phase, createSubMap(phase)
+                .put("fetchRefs", ExplainLeaf.printList(phase.fetchRefs())));
         }
 
         @Override
         public ImmutableMap.Builder<String, Object> visitMergePhase(MergePhase phase, Void context) {
-            ImmutableMap.Builder<String, Object> b = upstreamPhase(phase, visitExecutionPhase(phase, context));
-            return dqlPlanNode(phase, b);
+            ImmutableMap.Builder<String, Object> b = upstreamPhase(phase, createSubMap(phase));
+            return createMap(phase, dqlPlanNode(phase, b));
         }
 
         @Override
         public ImmutableMap.Builder<String, Object> visitNestedLoopPhase(NestedLoopPhase phase, Void context) {
             ImmutableMap.Builder<String, Object> b = upstreamPhase(
                 phase,
-                visitExecutionPhase(phase, context).put("joinType", phase.joinType()));
-            return dqlPlanNode(phase, b);
+                createSubMap(phase).put("joinType", phase.joinType()));
+            return createMap(phase, dqlPlanNode(phase, b));
         }
     }
 
@@ -166,14 +162,19 @@ public class PlanPrinter {
 
         private static final ExecutionPlan2MapVisitor INSTANCE = new ExecutionPlan2MapVisitor();
 
-        private static ImmutableMap.Builder<String, Object> newBuilder() {
-            return ImmutableMap.builder();
+        private static ImmutableMap.Builder<String, Object> createMap(ExecutionPlan executionPlan,
+                                                                      ImmutableMap.Builder<String, Object> subMap) {
+            return ImmutableMap.<String, Object>builder()
+                .put(executionPlan.getClass().getSimpleName(), subMap.build());
+        }
+
+        private static ImmutableMap.Builder<String, Object> createSubMap() {
+            return ImmutableMap.<String, Object>builder().put("type", "executionPlan");
         }
 
         @Override
         protected ImmutableMap.Builder<String, Object> visitPlan(ExecutionPlan executionPlan, Void context) {
-            return newBuilder()
-                .put("planType", executionPlan.getClass().getSimpleName());
+            return createMap(executionPlan, createSubMap());
         }
 
         private static Map<String, Object> phaseMap(@Nullable ExecutionPhase node) {
@@ -184,47 +185,45 @@ public class PlanPrinter {
             }
         }
 
-        static Map<String, Object> toMap(ExecutionPlan executionPlan) {
+        static Map<String, Object> createMap(ExecutionPlan executionPlan) {
             assert executionPlan != null : "plan must not be null";
             return INSTANCE.process(executionPlan, null).build();
         }
 
         @Override
         public ImmutableMap.Builder<String, Object> visitCollect(Collect plan, Void context) {
-            ImmutableMap.Builder<String, Object> b = visitPlan(plan, context)
-                .put("collectPhase", phaseMap(plan.collectPhase()));
-            return b;
+            return createMap(plan, createSubMap()
+                .put("collectPhase", phaseMap(plan.collectPhase())));
         }
 
         @Override
         public ImmutableMap.Builder<String, Object> visitNestedLoop(NestedLoop plan, Void context) {
-            return newBuilder()
-                .put("planType", plan.getClass().getSimpleName())
+            return createMap(plan, createSubMap()
                 .put("left", process(plan.left(), context).build())
                 .put("right", process(plan.right(), context).build())
-                .put("nestedLoopPhase", phaseMap(plan.nestedLoopPhase()));
+                .put("nestedLoopPhase", phaseMap(plan.nestedLoopPhase())));
         }
 
         @Override
         public ImmutableMap.Builder<String, Object> visitQueryThenFetch(QueryThenFetch plan, Void context) {
-            return visitPlan(plan, context)
-                .put("subPlan", toMap(plan.subPlan()))
-                .put("fetchPhase", phaseMap(plan.fetchPhase()));
+            return createMap(plan, createSubMap()
+                .put("subPlan", createMap(plan.subPlan()))
+                .put("fetchPhase", phaseMap(plan.fetchPhase())));
         }
 
         @Override
         public ImmutableMap.Builder<String, Object> visitMerge(Merge merge, Void context) {
-            return visitPlan(merge, context)
-                .put("subPlan", toMap(merge.subPlan()))
-                .put("mergePhase", phaseMap(merge.mergePhase()));
+            return createMap(merge, createSubMap()
+                .put("subPlan", createMap(merge.subPlan()))
+                .put("mergePhase", phaseMap(merge.mergePhase())));
         }
 
         @Override
         public ImmutableMap.Builder<String, Object> visitUnionPlan(UnionExecutionPlan unionExecutionPlan, Void context) {
-            return visitPlan(unionExecutionPlan, context)
-                .put("left", toMap(unionExecutionPlan.left()))
-                .put("right", toMap(unionExecutionPlan.right()))
-                .put("mergePhase", phaseMap(unionExecutionPlan.mergePhase()));
+            return createMap(unionExecutionPlan, createSubMap()
+                .put("left", createMap(unionExecutionPlan.left()))
+                .put("right", createMap(unionExecutionPlan.right()))
+                .put("mergePhase", phaseMap(unionExecutionPlan.mergePhase())));
         }
     }
 }

sql/src/main/java/io/crate/planner/node/management/ExplainPlan.java
@@ -27,12 +27,12 @@ import io.crate.data.InMemoryBatchIterator;
 import io.crate.data.Row;
 import io.crate.data.Row1;
 import io.crate.data.RowConsumer;
-import io.crate.execution.engine.pipeline.TopN;
 import io.crate.planner.DependencyCarrier;
 import io.crate.planner.ExecutionPlan;
 import io.crate.planner.Plan;
 import io.crate.planner.PlanPrinter;
 import io.crate.planner.PlannerContext;
+import io.crate.planner.operators.ExplainLogicalPlan;
 import io.crate.planner.operators.LogicalPlan;
 import io.crate.planner.statement.CopyStatementPlanner;
 
@@ -61,16 +61,7 @@ public class ExplainPlan implements Plan {
         Map<String, Object> map;
         try {
             if (subPlan instanceof LogicalPlan) {
-                ExecutionPlan executionPlan = ((LogicalPlan) subPlan).build(
-                    plannerContext,
-                    executor.projectionBuilder(),
-                    TopN.NO_LIMIT,
-                    0,
-                    null,
-                    null,
-                    params,
-                    valuesBySubQuery);
-                map = PlanPrinter.objectMap(executionPlan);
+                map = ExplainLogicalPlan.explainMap((LogicalPlan) subPlan, plannerContext, executor.projectionBuilder());
             } else if (subPlan instanceof CopyStatementPlanner.CopyFrom) {
                 ExecutionPlan executionPlan = CopyStatementPlanner.planCopyFromExecution(
                     executor.clusterService().state().nodes(),

sql/src/main/java/io/crate/planner/operators/Collect.java
@@ -39,6 +39,7 @@ import io.crate.analyze.symbol.SymbolVisitor;
 import io.crate.analyze.symbol.SymbolVisitors;
 import io.crate.analyze.symbol.Symbols;
 import io.crate.analyze.where.DocKeys;
+import io.crate.analyze.where.WhereClauseAnalyzer;
 import io.crate.data.Row;
 import io.crate.exceptions.UnsupportedFeatureException;
 import io.crate.exceptions.VersionInvalidException;
@@ -88,7 +89,7 @@ class Collect extends ZeroInputPlan {
 
     private static final String COLLECT_PHASE_NAME = "collect";
     final QueriedTableRelation relation;
-    final WhereClause where;
+    WhereClause where;
 
     final TableInfo tableInfo;
     private final long numExpectedRows;
@@ -225,6 +226,19 @@ class Collect extends ZeroInputPlan {
                 where
             );
         }
+
+        // bind all parameters and possible subQuery values and re-analyze the query
+        // (could result in a NO_MATCH, routing could've changed, etc).
+        // the <p>where</p> instance variable must be overwritten as the plan creation of outer operators relies on it
+        // (e.g. GroupHashAggregate will build different plans based on the collect routing)
+        where = WhereClauseAnalyzer.bindAndAnalyze(
+            where,
+            params,
+            subQueryValues,
+            relation.tableRelation(),
+            plannerContext.functions(),
+            plannerContext.transactionContext());
+
         return new RoutedCollectPhase(
             plannerContext.jobId(),
             plannerContext.nextExecutionPhaseId(),
@@ -271,6 +285,11 @@ class Collect extends ZeroInputPlan {
                ", [" + ExplainLeaf.printList(outputs) +
                "], " + where +
                '}';
+    }
+
+    @Override
+    public <C, R> R accept(LogicalPlanVisitor<C, R> visitor, C context) {
+        return visitor.visitCollect(this, context);
     }
 
     private static final  class NoPredicateVisitor extends SymbolVisitor<Void, Void> {

sql/src/main/java/io/crate/planner/operators/Count.java
@@ -27,18 +27,18 @@ import io.crate.analyze.WhereClause;
 import io.crate.analyze.relations.AbstractTableRelation;
 import io.crate.analyze.symbol.Function;
 import io.crate.analyze.symbol.SelectSymbol;
+import io.crate.analyze.where.WhereClauseAnalyzer;
 import io.crate.data.Row;
-import io.crate.metadata.Routing;
-import io.crate.metadata.RoutingProvider;
-import io.crate.metadata.table.TableInfo;
-import io.crate.planner.ExecutionPlan;
-import io.crate.planner.PlannerContext;
-import io.crate.planner.distribution.DistributionInfo;
 import io.crate.execution.dsl.phases.CountPhase;
-import io.crate.planner.node.dql.CountPlan;
 import io.crate.execution.dsl.phases.MergePhase;
 import io.crate.execution.dsl.projection.MergeCountProjection;
 import io.crate.execution.dsl.projection.builder.ProjectionBuilder;
+import io.crate.metadata.Routing;
+import io.crate.metadata.RoutingProvider;
+import io.crate.planner.ExecutionPlan;
+import io.crate.planner.PlannerContext;
+import io.crate.planner.distribution.DistributionInfo;
+import io.crate.planner.node.dql.CountPlan;
 import io.crate.types.DataTypes;
 
 import javax.annotation.Nullable;
@@ -52,10 +52,10 @@ public class Count extends ZeroInputPlan {
 
     private static final String COUNT_PHASE_NAME = "count-merge";
 
-    final AbstractTableRelation<TableInfo> tableRelation;
+    final AbstractTableRelation tableRelation;
     final WhereClause where;
 
-    Count(Function countFunction, AbstractTableRelation<TableInfo> tableRelation, WhereClause where) {
+    Count(Function countFunction, AbstractTableRelation tableRelation, WhereClause where) {
         super(Collections.singletonList(countFunction), Collections.singletonList(tableRelation));
         this.tableRelation = tableRelation;
         this.where = where;
@@ -70,16 +70,25 @@ public class Count extends ZeroInputPlan {
                                @Nullable Integer pageSizeHint,
                                Row params,
                                Map<SelectSymbol, Object> subQueryValues) {
+        // bind all parameters and possible subQuery values and re-analyze the query
+        // (could result in a NO_MATCH, routing could've changed, etc).
+        WhereClause boundWhere = WhereClauseAnalyzer.bindAndAnalyze(
+            where,
+            params,
+            subQueryValues,
+            tableRelation,
+            plannerContext.functions(),
+            plannerContext.transactionContext());
 
         Routing routing = plannerContext.allocateRouting(
             tableRelation.tableInfo(),
-            where,
+            boundWhere,
             RoutingProvider.ShardSelection.ANY,
             plannerContext.transactionContext().sessionContext());
         CountPhase countPhase = new CountPhase(
             plannerContext.nextExecutionPhaseId(),
             routing,
-            where,
+            boundWhere,
             DistributionInfo.DEFAULT_BROADCAST
         );
         MergePhase mergePhase = new MergePhase(
@@ -100,5 +109,10 @@ public class Count extends ZeroInputPlan {
     @Override
     public long numExpectedRows() {
         return 1L;
+    }
+
+    @Override
+    public <C, R> R accept(LogicalPlanVisitor<C, R> visitor, C context) {
+        return visitor.visitCount(this, context);
     }
 }

commit 4f9c2a1fe876ee8a415232e782cdc202e4235b6d

Fixed: Wrong results when WHERE clause contains parted and non-parted columns
Regression caused by: https://github.com/crate/crate/commit/c750e6bcfff1c75d0da31f2745933ecc5a7cf974
Fixes: https://github.com/crate/crate/issues/6990

When the ``WHERE`` clause is analyzed for the 1st time the part with the partitioned columns
is removed from the WhereClause.query and becomes a partition list in. When the ``WHERE`` claused
is re-analyzed this partition list is lost and therefore the final query hits all table partitions.
E.g.:

```
  WHERE part_col1 IN (1, 2) AND other_col = 'value'
  => WhereClause.query: other_col='value', WhereClause.partitions: (1, 2) (1st analysis)
  => WhereClause.query: other_col='value', WhereClause.parititons: () (2nd analysis)
```

sql/src/main/java/io/crate/analyze/where/WhereClauseAnalyzer.java
@@ -73,7 +73,7 @@ public class WhereClauseAnalyzer {
             Symbol query = SubQueryAndParamBinder.convert(where.query(), params, subQueryValues);
             if (tableRelation instanceof DocTableRelation) {
                 WhereClauseAnalyzer whereClauseAnalyzer = new WhereClauseAnalyzer(functions, (DocTableRelation) tableRelation);
-                return whereClauseAnalyzer.analyze(query, transactionContext);
+                return whereClauseAnalyzer.analyze(query, transactionContext, where.partitions());
             } else {
                 return new WhereClause(query);
             }
@@ -94,6 +94,10 @@ public class WhereClauseAnalyzer {
     }
 
     public WhereClause analyze(Symbol query, TransactionContext transactionContext) {
+        return analyze(query, transactionContext, Collections.emptyList());
+    }
+
+    private WhereClause analyze(Symbol query, TransactionContext transactionContext, List<String> partitions) {
         if (query.equals(Literal.BOOLEAN_TRUE)) {
             return WhereClause.MATCH_ALL;
         }
@@ -139,14 +143,15 @@ public class WhereClauseAnalyzer {
             clusteredBy = getClusteredByLiterals(query, eqExtractor, transactionContext);
         }
 
-        List<String> partitions = null;
-        if (table.isPartitioned() && docKeys == null) {
-            if (table.partitions().isEmpty()) {
-                return WhereClause.NO_MATCH;
+        if (partitions.isEmpty()) {
+            if (table.isPartitioned() && docKeys == null) {
+                if (table.partitions().isEmpty()) {
+                    return WhereClause.NO_MATCH;
+                }
+                PartitionResult partitionResult = resolvePartitions(query, table, functions, transactionContext);
+                partitions = partitionResult.partitions;
+                query = partitionResult.query;
             }
-            PartitionResult partitionResult = resolvePartitions(query, table, functions, transactionContext);
-            partitions = partitionResult.partitions;
-            query = partitionResult.query;
         }
         return new WhereClause(query, docKeys, partitions, clusteredBy);
     }

sql/src/test/java/io/crate/integrationtests/PartitionedTableIntegrationTest.java
@@ -2142,4 +2142,17 @@ public class PartitionedTableIntegrationTest extends SQLTransportIntegrationTest
             is("1| 1\n")
         );
     }
+
+    @Test
+    public void testWhereOnPartitionedAndOtherColumn() {
+        execute("create table t (p string primary key, v string) " +
+                "partitioned by (p) " +
+                "with (number_of_replicas = 0)");
+        execute("insert into t (p, v) values ('a', 'Marvin')");
+        execute("insert into t (p, v) values ('b', 'Marvin')");
+        execute("refresh table t");
+
+        execute("select * from t where p='a' and v='Marvin'");
+        assertThat(TestingHelpers.printedTable(response.rows()), is("a| Marvin\n"));
+    }
 }