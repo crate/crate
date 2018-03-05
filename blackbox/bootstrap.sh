ee64b2c7f9464dd6447003b83efd8958f07bb523
Yakov Zaytsev <me@ysz.io>
2018-03-05 15:50:19 +0300

    fix gradle :blackbox:bootstrap on macOS

--- blackbox/bootstrap.sh
+++ blackbox/bootstrap.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
 if hash python3.6 2> /dev/null; then
-    python3.6 -m venv .venv
+    PYTHON=python3.6 
 elif hash python3 2> /dev/null; then
     # fallback to python3 in case there is no python3.6 alias; should be 3.6
-    python3 -m venv .venv
+    PYTHON=python3
 else
     echo 'python3.6 required'
     exit 1
+fi
+
+$PYTHON -m venv .venv
+if [ ! -f .venv/bin/pip ]; then
+    wget https://bootstrap.pypa.io/get-pip.py
+    ./.venv/bin/python get-pip.py
+    rm -f get-pip.py
 fi
 
 .venv/bin/pip install -U pip setuptools wheel
