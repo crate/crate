577a4c3f4f15cfaf894c7bf31f174aec5d39a0d2
Mathias Fussenegger <f.mathias@zignar.net>
2018-03-12 16:58:21 +0100

    Tests: Fix statement logging on timeout
    (cherry picked from commit 8d8cd3b55b7e911115e7a9aa871a262eee40533f)

--- sql/src/test/java/io/crate/testing/SQLTransportExecutor.java
+++ sql/src/test/java/io/crate/testing/SQLTransportExecutor.java
@@ -169,7 +169,7 @@ public class SQLTransportExecutor {
             }
             return execute(stmt, args).actionGet(timeout);
         } catch (ElasticsearchTimeoutException e) {
-            LOGGER.error("Timeout on SQL statement: {}", e, stmt);
+            LOGGER.error("Timeout on SQL statement: {} {}", stmt, e);
             throw e;
         }
     }
