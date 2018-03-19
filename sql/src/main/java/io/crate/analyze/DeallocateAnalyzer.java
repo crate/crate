9e4d7f0d3121d8a34b6c330eabd36f1f0ca73a24
Marios Trivyzas <matriv@gmail.com>
2018-03-19 15:27:36 +0100

    Implement Analysis for DEALLOCATE statement

---
+++ sql/src/main/java/io/crate/analyze/DeallocateAnalyzer.java
@@ -0,0 +1,50 @@
+/*
+ * Licensed to Crate under one or more contributor license agreements.
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.  Crate licenses this file
+ * to you under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.  You may
+ * obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.  See the License for the specific language governing
+ * permissions and limitations under the License.
+ *
+ * However, if you have executed another commercial license agreement
+ * with Crate these terms will supersede the license and you may use the
+ * software solely pursuant to the terms of the relevant commercial
+ * agreement.
+ */
+
+package io.crate.analyze;
+
+import io.crate.sql.tree.DeallocateStatement;
+import io.crate.sql.tree.Expression;
+import io.crate.sql.tree.QualifiedNameReference;
+import io.crate.sql.tree.StringLiteral;
+
+public final class DeallocateAnalyzer {
+
+    private DeallocateAnalyzer() {
+    }
+
+    public static DeallocateAnalyzedStatement analyze(DeallocateStatement deallocateStatement) {
+        Expression preparedStmtExpression = deallocateStatement.preparedStmt();
+        String preparedStmt = null;
+        if (preparedStmtExpression != null) {
+            if (preparedStmtExpression instanceof StringLiteral) {
+                preparedStmt = ((StringLiteral) preparedStmtExpression).getValue();
+            } else if (preparedStmtExpression instanceof QualifiedNameReference) {
+                preparedStmt = ((QualifiedNameReference) preparedStmtExpression).getName().toString();
+            } else {
+                throw new AssertionError("Expression " + preparedStmtExpression.toString() + " not supported as " +
+                                         "preparedStmt expression for DEALLOCATE");
+            }
+        }
+        return new DeallocateAnalyzedStatement(preparedStmt);
+    }
+}
---
+++ sql/src/test/java/io/crate/analyze/DeallocateAnalyzerTest.java
@@ -0,0 +1,74 @@
+/*
+ * Licensed to Crate under one or more contributor license agreements.
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.  Crate licenses this file
+ * to you under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.  You may
+ * obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.  See the License for the specific language governing
+ * permissions and limitations under the License.
+ *
+ * However, if you have executed another commercial license agreement
+ * with Crate these terms will supersede the license and you may use the
+ * software solely pursuant to the terms of the relevant commercial
+ * agreement.
+ */
+
+package io.crate.analyze;
+
+import io.crate.sql.tree.DeallocateStatement;
+import io.crate.sql.tree.ParameterExpression;
+import io.crate.test.integration.CrateDummyClusterServiceUnitTest;
+import io.crate.testing.SQLExecutor;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.nullValue;
+import static org.hamcrest.core.Is.is;
+
+public class DeallocateAnalyzerTest extends CrateDummyClusterServiceUnitTest {
+
+    private SQLExecutor e;
+
+    @Before
+    public void prepare() {
+        e = SQLExecutor.builder(clusterService).build();
+    }
+
+    @Test
+    public void testAnalyzeDeallocateAll() {
+        DeallocateAnalyzedStatement stmt = e.analyze("deallocate all");
+        assertThat(stmt.preparedStmtName(), nullValue());
+    }
+
+    @Test
+    public void testAnalyzeDeallocateWithLiteral() {
+        DeallocateAnalyzedStatement stmt = e.analyze("deallocate 'test_prep_stmt'");
+        assertThat(stmt.preparedStmtName(), is("test_prep_stmt"));
+    }
+
+    @Test
+    public void testAnalyzeDeallocateWithUnquotedLiteral() {
+        DeallocateAnalyzedStatement stmt = e.analyze("deallocate test_prep_stmt");
+        assertThat(stmt.preparedStmtName(), is("test_prep_stmt"));
+    }
+
+    @Test
+    public void testAnalyzeDeallocateWithLiteralLikeQualifiedName() {
+        DeallocateAnalyzedStatement stmt = e.analyze("deallocate test.prep.stmt");
+        assertThat(stmt.preparedStmtName(), is("test.prep.stmt"));
+    }
+
+    @Test
+    public void testAnalyzeDeallocateWithWrongExpression() {
+        expectedException.expect(AssertionError.class);
+        expectedException.expectMessage("Expression $1 not supported as preparedStmt expression for DEALLOCATE");
+        DeallocateAnalyzer.analyze(new DeallocateStatement(new ParameterExpression(1)));
+    }
+}
