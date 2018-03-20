9da1a78944850a905b8bec80826a43329058c6e3
Mathias Fussenegger <f.mathias@zignar.net>
2018-03-20 14:09:24 +0100

    Add analyzer support for DROP VIEW

---
+++ sql/src/main/java/io/crate/analyze/DropViewStmt.java
@@ -0,0 +1,56 @@
+/*
+ * Licensed to Crate under one or more contributor license agreements.
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.  Crate licenses this file
+ * to you under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.  You may
+ * obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.  See the License for the specific language governing
+ * permissions and limitations under the License.
+ *
+ * However, if you have executed another commercial license agreement
+ * with Crate these terms will supersede the license and you may use the
+ * software solely pursuant to the terms of the relevant commercial
+ * agreement.
+ */
+
+package io.crate.analyze;
+
+import io.crate.metadata.TableIdent;
+
+import java.util.List;
+
+public final class DropViewStmt implements AnalyzedStatement {
+
+    private final List<TableIdent> views;
+    private final boolean ifExists;
+
+    DropViewStmt(List<TableIdent> views, boolean ifExists) {
+        this.views = views;
+        this.ifExists = ifExists;
+    }
+
+    public List<TableIdent> views() {
+        return views;
+    }
+
+    public boolean ifExists() {
+        return ifExists;
+    }
+
+    @Override
+    public <C, R> R accept(AnalyzedStatementVisitor<C, R> visitor, C context) {
+        return visitor.visitDropView(this, context);
+    }
+
+    @Override
+    public boolean isWriteOperation() {
+        return true;
+    }
+}
