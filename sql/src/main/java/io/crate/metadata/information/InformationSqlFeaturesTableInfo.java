47c2e6df010fd314535d5dac4bee81ff6885d9e2
Christian Haudum <christian.haudum@gmail.com>
2018-03-29 11:51:33 +0200

    use ColumnRegistrar in InformationSqlFeaturesTableInfo

--- sql/src/main/java/io/crate/metadata/information/InformationSqlFeaturesTableInfo.java
+++ sql/src/main/java/io/crate/metadata/information/InformationSqlFeaturesTableInfo.java
@@ -24,16 +24,13 @@ package io.crate.metadata.information;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.ImmutableSortedMap;
 import io.crate.metadata.ColumnIdent;
-import io.crate.metadata.Reference;
-import io.crate.metadata.ReferenceIdent;
 import io.crate.metadata.RelationName;
 import io.crate.metadata.RowContextCollectorExpression;
 import io.crate.metadata.RowGranularity;
 import io.crate.metadata.expressions.RowCollectExpressionFactory;
 import io.crate.execution.engine.collect.files.SqlFeatureContext;
-import io.crate.types.DataType;
+import io.crate.metadata.table.ColumnRegistrar;
 import io.crate.types.DataTypes;
 
 import java.util.Map;
@@ -45,68 +42,52 @@ public class InformationSqlFeaturesTableInfo extends InformationTableInfo {
     public static final RelationName IDENT = new RelationName(InformationSchemaInfo.NAME, NAME);
 
     public static class Columns {
-        public static final ColumnIdent FEATURE_ID = new ColumnIdent("feature_id");
-        public static final ColumnIdent FEATURE_NAME = new ColumnIdent("feature_name");
-        public static final ColumnIdent SUB_FEATURE_ID = new ColumnIdent("sub_feature_id");
-        public static final ColumnIdent SUB_FEATURE_NAME = new ColumnIdent("sub_feature_name");
-        public static final ColumnIdent IS_SUPPORTED = new ColumnIdent("is_supported");
-        public static final ColumnIdent IS_VERIFIED_BY = new ColumnIdent("is_verified_by");
-        public static final ColumnIdent COMMENTS = new ColumnIdent("comments");
+        static final ColumnIdent FEATURE_ID = new ColumnIdent("feature_id");
+        static final ColumnIdent FEATURE_NAME = new ColumnIdent("feature_name");
+        static final ColumnIdent SUB_FEATURE_ID = new ColumnIdent("sub_feature_id");
+        static final ColumnIdent SUB_FEATURE_NAME = new ColumnIdent("sub_feature_name");
+        static final ColumnIdent IS_SUPPORTED = new ColumnIdent("is_supported");
+        static final ColumnIdent IS_VERIFIED_BY = new ColumnIdent("is_verified_by");
+        static final ColumnIdent COMMENTS = new ColumnIdent("comments");
     }
 
-    public static class References {
-        public static final Reference FEATURE_ID = createRef(Columns.FEATURE_ID, DataTypes.STRING);
-        public static final Reference FEATURE_NAME = createRef(Columns.FEATURE_NAME, DataTypes.STRING);
-        public static final Reference SUB_FEATURE_ID = createRef(Columns.SUB_FEATURE_ID, DataTypes.STRING);
-        public static final Reference SUB_FEATURE_NAME = createRef(Columns.SUB_FEATURE_NAME, DataTypes.STRING);
-        public static final Reference IS_SUPPORTED = createRef(Columns.IS_SUPPORTED, DataTypes.BOOLEAN);
-        public static final Reference IS_VERIFIED_BY = createRef(Columns.IS_VERIFIED_BY, DataTypes.STRING);
-        public static final Reference COMMENTS = createRef(Columns.COMMENTS, DataTypes.STRING);
+    private static ColumnRegistrar columnRegistrar() {
+        return new ColumnRegistrar(IDENT, RowGranularity.DOC)
+            .register(Columns.FEATURE_ID, DataTypes.STRING)
+            .register(Columns.FEATURE_NAME, DataTypes.STRING)
+            .register(Columns.SUB_FEATURE_ID, DataTypes.STRING)
+            .register(Columns.SUB_FEATURE_NAME, DataTypes.STRING)
+            .register(Columns.IS_SUPPORTED, DataTypes.BOOLEAN)
+            .register(Columns.IS_VERIFIED_BY, DataTypes.STRING)
+            .register(Columns.COMMENTS, DataTypes.STRING);
     }
 
     public static Map<ColumnIdent, RowCollectExpressionFactory<SqlFeatureContext>> expressions() {
         return ImmutableMap.<ColumnIdent, RowCollectExpressionFactory<SqlFeatureContext>>builder()
-            .put(InformationSqlFeaturesTableInfo.Columns.FEATURE_ID,
+            .put(Columns.FEATURE_ID,
                 () -> RowContextCollectorExpression.objToBytesRef(SqlFeatureContext::getFeatureId))
-            .put(InformationSqlFeaturesTableInfo.Columns.FEATURE_NAME,
+            .put(Columns.FEATURE_NAME,
                 () -> RowContextCollectorExpression.objToBytesRef(SqlFeatureContext::getFeatureName))
-            .put(InformationSqlFeaturesTableInfo.Columns.SUB_FEATURE_ID,
+            .put(Columns.SUB_FEATURE_ID,
                 () -> RowContextCollectorExpression.objToBytesRef(SqlFeatureContext::getSubFeatureId))
-            .put(InformationSqlFeaturesTableInfo.Columns.SUB_FEATURE_NAME,
+            .put(Columns.SUB_FEATURE_NAME,
                 () -> RowContextCollectorExpression.objToBytesRef(SqlFeatureContext::getSubFeatureName))
-            .put(InformationSqlFeaturesTableInfo.Columns.IS_SUPPORTED,
+            .put(Columns.IS_SUPPORTED,
                 () -> RowContextCollectorExpression.forFunction(SqlFeatureContext::isSupported))
-            .put(InformationSqlFeaturesTableInfo.Columns.IS_VERIFIED_BY,
+            .put(Columns.IS_VERIFIED_BY,
                 () -> RowContextCollectorExpression.objToBytesRef(SqlFeatureContext::getIsVerifiedBy))
-            .put(InformationSqlFeaturesTableInfo.Columns.COMMENTS,
+            .put(Columns.COMMENTS,
                 () -> RowContextCollectorExpression.objToBytesRef(SqlFeatureContext::getComments))
             .build();
     }
 
-    private static Reference createRef(ColumnIdent columnIdent, DataType dataType) {
-        return new Reference(new ReferenceIdent(IDENT, columnIdent), RowGranularity.DOC, dataType);
-    }
-
-    protected InformationSqlFeaturesTableInfo() {
+    InformationSqlFeaturesTableInfo() {
         super(
             IDENT,
-            ImmutableList.of(Columns.FEATURE_ID,
-                Columns.FEATURE_NAME,
-                Columns.SUB_FEATURE_ID,
-                Columns.SUB_FEATURE_NAME,
-                Columns.IS_SUPPORTED,
-                Columns.IS_VERIFIED_BY,
-                Columns.COMMENTS),
-            ImmutableSortedMap.<ColumnIdent, Reference>naturalOrder()
-                .put(Columns.FEATURE_ID, References.FEATURE_ID)
-                .put(Columns.FEATURE_NAME, References.FEATURE_NAME)
-                .put(Columns.SUB_FEATURE_ID, References.SUB_FEATURE_ID)
-                .put(Columns.SUB_FEATURE_NAME, References.SUB_FEATURE_NAME)
-                .put(Columns.IS_SUPPORTED, References.IS_SUPPORTED)
-                .put(Columns.IS_VERIFIED_BY, References.IS_VERIFIED_BY)
-                .put(Columns.COMMENTS, References.COMMENTS)
-                .build()
+            columnRegistrar(),
+            ImmutableList.of(Columns.FEATURE_ID, Columns.FEATURE_NAME,
+                Columns.SUB_FEATURE_ID, Columns.SUB_FEATURE_NAME,
+                Columns.IS_SUPPORTED, Columns.IS_VERIFIED_BY)
         );
     }
-
 }
