crate/crate

commit b0f602c800d66ea501f422d7b246bd0438d5cc95

Rewrite outer-joins to inner-join if possible

sql/src/main/java/io/crate/analyze/WhereClause.java
@@ -30,6 +30,7 @@ import io.crate.analyze.symbol.Symbols;
 import io.crate.analyze.symbol.ValueSymbolVisitor;
 import io.crate.analyze.where.DocKeys;
 import io.crate.metadata.StmtCtx;
+import io.crate.operation.operator.AndOperator;
 import org.elasticsearch.common.Nullable;
 import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.io.stream.StreamOutput;
@@ -200,5 +201,24 @@ public class WhereClause extends QueryClause implements Streamable {
 
     public boolean hasVersions() {
         return docKeys.isPresent() && docKeys.get().withVersions();
+    }
+
+
+    /**
+     * Adds another query to this WhereClause.
+     *
+     * The result is either a new WhereClause or the same (but modified) instance.
+     */
+    public WhereClause add(Symbol otherQuery) {
+        assert !docKeys.isPresent() : "Cannot add otherQuery if there are docKeys in the WhereClause";
+        if (this == MATCH_ALL) {
+            return new WhereClause(otherQuery);
+        }
+        if (this == NO_MATCH) {
+            // NO_MATCH & anything is still NO_MATCH
+            return NO_MATCH;
+        }
+        this.query = AndOperator.of(this.query, otherQuery);
+        return this;
     }
 }

sql/src/main/java/io/crate/analyze/MultiSourceSelect.java
@@ -22,6 +22,7 @@
 package io.crate.analyze;
 
 import com.google.common.base.Optional;
+import com.google.common.collect.ImmutableList;
 import io.crate.analyze.relations.*;
 import io.crate.analyze.symbol.Field;
 import io.crate.analyze.symbol.Symbol;
@@ -38,12 +39,15 @@ public class MultiSourceSelect implements QueriedRelation {
     private final QuerySpec querySpec;
     private final Fields fields;
     private final List<JoinPair> joinPairs;
+    private final List<Symbol> outputSymbols;
     private QualifiedName qualifiedName;
 
     public MultiSourceSelect(Map<QualifiedName, AnalyzedRelation> sources,
+                             List<Symbol> outputSymbols,
                              Collection<? extends Path> outputNames,
                              QuerySpec querySpec,
                              List<JoinPair> joinPairs) {
+        this.outputSymbols = ImmutableList.copyOf(outputSymbols);
         assert sources.size() > 1 : "MultiSourceSelect requires at least 2 relations";
         this.splitter = new RelationSplitter(querySpec, sources.values(), joinPairs);
         this.sources = initializeSources(sources);
@@ -124,6 +128,10 @@ public class MultiSourceSelect implements QueriedRelation {
 
     public Optional<RemainingOrderBy> remainingOrderBy() {
         return splitter.remainingOrderBy();
+    }
+
+    public List<Symbol> outputSymbols() {
+        return outputSymbols;
     }
 
     public static class Source {

sql/src/main/java/io/crate/analyze/Rewriter.java
@@ -1,0 +1,248 @@
+/*
+ * Licensed to Crate under one or more contributor license agreements.
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.  Crate licenses this file
+ * to you under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.  You may
+ * obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.  See the License for the specific language governing
+ * permissions and limitations under the License.
+ *
+ * However, if you have executed another commercial license agreement
+ * with Crate these terms will supersede the license and you may use the
+ * software solely pursuant to the terms of the relevant commercial
+ * agreement.
+ */
+
+package io.crate.analyze;
+
+import com.google.common.base.Function;
+import com.google.common.base.Predicates;
+import com.google.common.collect.Sets;
+import io.crate.analyze.relations.JoinPair;
+import io.crate.analyze.relations.QuerySplitter;
+import io.crate.analyze.symbol.*;
+import io.crate.metadata.Functions;
+import io.crate.metadata.NestedReferenceResolver;
+import io.crate.metadata.RowGranularity;
+import io.crate.operation.operator.AndOperator;
+import io.crate.planner.node.dql.join.JoinType;
+import io.crate.sql.tree.QualifiedName;
+import org.elasticsearch.common.collect.Tuple;
+import org.elasticsearch.common.inject.Inject;
+import org.elasticsearch.common.inject.Singleton;
+
+import javax.annotation.Nullable;
+import java.util.*;
+
+@Singleton
+public class Rewriter {
+
+    private final EvaluatingNormalizer normalizer;
+
+    @Inject
+    public Rewriter(Functions functions, NestedReferenceResolver globalRefResolver) {
+        normalizer = new EvaluatingNormalizer(
+            functions,
+            RowGranularity.CLUSTER,
+            globalRefResolver,
+            null,
+            false
+        );
+    }
+
+
+    /**
+     * Rewrite an Outer join to an inner join if possible.
+     *
+     * Conditions on OUTER tables are not pushed down when a MultiSourceSelect is initially created because
+     * the whereClause needs to be checked AFTER the join
+     * (because if the join generates NULL rows the whereClause could become TRUE on those NULL rows)
+     *
+     * See the following two examples where <code>t2</code> is the OUTER table:
+     *
+     * <pre>
+     *     select * from t1
+     *          left join t2 on t1.t2_id = t2.id
+     *     where
+     *          coalesce(t2.x, 20) > 10   # becomes TRUE for null rows
+     * </pre>
+     *
+     *
+     *
+     * But if we know that the whereClause cannot possible become TRUE then we can push it down
+     * (re-writing this into an inner join)
+     *
+     * This is possible because all rows that are generated by the left-join would be removed again by the whereClause anyway.
+     *
+     * <pre>
+     *     select * from t1
+     *          left join t2 on t1.t2_id = t2.id
+     *     where
+     *          t2.x > 10   # if t2.x is NULL this is always FALSE
+     * </pre>
+     */
+    public void tryRewriteOuterToInnerJoin(JoinPair joinPair,
+                                           List<Symbol> mssOutputSymbols,
+                                           QuerySpec multiSourceQuerySpec,
+                                           QualifiedName left,
+                                           QualifiedName right,
+                                           QuerySpec leftQuerySpec,
+                                           QuerySpec rightQuerySpec) {
+        assert left.equals(joinPair.left()) : "This JoinPair has a different left Qualified name: " + joinPair.left();
+        assert right.equals(joinPair.right()) : "This JoinPair has a different left Qualified name: " + joinPair.right();
+
+        JoinType joinType = joinPair.joinType();
+        if (!joinType.isOuter()) {
+            return;
+        }
+        WhereClause where = multiSourceQuerySpec.where();
+        if (!where.hasQuery()) {
+            return;
+        }
+        final QualifiedName outerRelation;
+        final QuerySpec outerSpec;
+        if (joinType == JoinType.LEFT) {
+            outerRelation = right;
+            outerSpec = rightQuerySpec;
+        } else {
+            outerRelation = left;
+            outerSpec = leftQuerySpec;
+        }
+        Map<Set<QualifiedName>, Symbol> splitQueries = QuerySplitter.split(where.query());
+        Symbol outerRelationQuery = splitQueries.remove(Sets.newHashSet(outerRelation));
+        if (outerRelationQuery == null) {
+            return;
+        }
+
+        Symbol symbol = Symbols.replaceRelationColumn(
+            outerRelationQuery,
+            new Function<RelationColumn, Symbol>() {
+                @Nullable
+                @Override
+                public Symbol apply(@Nullable RelationColumn input) {
+                    if (input != null && input.relationName().equals(outerRelation)) {
+                        return Literal.NULL;
+                    }
+                    return input;
+                }
+            }
+        );
+        Symbol normalized = normalizer.normalize(symbol, null);
+        if (WhereClause.canMatch(normalized)) {
+            return; // can't rewrite
+        }
+
+        applyOuterToInnerJoinRewrite(
+            joinPair,
+            mssOutputSymbols,
+            multiSourceQuerySpec,
+            outerSpec,
+            outerRelation,
+            splitQueries,
+            outerRelationQuery
+        );
+    }
+
+    private void applyOuterToInnerJoinRewrite(JoinPair joinPair,
+                                              List<Symbol> mssOutputSymbols,
+                                              QuerySpec multiSourceQuerySpec,
+                                              QuerySpec outerSpec,
+                                              QualifiedName outerRelation,
+                                              Map<Set<QualifiedName>, Symbol> splitQueries,
+                                              Symbol outerRelationQuery) {
+        joinPair.joinType(JoinType.INNER);
+        DereferenceRelationColumnFunction dereferenceRelationColumnFunction =
+            new DereferenceRelationColumnFunction(outerRelation, outerSpec, mssOutputSymbols, joinPair.condition());
+        outerSpec.where(outerSpec.where().add(Symbols.replaceRelationColumn(
+            outerRelationQuery,
+            dereferenceRelationColumnFunction)));
+        if (splitQueries.isEmpty()) {
+            multiSourceQuerySpec.where(WhereClause.MATCH_ALL);
+        } else {
+            multiSourceQuerySpec.where(new WhereClause(AndOperator.join(splitQueries.values())));
+        }
+        for (Tuple<RelationColumn, Symbol> symbolToRemove : dereferenceRelationColumnFunction.symbolsToNotCollect()) {
+            final RelationColumn removedRC = symbolToRemove.v1();
+            outerSpec.outputs().remove(symbolToRemove.v2());
+            multiSourceQuerySpec.outputs().remove(removedRC);
+
+            // shift other relationColumns
+            // e.g. if the outerSpec had outputs: [o1, o2, o3]
+            // and o2 got removed
+            // a RelationColumn(o, 2) would now point to nothing instead of o3
+            // so all indices needs to be decreased
+            multiSourceQuerySpec.replace(new Function<Symbol, Symbol>() {
+                @Nullable
+                @Override
+                public Symbol apply(@Nullable Symbol input) {
+                    if (input instanceof RelationColumn) {
+                        RelationColumn relationColumn = (RelationColumn) input;
+                        if (relationColumn.relationName().equals(removedRC.relationName())
+                            && relationColumn.index() > removedRC.index()) {
+
+                            return new RelationColumn(
+                                relationColumn.relationName(),
+                                relationColumn.index() - 1,
+                                relationColumn.valueType());
+                        }
+                    }
+                    return input;
+                }
+            });
+        }
+    }
+
+    /**
+     * Function to replace RelationColumns with the symbol they're pointing to
+     *
+     * Also collects any RelationColumn which are being replaced and no longer required to be collected into {@link #symbolsToNotCollect()}
+     */
+    private class DereferenceRelationColumnFunction implements Function<RelationColumn, Symbol> {
+        private final QualifiedName outerRelation;
+        private final QuerySpec outerSpec;
+        private final List<Symbol> mssOutputSymbols;
+        private final Symbol joinCondition;
+        private final Set<Tuple<RelationColumn, Symbol>> symbolsToNotCollect;
+
+        DereferenceRelationColumnFunction(QualifiedName outerRelation,
+                                          QuerySpec outerSpec,
+                                          List<Symbol> mssOutputSymbols,
+                                          Symbol joinCondition) {
+            this.outerRelation = outerRelation;
+            this.outerSpec = outerSpec;
+            this.mssOutputSymbols = mssOutputSymbols;
+            this.joinCondition = joinCondition;
+            this.symbolsToNotCollect = new HashSet<>();
+        }
+
+        @Nullable
+        @Override
+        public Symbol apply(@Nullable RelationColumn input) {
+            if (input == null) {
+                return null;
+            }
+            if (!input.relationName().equals(outerRelation)) {
+                return input;
+            }
+            Symbol symbol = outerSpec.outputs().get(input.index());
+
+            // if the column was only added to the outerSpec outputs because of the whereClause
+            // it's possible to not collect it as long is it isn't used somewhere else
+            if (!mssOutputSymbols.contains(symbol) && !SymbolVisitors.any(Predicates.<Symbol>equalTo(input), joinCondition)) {
+                symbolsToNotCollect.add(new Tuple<>(input, symbol));
+            }
+            return symbol;
+        }
+
+        Collection<Tuple<RelationColumn, Symbol>> symbolsToNotCollect() {
+            return symbolsToNotCollect;
+        }
+    }
+}

sql/src/main/java/io/crate/analyze/relations/JoinPair.java
@@ -32,7 +32,7 @@ import javax.annotation.Nullable;
 
 public class JoinPair {
 
-    private final JoinType joinType;
+    private JoinType joinType;
 
     private QualifiedName left;
     private QualifiedName right;
@@ -114,5 +114,9 @@ public class JoinPair {
             }
         }
         return false;
+    }
+
+    public void joinType(JoinType joinType) {
+        this.joinType = joinType;
     }
 }

sql/src/main/java/io/crate/analyze/relations/QuerySplitter.java
@@ -93,7 +93,7 @@ public class QuerySplitter {
                 ManyTableConsumer.QualifiedNameCounter.INSTANCE.process(function, qualifiedNames);
                 Symbol prevQuery = splits.put(qualifiedNames, function);
                 if (prevQuery != null) {
-                    splits.put(qualifiedNames, AndOperator.join(Arrays.asList(prevQuery, function)));
+                    splits.put(qualifiedNames, AndOperator.of(prevQuery, function));
                 }
                 return null;
             }

sql/src/main/java/io/crate/analyze/relations/RelationAnalyzer.java
@@ -187,6 +187,7 @@ public class RelationAnalyzer extends DefaultTraversalVisitor<AnalyzedRelation, 
             // once this is used .normalize should for this class needs to be handled here too
             relation = new MultiSourceSelect(
                 context.sources(),
+                selectAnalysis.outputSymbols(),
                 selectAnalysis.outputNames(),
                 querySpec,
                 context.joinPairs()

sql/src/main/java/io/crate/analyze/relations/RelationNormalizer.java
@@ -103,7 +103,8 @@ final class RelationNormalizer extends AnalyzedRelationVisitor<RelationNormalize
         if (context.querySpec != null) {
             QuerySpec querySpec = mergeAndReplaceFields(multiSourceSelect, context.querySpec);
             // must create a new MultiSourceSelect because paths and query spec changed
-            relation = new MultiSourceSelect(mapSourceRelations(multiSourceSelect), context.paths(), querySpec,
+            relation = new MultiSourceSelect(mapSourceRelations(multiSourceSelect),
+                relation.outputSymbols(), context.paths(), querySpec,
                 multiSourceSelect.joinPairs());
         }
         relation.pushDownQuerySpecs();

sql/src/main/java/io/crate/analyze/symbol/Symbols.java
@@ -25,9 +25,7 @@ import com.google.common.base.Predicate;
 import com.google.common.collect.Lists;
 import io.crate.Streamer;
 import io.crate.exceptions.ConversionException;
-import io.crate.metadata.ColumnIdent;
-import io.crate.metadata.GeneratedReference;
-import io.crate.metadata.Reference;
+import io.crate.metadata.*;
 import io.crate.operation.Input;
 import io.crate.types.DataType;
 import org.elasticsearch.common.io.stream.StreamInput;
@@ -167,5 +165,26 @@ public class Symbols {
         public Boolean visitReference(Reference symbol, ColumnIdent context) {
             return context.equals(symbol.ident().columnIdent());
         }
+    }
+
+    private static class RCReplaceVisitor
+        extends ReplacingSymbolVisitor<com.google.common.base.Function<? super RelationColumn, ? extends Symbol>> {
+
+        public final static RCReplaceVisitor INSTANCE = new RCReplaceVisitor();
+
+        private RCReplaceVisitor() {
+            super(ReplaceMode.COPY);
+        }
+
+        @Override
+        public Symbol visitRelationColumn(RelationColumn relationColumn,
+                                          com.google.common.base.Function<? super RelationColumn, ? extends Symbol> replaceFunction) {
+            return replaceFunction.apply(relationColumn);
+        }
+    }
+
+    public static Symbol replaceRelationColumn(Symbol symbol,
+                                               com.google.common.base.Function<? super RelationColumn, ? extends Symbol> replaceFunction) {
+        return RCReplaceVisitor.INSTANCE.process(symbol, replaceFunction);
     }
 }

commit 820cb9d76341f60d4709a991040c7eacb1e1556b

Fixed WHERE pushDown through tryOptimize().
Regression caused by b0f602c800d66ea501f422d7b246bd0438d5cc95.

sql/src/main/java/io/crate/analyze/WhereClause.java
@@ -201,6 +201,9 @@ public class WhereClause extends QueryClause implements Streamable {
             // NO_MATCH & anything is still NO_MATCH
             return NO_MATCH;
         }
+        if (this.query == null) {
+            return new WhereClause(otherQuery);
+        }
         this.query = AndOperator.of(this.query, otherQuery);
         return this;
     }

sql/src/test/java/io/crate/analyze/WhereClauseTest.java
@@ -71,4 +71,36 @@ public class WhereClauseTest {
             EvaluatingNormalizer.functionOnlyNormalizer(getFunctions()), new TransactionContext());
         assertThat(normalizedWhere.query(), is(sqlExpressions.asSymbol("x = 10")));
     }
+
+    @Test
+    public void testAddWithNoMatch() {
+        WhereClause where1 = WhereClause.NO_MATCH;
+        WhereClause where2 = new WhereClause(sqlExpressions.asSymbol("x = 10"));
+        WhereClause where1_where2 = where1.add(where2.query);
+
+        assertThat(where1_where2.hasQuery(), is(false));
+        assertThat(where1_where2.noMatch, is(true));
+    }
+
+    @Test
+    public void testAddWithMatchAll() {
+        WhereClause where1 = WhereClause.MATCH_ALL;
+        WhereClause where2 = new WhereClause(sqlExpressions.asSymbol("x = 10"));
+        WhereClause where1_where2 = where1.add(where2.query);
+
+        assertThat(where1_where2.hasQuery(), is(true));
+        assertThat(where1_where2.noMatch, is(false));
+        assertThat(where1_where2.query(), is(where2.query()));
+    }
+
+    @Test
+    public void testAddWithNullQuery() {
+        WhereClause where1 = new WhereClause((Symbol) null);
+        WhereClause where2 = new WhereClause(sqlExpressions.asSymbol("x = 10"));
+        WhereClause where1_where2 = where1.add(where2.query);
+
+        assertThat(where1_where2.hasQuery(), is(true));
+        assertThat(where1_where2.noMatch, is(false));
+        assertThat(where1_where2.query(), is(where2.query()));
+    }
 }

sql/src/test/java/io/crate/planner/operators/PushDownTest.java
@@ -208,7 +208,6 @@ public class PushDownTest extends CrateDummyClusterServiceUnitTest {
                                                           "]\n"));
     }
 
-
     @Test
     public void testOrderByIsPushedDownToLeftSide() {
         sqlExecutor.getSessionContext().setHashJoinEnabled(false);
@@ -236,5 +235,20 @@ public class PushDownTest extends CrateDummyClusterServiceUnitTest {
                 "    Collect[doc.t1 | [_fetchid, x] | All]\n" +
                 "]\n")
         );
+    }
+
+    @Test
+    public void testWhereClauseIsPushedDown() {
+        sqlExecutor.getSessionContext().setHashJoinEnabled(false);
+        LogicalPlan plan = sqlExecutor.logicalPlan("SELECT name FROM (SELECT id, name FROM sys.nodes) t " +
+                                                   "WHERE id = 'nodeName'");
+
+        assertThat(
+            plan,
+            LogicalPlannerTest.isPlan(sqlExecutor.functions(),
+                "RootBoundary[name]\n" +
+                "FetchOrEval[name]\n" +
+                "Boundary[id, name]\n" +
+                "Collect[sys.nodes | [id, name] | (id = 'nodeName')]\n"));
     }
 }