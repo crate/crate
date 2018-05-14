c7d36b6abe827a76956376b1f803556b7b372433
Maximilian Michels <mxm@apache.org>
2018-05-14 14:20:39 +0200

    Add benchmark for splitting multi-query string

---
+++ benchmarks/src/test/java/io/crate/protocols/postgres/QueryStringSplitterBenchmark.java
@@ -0,0 +1,58 @@
+/*
+ * Licensed to Crate under one or more contributor license agreements.
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.  Crate licenses this file
+ * to you under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.  You may
+ * obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.  See the License for the specific language governing
+ * permissions and limitations under the License.
+ *
+ * However, if you have executed another commercial license agreement
+ * with Crate these terms will supersede the license and you may use the
+ * software solely pursuant to the terms of the relevant commercial
+ * agreement.
+ */
+
+package io.crate.protocols.postgres;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.infra.Blackhole;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@State(Scope.Benchmark)
+public class QueryStringSplitterBenchmark {
+
+    @Benchmark
+    public void checkSimpleQuery(Blackhole blackhole) {
+        List<String> queries = QueryStringSplitter.splitQuery("select * from users");
+        blackhole.consume(queries);
+    }
+
+    @Benchmark
+    public void checkMultiQuery(Blackhole blackhole) {
+        List<String> queries = QueryStringSplitter.splitQuery("select id from users;" +
+                                                              "select count(*) from users group by id;" +
+                                                              " -- comment " +
+                                                              "\n select * from users;" +
+                                                              "/* block commment \n */" +
+                                                              "select 1"
+        );
+        blackhole.consume(queries);
+    }
+}
