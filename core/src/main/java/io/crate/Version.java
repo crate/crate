6f73f659d31c6dce5e3e05950111dfdc9a04fd99
Autophagy <mika@crate.io>
2018-05-16 09:55:19 +0200

    Prepare 3.0.0 release

--- core/src/main/java/io/crate/Version.java
+++ core/src/main/java/io/crate/Version.java
@@ -40,7 +40,7 @@ public class Version {
     // the (internal) format of the id is there so we can easily do after/before checks on the id
 
 
-    public static final boolean SNAPSHOT = true;
+    public static final boolean SNAPSHOT = false;
     public static final Version CURRENT = new Version(3000099, SNAPSHOT, org.elasticsearch.Version.CURRENT);
 
     static {
