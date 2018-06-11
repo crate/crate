7413ba65492483ba5f19bdddc7ee2f56ab6a0446
Christian Haudum <christian.haudum@gmail.com>
2018-06-11 16:15:17 +0200

    prepare release 3.0.2

--- core/src/main/java/io/crate/Version.java
+++ core/src/main/java/io/crate/Version.java
@@ -41,7 +41,7 @@ public class Version {
 
 
     public static final boolean SNAPSHOT = false;
-    public static final Version CURRENT = new Version(3000199, SNAPSHOT, org.elasticsearch.Version.CURRENT);
+    public static final Version CURRENT = new Version(3000299, SNAPSHOT, org.elasticsearch.Version.CURRENT);
 
     static {
         // safe-guard that we don't release a version with DEBUG_MODE set to true
