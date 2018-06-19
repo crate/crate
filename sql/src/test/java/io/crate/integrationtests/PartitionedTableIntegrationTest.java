6c6f7ab41f407f3b13a5f42829a1bff9ae0eb48c
Andrei Dan <andrei@crate.io>
2018-06-19 12:08:40 +0200

    Repeat offending test 300 times.

--- sql/src/test/java/io/crate/integrationtests/PartitionedTableIntegrationTest.java
+++ sql/src/test/java/io/crate/integrationtests/PartitionedTableIntegrationTest.java
@@ -21,6 +21,7 @@
 
 package io.crate.integrationtests;
 
+import com.carrotsearch.randomizedtesting.annotations.Repeat;
 import com.google.common.base.Joiner;
 import com.google.common.collect.ImmutableList;
 import io.crate.Constants;
@@ -1412,6 +1413,7 @@ public class PartitionedTableIntegrationTest extends SQLTransportIntegrationTest
 
     @Test
     @TestLogging("org.elasticsearch.indices.store:TRACE")
+    @Repeat(iterations = 300)
     public void testAlterTableResetPartitionedTable() throws Exception {
         execute("create table quotes (id integer, quote string, date timestamp) " +
                 "partitioned by(date) clustered into 3 shards with( number_of_replicas = '1-all')");
