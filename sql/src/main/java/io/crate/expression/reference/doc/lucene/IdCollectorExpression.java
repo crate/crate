crate/crate

commit 4bafcae4a5eafa64f01374b7c83fefe16bf56196

Use stored fields to lookup _id
Using the fieldcache requires a lot of HEAP as it loads all values of
the field into memory.
This should improve the performance for most uses-cases, but can result
in a performance decrease for delete queries which match most records.

Benchmark results:

    Q: delete from t where id < 300
    C: 1
      119.44% mean difference
                V1   →    V2
      mean:  329.456 →  83.083

    Q: delete from t
    C: 1
      50.59% mean difference.
                V1  →     V2
      mean:  4606.528 → 7725.752

sql/src/main/java/io/crate/expression/reference/doc/lucene/IdCollectorExpression.java
@@ -21,51 +21,75 @@
 
 package io.crate.expression.reference.doc.lucene;
 
-import io.crate.exceptions.GroupByOnArrayUnsupportedException;
 import io.crate.metadata.doc.DocSysColumns;
+import org.apache.lucene.index.FieldInfo;
+import org.apache.lucene.index.LeafReader;
 import org.apache.lucene.index.LeafReaderContext;
+import org.apache.lucene.index.StoredFieldVisitor;
 import org.apache.lucene.util.BytesRef;
-import org.elasticsearch.index.fielddata.IndexFieldData;
-import org.elasticsearch.index.fielddata.SortedBinaryDocValues;
-import org.elasticsearch.index.mapper.MappedFieldType;
+import org.elasticsearch.index.mapper.Uid;
 
 import java.io.IOException;
 
-public class IdCollectorExpression extends FieldCacheExpression<IndexFieldData, BytesRef> {
+public final class IdCollectorExpression extends LuceneCollectorExpression<BytesRef> {
 
     public static final String COLUMN_NAME = DocSysColumns.ID.name();
+    private final IDVisitor visitor = new IDVisitor();
+    private LeafReader reader;
 
-    private SortedBinaryDocValues values;
-    private BytesRef value;
-
-    public IdCollectorExpression(MappedFieldType mappedFieldType) {
-        super(COLUMN_NAME, mappedFieldType);
+    public IdCollectorExpression() {
+        super(COLUMN_NAME);
     }
 
     @Override
     public void setNextDocId(int docId) throws IOException {
-        super.setNextDocId(docId);
-        if (values.advanceExact(docId)) {
-            switch (values.docValueCount()) {
-                case 1:
-                    value = BytesRef.deepCopyOf(values.nextValue());
-                    break;
-
-                default:
-                    throw new GroupByOnArrayUnsupportedException(columnName);
-            }
-        } else {
-            value = null;
-        }
+        visitor.canStop = false;
+        reader.document(docId, visitor);
     }
 
     @Override
     public BytesRef value() {
-        return value;
+        return visitor.id;
     }
 
     @Override
     public void setNextReader(LeafReaderContext context) {
-        values = indexFieldData.load(context).getBytesValues();
+        reader = context.reader();
+    }
+
+
+    private static class IDVisitor extends StoredFieldVisitor {
+
+        private static final int UTF8 = 0xff;
+
+        private boolean canStop = false;
+        private BytesRef id;
+
+        @Override
+        public Status needsField(FieldInfo fieldInfo) {
+            if (canStop) {
+                return Status.STOP;
+            }
+            if (COLUMN_NAME.equals(fieldInfo.name)) {
+                canStop = true;
+                return Status.YES;
+            }
+            return Status.NO;
+        }
+
+        @Override
+        public void binaryField(FieldInfo fieldInfo, byte[] value) {
+            assert COLUMN_NAME.equals(fieldInfo.name) : "binaryField must only be called for id";
+            id = decodeId(value);
+        }
+
+        static BytesRef decodeId(byte[] idBytes) {
+            final int magicChar = Byte.toUnsignedInt(idBytes[0]);
+            if (magicChar == UTF8) {
+                // handle this directly to avoid BytesRef -> String -> BytesRef round-trip
+                return new BytesRef(idBytes, 1, idBytes.length - 1);
+            }
+            return new BytesRef(Uid.decodeId(idBytes));
+        }
     }
 }

sql/src/main/java/io/crate/expression/reference/doc/lucene/LuceneReferenceResolver.java
@@ -52,8 +52,6 @@ import org.elasticsearch.index.mapper.MappedFieldType;
 
 import java.util.Locale;
 
-import static java.util.Objects.requireNonNull;
-
 public class LuceneReferenceResolver implements ReferenceResolver<LuceneCollectorExpression<?>> {
 
     private final FieldTypeLookup fieldTypeLookup;
@@ -91,14 +89,12 @@ public class LuceneReferenceResolver implements ReferenceResolver<LuceneCollecto
         } else if (UidCollectorExpression.COLUMN_NAME.equals(name)) {
             if (indexSettings.isSingleType()) {
                 // _uid and _id is the same
-                return new IdCollectorExpression(
-                    requireNonNull(fieldTypeLookup.get(IdCollectorExpression.COLUMN_NAME), "_id field must have a fieldType"));
+                return new IdCollectorExpression();
             }
             return new UidCollectorExpression();
         } else if (IdCollectorExpression.COLUMN_NAME.equals(name)) {
             if (indexSettings.isSingleType()) {
-                return new IdCollectorExpression(
-                    requireNonNull(fieldTypeLookup.get(IdCollectorExpression.COLUMN_NAME), "_id field must have a fieldType"));
+                return new IdCollectorExpression();
             }
             return new IdFromUidCollectorExpression();
         } else if (DocCollectorExpression.COLUMN_NAME.equals(name)) {

commit 3162a9d384a1e8b07e22bc7e36fe3ccf1a734cba

Make IdCollectorExpression compatible for old indices
Fixes a regression caused by 4bafcae4a5
Test for this is in https://github.com/crate/crate-qa/pull/66

sql/src/main/java/io/crate/expression/reference/doc/lucene/IdCollectorExpression.java
@@ -83,6 +83,13 @@ public final class IdCollectorExpression extends LuceneCollectorExpression<Bytes
             id = decodeId(value);
         }
 
+        @Override
+        public void stringField(FieldInfo fieldInfo, byte[] value) {
+            assert COLUMN_NAME.equals(fieldInfo.name) : "stringField must only be called for id";
+            // Indices prior to CrateDB 3.0 have id stored as string
+            id = new BytesRef(value);
+        }
+
         static BytesRef decodeId(byte[] idBytes) {
             final int magicChar = Byte.toUnsignedInt(idBytes[0]);
             if (magicChar == UTF8) {