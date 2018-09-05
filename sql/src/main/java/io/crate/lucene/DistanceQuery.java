1e77cae66bb1c28ce1fa0b3283fef2b0999e24b5
Mathias Fussenegger <f.mathias@zignar.net>
2018-09-05 11:32:33 +0200

    Move distance query building functionality into 1 class

--- sql/src/main/java/io/crate/lucene/DistanceQuery.java
+++ sql/src/main/java/io/crate/lucene/DistanceQuery.java
@@ -23,12 +23,22 @@
 package io.crate.lucene;
 
 import io.crate.data.Input;
+import io.crate.expression.operator.EqOperator;
+import io.crate.expression.operator.GtOperator;
+import io.crate.expression.operator.GteOperator;
+import io.crate.expression.operator.LtOperator;
+import io.crate.expression.operator.LteOperator;
 import io.crate.expression.scalar.geo.DistanceFunction;
 import io.crate.expression.symbol.Function;
 import io.crate.types.DataTypes;
+import org.apache.lucene.document.LatLonPoint;
+import org.apache.lucene.search.BooleanClause;
+import org.apache.lucene.search.BooleanQuery;
 import org.apache.lucene.search.Query;
+import org.elasticsearch.common.geo.GeoUtils;
+import org.elasticsearch.common.lucene.search.Queries;
 
-import static io.crate.lucene.DistanceQueries.esV5DistanceQuery;
+import static io.crate.lucene.LuceneQueryBuilder.genericFunctionFilter;
 
 class DistanceQuery implements InnerFunctionToQuery {
 
@@ -61,5 +71,76 @@ class DistanceQuery implements InnerFunctionToQuery {
         String fieldName = distanceRefLiteral.reference().column().fqn();
         Double[] pointValue = (Double[]) geoPointInput.value();
         return esV5DistanceQuery(parent, context, parentName, fieldName, distance, pointValue);
+    }
+
+
+    /**
+     * Create a LatLonPoint distance query.
+     *
+     *
+     * <pre>
+     *          , - ~ ~ ~ - ,
+     *      , '               ' ,
+     *    ,                       ,     X = lonLat coordinates
+     *   ,                         ,
+     *  ,              [distance]   ,
+     *  ,            p<------------>X
+     *  ,            ^              ,
+     *   ,           |             ,
+     *    ,      [columnName]     ,
+     *      ,     point        , '
+     *        ' - , _ _ _ ,  '
+     *
+     *  lt and lte -> match everything WITHIN distance
+     *  gt and gte -> match everything OUTSIDE distance
+     *
+     *  eq distance ~ 0 -> match everything within distance + tolerance
+     *
+     *  eq distance > 0 -> build two circles, one slightly smaller, one slightly larger
+     *                          distance must not be within the smaller distance but within the larger.
+     * </pre>
+     */
+    private static Query esV5DistanceQuery(Function parentFunction,
+                                           LuceneQueryBuilder.Context context,
+                                           String parentOperatorName,
+                                           String columnName,
+                                           Double distance,
+                                           Double[] lonLat) {
+        switch (parentOperatorName) {
+            // We documented that using distance in the WHERE clause utilizes the index which isn't precise so treating
+            // lte & lt the same should be acceptable
+            case LteOperator.NAME:
+            case LtOperator.NAME:
+                return LatLonPoint.newDistanceQuery(columnName, lonLat[1], lonLat[0], distance);
+            case GteOperator.NAME:
+                if (distance - GeoUtils.TOLERANCE <= 0.0d) {
+                    return Queries.newMatchAllQuery();
+                }
+                // fall through
+            case GtOperator.NAME:
+                return Queries.not(LatLonPoint.newDistanceQuery(columnName, lonLat[1], lonLat[0], distance));
+            case EqOperator.NAME:
+                return eqDistance(parentFunction, context, columnName, distance, lonLat);
+            default:
+                return null;
+        }
+    }
+
+    private static Query eqDistance(Function parentFunction,
+                                    LuceneQueryBuilder.Context context,
+                                    String columnName,
+                                    Double distance,
+                                    Double[] lonLat) {
+        double smallDistance = distance * 0.99;
+        if (smallDistance <= 0.0) {
+            return LatLonPoint.newDistanceQuery(columnName, lonLat[1], lonLat[0], 0);
+        }
+        Query withinSmallCircle = LatLonPoint.newDistanceQuery(columnName, lonLat[1], lonLat[0], smallDistance);
+        Query withinLargeCircle = LatLonPoint.newDistanceQuery(columnName, lonLat[1], lonLat[0], distance * 1.01);
+        return new BooleanQuery.Builder()
+            .add(withinLargeCircle, BooleanClause.Occur.MUST)
+            .add(withinSmallCircle, BooleanClause.Occur.MUST_NOT)
+            .add(genericFunctionFilter(parentFunction, context), BooleanClause.Occur.FILTER)
+            .build();
     }
 }
