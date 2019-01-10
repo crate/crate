a413f386a67cad08fb551b72b24497aaa8fb3520
Sebastian Utz <su@rtme.net>
2019-01-10 11:27:36 +0100

    Remove ES secure-sm module, it isn’t used by CrateDB.
    It requires to remove all usages of the SecureSM class including
    classes which are starting Elasticsearch. CrateDB already uses its
    own entry point classes.

--- es/es-server/src/main/java/org/elasticsearch/bootstrap/Security.java
+++ es/es-server/src/main/java/org/elasticsearch/bootstrap/Security.java
@@ -19,44 +19,22 @@
 
 package org.elasticsearch.bootstrap;
 
-import org.elasticsearch.cli.Command;
 import org.elasticsearch.common.SuppressForbidden;
-import org.elasticsearch.common.io.PathUtils;
-import org.elasticsearch.common.network.NetworkModule;
-import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.env.Environment;
-import org.elasticsearch.http.HttpTransportSettings;
-import org.elasticsearch.plugins.PluginInfo;
-import org.elasticsearch.plugins.PluginsService;
-import org.elasticsearch.secure_sm.SecureSM;
-import org.elasticsearch.transport.TcpTransport;
 
 import java.io.IOException;
-import java.net.SocketPermission;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.AccessMode;
-import java.nio.file.DirectoryStream;
 import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
 import java.nio.file.NotDirectoryException;
 import java.nio.file.Path;
 import java.security.NoSuchAlgorithmException;
-import java.security.Permissions;
 import java.security.Policy;
 import java.security.URIParameter;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedHashMap;
-import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
-
-import static org.elasticsearch.bootstrap.FilePermissionUtils.addDirectoryPath;
-import static org.elasticsearch.bootstrap.FilePermissionUtils.addSingleFilePath;
 
 /**
  * Initializes SecurityManager with necessary permissions.
@@ -110,94 +88,6 @@ final class Security {
     private Security() {}
 
     /**
-     * Initializes SecurityManager for the environment
-     * Can only happen once!
-     * @param environment configuration for generating dynamic permissions
-     * @param filterBadDefaults true if we should filter out bad java defaults in the system policy.
-     */
-    static void configure(Environment environment, boolean filterBadDefaults) throws IOException, NoSuchAlgorithmException {
-
-        // enable security policy: union of template and environment-based paths, and possibly plugin permissions
-        Map<String, URL> codebases = getCodebaseJarMap(JarHell.parseClassPath());
-        Policy.setPolicy(new ESPolicy(codebases, createPermissions(environment), getPluginPermissions(environment), filterBadDefaults));
-
-        // enable security manager
-        final String[] classesThatCanExit =
-                new String[]{
-                        // SecureSM matches class names as regular expressions so we escape the $ that arises from the nested class name
-                        ElasticsearchUncaughtExceptionHandler.PrivilegedHaltAction.class.getName().replace("$", "\\$"),
-                        Command.class.getName()};
-        System.setSecurityManager(new SecureSM(classesThatCanExit));
-
-        // do some basic tests
-        selfTest();
-    }
-
-    /**
-     * Return a map from codebase name to codebase url of jar codebases used by ES core.
-     */
-    @SuppressForbidden(reason = "find URL path")
-    static Map<String, URL> getCodebaseJarMap(Set<URL> urls) {
-        Map<String, URL> codebases = new LinkedHashMap<>(); // maintain order
-        for (URL url : urls) {
-            try {
-                String fileName = PathUtils.get(url.toURI()).getFileName().toString();
-                if (fileName.endsWith(".jar") == false) {
-                    // tests :(
-                    continue;
-                }
-                codebases.put(fileName, url);
-            } catch (URISyntaxException e) {
-                throw new RuntimeException(e);
-            }
-        }
-        return codebases;
-    }
-
-    /**
-     * Sets properties (codebase URLs) for policy files.
-     * we look for matching plugins and set URLs to fit
-     */
-    @SuppressForbidden(reason = "proper use of URL")
-    static Map<String,Policy> getPluginPermissions(Environment environment) throws IOException, NoSuchAlgorithmException {
-        Map<String,Policy> map = new HashMap<>();
-        // collect up set of plugins and modules by listing directories.
-        Set<Path> pluginsAndModules = new LinkedHashSet<>(PluginsService.findPluginDirs(environment.pluginsFile()));
-        pluginsAndModules.addAll(PluginsService.findPluginDirs(environment.modulesFile()));
-
-        // now process each one
-        for (Path plugin : pluginsAndModules) {
-            Path policyFile = plugin.resolve(PluginInfo.ES_PLUGIN_POLICY);
-            if (Files.exists(policyFile)) {
-                // first get a list of URLs for the plugins' jars:
-                // we resolve symlinks so map is keyed on the normalize codebase name
-                Set<URL> codebases = new LinkedHashSet<>(); // order is already lost, but some filesystems have it
-                try (DirectoryStream<Path> jarStream = Files.newDirectoryStream(plugin, "*.jar")) {
-                    for (Path jar : jarStream) {
-                        URL url = jar.toRealPath().toUri().toURL();
-                        if (codebases.add(url) == false) {
-                            throw new IllegalStateException("duplicate module/plugin: " + url);
-                        }
-                    }
-                }
-
-                // parse the plugin's policy file into a set of permissions
-                Policy policy = readPolicy(policyFile.toUri().toURL(), getCodebaseJarMap(codebases));
-
-                // consult this policy for each of the plugin's jars:
-                for (URL url : codebases) {
-                    if (map.put(url.getFile(), policy) != null) {
-                        // just be paranoid ok?
-                        throw new IllegalStateException("per-plugin permissions already granted for jar file: " + url);
-                    }
-                }
-            }
-        }
-
-        return Collections.unmodifiableMap(map);
-    }
-
-    /**
      * Reads and returns the specified {@code policyFile}.
      * <p>
      * Jar files listed in {@code codebases} location will be provided to the policy file via
@@ -246,156 +136,6 @@ final class Security {
         }
     }
 
-    /** returns dynamic Permissions to configured paths and bind ports */
-    static Permissions createPermissions(Environment environment) throws IOException {
-        Permissions policy = new Permissions();
-        addClasspathPermissions(policy);
-        addFilePermissions(policy, environment);
-        addBindPermissions(policy, environment.settings());
-        return policy;
-    }
-
-    /** Adds access to classpath jars/classes for jar hell scan, etc */
-    @SuppressForbidden(reason = "accesses fully qualified URLs to configure security")
-    static void addClasspathPermissions(Permissions policy) throws IOException {
-        // add permissions to everything in classpath
-        // really it should be covered by lib/, but there could be e.g. agents or similar configured)
-        for (URL url : JarHell.parseClassPath()) {
-            Path path;
-            try {
-                path = PathUtils.get(url.toURI());
-            } catch (URISyntaxException e) {
-                throw new RuntimeException(e);
-            }
-            // resource itself
-            if (Files.isDirectory(path)) {
-                addDirectoryPath(policy, "class.path", path, "read,readlink");
-            } else {
-                addSingleFilePath(policy, path, "read,readlink");
-            }
-        }
-    }
-
-    /**
-     * Adds access to all configurable paths.
-     */
-    static void addFilePermissions(Permissions policy, Environment environment) throws IOException {
-        // read-only dirs
-        addDirectoryPath(policy, Environment.PATH_HOME_SETTING.getKey(), environment.binFile(), "read,readlink");
-        addDirectoryPath(policy, Environment.PATH_HOME_SETTING.getKey(), environment.libFile(), "read,readlink");
-        addDirectoryPath(policy, Environment.PATH_HOME_SETTING.getKey(), environment.modulesFile(), "read,readlink");
-        addDirectoryPath(policy, Environment.PATH_HOME_SETTING.getKey(), environment.pluginsFile(), "read,readlink");
-        addDirectoryPath(policy, "path.conf'", environment.configFile(), "read,readlink");
-        // read-write dirs
-        addDirectoryPath(policy, "java.io.tmpdir", environment.tmpFile(), "read,readlink,write,delete");
-        addDirectoryPath(policy, Environment.PATH_LOGS_SETTING.getKey(), environment.logsFile(), "read,readlink,write,delete");
-        if (environment.sharedDataFile() != null) {
-            addDirectoryPath(policy, Environment.PATH_SHARED_DATA_SETTING.getKey(), environment.sharedDataFile(),
-                "read,readlink,write,delete");
-        }
-        final Set<Path> dataFilesPaths = new HashSet<>();
-        for (Path path : environment.dataFiles()) {
-            addDirectoryPath(policy, Environment.PATH_DATA_SETTING.getKey(), path, "read,readlink,write,delete");
-            /*
-             * We have to do this after adding the path because a side effect of that is that the directory is created; the Path#toRealPath
-             * invocation will fail if the directory does not already exist. We use Path#toRealPath to follow symlinks and handle issues
-             * like unicode normalization or case-insensitivity on some filesystems (e.g., the case-insensitive variant of HFS+ on macOS).
-             */
-            try {
-                final Path realPath = path.toRealPath();
-                if (!dataFilesPaths.add(realPath)) {
-                    throw new IllegalStateException("path [" + realPath + "] is duplicated by [" + path + "]");
-                }
-            } catch (final IOException e) {
-                throw new IllegalStateException("unable to access [" + path + "]", e);
-            }
-        }
-        for (Path path : environment.repoFiles()) {
-            addDirectoryPath(policy, Environment.PATH_REPO_SETTING.getKey(), path, "read,readlink,write,delete");
-        }
-        if (environment.pidFile() != null) {
-            // we just need permission to remove the file if its elsewhere.
-            addSingleFilePath(policy, environment.pidFile(), "delete");
-        }
-    }
-
-    /**
-     * Add dynamic {@link SocketPermission}s based on HTTP and transport settings.
-     *
-     * @param policy the {@link Permissions} instance to apply the dynamic {@link SocketPermission}s to.
-     * @param settings the {@link Settings} instance to read the HTTP and transport settings from
-     */
-    private static void addBindPermissions(Permissions policy, Settings settings) {
-        addSocketPermissionForHttp(policy, settings);
-        addSocketPermissionForTransportProfiles(policy, settings);
-        addSocketPermissionForTribeNodes(policy, settings);
-    }
-
-    /**
-     * Add dynamic {@link SocketPermission} based on HTTP settings.
-     *
-     * @param policy the {@link Permissions} instance to apply the dynamic {@link SocketPermission}s to.
-     * @param settings the {@link Settings} instance to read the HTTP settings from
-     */
-    private static void addSocketPermissionForHttp(final Permissions policy, final Settings settings) {
-        // http is simple
-        final String httpRange = HttpTransportSettings.SETTING_HTTP_PORT.get(settings).getPortRangeString();
-        addSocketPermissionForPortRange(policy, httpRange);
-    }
-
-    /**
-     * Add dynamic {@link SocketPermission} based on transport settings. This method will first check if there is a port range specified in
-     * the transport profile specified by {@code profileSettings} and will fall back to {@code settings}.
-     *
-     * @param policy          the {@link Permissions} instance to apply the dynamic {@link SocketPermission}s to
-     * @param settings        the {@link Settings} instance to read the transport settings from
-     */
-    private static void addSocketPermissionForTransportProfiles(final Permissions policy, final Settings settings) {
-        // transport is way over-engineered
-        Set<TcpTransport.ProfileSettings> profiles = TcpTransport.getProfileSettings(settings);
-        Set<String> uniquePortRanges = new HashSet<>();
-        // loop through all profiles and add permissions for each one
-        for (final TcpTransport.ProfileSettings profile : profiles) {
-            if (uniquePortRanges.add(profile.portOrRange)) {
-                // profiles fall back to the transport.port if it's not explicit but we want to only add one permission per range
-                addSocketPermissionForPortRange(policy, profile.portOrRange);
-            }
-        }
-    }
-
-    /**
-     * Add dynamic {@link SocketPermission} based on transport settings.
-     *
-     * @param policy          the {@link Permissions} instance to apply the dynamic {@link SocketPermission}s to
-     * @param settings        the {@link Settings} instance to read the transport settings from
-     */
-    private static void addSocketPermissionForTransport(final Permissions policy, final Settings settings) {
-        final String transportRange = TcpTransport.PORT.get(settings);
-        addSocketPermissionForPortRange(policy, transportRange);
-    }
-
-    private static void addSocketPermissionForTribeNodes(final Permissions policy, final Settings settings) {
-        for (final Settings tribeNodeSettings : settings.getGroups("tribe", true).values()) {
-            // tribe nodes have HTTP disabled by default, so we check if HTTP is enabled before granting
-            if (NetworkModule.HTTP_ENABLED.exists(tribeNodeSettings) && NetworkModule.HTTP_ENABLED.get(tribeNodeSettings)) {
-                addSocketPermissionForHttp(policy, tribeNodeSettings);
-            }
-            addSocketPermissionForTransport(policy, tribeNodeSettings);
-        }
-    }
-
-    /**
-     * Add dynamic {@link SocketPermission} for the specified port range.
-     *
-     * @param policy the {@link Permissions} instance to apply the dynamic {@link SocketPermission} to.
-     * @param portRange the port range
-     */
-    private static void addSocketPermissionForPortRange(final Permissions policy, final String portRange) {
-        // listen is always called with 'localhost' but use wildcard to be sure, no name service is consulted.
-        // see SocketPermission implies() code
-        policy.add(new SocketPermission("*:" + portRange, "listen,resolve"));
-    }
-
     /**
      * Ensures configured directory {@code path} exists.
      * @throws IOException if {@code path} exists, but is not a directory, not accessible, or broken symbolic link.
@@ -416,22 +156,6 @@ final class Security {
                 e2.addSuppressed(e);
                 throw e2;
             }
-        }
-    }
-
-    /** Simple checks that everything is ok */
-    @SuppressForbidden(reason = "accesses jvm default tempdir as a self-test")
-    static void selfTest() throws IOException {
-        // check we can manipulate temporary files
-        try {
-            Path p = Files.createTempFile(null, null);
-            try {
-                Files.delete(p);
-            } catch (IOException ignored) {
-                // potentially virus scanner
-            }
-        } catch (SecurityException problem) {
-            throw new SecurityException("Security misconfiguration: cannot access java.io.tmpdir", problem);
         }
     }
 }
