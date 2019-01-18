c35aab9bb02657203af10832341c112aef00783f
Mathias Fussenegger <f.mathias@zignar.net>
2019-01-18 14:55:57 +0100

    Remove more unused action code

--- es/es-server/src/main/java/org/elasticsearch/action/support/WriteResponse.java
+++ es/es-server/src/main/java/org/elasticsearch/action/support/WriteResponse.java
@@ -19,13 +19,11 @@
 
 package org.elasticsearch.action.support;
 
-import org.elasticsearch.action.DocWriteResponse;
 import org.elasticsearch.action.support.WriteRequest.RefreshPolicy;
 import org.elasticsearch.index.IndexSettings;
 
 /**
  * Interface implemented by responses for actions that modify the documents in an index.
- * Rather than implement this directly most implementers should extend {@link DocWriteResponse}.
  */
 public interface WriteResponse {
     /**
