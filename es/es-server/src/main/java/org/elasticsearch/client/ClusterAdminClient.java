14c6f19a3afa56703bd58486781c946ae72c8b34
Mathias Fussenegger <f.mathias@zignar.net>
2019-01-21 14:39:14 +0100

    Trim down client interfaces

--- es/es-server/src/main/java/org/elasticsearch/client/ClusterAdminClient.java
+++ es/es-server/src/main/java/org/elasticsearch/client/ClusterAdminClient.java
@@ -25,9 +25,7 @@ import org.elasticsearch.action.admin.cluster.health.ClusterHealthRequest;
 import org.elasticsearch.action.admin.cluster.health.ClusterHealthRequestBuilder;
 import org.elasticsearch.action.admin.cluster.health.ClusterHealthResponse;
 import org.elasticsearch.action.admin.cluster.node.info.NodesInfoRequestBuilder;
-import org.elasticsearch.action.admin.cluster.repositories.delete.DeleteRepositoryRequest;
 import org.elasticsearch.action.admin.cluster.repositories.delete.DeleteRepositoryRequestBuilder;
-import org.elasticsearch.action.admin.cluster.repositories.put.PutRepositoryRequest;
 import org.elasticsearch.action.admin.cluster.repositories.put.PutRepositoryRequestBuilder;
 import org.elasticsearch.action.admin.cluster.reroute.ClusterRerouteRequest;
 import org.elasticsearch.action.admin.cluster.reroute.ClusterRerouteRequestBuilder;
@@ -35,24 +33,13 @@ import org.elasticsearch.action.admin.cluster.reroute.ClusterRerouteResponse;
 import org.elasticsearch.action.admin.cluster.settings.ClusterUpdateSettingsRequest;
 import org.elasticsearch.action.admin.cluster.settings.ClusterUpdateSettingsRequestBuilder;
 import org.elasticsearch.action.admin.cluster.settings.ClusterUpdateSettingsResponse;
-import org.elasticsearch.action.admin.cluster.snapshots.create.CreateSnapshotRequest;
 import org.elasticsearch.action.admin.cluster.snapshots.create.CreateSnapshotRequestBuilder;
-import org.elasticsearch.action.admin.cluster.snapshots.create.CreateSnapshotResponse;
-import org.elasticsearch.action.admin.cluster.snapshots.delete.DeleteSnapshotRequest;
 import org.elasticsearch.action.admin.cluster.snapshots.delete.DeleteSnapshotRequestBuilder;
-import org.elasticsearch.action.admin.cluster.snapshots.get.GetSnapshotsRequest;
 import org.elasticsearch.action.admin.cluster.snapshots.get.GetSnapshotsRequestBuilder;
-import org.elasticsearch.action.admin.cluster.snapshots.get.GetSnapshotsResponse;
-import org.elasticsearch.action.admin.cluster.snapshots.restore.RestoreSnapshotRequest;
-import org.elasticsearch.action.admin.cluster.snapshots.restore.RestoreSnapshotRequestBuilder;
-import org.elasticsearch.action.admin.cluster.snapshots.restore.RestoreSnapshotResponse;
 import org.elasticsearch.action.admin.cluster.state.ClusterStateRequest;
 import org.elasticsearch.action.admin.cluster.state.ClusterStateRequestBuilder;
 import org.elasticsearch.action.admin.cluster.state.ClusterStateResponse;
-import org.elasticsearch.action.admin.cluster.tasks.PendingClusterTasksRequest;
 import org.elasticsearch.action.admin.cluster.tasks.PendingClusterTasksRequestBuilder;
-import org.elasticsearch.action.admin.cluster.tasks.PendingClusterTasksResponse;
-import org.elasticsearch.action.support.master.AcknowledgedResponse;
 
 /**
  * Administrative actions/operations against indices.
@@ -141,27 +128,7 @@ public interface ClusterAdminClient extends ElasticsearchClient {
     /**
      * Registers a snapshot repository.
      */
-    ActionFuture<AcknowledgedResponse> putRepository(PutRepositoryRequest request);
-
-    /**
-     * Registers a snapshot repository.
-     */
-    void putRepository(PutRepositoryRequest request, ActionListener<AcknowledgedResponse> listener);
-
-    /**
-     * Registers a snapshot repository.
-     */
     PutRepositoryRequestBuilder preparePutRepository(String name);
-
-    /**
-     * Unregisters a repository.
-     */
-    ActionFuture<AcknowledgedResponse> deleteRepository(DeleteRepositoryRequest request);
-
-    /**
-     * Unregisters a repository.
-     */
-    void deleteRepository(DeleteRepositoryRequest request, ActionListener<AcknowledgedResponse> listener);
 
     /**
      * Unregisters a repository.
@@ -171,27 +138,7 @@ public interface ClusterAdminClient extends ElasticsearchClient {
     /**
      * Creates a new snapshot.
      */
-    ActionFuture<CreateSnapshotResponse> createSnapshot(CreateSnapshotRequest request);
-
-    /**
-     * Creates a new snapshot.
-     */
-    void createSnapshot(CreateSnapshotRequest request, ActionListener<CreateSnapshotResponse> listener);
-
-    /**
-     * Creates a new snapshot.
-     */
     CreateSnapshotRequestBuilder prepareCreateSnapshot(String repository, String name);
-
-    /**
-     * Get snapshot.
-     */
-    ActionFuture<GetSnapshotsResponse> getSnapshots(GetSnapshotsRequest request);
-
-    /**
-     * Get snapshot.
-     */
-    void getSnapshots(GetSnapshotsRequest request, ActionListener<GetSnapshotsResponse> listener);
 
     /**
      * Get snapshot.
@@ -201,44 +148,7 @@ public interface ClusterAdminClient extends ElasticsearchClient {
     /**
      * Delete snapshot.
      */
-    ActionFuture<AcknowledgedResponse> deleteSnapshot(DeleteSnapshotRequest request);
-
-    /**
-     * Delete snapshot.
-     */
-    void deleteSnapshot(DeleteSnapshotRequest request, ActionListener<AcknowledgedResponse> listener);
-
-    /**
-     * Delete snapshot.
-     */
     DeleteSnapshotRequestBuilder prepareDeleteSnapshot(String repository, String snapshot);
-
-    /**
-     * Restores a snapshot.
-     */
-    ActionFuture<RestoreSnapshotResponse> restoreSnapshot(RestoreSnapshotRequest request);
-
-    /**
-     * Restores a snapshot.
-     */
-    void restoreSnapshot(RestoreSnapshotRequest request, ActionListener<RestoreSnapshotResponse> listener);
-
-    /**
-     * Restores a snapshot.
-     */
-    RestoreSnapshotRequestBuilder prepareRestoreSnapshot(String repository, String snapshot);
-
-    /**
-     * Returns a list of the pending cluster tasks, that are scheduled to be executed. This includes operations
-     * that update the cluster state (for example, a create index operation)
-     */
-    void pendingClusterTasks(PendingClusterTasksRequest request, ActionListener<PendingClusterTasksResponse> listener);
-
-    /**
-     * Returns a list of the pending cluster tasks, that are scheduled to be executed. This includes operations
-     * that update the cluster state (for example, a create index operation)
-     */
-    ActionFuture<PendingClusterTasksResponse> pendingClusterTasks(PendingClusterTasksRequest request);
 
     /**
      * Returns a list of the pending cluster tasks, that are scheduled to be executed. This includes operations
