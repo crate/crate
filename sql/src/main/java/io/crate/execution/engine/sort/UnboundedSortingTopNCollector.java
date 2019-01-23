fa2dfb6c81acd61bb1b0f230fad583ffb8bb5d76
Andrei Dan <andrei@crate.io>
2019-01-23 18:00:57 +0000

    Use a threshold above which switch from a bounded to an unbounded priority queue.
    In order to process ordered queries we use a PriorityQueue to store the rows
    and then pop them in the correct order. This caused issues as before we used
    a bounded queue with the bound being limit + offset. This was overly pessimistic
    as the number of rows available in the table could be much lower than that,
    but the PriorityQueue container was reserving the memory for this worse case.
    
    This changes the implementation to use a threshold over which we switch from
    using a bounded queue to an unbounded queue that will grow as needed.
    
    (cherry picked from commit 455570b8988cd79070c8071347b31da80850ade1)

--- sql/src/main/java/io/crate/execution/engine/sort/UnboundedSortingTopNCollector.java
+++ sql/src/main/java/io/crate/execution/engine/sort/UnboundedSortingTopNCollector.java
@@ -28,12 +28,12 @@ import io.crate.data.Bucket;
 import io.crate.data.Input;
 import io.crate.data.Row;
 import io.crate.execution.engine.collect.CollectExpression;
-import io.crate.execution.engine.sort.RowPriorityQueue;
 import org.apache.lucene.util.ArrayUtil;
 
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.PriorityQueue;
 import java.util.Set;
 import java.util.function.BiConsumer;
 import java.util.function.BinaryOperator;
@@ -43,34 +43,38 @@ import java.util.stream.Collector;
 
 
 /**
- * Collector implementation which collects rows into a priorityQueue in order to sort the rows and apply a limit + offset.
+ * Collector implementation which collects rows into an unbounded priorityQueue in order to sort the rows and apply a
+ * limit + offset.
  * The final result is a sorted bucket with limit and offset applied.
  */
-public class SortingTopNCollector implements Collector<Row, RowPriorityQueue<Object[]>, Bucket> {
+public class UnboundedSortingTopNCollector implements Collector<Row, PriorityQueue<Object[]>, Bucket> {
 
     private final Collection<? extends Input<?>> inputs;
     private final Iterable<? extends CollectExpression<Row, ?>> expressions;
     private final int numOutputs;
     private final Comparator<Object[]> comparator;
+    private final int initialCapacity;
     private final int offset;
-    private final int maxSize;
-
-    private Object[] spare;
+    private final int maxNumberOfRowsInQueue;
 
     /**
-     * @param inputs             contains output {@link Input}s and orderBy {@link Input}s
-     * @param expressions        expressions linked to the inputs
-     * @param numOutputs         number of output columns
-     * @param comparator         used to sort the rows
-     * @param limit              the max number of rows the result should contain
-     * @param offset             the number of rows to skip (after sort)
+     * @param inputs          contains output {@link Input}s and orderBy {@link Input}s
+     * @param expressions     expressions linked to the inputs
+     * @param numOutputs      number of output columns
+     * @param comparator      used to sort the rows
+     * @param initialCapacity the initial capacity of the backing queue
+     * @param limit           the max number of rows the result should contain
+     * @param offset          the number of rows to skip (after sort)
      */
-    public SortingTopNCollector(Collection<? extends Input<?>> inputs,
-                                Iterable<? extends CollectExpression<Row, ?>> expressions,
-                                int numOutputs,
-                                Comparator<Object[]> comparator,
-                                int limit,
-                                int offset) {
+    public UnboundedSortingTopNCollector(Collection<? extends Input<?>> inputs,
+                                         Iterable<? extends CollectExpression<Row, ?>> expressions,
+                                         int numOutputs,
+                                         Comparator<Object[]> comparator,
+                                         int initialCapacity,
+                                         int limit,
+                                         int offset) {
+        Preconditions.checkArgument(
+            initialCapacity > 0, "Invalid initial capacity: value must be > 0; got: " + initialCapacity);
         Preconditions.checkArgument(limit > 0, "Invalid LIMIT: value must be > 0; got: " + limit);
         Preconditions.checkArgument(offset >= 0, "Invalid OFFSET: value must be >= 0; got: " + offset);
 
@@ -78,36 +82,38 @@ public class SortingTopNCollector implements Collector<Row, RowPriorityQueue<Obj
         this.expressions = expressions;
         this.numOutputs = numOutputs;
         this.comparator = comparator;
+        this.initialCapacity = initialCapacity;
         this.offset = offset;
-        this.maxSize = limit + offset;
+        this.maxNumberOfRowsInQueue = limit + offset;
 
-        if (maxSize >= ArrayUtil.MAX_ARRAY_LENGTH || maxSize < 0)  {
+        if (maxNumberOfRowsInQueue >= ArrayUtil.MAX_ARRAY_LENGTH || maxNumberOfRowsInQueue < 0) {
             // Throw exception to prevent confusing OOME in PriorityQueue
             // 1) if offset + limit exceeds maximum array length
             // 2) if offset + limit exceeds Integer.MAX_VALUE (then maxSize is negative!)
-            throw new IllegalArgumentException("Invalid LIMIT + OFFSET: value must be <= " + (ArrayUtil.MAX_ARRAY_LENGTH - 1) + "; got: " + maxSize);
+            throw new IllegalArgumentException(
+                "Invalid LIMIT + OFFSET: value must be <= " + (ArrayUtil.MAX_ARRAY_LENGTH - 1) + "; got: " + maxNumberOfRowsInQueue);
         }
     }
 
     @Override
-    public Supplier<RowPriorityQueue<Object[]>> supplier() {
-        return () -> new RowPriorityQueue<>(maxSize, comparator);
+    public Supplier<PriorityQueue<Object[]>> supplier() {
+        return () -> new PriorityQueue<>(initialCapacity, comparator);
     }
 
     @Override
-    public BiConsumer<RowPriorityQueue<Object[]>, Row> accumulator() {
+    public BiConsumer<PriorityQueue<Object[]>, Row> accumulator() {
         return this::onNextRow;
     }
 
     @Override
-    public BinaryOperator<RowPriorityQueue<Object[]>> combiner() {
+    public BinaryOperator<PriorityQueue<Object[]>> combiner() {
         return (pq1, pq2) -> {
             throw new UnsupportedOperationException("combine not supported");
         };
     }
 
     @Override
-    public Function<RowPriorityQueue<Object[]>, Bucket> finisher() {
+    public Function<PriorityQueue<Object[]>, Bucket> finisher() {
         return this::pqToIterable;
     }
 
@@ -116,26 +122,37 @@ public class SortingTopNCollector implements Collector<Row, RowPriorityQueue<Obj
         return Collections.emptySet();
     }
 
-    private void onNextRow(RowPriorityQueue<Object[]> pq, Row row) {
+    private void onNextRow(PriorityQueue<Object[]> pq, Row row) {
         for (CollectExpression<Row, ?> expression : expressions) {
             expression.setNextRow(row);
         }
-        if (spare == null) {
-            spare = new Object[inputs.size()];
-        }
+        Object[] rowCells = new Object[inputs.size()];
         int i = 0;
         for (Input<?> input : inputs) {
-            spare[i] = input.value();
+            rowCells[i] = input.value();
             i++;
         }
-        spare = pq.insertWithOverflow(spare);
+
+        if (pq.size() == maxNumberOfRowsInQueue) {
+            Object[] highestElementInOrder = pq.peek();
+            if (highestElementInOrder == null || comparator.compare(rowCells, highestElementInOrder) > 0) {
+                pq.poll();
+                pq.add(rowCells);
+            }
+        } else {
+            pq.add(rowCells);
+        }
     }
 
-    private Bucket pqToIterable(RowPriorityQueue<Object[]> pq) {
-        int resultSize = Math.max(pq.size() - offset, 0);
+    private Bucket pqToIterable(PriorityQueue<Object[]> pq) {
+        if (offset > pq.size()) {
+            return new ArrayBucket(new Object[0][0], numOutputs);
+        }
+        int resultSize = Math.max(Math.min(maxNumberOfRowsInQueue - offset, pq.size() - offset), 0);
+
         Object[][] rows = new Object[resultSize][];
         for (int i = resultSize - 1; i >= 0; i--) {
-            rows[i] = pq.pop();
+            rows[i] = pq.poll();
         }
         return new ArrayBucket(rows, numOutputs);
     }
