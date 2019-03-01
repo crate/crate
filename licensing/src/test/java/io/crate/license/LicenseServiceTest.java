15b7e321caace89122e78cc48769878b1a8c3a00
Savvas Makalias <savvas@crate.io>
2019-03-01 14:02:01 +0200

    Allow Decommission statement when license is expired

--- licensing/src/test/java/io/crate/license/LicenseServiceTest.java
+++ licensing/src/test/java/io/crate/license/LicenseServiceTest.java
@@ -81,7 +81,6 @@ public class LicenseServiceTest extends CrateDummyClusterServiceUnitTest {
         );
     }
 
-
     private static byte[] encrypt(byte[] data, byte[] privateKeyBytes) {
         return CryptoUtilsTest.encryptRsaUsingPrivateKey(data, privateKeyBytes);
     }
