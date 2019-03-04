b3e0d4b7d045b4f2ede7e1202f73d6196fd88c3f
Savvas Makalias <savvas@crate.io>
2019-03-04 11:43:15 +0200

    Add 'number of nodes' restriction to license

--- udc/src/test/java/io/crate/udc/ping/PingTaskTest.java
+++ udc/src/test/java/io/crate/udc/ping/PingTaskTest.java
@@ -53,7 +53,8 @@ public class PingTaskTest extends CrateDummyClusterServiceUnitTest {
 
     static private DecryptedLicenseData LICENSE = new DecryptedLicenseData(
         System.currentTimeMillis() + TimeUnit.DAYS.toMillis(30),
-        "crate"
+        "crate" ,
+        3
         );
 
     private ExtendedNodeInfo extendedNodeInfo;
@@ -158,6 +159,8 @@ public class PingTaskTest extends CrateDummyClusterServiceUnitTest {
             assertThat(map.get("license_expiry_date"), is(String.valueOf(LICENSE.expiryDateInMs())));
             assertThat(map, hasKey("license_issued_to"));
             assertThat(map.get("license_issued_to"), is(LICENSE.issuedTo()));
+            assertThat(map, hasKey("license_max_nodes"));
+            assertThat(map.get("license_max_nodes"), is(String.valueOf(LICENSE.maxNumberOfNodes())));
         }
     }
 
@@ -185,6 +188,7 @@ public class PingTaskTest extends CrateDummyClusterServiceUnitTest {
 
         assertThat(map, not(hasKey("license_expiry_date")));
         assertThat(map, not(hasKey("license_issued_to")));
+        assertThat(map, not(hasKey("license_max_nodes")));
     }
 
     @Test
@@ -238,6 +242,8 @@ public class PingTaskTest extends CrateDummyClusterServiceUnitTest {
             assertThat(map.get("license_expiry_date"), is(notNullValue()));
             assertThat(map, hasKey("license_issued_to"));
             assertThat(map.get("license_issued_to"), is(notNullValue()));
+            assertThat(map, hasKey("license_max_nodes"));
+            assertThat(map.get("license_max_nodes"), is(notNullValue()));
         }
     }
 }
