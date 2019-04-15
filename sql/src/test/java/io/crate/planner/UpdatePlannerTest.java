f9682b9dbe0afc660e5c495edb88b6e5524778b0
Ruslan Kovalov <ruslan.kovalyov@gmail.com>
2019-04-15 11:22:51 +0200

    Make nested subqueries ignore the limit of outer subqueries.
    (cherry picked from commit 2650e8a6c8345bd1abacf78a987e865a48c8824d)

--- sql/src/test/java/io/crate/planner/UpdatePlannerTest.java
+++ sql/src/test/java/io/crate/planner/UpdatePlannerTest.java
@@ -30,6 +30,7 @@ import io.crate.execution.dsl.phases.RoutedCollectPhase;
 import io.crate.execution.dsl.projection.MergeCountProjection;
 import io.crate.execution.dsl.projection.UpdateProjection;
 import io.crate.expression.symbol.InputColumn;
+import io.crate.expression.symbol.SelectSymbol;
 import io.crate.expression.symbol.Symbol;
 import io.crate.metadata.CoordinatorTxnCtx;
 import io.crate.metadata.TransactionContext;
@@ -37,6 +38,7 @@ import io.crate.metadata.Reference;
 import io.crate.planner.consumer.UpdatePlanner;
 import io.crate.planner.node.dml.UpdateById;
 import io.crate.planner.node.dql.Collect;
+import io.crate.planner.operators.LogicalPlan;
 import io.crate.planner.operators.SubQueryResults;
 import io.crate.test.integration.CrateDummyClusterServiceUnitTest;
 import io.crate.testing.SQLExecutor;
@@ -46,7 +48,10 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.Map;
 
+import static io.crate.expression.symbol.SelectSymbol.ResultType.SINGLE_COLUMN_MULTIPLE_VALUES;
+import static io.crate.expression.symbol.SelectSymbol.ResultType.SINGLE_COLUMN_SINGLE_VALUE;
 import static io.crate.testing.SymbolMatchers.isLiteral;
 import static io.crate.testing.SymbolMatchers.isReference;
 import static io.crate.testing.TestingHelpers.isSQL;
@@ -130,5 +135,23 @@ public class UpdatePlannerTest extends CrateDummyClusterServiceUnitTest {
         Collect collect = (Collect) update.createExecutionPlan.create(
             e.getPlannerContext(clusterService.state()), Row.EMPTY, SubQueryResults.EMPTY);
         assertThat(((RoutedCollectPhase) collect.collectPhase()).routing().nodes(), Matchers.emptyIterable());
+    }
+
+    public void testMultiValueSubQueryWithinSingleValueSubQueryDoesNotInheritSoftLimit() {
+        MultiPhasePlan plan = e.plan(
+            "update users set ints = (" +
+                "   select count(id) from users where id in (select unnest([1, 2, 3, 4])))");
+        assertThat(plan.rootPlan, instanceOf(UpdatePlanner.Update.class));
+
+        Map<LogicalPlan, SelectSymbol> rootPlanDependencies = plan.dependencies;
+        LogicalPlan outerSubSelectPlan = rootPlanDependencies.keySet().iterator().next();
+        SelectSymbol outerSubSelectSymbol = rootPlanDependencies.values().iterator().next();
+        assertThat(outerSubSelectSymbol.getResultType(), is(SINGLE_COLUMN_SINGLE_VALUE));
+        assertThat(outerSubSelectPlan.numExpectedRows(), is(2L));
+
+        LogicalPlan innerSubSelectPlan = outerSubSelectPlan.dependencies().keySet().iterator().next();
+        SelectSymbol innerSubSelectSymbol = outerSubSelectPlan.dependencies().values().iterator().next();
+        assertThat(innerSubSelectSymbol.getResultType(), is(SINGLE_COLUMN_MULTIPLE_VALUES));
+        assertThat(innerSubSelectPlan.numExpectedRows(), is(-1L));
     }
 }
