c7463df2bd040691a0a3a17b842b70fc6b2746f0
Mathias Fussenegger <f.mathias@zignar.net>
2019-04-23 12:07:13 +0200

    Remove the shard limit enforcement logic
    We inherited the logic from ES. So far it resulted in a deprecation
    warning if the shard limit was exceeded. In ES 7 the limit became a hard
    limit that aborts index creation.
    
    Given that we didn't expose the setting, and that the strict enforcement
    might be a bit too imposing on partitioned tables, this removes the
    enforcement logic altogether.
    
    We might re-introduce this at a later point more tailored to our
    tables/partitioned table structure.

--- es/es-server/src/main/java/org/elasticsearch/cluster/metadata/MetaData.java
+++ es/es-server/src/main/java/org/elasticsearch/cluster/metadata/MetaData.java
@@ -122,9 +122,6 @@ public class MetaData implements Iterable<IndexMetaData>, Diffable<MetaData>, To
         EnumSet<XContentContext> context();
     }
 
-    public static final Setting<Integer> SETTING_CLUSTER_MAX_SHARDS_PER_NODE =
-        Setting.intSetting("cluster.max_shards_per_node", 1000, 1, Property.Dynamic, Property.NodeScope);
-
     public static final Setting<Boolean> SETTING_READ_ONLY_SETTING =
         Setting.boolSetting("cluster.blocks.read_only", false, Property.Dynamic, Property.NodeScope);
 
