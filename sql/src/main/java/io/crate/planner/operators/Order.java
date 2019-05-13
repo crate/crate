51c79d93ce40abf15dea7557f58f4e3c856df243
Mathias Fussenegger <f.mathias@zignar.net>
2019-05-13 13:24:22 +0200

    Keep data distributed for ordering
    If there is no limit or offset it is not necessary to merge the data to
    a single node to sort it.
    
    To sort data it is not necessary to merge the data

--- sql/src/main/java/io/crate/planner/operators/Order.java
+++ sql/src/main/java/io/crate/planner/operators/Order.java
@@ -82,16 +82,16 @@ public class Order implements LogicalPlan {
                                @Nullable Integer pageSizeHint,
                                Row params,
                                SubQueryResults subQueryResults) {
-        ExecutionPlan executionPlan = source.build(
+        ExecutionPlan plan = source.build(
             plannerContext, projectionBuilder, limit, offset, orderBy, pageSizeHint, params, subQueryResults);
-        if (executionPlan.resultDescription().orderBy() != null) {
+        if (plan.resultDescription().orderBy() != null) {
             // Collect applied ORDER BY eagerly to produce a optimized execution plan;
             if (source instanceof Collect) {
-                return executionPlan;
+                return plan;
             }
-            // merge to finalize the sorting and apply the order of *this* operator.
-            // This is likely a order by on a virtual table which is sorted as well
-            executionPlan = Merge.ensureOnHandler(executionPlan, plannerContext);
+        }
+        if (plan.resultDescription().hasRemainingLimitOrOffset()) {
+            plan = Merge.ensureOnHandler(plan, plannerContext);
         }
         InputColumns.SourceSymbols ctx = new InputColumns.SourceSymbols(source.outputs());
         ensureOrderByColumnsArePresentInOutputs(source.outputs(), orderBy.orderBySymbols());
@@ -104,13 +104,13 @@ public class Order implements LogicalPlan {
             orderBy.nullsFirst()
         );
         PositionalOrderBy positionalOrderBy = PositionalOrderBy.of(orderBy, outputs);
-        executionPlan.addProjection(
+        plan.addProjection(
             topNProjection,
             limit,
             offset,
             positionalOrderBy
         );
-        return executionPlan;
+        return plan;
     }
 
     @Override
