378aa4bbeaa2c13aaafd46cd460f86d248e52166
Ruslan Kovalov <ruslan.kovalyov@gmail.com>
2019-05-15 17:27:39 +0200

    Remove KeyStoreWrapper, SecureStringSetting, and SecureFileSetting.
    Classes are not used.

--- es/es-server/src/main/java/org/elasticsearch/common/settings/SecureSetting.java
+++ es/es-server/src/main/java/org/elasticsearch/common/settings/SecureSetting.java
@@ -19,7 +19,6 @@
 
 package org.elasticsearch.common.settings;
 
-import java.io.InputStream;
 import java.security.GeneralSecurityException;
 import java.util.EnumSet;
 import java.util.Set;
@@ -46,7 +45,6 @@ public abstract class SecureSetting<T> extends Setting<T> {
     private SecureSetting(String key, Property... properties) {
         super(key, (String)null, null, ArrayUtils.concat(properties, FIXED_PROPERTIES, Property.class));
         assert assertAllowedProperties(properties);
-        KeyStoreWrapper.validateSettingName(key);
     }
 
     private boolean assertAllowedProperties(Setting.Property... properties) {
@@ -113,53 +111,10 @@ public abstract class SecureSetting<T> extends Setting<T> {
     }
 
     /**
-     * A setting which contains a sensitive string.
-     *
-     * This may be any sensitive string, e.g. a username, a password, an auth token, etc.
-     */
-    public static Setting<SecureString> secureString(String name, Setting<SecureString> fallback,
-                                                     Property... properties) {
-        return new SecureStringSetting(name, fallback, properties);
-    }
-
-    /**
      * A setting which contains a sensitive string, but which for legacy reasons must be found outside secure settings.
-     * @see #secureString(String, Setting, Property...)
      */
     public static Setting<SecureString> insecureString(String name) {
         return new InsecureStringSetting(name);
-    }
-
-    /**
-     * A setting which contains a file. Reading the setting opens an input stream to the file.
-     *
-     * This may be any sensitive file, e.g. a set of credentials normally in plaintext.
-     */
-    public static Setting<InputStream> secureFile(String name, Setting<InputStream> fallback,
-                                                  Property... properties) {
-        return new SecureFileSetting(name, fallback, properties);
-    }
-
-    private static class SecureStringSetting extends SecureSetting<SecureString> {
-        private final Setting<SecureString> fallback;
-
-        private SecureStringSetting(String name, Setting<SecureString> fallback, Property... properties) {
-            super(name, properties);
-            this.fallback = fallback;
-        }
-
-        @Override
-        protected SecureString getSecret(SecureSettings secureSettings) throws GeneralSecurityException {
-            return secureSettings.getString(getKey());
-        }
-
-        @Override
-        SecureString getFallback(Settings settings) {
-            if (fallback != null) {
-                return fallback.get(settings);
-            }
-            return new SecureString(new char[0]); // this means "setting does not exist"
-        }
     }
 
     private static class InsecureStringSetting extends Setting<SecureString> {
@@ -177,28 +132,6 @@ public abstract class SecureSetting<T> extends Setting<T> {
                     "but property [allow_insecure_settings] is not set");
             }
             return super.get(settings);
-        }
-    }
-
-    private static class SecureFileSetting extends SecureSetting<InputStream> {
-        private final Setting<InputStream> fallback;
-
-        private SecureFileSetting(String name, Setting<InputStream> fallback, Property... properties) {
-            super(name, properties);
-            this.fallback = fallback;
-        }
-
-        @Override
-        protected InputStream getSecret(SecureSettings secureSettings) throws GeneralSecurityException {
-            return secureSettings.getFile(getKey());
-        }
-
-        @Override
-        InputStream getFallback(Settings settings) {
-            if (fallback != null) {
-                return fallback.get(settings);
-            }
-            return null;
         }
     }
 }
