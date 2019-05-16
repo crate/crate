847f64acee80f028aa5ba6a9ffbdffc400bb8374
Mathias Fussenegger <f.mathias@zignar.net>
2019-05-16 10:09:28 +0200

    Remove redundant sanity check in Order operator
    `InputColumns.create` will fail if a column cannot be resolved into an
    `InputColumn`, so the extra check is redundant.
    
    (This was mostly a protection against implementation errors. Users
    should never see these errors)

--- sql/src/main/java/io/crate/planner/operators/Order.java
+++ sql/src/main/java/io/crate/planner/operators/Order.java
@@ -29,7 +29,6 @@ import io.crate.execution.dsl.projection.OrderedTopNProjection;
 import io.crate.execution.dsl.projection.builder.InputColumns;
 import io.crate.execution.dsl.projection.builder.ProjectionBuilder;
 import io.crate.expression.symbol.Symbol;
-import io.crate.expression.symbol.format.SymbolFormatter;
 import io.crate.planner.ExecutionPlan;
 import io.crate.planner.Merge;
 import io.crate.planner.PlannerContext;
@@ -39,8 +38,6 @@ import javax.annotation.Nullable;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
-
-import static io.crate.planner.operators.LogicalPlanner.extractColumns;
 
 public class Order extends ForwardingLogicalPlan {
 
@@ -90,7 +87,6 @@ public class Order extends ForwardingLogicalPlan {
             plan = Merge.ensureOnHandler(plan, plannerContext);
         }
         InputColumns.SourceSymbols ctx = new InputColumns.SourceSymbols(source.outputs());
-        ensureOrderByColumnsArePresentInOutputs(source.outputs(), orderBy.orderBySymbols());
         OrderedTopNProjection topNProjection = new OrderedTopNProjection(
             Limit.limitAndOffset(limit, offset),
             0,
@@ -117,17 +113,6 @@ public class Order extends ForwardingLogicalPlan {
     @Override
     public LogicalPlan replaceSources(List<LogicalPlan> sources) {
         return new Order(Lists2.getOnlyElement(sources), orderBy);
-    }
-
-    private static void ensureOrderByColumnsArePresentInOutputs(List<Symbol> outputs, List<Symbol> orderBySymbols) {
-        Set<Symbol> columnsInOutputs = extractColumns(outputs);
-        for (Symbol columnInOrderBy : extractColumns(orderBySymbols)) {
-            if (!columnsInOutputs.contains(columnInOrderBy)) {
-                throw new UnsupportedOperationException(SymbolFormatter.format(
-                    "Cannot order by \"%s\", as the column does not appear in the outputs of the underlying relation",
-                    columnInOrderBy));
-            }
-        }
     }
 
     @Override
