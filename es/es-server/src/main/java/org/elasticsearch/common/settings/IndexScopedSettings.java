35f7c942d3f0f0e180485eefa28cc97fa26ad186
Mathias Fussenegger <f.mathias@zignar.net>
2019-05-27 14:01:01 +0200

    Remove dynamic type related logic from MapperService
    We are always using the same type and never update the default template
    for new types.

--- es/es-server/src/main/java/org/elasticsearch/common/settings/IndexScopedSettings.java
+++ es/es-server/src/main/java/org/elasticsearch/common/settings/IndexScopedSettings.java
@@ -105,7 +105,6 @@ public final class IndexScopedSettings extends AbstractScopedSettings {
         FieldMapper.IGNORE_MALFORMED_SETTING,
         FieldMapper.COERCE_SETTING,
         Store.INDEX_STORE_STATS_REFRESH_INTERVAL_SETTING,
-        MapperService.INDEX_MAPPER_DYNAMIC_SETTING,
         MapperService.INDEX_MAPPING_TOTAL_FIELDS_LIMIT_SETTING,
         MapperService.INDEX_MAPPING_DEPTH_LIMIT_SETTING,
         BitsetFilterCache.INDEX_LOAD_RANDOM_ACCESS_FILTERS_EAGERLY_SETTING,
