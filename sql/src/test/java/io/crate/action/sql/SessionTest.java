crate/crate

commit 63ae34a98b3771c819af29ad2e511fe245b47bee

Close related portals if a prepared statement is closed
From the PostgreSQL wire protocol docs:

> Note that closing a prepared statement implicitly closes any open
> portals that were constructed from that statement.

We didn't do this so far, which caused the issue described in
https://github.com/crate/crate/issues/8782

This fixes https://github.com/crate/crate/issues/8782

sql/src/main/java/io/crate/action/sql/Session.java
@@ -65,6 +65,7 @@ import org.elasticsearch.common.Randomness;
 import javax.annotation.Nullable;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
@@ -607,6 +608,17 @@ public class Session implements AutoCloseable {
     /**
      * Close a portal or prepared statement
      *
+     * <p>
+     *     From PostgreSQL ExtendedQuery protocol spec:
+     * </p>
+     *
+     * <p>
+     *     The Close message closes an existing prepared statement or portal and releases resources.
+     *     It is not an error to issue Close against a nonexistent statement or portal name.
+     *     [..]
+     *     Note that closing a prepared statement implicitly closes any open portals that were constructed from that statement.
+     * </p>
+     *
      * @param type <b>S</b> for prepared statement, <b>P</b> for portal.
      * @param name name of the prepared statement or the portal (depending on type)
      */
@@ -616,17 +628,30 @@ public class Session implements AutoCloseable {
         }
 
         switch (type) {
-            case 'P':
+            case 'P': {
                 Portal portal = portals.remove(name);
                 if (portal != null) {
                     portal.closeActiveConsumer();
                 }
                 return;
-            case 'S':
-                preparedStatements.remove(name);
+            }
+            case 'S': {
+                PreparedStmt preparedStmt = preparedStatements.remove(name);
+                if (preparedStmt != null) {
+                    Iterator<Map.Entry<String, Portal>> it = portals.entrySet().iterator();
+                    while (it.hasNext()) {
+                        var entry = it.next();
+                        var portal = entry.getValue();
+                        if (portal.preparedStmt().equals(preparedStmt)) {
+                            portal.closeActiveConsumer();
+                            it.remove();
+                        }
+                    }
+                }
                 return;
+            }
             default:
-                throw new IllegalArgumentException("Invalid type: " + type);
+                throw new IllegalArgumentException("Invalid type: " + type + ", valid types are: [P, S]");
         }
     }
 

sql/src/test/java/io/crate/action/sql/SessionTest.java
@@ -38,7 +38,9 @@ import io.crate.testing.SQLExecutor;
 import io.crate.types.DataType;
 import io.crate.types.DataTypes;
 import org.elasticsearch.threadpool.ThreadPool;
+import org.hamcrest.Matchers;
 import org.junit.Test;
+import org.mockito.Answers;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -354,5 +356,29 @@ public class SessionTest extends CrateDummyClusterServiceUnitTest {
         assertThat(session.portals.size(), greaterThan(0));
         assertThat(session.preparedStatements.size(), is(1));
         assertThat(session.preparedStatements.get("stmt").rawStatement(), is("DEALLOCATE test_prep_stmt;"));
+    }
+
+    @Test
+    public void test_closing_a_statement_closes_related_portals() {
+        SQLExecutor sqlExecutor = SQLExecutor.builder(clusterService).build();
+        DependencyCarrier executor = mock(DependencyCarrier.class, Answers.RETURNS_MOCKS);
+        Session session = new Session(
+            sqlExecutor.analyzer,
+            sqlExecutor.planner,
+            new JobsLogs(() -> false),
+            false,
+            executor,
+            AccessControl.DISABLED,
+            SessionContext.systemSessionContext());
+
+        session.parse("S_1", "SELECT 1", List.of());
+        session.bind("P_1", "S_1", List.of(), null);
+
+        assertThat(session.portals.size(), is(1));
+        assertThat(session.preparedStatements.size(), is(1));
+
+        session.close((byte) 'S', "S_1");
+        assertThat(session.portals.entrySet(), Matchers.empty());
+        assertThat(session.preparedStatements.entrySet(), Matchers.empty());
     }
 }

commit e61bd8d86a299ae3e60eb2f9442b07b064ab0298

Make sure bulk operations are tracked in jobsLog
Fixes a regression introduced in https://github.com/crate/crate/pull/8805

sql/src/main/java/io/crate/action/sql/Session.java
@@ -455,6 +455,7 @@ public class Session implements AutoCloseable {
     }
 
     private CompletableFuture<?> bulkExec(Statement statement, List<DeferredExecution> toExec) {
+        assert toExec.size() >= 1 : "Must have at least 1 deferred execution for bulk exec";
         var jobId = UUID.randomUUID();
         var routingProvider = new RoutingProvider(Randomness.get().nextInt(), planner.getAwarenessAttributes());
         var clusterState = executor.clusterService().state();
@@ -472,14 +473,20 @@ public class Session implements AutoCloseable {
         Analysis analysis = analyzer.boundAnalyze(
             statement, currentTxnCtx, new ParameterContext(Row.EMPTY, bulkArgs));
         Plan plan;
+        PreparedStmt firstPreparedStatement = toExec.get(0).portal().preparedStmt();
         try {
             plan = planner.plan(analysis.analyzedStatement(), plannerContext);
         } catch (Throwable t) {
-            PreparedStmt preparedStmt = toExec.get(0).portal().preparedStmt();
             jobsLogs.logPreExecutionFailure(
-                jobId, preparedStmt.rawStatement(), SQLExceptions.messageOf(t), sessionContext.user());
+                jobId, firstPreparedStatement.rawStatement(), SQLExceptions.messageOf(t), sessionContext.user());
             throw t;
         }
+        jobsLogs.logExecutionStart(
+            jobId,
+            firstPreparedStatement.rawStatement(),
+            sessionContext.user(),
+            StatementClassifier.classify(plan)
+        );
         List<CompletableFuture<Long>> rowCounts = plan.executeBulk(
             executor,
             plannerContext,

sql/src/test/java/io/crate/action/sql/SessionTest.java
@@ -27,11 +27,17 @@ import io.crate.analyze.ParamTypeHints;
 import io.crate.analyze.Relations;
 import io.crate.analyze.TableDefinitions;
 import io.crate.auth.user.AccessControl;
+import io.crate.data.Row;
+import io.crate.data.RowConsumer;
 import io.crate.execution.engine.collect.stats.JobsLogs;
 import io.crate.expression.symbol.Literal;
 import io.crate.expression.symbol.ParameterSymbol;
 import io.crate.expression.symbol.Symbol;
 import io.crate.planner.DependencyCarrier;
+import io.crate.planner.Plan;
+import io.crate.planner.Planner;
+import io.crate.planner.PlannerContext;
+import io.crate.planner.operators.SubQueryResults;
 import io.crate.sql.parser.SqlParser;
 import io.crate.test.integration.CrateDummyClusterServiceUnitTest;
 import io.crate.testing.SQLExecutor;
@@ -41,12 +47,16 @@ import org.elasticsearch.threadpool.ThreadPool;
 import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.mockito.Answers;
+import org.mockito.ArgumentMatchers;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 
+import static java.util.concurrent.CompletableFuture.completedFuture;
 import static org.hamcrest.Matchers.arrayContaining;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.greaterThan;
@@ -380,5 +390,51 @@ public class SessionTest extends CrateDummyClusterServiceUnitTest {
         session.close((byte) 'S', "S_1");
         assertThat(session.portals.entrySet(), Matchers.empty());
         assertThat(session.preparedStatements.entrySet(), Matchers.empty());
+    }
+
+    @Test
+    public void test_bulk_operations_result_in_jobslog_entries() throws Exception {
+        SQLExecutor sqlExecutor = SQLExecutor.builder(clusterService)
+            .addTable("create table t1 (x int)")
+            .build();
+        DependencyCarrier executor = mock(DependencyCarrier.class, Answers.RETURNS_MOCKS);
+        Planner planner = mock(Planner.class);
+        when(planner.plan(ArgumentMatchers.any(AnalyzedStatement.class), ArgumentMatchers.any(PlannerContext.class)))
+            .thenReturn(new Plan() {
+                            @Override
+                            public StatementType type() {
+                                return StatementType.INSERT;
+                            }
+
+                            @Override
+                            public void executeOrFail(DependencyCarrier dependencies, PlannerContext plannerContext, RowConsumer consumer, Row params, SubQueryResults subQueryResults) throws Exception {
+
+                            }
+
+                            @Override
+                            public List<CompletableFuture<Long>> executeBulk(DependencyCarrier executor, PlannerContext plannerContext, List<Row> bulkParams, SubQueryResults subQueryResults) {
+                                return List.of(completedFuture(1L), completedFuture(1L));
+                            }
+                        }
+            );
+        JobsLogs jobsLogs = new JobsLogs(() -> true);
+        Session session = new Session(
+            sqlExecutor.analyzer,
+            planner,
+            jobsLogs,
+            false,
+            executor,
+            AccessControl.DISABLED,
+            SessionContext.systemSessionContext());
+
+        session.parse("S_1", "INSERT INTO t1 (x) VALUES (1)", List.of());
+        session.bind("P_1", "S_1", List.of(), null);
+        session.execute("P_1", 0, new BaseResultReceiver());
+
+        session.bind("P_1", "S_1", List.of(), null);
+        session.execute("P_1", 0, new BaseResultReceiver());
+
+        session.sync().get(5, TimeUnit.SECONDS);
+        assertThat(jobsLogs.metrics().iterator().next().totalCount(), is(1L));
     }
 }