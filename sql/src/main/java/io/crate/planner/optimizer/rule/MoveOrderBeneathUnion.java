564fee89ccb5fec658855cd83b57b56ba3ef7225
Sebastian Utz <su@rtme.net>
2019-06-24 11:19:50 +0200

    Revert "Do not move Order below Union if ordered by literals"
    This reverts commit 33ece05e4dd88afc551317df3c028e2b9ce6f660.
    The fix was wrong as it only solved this special union case
    while ordering by literal on simple selects still raises errors.

--- sql/src/main/java/io/crate/planner/optimizer/rule/MoveOrderBeneathUnion.java
+++ sql/src/main/java/io/crate/planner/optimizer/rule/MoveOrderBeneathUnion.java
@@ -22,9 +22,6 @@
 
 package io.crate.planner.optimizer.rule;
 
-import io.crate.expression.symbol.DefaultTraversalSymbolVisitor;
-import io.crate.expression.symbol.Field;
-import io.crate.expression.symbol.Literal;
 import io.crate.expression.symbol.Symbol;
 import io.crate.planner.operators.LogicalPlan;
 import io.crate.planner.operators.Order;
@@ -47,8 +44,7 @@ public final class MoveOrderBeneathUnion implements Rule<Order> {
     public MoveOrderBeneathUnion() {
         this.unionCapture = new Capture<>();
         this.pattern = typeOf(Order.class)
-            .with(source(), typeOf(Union.class).capturedAs(unionCapture))
-            .with(MoveOrderBeneathUnion::orderDoesNotContainAnyLiteral);
+            .with(source(), typeOf(Union.class).capturedAs(unionCapture));
     }
 
     @Override
@@ -66,60 +62,15 @@ public final class MoveOrderBeneathUnion implements Rule<Order> {
         return union.replaceSources(List.of(lhsOrder, rhsOrder));
     }
 
-    private static Order updateSources(Order order, LogicalPlan source) {
+    private static Order updateSources(Order order, LogicalPlan rhs) {
         List<Symbol> sourceOutputs = order.source().outputs();
-        return new Order(source, order.orderBy().map(s -> {
+        return new Order(rhs, order.orderBy().map(s -> {
             int idx = sourceOutputs.indexOf(s);
             if (idx < 0) {
                 throw new IllegalArgumentException(
                     "The ORDER BY expression " + s + " must be part of the child union: " + sourceOutputs);
             }
-            return source.outputs().get(idx);
+            return rhs.outputs().get(idx);
         }));
-    }
-
-    /**
-     * An order on a union always references to the position on each sub relations outputs.
-     * If one symbol is a literal, it must be applied after the union as each sub relation may return different literals.
-     * In such cases, the push down must be prevented.
-     * (Pushing down parts of the order by could be done but requires order symbols splitting)
-     *
-     * Example query:
-     *
-     *  SELECT * FROM (
-     *     SELECT 1 AS x, y FROM t1
-     *     UNION ALL
-     *     SELECT 2, z FROM t2
-     *  ) c
-     *  ORDER BY x
-     *
-     */
-    private static boolean orderDoesNotContainAnyLiteral(Order order) {
-        for (Symbol symbol : order.orderBy().orderBySymbols()) {
-            if (ContainsOrPointsToAnyLiteralSymbol.INSTANCE.process(symbol, null)) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    private static class ContainsOrPointsToAnyLiteralSymbol extends DefaultTraversalSymbolVisitor<Void, Boolean> {
-
-        private static final ContainsOrPointsToAnyLiteralSymbol INSTANCE = new ContainsOrPointsToAnyLiteralSymbol();
-
-        @Override
-        protected Boolean visitSymbol(Symbol symbol, Void context) {
-            return false;
-        }
-
-        @Override
-        public Boolean visitLiteral(Literal symbol, Void context) {
-            return true;
-        }
-
-        @Override
-        public Boolean visitField(Field field, Void context) {
-            return process(field.pointer(), context);
-        }
     }
 }
