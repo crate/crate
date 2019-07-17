a027659ac0b66d4b9988ee2fb060c5e0ca92bb42
Sebastian Utz <su@rtme.net>
2019-07-17 08:34:56 +0100

    bp: Use applied cluster state in cluster health (#44426)
    https://github.com/elastic/elasticsearch/commit/0e958b6650b6b2c477219bf8617936bc6ed731c2

--- server/src/test/java/org/elasticsearch/gateway/GatewayIndexStateIT.java
+++ server/src/test/java/org/elasticsearch/gateway/GatewayIndexStateIT.java
@@ -217,11 +217,13 @@ public class GatewayIndexStateIT extends SQLTransportIntegrationTest {
                 new TimeValue(90, TimeUnit.SECONDS));
         var tableName = getFqn("test");
 
-        logger.info("--> closing master node");
-        internalCluster().closeNonSharedNodes(false);
-
-        logger.info("--> starting 1 master node non data again");
-        internalCluster().startNode(Settings.builder().put(Node.NODE_DATA_SETTING.getKey(), false).build());
+        logger.info("--> restarting master node");
+        internalCluster().fullRestart(new RestartCallback(){
+            @Override
+            public Settings onNodeStopped(String nodeName) {
+                return Settings.builder().put(Node.NODE_DATA_SETTING.getKey(), false).build();
+            }
+        });
 
         logger.info("--> waiting for test index to be created");
         ClusterHealthResponse health = client().admin().cluster().prepareHealth().setWaitForEvents(Priority.LANGUID)
@@ -237,7 +239,7 @@ public class GatewayIndexStateIT extends SQLTransportIntegrationTest {
     }
 
     @Test
-    public void testJustMasterNodeAndJustDataNode() throws Exception {
+    public void testJustMasterNodeAndJustDataNode() {
         logger.info("--> cleaning nodes");
 
         logger.info("--> starting 1 master node non data");
