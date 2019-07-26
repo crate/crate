686d9017a69a3dc54e9012c615cdf45451cc2397
Mathias Fussenegger <f.mathias@zignar.net>
2019-07-26 09:52:16 +0200

    Update lucene to 8.2.0

--- es/es-server/src/main/java/org/elasticsearch/Version.java
+++ es/es-server/src/main/java/org/elasticsearch/Version.java
@@ -76,7 +76,7 @@ public class Version implements Comparable<Version>, ToXContentFragment {
     public static final Version V_4_0_1 = new Version(ES_V_7_0_1_ID, false, org.apache.lucene.util.Version.LUCENE_8_0_0);
     public static final Version V_4_0_2 = new Version(ES_V_7_0_2_ID, false, org.apache.lucene.util.Version.LUCENE_8_0_0);
     public static final Version V_4_0_3 = new Version(ES_V_7_0_3_ID, false, org.apache.lucene.util.Version.LUCENE_8_0_0);
-    public static final Version V_4_1_0 = new Version(ES_V_7_1_0_ID, true, org.apache.lucene.util.Version.LUCENE_8_1_1);
+    public static final Version V_4_1_0 = new Version(ES_V_7_1_0_ID, true, org.apache.lucene.util.Version.LUCENE_8_2_0);
 
     public static final Version CURRENT = V_4_1_0;
 
