3c4e733eec2426fe4a5dec33d120b1f39e06aaa8
Ruslan Kovalov <ruslan.kovalyov@gmail.com>
2019-08-07 17:08:37 +0200

    Process window function filter symbols in WidnowAgg.
    Window function filter symbols were not considered in WindowAgg
    during the creation of the execution plan. It has resulted in creation
    of an incorrect `Collect` plan operator.
    
    This change occurs when running distributed queries that contains
    a standalone reference and window function call against concrete
    table relations.

--- sql/src/test/java/io/crate/planner/operators/WindowAggTest.java
+++ sql/src/test/java/io/crate/planner/operators/WindowAggTest.java
@@ -67,6 +67,24 @@ public class WindowAggTest extends CrateDummyClusterServiceUnitTest {
     }
 
     @Test
+    public void test_window_agg_output_for_select_with_standalone_ref_and_window_func_with_filter() {
+        var plan = plan("SELECT y, AVG(x) FILTER (WHERE x > 1) OVER() FROM t1");
+        var expectedPlan = "FetchOrEval[y, avg(x) : (x > 1)]\n" +
+                           "WindowAgg[avg(x) : (x > 1)]\n" +
+                           "Collect[doc.t1 | [_fetchid, x] | All]\n";
+        assertThat(plan, isPlan(e.functions(), expectedPlan));
+    }
+
+    @Test
+    public void test_window_agg_with_filter_that_contains_column_that_is_not_in_outputs() {
+        var plan = plan("SELECT x, COUNT(*) FILTER (WHERE y > 1) OVER() FROM t1");
+        var expectedPlan = "FetchOrEval[x, count(*) : (y > 1)]\n" +
+                           "WindowAgg[count(*) : (y > 1)]\n" +
+                           "Collect[doc.t1 | [_fetchid, y] | All]\n";
+        assertThat(plan, isPlan(e.functions(), expectedPlan));
+    }
+
+    @Test
     public void testNoOrderByIfNoPartitionsAndNoOrderBy() {
         OrderBy orderBy = WindowAgg.createOrderByInclPartitionBy(wd("avg(x) OVER ()"));
         assertThat(orderBy, Matchers.nullValue());
