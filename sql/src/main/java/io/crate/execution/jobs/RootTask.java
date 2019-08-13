b5a718f0ee1b482d535d1eaca664f730749b062f
Mathias Fussenegger <f.mathias@zignar.net>
2019-08-13 14:52:09 +0200

    Remove `operationFinished` handling in `RootTask.prepare`
    The `RemoveTaskListener` already takes care of calling
    `operationFinished`, and that the listener is triggered is currently
    made sure by the prepare handling in `AbstractTask`.

--- sql/src/main/java/io/crate/execution/jobs/RootTask.java
+++ sql/src/main/java/io/crate/execution/jobs/RootTask.java
@@ -190,15 +190,7 @@ public class RootTask implements CompletionListenable<Void> {
             int id = orderedTaskIds.get(i);
             Task task = orderedTasks.get(i);
             jobsLogs.operationStarted(id, jobId, task.name());
-            try {
-                task.prepare();
-            } catch (Exception e) {
-                for (; i >= 0; i--) {
-                    id = orderedTaskIds.get(i);
-                    jobsLogs.operationFinished(id, jobId, "Prepare: " + SQLExceptions.messageOf(e), -1);
-                }
-                throw e;
-            }
+            task.prepare();
         }
     }
 
