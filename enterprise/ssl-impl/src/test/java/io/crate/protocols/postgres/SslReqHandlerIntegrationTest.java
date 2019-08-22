851dfa153cc3e66539d08afaeaba1e2009f9421a
Sebastian Utz <su@rtme.net>
2019-08-22 11:22:44 +0200

    Add exception msg to reload-ssl-context test to assertions
    Adding the exception message should give us more information why
    these tests are failing.

--- enterprise/ssl-impl/src/test/java/io/crate/protocols/postgres/SslReqHandlerIntegrationTest.java
+++ enterprise/ssl-impl/src/test/java/io/crate/protocols/postgres/SslReqHandlerIntegrationTest.java
@@ -110,7 +110,7 @@ public class SslReqHandlerIntegrationTest extends SQLTransportIntegrationTest {
                 SQLResponse response = execute("select name from sys.nodes");
                 assertEquals(1, response.rowCount());
             } catch (Exception e) {
-                fail();
+                fail(e.getMessage());
             }
         }, 20, TimeUnit.SECONDS);
     }
