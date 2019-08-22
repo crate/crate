fe3500141119a3bc2fb8f6da31b04723119d0d62
Sebastian Utz <su@rtme.net>
2019-08-22 11:22:44 +0200

    Add exception msg to reload-ssl-context test to assertions
    Adding the exception message should give us more information why
    these tests are failing.
    
    (cherry picked from commit 851dfa153cc3e66539d08afaeaba1e2009f9421a)

--- enterprise/ssl-impl/src/test/java/io/crate/protocols/http/HttpsTransportKeyStoreReloadIntegrationTest.java
+++ enterprise/ssl-impl/src/test/java/io/crate/protocols/http/HttpsTransportKeyStoreReloadIntegrationTest.java
@@ -42,7 +42,6 @@ import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.nullValue;
 
-
 @UseJdbc(value = 0)
 @ESIntegTestCase.ClusterScope(numDataNodes = 1, numClientNodes = 0, supportsDedicatedMasters = false)
 public class HttpsTransportKeyStoreReloadIntegrationTest extends SQLHttpIntegrationTest {
@@ -133,7 +132,7 @@ public class HttpsTransportKeyStoreReloadIntegrationTest extends SQLHttpIntegrat
                 assertThat(result, containsString("\"rowcount\":1"));
                 assertThat(result, containsString("sslWorks"));
             } catch (Exception e) {
-                fail();
+                fail(e.getMessage());
             }
         }, 20, TimeUnit.SECONDS);
     }
