b87d828d192fd75021493f27af1e6063df1745b4
Ruslan Kovalov <ruslan.kovalyov@gmail.com>
2019-09-13 15:22:50 +0200

    Exclude `node.store.allow_mmap` from table/partition defaults.

--- sql/src/main/java/io/crate/analyze/TableParameters.java
+++ sql/src/main/java/io/crate/analyze/TableParameters.java
@@ -110,7 +110,8 @@ public class TableParameters {
      */
     static final Set<Setting> SETTINGS_NOT_INCLUDED_IN_DEFAULT = Set.of(
         IndexMetaData.INDEX_NUMBER_OF_ROUTING_SHARDS_SETTING,
-        IndexService.GLOBAL_CHECKPOINT_SYNC_INTERVAL_SETTING
+        IndexService.GLOBAL_CHECKPOINT_SYNC_INTERVAL_SETTING,
+        IndexModule.NODE_STORE_ALLOW_MMAP
     );
 
     private static final Map<String, Setting<?>> SUPPORTED_SETTINGS_DEFAULT
