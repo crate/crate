f2b41a048a39cfcc1da8520cb34b911c2945e68c
marregui <miguel.arregui@gmail.com>
2019-09-17 13:50:52 +0200

    Remove unused import

--- sql/src/main/java/io/crate/execution/dml/upsert/TransportShardUpsertAction.java
+++ sql/src/main/java/io/crate/execution/dml/upsert/TransportShardUpsertAction.java
@@ -71,7 +71,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
