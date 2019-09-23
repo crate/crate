bef03ab8996071e6bf430758e55b191531a8eff8
Mathias Fussenegger <f.mathias@zignar.net>
2019-09-23 17:02:59 +0200

    Extract a ShardStateObserver from ShardStateAwareRemoteCollector
    This extracts a `ShardStateObserver` from
    `ShardStateAwareRemoteCollector`. This should separate the concerns of
    the components better. `ShardStateObserver` is only responsible to wait
    for a shard to become active again. Everything else moves into
      `RemoteCollectorFactory`.

---
+++ sql/src/test/java/io/crate/execution/engine/collect/collectors/ShardStateObserverTest.java
@@ -0,0 +1,70 @@
+/*
+ * Licensed to Crate under one or more contributor license agreements.
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.  Crate licenses this file
+ * to you under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.  You may
+ * obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.  See the License for the specific language governing
+ * permissions and limitations under the License.
+ *
+ * However, if you have executed another commercial license agreement
+ * with Crate these terms will supersede the license and you may use the
+ * software solely pursuant to the terms of the relevant commercial
+ * agreement.
+ */
+
+package io.crate.execution.engine.collect.collectors;
+
+import io.crate.test.integration.CrateDummyClusterServiceUnitTest;
+import io.crate.testing.SQLExecutor;
+import org.elasticsearch.cluster.ClusterState;
+import org.elasticsearch.cluster.routing.IndexRoutingTable;
+import org.elasticsearch.cluster.routing.IndexShardRoutingTable;
+import org.elasticsearch.cluster.routing.RoutingTable;
+import org.elasticsearch.cluster.routing.ShardRouting;
+import org.elasticsearch.common.Randomness;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.util.concurrent.ThreadContext;
+import org.elasticsearch.index.shard.ShardId;
+import org.elasticsearch.test.ClusterServiceUtils;
+import org.junit.Test;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+
+import static org.hamcrest.Matchers.is;
+
+public class ShardStateObserverTest extends CrateDummyClusterServiceUnitTest {
+
+    @Test
+    public void test_wait_for_active_shard_completes_on_shard_state_change() throws Throwable {
+        // Add 2 nodes and table to cluster state
+        SQLExecutor.builder(clusterService, 2, Randomness.get())
+            .addTable("create table t1 (x int) clustered into 1 shards");
+
+        var observer = new ShardStateObserver(clusterService, new ThreadContext(Settings.EMPTY));
+        IndexShardRoutingTable routingTable = clusterService.state().routingTable().shardRoutingTable("t1", 0);
+        ShardId shardId = routingTable.shardId();
+        CompletableFuture<ShardRouting> shard0Active = observer.waitForActiveShard(shardId);
+        assertThat(shard0Active.isDone(), is(false));
+
+        ShardRouting startedPrimaryShard = routingTable.primaryShard().moveToStarted();
+        ClusterState newClusterState = ClusterState.builder(clusterService.state())
+            .routingTable(
+                RoutingTable.builder()
+                    .add(IndexRoutingTable.builder(shardId.getIndex()).addShard(startedPrimaryShard).build())
+                    .build()
+            ).build();
+        ClusterServiceUtils.setState(clusterService, newClusterState);
+
+        // This now shouldn't timeout
+        shard0Active.get(5, TimeUnit.SECONDS);
+    }
+}
