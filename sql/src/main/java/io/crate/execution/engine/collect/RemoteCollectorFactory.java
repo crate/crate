crate/crate

commit bef03ab8996071e6bf430758e55b191531a8eff8

Extract a ShardStateObserver from ShardStateAwareRemoteCollector
This extracts a `ShardStateObserver` from
`ShardStateAwareRemoteCollector`. This should separate the concerns of
the components better. `ShardStateObserver` is only responsible to wait
for a shard to become active again. Everything else moves into
  `RemoteCollectorFactory`.

sql/src/main/java/io/crate/execution/engine/collect/RemoteCollectorFactory.java
@@ -24,27 +24,27 @@ package io.crate.execution.engine.collect;
 
 import com.carrotsearch.hppc.IntArrayList;
 import com.carrotsearch.hppc.IntIndexedContainer;
+import com.google.common.collect.Lists;
 import io.crate.common.collections.TreeMapBuilder;
 import io.crate.data.BatchIterator;
 import io.crate.data.Buckets;
 import io.crate.data.CollectingBatchIterator;
 import io.crate.data.CollectingRowConsumer;
 import io.crate.data.Row;
-import io.crate.data.RowConsumer;
 import io.crate.exceptions.Exceptions;
 import io.crate.execution.TransportActionProvider;
 import io.crate.execution.dsl.phases.RoutedCollectPhase;
 import io.crate.execution.dsl.projection.Projections;
 import io.crate.execution.engine.collect.collectors.RemoteCollector;
-import io.crate.execution.engine.collect.collectors.ShardStateAwareRemoteCollector;
+import io.crate.execution.engine.collect.collectors.ShardStateObserver;
 import io.crate.execution.engine.collect.sources.ShardCollectorProviderFactory;
 import io.crate.execution.jobs.TasksService;
 import io.crate.metadata.Routing;
 import io.crate.planner.distribution.DistributionInfo;
+import org.elasticsearch.cluster.routing.ShardRouting;
 import org.elasticsearch.cluster.service.ClusterService;
 import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.common.inject.Singleton;
-import org.elasticsearch.index.shard.IndexShard;
 import org.elasticsearch.index.shard.ShardId;
 import org.elasticsearch.indices.IndicesService;
 import org.elasticsearch.threadpool.ThreadPool;
@@ -53,8 +53,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
-import java.util.function.Function;
+import java.util.function.Consumer;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
@@ -98,73 +99,64 @@ public class RemoteCollectorFactory {
                                               RoutedCollectPhase collectPhase,
                                               CollectTask collectTask,
                                               ShardCollectorProviderFactory shardCollectorProviderFactory) {
-        final UUID childJobId = UUID.randomUUID(); // new job because subContexts can't be merged into an existing job
-
-        Collector<Row, ?, List<Object[]>> listCollector = Collectors.mapping(Row::materialize, Collectors.toList());
-        CollectingRowConsumer<?, List<Object[]>> consumer = new CollectingRowConsumer<>(listCollector);
-
-        ShardStateAwareRemoteCollector shardStateAwareRemoteCollector = new ShardStateAwareRemoteCollector(
-            shardId,
-            consumer,
-            clusterService,
-            indicesService,
-            getLocalCollectorProvider(shardCollectorProviderFactory, collectPhase, collectTask, consumer),
-            getRemoteCollectorProvider(childJobId, shardId, collectPhase, collectTask, consumer),
-            searchTp,
-            threadPool.getThreadContext());
-
+        ShardStateObserver shardStateObserver = new ShardStateObserver(clusterService, threadPool.getThreadContext());
+        CompletableFuture<ShardRouting> shardBecameActive = shardStateObserver.waitForActiveShard(shardId);
+        Runnable onClose = () -> {};
+        Consumer<Throwable> kill = killReason -> {
+            shardBecameActive.cancel(true);
+            shardBecameActive.completeExceptionally(killReason);
+        };
         return CollectingBatchIterator.newInstance(
-            () -> {},
-            shardStateAwareRemoteCollector::kill,
-            () -> {
-                shardStateAwareRemoteCollector.doCollect();
-                return consumer.completionFuture().thenApply(results -> results.stream().map(Buckets.arrayToSharedRow())::iterator);
-            },
+            onClose,
+            kill,
+            () -> shardBecameActive.thenCompose(
+                activePrimaryRouting -> retrieveRows(activePrimaryRouting, collectPhase, collectTask, shardCollectorProviderFactory)),
             true
         );
     }
 
-    private Function<IndexShard, BatchIterator<Row>> getLocalCollectorProvider(ShardCollectorProviderFactory shardCollectorProviderFactory,
-                                                                               RoutedCollectPhase collectPhase,
-                                                                               CollectTask collectTask,
-                                                                               RowConsumer consumer) {
-        return indexShard -> {
+    private CompletableFuture<List<Row>> retrieveRows(ShardRouting activePrimaryRouting,
+                                                      RoutedCollectPhase collectPhase,
+                                                      CollectTask collectTask,
+                                                      ShardCollectorProviderFactory shardCollectorProviderFactory) {
+        Collector<Row, ?, List<Object[]>> listCollector = Collectors.mapping(Row::materialize, Collectors.toList());
+        CollectingRowConsumer<?, List<Object[]>> consumer = new CollectingRowConsumer<>(listCollector);
+        String nodeId = activePrimaryRouting.currentNodeId();
+        String localNodeId = clusterService.localNode().getId();
+        if (localNodeId.equalsIgnoreCase(nodeId)) {
+            var indexShard = indicesService.indexServiceSafe(activePrimaryRouting.index()).getShard(activePrimaryRouting.shardId().id());
+            var collectorProvider = shardCollectorProviderFactory.create(indexShard);
+            BatchIterator<Row> it;
             try {
-                return shardCollectorProviderFactory
-                    .create(indexShard)
-                    .getIterator(collectPhase, consumer.requiresScroll(), collectTask);
+                it = collectorProvider.getIterator(collectPhase, consumer.requiresScroll(), collectTask);
             } catch (Exception e) {
-                Exceptions.rethrowUnchecked(e);
-                return null;
+                return Exceptions.rethrowRuntimeException(e);
             }
-        };
+            consumer.accept(it, null);
+        } else {
+            UUID childJobId = UUID.randomUUID();
+            RemoteCollector remoteCollector = new RemoteCollector(
+                childJobId,
+                collectTask.txnCtx().sessionSettings(),
+                localNodeId,
+                nodeId,
+                transportActionProvider.transportJobInitAction(),
+                transportActionProvider.transportKillJobsNodeAction(),
+                searchTp,
+                tasksService,
+                collectTask.queryPhaseRamAccountingContext(),
+                consumer,
+                createRemoteCollectPhase(childJobId, collectPhase, activePrimaryRouting.shardId(), nodeId)
+            );
+            remoteCollector.doCollect();
+        }
+        return consumer.completionFuture().thenApply(rows -> Lists.transform(rows, Buckets.arrayToSharedRow()::apply));
     }
 
-    private Function<String, RemoteCollector> getRemoteCollectorProvider(UUID jobId,
-                                                                         ShardId shardId,
-                                                                         RoutedCollectPhase collectPhase,
-                                                                         CollectTask collectTask,
-                                                                         RowConsumer consumer) {
-        String localNode = clusterService.localNode().getId();
-        return remoteNode -> new RemoteCollector(
-            jobId,
-            collectTask.txnCtx().sessionSettings(),
-            localNode,
-            remoteNode,
-            transportActionProvider.transportJobInitAction(),
-            transportActionProvider.transportKillJobsNodeAction(),
-            searchTp,
-            tasksService,
-            collectTask.queryPhaseRamAccountingContext(),
-            consumer,
-            createRemoteCollectPhase(jobId, collectPhase, shardId, remoteNode)
-        );
-    }
-
-    private RoutedCollectPhase createRemoteCollectPhase(UUID childJobId,
-                                                        RoutedCollectPhase collectPhase,
-                                                        ShardId shardId,
-                                                        String nodeId) {
+    private static RoutedCollectPhase createRemoteCollectPhase(UUID childJobId,
+                                                               RoutedCollectPhase collectPhase,
+                                                               ShardId shardId,
+                                                               String nodeId) {
 
         Routing routing = new Routing(TreeMapBuilder.<String, Map<String, IntIndexedContainer>>newMapBuilder()
             .put(nodeId, TreeMapBuilder.<String, IntIndexedContainer>newMapBuilder()

sql/src/main/java/io/crate/execution/engine/collect/collectors/ShardStateObserver.java
@@ -1,0 +1,126 @@
+/*
+ * Licensed to Crate under one or more contributor license agreements.
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.  Crate licenses this file
+ * to you under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.  You may
+ * obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.  See the License for the specific language governing
+ * permissions and limitations under the License.
+ *
+ * However, if you have executed another commercial license agreement
+ * with Crate these terms will supersede the license and you may use the
+ * software solely pursuant to the terms of the relevant commercial
+ * agreement.
+ */
+
+package io.crate.execution.engine.collect.collectors;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.elasticsearch.cluster.ClusterState;
+import org.elasticsearch.cluster.ClusterStateObserver;
+import org.elasticsearch.cluster.routing.ShardRouting;
+import org.elasticsearch.cluster.service.ClusterService;
+import org.elasticsearch.common.unit.TimeValue;
+import org.elasticsearch.common.util.concurrent.ThreadContext;
+import org.elasticsearch.index.IndexNotFoundException;
+import org.elasticsearch.index.shard.ShardId;
+import org.elasticsearch.index.shard.ShardNotFoundException;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeoutException;
+
+public final class ShardStateObserver {
+
+    private static final TimeValue MAX_WAIT_TIME_FOR_NEW_STATE = TimeValue.timeValueSeconds(30);
+    private static final Logger LOGGER = LogManager.getLogger(ShardStateObserver.class);
+
+    private final ClusterService clusterService;
+    private final ThreadContext threadContext;
+
+    public ShardStateObserver(ClusterService clusterService, ThreadContext threadContext) {
+        this.clusterService = clusterService;
+        this.threadContext = threadContext;
+    }
+
+    public CompletableFuture<ShardRouting> waitForActiveShard(ShardId shardId) {
+        ClusterState state = clusterService.state();
+        try {
+            var routingTable = state.routingTable().shardRoutingTable(shardId);
+            var primaryShardRouting = routingTable.primaryShard();
+            if (primaryShardRouting.active() || primaryShardRouting.started()) {
+                return CompletableFuture.completedFuture(primaryShardRouting);
+            } else {
+                return waitForActiveShard(shardId, state);
+            }
+        } catch (ShardNotFoundException e) {
+            return waitForActiveShard(shardId, state);
+        } catch (IndexNotFoundException e) {
+            return CompletableFuture.failedFuture(e);
+        }
+    }
+
+    private CompletableFuture<ShardRouting> waitForActiveShard(ShardId shardId, ClusterState state) {
+        var stateObserver = new ClusterStateObserver(
+            state, clusterService, MAX_WAIT_TIME_FOR_NEW_STATE, LOGGER, threadContext);
+        var listener = new RetryIsShardActive(shardId);
+        stateObserver.waitForNextChange(listener, newState -> shardBecameActiveOrIndexWasDeleted(newState, shardId));
+        return listener.result();
+    }
+
+    private static boolean shardBecameActiveOrIndexWasDeleted(ClusterState state, ShardId shardId) {
+        try {
+            var routingTable = state.routingTable().shardRoutingTable(shardId);
+            return routingTable.primaryShard().started() || routingTable.primaryShard().active();
+        } catch (ShardNotFoundException e) {
+            return false;
+        } catch (IndexNotFoundException e) {
+            return true;
+        }
+    }
+
+    private class RetryIsShardActive implements ClusterStateObserver.Listener {
+
+        private final ShardId shardId;
+
+        private final CompletableFuture<ShardRouting> result = new CompletableFuture<>();
+
+        RetryIsShardActive(ShardId shardId) {
+            this.shardId = shardId;
+        }
+
+        @Override
+        public void onNewClusterState(ClusterState state) {
+            waitForActiveShard(shardId).whenComplete((routingTable, err) -> {
+                if (routingTable == null) {
+                    result.completeExceptionally(err);
+                } else {
+                    result.complete(routingTable);
+                }
+            });
+        }
+
+        @Override
+        public void onClusterServiceClose() {
+            result.completeExceptionally(new IllegalStateException(
+                "ClusterService was closed while waiting for  shard=" + shardId + " to become active"));
+        }
+
+        @Override
+        public void onTimeout(TimeValue timeout) {
+            result.completeExceptionally(
+                new TimeoutException("Timeout waiting for shard=" + shardId + " to become active"));
+        }
+
+        public CompletableFuture<ShardRouting> result() {
+            return result;
+        }
+    }
+}

sql/src/main/java/io/crate/execution/engine/collect/collectors/ShardStateAwareRemoteCollector.java
@@ -1,232 +1,0 @@
-/*
- * Licensed to Crate under one or more contributor license agreements.
- * See the NOTICE file distributed with this work for additional
- * information regarding copyright ownership.  Crate licenses this file
- * to you under the Apache License, Version 2.0 (the "License"); you may
- * not use this file except in compliance with the License.  You may
- * obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
- * implied.  See the License for the specific language governing
- * permissions and limitations under the License.
- *
- * However, if you have executed another commercial license agreement
- * with Crate these terms will supersede the license and you may use the
- * software solely pursuant to the terms of the relevant commercial
- * agreement.
- */
-
-package io.crate.execution.engine.collect.collectors;
-
-import io.crate.data.BatchIterator;
-import io.crate.data.Row;
-import io.crate.data.RowConsumer;
-import org.apache.logging.log4j.Logger;
-import org.elasticsearch.cluster.ClusterState;
-import org.elasticsearch.cluster.ClusterStateObserver;
-import org.elasticsearch.cluster.metadata.IndexMetaData;
-import org.elasticsearch.cluster.routing.IndexShardRoutingTable;
-import org.elasticsearch.cluster.routing.ShardRouting;
-import org.elasticsearch.cluster.service.ClusterService;
-import org.apache.logging.log4j.LogManager;
-import org.elasticsearch.common.unit.TimeValue;
-import org.elasticsearch.common.util.concurrent.ThreadContext;
-import org.elasticsearch.index.IndexNotFoundException;
-import org.elasticsearch.index.IndexService;
-import org.elasticsearch.index.shard.IndexShard;
-import org.elasticsearch.index.shard.ShardId;
-import org.elasticsearch.index.shard.ShardNotFoundException;
-import org.elasticsearch.indices.IndicesService;
-
-import javax.annotation.Nullable;
-import java.util.concurrent.ExecutorService;
-import java.util.function.Function;
-
-/**
- * This component will collect data from a remote node when the provided shard is not available on the local node due to
- * a relocation.
- * In case the cluster state does not yet reflect the fact that the target shard was relocated (ie. it is closed locally
- * but it's routing status is still RELOCATING) it will wait for the shard to get started and then start collecting
- * (remote collecting in case the shard relocated to a different node or a local collection if the shard ended up back
- * on the local node)
- */
-public class ShardStateAwareRemoteCollector {
-
-    private static final Logger LOGGER = LogManager.getLogger(ShardStateAwareRemoteCollector.class);
-    private static final TimeValue STATE_OBSERVER_WAIT_FOR_CHANGE_TIMEOUT = new TimeValue(60000);
-
-    private final String localNodeId;
-    private final RowConsumer consumer;
-    private final ClusterService clusterService;
-    private final ShardId shardId;
-    private final String indexName;
-    private final IndicesService indicesService;
-    private final Function<IndexShard, BatchIterator<Row>> localCollectorProvider;
-    private final Function<String, RemoteCollector> remoteCollectorProvider;
-    private final ExecutorService executorService;
-    private final ThreadContext threadContext;
-    private final Object killLock = new Object();
-
-    private boolean collectorKilled = false;
-    private RemoteCollector remoteCollector;
-    private BatchIterator<Row> batchIterator;
-
-    public ShardStateAwareRemoteCollector(ShardId shardId,
-                                          RowConsumer consumer,
-                                          ClusterService clusterService,
-                                          IndicesService indicesService,
-                                          Function<IndexShard, BatchIterator<Row>> localCollectorProvider,
-                                          Function<String, RemoteCollector> remoteCollectorProvider,
-                                          ExecutorService executorService,
-                                          ThreadContext threadContext) {
-        this.indexName = shardId.getIndexName();
-        this.shardId = shardId;
-        this.consumer = consumer;
-        this.clusterService = clusterService;
-        this.localNodeId = clusterService.localNode().getId();
-        this.indicesService = indicesService;
-        this.localCollectorProvider = localCollectorProvider;
-        this.remoteCollectorProvider = remoteCollectorProvider;
-        this.executorService = executorService;
-        this.threadContext = threadContext;
-    }
-
-    public void doCollect() {
-        ClusterState clusterState = clusterService.state();
-        checkStateAndCollect(clusterState);
-    }
-
-    private void checkStateAndCollect(ClusterState clusterState) {
-        IndexShardRoutingTable shardRoutings;
-        try {
-            shardRoutings = clusterState.routingTable().shardRoutingTable(indexName, shardId.getId());
-        } catch (ShardNotFoundException e) {
-            addClusterStateObserverWaitingForNewShardState(clusterState);
-            return;
-        } catch (IndexNotFoundException e) {
-            consumer.accept(null, e);
-            return;
-        }
-
-        // for update operations primaryShards must be used
-        // (for others that wouldn't be the case, but at this point it is not easily visible which is the case)
-        ShardRouting shardRouting = shardRoutings.primaryShard();
-        String remoteNodeId = shardRouting.currentNodeId();
-        if (localNodeId.equals(remoteNodeId) == false) {
-            triggerRemoteCollect(remoteNodeId);
-        } else if (shardRouting.started()) {
-            triggerLocalCollect(clusterState);
-        } else {
-            addClusterStateObserverWaitingForNewShardState(clusterState);
-        }
-    }
-
-    private void triggerRemoteCollect(String remoteNodeId) {
-        synchronized (killLock) {
-            if (collectorKilled == false) {
-                remoteCollector = remoteCollectorProvider.apply(remoteNodeId);
-            } else {
-                consumer.accept(null, new InterruptedException());
-            }
-        }
-        // collect outside the synchronized block as the collectors handle kill signals that arrive while collecting
-        if (remoteCollector != null) {
-            remoteCollector.doCollect();
-        }
-    }
-
-    private void triggerLocalCollect(ClusterState clusterState) {
-        // The shard is on the local node, so don't create a remote collector but a local one.
-        // We might've arrived here as a result of the cluster state observer detecting the shard relocated, in
-        // which case the ClusterState-Update thread is running this.
-        // The creation of a ShardCollectorProvider accesses the clusterState, which leads to an assertionError
-        // if accessed within a ClusterState-Update thread.
-
-        executorService.submit(() -> {
-            IndexMetaData indexMetaData = clusterState.metaData().index(indexName);
-            if (indexMetaData != null) {
-                IndexService indexShards = indicesService.indexService(indexMetaData.getIndex());
-                if (indexShards == null) {
-                    consumer.accept(null, new IndexNotFoundException(indexName));
-                    return;
-                }
-                try {
-                    synchronized (killLock) {
-                        if (collectorKilled == false) {
-                            IndexShard indexShard = indexShards.getShard(shardId.getId());
-                            batchIterator = localCollectorProvider.apply(indexShard);
-                        } else {
-                            consumer.accept(null, new InterruptedException());
-                            return;
-                        }
-                    }
-
-                    consumer.accept(batchIterator, null);
-                } catch (Exception e) {
-                    consumer.accept(null, e);
-                }
-            } else {
-                consumer.accept(null, new IndexNotFoundException(indexName));
-            }
-        });
-    }
-
-
-    private void addClusterStateObserverWaitingForNewShardState(ClusterState clusterState) {
-        if (LOGGER.isWarnEnabled()) {
-            LOGGER.warn("Waiting for primary shard {} to be started.", shardId);
-        }
-
-        ClusterStateObserver clusterStateObserver = new ClusterStateObserver(clusterState, clusterService,
-            STATE_OBSERVER_WAIT_FOR_CHANGE_TIMEOUT, LOGGER, threadContext);
-        clusterStateObserver.waitForNextChange(getClusterStateListener(),
-            newState -> {
-                try {
-                    return newState.routingTable().shardRoutingTable(indexName, shardId.getId()).primaryShard().started();
-                } catch (ShardNotFoundException e) {
-                    return false;
-                } catch (IndexNotFoundException e) {
-                    // we'll handle this as a index deleted change
-                    return true;
-                }
-            });
-    }
-
-    private ClusterStateObserver.Listener getClusterStateListener() {
-        return new ClusterStateObserver.Listener() {
-            @Override
-            public void onNewClusterState(ClusterState state) {
-                checkStateAndCollect(state);
-            }
-
-            @Override
-            public void onClusterServiceClose() {
-                consumer.accept(null,
-                    new InterruptedException("The cluster service was closed while waiting for shard " +
-                                             shardId + " to start"));
-            }
-
-            @Override
-            public void onTimeout(TimeValue timeout) {
-                consumer.accept(null, new IllegalStateException(
-                    "Timed out waiting for shard " + shardId + " to start"));
-            }
-        };
-    }
-
-    public void kill(@Nullable Throwable throwable) {
-        synchronized (killLock) {
-            collectorKilled = true;
-            if (remoteCollector != null) {
-                remoteCollector.kill(throwable);
-            }
-            if (batchIterator != null) {
-                batchIterator.kill(throwable);
-            }
-        }
-    }
-}

commit 1f716c0f6963fd6c96547f63f303f7a101cd57a8

Fix regression in RemoteCollectorFactory
The ShardStateObserver returned too early in some cases, which could
lead to a ShardNotFoundException in the following local collect.

The decommission tests were flaky because of this.
Regression was introduced in bef03ab8996071e6bf430758e55b191531a8eff8

sql/src/main/java/io/crate/execution/engine/collect/RemoteCollectorFactory.java
@@ -124,7 +124,8 @@ public class RemoteCollectorFactory {
         String nodeId = activePrimaryRouting.currentNodeId();
         String localNodeId = clusterService.localNode().getId();
         if (localNodeId.equalsIgnoreCase(nodeId)) {
-            var indexShard = indicesService.indexServiceSafe(activePrimaryRouting.index()).getShard(activePrimaryRouting.shardId().id());
+            var indexShard = indicesService.indexServiceSafe(activePrimaryRouting.index())
+                .getShard(activePrimaryRouting.shardId().id());
             var collectorProvider = shardCollectorProviderFactory.create(indexShard);
             BatchIterator<Row> it;
             try {

sql/src/main/java/io/crate/execution/engine/collect/collectors/ShardStateObserver.java
@@ -51,11 +51,14 @@ public final class ShardStateObserver {
     }
 
     public CompletableFuture<ShardRouting> waitForActiveShard(ShardId shardId) {
-        ClusterState state = clusterService.state();
+        return checkStateOrWaitForActiveShard(shardId, clusterService.state());
+    }
+
+    private CompletableFuture<ShardRouting> checkStateOrWaitForActiveShard(ShardId shardId, ClusterState state) {
         try {
             var routingTable = state.routingTable().shardRoutingTable(shardId);
             var primaryShardRouting = routingTable.primaryShard();
-            if (primaryShardRouting.active() || primaryShardRouting.started()) {
+            if (primaryShardRouting.started()) {
                 return CompletableFuture.completedFuture(primaryShardRouting);
             } else {
                 return waitForActiveShard(shardId, state);
@@ -71,14 +74,13 @@ public final class ShardStateObserver {
         var stateObserver = new ClusterStateObserver(
             state, clusterService, MAX_WAIT_TIME_FOR_NEW_STATE, LOGGER, threadContext);
         var listener = new RetryIsShardActive(shardId);
-        stateObserver.waitForNextChange(listener, newState -> shardBecameActiveOrIndexWasDeleted(newState, shardId));
+        stateObserver.waitForNextChange(listener, newState -> shardStartedOrIndexDeleted(newState, shardId));
         return listener.result();
     }
 
-    private static boolean shardBecameActiveOrIndexWasDeleted(ClusterState state, ShardId shardId) {
+    private static boolean shardStartedOrIndexDeleted(ClusterState state, ShardId shardId) {
         try {
-            var routingTable = state.routingTable().shardRoutingTable(shardId);
-            return routingTable.primaryShard().started() || routingTable.primaryShard().active();
+            return state.routingTable().shardRoutingTable(shardId).primaryShard().started();
         } catch (ShardNotFoundException e) {
             return false;
         } catch (IndexNotFoundException e) {
@@ -98,7 +100,7 @@ public final class ShardStateObserver {
 
         @Override
         public void onNewClusterState(ClusterState state) {
-            waitForActiveShard(shardId).whenComplete((routingTable, err) -> {
+            checkStateOrWaitForActiveShard(shardId, state).whenComplete((routingTable, err) -> {
                 if (routingTable == null) {
                     result.completeExceptionally(err);
                 } else {