5cefa06f379b7f1eb6a55def9236423e03463491
Mathias Fussenegger <f.mathias@zignar.net>
2019-10-15 14:56:00 +0200

    Expose `indices.breaker.total.limit`
    We documented the setting in https://github.com/crate/crate/pull/8709
    but it couldn't be changed using `SET GLOBAL`.
    
    (cherry picked from commit 021335400b8dcfc6c797a7f29e3acf3da1da9f13)

--- sql/src/test/java/io/crate/integrationtests/InformationSchemaTest.java
+++ sql/src/test/java/io/crate/integrationtests/InformationSchemaTest.java
@@ -586,7 +586,7 @@ public class InformationSchemaTest extends SQLTransportIntegrationTest {
     @Test
     public void testDefaultColumns() {
         execute("select * from information_schema.columns order by table_schema, table_name");
-        assertEquals(662, response.rowCount());
+        assertEquals(664, response.rowCount());
     }
 
     @Test
