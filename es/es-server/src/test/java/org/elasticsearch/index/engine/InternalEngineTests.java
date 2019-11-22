dc08f36ce998f811b86a19f14cce53e7475471de
marregui <miguel.arregui@gmail.com>
2019-11-22 13:03:39 +0100

    Disable merges in testRebuildLocalCheckpointTracker
    In this test, we keep track of a list of index commits then verify that
    we reload exactly every operation from the safe commit. If a background
    merge is triggered, then we might have a new index commit which is not
    recorded in the tracking list. This change disables merges in the test.
    
    Port of ES https://github.com/elastic/elasticsearch/commit/1ae53e6b20771fb29dec0095dc3a996e28540c08
    which addresses https://github.com/elastic/elasticsearch/issues/36470.
    
    (cherry picked from commit 628c5fdca926e9867af5966a3e4d149a6ced954c)

--- es/es-server/src/test/java/org/elasticsearch/index/engine/InternalEngineTests.java
+++ es/es-server/src/test/java/org/elasticsearch/index/engine/InternalEngineTests.java
@@ -5048,24 +5048,7 @@ public class InternalEngineTests extends EngineTestCase {
         final IndexSettings indexSettings = IndexSettingsModule.newIndexSettings(indexMetaData);
         final AtomicLong globalCheckpoint = new AtomicLong(SequenceNumbers.NO_OPS_PERFORMED);
         Path translogPath = createTempDir();
-        int numOps = scaledRandomIntBetween(1, 500);
-        List<Engine.Operation> operations = new ArrayList<>();
-        for (int i = 0; i < numOps; i++) {
-            long seqNo = i;
-            final ParsedDocument doc = EngineTestCase.createParsedDoc(Integer.toString(between(1, 100)), null);
-            if (randomBoolean()) {
-                operations.add(new Engine.Index(EngineTestCase.newUid(doc), doc, seqNo, primaryTerm.get(),
-                                                i, null, Engine.Operation.Origin.REPLICA, threadPool.relativeTimeInMillis(), -1, true, UNASSIGNED_SEQ_NO, 0L));
-            } else if (randomBoolean()) {
-                operations.add(new Engine.Delete("default", doc.id(), EngineTestCase.newUid(doc), seqNo, primaryTerm.get(),
-                                                 i, null, Engine.Operation.Origin.REPLICA, threadPool.relativeTimeInMillis(),
-                                                 UNASSIGNED_SEQ_NO, 0L));
-            } else {
-                operations.add(new Engine.NoOp(seqNo, primaryTerm.get(), Engine.Operation.Origin.REPLICA,
-                                               threadPool.relativeTimeInMillis(), "test-" + i));
-            }
-        }
-        Randomness.shuffle(operations);
+        List<Engine.Operation> operations = generateHistoryOnReplica(between(1, 500), randomBoolean(), randomBoolean());
         List<List<Engine.Operation>> commits = new ArrayList<>();
         commits.add(new ArrayList<>());
         try (Store store = createStore()) {
