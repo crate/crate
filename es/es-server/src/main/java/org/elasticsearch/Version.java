6a163eb943147adb282f81eae8f36623a92cec80
Mathias Fussenegger <f.mathias@zignar.net>
2019-11-25 16:05:39 +0100

    Bump version to 4.0.10-SNAPSHOT

--- es/es-server/src/main/java/org/elasticsearch/Version.java
+++ es/es-server/src/main/java/org/elasticsearch/Version.java
@@ -77,6 +77,7 @@ public class Version implements Comparable<Version>, ToXContentFragment {
     public static final int ES_V_7_0_7_ID = 7_00_07_99;
     public static final int ES_V_7_0_8_ID = 7_00_08_99;
     public static final int ES_V_7_0_9_ID = 7_00_09_99;
+    public static final int ES_V_7_0_10_ID = 7_00_10_99;
     public static final Version V_4_0_0 = new Version(ES_V_7_0_0_ID, ES_V_7_0_0_ID - INTERNAL_OFFSET, org.apache.lucene.util.Version.LUCENE_8_0_0);
     public static final Version V_4_0_1 = new Version(ES_V_7_0_1_ID, ES_V_7_0_1_ID - INTERNAL_OFFSET, org.apache.lucene.util.Version.LUCENE_8_0_0);
     public static final Version V_4_0_2 = new Version(ES_V_7_0_2_ID, ES_V_7_0_2_ID - INTERNAL_OFFSET, org.apache.lucene.util.Version.LUCENE_8_0_0);
@@ -87,8 +88,9 @@ public class Version implements Comparable<Version>, ToXContentFragment {
     public static final Version V_4_0_7 = new Version(ES_V_7_0_7_ID, ES_V_7_0_7_ID - INTERNAL_OFFSET, org.apache.lucene.util.Version.LUCENE_8_0_0);
     public static final Version V_4_0_8 = new Version(ES_V_7_0_8_ID, ES_V_7_0_8_ID - INTERNAL_OFFSET, org.apache.lucene.util.Version.LUCENE_8_0_0);
     public static final Version V_4_0_9 = new Version(ES_V_7_0_9_ID, ES_V_7_0_9_ID - INTERNAL_OFFSET, org.apache.lucene.util.Version.LUCENE_8_0_0);
+    public static final Version V_4_0_10 = new Version(ES_V_7_0_10_ID, ES_V_7_0_10_ID - INTERNAL_OFFSET, true, org.apache.lucene.util.Version.LUCENE_8_0_0);
 
-    public static final Version CURRENT = V_4_0_9;
+    public static final Version CURRENT = V_4_0_10;
 
     static {
         assert CURRENT.luceneVersion.equals(org.apache.lucene.util.Version.LATEST) : "Version must be upgraded to ["
@@ -125,6 +127,8 @@ public class Version implements Comparable<Version>, ToXContentFragment {
                 return V_4_0_8;
             case ES_V_7_0_9_ID:
                 return V_4_0_9;
+            case ES_V_7_0_10_ID:
+                return V_4_0_10;
             case V_EMPTY_ID:
                 return V_EMPTY;
             default:
