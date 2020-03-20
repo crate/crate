6a9f8ebc5fefd63f666caa6f28e29b4b214ac7fc
Mathias Fussenegger <f.mathias@zignar.net>
2020-03-20 10:02:43 +0100

    Release 4.1.4

--- docs/appendices/release-notes/unreleased.rst
+++ docs/appendices/release-notes/unreleased.rst
@@ -57,8 +57,4 @@ None
 Fixes
 =====
 
-- Improved the resiliency of ``INSERT INTO ..`` queries that have a table
-  function like ``generate_series`` as a source.
-
-- Fixed a regression introduced in 4.1 that led to a ``ClassCastException``
-  running queries with ``GROUP BY``, no aggregations and a ``LIMIT`` clause.
+None
--- es/es-server/src/main/java/org/elasticsearch/Version.java
+++ es/es-server/src/main/java/org/elasticsearch/Version.java
@@ -102,7 +102,7 @@ public class Version implements Comparable<Version>, ToXContentFragment {
     public static final Version V_4_1_1 = new Version(ES_V_7_1_1_ID, false, org.apache.lucene.util.Version.LUCENE_8_4_0);
     public static final Version V_4_1_2 = new Version(ES_V_7_1_2_ID, false, org.apache.lucene.util.Version.LUCENE_8_4_0);
     public static final Version V_4_1_3 = new Version(ES_V_7_1_3_ID, false, org.apache.lucene.util.Version.LUCENE_8_4_0);
-    public static final Version V_4_1_4 = new Version(ES_V_7_1_4_ID, true, org.apache.lucene.util.Version.LUCENE_8_4_0);
+    public static final Version V_4_1_4 = new Version(ES_V_7_1_4_ID, false, org.apache.lucene.util.Version.LUCENE_8_4_0);
 
     public static final Version CURRENT = V_4_1_4;
 
