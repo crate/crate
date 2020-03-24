4078622d083d61c93a1b29c9a229c066e8b3e31a
Mathias Fussenegger <f.mathias@zignar.net>
2020-03-24 10:51:47 +0100

    Revert "Update minimum wire compat version to 4.1"
    This reverts commit 06f8f2e1acf0c60ac839bd2427a31d91043fcbaf.
    
    It broke the rolling upgrade tests because nodes use their
    `minimumCompatbilityVersion` initially in the handshake. See
    
    https://github.com/crate/crate/blob/master/es/es-server/src/main/java/org/elasticsearch/transport/TcpTransport.java#L1384
    
    So a 4.1 node sends 4.0 as version.

--- es/es-server/src/main/java/org/elasticsearch/Version.java
+++ es/es-server/src/main/java/org/elasticsearch/Version.java
@@ -336,21 +336,14 @@ public class Version implements Comparable<Version>, ToXContentFragment {
     }
 
     /**
-     * The minimum version other nodes need to have to communicate with this node.
-     *
-     * We support rolling upgrades between successive versions.
-     * So:
-     *
-     * <pre>
-     *  current | min support
-     *      4.1 |         4.0
-     *      4.2 |         4.1
-     *      ...
-     *      5.0 |         4.x where x is the last within 4.
-     * </pre>
+     * Returns the minimum compatible version based on the current
+     * version. Ie a node needs to have at least the return version in order
+     * to communicate with a node running the current version. The returned version
+     * is in most of the cases the smallest major version release unless the current version
+     * is a beta or RC release then the version itself is returned.
      */
     public Version minimumCompatibilityVersion() {
-        return V_4_1_0;
+        return V_4_0_0;
     }
 
     /**
