5ac9d6fa95f840cbd215e8f5d5daf6d33e6a3973
Ruslan Kovalov <ruslan@crate.io>
2020-04-02 16:10:14 +0200

    Refactor min/max agg functions to use new function registry.

--- sql/src/main/java/io/crate/execution/engine/aggregation/impl/MaximumAggregation.java
+++ sql/src/main/java/io/crate/execution/engine/aggregation/impl/MaximumAggregation.java
@@ -21,7 +21,6 @@
 
 package io.crate.execution.engine.aggregation.impl;
 
-import com.google.common.collect.ImmutableList;
 import io.crate.breaker.RamAccounting;
 import io.crate.breaker.SizeEstimator;
 import io.crate.breaker.SizeEstimatorFactory;
@@ -30,6 +29,7 @@ import io.crate.memory.MemoryManager;
 import io.crate.metadata.FunctionIdent;
 import io.crate.metadata.FunctionInfo;
 import io.crate.execution.engine.aggregation.AggregationFunction;
+import io.crate.metadata.functions.Signature;
 import io.crate.types.DataType;
 import io.crate.types.DataTypes;
 import io.crate.types.FixedWidthType;
@@ -45,15 +45,25 @@ public abstract class MaximumAggregation extends AggregationFunction<Comparable,
     private final FunctionInfo info;
 
     public static void register(AggregationImplModule mod) {
-        for (final DataType dataType : DataTypes.PRIMITIVE_TYPES) {
-            FunctionInfo functionInfo = new FunctionInfo(
-                new FunctionIdent(NAME, ImmutableList.of(dataType)), dataType, FunctionInfo.Type.AGGREGATE);
-
-            if (dataType instanceof FixedWidthType) {
-                mod.register(new FixedMaximumAggregation(functionInfo));
-            } else {
-                mod.register(new VariableMaximumAggregation(functionInfo));
-            }
+        for (var supportedType : DataTypes.PRIMITIVE_TYPES) {
+            var fixedWidthType = supportedType instanceof FixedWidthType;
+            mod.register(
+                Signature.aggregate(
+                    NAME,
+                    supportedType.getTypeSignature(),
+                    supportedType.getTypeSignature()),
+                args -> {
+                    var arg = args.get(0); // f(x) -> x
+                    var info = new FunctionInfo(
+                        new FunctionIdent(NAME, args),
+                        arg,
+                        FunctionInfo.Type.AGGREGATE
+                    );
+                    return fixedWidthType
+                        ? new FixedMaximumAggregation(info)
+                        : new VariableMaximumAggregation(info);
+                }
+            );
         }
     }
 
