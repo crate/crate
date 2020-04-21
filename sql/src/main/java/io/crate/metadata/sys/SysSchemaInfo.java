f487fe74b5e00588a1a0548932e0cb315ed9513b
Mathias Fussenegger <f.mathias@zignar.net>
2020-04-21 11:13:40 +0200

    Migrate SysSegmentsTableInfo to SystemTable

--- sql/src/main/java/io/crate/metadata/sys/SysSchemaInfo.java
+++ sql/src/main/java/io/crate/metadata/sys/SysSchemaInfo.java
@@ -62,7 +62,7 @@ public class SysSchemaInfo implements SchemaInfo {
         tableInfos.put(SysAllocationsTableInfo.IDENT.name(), SysAllocationsTableInfo.create());
         tableInfos.put(SysHealth.IDENT.name(), SysHealth.create());
         tableInfos.put(SysMetricsTableInfo.NAME.name(), SysMetricsTableInfo.create(localNode));
-        tableInfos.put(SysSegmentsTableInfo.IDENT.name(), new SysSegmentsTableInfo(clusterService::localNode));
+        tableInfos.put(SysSegmentsTableInfo.IDENT.name(), SysSegmentsTableInfo.create(clusterService::localNode));
     }
 
     @Override
