8389805cf23b9f686954a9f06749ed5980c00cee
Naomi Slater <23469+mechanomi@users.noreply.github.com>
2020-05-07 15:25:25 +0200

    Prune config (no longer needed)

--- blackbox/docs/conf.py
+++ blackbox/docs/conf.py
@@ -2,7 +2,4 @@ from crate.theme.rtd.conf.crate_reference import *
 
 exclude_patterns = ['out/**', 'tmp/**', 'eggs/**', 'requirements.txt']
 
-extensions = ['crate.sphinx.csv']
-# crate.theme sets html_favicon to favicon.png which causes a warning because it should be a .ico
-# and in addition there is no favicon.png in this project so it can't find the file
-html_favicon = None
+extensions = ['crate.sphinx.csv', 'sphinx_sitemap']
