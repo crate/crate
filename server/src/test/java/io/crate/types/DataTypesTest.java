5c3b9efc1a0e035bb383983de6e10bcf0898e23d
Ruslan Kovalov <ruslan@crate.io>
2020-05-18 16:27:07 +0200

    Add the length limit field to the crate string type.

--- server/src/test/java/io/crate/types/DataTypesTest.java
+++ server/src/test/java/io/crate/types/DataTypesTest.java
@@ -278,6 +278,26 @@ public class DataTypesTest extends CrateUnitTest {
         assertThat(DataTypes.ofName("varchar"), is(DataTypes.STRING));
     }
 
+    public void test_text_with_length_limit() throws Exception {
+        var stringDataType = StringType.of(10);
+        assertThat(stringDataType.unbound(), is(false));
+        assertThat(stringDataType.lengthLimit(), is(10));
+    }
+
+    @Test
+    public void test_text_with_negative_length_limit() throws Exception {
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage("The text type length must be at least 1, received: -1");
+        StringType.of(-1);
+    }
+
+    @Test
+    public void test_crate_text_type_with_wrong_number_of_parameters_throws_exception() {
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage("The text type can only have a single parameter value, received: 2");
+        StringType.of(List.of(1, 2));
+    }
+
     @Test
     public void testInt4IsAliasedToInteger() {
         assertThat(DataTypes.ofName("int4"), is(DataTypes.INTEGER));
