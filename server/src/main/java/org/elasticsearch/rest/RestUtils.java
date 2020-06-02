69dc35b62ba043e11b299038a605d5782e77c543
Mathias Fussenegger <f.mathias@zignar.net>
2020-06-02 17:13:32 +0200

    Remove unused PathTrie

--- server/src/main/java/org/elasticsearch/rest/RestUtils.java
+++ server/src/main/java/org/elasticsearch/rest/RestUtils.java
@@ -20,203 +20,11 @@
 package org.elasticsearch.rest;
 
 import org.elasticsearch.common.Strings;
-import org.elasticsearch.common.path.PathTrie;
 
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
-import java.util.Map;
 import java.util.regex.Pattern;
 
 public class RestUtils {
-
-    public static final PathTrie.Decoder REST_DECODER = new PathTrie.Decoder() {
-        @Override
-        public String decode(String value) {
-            return RestUtils.decodeComponent(value);
-        }
-    };
-
-    public static void decodeQueryString(String s, int fromIndex, Map<String, String> params) {
-        if (fromIndex < 0) {
-            return;
-        }
-        if (fromIndex >= s.length()) {
-            return;
-        }
-
-        int queryStringLength = s.contains("#") ? s.indexOf("#") : s.length();
-
-        String name = null;
-        int pos = fromIndex; // Beginning of the unprocessed region
-        int i;       // End of the unprocessed region
-        char c = 0;  // Current character
-        for (i = fromIndex; i < queryStringLength; i++) {
-            c = s.charAt(i);
-            if (c == '=' && name == null) {
-                if (pos != i) {
-                    name = decodeComponent(s.substring(pos, i));
-                }
-                pos = i + 1;
-            } else if (c == '&' || c == ';') {
-                if (name == null && pos != i) {
-                    // We haven't seen an `=' so far but moved forward.
-                    // Must be a param of the form '&a&' so add it with
-                    // an empty value.
-                    addParam(params, decodeComponent(s.substring(pos, i)), "");
-                } else if (name != null) {
-                    addParam(params, name, decodeComponent(s.substring(pos, i)));
-                    name = null;
-                }
-                pos = i + 1;
-            }
-        }
-
-        if (pos != i) {  // Are there characters we haven't dealt with?
-            if (name == null) {     // Yes and we haven't seen any `='.
-                addParam(params, decodeComponent(s.substring(pos, i)), "");
-            } else {                // Yes and this must be the last value.
-                addParam(params, name, decodeComponent(s.substring(pos, i)));
-            }
-        } else if (name != null) {  // Have we seen a name without value?
-            addParam(params, name, "");
-        }
-    }
-
-    private static void addParam(Map<String, String> params, String name, String value) {
-        params.put(name, value);
-    }
-
-    /**
-     * Decodes a bit of an URL encoded by a browser.
-     * <p>
-     * This is equivalent to calling {@link #decodeComponent(String, Charset)}
-     * with the UTF-8 charset (recommended to comply with RFC 3986, Section 2).
-     *
-     * @param s The string to decode (can be empty).
-     * @return The decoded string, or {@code s} if there's nothing to decode.
-     *         If the string to decode is {@code null}, returns an empty string.
-     * @throws IllegalArgumentException if the string contains a malformed
-     *                                  escape sequence.
-     */
-    public static String decodeComponent(final String s) {
-        return decodeComponent(s, StandardCharsets.UTF_8);
-    }
-
-    /**
-     * Decodes a bit of an URL encoded by a browser.
-     * <p>
-     * The string is expected to be encoded as per RFC 3986, Section 2.
-     * This is the encoding used by JavaScript functions {@code encodeURI}
-     * and {@code encodeURIComponent}, but not {@code escape}.  For example
-     * in this encoding, &eacute; (in Unicode {@code U+00E9} or in UTF-8
-     * {@code 0xC3 0xA9}) is encoded as {@code %C3%A9} or {@code %c3%a9}.
-     * <p>
-     * This is essentially equivalent to calling
-     * <code>{@link java.net.URLDecoder URLDecoder}.{@link
-     * java.net.URLDecoder#decode(String, String)}</code>
-     * except that it's over 2x faster and generates less garbage for the GC.
-     * Actually this function doesn't allocate any memory if there's nothing
-     * to decode, the argument itself is returned.
-     *
-     * @param s       The string to decode (can be empty).
-     * @param charset The charset to use to decode the string (should really
-     *                be {@link StandardCharsets#UTF_8}.
-     * @return The decoded string, or {@code s} if there's nothing to decode.
-     *         If the string to decode is {@code null}, returns an empty string.
-     * @throws IllegalArgumentException if the string contains a malformed
-     *                                  escape sequence.
-     */
-    public static String decodeComponent(final String s, final Charset charset) {
-        if (s == null) {
-            return "";
-        }
-        final int size = s.length();
-        if (!decodingNeeded(s, size)) {
-            return s;
-        }
-        final byte[] buf = new byte[size];
-        int pos = decode(s, size, buf);
-        return new String(buf, 0, pos, charset);
-    }
-
-    @SuppressWarnings("fallthrough")
-    private static boolean decodingNeeded(String s, int size) {
-        boolean decodingNeeded = false;
-        for (int i = 0; i < size; i++) {
-            final char c = s.charAt(i);
-            switch (c) {
-                case '%':
-                    i++;  // We can skip at least one char, e.g. `%%'.
-                    // fall through.
-                case '+':
-                    decodingNeeded = true;
-                    break;
-
-                default:
-                    break;
-            }
-        }
-        return decodingNeeded;
-    }
-
-    @SuppressWarnings("fallthrough")
-    private static int decode(String s, int size, byte[] buf) {
-        int pos = 0;  // position in `buf'.
-        for (int i = 0; i < size; i++) {
-            char c = s.charAt(i);
-            switch (c) {
-                case '+':
-                    buf[pos++] = ' ';  // "+" -> " "
-                    break;
-                case '%':
-                    if (i == size - 1) {
-                        throw new IllegalArgumentException("unterminated escape sequence at end of string: " + s);
-                    }
-                    c = s.charAt(++i);
-                    if (c == '%') {
-                        buf[pos++] = '%';  // "%%" -> "%"
-                        break;
-                    } else if (i == size - 1) {
-                        throw new IllegalArgumentException("partial escape sequence at end of string: " + s);
-                    }
-                    c = decodeHexNibble(c);
-                    final char c2 = decodeHexNibble(s.charAt(++i));
-                    if (c == Character.MAX_VALUE || c2 == Character.MAX_VALUE) {
-                        throw new IllegalArgumentException(
-                            "invalid escape sequence `%" + s.charAt(i - 1)
-                                + s.charAt(i) + "' at index " + (i - 2)
-                                + " of: " + s);
-                    }
-                    c = (char) (c * 16 + c2);
-                    // fall through.
-                default:
-                    buf[pos++] = (byte) c;
-                    break;
-            }
-        }
-        return pos;
-    }
-
-    /**
-     * Helper to decode half of a hexadecimal number from a string.
-     *
-     * @param c The ASCII character of the hexadecimal number to decode.
-     *          Must be in the range {@code [0-9a-fA-F]}.
-     * @return The hexadecimal value represented in the ASCII character
-     *         given, or {@link Character#MAX_VALUE} if the character is invalid.
-     */
-    private static char decodeHexNibble(final char c) {
-        if ('0' <= c && c <= '9') {
-            return (char) (c - '0');
-        } else if ('a' <= c && c <= 'f') {
-            return (char) (c - 'a' + 10);
-        } else if ('A' <= c && c <= 'F') {
-            return (char) (c - 'A' + 10);
-        } else {
-            return Character.MAX_VALUE;
-        }
-    }
 
     /**
      * Determine if CORS setting is a regex
@@ -233,7 +41,6 @@ public class RestUtils {
         if (isRegex) {
             return Pattern.compile(corsSetting.substring(1, corsSetting.length() - 1));
         }
-
         return null;
     }
 
@@ -248,9 +55,8 @@ public class RestUtils {
         if (Strings.isNullOrEmpty(corsSetting)) {
             return new String[0];
         }
-        return Arrays.asList(corsSetting.split(","))
-                     .stream()
+        return Arrays.stream(corsSetting.split(","))
                      .map(String::trim)
-                     .toArray(size -> new String[size]);
+                     .toArray(String[]::new);
     }
 }
