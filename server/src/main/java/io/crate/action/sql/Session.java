crate/crate

commit 4952ae49317d8089ed5fa3b2580e24d4dd07f45a

Send transaction-state based on BEGIN/COMMIT in ready-for-query messages
Solves https://github.com/crate/crate/issues/10154

server/src/main/java/io/crate/action/sql/Session.java
@@ -39,6 +39,7 @@ import org.elasticsearch.cluster.ClusterState;
 import org.elasticsearch.common.Randomness;
 
 import io.crate.analyze.AnalyzedBegin;
+import io.crate.analyze.AnalyzedCommit;
 import io.crate.analyze.AnalyzedDeallocate;
 import io.crate.analyze.AnalyzedDiscard;
 import io.crate.analyze.AnalyzedStatement;
@@ -69,6 +70,7 @@ import io.crate.protocols.postgres.FormatCodes;
 import io.crate.protocols.postgres.JobsLogsUpdateListener;
 import io.crate.protocols.postgres.Portal;
 import io.crate.protocols.postgres.RetryOnFailureResultReceiver;
+import io.crate.protocols.postgres.TransactionState;
 import io.crate.sql.parser.SqlParser;
 import io.crate.sql.tree.DiscardStatement.Target;
 import io.crate.sql.tree.Statement;
@@ -132,6 +134,8 @@ public class Session implements AutoCloseable {
     private final JobsLogs jobsLogs;
     private final boolean isReadOnly;
     private final ParameterTypeExtractor parameterTypeExtractor;
+
+    private TransactionState currentTransactionState = TransactionState.IDLE;
 
     public Session(Analyzer analyzer,
                    Planner planner,
@@ -368,6 +372,10 @@ public class Session implements AutoCloseable {
             throw new ReadOnlyException(portal.preparedStmt().rawStatement());
         }
         if (analyzedStmt instanceof AnalyzedBegin) {
+            currentTransactionState = TransactionState.IN_TRANSACTION;
+            resultReceiver.allFinished(false);
+        } else if (analyzedStmt instanceof AnalyzedCommit) {
+            currentTransactionState = TransactionState.IDLE;
             resultReceiver.allFinished(false);
         } else if (analyzedStmt instanceof AnalyzedDeallocate) {
             String stmtToDeallocate = ((AnalyzedDeallocate) analyzedStmt).preparedStmtName();
@@ -686,6 +694,7 @@ public class Session implements AutoCloseable {
 
     @Override
     public void close() {
+        currentTransactionState = TransactionState.IDLE;
         resetDeferredExecutions();
         activeExecution = null;
         for (Portal portal : portals.values()) {
@@ -707,5 +716,9 @@ public class Session implements AutoCloseable {
             }
         }
         deferredExecutionsByStmt.clear();
+    }
+
+    public TransactionState transactionState() {
+        return currentTransactionState;
     }
 }

server/src/main/java/io/crate/protocols/postgres/Messages.java
@@ -130,11 +130,11 @@ public class Messages {
      * block; or 'E' if in a failed transaction block (queries will be
      * rejected until block is ended).
      */
-    static void sendReadyForQuery(Channel channel) {
+    static void sendReadyForQuery(Channel channel, TransactionState transactionState) {
         ByteBuf buffer = channel.alloc().buffer(6);
         buffer.writeByte('Z');
         buffer.writeInt(5);
-        buffer.writeByte('I');
+        buffer.writeByte(transactionState.code());
         ChannelFuture channelFuture = channel.writeAndFlush(buffer);
         if (LOGGER.isTraceEnabled()) {
             channelFuture.addListener((ChannelFutureListener) future -> LOGGER.trace("sentReadyForQuery"));

server/src/main/java/io/crate/protocols/postgres/PostgresWireProtocol.java
@@ -272,9 +272,11 @@ public class PostgresWireProtocol {
 
     private static class ReadyForQueryCallback implements BiConsumer<Object, Throwable> {
         private final Channel channel;
+        private final TransactionState transactionState;
 
-        private ReadyForQueryCallback(Channel channel) {
+        private ReadyForQueryCallback(Channel channel, TransactionState transactionState) {
             this.channel = channel;
+            this.transactionState = transactionState;
         }
 
         @Override
@@ -282,7 +284,7 @@ public class PostgresWireProtocol {
             boolean clientInterrupted = t instanceof ClientInterrupted
                                         || (t != null && t.getCause() instanceof ClientInterrupted);
             if (!clientInterrupted) {
-                Messages.sendReadyForQuery(channel);
+                Messages.sendReadyForQuery(channel, transactionState);
             }
         }
     }
@@ -455,7 +457,7 @@ public class PostgresWireProtocol {
         Messages.sendParameterStatus(channel, "datestyle", "ISO");
         Messages.sendParameterStatus(channel, "TimeZone", "UTC");
         Messages.sendParameterStatus(channel, "integer_datetimes", "on");
-        Messages.sendReadyForQuery(channel);
+        Messages.sendReadyForQuery(channel, TransactionState.IDLE);
     }
 
     /**
@@ -639,6 +641,7 @@ public class PostgresWireProtocol {
             resultReceiver = new ResultSetReceiver(
                 query,
                 channel.bypassDelay(),
+                session.transactionState(),
                 SQLExceptions.forWireTransmission(getAccessControl.apply(session.sessionContext())),
                 Lists2.map(outputTypes, PGTypes::get),
                 session.getResultFormatCodes(portalName)
@@ -679,15 +682,15 @@ public class PostgresWireProtocol {
             //  4) p, b, e    -> We've a new query deferred.
             //  5) `sync`     -> We must execute the query from 4, but not 1)
             session.resetDeferredExecutions();
-            Messages.sendReadyForQuery(channel);
+            Messages.sendReadyForQuery(channel, TransactionState.FAILED_TRANSACTION);
             return;
         }
         try {
-            ReadyForQueryCallback readyForQueryCallback = new ReadyForQueryCallback(channel);
+            ReadyForQueryCallback readyForQueryCallback = new ReadyForQueryCallback(channel, session.transactionState());
             session.sync().whenComplete(readyForQueryCallback);
         } catch (Throwable t) {
             Messages.sendErrorResponse(channel, t);
-            Messages.sendReadyForQuery(channel);
+            Messages.sendReadyForQuery(channel, TransactionState.FAILED_TRANSACTION);
         }
     }
 
@@ -712,7 +715,7 @@ public class PostgresWireProtocol {
         for (String query : queries) {
             composedFuture = composedFuture.thenCompose(result -> handleSingleQuery(query, channel));
         }
-        composedFuture.whenComplete(new ReadyForQueryCallback(channel));
+        composedFuture.whenComplete(new ReadyForQueryCallback(channel, TransactionState.IDLE));
     }
 
     private CompletableFuture<?> handleSingleQuery(String query, DelayableWriteChannel channel) {
@@ -742,6 +745,7 @@ public class PostgresWireProtocol {
                 ResultSetReceiver resultSetReceiver = new ResultSetReceiver(
                     query,
                     channel.bypassDelay(),
+                    TransactionState.IDLE,
                     wrapError,
                     Lists2.map(fields, x -> PGTypes.get(x.valueType())),
                     null

server/src/main/java/io/crate/protocols/postgres/ResultSetReceiver.java
@@ -39,6 +39,7 @@ class ResultSetReceiver extends BaseResultReceiver {
     private final Channel channel;
     private final Function<Throwable, Exception> wrapError;
     private final List<PGType<?>> columnTypes;
+    private final TransactionState transactionState;
 
     @Nullable
     private final FormatCodes.FormatCode[] formatCodes;
@@ -47,11 +48,13 @@ class ResultSetReceiver extends BaseResultReceiver {
 
     ResultSetReceiver(String query,
                       Channel channel,
+                      TransactionState transactionState,
                       Function<Throwable, Exception> wrapError,
                       List<PGType<?>> columnTypes,
                       @Nullable FormatCodes.FormatCode[] formatCodes) {
         this.query = query;
         this.channel = channel;
+        this.transactionState = transactionState;
         this.wrapError = wrapError;
         this.columnTypes = columnTypes;
         this.formatCodes = formatCodes;
@@ -69,7 +72,7 @@ class ResultSetReceiver extends BaseResultReceiver {
     @Override
     public void batchFinished() {
         Messages.sendPortalSuspended(channel);
-        Messages.sendReadyForQuery(channel);
+        Messages.sendReadyForQuery(channel, transactionState);
     }
 
     @Override

server/src/main/java/io/crate/protocols/postgres/TransactionState.java
@@ -22,36 +22,19 @@
 
 package io.crate.protocols.postgres;
 
-import io.crate.data.Row1;
-import io.crate.protocols.postgres.types.PGTypes;
-import io.crate.types.DataTypes;
-import io.netty.channel.Channel;
-import org.junit.Test;
-import org.mockito.Answers;
 
-import java.util.Collections;
+public enum TransactionState {
+    IDLE('I'),
+    IN_TRANSACTION('T'),
+    FAILED_TRANSACTION('E');
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+    private final char code;
 
-public class ResultSetReceiverTest {
+    private TransactionState(char code) {
+        this.code = code;
+    }
 
-    @Test
-    public void testChannelIsPeriodicallyFlushedToAvoidConsumingTooMuchMemory() {
-        Channel channel = mock(Channel.class, Answers.RETURNS_DEEP_STUBS);
-        ResultSetReceiver resultSetReceiver = new ResultSetReceiver(
-            "select * from t",
-            channel,
-            TransactionState.IDLE,
-            RuntimeException::new,
-            Collections.singletonList(PGTypes.get(DataTypes.INTEGER)),
-            null
-        );
-        Row1 row1 = new Row1(1);
-        for (int i = 0; i < 1500; i++) {
-            resultSetReceiver.setNextRow(row1);
-        }
-        verify(channel, times(1)).flush();
+    public int code() {
+        return (int) code;
     }
 }

server/src/test/java/io/crate/analyze/SetAnalyzerTest.java
@@ -22,24 +22,23 @@
 
 package io.crate.analyze;
 
+import static io.crate.testing.SymbolMatchers.isLiteral;
+import static org.hamcrest.Matchers.is;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Test;
+
 import io.crate.expression.symbol.Literal;
 import io.crate.sql.tree.Assignment;
 import io.crate.sql.tree.SetStatement;
 import io.crate.sql.tree.SetTransactionStatement;
 import io.crate.test.integration.CrateDummyClusterServiceUnitTest;
 import io.crate.testing.SQLExecutor;
-
-import org.hamcrest.Matchers;
-import org.junit.Before;
-import org.junit.Test;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import static io.crate.testing.SymbolMatchers.isLiteral;
-import static org.hamcrest.Matchers.instanceOf;
-import static org.hamcrest.Matchers.is;
 
 public class SetAnalyzerTest extends CrateDummyClusterServiceUnitTest {
 

server/src/test/java/io/crate/integrationtests/PostgresITest.java
@@ -263,8 +263,6 @@ public class PostgresITest extends SQLTransportIntegrationTest {
                 statement.executeUpdate("refresh table t");
             }
 
-            // Because our ReadyForQuery messages always sends transaction-status IDLE, rollback and commit don't do anything
-            conn.rollback();
             conn.commit();
 
             try (Statement statement = conn.createStatement()) {

server/src/test/java/io/crate/protocols/postgres/PostgresWireProtocolTest.java
@@ -494,6 +494,7 @@ public class PostgresWireProtocolTest extends CrateDummyClusterServiceUnitTest {
         DescribeResult describeResult = mock(DescribeResult.class);
         when(describeResult.getFields()).thenReturn(null);
         when(session.describe(anyChar(), anyString())).thenReturn(describeResult);
+        when(session.transactionState()).thenReturn(TransactionState.IDLE);
 
         PostgresWireProtocol ctx =
             new PostgresWireProtocol(

commit 56cec0168a157ec3718a86ba5154e138b02af82d

Fix stuck query on pg-wire-fetch
The change in https://github.com/crate/crate/commit/4952ae49317d8089ed5fa3b2580e24d4dd07f45a
broke the asyncpg tests and led to stuck queries.

server/src/main/java/io/crate/action/sql/Session.java
@@ -377,6 +377,7 @@ public class Session implements AutoCloseable {
         } else if (analyzedStmt instanceof AnalyzedCommit) {
             currentTransactionState = TransactionState.IDLE;
             resultReceiver.allFinished(false);
+            return resultReceiver.completionFuture();
         } else if (analyzedStmt instanceof AnalyzedDeallocate) {
             String stmtToDeallocate = ((AnalyzedDeallocate) analyzedStmt).preparedStmtName();
             if (stmtToDeallocate != null) {