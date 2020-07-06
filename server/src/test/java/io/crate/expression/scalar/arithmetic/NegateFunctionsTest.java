ee1f26655afbed24ba4d50a624577fe430530cdc
Mathias Fussenegger <f.mathias@zignar.net>
2020-07-06 12:49:10 +0200

    Add pretty printing for negate function

--- server/src/test/java/io/crate/expression/scalar/arithmetic/NegateFunctionsTest.java
+++ server/src/test/java/io/crate/expression/scalar/arithmetic/NegateFunctionsTest.java
@@ -40,7 +40,7 @@ public class NegateFunctionsTest extends AbstractScalarFunctionsTest {
     @Test
     public void testNegateOnStringResultsInError() {
         expectedException.expect(UnsupportedOperationException.class);
-        expectedException.expectMessage("Unknown function: _negate(doc.users.name)," +
+        expectedException.expectMessage("Unknown function: - doc.users.name," +
                                         " no overload found for matching argument types: (text).");
         assertEvaluate("- name", nullValue(), Literal.of("foo"));
     }
