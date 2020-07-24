crate/crate

commit 468ef007fa837696f37380360ab7aae665d5fe4e

Deduplicate insert from values/subquery analyzer logic.
This change deduplicates the ``INSERT INTO ... VALUES ...`` and
``INSERT INTO ... (SELECT ...)`` analyzer logic.

It removes special ``INSERT INTO ... VALUES ...`` analyzer logic
and parts of the dedicated execution logic for the insert from values
case, e.g. `LegacyUpsertByIdTask`, `LegacyUpsertById`. Instead, we rely
on the ``INSERT INTO ... (SELECT ...)`` anayzer and the new
``INSERT INTO ... VALUES ...`` plan.

sql/src/test/java/io/crate/integrationtests/InsertIntoIntegrationTest.java
@@ -24,8 +24,6 @@ package io.crate.integrationtests;
 import com.carrotsearch.randomizedtesting.annotations.Repeat;
 import io.crate.action.sql.SQLActionException;
 import io.crate.exceptions.VersioninigValidationException;
-import io.crate.metadata.PartitionName;
-import io.crate.metadata.RelationName;
 import io.crate.testing.SQLResponse;
 import io.crate.testing.UseJdbc;
 import org.elasticsearch.common.collect.MapBuilder;
@@ -35,7 +33,6 @@ import org.junit.Test;
 import org.locationtech.spatial4j.context.jts.JtsSpatialContext;
 import org.locationtech.spatial4j.shape.impl.PointImpl;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -185,7 +182,7 @@ public class InsertIntoIntegrationTest extends SQLTransportIntegrationTest {
     }
 
     @Test
-    public void testInsertBadIpAdress() throws Exception {
+    public void testInsertBadIPAddress() throws Exception {
         execute("create table t (i ip) with (number_of_replicas=0)");
         ensureYellow();
         expectedException.expect(SQLActionException.class);
@@ -417,7 +414,7 @@ public class InsertIntoIntegrationTest extends SQLTransportIntegrationTest {
         };
 
         expectedException.expect(SQLActionException.class);
-        expectedException.expectMessage("Primary key is required but is missing from the insert statement");
+        expectedException.expectMessage("Column \"pk_col\" is required but is missing from the insert statement");
 
         execute("insert into test (message) values (?)", args);
     }
@@ -985,8 +982,11 @@ public class InsertIntoIntegrationTest extends SQLTransportIntegrationTest {
     @Test
     public void testInsertIntoLongPartitionedBy() throws Exception {
         execute("create table import (col1 int, col2 long primary key) partitioned by (col2)");
-        ensureYellow();
         execute("insert into import (col1, col2) values (1, 1)");
+        assertThat(response.rowCount(), is(1L));
+        refresh();
+        execute("select * from import");
+        assertThat(printedTable(response.rows()), is("1| 1\n"));
     }
 
     @Test
@@ -1041,12 +1041,12 @@ public class InsertIntoIntegrationTest extends SQLTransportIntegrationTest {
                 ") with (number_of_replicas=0)");
         ensureYellow();
         execute("insert into test_generated_column (id, ts) values (?, ?)", new Object[]{
-            1, "2015-11-18T11:11:00"});
+            1, "2015-11-18T11:11:00Z"});
         refresh();
 
         execute("insert into test_generated_column (id, ts) values (?, ?)" +
                 "on conflict (id) do update set ts = ?",
-            new Object[]{1, "2015-11-18T11:11:00", "2015-11-23T14:43:00"});
+            new Object[]{1, "2015-11-18T11:11:00Z", "2015-11-23T14:43:00Z"});
         refresh();
 
         execute("select ts, day from test_generated_column");
@@ -1234,7 +1234,7 @@ public class InsertIntoIntegrationTest extends SQLTransportIntegrationTest {
 
         // wrong value
         expectedException.expect(SQLActionException.class);
-        expectedException.expectMessage("Given value 0 for generated column does not match defined generated expression value 4");
+        expectedException.expectMessage("Given value 0 for generated column col2 does not match calculation (col1 + 3) = 4");
         execute("insert into test(col1, col2) values (1, 0)");
     }
 
@@ -1306,18 +1306,10 @@ public class InsertIntoIntegrationTest extends SQLTransportIntegrationTest {
 
     @Test
     public void testGeneratedColumnAsPrimaryKeyValueEvaluateToNull() throws Exception {
-        // test that correct exception message is thrown
-        execute("create table generated_test (" +
-                " a double," +
-                " b double," +
-                " c double," +
-                " sum as (a+b/c) PRIMARY KEY" +
-                ")");
-        ensureYellow();
-
+        execute("CREATE TABLE test (col1 TEXT, col2 AS try_cast(col1 AS INT) PRIMARY KEY)");
         expectedException.expect(SQLActionException.class);
         expectedException.expectMessage("Primary key value must not be NULL");
-        execute("insert into generated_test (a, c) values (1.0, 3.0)");
+        execute("insert into test (col1) values ('a')");
     }
 
     @Test

enterprise/users/src/main/java/io/crate/auth/user/AccessControlImpl.java
@@ -52,7 +52,6 @@ import io.crate.analyze.AnalyzedDropUser;
 import io.crate.analyze.AnalyzedDropView;
 import io.crate.analyze.ExplainAnalyzedStatement;
 import io.crate.analyze.InsertFromSubQueryAnalyzedStatement;
-import io.crate.analyze.InsertFromValuesAnalyzedStatement;
 import io.crate.analyze.AnalyzedKill;
 import io.crate.analyze.MultiSourceSelect;
 import io.crate.analyze.AnalyzedPrivileges;
@@ -304,17 +303,6 @@ public final class AccessControlImpl implements AccessControl {
         @Override
         protected Void visitAnalyzedDeleteStatement(AnalyzedDeleteStatement delete, User user) {
             visitRelation(delete.relation(), user, Privilege.Type.DML);
-            return null;
-        }
-
-        @Override
-        protected Void visitInsertFromValuesStatement(InsertFromValuesAnalyzedStatement analysis, User user) {
-            Privileges.ensureUserHasPrivilege(
-                Privilege.Type.DML,
-                Privilege.Clazz.TABLE,
-                analysis.tableInfo().ident().toString(),
-                user,
-                defaultSchema);
             return null;
         }
 

sql-parser/src/main/antlr/SqlBase.g4
@@ -435,8 +435,7 @@ objectKeyValue
     ;
 
 insertSource
-   : VALUES  values (',' values)*
-   | query
+   : query
    | '(' query ')'
    ;
 

sql-parser/src/main/java/io/crate/sql/parser/AstBuilder.java
@@ -105,7 +105,6 @@ import io.crate.sql.tree.IndexColumnConstraint;
 import io.crate.sql.tree.IndexDefinition;
 import io.crate.sql.tree.Insert;
 import io.crate.sql.tree.InsertFromSubquery;
-import io.crate.sql.tree.InsertFromValues;
 import io.crate.sql.tree.Intersect;
 import io.crate.sql.tree.IntervalLiteral;
 import io.crate.sql.tree.IsNotNullPredicate;
@@ -561,14 +560,6 @@ class AstBuilder extends SqlBaseBaseVisitor<Node> {
                 }
             }
             throw e;
-        }
-
-        if (context.insertSource().VALUES() != null) {
-            return new InsertFromValues(
-                table,
-                visitCollection(context.insertSource().values(), ValuesList.class),
-                columns,
-                createDuplicateKeyContext(context));
         }
         return new InsertFromSubquery(
             table,

sql-parser/src/main/java/io/crate/sql/tree/AstVisitor.java
@@ -492,10 +492,6 @@ public abstract class AstVisitor<R, C> {
         return visitTableElement(node, context);
     }
 
-    public R visitInsertFromValues(InsertFromValues<?> node, C context) {
-        return visitInsert(node, context);
-    }
-
     public R visitIntervalLiteral(IntervalLiteral node, C context) {
         return visitLiteral(node, context);
     }

sql-parser/src/main/java/io/crate/sql/tree/DefaultTraversalVisitor.java
@@ -278,15 +278,6 @@ public abstract class DefaultTraversalVisitor<R, C> extends AstVisitor<R, C> {
     }
 
     @Override
-    public R visitInsertFromValues(InsertFromValues<?> node, C context) {
-        node.table().accept(this, context);
-        for (ValuesList valuesList : node.valuesLists()) {
-            valuesList.accept(this, context);
-        }
-        return null;
-    }
-
-    @Override
     public R visitValuesList(ValuesList node, C context) {
         for (Expression value : node.values()) {
             value.accept(this, context);

sql-parser/src/main/java/io/crate/sql/tree/InsertFromValues.java
@@ -1,90 +1,0 @@
-/*
- * Licensed to CRATE Technology GmbH ("Crate") under one or more contributor
- * license agreements.  See the NOTICE file distributed with this work for
- * additional information regarding copyright ownership.  Crate licenses
- * this file to you under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.  You may
- * obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
- * License for the specific language governing permissions and limitations
- * under the License.
- *
- * However, if you have executed another commercial license agreement
- * with Crate these terms will supersede the license and you may use the
- * software solely pursuant to the terms of the relevant commercial agreement.
- */
-
-package io.crate.sql.tree;
-
-import com.google.common.base.MoreObjects;
-import com.google.common.base.Objects;
-
-import java.util.List;
-
-public class InsertFromValues<T> extends Insert<T> {
-
-    private final List<ValuesList> valuesLists;
-    private final int maxValuesLength;
-
-    public InsertFromValues(Table<T> table,
-                            List<ValuesList> valuesLists,
-                            List<String> columns,
-                            DuplicateKeyContext<T> duplicateKeyContext) {
-        super(table, columns, duplicateKeyContext);
-        this.valuesLists = valuesLists;
-
-        int i = 0;
-        for (ValuesList valuesList : valuesLists) {
-            i = Math.max(i, valuesList.values().size());
-        }
-        maxValuesLength = i;
-    }
-
-    public List<ValuesList> valuesLists() {
-        return valuesLists;
-    }
-
-    /**
-     * returns the length of the longest values List
-     */
-    public int maxValuesLength() {
-        return maxValuesLength;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(super.hashCode(), valuesLists);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        if (!super.equals(o)) return false;
-
-        InsertFromValues that = (InsertFromValues) o;
-
-        if (!valuesLists.equals(that.valuesLists)) return false;
-
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return MoreObjects.toStringHelper(this)
-            .add("table", table)
-            .add("columns", columns)
-            .add("values", valuesLists)
-            .toString();
-    }
-
-    @Override
-    public <R, C> R accept(AstVisitor<R, C> visitor, C context) {
-        return visitor.visitInsertFromValues(this, context);
-    }
-}

sql-parser/src/test/java/io/crate/sql/parser/TestStatementBuilder.java
@@ -50,7 +50,7 @@ import io.crate.sql.tree.Expression;
 import io.crate.sql.tree.FunctionCall;
 import io.crate.sql.tree.GCDanglingArtifacts;
 import io.crate.sql.tree.GrantPrivilege;
-import io.crate.sql.tree.InsertFromValues;
+import io.crate.sql.tree.InsertFromSubquery;
 import io.crate.sql.tree.KillStatement;
 import io.crate.sql.tree.LongLiteral;
 import io.crate.sql.tree.MatchPredicate;
@@ -1041,7 +1041,7 @@ public class TestStatementBuilder {
         printStatement("insert into t (a, b, c) values (1, 2), (3, 4) on conflict (c) do update set a = excluded.a + 1, b = 4");
         printStatement("insert into t (a, b, c) values (1, 2), (3, 4) on conflict (c) do update set a = excluded.a + 1, b = excluded.b - 2");
 
-        InsertFromValues<Expression> insert = (InsertFromValues<Expression>) SqlParser.createStatement(
+        InsertFromSubquery<Expression> insert = (InsertFromSubquery<Expression>) SqlParser.createStatement(
                 "insert into test_generated_column (id, ts) values (?, ?) on conflict (id) do update set ts = ?");
         Assignment<Expression> onDuplicateAssignment = insert.getDuplicateKeyContext().getAssignments().get(0);
         assertThat(onDuplicateAssignment.expression(), instanceOf(ParameterExpression.class));

commit 6ffda3f13e38273ddc3d1953975734a9876ad6b0

Fix clustered-by column validation for inserts
Fixes a regression introduced in https://github.com/crate/crate/pull/9465

Closes https://github.com/crate/crate/issues/10270

server/src/main/java/io/crate/analyze/InsertAnalyzer.java
@@ -45,6 +45,7 @@ import io.crate.analyze.relations.ParentRelations;
 import io.crate.analyze.relations.RelationAnalyzer;
 import io.crate.analyze.relations.StatementAnalysisContext;
 import io.crate.analyze.relations.select.SelectAnalyzer;
+import io.crate.common.collections.LazyMapList;
 import io.crate.common.collections.Lists2;
 import io.crate.exceptions.ColumnUnknownException;
 import io.crate.expression.eval.EvaluatingNormalizer;
@@ -250,37 +251,36 @@ class InsertAnalyzer {
         return columns;
     }
 
-    private static void ensureClusteredByPresentOrNotRequired(List<Reference> targetColumns, DocTableInfo tableInfo) {
+    private static void ensureClusteredByPresentOrNotRequired(List<Reference> targetColumnRefs, DocTableInfo tableInfo) {
         ColumnIdent clusteredBy = tableInfo.clusteredBy();
-        if (clusteredBy != null &&
-            !clusteredBy.name().equalsIgnoreCase(DocSysColumns.Names.ID) &&
-            !targetColumns.contains(tableInfo.getReference(clusteredBy)) &&
-            !tableInfo.primaryKey().contains(clusteredBy)) {
+        if (clusteredBy == null || clusteredBy.equals(DocSysColumns.ID)) {
+            return;
+        }
+        Reference clusteredByRef = tableInfo.getReference(clusteredBy);
+        if (clusteredByRef.defaultExpression() != null) {
+            return;
+        }
 
-            var clusterByRef = tableInfo.getReference(clusteredBy);
-            if (clusterByRef != null
-                && clusterByRef.defaultExpression() == null
-                && !isGeneratedColumnAndReferencedColumnsArePresent(clusteredBy, tableInfo)) {
-                throw new IllegalArgumentException(
-                    "Clustered by value is required but is missing from the insert statement");
+        // target columns are always top level columns;
+        // so we can compare against clusteredBy-root column in case clustered by is nested
+        // In insert-from-query cases we cannot peek into object values to ensure the value is present
+        // and need to rely on later runtime failures
+        ColumnIdent clusteredByRoot = clusteredBy.getRoot();
+
+        List<ColumnIdent> targetColumns = LazyMapList.of(targetColumnRefs, Reference::column);
+        if (targetColumns.contains(clusteredByRoot)) {
+            return;
+        }
+        if (clusteredByRef instanceof GeneratedReference) {
+            GeneratedReference generatedClusteredBy = (GeneratedReference) clusteredByRef;
+            var topLevelDependencies = LazyMapList.of(generatedClusteredBy.referencedReferences(), x -> x.column().getRoot());
+            if (targetColumns.containsAll(topLevelDependencies)) {
+                return;
             }
         }
-    }
 
-    private static boolean isGeneratedColumnAndReferencedColumnsArePresent(ColumnIdent columnIdent,
-                                                                           DocTableInfo tableInfo) {
-        Reference reference = tableInfo.getReference(columnIdent);
-        if (reference instanceof GeneratedReference) {
-            for (Reference referencedReference : ((GeneratedReference) reference).referencedReferences()) {
-                for (Reference columnRef : tableInfo.columns()) {
-                    if (columnRef.equals(referencedReference) ||
-                        referencedReference.column().isChildOf(columnRef.column())) {
-                        return true;
-                    }
-                }
-            }
-        }
-        return false;
+        throw new IllegalArgumentException(
+            "Column `" + clusteredBy + "` is required but is missing from the insert statement");
     }
 
     private static void checkSourceAndTargetColsForLengthAndTypesCompatibility(

server/src/test/java/io/crate/analyze/InsertAnalyzerTest.java
@@ -87,6 +87,11 @@ public class InsertAnalyzerTest extends CrateDummyClusterServiceUnitTest {
                 "  two int default 1+1" +
                 ")")
             .addTable("create table doc.nested (o object as (id int primary key), x int)")
+            .addTable(
+                "create table doc.nested_clustered (" +
+                "   obj object(strict) as (id int, name text)" +
+                ") clustered by (obj['id'])"
+            )
             .build();
     }
 
@@ -291,7 +296,7 @@ public class InsertAnalyzerTest extends CrateDummyClusterServiceUnitTest {
     @Test
     public void testMissingPrimaryKey() throws Exception {
         expectedException.expect(IllegalArgumentException.class);
-        expectedException.expectMessage("Column \"id\" is required but is missing from the insert statement");
+        expectedException.expectMessage("Column `id` is required but is missing from the insert statement");
         e.analyze("insert into users (name) (select name from users)");
     }
 
@@ -378,7 +383,7 @@ public class InsertAnalyzerTest extends CrateDummyClusterServiceUnitTest {
     @Test
     public void test_insert_from_query_with_missing_clustered_by_column() {
         expectedException.expect(IllegalArgumentException.class);
-        expectedException.expectMessage("Clustered by value is required but is missing from the insert statement");
+        expectedException.expectMessage("Column `id` is required but is missing from the insert statement");
         e.analyze("insert into users_clustered_by_only (name) (select 'user')");
     }
 
@@ -450,4 +455,11 @@ public class InsertAnalyzerTest extends CrateDummyClusterServiceUnitTest {
         expectedException.expect(ColumnUnknownException.class);
         e.analyze("insert into users (id, name) (select 1, 'Arthur') on conflict (\"ID\") do update set NAME = excluded.name");
     }
+
+    @Test
+    public void test_insert_into_table_with_clustered_by_on_nested_column() throws Exception {
+        // doesn't raise an error
+        e.analyze("insert into doc.nested_clustered (obj) values ({id=4, name='George'})");
+    }
+
 }

server/src/test/java/io/crate/integrationtests/InsertIntoIntegrationTest.java
@@ -414,7 +414,7 @@ public class InsertIntoIntegrationTest extends SQLTransportIntegrationTest {
         };
 
         expectedException.expect(SQLActionException.class);
-        expectedException.expectMessage("Column \"pk_col\" is required but is missing from the insert statement");
+        expectedException.expectMessage("Column `pk_col` is required but is missing from the insert statement");
 
         execute("insert into test (message) values (?)", args);
     }
@@ -435,10 +435,9 @@ public class InsertIntoIntegrationTest extends SQLTransportIntegrationTest {
     public void testInsertWithClusteredByWithoutValue() throws Exception {
         execute("create table quotes (id integer, quote string) clustered by(id) " +
                 "with (number_of_replicas=0)");
-        ensureYellow();
 
         expectedException.expect(SQLActionException.class);
-        expectedException.expectMessage("Clustered by value is required but is missing from the insert statement");
+        expectedException.expectMessage("Column `id` is required but is missing from the insert statement");
         execute("insert into quotes (quote) values(?)",
             new Object[]{"I'd far rather be happy than right any day."});
     }