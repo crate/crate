463bb369fcdcfe1ef0101f453bb7d2097a12d832
Naomi Slater <23469+mechanomi@users.noreply.github.com>
2020-08-10 17:58:06 +0200

    Backport changes for newest crate-docs-theme, plus missing 1.1.3 release notes, plus RST fix

--- blackbox/docs/conf.py
+++ blackbox/docs/conf.py
@@ -3,3 +3,5 @@ from crate.theme.rtd.conf.crate_reference import *
 exclude_patterns = ['out/**', 'tmp/**', 'eggs/**', 'requirements.txt']
 
 extensions = ['crate.sphinx.csv', 'sphinx_sitemap']
+
+source_suffix = '.txt'
