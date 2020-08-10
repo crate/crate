272950ec4c62e1505e3a879c257732ef3fde512a
Naomi Slater <23469+mechanomi@users.noreply.github.com>
2020-08-10 17:41:59 +0200

    Backport changes for newest crate-docs-theme, plus RST fix

--- blackbox/docs/conf.py
+++ blackbox/docs/conf.py
@@ -1,4 +1,4 @@
-from crate.theme.rtd.conf.crate_server import *
+from crate.theme.rtd.conf.crate_reference import *
 
 exclude_patterns = ['out/**', 'tmp/**', 'eggs/**', 'requirements.txt']
 
@@ -6,3 +6,5 @@ extensions = ['crate.sphinx.csv']
 # crate.theme sets html_favicon to favicon.png which causes a warning because it should be a .ico
 # and in addition there is no favicon.png in this project so it can't find the file
 html_favicon = None
+
+source_suffix = '.txt'
