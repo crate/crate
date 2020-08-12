eb661b3f553220276416ca5686a937eeddf49cbb
Mathias Fussenegger <f.mathias@zignar.net>
2020-08-12 09:29:01 +0200

    bp: Reject illegal flush parameters
    https://github.com/elastic/elasticsearch/commit/fe03d24229a2152e6bdb704a215577ab3304e41a

--- server/src/test/java/org/elasticsearch/index/engine/InternalEngineTests.java
+++ server/src/test/java/org/elasticsearch/index/engine/InternalEngineTests.java
@@ -4607,7 +4607,7 @@ public class InternalEngineTests extends EngineTestCase {
             for (int docId = 0; docId < numDocs; docId++) {
                 index(engine, docId);
                 if (frequently()) {
-                    engine.flush(randomBoolean(), randomBoolean());
+                    engine.flush(randomBoolean(), true);
                 }
             }
             engine.flush(false, randomBoolean());
