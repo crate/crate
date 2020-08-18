f181efca7765d23ebff33a895b1e055d4371bc37
Ruslan Kovalov <ruslan@crate.io>
2020-08-18 11:24:18 +0200

    Bump version to 4.2.4-SNAPSHOT.

--- server/src/main/java/org/elasticsearch/Version.java
+++ server/src/main/java/org/elasticsearch/Version.java
@@ -95,6 +95,7 @@ public class Version implements Comparable<Version>, ToXContentFragment {
     public static final int ES_V_7_2_1_ID = 7_02_01_99;
     public static final int ES_V_7_2_2_ID = 7_02_02_99;
     public static final int ES_V_7_2_3_ID = 7_02_03_99;
+    public static final int ES_V_7_2_4_ID = 7_02_04_99;
 
     public static final Version V_4_0_0 = new Version(ES_V_7_0_0_ID, false, org.apache.lucene.util.Version.LUCENE_8_0_0);
     public static final Version V_4_0_1 = new Version(ES_V_7_0_1_ID, false, org.apache.lucene.util.Version.LUCENE_8_0_0);
@@ -124,8 +125,9 @@ public class Version implements Comparable<Version>, ToXContentFragment {
     public static final Version V_4_2_1 = new Version(ES_V_7_2_1_ID, false, org.apache.lucene.util.Version.LUCENE_8_5_1);
     public static final Version V_4_2_2 = new Version(ES_V_7_2_2_ID, false, org.apache.lucene.util.Version.LUCENE_8_5_1);
     public static final Version V_4_2_3 = new Version(ES_V_7_2_3_ID, false, org.apache.lucene.util.Version.LUCENE_8_5_1);
+    public static final Version V_4_2_4 = new Version(ES_V_7_2_4_ID, true, org.apache.lucene.util.Version.LUCENE_8_5_1);
 
-    public static final Version CURRENT = V_4_2_3;
+    public static final Version CURRENT = V_4_2_4;
 
     static {
         assert CURRENT.luceneVersion.equals(org.apache.lucene.util.Version.LATEST) : "Version must be upgraded to ["
@@ -196,6 +198,8 @@ public class Version implements Comparable<Version>, ToXContentFragment {
                 return V_4_2_2;
             case ES_V_7_2_3_ID:
                 return V_4_2_3;
+            case ES_V_7_2_4_ID:
+                return V_4_2_4;
 
             case V_EMPTY_ID:
                 return V_EMPTY;
