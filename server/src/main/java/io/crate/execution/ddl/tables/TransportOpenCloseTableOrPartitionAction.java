19cee7980399c7a4e789db4d84c0981bbdec015c
Mathias Fussenegger <f.mathias@zignar.net>
2020-08-20 11:56:01 +0200

    Make close/open table implementation more concrete
    Removes the use of some abstractions to make it easier to apply changes
    similar to the ones made in https://github.com/elastic/elasticsearch/commit/8e5dd20efb667d272fac4b2a151f8c09f0284024
    
    This will be required for https://github.com/crate/crate/pull/10404

--- server/src/main/java/io/crate/execution/ddl/tables/TransportOpenCloseTableOrPartitionAction.java
+++ server/src/main/java/io/crate/execution/ddl/tables/TransportOpenCloseTableOrPartitionAction.java
@@ -22,28 +22,39 @@
 
 package io.crate.execution.ddl.tables;
 
-import io.crate.execution.ddl.AbstractDDLTransportAction;
-import io.crate.metadata.cluster.CloseTableClusterStateTaskExecutor;
-import io.crate.metadata.cluster.DDLClusterStateService;
-import io.crate.metadata.cluster.OpenTableClusterStateTaskExecutor;
+import java.io.IOException;
+
+import javax.annotation.Nullable;
+
+import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.action.support.IndicesOptions;
 import org.elasticsearch.action.support.master.AcknowledgedResponse;
+import org.elasticsearch.action.support.master.TransportMasterNodeAction;
+import org.elasticsearch.cluster.AckedClusterStateTaskListener;
 import org.elasticsearch.cluster.ClusterState;
-import org.elasticsearch.cluster.ClusterStateTaskExecutor;
+import org.elasticsearch.cluster.ClusterStateTaskConfig;
 import org.elasticsearch.cluster.block.ClusterBlockException;
 import org.elasticsearch.cluster.block.ClusterBlockLevel;
 import org.elasticsearch.cluster.metadata.IndexNameExpressionResolver;
 import org.elasticsearch.cluster.metadata.MetadataIndexUpgradeService;
+import org.elasticsearch.cluster.node.DiscoveryNode;
 import org.elasticsearch.cluster.routing.allocation.AllocationService;
 import org.elasticsearch.cluster.service.ClusterService;
+import org.elasticsearch.common.Priority;
 import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.common.inject.Singleton;
+import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.indices.IndicesService;
 import org.elasticsearch.threadpool.ThreadPool;
 import org.elasticsearch.transport.TransportService;
 
+import io.crate.common.unit.TimeValue;
+import io.crate.metadata.cluster.CloseTableClusterStateTaskExecutor;
+import io.crate.metadata.cluster.DDLClusterStateService;
+import io.crate.metadata.cluster.OpenTableClusterStateTaskExecutor;
+
 @Singleton
-public class TransportOpenCloseTableOrPartitionAction extends AbstractDDLTransportAction<OpenCloseTableOrPartitionRequest, AcknowledgedResponse> {
+public class TransportOpenCloseTableOrPartitionAction extends TransportMasterNodeAction<OpenCloseTableOrPartitionRequest, AcknowledgedResponse> {
 
     private static final IndicesOptions STRICT_INDICES_OPTIONS = IndicesOptions.fromOptions(false, false, false, false);
     private static final String ACTION_NAME = "internal:crate:sql/table_or_partition/open_close";
@@ -64,29 +75,124 @@ public class TransportOpenCloseTableOrPartitionAction extends AbstractDDLTranspo
             transportService,
             clusterService,
             threadPool,
-            indexNameExpressionResolver,
             OpenCloseTableOrPartitionRequest::new,
-            AcknowledgedResponse::new,
-            AcknowledgedResponse::new,
-            "open-table-or-partition");
-        openExecutor = new OpenTableClusterStateTaskExecutor(indexNameExpressionResolver, allocationService,
-            ddlClusterStateService, metadataIndexUpgradeService, indexServices);
-        closeExecutor = new CloseTableClusterStateTaskExecutor(indexNameExpressionResolver, allocationService,
-            ddlClusterStateService);
-    }
-
-    @Override
-    public ClusterStateTaskExecutor<OpenCloseTableOrPartitionRequest> clusterStateTaskExecutor(OpenCloseTableOrPartitionRequest request) {
-        if (request.isOpenTable()) {
-            return openExecutor;
-        } else {
-            return closeExecutor;
-        }
+            indexNameExpressionResolver
+        );
+        openExecutor = new OpenTableClusterStateTaskExecutor(
+            indexNameExpressionResolver,
+            allocationService,
+            ddlClusterStateService,
+            metadataIndexUpgradeService,
+            indexServices
+        );
+        closeExecutor = new CloseTableClusterStateTaskExecutor(
+            indexNameExpressionResolver,
+            allocationService,
+            ddlClusterStateService
+        );
     }
 
     @Override
     protected ClusterBlockException checkBlock(OpenCloseTableOrPartitionRequest request, ClusterState state) {
-        return state.blocks().indicesBlockedException(ClusterBlockLevel.METADATA_WRITE,
-            indexNameExpressionResolver.concreteIndexNames(state, STRICT_INDICES_OPTIONS, request.tableIdent().indexNameOrAlias()));
+        return state.blocks().indicesBlockedException(
+            ClusterBlockLevel.METADATA_WRITE,
+            indexNameExpressionResolver.concreteIndexNames(state, STRICT_INDICES_OPTIONS, request.tableIdent().indexNameOrAlias())
+        );
+    }
+
+    @Override
+    protected String executor() {
+        // no need to use a thread pool, we go async right away
+        return ThreadPool.Names.SAME;
+    }
+
+    @Override
+    protected AcknowledgedResponse read(StreamInput in) throws IOException {
+        return new AcknowledgedResponse(in);
+    }
+
+    @Override
+    protected void masterOperation(OpenCloseTableOrPartitionRequest request,
+                                   ClusterState state,
+                                   ActionListener<AcknowledgedResponse> listener) throws Exception {
+        if (request.isOpenTable()) {
+            openTable(request, state, listener);
+        } else {
+            closeTable(request, state, listener);
+        }
+    }
+
+    private void closeTable(OpenCloseTableOrPartitionRequest request,
+                            ClusterState state,
+                            ActionListener<AcknowledgedResponse> listener) throws Exception {
+        clusterService.submitStateUpdateTask(
+            "close-table-or-partition",
+            request,
+            ClusterStateTaskConfig.build(Priority.HIGH, request.masterNodeTimeout()),
+            closeExecutor,
+            new AckedClusterStateTaskListener() {
+                @Override
+                public void onFailure(String source, Exception e) {
+                    listener.onFailure(e);
+                }
+
+                @Override
+                public boolean mustAck(DiscoveryNode discoveryNode) {
+                    return true;
+                }
+
+                @Override
+                public void onAllNodesAcked(@Nullable Exception e) {
+                    listener.onResponse(new AcknowledgedResponse(true));
+                }
+
+                @Override
+                public void onAckTimeout() {
+                    listener.onResponse(new AcknowledgedResponse(false));
+                }
+
+                @Override
+                public TimeValue ackTimeout() {
+                    return request.ackTimeout();
+                }
+            }
+        );
+    }
+
+    private void openTable(OpenCloseTableOrPartitionRequest request,
+                           ClusterState state,
+                           ActionListener<AcknowledgedResponse> listener) {
+        clusterService.submitStateUpdateTask(
+            "open-table-or-partition",
+            request,
+            ClusterStateTaskConfig.build(Priority.HIGH, request.masterNodeTimeout()),
+            openExecutor,
+            new AckedClusterStateTaskListener() {
+                @Override
+                public void onFailure(String source, Exception e) {
+                    listener.onFailure(e);
+                }
+
+                @Override
+                public boolean mustAck(DiscoveryNode discoveryNode) {
+                    return true;
+                }
+
+                @Override
+                public void onAllNodesAcked(@Nullable Exception e) {
+                    listener.onResponse(new AcknowledgedResponse(true));
+                }
+
+                @Override
+                public void onAckTimeout() {
+                    listener.onResponse(new AcknowledgedResponse(false));
+                }
+
+                @Override
+                public TimeValue ackTimeout() {
+                    return request.ackTimeout();
+                }
+            }
+        );
     }
 }
