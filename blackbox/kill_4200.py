6780d2c2021b79029120e5484fb955c8a03a7ae7
Mathias Fussenegger <f.mathias@zignar.net>
2020-10-19 16:22:22 +0200

    WIP

--- blackbox/kill_4200.py
+++ blackbox/kill_4200.py
@@ -4,6 +4,7 @@ import socket
 import os
 import signal
 import subprocess
+from subprocess import (check_output, run)
 
 
 def is_up(host: str, port: int) -> bool:
@@ -19,7 +20,7 @@ def kill():
     if not is_up('localhost', 4200):
         return
 
-    output = subprocess.check_output(['jps'], universal_newlines=True)
+    output = check_output(['jps'], universal_newlines=True)
     for line in output.split('\n'):
         try:
             pid, procname = line.split(' ')
@@ -28,6 +29,14 @@ def kill():
         if procname == 'CrateDB':
             print(f'CrateDB process with pid {pid} found. Killing it')
             os.kill(int(pid), signal.SIGKILL)
+
+    try:
+        containers = check_output(['docker ps -q'], universal_newlines=True)
+    except subprocess.CalledProcessError:
+        return
+    for container in containers.split('\n'):
+        run(['docker', 'stop', container])
+        run(['docker', 'rm', container])
 
 
 if __name__ == "__main__":
