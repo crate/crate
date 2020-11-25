2906b6eccdc470275d3906be093f4221a032892f
Mathias Fussenegger <f.mathias@zignar.net>
2020-11-25 11:56:19 +0100

    Fix testRecentPrimaryInformation

--- server/src/test/java/org/elasticsearch/gateway/ReplicaShardAllocatorIT.java
+++ server/src/test/java/org/elasticsearch/gateway/ReplicaShardAllocatorIT.java
@@ -19,13 +19,13 @@
 
 package org.elasticsearch.gateway;
 
-import static org.elasticsearch.test.hamcrest.ElasticsearchAssertions.assertAcked;
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasItem;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Map;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
@@ -44,7 +44,7 @@ import org.elasticsearch.test.InternalSettingsPlugin;
 import org.elasticsearch.test.InternalTestCluster;
 import org.elasticsearch.test.transport.MockTransportService;
 import org.elasticsearch.transport.TransportService;
-import org.junit.Ignore;
+import org.hamcrest.Matchers;
 import org.junit.Test;
 
 import io.crate.integrationtests.SQLTransportIntegrationTest;
@@ -65,7 +65,6 @@ public class ReplicaShardAllocatorIT extends SQLTransportIntegrationTest {
      * for the copy on the newly joined node unless we can perform a noop recovery with that node.
      */
     @Test
-    @Ignore("Fails currently, maybe missing backports")
     public void testRecentPrimaryInformation() throws Exception {
         String indexName = "test";
         String nodeWithPrimary = internalCluster().startNode();
@@ -76,6 +75,7 @@ public class ReplicaShardAllocatorIT extends SQLTransportIntegrationTest {
                 number_of_replicas = 1,
                 "recovery.file_based_threshold" = 1.0,
                 "global_checkpoint_sync.interval" = '100ms',
+                "soft_deletes.retention_lease.sync_interval" = '100ms',
                 "unassigned.node_left.delayed_timeout" = '1ms'
             )
         """);
@@ -133,7 +133,7 @@ public class ReplicaShardAllocatorIT extends SQLTransportIntegrationTest {
             // AllocationService only calls GatewayAllocator if there are unassigned shards
             execute("""
                 create table doc.dummy (x int)
-                with ("routing.allocation.require.attr" = 'not-found')
+                with ("number_of_replicas" = 1, "write.wait_for_active_shards" = 0)
             """);
             internalCluster().startDataOnlyNode(nodeWithReplicaSettings);
             // need to wait for events to ensure the reroute has happened since we perform it async when a new node joins.
@@ -146,11 +146,11 @@ public class ReplicaShardAllocatorIT extends SQLTransportIntegrationTest {
             blockRecovery.countDown();
             ensureGreen(indexName);
             assertThat(internalCluster().nodesInclude(indexName), hasItem(newNode));
-            //for (RecoveryState recovery : client().admin().indices().prepareRecoveries(indexName).get().shardRecoveryStates().get(indexName)) {
-            //    if (recovery.getPrimary() == false) {
-            //        assertThat(recovery.getIndex().fileDetails(), not(empty()));
-            //    }
-            //}
+
+            execute("select recovery['files'] from sys.shards where table_name = 'test'");
+            for (var row : response.rows()) {
+                assertThat((Map<String, Object>) row[0], not(Matchers.anEmptyMap()));
+            }
         } finally {
             transportServiceOnPrimary.clearAllRules();
         }
