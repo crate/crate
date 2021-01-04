crate/crate

commit 468ef007fa837696f37380360ab7aae665d5fe4e

Deduplicate insert from values/subquery analyzer logic.
This change deduplicates the ``INSERT INTO ... VALUES ...`` and
``INSERT INTO ... (SELECT ...)`` analyzer logic.

It removes special ``INSERT INTO ... VALUES ...`` analyzer logic
and parts of the dedicated execution logic for the insert from values
case, e.g. `LegacyUpsertByIdTask`, `LegacyUpsertById`. Instead, we rely
on the ``INSERT INTO ... (SELECT ...)`` anayzer and the new
``INSERT INTO ... VALUES ...`` plan.

sql/src/main/java/io/crate/execution/dsl/projection/ColumnIndexWriterProjection.java
@@ -21,13 +21,13 @@
 
 package io.crate.execution.dsl.projection;
 
-import io.crate.execution.dsl.projection.builder.InputColumns;
 import io.crate.expression.symbol.Symbol;
 import io.crate.expression.symbol.SymbolVisitors;
 import io.crate.expression.symbol.Symbols;
 import io.crate.metadata.ColumnIdent;
 import io.crate.metadata.Reference;
 import io.crate.metadata.RelationName;
+import org.elasticsearch.Version;
 import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.io.stream.StreamOutput;
 import org.elasticsearch.common.settings.Settings;
@@ -39,28 +39,30 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 
 public class ColumnIndexWriterProjection extends AbstractIndexWriterProjection {
 
-    private final List<Symbol> columnSymbols;
+    private final List<Symbol> targetColsSymbolsExclPartition;
     private final List<Reference> targetColsExclPartitionCols;
     private final boolean ignoreDuplicateKeys;
-    @Nullable
     private final Map<Reference, Symbol> onDuplicateKeyAssignments;
+    private final List<Reference> allTargetColumns;
 
     /**
-     * @param relationName                identifying the table to write to
-     * @param columns                   the columnReferences of all the columns to be written in order of appearance
+     * @param relationName              identifying the table to write to
+     * @param allTargetColumns          all the columns to be written in order of appearance
      * @param onDuplicateKeyAssignments reference to symbol map used for update on duplicate key
      */
     public ColumnIndexWriterProjection(RelationName relationName,
                                        @Nullable String partitionIdent,
                                        List<ColumnIdent> primaryKeys,
-                                       List<Reference> columns,
+                                       List<Reference> allTargetColumns,
+                                       List<Reference> targetColsExclPartitionCols,
+                                       List<Symbol> targetColsSymbolsExclPartition,
                                        boolean ignoreDuplicateKeys,
-                                       @Nullable Map<Reference, Symbol> onDuplicateKeyAssignments,
+                                       Map<Reference, Symbol> onDuplicateKeyAssignments,
                                        List<Symbol> primaryKeySymbols,
-                                       List<ColumnIdent> partitionedByColumns,
                                        List<Symbol> partitionedBySymbols,
                                        @Nullable ColumnIdent clusteredByColumn,
                                        @Nullable Symbol clusteredBySymbol,
@@ -69,27 +71,22 @@ public class ColumnIndexWriterProjection extends AbstractIndexWriterProjection {
         super(relationName, partitionIdent, primaryKeys, clusteredByColumn, settings, primaryKeySymbols, autoCreateIndices);
         assert partitionedBySymbols.stream().noneMatch(s -> SymbolVisitors.any(Symbols.IS_COLUMN, s))
             : "All references and fields in partitionedBySymbols must be resolved to inputColumns, got: " + partitionedBySymbols;
+        this.allTargetColumns = allTargetColumns;
         this.partitionedBySymbols = partitionedBySymbols;
         this.ignoreDuplicateKeys = ignoreDuplicateKeys;
         this.onDuplicateKeyAssignments = onDuplicateKeyAssignments;
-        this.targetColsExclPartitionCols = new ArrayList<>(columns.size() - partitionedByColumns.size());
-        for (Reference column : columns) {
-            if (partitionedByColumns.contains(column.column())) {
-                continue;
-            }
-            targetColsExclPartitionCols.add(column);
-        }
+        this.targetColsExclPartitionCols = targetColsExclPartitionCols;
         this.clusteredBySymbol = clusteredBySymbol;
-        columnSymbols = InputColumns.create(targetColsExclPartitionCols, new InputColumns.SourceSymbols(columns));
+        this.targetColsSymbolsExclPartition = targetColsSymbolsExclPartition;
     }
 
     ColumnIndexWriterProjection(StreamInput in) throws IOException {
         super(in);
 
         if (in.readBoolean()) {
-            columnSymbols = Symbols.listFromStream(in);
+            targetColsSymbolsExclPartition = Symbols.listFromStream(in);
         } else {
-            columnSymbols = Collections.emptyList();
+            targetColsSymbolsExclPartition = Collections.emptyList();
         }
         if (in.readBoolean()) {
             int length = in.readVInt();
@@ -111,13 +108,27 @@ public class ColumnIndexWriterProjection extends AbstractIndexWriterProjection {
         } else {
             onDuplicateKeyAssignments = Collections.emptyMap();
         }
+
+        if (in.getVersion().onOrAfter(Version.V_4_2_0)) {
+            int mapSize = in.readVInt();
+            allTargetColumns = new ArrayList<>();
+            for (int i = 0; i < mapSize; i++) {
+                allTargetColumns.add(Reference.fromStream(in));
+            }
+        } else {
+            allTargetColumns = List.of();
+        }
     }
 
-    public List<Symbol> columnSymbols() {
-        return columnSymbols;
+    public List<Reference> allTargetColumns() {
+        return allTargetColumns;
     }
 
-    public List<Reference> columnReferences() {
+    public List<Symbol> columnSymbolsExclPartition() {
+        return targetColsSymbolsExclPartition;
+    }
+
+    public List<Reference> columnReferencesExclPartition() {
         return targetColsExclPartitionCols;
     }
 
@@ -149,7 +160,7 @@ public class ColumnIndexWriterProjection extends AbstractIndexWriterProjection {
         ColumnIndexWriterProjection that = (ColumnIndexWriterProjection) o;
 
         if (!targetColsExclPartitionCols.equals(that.targetColsExclPartitionCols)) return false;
-        if (!columnSymbols.equals(that.columnSymbols)) return false;
+        if (!targetColsSymbolsExclPartition.equals(that.targetColsSymbolsExclPartition)) return false;
         return !(onDuplicateKeyAssignments != null ?
                      !onDuplicateKeyAssignments.equals(that.onDuplicateKeyAssignments)
                      : that.onDuplicateKeyAssignments != null);
@@ -158,7 +169,7 @@ public class ColumnIndexWriterProjection extends AbstractIndexWriterProjection {
     @Override
     public int hashCode() {
         int result = super.hashCode();
-        result = 31 * result + columnSymbols.hashCode();
+        result = 31 * result + targetColsSymbolsExclPartition.hashCode();
         result = 31 * result + targetColsExclPartitionCols.hashCode();
         result = 31 * result + (onDuplicateKeyAssignments != null ? onDuplicateKeyAssignments.hashCode() : 0);
         return result;
@@ -168,11 +179,11 @@ public class ColumnIndexWriterProjection extends AbstractIndexWriterProjection {
     public void writeTo(StreamOutput out) throws IOException {
         super.writeTo(out);
 
-        if (columnSymbols == null) {
+        if (targetColsSymbolsExclPartition == null) {
             out.writeBoolean(false);
         } else {
             out.writeBoolean(true);
-            Symbols.toStream(columnSymbols, out);
+            Symbols.toStream(targetColsSymbolsExclPartition, out);
         }
         if (targetColsExclPartitionCols == null) {
             out.writeBoolean(false);
@@ -196,5 +207,37 @@ public class ColumnIndexWriterProjection extends AbstractIndexWriterProjection {
             }
         }
 
+        if (out.getVersion().onOrAfter(Version.V_4_2_0)) {
+            out.write(allTargetColumns.size());
+            for (var ref : allTargetColumns) {
+                Symbols.toStream(ref, out);
+            }
+        }
+    }
+
+    public ColumnIndexWriterProjection bind(Function<? super Symbol, Symbol> binder) {
+        HashMap<Reference, Symbol> boundOnDuplicateKeyAssignments =
+            new HashMap<>(onDuplicateKeyAssignments.size());
+        for (var assignment : onDuplicateKeyAssignments.entrySet()) {
+            boundOnDuplicateKeyAssignments.put(
+                assignment.getKey(),
+                binder.apply(assignment.getValue()));
+        }
+
+        return new ColumnIndexWriterProjection(
+            tableIdent(),
+            null,
+            primaryKeys,
+            allTargetColumns,
+            targetColsExclPartitionCols,
+            targetColsSymbolsExclPartition,
+            ignoreDuplicateKeys,
+            boundOnDuplicateKeyAssignments,
+            (List<Symbol>) ids(),
+            partitionedBySymbols,
+            clusteredByIdent(),
+            clusteredBySymbol,
+            Settings.EMPTY,
+            autoCreateIndices());
     }
 }

sql/src/test/java/io/crate/execution/dsl/projection/ColumnIndexWriterProjectionTest.java
@@ -52,7 +52,7 @@ public class ColumnIndexWriterProjectionTest {
          *  partitioned by: [ymd, isp]
          *  insert into t   (ymd, domain, area, isp, h) select ...
          *
-         *  We don't write PartitionedBy columns, so they're excluded from the targetColumns:
+         *  We don't write PartitionedBy columns, so they're excluded from the allTargetColumns:
          *
          *  expected targetRefs:      [ domain, area, h ]
          *  expected column symbols:  [ ic1,   ic2, ic4 ]
@@ -69,21 +69,25 @@ public class ColumnIndexWriterProjectionTest {
         Reference hRef = ref(h, DataTypes.INTEGER);
         List<ColumnIdent> primaryKeys = Arrays.asList(ymd, domain, area, isp);
         List<Reference> targetColumns = Arrays.asList(ymdRef, domainRef, areaRef, ispRef, hRef);
+        List<Reference> targetColumnsExclPartitionColumns = Arrays.asList(domainRef, areaRef, hRef);
         InputColumns.SourceSymbols targetColsCtx = new InputColumns.SourceSymbols(targetColumns);
         List<Symbol> primaryKeySymbols = InputColumns.create(
             Arrays.asList(ymdRef, domainRef, areaRef, ispRef), targetColsCtx);
-        List<ColumnIdent> partitionedByColumns = Arrays.asList(ymd, isp);
         List<Symbol> partitionedBySymbols = InputColumns.create(Arrays.asList(ymdRef, ispRef), targetColsCtx);
+        List<Symbol> columnSymbols = InputColumns.create(
+            targetColumnsExclPartitionColumns,
+            targetColsCtx);
 
         ColumnIndexWriterProjection projection = new ColumnIndexWriterProjection(
             relationName,
             null,
             primaryKeys,
             targetColumns,
+            targetColumnsExclPartitionColumns,
+            columnSymbols,
             false,
             null,
             primaryKeySymbols,
-            partitionedByColumns,
             partitionedBySymbols,
             null,
             null,
@@ -91,10 +95,10 @@ public class ColumnIndexWriterProjectionTest {
             true
         );
 
-        assertThat(projection.columnReferences(), Matchers.is(Arrays.asList(
+        assertThat(projection.columnReferencesExclPartition(), Matchers.is(Arrays.asList(
             domainRef, areaRef, hRef)
         ));
-        assertThat(projection.columnSymbols(), Matchers.is(Arrays.asList(
+        assertThat(projection.columnSymbolsExclPartition(), Matchers.is(Arrays.asList(
             new InputColumn(1, DataTypes.STRING),
             new InputColumn(2, DataTypes.STRING),
             new InputColumn(4, DataTypes.INTEGER))));

enterprise/users/src/main/java/io/crate/auth/user/AccessControlImpl.java
@@ -52,7 +52,6 @@ import io.crate.analyze.AnalyzedDropUser;
 import io.crate.analyze.AnalyzedDropView;
 import io.crate.analyze.ExplainAnalyzedStatement;
 import io.crate.analyze.InsertFromSubQueryAnalyzedStatement;
-import io.crate.analyze.InsertFromValuesAnalyzedStatement;
 import io.crate.analyze.AnalyzedKill;
 import io.crate.analyze.MultiSourceSelect;
 import io.crate.analyze.AnalyzedPrivileges;
@@ -304,17 +303,6 @@ public final class AccessControlImpl implements AccessControl {
         @Override
         protected Void visitAnalyzedDeleteStatement(AnalyzedDeleteStatement delete, User user) {
             visitRelation(delete.relation(), user, Privilege.Type.DML);
-            return null;
-        }
-
-        @Override
-        protected Void visitInsertFromValuesStatement(InsertFromValuesAnalyzedStatement analysis, User user) {
-            Privileges.ensureUserHasPrivilege(
-                Privilege.Type.DML,
-                Privilege.Clazz.TABLE,
-                analysis.tableInfo().ident().toString(),
-                user,
-                defaultSchema);
             return null;
         }
 

sql-parser/src/main/antlr/SqlBase.g4
@@ -435,8 +435,7 @@ objectKeyValue
     ;
 
 insertSource
-   : VALUES  values (',' values)*
-   | query
+   : query
    | '(' query ')'
    ;
 

sql-parser/src/main/java/io/crate/sql/parser/AstBuilder.java
@@ -105,7 +105,6 @@ import io.crate.sql.tree.IndexColumnConstraint;
 import io.crate.sql.tree.IndexDefinition;
 import io.crate.sql.tree.Insert;
 import io.crate.sql.tree.InsertFromSubquery;
-import io.crate.sql.tree.InsertFromValues;
 import io.crate.sql.tree.Intersect;
 import io.crate.sql.tree.IntervalLiteral;
 import io.crate.sql.tree.IsNotNullPredicate;
@@ -561,14 +560,6 @@ class AstBuilder extends SqlBaseBaseVisitor<Node> {
                 }
             }
             throw e;
-        }
-
-        if (context.insertSource().VALUES() != null) {
-            return new InsertFromValues(
-                table,
-                visitCollection(context.insertSource().values(), ValuesList.class),
-                columns,
-                createDuplicateKeyContext(context));
         }
         return new InsertFromSubquery(
             table,

sql-parser/src/main/java/io/crate/sql/tree/AstVisitor.java
@@ -492,10 +492,6 @@ public abstract class AstVisitor<R, C> {
         return visitTableElement(node, context);
     }
 
-    public R visitInsertFromValues(InsertFromValues<?> node, C context) {
-        return visitInsert(node, context);
-    }
-
     public R visitIntervalLiteral(IntervalLiteral node, C context) {
         return visitLiteral(node, context);
     }

sql-parser/src/main/java/io/crate/sql/tree/DefaultTraversalVisitor.java
@@ -278,15 +278,6 @@ public abstract class DefaultTraversalVisitor<R, C> extends AstVisitor<R, C> {
     }
 
     @Override
-    public R visitInsertFromValues(InsertFromValues<?> node, C context) {
-        node.table().accept(this, context);
-        for (ValuesList valuesList : node.valuesLists()) {
-            valuesList.accept(this, context);
-        }
-        return null;
-    }
-
-    @Override
     public R visitValuesList(ValuesList node, C context) {
         for (Expression value : node.values()) {
             value.accept(this, context);

sql-parser/src/main/java/io/crate/sql/tree/InsertFromValues.java
@@ -1,90 +1,0 @@
-/*
- * Licensed to CRATE Technology GmbH ("Crate") under one or more contributor
- * license agreements.  See the NOTICE file distributed with this work for
- * additional information regarding copyright ownership.  Crate licenses
- * this file to you under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.  You may
- * obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
- * License for the specific language governing permissions and limitations
- * under the License.
- *
- * However, if you have executed another commercial license agreement
- * with Crate these terms will supersede the license and you may use the
- * software solely pursuant to the terms of the relevant commercial agreement.
- */
-
-package io.crate.sql.tree;
-
-import com.google.common.base.MoreObjects;
-import com.google.common.base.Objects;
-
-import java.util.List;
-
-public class InsertFromValues<T> extends Insert<T> {
-
-    private final List<ValuesList> valuesLists;
-    private final int maxValuesLength;
-
-    public InsertFromValues(Table<T> table,
-                            List<ValuesList> valuesLists,
-                            List<String> columns,
-                            DuplicateKeyContext<T> duplicateKeyContext) {
-        super(table, columns, duplicateKeyContext);
-        this.valuesLists = valuesLists;
-
-        int i = 0;
-        for (ValuesList valuesList : valuesLists) {
-            i = Math.max(i, valuesList.values().size());
-        }
-        maxValuesLength = i;
-    }
-
-    public List<ValuesList> valuesLists() {
-        return valuesLists;
-    }
-
-    /**
-     * returns the length of the longest values List
-     */
-    public int maxValuesLength() {
-        return maxValuesLength;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(super.hashCode(), valuesLists);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        if (!super.equals(o)) return false;
-
-        InsertFromValues that = (InsertFromValues) o;
-
-        if (!valuesLists.equals(that.valuesLists)) return false;
-
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return MoreObjects.toStringHelper(this)
-            .add("table", table)
-            .add("columns", columns)
-            .add("values", valuesLists)
-            .toString();
-    }
-
-    @Override
-    public <R, C> R accept(AstVisitor<R, C> visitor, C context) {
-        return visitor.visitInsertFromValues(this, context);
-    }
-}

commit b2c11aa5fcfde4ac0c98df77b62bfb927ae2b433

Fix streaming of target columns of the ColumnIndexWriterProjection
Fixes a regression introduced by
https://github.com/crate/crate/commit/468ef007fa8
which added an incorrect writing of the target column size.
As an result, copying data from a source table with > 128
columns caused a NPE.

Fixes #10874.

server/src/main/java/io/crate/execution/dsl/projection/ColumnIndexWriterProjection.java
@@ -259,7 +259,7 @@ public class ColumnIndexWriterProjection extends AbstractIndexWriterProjection {
         }
 
         if (out.getVersion().onOrAfter(Version.V_4_2_0)) {
-            out.write(allTargetColumns.size());
+            out.writeVInt(allTargetColumns.size());
             for (var ref : allTargetColumns) {
                 Symbols.toStream(ref, out);
             }

server/src/test/java/io/crate/execution/dsl/projection/ColumnIndexWriterProjectionTest.java
@@ -32,13 +32,18 @@ import io.crate.metadata.RelationName;
 import io.crate.metadata.RowGranularity;
 import io.crate.types.DataType;
 import io.crate.types.DataTypes;
+import org.elasticsearch.common.io.stream.BytesStreamOutput;
+import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.settings.Settings;
-import org.hamcrest.Matchers;
 import org.junit.Test;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 public class ColumnIndexWriterProjectionTest {
@@ -97,20 +102,65 @@ public class ColumnIndexWriterProjectionTest {
             null
         );
 
-        assertThat(projection.columnReferencesExclPartition(), Matchers.is(Arrays.asList(
+        assertThat(projection.columnReferencesExclPartition(), is(Arrays.asList(
             domainRef, areaRef, hRef)
         ));
-        assertThat(projection.columnSymbolsExclPartition(), Matchers.is(Arrays.asList(
+        assertThat(projection.columnSymbolsExclPartition(), is(Arrays.asList(
             new InputColumn(1, DataTypes.STRING),
             new InputColumn(2, DataTypes.STRING),
             new InputColumn(4, DataTypes.INTEGER))));
     }
 
-    private Reference ref(ColumnIdent column, DataType type) {
+    /**
+     * Tests a fix for a regression introduced by an incorrect writing of the columns size by
+     * https://github.com/crate/crate/commit/468ef007fa8#diff-43b49f4a960311009a1e5c62672bcc13f8721f0c91f5b87e4d7c0d53146005c8R211
+     * This led to a NPE https://github.com/crate/crate/issues/10874.
+     */
+    @Test
+    public void test_streaming_with_lot_of_target_columns() throws Exception {
+        int numColumns = 200;
+        ArrayList<Reference> targetColumns = new ArrayList<>(numColumns);
+        for (int i = 0; i < numColumns; i++) {
+            var ident = new ColumnIdent(String.valueOf(i));
+            targetColumns.add(ref(ident, DataTypes.INTEGER));
+        }
+        InputColumns.SourceSymbols targetColsCtx = new InputColumns.SourceSymbols(targetColumns);
+        List<Symbol> columnSymbols = InputColumns.create(
+            targetColumns,
+            targetColsCtx);
+        var projection = new ColumnIndexWriterProjection(
+            relationName,
+            null,
+            List.of(),
+            targetColumns,
+            targetColumns,
+            columnSymbols,
+            false,
+            Collections.emptyMap(),
+            List.of(),
+            List.of(),
+            null,
+            null,
+            Settings.EMPTY,
+            true,
+            List.of(),
+            List.of()
+        );
+
+        BytesStreamOutput out = new BytesStreamOutput();
+        Projection.toStream(projection, out);
+
+        StreamInput in = out.bytes().streamInput();
+        ColumnIndexWriterProjection p2 = (ColumnIndexWriterProjection) Projection.fromStream(in);
+
+        assertThat(p2, is(projection));
+    }
+
+    private Reference ref(ColumnIdent column, DataType<?> type) {
         return new Reference(new ReferenceIdent(relationName, column), RowGranularity.DOC, type, null, null);
     }
 
-    private Reference partitionRef(ColumnIdent column, DataType type) {
+    private Reference partitionRef(ColumnIdent column, DataType<?> type) {
         return new Reference(new ReferenceIdent(relationName, column), RowGranularity.PARTITION, type, null, null);
     }
 }