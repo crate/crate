d6c6f0c44fbcd57cb5048a8a0c7e33fecbe631c9
Mathias Fussenegger <f.mathias@zignar.net>
2021-02-01 16:40:18 +0100

    Relicense enterprise module under Apache License, Version 2.0

--- enterprise/lang-js/src/test/java/io/crate/operation/language/JavaScriptUDFIntegrationTest.java
+++ enterprise/lang-js/src/test/java/io/crate/operation/language/JavaScriptUDFIntegrationTest.java
@@ -1,19 +1,23 @@
 /*
- * This file is part of a module with proprietary Enterprise Features.
+ * Licensed to Crate under one or more contributor license agreements.
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.  Crate licenses this file
+ * to you under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.  You may
+ * obtain a copy of the License at
  *
- * Licensed to Crate.io Inc. ("Crate.io") under one or more contributor
- * license agreements.  See the NOTICE file distributed with this work for
- * additional information regarding copyright ownership.
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unauthorized copying of this file, via any medium is strictly prohibited.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.  See the License for the specific language governing
+ * permissions and limitations under the License.
  *
- * To use this file, Crate.io must have given you permission to enable and
- * use such Enterprise Features and you must have a valid Enterprise or
- * Subscription Agreement with Crate.io.  If you enable or use the Enterprise
- * Features, you represent and warrant that you have a valid Enterprise or
- * Subscription Agreement with Crate.io.  Your use of the Enterprise Features
- * if governed by the terms and conditions of your Enterprise or Subscription
- * Agreement with Crate.io.
+ * However, if you have executed another commercial license agreement
+ * with Crate these terms will supersede the license and you may use the
+ * software solely pursuant to the terms of the relevant commercial
+ * agreement.
  */
 
 package io.crate.operation.language;
