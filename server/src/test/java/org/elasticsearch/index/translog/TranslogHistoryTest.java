3ccee29ea56078a683612d2329cc2b2ef72583cd
Michael Kleen <michael.kleen@crate.io>
2021-03-22 18:41:54 +0100

    fixup! Add integration test to test if translog was trimmed correctly

--- server/src/test/java/org/elasticsearch/index/translog/TranslogHistoryTest.java
+++ server/src/test/java/org/elasticsearch/index/translog/TranslogHistoryTest.java
@@ -23,20 +23,23 @@
 package org.elasticsearch.index.translog;
 
 import io.crate.integrationtests.SQLTransportIntegrationTest;
+import org.elasticsearch.test.ESIntegTestCase;
+
 import static org.hamcrest.Matchers.is;
 
 public class TranslogHistoryTest extends SQLTransportIntegrationTest {
 
     public void test_translog_is_trimmed_with_soft_deletes_enabled() throws Exception {
-        execute("create table doc.test(x int) clustered into 1 shards with(number_of_replicas=1, \"soft_deletes.enabled\"='true')");
-        ensureGreen("test");
+        execute("create table doc.test(x int) clustered into 1 shards " +
+                "with(number_of_replicas=1, \"write.wait_for_active_shards\" = 2, \"soft_deletes.enabled\"='true')");
+        ensureGreen();
         int numDocs = randomIntBetween(1, 10);
         for(int i = 0; i < numDocs; i++) {
             execute("insert into doc.test(x) values(?)", new Object[]{i});
         }
         execute("refresh table doc.test");
         execute("optimize table doc.test");
-        ensureGreen("test");
+        ensureGreen();
         assertBusy(() -> {
             execute("select translog_stats['number_of_operations'] from sys.shards where table_name='test' and primary=true");
             assertThat(response.rows()[0][0], is(0));
