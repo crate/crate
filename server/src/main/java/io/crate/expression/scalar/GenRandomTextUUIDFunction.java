ebc986099a0f606ee528cab69390013794643585
Mathias Fussenegger <f.mathias@zignar.net>
2021-03-22 15:51:54 +0100

    Add a `gen_random_text_uuid` scalar
    Closes https://github.com/crate/crate/issues/11039

---
+++ server/src/main/java/io/crate/expression/scalar/GenRandomTextUUIDFunction.java
@@ -0,0 +1,69 @@
+/*
+ * Licensed to CRATE Technology GmbH ("Crate") under one or more contributor
+ * license agreements.  See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.  Crate licenses
+ * this file to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.  You may
+ * obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * However, if you have executed another commercial license agreement
+ * with Crate these terms will supersede the license and you may use the
+ * software solely pursuant to the terms of the relevant commercial agreement.
+ */
+
+package io.crate.expression.scalar;
+
+import org.elasticsearch.common.UUIDs;
+
+import io.crate.data.Input;
+import io.crate.metadata.NodeContext;
+import io.crate.metadata.Scalar;
+import io.crate.metadata.TransactionContext;
+import io.crate.metadata.functions.Signature;
+import io.crate.types.DataTypes;
+
+public final class GenRandomTextUUIDFunction extends Scalar<String, Void> {
+
+    private static final String NAME = "gen_random_text_uuid";
+    private final Signature signature;
+    private final Signature boundSignature;
+
+    public static void register(ScalarFunctionModule module) {
+        module.register(
+            Signature.scalar(
+                NAME,
+                DataTypes.STRING.getTypeSignature()
+            ).withFeatures(NO_FEATURES),
+            GenRandomTextUUIDFunction::new
+        );
+    }
+
+    public GenRandomTextUUIDFunction(Signature signature, Signature boundSignature) {
+        this.signature = signature;
+        this.boundSignature = boundSignature;
+    }
+
+    @Override
+    public Signature signature() {
+        return signature;
+    }
+
+    @Override
+    public Signature boundSignature() {
+        return boundSignature;
+    }
+
+    @SafeVarargs
+    @Override
+    public final String evaluate(TransactionContext txnCtx, NodeContext nodeContext, Input<Void>... args) {
+        return UUIDs.base64UUID();
+    }
+}
