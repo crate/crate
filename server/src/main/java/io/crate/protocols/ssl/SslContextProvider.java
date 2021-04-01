bead982709399a9a040fd25b96ca76aef099c3b0
Mathias Fussenegger <f.mathias@zignar.net>
2021-04-01 12:13:40 +0200

    Merge SslConfiguration into SslContextProvider
    The `SslConfiguration` class was primarily concerned with creating a
    `SslContext` and the only other non-test class using it was the
    `SslContextProvider`. Both indications that this should be one class.

--- server/src/main/java/io/crate/protocols/ssl/SslContextProvider.java
+++ server/src/main/java/io/crate/protocols/ssl/SslContextProvider.java
@@ -22,12 +22,39 @@
 
 package io.crate.protocols.ssl;
 
-import io.netty.handler.ssl.SslContext;
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.security.Key;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.UnrecoverableKeyException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.List;
+
+import javax.net.ssl.KeyManager;
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.TrustManagerFactory;
+import java.security.cert.Certificate;
+import java.security.cert.X509Certificate;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.common.inject.Singleton;
 import org.elasticsearch.common.settings.Settings;
+
+import io.crate.auth.AuthSettings;
+import io.crate.common.Optionals;
+import io.netty.handler.ssl.ApplicationProtocolConfig;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.SslProvider;
 
 @Singleton
 public class SslContextProvider {
@@ -49,7 +76,7 @@ public class SslContextProvider {
             synchronized (this) {
                 localRef = sslContext;
                 if (localRef == null) {
-                    sslContext = localRef = SslConfiguration.buildSslContext(this.settings);
+                    sslContext = localRef = buildSslContext();
                 }
             }
         }
@@ -58,8 +85,140 @@ public class SslContextProvider {
 
     public void reloadSslContext() {
         synchronized (this) {
-            sslContext = SslConfiguration.buildSslContext(this.settings);
+            sslContext = buildSslContext();
             LOGGER.info("SSL configuration is reloaded.");
         }
+    }
+
+    private SslContext buildSslContext() {
+        var keystorePath = SslConfigSettings.SSL_KEYSTORE_FILEPATH.setting().get(settings);
+        var keystorePass = SslConfigSettings.SSL_KEYSTORE_PASSWORD.setting().get(settings).toCharArray();
+        var keystoreKeyPass = SslConfigSettings.SSL_KEYSTORE_KEY_PASSWORD.setting().get(settings).toCharArray();
+
+        var trustStorePath = SslConfigSettings.SSL_TRUSTSTORE_FILEPATH.setting().get(settings);
+        var trustStorePass = SslConfigSettings.SSL_TRUSTSTORE_PASSWORD.setting().get(settings).toCharArray();
+
+        try {
+            var keyStore = loadKeyStore(keystorePath, keystorePass);
+            var keyManagers = createKeyManagers(keyStore, keystoreKeyPass);
+
+            var trustStore = Optionals.of(() -> loadKeyStore(trustStorePath, trustStorePass));
+            var trustManagers = trustStore
+                .map(SslContextProvider::createTrustManagers)
+                .orElseGet(() -> new TrustManager[0]);
+
+            // Use the newest SSL standard which is (at the time of writing) TLSv1.2
+            // If we just specify "TLS" here, it depends on the JVM implementation which version we'll get.
+            SSLContext sslContext = SSLContext.getInstance("TLSv1.2");
+            sslContext.init(keyManagers, trustManagers, null);
+
+            var keyStoreRootCerts = getRootCertificates(keyStore);
+            var keyStoreCertChain = getCertificateChain(keyStore);
+            var trustStoreRootCerts = trustStore
+                .map(SslContextProvider::getRootCertificates)
+                .orElseGet(() -> new X509Certificate[0]);
+            PrivateKey privateKey = getPrivateKey(keyStore, keystoreKeyPass);
+            return SslContextBuilder
+                .forServer(privateKey, keyStoreCertChain)
+                .ciphers(List.of(sslContext.createSSLEngine().getEnabledCipherSuites()))
+                .applicationProtocolConfig(ApplicationProtocolConfig.DISABLED)
+                .clientAuth(AuthSettings.resolveClientAuth(settings))
+                .trustManager(concat(keyStoreRootCerts, trustStoreRootCerts))
+                .sessionCacheSize(0)
+                .sessionTimeout(0)
+                .startTls(false)
+                .sslProvider(SslProvider.JDK)
+                .build();
+        } catch (SslConfigurationException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new SslConfigurationException("Failed to build SSL configuration: " + e.getMessage(), e);
+        }
+    }
+
+    static KeyStore loadKeyStore(String path, char[] pass) throws Exception {
+        KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+        try (var stream = new BufferedInputStream(new FileInputStream(path))) {
+            keyStore.load(stream, pass);
+        }
+        return keyStore;
+    }
+
+    static KeyManager[] createKeyManagers(KeyStore keyStore, char[] pass) throws Exception {
+        var keyFactory = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+        keyFactory.init(keyStore, pass);
+        return keyFactory.getKeyManagers();
+    }
+
+    private static TrustManager[] createTrustManagers(KeyStore keyStore) {
+        TrustManagerFactory trustFactory;
+        try {
+            trustFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+            trustFactory.init(keyStore);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return trustFactory.getTrustManagers();
+    }
+
+    private static <T> T[] concat(T[] xs, T[] ys) {
+        T[] result = Arrays.copyOf(xs, xs.length + ys.length);
+        System.arraycopy(ys, 0, result, xs.length, ys.length);
+        return result;
+    }
+
+    static X509Certificate[] getRootCertificates(KeyStore keyStore) {
+        ArrayList<X509Certificate> certs = new ArrayList<>();
+        try {
+            Enumeration<String> aliases = keyStore.aliases();
+            while (aliases.hasMoreElements()) {
+                String alias = aliases.nextElement();
+                if (keyStore.isCertificateEntry(alias)) {
+                    var cert = (X509Certificate) keyStore.getCertificate(alias);
+                    if (cert != null) {
+                        certs.add(cert);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return certs.toArray(new X509Certificate[0]);
+    }
+
+    static X509Certificate[] getCertificateChain(KeyStore keyStore) {
+        ArrayList<X509Certificate> certs = new ArrayList<>();
+        try {
+            Enumeration<String> aliases = keyStore.aliases();
+            while (aliases.hasMoreElements()) {
+                String alias = aliases.nextElement();
+                if (keyStore.isKeyEntry(alias)) {
+                    Certificate[] certificateChain = keyStore.getCertificateChain(alias);
+                    if (certificateChain != null) {
+                        for (Certificate certificate : certificateChain) {
+                            certs.add((X509Certificate) certificate);
+                        }
+                    }
+
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return certs.toArray(new X509Certificate[0]);
+    }
+
+    static PrivateKey getPrivateKey(KeyStore keyStore, char[] password) throws KeyStoreException, UnrecoverableKeyException, NoSuchAlgorithmException {
+        Enumeration<String> aliases = keyStore.aliases();
+        while (aliases.hasMoreElements()) {
+            String alias = aliases.nextElement();
+            if (keyStore.isKeyEntry(alias)) {
+                Key key = keyStore.getKey(alias, password);
+                if (key instanceof PrivateKey) {
+                    return (PrivateKey) key;
+                }
+            }
+        }
+        throw new KeyStoreException("No fitting private key found in keyStore");
     }
 }
--- server/src/test/java/io/crate/protocols/ssl/SslContextProviderTest.java
+++ server/src/test/java/io/crate/protocols/ssl/SslContextProviderTest.java
@@ -22,25 +22,44 @@
 
 package io.crate.protocols.ssl;
 
-import org.elasticsearch.test.ESTestCase;
-import io.netty.handler.ssl.SslContext;
-import org.elasticsearch.common.settings.Settings;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.Test;
-
-import java.io.File;
-import java.io.IOException;
-import java.security.KeyStore;
-import java.security.Security;
-
-import static io.crate.protocols.ssl.SslConfigurationTest.getAbsoluteFilePathFromClassPath;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.hasItem;
 import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.security.KeyStore;
+import java.security.PrivateKey;
+import java.security.Security;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.KeyManager;
+import javax.net.ssl.SSLContext;
+
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.test.ESTestCase;
+import org.hamcrest.Matchers;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import io.netty.handler.ssl.SslContext;
+
 public class SslContextProviderTest extends ESTestCase {
+
+    private static final String KEYSTORE_PASSWORD = "keystorePassword";
+    private static final String KEYSTORE_KEY_PASSWORD = "serverKeyPassword";
+    private static final String TRUSTSTORE_PASSWORD = "truststorePassword";
+    private static final String ROOT_CA_ALIAS = "theCAroot";
 
     private static File trustStoreFile;
     private static File keyStoreFile;
@@ -87,5 +106,102 @@ public class SslContextProviderTest extends ESTestCase {
         assertThat(sslContext, instanceOf(SslContext.class));
         assertThat(sslContext.isServer(), is(true));
         assertThat(sslContext.cipherSuites(), not(empty()));
+    }
+
+    @Test
+    public void test_netty_ssl_context_can_be_built_and_doesnt_override_default_context() throws Exception {
+        var defaultSSLContext = SSLContext.getDefault();
+        Settings settings = Settings.builder()
+            .put(SslConfigSettings.SSL_TRUSTSTORE_FILEPATH_SETTING_NAME, trustStoreFile.getAbsolutePath())
+            .put(SslConfigSettings.SSL_TRUSTSTORE_PASSWORD_SETTING_NAME, TRUSTSTORE_PASSWORD)
+            .put(SslConfigSettings.SSL_KEYSTORE_FILEPATH_SETTING_NAME, keyStoreFile.getAbsolutePath())
+            .put(SslConfigSettings.SSL_KEYSTORE_PASSWORD_SETTING_NAME, KEYSTORE_PASSWORD)
+            .put(SslConfigSettings.SSL_KEYSTORE_KEY_PASSWORD_SETTING_NAME, KEYSTORE_KEY_PASSWORD)
+        .build();
+        SslContext sslContext = new SslContextProvider(settings).getSslContext();
+        assertThat(sslContext.isServer(), is(true));
+        assertThat(sslContext.cipherSuites(), not(empty()));
+        // check that we don't offer NULL ciphers which do not encrypt
+        assertThat(sslContext.cipherSuites(), not(hasItem(containsString("NULL"))));
+
+        assertThat(defaultSSLContext, Matchers.sameInstance(SSLContext.getDefault()));
+    }
+
+    @Test
+    public void testKeyStoreLoading() throws Exception {
+        KeyStore keyStore = SslContextProvider.loadKeyStore(keyStoreFile.getAbsolutePath(), KEYSTORE_PASSWORD.toCharArray());
+        assertThat(keyStore.getType(), is("jks"));
+        assertThat(keyStore.getCertificate(ROOT_CA_ALIAS), notNullValue());
+
+        KeyManager[] keyManagers = SslContextProvider.createKeyManagers(keyStore, KEYSTORE_KEY_PASSWORD.toCharArray());
+        assertThat(keyManagers.length, is(1));
+    }
+
+    @Test
+    public void testKeyStoreLoadingFailWrongPassword() throws Exception {
+        expectedException.expect(IOException.class);
+        expectedException.expectMessage("Keystore was tampered with, or password was incorrect");
+
+        SslContextProvider.loadKeyStore(keyStoreFile.getAbsolutePath(), "wrongpassword".toCharArray());
+    }
+
+    @Test
+    public void testKeyStoreLoadingFailWrongKeyPassword() throws Exception {
+        KeyStore keyStore = SslContextProvider.loadKeyStore(keyStoreFile.getAbsolutePath(), KEYSTORE_PASSWORD.toCharArray());
+
+        expectedException.expect(UnrecoverableKeyException.class);
+        expectedException.expectMessage("Cannot recover key");
+        SslContextProvider.createKeyManagers(keyStore, "wrongpassword".toCharArray());
+    }
+
+    @Test
+    public void testExportRootCertificates() throws Exception {
+        KeyStore keyStore = SslContextProvider.loadKeyStore(keyStoreFile.getAbsolutePath(), KEYSTORE_PASSWORD.toCharArray());
+
+        X509Certificate[] certificates = SslContextProvider.getRootCertificates(keyStore);
+        assertThat(certificates.length, is(1));
+
+        assertThat(certificates[0].getIssuerDN().getName(), containsString("CN=*.crate.io"));
+        assertThat(certificates[0].getNotAfter().getTime(), is(4651463625000L));
+    }
+
+    @Test
+    public void testExportServerCertChain() throws Exception {
+        KeyStore keyStore = SslContextProvider.loadKeyStore(keyStoreFile.getAbsolutePath(), KEYSTORE_PASSWORD.toCharArray());
+
+        X509Certificate[] certificates = SslContextProvider.getCertificateChain(keyStore);
+
+        assertThat(certificates.length, is(4));
+        assertThat(certificates[0].getIssuerDN().getName(), containsString("CN=*.crate.io"));
+        assertThat(certificates[1].getIssuerDN().getName(), containsString("CN=*.crate.io"));
+        assertThat(certificates[2].getIssuerDN().getName(), containsString("CN=*.crate.io"));
+        assertThat(certificates[3].getIssuerDN().getName(), containsString("CN=*.crate.io"));
+        assertThat(certificates[0].getSubjectDN().getName(), containsString("CN=localhost"));
+        assertThat(certificates[0].getSubjectDN().getName(), containsString("OU=Testing Department"));
+        assertThat(certificates[0].getSubjectDN().getName(), containsString("L=San Francisco"));
+        assertThat(certificates[1].getSubjectDN().getName(), containsString("CN=*.crate.io"));
+        assertThat(certificates[1].getSubjectDN().getName(), containsString("OU=Cryptography Department"));
+        assertThat(certificates[1].getSubjectDN().getName(), containsString("L=Dornbirn"));
+        assertThat(certificates[2].getSubjectDN().getName(), containsString("CN=ssl.crate.io"));
+        assertThat(certificates[2].getSubjectDN().getName(), containsString("OU=Cryptography Department"));
+        assertThat(certificates[2].getSubjectDN().getName(), containsString("L=Berlin"));
+        assertThat(certificates[3].getSubjectDN().getName(), containsString("CN=*.crate.io"));
+        assertThat(certificates[3].getSubjectDN().getName(), containsString("OU=Cryptography Department"));
+        assertThat(certificates[3].getSubjectDN().getName(), containsString("L=Dornbirn"));
+    }
+
+    @Test
+    public void testExportDecryptedKey() throws Exception {
+        KeyStore keyStore = SslContextProvider.loadKeyStore(keyStoreFile.getAbsolutePath(), KEYSTORE_PASSWORD.toCharArray());
+        PrivateKey privateKey = SslContextProvider.getPrivateKey(keyStore, KEYSTORE_KEY_PASSWORD.toCharArray());
+        assertThat(privateKey, Matchers.notNullValue());
+    }
+
+    public static File getAbsoluteFilePathFromClassPath(final String fileNameFromClasspath) throws IOException {
+        final URL fileUrl = SslContextProviderTest.class.getClassLoader().getResource(fileNameFromClasspath);
+        if (fileUrl == null) {
+            throw new FileNotFoundException("Resource was not found: " + fileNameFromClasspath);
+        }
+        return new File(URLDecoder.decode(fileUrl.getFile(), StandardCharsets.UTF_8));
     }
 }
