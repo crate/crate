f65cfe12ad0db8b0d69d99dcf2dc9698e2a49b9f
Michael Kleen <michael.kleen@crate.io>
2021-04-16 11:54:31 +0200

    bp: Always use last properly persisted metadata as previous state
    https://github.com/elastic/elasticsearch/commit/8360248

--- server/src/main/java/org/elasticsearch/gateway/GatewayMetaState.java
+++ server/src/main/java/org/elasticsearch/gateway/GatewayMetaState.java
@@ -281,7 +281,7 @@ public class GatewayMetaState {
                     incrementalClusterStateWriter.setCurrentTerm(event.state().term());
                 }
 
-                incrementalClusterStateWriter.updateClusterState(event.state(), event.previousState());
+                incrementalClusterStateWriter.updateClusterState(event.state());
                 incrementalClusterStateWriter.setIncrementalWrite(true);
             } catch (WriteStateException e) {
                 LOGGER.warn("Exception occurred when storing new meta data", e);
@@ -323,9 +323,9 @@ public class GatewayMetaState {
         @Override
         public void setLastAcceptedState(ClusterState clusterState) {
             try {
-                final ClusterState previousClusterState = incrementalClusterStateWriter.getPreviousClusterState();
-                incrementalClusterStateWriter.setIncrementalWrite(previousClusterState.term() == clusterState.term());
-                incrementalClusterStateWriter.updateClusterState(clusterState, previousClusterState);
+                incrementalClusterStateWriter.setIncrementalWrite(
+                    incrementalClusterStateWriter.getPreviousClusterState().term() == clusterState.term());
+                incrementalClusterStateWriter.updateClusterState(clusterState);
             } catch (WriteStateException e) {
                 LOGGER.error(new ParameterizedMessage("Failed to set last accepted state with version {}", clusterState.version()), e);
                 e.rethrowAsErrorOrUncheckedException();
