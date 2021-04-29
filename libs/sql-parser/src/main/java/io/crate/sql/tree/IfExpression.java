2d752280ec5fefa2ba74ba2ae8229d978f51456f
Naomi Slater <23469+mechanomi@users.noreply.github.com>
2021-04-29 19:53:02 +0200

    Update license boilerplates to standardize commercial clause
    Specifically, this change updates (and standardizes) boilerplates that do not
    contain the phrase "commercial license agreement".
    
    This change was done by hand (using `grep`). For every file that matched my
    inclusion criteria, I replaced the whole boilerplate with the full correct
    version.

--- libs/sql-parser/src/main/java/io/crate/sql/tree/IfExpression.java
+++ libs/sql-parser/src/main/java/io/crate/sql/tree/IfExpression.java
@@ -1,5 +1,5 @@
 /*
- * Licensed to CRATE Technology GmbH ("Crate") under one or more contributor
+ * Licensed to Crate.io GmbH ("Crate") under one or more contributor
  * license agreements.  See the NOTICE file distributed with this work for
  * additional information regarding copyright ownership.  Crate licenses
  * this file to you under the Apache License, Version 2.0 (the "License");
