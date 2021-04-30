e431c974ffaf63ef71e046d42aa3d97f6d6eb959
Naomi Slater <23469+mechanomi@users.noreply.github.com>
2021-04-30 11:15:28 +0200

    Update license boilerplates to remove "Technology" from company name
    Specifically, this change updates (and standardizes) boilerplates that used the
    word "Technology" when referencing "Crate.io GmbH" (e.g., "CRATE Technology
    GmbH").
    
    This change was done using my editor's multi-file find and replace
    functionality. For every file that matched my inclusion criteria, I replaced
    the whole boilerplate with the full correct version.

--- blackbox/test_decommission.py
+++ blackbox/test_decommission.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8; -*-
 #
-# Licensed to CRATE Technology GmbH ("Crate") under one or more contributor
+# Licensed to Crate.io GmbH ("Crate") under one or more contributor
 # license agreements.  See the NOTICE file distributed with this work for
 # additional information regarding copyright ownership.  Crate licenses
 # this file to you under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.  You may
 # obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#   http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
