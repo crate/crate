f1c38ca12db3888c2ed3bd7a866a6491ba851b8d
Michael Kleen <michael.kleen@crate.io>
2021-05-17 14:57:42 +0200

    bp: Separate translog from index deletion conditions
    https://github.com/elastic/elasticsearch/commit/82ab1bc1ff4

--- server/src/main/java/org/elasticsearch/index/engine/InternalEngine.java
+++ server/src/main/java/org/elasticsearch/index/engine/InternalEngine.java
@@ -558,8 +558,8 @@ public class InternalEngine extends Engine {
     private void revisitIndexDeletionPolicyOnTranslogSynced() throws IOException {
         if (combinedDeletionPolicy.hasUnreferencedCommits()) {
             indexWriter.deleteUnusedFiles();
-            translog.trimUnreferencedReaders();
         }
+        translog.trimUnreferencedReaders();
     }
 
     @Override
