ca3baceed0f784e7de01dafbd8adef0db97bdd1c
Mathias Fussenegger <f.mathias@zignar.net>
2021-06-01 14:36:56 +0200

    Remove phrase-/multiPhaseQuery from MappedFieldType
    These two methods are only used in the `MatchQuery` and were only
    implemented in the StringFieldType.
    
    It isn't necessary to have them in an abstract class that is used for
    all types.
    
    Motivated by https://github.com/crate/crate/issues/10025

--- server/src/main/java/org/elasticsearch/index/mapper/StringFieldType.java
+++ server/src/main/java/org/elasticsearch/index/mapper/StringFieldType.java
@@ -19,14 +19,11 @@
 
 package org.elasticsearch.index.mapper;
 
-import org.apache.lucene.analysis.TokenStream;
-import org.apache.lucene.analysis.tokenattributes.PositionIncrementAttribute;
-import org.apache.lucene.analysis.tokenattributes.TermToBytesRefAttribute;
+import java.util.List;
+
 import org.apache.lucene.index.Term;
 import org.apache.lucene.search.FuzzyQuery;
-import org.apache.lucene.search.MultiPhraseQuery;
 import org.apache.lucene.search.MultiTermQuery;
-import org.apache.lucene.search.PhraseQuery;
 import org.apache.lucene.search.PrefixQuery;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.TermInSetQuery;
@@ -35,10 +32,6 @@ import org.apache.lucene.util.BytesRef;
 import org.elasticsearch.common.lucene.BytesRefs;
 import org.elasticsearch.common.unit.Fuzziness;
 import org.elasticsearch.index.query.QueryShardContext;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 /** Base class for {@link MappedFieldType} implementations that use the same
  * representation for internal index terms as the external representation so
@@ -88,64 +81,5 @@ public abstract class StringFieldType extends TermBasedFieldType {
             lowerTerm == null ? null : indexedValueForSearch(lowerTerm),
             upperTerm == null ? null : indexedValueForSearch(upperTerm),
             includeLower, includeUpper);
-    }
-
-    @Override
-    public Query phraseQuery(String field, TokenStream stream, int slop, boolean enablePosIncrements) throws IOException {
-
-        PhraseQuery.Builder builder = new PhraseQuery.Builder();
-        builder.setSlop(slop);
-
-        TermToBytesRefAttribute termAtt = stream.getAttribute(TermToBytesRefAttribute.class);
-        PositionIncrementAttribute posIncrAtt = stream.getAttribute(PositionIncrementAttribute.class);
-        int position = -1;
-
-        stream.reset();
-        while (stream.incrementToken()) {
-            if (enablePosIncrements) {
-                position += posIncrAtt.getPositionIncrement();
-            } else {
-                position += 1;
-            }
-            builder.add(new Term(field, termAtt.getBytesRef()), position);
-        }
-
-        return builder.build();
-    }
-
-    @Override
-    public Query multiPhraseQuery(String field, TokenStream stream, int slop, boolean enablePositionIncrements) throws IOException {
-
-        MultiPhraseQuery.Builder mpqb = new MultiPhraseQuery.Builder();
-        mpqb.setSlop(slop);
-
-        TermToBytesRefAttribute termAtt = stream.getAttribute(TermToBytesRefAttribute.class);
-
-        PositionIncrementAttribute posIncrAtt = stream.getAttribute(PositionIncrementAttribute.class);
-        int position = -1;
-
-        List<Term> multiTerms = new ArrayList<>();
-        stream.reset();
-        while (stream.incrementToken()) {
-            int positionIncrement = posIncrAtt.getPositionIncrement();
-
-            if (positionIncrement > 0 && multiTerms.size() > 0) {
-                if (enablePositionIncrements) {
-                    mpqb.add(multiTerms.toArray(new Term[0]), position);
-                } else {
-                    mpqb.add(multiTerms.toArray(new Term[0]));
-                }
-                multiTerms.clear();
-            }
-            position += positionIncrement;
-            multiTerms.add(new Term(field, termAtt.getBytesRef()));
-        }
-
-        if (enablePositionIncrements) {
-            mpqb.add(multiTerms.toArray(new Term[0]), position);
-        } else {
-            mpqb.add(multiTerms.toArray(new Term[0]));
-        }
-        return mpqb.build();
     }
 }
