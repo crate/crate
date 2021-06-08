24240299a87755dcb72f42e7a080e60f8e9140b1
Naomi Slater <23469+mechanomi@users.noreply.github.com>
2021-06-08 18:11:44 +0200

    Fix Sphinx extensions configuration
    The `sphinx-docs-theme` specifies a list of default Sphinx extensions in the
    [src/crate/theme/rtd/conf/__init__.py][1] file.
    
    The `conf.py` file in this Sphinx project also specified a list of Sphinx
    extensions. However, the way this was done meant that a direct assignment was
    overwriting the default value of the extensions variable (imported at the start
    of the file).
    
    As a consequence, the CrateDB Reference has not yet picked up any of the
    following improvements:
    
    - Intersphinx support
    - PlantUML support
    - Open Graph support
    - [Sphinx copy button][2] support
    
    This change replaces the direct assignment with a list append operation and
    removes a redundant extension (`sphinx_sitemap`) that is already present in the
    default list.
    
    I have verified that as a result of this change, the Sphinx copy button
    correctly appears on code examples as expected ([which was previously not the
    case][3]).
    
    [1]: https://github.com/crate/crate-docs-theme/blob/main/src/crate/theme/rtd/conf/__init__.py#L32
    [2]: https://pypi.org/project/sphinx-copybutton/
    [3]: https://github.com/crate/crate-docs-theme/issues/258#issuecomment-852080493
    
    (cherry picked from commit 41976d829b636e0cc3331f1e1fce0f8a9b37c3f2)

--- blackbox/docs/conf.py
+++ blackbox/docs/conf.py
@@ -2,4 +2,4 @@ from crate.theme.rtd.conf.crate_reference import *
 
 exclude_patterns = ['out/**', 'tmp/**', 'eggs/**', 'requirements.txt', 'README.rst']
 
-extensions = ['crate.sphinx.csv', 'sphinx_sitemap']
+extensions.append('crate.sphinx.csv')
