b570ecfb8867b0faa86c110dd9795ff8d5837eb7
Andreas Motl <andreas.motl@crate.io>
2021-06-09 20:21:12 +0200

    Documentation: Fix "html_extra_path entry '_extra' does not exist"

--- blackbox/docs/conf.py
+++ blackbox/docs/conf.py
@@ -3,3 +3,5 @@ from crate.theme.rtd.conf.crate_reference import *
 exclude_patterns = ['out/**', 'tmp/**', 'eggs/**', 'requirements.txt', 'README.rst']
 
 extensions.append('crate.sphinx.csv')
+
+html_extra_path = []
