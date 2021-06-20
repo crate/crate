3ca707232484f0715d8504375f23d409813f30b9
Michael Kleen <michael.kleen@crate.io>
2021-06-20 09:26:20 +0200

    Update Lucene to 8.9.0
    https://lucene.apache.org/core/8_9_0/changes/Changes.html

--- server/src/main/java/org/elasticsearch/Version.java
+++ server/src/main/java/org/elasticsearch/Version.java
@@ -115,7 +115,7 @@ public class Version implements Comparable<Version>, ToXContentFragment {
     public static final Version V_4_5_1 = new Version(7_05_01_99, false, org.apache.lucene.util.Version.LUCENE_8_7_0);
     public static final Version V_4_5_2 = new Version(7_05_02_99, false, org.apache.lucene.util.Version.LUCENE_8_7_0);
 
-    public static final Version V_4_6_0 = new Version(7_06_00_99, true, org.apache.lucene.util.Version.LUCENE_8_8_2);
+    public static final Version V_4_6_0 = new Version(7_06_00_99, true, org.apache.lucene.util.Version.LUCENE_8_9_0);
 
     public static final Version CURRENT = V_4_6_0;
 
