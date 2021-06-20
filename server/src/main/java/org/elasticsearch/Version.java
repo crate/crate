3d45eb8e8261caac430f80a420ef388b4bd8ac84
Michael Kleen <michael.kleen@crate.io>
2021-06-20 09:26:20 +0200

    Update Lucene to 8.9.0
    https://lucene.apache.org/core/8_9_0/changes/Changes.html
    
    This update was previously reverted due to a performance regression.
    (https://github.com/crate/crate/pull/11526)
    
    We changed DocValuesFormat on fields to `BEST_SPEED` to prevent the
    performance regression (at the cost of increased disk space
    requirements).
    (https://github.com/crate/crate/pull/11658)
    
    Benchmarks which previously showed a ~80% slow-down are now looking
    good:
    
         Results (server side duration in ms)
        V1: 4.7.0-4d1a9894c2ad8b058f47b27db02f044ef63236a1
        V2: 4.7.0-a9118156c6780243514e4089b9f238580f347bba
    
        Q: select hyperloglog_distinct("cCode") from uservisits
        C: 1
        | Version |         Mean ±    Stdev |        Min |     Median |         Q3 |        Max |
        |   V1    |      419.403 ±  166.596 |    372.875 |    389.166 |    396.250 |   1554.924 |
        |   V2    |      426.001 ±  126.988 |    386.096 |    401.648 |    410.437 |   1291.026 |
        ├---------┴-------------------------┴------------┴------------┴------------┴------------┘
        |               +   1.56%                           +   3.16%
        There is a 17.58% probability that the observed difference is not random, and the best estimate of that difference is 1.56%
        The test has no statistical significance
    
        System/JVM Metrics (durations in ms, byte-values in MB)
            |    YOUNG GC            |       OLD GC           |      HEAP         |     ALLOC
            |  cnt      avg      max |  cnt      avg      max |  initial     used |     rate      total
         V1 |    3     7.36    11.41 |    0     0.00     0.00 |     4295       70 |     0.00          0
         V2 |    1     4.85     4.85 |    0     0.00     0.00 |     4295      765 |     0.00          0

--- server/src/main/java/org/elasticsearch/Version.java
+++ server/src/main/java/org/elasticsearch/Version.java
@@ -121,7 +121,7 @@ public class Version implements Comparable<Version>, ToXContentFragment {
     public static final Version V_4_6_0 = new Version(7_06_00_99, false, org.apache.lucene.util.Version.LUCENE_8_8_2);
     public static final Version V_4_6_1 = new Version(7_06_01_99, false, org.apache.lucene.util.Version.LUCENE_8_8_2);
 
-    public static final Version V_4_7_0 = new Version(7_07_00_99, true, org.apache.lucene.util.Version.LUCENE_8_8_2);
+    public static final Version V_4_7_0 = new Version(7_07_00_99, true, org.apache.lucene.util.Version.LUCENE_8_9_0);
 
     public static final Version CURRENT = V_4_7_0;
 
