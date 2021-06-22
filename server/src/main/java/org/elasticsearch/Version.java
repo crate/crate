3237313e14d95fbb7ce30de9faaea0d82811734f
Mathias Fussenegger <f.mathias@zignar.net>
2021-06-22 10:51:15 +0200

    Reflect 4.5.3 release

--- server/src/main/java/org/elasticsearch/Version.java
+++ server/src/main/java/org/elasticsearch/Version.java
@@ -114,7 +114,7 @@ public class Version implements Comparable<Version>, ToXContentFragment {
     public static final Version V_4_5_0 = new Version(7_05_00_99, false, org.apache.lucene.util.Version.LUCENE_8_7_0);
     public static final Version V_4_5_1 = new Version(7_05_01_99, false, org.apache.lucene.util.Version.LUCENE_8_7_0);
     public static final Version V_4_5_2 = new Version(7_05_02_99, false, org.apache.lucene.util.Version.LUCENE_8_7_0);
-    public static final Version V_4_5_3 = new Version(7_05_03_99, true, org.apache.lucene.util.Version.LUCENE_8_7_0);
+    public static final Version V_4_5_3 = new Version(7_05_03_99, false, org.apache.lucene.util.Version.LUCENE_8_7_0);
 
     public static final Version V_4_6_0 = new Version(7_06_00_99, true, org.apache.lucene.util.Version.LUCENE_8_9_0);
 
