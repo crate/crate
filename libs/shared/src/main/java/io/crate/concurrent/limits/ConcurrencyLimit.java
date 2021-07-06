crate/crate

commit e1aa90635a492922563cf6f8e015b69cf6e85254

Improve insert-from-query throttle/ingestion control behavior
This improves the current throttling mechanism by dynamically adapting
the concurency limit and by taking the round trip time into
considerating when pausing operations.

The mechanism so far was too aggressive in some cases and led to a
mostly idle cluster taking a long time to process insert-from-query
operations.

The behavior so far:

    n1:
     q1  -> inserts to n2
     q2  -> inserts to n2
     q3  -> inserts to n2
     q4  -> inserts to n2
     q5  -> inserts to n2; concurrency limit reached, pause
     ... now q1 to q5 start competing and all but one go to sleep (starts at 1000ms, with exponential backoff)

New behavior:

- The concurrency limit is dynamic. (Currently between 2 - 2000, we might
  want to tune these numbers)

- We track the round-trip time for the requests by target node.
  This round-trip time is used to adjust the concurrency limit and also
  as sleep intervall in case the number of inflight requests exceeds the
  concurreny limit.

libs/shared/src/main/java/io/crate/concurrent/limits/ConcurrencyLimit.java
@@ -1,0 +1,221 @@
+/**
+ * Copyright 2018 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.crate.concurrent.limits;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Function;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+/**
+ * This class is based on the `Gradient2Limit` from netflix/concurrency-limits
+ *
+ *
+ * Concurrency limit algorithm that adjusts the limit based on the gradient of change of the current average RTT and
+ * a long term exponentially smoothed average RTT.  Unlike traditional congestion control algorithms we use average
+ * instead of minimum since RPC methods can be very bursty due to various factors such as non-homogenous request
+ * processing complexity as well as a wide distribution of data size.  We have also found that using minimum can result
+ * in an bias towards an impractically low base RTT resulting in excessive load shedding.  An exponential decay is
+ * applied to the base RTT so that the value is kept stable yet is allowed to adapt to long term changes in latency
+ * characteristics.
+ *
+ * The core algorithm re-calculates the limit every sampling window (ex. 1 second) using the formula
+ *
+ *      // Calculate the gradient limiting to the range [0.5, 1.0] to filter outliers
+ *      gradient = max(0.5, min(1.0, longtermRtt / currentRtt));
+ *
+ *      // Calculate the new limit by applying the gradient and allowing for some queuing
+ *      newLimit = gradient * currentLimit + queueSize;
+ *
+ *      // Update the limit using a smoothing factor (default 0.2)
+ *      newLimit = currentLimit * (1-smoothing) + newLimit * smoothing
+ *
+ * The limit can be in one of three main states
+ *
+ * 1.  Steady state
+ *
+ * In this state the average RTT is very stable and the current measurement whipsaws around this value, sometimes reducing
+ * the limit, sometimes increasing it.
+ *
+ * 2.  Transition from steady state to load
+ *
+ * In this state either the RPS to latency has spiked. The gradient is {@literal <} 1.0 due to a growing request queue that
+ * cannot be handled by the system. Excessive requests and rejected due to the low limit. The baseline RTT grows using
+ * exponential decay but lags the current measurement, which keeps the gradient {@literal <} 1.0 and limit low.
+ *
+ * 3.  Transition from load to steady state
+ *
+ * In this state the system goes back to steady state after a prolonged period of excessive load.  Requests aren't rejected
+ * and the sample RTT remains low. During this state the long term RTT may take some time to go back to normal and could
+ * potentially be several multiples higher than the current RTT.
+ */
+public final class ConcurrencyLimit {
+
+    private static final Logger LOG = LogManager.getLogger(ConcurrencyLimit.class);
+
+    public static class Builder {
+        private int initialLimit = 50;
+        private int minLimit = 2;
+        private int maxConcurrency = 2000;
+
+        private double smoothing = 0.2;
+        private Function<Integer, Integer> queueSize = concurrency -> 200;
+        private int longWindow = 600;
+        private double rttTolerance = 1.5;
+
+        public ConcurrencyLimit build() {
+            return new ConcurrencyLimit(this);
+        }
+    }
+
+    public static ConcurrencyLimit newDefault() {
+        return new Builder().build();
+    }
+
+    /**
+     * Estimated concurrency limit based on our algorithm
+     */
+    private volatile double estimatedLimit;
+
+    /**
+     * Tracks a measurement of the short time, and more volatile, RTT meant to represent the current system latency
+     */
+    private long lastRtt;
+
+    /**
+     * Tracks a measurement of the long term, less volatile, RTT meant to represent the baseline latency.  When the system
+     * is under load this number is expect to trend higher.
+     */
+    private final Measurement longRtt;
+
+    /**
+     * Maximum allowed limit providing an upper bound failsafe
+     */
+    private final int maxLimit;
+
+    private final int minLimit;
+
+    private final Function<Integer, Integer> queueSize;
+
+    private final double smoothing;
+
+    private final double tolerance;
+
+    private volatile int limit;
+
+    private final AtomicInteger numInflight = new AtomicInteger();
+
+    private ConcurrencyLimit(Builder builder) {
+        this.limit = builder.initialLimit;
+        this.estimatedLimit = builder.initialLimit;
+        this.maxLimit = builder.maxConcurrency;
+        this.minLimit = builder.minLimit;
+        this.queueSize = builder.queueSize;
+        this.smoothing = builder.smoothing;
+        this.tolerance = builder.rttTolerance;
+        this.lastRtt = 0;
+        this.longRtt = new ExpAvgMeasurement(builder.longWindow, 10);
+    }
+
+    /**
+     * Start a sample, increasing the number of inflight operations and returning a
+     * monotonic time value which should be used for `onSample`
+     **/
+    public long startSample() {
+        numInflight.incrementAndGet();
+        return System.nanoTime();
+    }
+
+    public final synchronized void onSample(long startTime, boolean didDrop) {
+        long rtt = System.nanoTime() - startTime;
+        int newLimit = update(rtt, numInflight.decrementAndGet(), didDrop);
+        if (newLimit != limit) {
+            limit = newLimit;
+        }
+    }
+
+    public final int getLimit() {
+        return limit;
+    }
+
+    private int update(final long rtt, final int inflight, final boolean didDrop) {
+        final double queueSize = this.queueSize.apply((int)this.estimatedLimit);
+
+        this.lastRtt = rtt;
+        final double shortRtt = (double)rtt;
+        final double longRtt = this.longRtt.add(rtt);
+
+
+        // If the long RTT is substantially larger than the short RTT then reduce the long RTT measurement.
+        // This can happen when latency returns to normal after a prolonged prior of excessive load.  Reducing the
+        // long RTT without waiting for the exponential smoothing helps bring the system back to steady state.
+        if (longRtt / shortRtt > 2) {
+            this.longRtt.update(current -> current * 0.95);
+        }
+
+        // Don't grow the limit if we are app limited
+        if (inflight < estimatedLimit / 2) {
+            return (int) estimatedLimit;
+        }
+
+        // Rtt could be higher than rtt_noload because of smoothing rtt noload updates
+        // so set to 1.0 to indicate no queuing.  Otherwise calculate the slope and don't
+        // allow it to be reduced by more than half to avoid aggressive load-shedding due to
+        // outliers.
+        final double gradient = Math.max(0.5, Math.min(1.0, tolerance * longRtt / shortRtt));
+        double newLimit = estimatedLimit * gradient + queueSize;
+        newLimit = estimatedLimit * (1 - smoothing) + newLimit * smoothing;
+        newLimit = Math.max(minLimit, Math.min(maxLimit, newLimit));
+
+        if ((int)estimatedLimit != newLimit) {
+            LOG.debug("New limit={} shortRtt={} ms longRtt={} ms queueSize={} gradient={}",
+                    (int)newLimit,
+                    getLastRtt(TimeUnit.MICROSECONDS) / 1000.0,
+                    getRttNoLoad(TimeUnit.MICROSECONDS) / 1000.0,
+                    queueSize,
+                    gradient);
+        }
+
+        estimatedLimit = newLimit;
+
+        return (int)estimatedLimit;
+    }
+
+    public long getLastRtt(TimeUnit units) {
+        return units.convert(lastRtt, TimeUnit.NANOSECONDS);
+    }
+
+    public long getRttNoLoad(TimeUnit units) {
+        return units.convert((long) longRtt.get(), TimeUnit.NANOSECONDS);
+    }
+
+    @Override
+    public String toString() {
+        return "GradientLimit [limit=" + (int)estimatedLimit + "]";
+    }
+
+    public boolean exceedsLimit() {
+        return numInflight() > limit;
+    }
+
+    public int numInflight() {
+        return numInflight.get();
+    }
+}

server/src/main/java/io/crate/action/LimitedExponentialBackoff.java
@@ -22,14 +22,19 @@
 
 package io.crate.action;
 
-import org.elasticsearch.action.bulk.BackoffPolicy;
-import io.crate.common.unit.TimeValue;
-
 import java.util.Iterator;
 import java.util.NoSuchElementException;
+import java.util.concurrent.TimeUnit;
+
+import org.elasticsearch.action.bulk.BackoffPolicy;
+
+import io.crate.common.unit.TimeValue;
+import io.crate.concurrent.limits.ConcurrencyLimit;
 
 
 public class LimitedExponentialBackoff extends BackoffPolicy {
+
+    private static final int MAX_ITERS = 10_000;
 
     private final int firstDelayInMS;
     private final int maxIterations;
@@ -50,6 +55,33 @@ public class LimitedExponentialBackoff extends BackoffPolicy {
      */
     public static BackoffPolicy limitedExponential(int maxDelayInMS) {
         return new LimitedExponentialBackoff(10, Integer.MAX_VALUE, maxDelayInMS);
+    }
+
+    public static BackoffPolicy limitedExponential(ConcurrencyLimit concurrencyLimit) {
+        return new BackoffPolicy() {
+
+            int iterations = 0;
+
+            @Override
+            public Iterator<TimeValue> iterator() {
+                return new Iterator<>() {
+
+                    @Override
+                    public boolean hasNext() {
+                        return iterations < MAX_ITERS;
+                    }
+
+                    @Override
+                    public TimeValue next() {
+                        if (!hasNext()) {
+                            throw new NoSuchElementException("Iterator is exhausted");
+                        }
+                        iterations++;
+                        return TimeValue.timeValueNanos(concurrencyLimit.getLastRtt(TimeUnit.NANOSECONDS));
+                    }
+                };
+            }
+        };
     }
 
     private static class LimitedExponentialBackoffIterator implements Iterator<TimeValue> {

server/src/main/java/io/crate/execution/engine/indexing/ShardDMLExecutor.java
@@ -22,7 +22,6 @@
 
 package io.crate.execution.engine.indexing;
 
-import static io.crate.execution.jobs.NodeJobsCounter.MAX_NODE_CONCURRENT_OPERATIONS;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,6 +29,7 @@ import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import java.util.function.BiConsumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
@@ -41,11 +41,12 @@ import javax.annotation.Nullable;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.elasticsearch.action.ActionListener;
-import org.elasticsearch.action.bulk.BackoffPolicy;
+import org.elasticsearch.cluster.routing.ShardRouting;
 import org.elasticsearch.cluster.service.ClusterService;
 
 import io.crate.action.FutureActionListener;
 import io.crate.action.LimitedExponentialBackoff;
+import io.crate.concurrent.limits.ConcurrencyLimit;
 import io.crate.data.BatchIterator;
 import io.crate.data.BatchIterators;
 import io.crate.data.CollectionBucket;
@@ -55,7 +56,7 @@ import io.crate.exceptions.Exceptions;
 import io.crate.execution.dml.ShardRequest;
 import io.crate.execution.dml.ShardResponse;
 import io.crate.execution.engine.collect.CollectExpression;
-import io.crate.execution.jobs.NodeJobsCounter;
+import io.crate.execution.jobs.NodeLimits;
 import io.crate.execution.support.RetryListener;
 
 public class ShardDMLExecutor<TReq extends ShardRequest<TReq, TItem>,
@@ -66,7 +67,6 @@ public class ShardDMLExecutor<TReq extends ShardRequest<TReq, TItem>,
 
     private static final Logger LOGGER = LogManager.getLogger(ShardDMLExecutor.class);
 
-    private static final BackoffPolicy BACKOFF_POLICY = LimitedExponentialBackoff.limitedExponential(1000);
     public static final int DEFAULT_BULK_SIZE = 10_000;
 
     private final UUID jobId;
@@ -74,13 +74,14 @@ public class ShardDMLExecutor<TReq extends ShardRequest<TReq, TItem>,
     private final ScheduledExecutorService scheduler;
     private final Executor executor;
     private final CollectExpression<Row, ?> uidExpression;
-    private final NodeJobsCounter nodeJobsCounter;
+    private final NodeLimits nodeLimits;
     private final Supplier<TReq> requestFactory;
     private final Function<String, TItem> itemFactory;
     private final BiConsumer<TReq, ActionListener<ShardResponse>> operation;
-    private final String localNodeId;
     private final Collector<ShardResponse, TAcc, TResult> collector;
     private int numItems = -1;
+
+    private ClusterService clusterService;
 
     public ShardDMLExecutor(UUID jobId,
                             int bulkSize,
@@ -88,7 +89,7 @@ public class ShardDMLExecutor<TReq extends ShardRequest<TReq, TItem>,
                             Executor executor,
                             CollectExpression<Row, ?> uidExpression,
                             ClusterService clusterService,
-                            NodeJobsCounter nodeJobsCounter,
+                            NodeLimits nodeLimits,
                             Supplier<TReq> requestFactory,
                             Function<String, TItem> itemFactory,
                             BiConsumer<TReq, ActionListener<ShardResponse>> transportAction,
@@ -99,11 +100,11 @@ public class ShardDMLExecutor<TReq extends ShardRequest<TReq, TItem>,
         this.scheduler = scheduler;
         this.executor = executor;
         this.uidExpression = uidExpression;
-        this.nodeJobsCounter = nodeJobsCounter;
+        this.clusterService = clusterService;
+        this.nodeLimits = nodeLimits;
         this.requestFactory = requestFactory;
         this.itemFactory = itemFactory;
         this.operation = transportAction;
-        this.localNodeId = getLocalNodeId(clusterService);
         this.collector = collector;
     }
 
@@ -113,25 +114,39 @@ public class ShardDMLExecutor<TReq extends ShardRequest<TReq, TItem>,
         req.add(numItems, itemFactory.apply((String) uidExpression.value()));
     }
 
+    @Nullable
+    private String resolveNodeId(TReq request) {
+        // The primary shard might be moving to another node,
+        // so this is not 100% accurate but good enough for the congestion control purposes.
+        ShardRouting primaryShard = clusterService
+            .state()
+            .routingTable()
+            .shardRoutingTable(request.shardId())
+            .primaryShard();
+        return primaryShard == null ? null : primaryShard.currentNodeId();
+    }
+
     private CompletableFuture<TAcc> executeBatch(TReq request) {
+        ConcurrencyLimit nodeLimit = nodeLimits.get(resolveNodeId(request));
+        long startTime = nodeLimit.startSample();
         FutureActionListener<ShardResponse, TAcc> listener = new FutureActionListener<>((a) -> {
+            nodeLimit.onSample(startTime, false);
             TAcc acc = collector.supplier().get();
             collector.accumulator().accept(acc, a);
             return acc;
         });
-
-        nodeJobsCounter.increment(localNodeId);
-        CompletableFuture<TAcc> result = listener.whenComplete((r, f) -> nodeJobsCounter.decrement(localNodeId));
-        operation.accept(request, withRetry(request, listener));
-        return result;
+        operation.accept(request, withRetry(request, nodeLimit, listener));
+        return listener;
     }
 
-    private RetryListener<ShardResponse> withRetry(TReq request, FutureActionListener<ShardResponse, TAcc> listener) {
+    private RetryListener<ShardResponse> withRetry(TReq request,
+                                                   ConcurrencyLimit nodeLimit,
+                                                   FutureActionListener<ShardResponse, TAcc> listener) {
         return new RetryListener<>(
             scheduler,
             l -> operation.accept(request, l),
             listener,
-            BACKOFF_POLICY
+            LimitedExponentialBackoff.limitedExponential(nodeLimit)
         );
     }
 
@@ -143,10 +158,8 @@ public class ShardDMLExecutor<TReq extends ShardRequest<TReq, TItem>,
         // as soon as possible. We do not want to throttle based on the targets node counter in such cases.
         Predicate<TReq> shouldPause = ignored -> true;
         if (batchIterator.hasLazyResultSet()) {
-            shouldPause = ignored ->
-                nodeJobsCounter.getInProgressJobsForNode(localNodeId) >= MAX_NODE_CONCURRENT_OPERATIONS;
+            shouldPause = req -> nodeLimits.get(resolveNodeId(req)).exceedsLimit();
         }
-
         return new BatchIteratorBackpressureExecutor<>(
             jobId,
             scheduler,
@@ -156,7 +169,7 @@ public class ShardDMLExecutor<TReq extends ShardRequest<TReq, TItem>,
             collector.combiner(),
             collector.supplier().get(),
             shouldPause,
-            BACKOFF_POLICY
+            req -> nodeLimits.get(resolveNodeId(req)).getLastRtt(TimeUnit.MILLISECONDS)
         ).consumeIteratorAndExecute()
             .thenApply(collector.finisher());
     }

commit bbb121b8f47a6c62157ec436386268f2855a955d

Use long-rtt in insert back-off policy
The backoff policy used `concurrencyLimit.getLastRtt`. This could cause
requests to be delayed way longer than necessary. For example if a
partition gets created, the last round-trip time might have been long,
but that doesn't mean that the next requests would need to get throttled
that much.

This changed the policy to use `concurrencyLimit.getLongRtt`.

This fixed a regression introduced with https://github.com/crate/crate/commit/e1aa90635a492922563cf6f8e015b69cf6e85254

cr8 run-spec specs/partitioned_bulk_insert.py localhost:4200:

Before the change:

    Runtime (in ms):
        mean:    6873.725 ± 882.141
        min/max: 747.398 → 11790.370
    Percentile:
        50:   6951.108 ± 3182.490 (stdev)
        95:   11431.199
        99.9: 11790.370

After:

    Runtime (in ms):
        mean:    755.441 ± 66.171
        min/max: 459.070 → 1502.214
    Percentile:
        50:   765.613 ± 238.725 (stdev)
        95:   1264.758
        99.9: 1502.214

libs/shared/src/main/java/io/crate/concurrent/limits/ConcurrencyLimit.java
@@ -143,11 +143,14 @@ public final class ConcurrencyLimit {
         return System.nanoTime();
     }
 
-    public final synchronized void onSample(long startTime, boolean didDrop) {
+    public final void onSample(long startTime, boolean didDrop) {
         long rtt = System.nanoTime() - startTime;
-        int newLimit = update(rtt, numInflight.decrementAndGet(), didDrop);
-        if (newLimit != limit) {
-            limit = newLimit;
+        int decrementedNumInflight = numInflight.decrementAndGet();
+        synchronized (this) {
+            int newLimit = update(rtt, decrementedNumInflight, didDrop);
+            if (newLimit != limit) {
+                limit = newLimit;
+            }
         }
     }
 
@@ -185,12 +188,13 @@ public final class ConcurrencyLimit {
         newLimit = Math.max(minLimit, Math.min(maxLimit, newLimit));
 
         if ((int)estimatedLimit != newLimit) {
-            LOG.debug("New limit={} shortRtt={} ms longRtt={} ms queueSize={} gradient={}",
+            LOG.debug("New limit={} lastRtt={} ms longRtt={} ms queueSize={} gradient={} inflight={}",
                     (int)newLimit,
                     getLastRtt(TimeUnit.MICROSECONDS) / 1000.0,
-                    getRttNoLoad(TimeUnit.MICROSECONDS) / 1000.0,
+                    getLongRtt(TimeUnit.MICROSECONDS) / 1000.0,
                     queueSize,
-                    gradient);
+                    gradient,
+                    inflight);
         }
 
         estimatedLimit = newLimit;
@@ -202,7 +206,7 @@ public final class ConcurrencyLimit {
         return units.convert(lastRtt, TimeUnit.NANOSECONDS);
     }
 
-    public long getRttNoLoad(TimeUnit units) {
+    public long getLongRtt(TimeUnit units) {
         return units.convert((long) longRtt.get(), TimeUnit.NANOSECONDS);
     }
 

server/src/main/java/io/crate/action/LimitedExponentialBackoff.java
@@ -23,17 +23,13 @@ package io.crate.action;
 
 import java.util.Iterator;
 import java.util.NoSuchElementException;
-import java.util.concurrent.TimeUnit;
 
 import org.elasticsearch.action.bulk.BackoffPolicy;
 
 import io.crate.common.unit.TimeValue;
-import io.crate.concurrent.limits.ConcurrencyLimit;
 
 
 public class LimitedExponentialBackoff extends BackoffPolicy {
-
-    private static final int MAX_ITERS = 10_000;
 
     private final int firstDelayInMS;
     private final int maxIterations;
@@ -54,33 +50,6 @@ public class LimitedExponentialBackoff extends BackoffPolicy {
      */
     public static BackoffPolicy limitedExponential(int maxDelayInMS) {
         return new LimitedExponentialBackoff(10, Integer.MAX_VALUE, maxDelayInMS);
-    }
-
-    public static BackoffPolicy limitedExponential(ConcurrencyLimit concurrencyLimit) {
-        return new BackoffPolicy() {
-
-            int iterations = 0;
-
-            @Override
-            public Iterator<TimeValue> iterator() {
-                return new Iterator<>() {
-
-                    @Override
-                    public boolean hasNext() {
-                        return iterations < MAX_ITERS;
-                    }
-
-                    @Override
-                    public TimeValue next() {
-                        if (!hasNext()) {
-                            throw new NoSuchElementException("Iterator is exhausted");
-                        }
-                        iterations++;
-                        return TimeValue.timeValueNanos(concurrencyLimit.getLastRtt(TimeUnit.NANOSECONDS));
-                    }
-                };
-            }
-        };
     }
 
     private static class LimitedExponentialBackoffIterator implements Iterator<TimeValue> {

server/src/main/java/io/crate/execution/engine/indexing/ShardDMLExecutor.java
@@ -40,11 +40,11 @@ import javax.annotation.Nullable;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.elasticsearch.action.ActionListener;
+import org.elasticsearch.action.bulk.BackoffPolicy;
 import org.elasticsearch.cluster.routing.ShardRouting;
 import org.elasticsearch.cluster.service.ClusterService;
 
 import io.crate.action.FutureActionListener;
-import io.crate.action.LimitedExponentialBackoff;
 import io.crate.concurrent.limits.ConcurrencyLimit;
 import io.crate.data.BatchIterator;
 import io.crate.data.BatchIterators;
@@ -145,7 +145,7 @@ public class ShardDMLExecutor<TReq extends ShardRequest<TReq, TItem>,
             scheduler,
             l -> operation.accept(request, l),
             listener,
-            LimitedExponentialBackoff.limitedExponential(nodeLimit)
+            BackoffPolicy.unlimitedDynamic(nodeLimit)
         );
     }
 

server/src/main/java/io/crate/execution/engine/indexing/ShardingUpsertExecutor.java
@@ -44,6 +44,7 @@ import org.apache.logging.log4j.Logger;
 import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.action.admin.indices.create.CreatePartitionsRequest;
 import org.elasticsearch.action.admin.indices.create.TransportCreatePartitionsAction;
+import org.elasticsearch.action.bulk.BackoffPolicy;
 import org.elasticsearch.action.bulk.BulkRequestExecutor;
 import org.elasticsearch.action.support.master.AcknowledgedResponse;
 import org.elasticsearch.cluster.service.ClusterService;
@@ -53,7 +54,6 @@ import org.elasticsearch.common.unit.ByteSizeUnit;
 import org.elasticsearch.index.shard.ShardId;
 
 import io.crate.action.FutureActionListener;
-import io.crate.action.LimitedExponentialBackoff;
 import io.crate.breaker.BlockBasedRamAccounting;
 import io.crate.breaker.RamAccounting;
 import io.crate.breaker.TypeGuessEstimateRowSize;
@@ -216,7 +216,7 @@ public class ShardingUpsertExecutor
                 scheduler,
                 l -> requestExecutor.execute(request, l),
                 listener,
-                LimitedExponentialBackoff.limitedExponential(nodeLimit)
+                BackoffPolicy.unlimitedDynamic(nodeLimit)
             );
             requestExecutor.execute(request, listener);
         }

server/src/main/java/io/crate/planner/operators/InsertFromValues.java
@@ -49,6 +49,7 @@ import com.carrotsearch.hppc.IntArrayList;
 import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.action.admin.indices.create.CreatePartitionsRequest;
 import org.elasticsearch.action.admin.indices.create.TransportCreatePartitionsAction;
+import org.elasticsearch.action.bulk.BackoffPolicy;
 import org.elasticsearch.action.support.master.AcknowledgedResponse;
 import org.elasticsearch.cluster.ClusterState;
 import org.elasticsearch.cluster.block.ClusterBlock;
@@ -61,7 +62,6 @@ import org.elasticsearch.common.io.stream.NotSerializableExceptionWrapper;
 import org.elasticsearch.index.IndexNotFoundException;
 
 import io.crate.action.FutureActionListener;
-import io.crate.action.LimitedExponentialBackoff;
 import io.crate.analyze.OrderBy;
 import io.crate.analyze.SymbolEvaluator;
 import io.crate.analyze.relations.AbstractTableRelation;
@@ -705,7 +705,7 @@ public class InsertFromValues implements LogicalPlan {
                     scheduler,
                     l -> shardUpsertAction.execute(request, l),
                     listener,
-                    LimitedExponentialBackoff.limitedExponential(nodeLimit)
+                    BackoffPolicy.limitedDynamic(nodeLimit)
                 )
             );
         }

server/src/main/java/org/elasticsearch/action/bulk/BackoffPolicy.java
@@ -19,10 +19,12 @@
 
 package org.elasticsearch.action.bulk;
 
-import io.crate.common.unit.TimeValue;
-
 import java.util.Iterator;
 import java.util.NoSuchElementException;
+import java.util.concurrent.TimeUnit;
+
+import io.crate.common.unit.TimeValue;
+import io.crate.concurrent.limits.ConcurrencyLimit;
 
 /**
  * Provides a backoff policy for bulk requests. Whenever a bulk request is rejected due to resource constraints (i.e. the client's internal
@@ -42,6 +44,56 @@ import java.util.NoSuchElementException;
  * Note that backoff policies are exposed as <code>Iterables</code> in order to be consumed multiple times.
  */
 public abstract class BackoffPolicy implements Iterable<TimeValue> {
+
+    private static final int DEFAULT_RETRY_LIMIT = 10;
+
+    public static BackoffPolicy unlimitedDynamic(ConcurrencyLimit concurrencyLimit) {
+        return new BackoffPolicy() {
+
+            @Override
+            public Iterator<TimeValue> iterator() {
+                return new Iterator<>() {
+
+                    @Override
+                    public boolean hasNext() {
+                        return true;
+                    }
+
+                    @Override
+                    public TimeValue next() {
+                        return TimeValue.timeValueNanos(concurrencyLimit.getLastRtt(TimeUnit.NANOSECONDS));
+                    }
+                };
+            }
+        };
+    }
+
+    public static BackoffPolicy limitedDynamic(ConcurrencyLimit concurrencyLimit) {
+        return new BackoffPolicy() {
+
+            int iterations = 0;
+
+            @Override
+            public Iterator<TimeValue> iterator() {
+                return new Iterator<>() {
+
+                    @Override
+                    public boolean hasNext() {
+                        return iterations < DEFAULT_RETRY_LIMIT;
+                    }
+
+                    @Override
+                    public TimeValue next() {
+                        if (!hasNext()) {
+                            throw new NoSuchElementException("BackoffPolicy iterator exhausted");
+                        }
+                        return TimeValue.timeValueNanos(concurrencyLimit.getLongRtt(TimeUnit.NANOSECONDS));
+                    }
+                };
+            }
+        };
+    }
+
 
     /**
      * Creates an new exponential backoff policy with a default configuration of 50 ms initial wait period and 8 retries taking