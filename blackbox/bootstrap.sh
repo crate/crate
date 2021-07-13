40a0ba495d6979695e2b8843bec88e9077c2928e
Mathias Fussenegger <f.mathias@zignar.net>
2021-07-13 16:53:51 +0200

    Remove pip `--use-feature` option for blackbox bootstrap
        WARNING: --use-feature=2020-resolver no longer has any effect, since it is now the default dependency resolver in pip. This will become an error in pip 21.0
    
    (cherry picked from commit f3460ed218f0efba228d2246655e705806683981)

--- blackbox/bootstrap.sh
+++ blackbox/bootstrap.sh
@@ -5,10 +5,10 @@ DIR=$(dirname "$0")
 if hash python3.7 2> /dev/null; then
     PYTHON=python3.7
 elif hash python3 2> /dev/null; then
-    # fallback to python3 in case there is no python3.7 alias; should be 3.7
+    # fallback to python3 in case there is no python3.7 alias; should be 3.7+
     PYTHON=python3
 else
-    echo 'python3.7 required'
+    echo 'python3.7+ required'
     exit 1
 fi
 
@@ -20,4 +20,4 @@ if [ ! -f $DIR/.venv/bin/pip ]; then
 fi
 
 $DIR/.venv/bin/pip install -U pip setuptools wheel
-$DIR/.venv/bin/pip install --use-feature=2020-resolver -r $DIR/requirements.txt
+$DIR/.venv/bin/pip install -r $DIR/requirements.txt
