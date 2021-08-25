crate/crate

commit 25b6d5e01aef108fe840d3f09f9584ea83c14a4a

bp: Make Repository.getRepositoryData an Async API
https://github.com/elastic/elasticsearch/commit/4d659c4bdbfb6b9eb9ab271a4e247663cd6b63cb

server/src/test/java/io/crate/integrationtests/SnapshotRestoreIntegrationTest.java
@@ -32,6 +32,7 @@ import org.elasticsearch.cluster.metadata.IndexMetadata;
 import org.elasticsearch.common.collect.ImmutableOpenMap;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.plugins.Plugin;
+import org.elasticsearch.repositories.ESBlobStoreTestCase;
 import org.elasticsearch.repositories.RepositoriesService;
 import org.elasticsearch.repositories.Repository;
 import org.elasticsearch.repositories.RepositoryData;
@@ -796,7 +797,7 @@ public class SnapshotRestoreIntegrationTest extends SQLIntegrationTestCase {
         final SetOnce<RepositoryData> repositoryData = new SetOnce<>();
         final CountDownLatch latch = new CountDownLatch(1);
         threadPool.executor(ThreadPool.Names.SNAPSHOT).execute(() -> {
-            repositoryData.set(repository.getRepositoryData());
+            repositoryData.set(ESBlobStoreTestCase.getRepositoryData(repository));
             latch.countDown();
         });
         latch.await();

server/src/main/java/io/crate/expression/reference/sys/snapshot/SysSnapshots.java
@@ -21,8 +21,10 @@
 
 package io.crate.expression.reference.sys.snapshot;
 
+import io.crate.action.FutureActionListener;
 import io.crate.common.annotations.VisibleForTesting;
 import io.crate.common.collections.Lists2;
+import io.crate.concurrent.CompletableFutures;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.elasticsearch.Version;
@@ -30,15 +32,18 @@ import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.common.inject.Singleton;
 import org.elasticsearch.repositories.RepositoriesService;
 import org.elasticsearch.repositories.Repository;
+import org.elasticsearch.repositories.RepositoryData;
 import org.elasticsearch.snapshots.SnapshotException;
 import org.elasticsearch.snapshots.SnapshotId;
 import org.elasticsearch.snapshots.SnapshotInfo;
 import org.elasticsearch.snapshots.SnapshotShardFailure;
 import org.elasticsearch.snapshots.SnapshotState;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import java.util.function.Supplier;
 
 @Singleton
@@ -57,11 +62,27 @@ public class SysSnapshots {
         this.getRepositories = getRepositories;
     }
 
-    public Iterable<SysSnapshot> currentSnapshots() {
-        return () -> getRepositories.get().stream()
-            .flatMap(repository -> repository.getRepositoryData().getSnapshotIds().stream()
-                .map(snapshotId -> createSysSnapshot(repository, snapshotId))
-            ).iterator();
+    public CompletableFuture<Iterable<SysSnapshot>> currentSnapshots() {
+        ArrayList<FutureActionListener<RepositoryData, List<SysSnapshot>>> futureActionListeners = new ArrayList<>();
+        for (Repository repository : getRepositories.get()) {
+            var listener = new FutureActionListener<RepositoryData, List<SysSnapshot>>(repositoryData -> {
+                ArrayList<SysSnapshot> result = new ArrayList<>();
+                Collection<SnapshotId> snapshotIds = repositoryData.getSnapshotIds();
+                for (SnapshotId snapshotId : snapshotIds) {
+                    result.add(createSysSnapshot(repository, snapshotId));
+                }
+                return result;
+            });
+            repository.getRepositoryData(listener);
+            futureActionListeners.add(listener);
+        }
+        return CompletableFutures.allAsList(futureActionListeners).thenApply(data -> {
+            ArrayList<SysSnapshot> result = new ArrayList<>();
+            for (List<SysSnapshot> datum : data) {
+                result.addAll(datum);
+            }
+            return result;
+        });
     }
 
     private static SysSnapshot createSysSnapshot(Repository repository, SnapshotId snapshotId) {

server/src/main/java/io/crate/metadata/sys/SysTableDefinitions.java
@@ -105,7 +105,7 @@ public class SysTableDefinitions {
             SysRepositoriesTableInfo.create(clusterService.getClusterSettings().maskedSettings()).expressions(),
             false));
         tableDefinitions.put(SysSnapshotsTableInfo.IDENT, new StaticTableDefinition<>(
-            () -> completedFuture(sysSnapshots.currentSnapshots()),
+            sysSnapshots::currentSnapshots,
             SysSnapshotsTableInfo.create().expressions(),
             true));
         tableDefinitions.put(SysSnapshotRestoreTableInfo.IDENT, new StaticTableDefinition<>(

server/src/main/java/org/elasticsearch/index/shard/StoreRecovery.java
@@ -493,20 +493,21 @@ final class StoreRecovery {
             translogState.totalOperations(0);
             translogState.totalOperationsOnStart(0);
             indexShard.prepareForIndexRecovery();
-            ShardId snapshotShardId = shardId;
+            final ShardId snapshotShardId;
             final String indexName = restoreSource.index();
             if (!shardId.getIndexName().equals(indexName)) {
                 snapshotShardId = new ShardId(indexName, IndexMetadata.INDEX_UUID_NA_VALUE, shardId.id());
+            } else {
+                snapshotShardId = shardId;
             }
-            final IndexId indexId = repository.getRepositoryData().resolveIndexId(indexName);
-            assert indexShard.getEngineOrNull() == null;
-            repository.restoreShard(
-                indexShard.store(),
-                restoreSource.snapshot().getSnapshotId(),
-                indexId,
-                snapshotShardId,
-                indexShard.recoveryState(),
-                restoreListener);
+            repository.getRepositoryData(ActionListener.wrap(
+                repositoryData -> {
+                    final IndexId indexId = repositoryData.resolveIndexId(indexName);
+                    assert indexShard.getEngineOrNull() == null;
+                    repository.restoreShard(indexShard.store(), restoreSource.snapshot().getSnapshotId(), indexId, snapshotShardId,
+                        indexShard.recoveryState(), restoreListener);
+                }, restoreListener::onFailure
+            ));
         } catch (Exception e) {
             restoreListener.onFailure(e);
         }

server/src/main/java/org/elasticsearch/repositories/RepositoriesService.java
@@ -481,8 +481,8 @@ public class RepositoriesService implements ClusterStateApplier {
         public UnregisterRepositoryRequest(String cause, String name) {
             this.cause = cause;
             this.name = name;
-
         }
+
     }
 
     /**

server/src/main/java/org/elasticsearch/repositories/Repository.java
@@ -112,7 +112,7 @@ public interface Repository extends LifecycleComponent {
      * and the indices across all snapshots found in the repository.  Throws a {@link RepositoryException}
      * if there was an error in reading the data.
      */
-    RepositoryData getRepositoryData();
+    void getRepositoryData(ActionListener<RepositoryData> listener);
 
     /**
      * Finalizes snapshotting process

server/src/main/java/org/elasticsearch/repositories/blobstore/BlobStoreRepository.java
@@ -123,6 +123,8 @@ import org.elasticsearch.threadpool.ThreadPool;
 import io.crate.common.collections.Tuple;
 import io.crate.exceptions.InvalidArgumentException;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.Consumer;
+
 
 /**
  * BlobStore - based implementation of Snapshot Repository
@@ -767,19 +769,22 @@ public abstract class BlobStoreRepository extends AbstractLifecycleComponent imp
         // directory if all nodes are at least at version SnapshotsService#SHARD_GEN_IN_REPO_DATA_VERSION
         // If there are older version nodes in the cluster, we don't need to run this cleanup as it will have already happened
         // when writing the index-${N} to each shard directory.
+        final Consumer<Exception> onUpdateFailure =
+            e -> listener.onFailure(new SnapshotException(metadata.name(), snapshotId, "failed to update snapshot in repository", e));
         final ActionListener<SnapshotInfo> allMetaListener = new GroupedActionListener<>(
             ActionListener.wrap(snapshotInfos -> {
                 assert snapshotInfos.size() == 1 : "Should have only received a single SnapshotInfo but received " + snapshotInfos;
                 final SnapshotInfo snapshotInfo = snapshotInfos.iterator().next();
-                final RepositoryData existingRepositoryData = getRepositoryData();
-                final RepositoryData updatedRepositoryData = existingRepositoryData.addSnapshot(snapshotId, snapshotInfo.state(), shardGenerations);
-                writeIndexGen(updatedRepositoryData, repositoryStateId, writeShardGens);
-                if (writeShardGens) {
-                    cleanupOldShardGens(existingRepositoryData, updatedRepositoryData);
-                }
-                listener.onResponse(snapshotInfo);
-            }, e -> listener.onFailure(new SnapshotException(metadata.name(), snapshotId, "failed to update snapshot in repository", e))),
-            2 + indices.size());
+                getRepositoryData(ActionListener.wrap(existingRepositoryData -> {
+                    final RepositoryData updatedRepositoryData =
+                        existingRepositoryData.addSnapshot(snapshotId, snapshotInfo.state(), shardGenerations);
+                    writeIndexGen(updatedRepositoryData, repositoryStateId, writeShardGens);
+                    if (writeShardGens) {
+                        cleanupOldShardGens(existingRepositoryData, updatedRepositoryData);
+                    }
+                    listener.onResponse(snapshotInfo);
+                }, onUpdateFailure));
+            }, onUpdateFailure), 2 + indices.size());
         final Executor executor = threadPool.executor(ThreadPool.Names.SNAPSHOT);
 
         // We ignore all FileAlreadyExistsException when writing metadata since otherwise a master failover while in this method will
@@ -946,31 +951,33 @@ public abstract class BlobStoreRepository extends AbstractLifecycleComponent imp
     protected final AtomicLong latestKnownRepoGen = new AtomicLong(RepositoryData.EMPTY_REPO_GEN);
 
     @Override
-    public RepositoryData getRepositoryData() {
-        // Retry loading RepositoryData in a loop in case we run into concurrent modifications of the repository.
-        while (true) {
-            final long generation;
-            try {
-                generation = latestIndexBlobId();
-            } catch (IOException ioe) {
-                throw new RepositoryException(metadata.name(), "Could not determine repository generation from root blobs", ioe);
-            }
-            final long genToLoad = latestKnownRepoGen.updateAndGet(known -> Math.max(known, generation));
-            if (genToLoad > generation) {
-                LOGGER.info("Determined repository generation [" + generation
-                    + "] from repository contents but correct generation must be at least [" + genToLoad + "]");
-            }
-            try {
-                return getRepositoryData(genToLoad);
-            } catch (RepositoryException e) {
-                if (genToLoad != latestKnownRepoGen.get()) {
-                    LOGGER.warn("Failed to load repository data generation [" + genToLoad +
-                        "] because a concurrent operation moved the current generation to [" + latestKnownRepoGen.get() + "]", e);
-                    continue;
+    public void getRepositoryData(ActionListener<RepositoryData> listener) {
+        ActionListener.completeWith(listener, () -> {
+            // Retry loading RepositoryData in a loop in case we run into concurrent modifications of the repository.
+            while (true) {
+                final long generation;
+                try {
+                    generation = latestIndexBlobId();
+                } catch (IOException ioe) {
+                    throw new RepositoryException(metadata.name(), "Could not determine repository generation from root blobs", ioe);
                 }
-                throw e;
+                final long genToLoad = latestKnownRepoGen.updateAndGet(known -> Math.max(known, generation));
+                if (genToLoad > generation) {
+                    LOGGER.info("Determined repository generation [" + generation
+                        + "] from repository contents but correct generation must be at least [" + genToLoad + "]");
+                }
+                try {
+                    return getRepositoryData(genToLoad);
+                } catch (RepositoryException e) {
+                    if (genToLoad != latestKnownRepoGen.get()) {
+                        LOGGER.warn("Failed to load repository data generation [" + genToLoad +
+                            "] because a concurrent operation moved the current generation to [" + latestKnownRepoGen.get() + "]", e);
+                        continue;
+                    }
+                    throw e;
+                }
             }
-        }
+        });
     }
 
 

commit f31701644592572b627ed2e3ae1bb58843e4c7a3

Fix restoring of concrete custom metadata only
Fixes a regression introduced by
https://github.com/crate/crate/commit/25b6d5e01aef108fe840d3f09f9584ea83c14a4a

Fixes #11671

server/src/main/java/org/elasticsearch/snapshots/RestoreService.java
@@ -403,20 +403,20 @@ public class RestoreService implements ClusterStateApplier {
 
                             clusterSettings.validateUpdate(settings);
                             mdBuilder.persistentSettings(settings);
+                        }
 
-                            if (request.includeCustomMetadata() && metadata.customs() != null) {
-                                // CrateDB patch to only restore defined custom metadata types
-                                List<String> customMetadataTypes = Arrays.asList(request.customMetadataTypes());
-                                boolean includeAll = customMetadataTypes.size() == 0;
+                        if (request.includeCustomMetadata() && metadata.customs() != null) {
+                            // CrateDB patch to only restore defined custom metadata types
+                            List<String> customMetadataTypes = Arrays.asList(request.customMetadataTypes());
+                            boolean includeAll = customMetadataTypes.size() == 0;
 
-                                for (ObjectObjectCursor<String, Metadata.Custom> cursor : metadata.customs()) {
-                                    if (!RepositoriesMetadata.TYPE.equals(cursor.key)) {
-                                        // Don't restore repositories while we are working with them
-                                        // TODO: Should we restore them at the end?
+                            for (ObjectObjectCursor<String, Metadata.Custom> cursor : metadata.customs()) {
+                                if (!RepositoriesMetadata.TYPE.equals(cursor.key)) {
+                                    // Don't restore repositories while we are working with them
+                                    // TODO: Should we restore them at the end?
 
-                                        if (includeAll || customMetadataTypes.contains(cursor.key)) {
-                                            mdBuilder.putCustom(cursor.key, cursor.value);
-                                        }
+                                    if (includeAll || customMetadataTypes.contains(cursor.key)) {
+                                        mdBuilder.putCustom(cursor.key, cursor.value);
                                     }
                                 }
                             }

server/src/test/java/io/crate/integrationtests/SnapshotRestoreIntegrationTest.java
@@ -706,6 +706,9 @@ public class SnapshotRestoreIntegrationTest extends SQLIntegrationTestCase {
 
     }
 
+    /**
+     * Restoring ANALYZERS will result in restoring analyzer settings out of the global settings
+     */
     @Test
     public void test_restore_analyzers_only() throws Exception {
         createSnapshotWithTablesAndMetadata();
@@ -729,6 +732,22 @@ public class SnapshotRestoreIntegrationTest extends SQLIntegrationTestCase {
 
         execute("SELECT type FROM sys.privileges WHERE grantee = 'my_user'");
         assertThat(response.rowCount(), is(0L));
+    }
+
+    /**
+     * Restoring USERS will result in restoring custom metadata only and NO global settings.
+     * This test a regression which resulted in restoring the custom metadata only if global settings
+     * were also marked to be restored.
+     */
+    @Test
+    public void test_restore_custom_metadata_only() throws Exception {
+        createSnapshotWithTablesAndMetadata();
+
+        execute("RESTORE SNAPSHOT " + snapshotName() + " USERS WITH (wait_for_completion=true)");
+        waitNoPendingTasksOnAll();
+
+        execute("SELECT name FROM sys.users WHERE name = 'my_user'");
+        assertThat(printedTable(response.rows()), is("my_user\n"));
     }
 
     @Test