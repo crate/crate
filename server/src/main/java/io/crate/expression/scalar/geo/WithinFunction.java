65c54258e59cfaec262e172a8419b135ac9689cc
Mathias Fussenegger <f.mathias@zignar.net>
2021-09-29 14:45:44 +0200

    Migrate within lucene query migration to scalar

--- server/src/main/java/io/crate/expression/scalar/geo/WithinFunction.java
+++ server/src/main/java/io/crate/expression/scalar/geo/WithinFunction.java
@@ -21,24 +21,41 @@
 
 package io.crate.expression.scalar.geo;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.lucene.document.LatLonPoint;
+import org.apache.lucene.geo.Polygon;
+import org.apache.lucene.search.Query;
+import org.elasticsearch.common.lucene.search.Queries;
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.CoordinateArrays;
+import org.locationtech.jts.geom.Geometry;
+import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.spatial4j.context.SpatialContext;
+import org.locationtech.spatial4j.context.jts.JtsSpatialContext;
+import org.locationtech.spatial4j.exception.InvalidShapeException;
+import org.locationtech.spatial4j.shape.Point;
+import org.locationtech.spatial4j.shape.Shape;
+import org.locationtech.spatial4j.shape.ShapeCollection;
+import org.locationtech.spatial4j.shape.SpatialRelation;
+
 import io.crate.data.Input;
+import io.crate.expression.operator.EqOperator;
 import io.crate.expression.scalar.ScalarFunctionModule;
 import io.crate.expression.symbol.Function;
 import io.crate.expression.symbol.Literal;
 import io.crate.expression.symbol.Symbol;
 import io.crate.geo.GeoJSONUtils;
+import io.crate.lucene.LuceneQueryBuilder.Context;
+import io.crate.metadata.ColumnIdent;
 import io.crate.metadata.NodeContext;
+import io.crate.metadata.Reference;
 import io.crate.metadata.Scalar;
 import io.crate.metadata.TransactionContext;
 import io.crate.metadata.functions.Signature;
 import io.crate.types.DataTypes;
-import org.locationtech.spatial4j.context.SpatialContext;
-import org.locationtech.spatial4j.shape.Point;
-import org.locationtech.spatial4j.shape.Shape;
-import org.locationtech.spatial4j.shape.SpatialRelation;
-
-import java.util.List;
-import java.util.Map;
 
 public class WithinFunction extends Scalar<Boolean, Object> {
 
@@ -150,5 +167,74 @@ public class WithinFunction extends Scalar<Boolean, Object> {
         }
 
         return symbol;
+    }
+
+    @Override
+    public Query toQuery(Function parent, Function inner, Context context) {
+        // within(p, pointOrShape) = [ true | false ]
+        if (parent.name().equals(EqOperator.NAME)
+                && parent.arguments().get(1) instanceof Literal<?> eqLiteral
+                && inner.arguments().get(0) instanceof Reference ref
+                && inner.arguments().get(1) instanceof Literal<?> pointOrShape) {
+
+            Query query = toQuery(ref, pointOrShape, context);
+            if (query == null) {
+                return null;
+            }
+            Boolean isWithin = (Boolean) eqLiteral.value();
+            if (isWithin == null) {
+                // Need to fallback to generic function filter because `null = null` is `null`
+                // and depending on parent queries that could turn into a match.
+                return null;
+            }
+            return isWithin ? query : Queries.not(query);
+        }
+        return null;
+    }
+
+    @Override
+    public Query toQuery(Reference ref, Literal<?> literal, Context context) {
+        if (ref.valueType().equals(DataTypes.GEO_SHAPE)) {
+            // Can only optimize on point columns, not on shapes
+            return null;
+        }
+        Map<String, Object> geoJSON = DataTypes.GEO_SHAPE.implicitCast(literal.value());
+        Geometry geometry;
+        Shape shape = GeoJSONUtils.map2Shape(geoJSON);
+        if (shape instanceof ShapeCollection<?> collection) {
+            int i = 0;
+            org.locationtech.jts.geom.Polygon[] polygons = new org.locationtech.jts.geom.Polygon[collection.size()];
+            for (Shape s : collection.getShapes()) {
+                Geometry subGeometry = JtsSpatialContext.GEO.getShapeFactory().getGeometryFrom(s);
+                if (subGeometry instanceof org.locationtech.jts.geom.Polygon polygon) {
+                    polygons[i++] = polygon;
+                } else {
+                    throw new InvalidShapeException("Shape collection must contain only Polygon shapes.");
+                }
+            }
+            GeometryFactory geometryFactory = JtsSpatialContext.GEO.getShapeFactory().getGeometryFactory();
+            geometry = geometryFactory.createMultiPolygon(polygons);
+        } else {
+            geometry = JtsSpatialContext.GEO.getShapeFactory().getGeometryFrom(shape);
+        }
+
+        return getPolygonQuery(ref.column(), geometry);
+    }
+
+    private static Query getPolygonQuery(ColumnIdent column, Geometry geometry) {
+        Coordinate[] coordinates = geometry.getCoordinates();
+        // close the polygon shape if startpoint != endpoint
+        if (!CoordinateArrays.isRing(coordinates)) {
+            coordinates = Arrays.copyOf(coordinates, coordinates.length + 1);
+            coordinates[coordinates.length - 1] = coordinates[0];
+        }
+
+        final double[] lats = new double[coordinates.length];
+        final double[] lons = new double[coordinates.length];
+        for (int i = 0; i < coordinates.length; i++) {
+            lats[i] = coordinates[i].y;
+            lons[i] = coordinates[i].x;
+        }
+        return LatLonPoint.newPolygonQuery(column.fqn(), new Polygon(lats, lons));
     }
 }
