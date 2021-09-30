968133f06c15279e702ff66c130d301f7479d108
Michael Kleen <michael.kleen@crate.io>
2021-09-30 15:42:35 +0200

    Bump version to 4.6.5-SNAPSHOT

--- server/src/main/java/org/elasticsearch/Version.java
+++ server/src/main/java/org/elasticsearch/Version.java
@@ -122,8 +122,9 @@ public class Version implements Comparable<Version>, ToXContentFragment {
     public static final Version V_4_6_2 = new Version(7_06_02_99, false, org.apache.lucene.util.Version.LUCENE_8_8_2);
     public static final Version V_4_6_3 = new Version(7_06_03_99, false, org.apache.lucene.util.Version.LUCENE_8_8_2);
     public static final Version V_4_6_4 = new Version(7_06_04_99, false, org.apache.lucene.util.Version.LUCENE_8_8_2);
+    public static final Version V_4_6_5 = new Version(7_06_05_99, true, org.apache.lucene.util.Version.LUCENE_8_8_2);
 
-    public static final Version CURRENT = V_4_6_4;
+    public static final Version CURRENT = V_4_6_5;
 
     private static final ImmutableOpenIntMap<Version> ID_TO_VERSION;
 
