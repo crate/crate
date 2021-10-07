1245d82d6f290ae67c28cc0fad258c8d6cb99768
Sebastian Utz <su@rtme.net>
2021-10-07 17:10:20 +0200

    Add analyzer support for create/drop/alter publication + subscription

---
+++ server/src/main/java/io/crate/replication/logical/analyze/AnalyzedDropSubscription.java
@@ -0,0 +1,56 @@
+/*
+ * Licensed to Crate.io GmbH ("Crate") under one or more contributor
+ * license agreements.  See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.  Crate licenses
+ * this file to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.  You may
+ * obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * However, if you have executed another commercial license agreement
+ * with Crate these terms will supersede the license and you may use the
+ * software solely pursuant to the terms of the relevant commercial agreement.
+ */
+
+package io.crate.replication.logical.analyze;
+
+import io.crate.analyze.AnalyzedStatementVisitor;
+import io.crate.analyze.DDLStatement;
+import io.crate.expression.symbol.Symbol;
+
+import java.util.function.Consumer;
+
+public class AnalyzedDropSubscription implements DDLStatement {
+
+    private final String name;
+    private final boolean ifExists;
+
+    public AnalyzedDropSubscription(String name, boolean ifExists) {
+        this.name = name;
+        this.ifExists = ifExists;
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public boolean ifExists() {
+        return ifExists;
+    }
+
+    @Override
+    public void visitSymbols(Consumer<? super Symbol> consumer) {
+    }
+
+    @Override
+    public <C, R> R accept(AnalyzedStatementVisitor<C, R> visitor, C context) {
+        return visitor.visitDropSubscription(this, context);
+    }
+}
