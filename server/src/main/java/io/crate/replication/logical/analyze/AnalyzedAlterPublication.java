46169ea8e8842f7867cd461ba87036ae5f0f51f9
Sebastian Utz <su@rtme.net>
2021-10-07 17:10:20 +0200

    Add analyzer support for create/drop/alter publication + subscription

---
+++ server/src/main/java/io/crate/replication/logical/analyze/AnalyzedAlterPublication.java
@@ -0,0 +1,65 @@
+/*
+ * Licensed to Crate.io GmbH ("Crate") under one or more contributor
+ * license agreements.  See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.  Crate licenses
+ * this file to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.  You may
+ * obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * However, if you have executed another commercial license agreement
+ * with Crate these terms will supersede the license and you may use the
+ * software solely pursuant to the terms of the relevant commercial agreement.
+ */
+
+package io.crate.replication.logical.analyze;
+
+import io.crate.analyze.AnalyzedStatementVisitor;
+import io.crate.analyze.DDLStatement;
+import io.crate.expression.symbol.Symbol;
+import io.crate.metadata.RelationName;
+import io.crate.sql.tree.AlterPublication;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+public class AnalyzedAlterPublication implements DDLStatement {
+
+    private final String name;
+    private final AlterPublication.Operation operation;
+    private final List<RelationName> tables;
+
+    public AnalyzedAlterPublication(String name, AlterPublication.Operation operation, List<RelationName> tables) {
+        this.name = name;
+        this.operation = operation;
+        this.tables = tables;
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public AlterPublication.Operation operation() {
+        return operation;
+    }
+
+    public List<RelationName> tables() {
+        return tables;
+    }
+
+    @Override
+    public void visitSymbols(Consumer<? super Symbol> consumer) {
+    }
+
+    @Override
+    public <C, R> R accept(AnalyzedStatementVisitor<C, R> visitor, C context) {
+        return visitor.visitAlterPublication(this, context);
+    }
+}
