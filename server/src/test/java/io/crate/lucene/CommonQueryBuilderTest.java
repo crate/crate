crate/crate

commit 81d92fa4a0003e03ec23086b9ffd01723db48432

Make lucene query generation part of Scalar interface
Relates to https://github.com/crate/crate/issues/10025

Currently we use `termQuery`, `termsQuery`, `rangeQuery` and
`existsQuery` of `MappedFieldType` within the `LuceneQueryBuilder`.

This was done to re-use existing code, but the overall pattern somehow
doesn't fit with the rest of the design.

We have `Function` and the corresponding `FunctionImplementation` as
main entry points and they can be type specific (have
different overloads for different types). The Lucene Query generation is
an optimization step, that relates to these implementations (*and* their
type signatures)

This commit adds a `toQuery` method to the `Scalar` class to allow any
scalar implementation to optionally generate a Lucene query.

This also allows extensions outside of `server` to implement Lucene
query logic, no longer limiting optimizations to built-in operations.

server/src/main/java/io/crate/expression/operator/EqOperator.java
@@ -21,13 +21,51 @@
 
 package io.crate.expression.operator;
 
-import io.crate.data.Input;
-import io.crate.metadata.NodeContext;
-import io.crate.metadata.TransactionContext;
-import io.crate.metadata.functions.Signature;
-
+import static io.crate.lucene.LuceneQueryBuilder.genericFunctionFilter;
 import static io.crate.metadata.functions.TypeVariableConstraint.typeVariable;
 import static io.crate.types.TypeSignature.parseTypeSignature;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+import org.apache.lucene.document.DoublePoint;
+import org.apache.lucene.document.FloatPoint;
+import org.apache.lucene.document.IntPoint;
+import org.apache.lucene.document.LongPoint;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.search.BooleanClause;
+import org.apache.lucene.search.BooleanQuery;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.TermQuery;
+import org.elasticsearch.common.lucene.BytesRefs;
+import org.elasticsearch.common.lucene.search.Queries;
+import org.elasticsearch.index.mapper.MappedFieldType;
+import org.elasticsearch.index.mapper.Uid;
+
+import io.crate.data.Input;
+import io.crate.expression.symbol.Function;
+import io.crate.expression.symbol.Literal;
+import io.crate.expression.symbol.Symbol;
+import io.crate.lucene.LuceneQueryBuilder;
+import io.crate.lucene.LuceneQueryBuilder.Context;
+import io.crate.metadata.NodeContext;
+import io.crate.metadata.Reference;
+import io.crate.metadata.Reference.IndexType;
+import io.crate.metadata.TransactionContext;
+import io.crate.metadata.doc.DocSysColumns;
+import io.crate.metadata.functions.Signature;
+import io.crate.types.ArrayType;
+import io.crate.types.ByteType;
+import io.crate.types.DataType;
+import io.crate.types.DoubleType;
+import io.crate.types.FloatType;
+import io.crate.types.IntegerType;
+import io.crate.types.LongType;
+import io.crate.types.ObjectType;
+import io.crate.types.ShortType;
+import io.crate.types.StringType;
 
 public final class EqOperator extends Operator<Object> {
 
@@ -49,6 +87,7 @@ public final class EqOperator extends Operator<Object> {
 
     private final Signature signature;
     private final Signature boundSignature;
+
 
     private EqOperator(Signature signature, Signature boundSignature) {
         this.signature = signature;
@@ -77,5 +116,115 @@ public final class EqOperator extends Operator<Object> {
     @Override
     public Signature boundSignature() {
         return boundSignature;
+    }
+
+    @Override
+    public Query toQuery(Function function, Context context) {
+        List<Symbol> args = function.arguments();
+        if (!(args.get(0) instanceof Reference ref && args.get(1) instanceof Literal<?> literal)) {
+            return null;
+        }
+        String fqn = ref.column().fqn();
+        Object value = literal.value();
+        if (value == null) {
+            return Queries.newMatchNoDocsQuery("`" + fqn + "` = null is always null");
+        }
+        DataType<?> dataType = ref.valueType();
+        if (dataType.id() != ObjectType.ID && ref.indexType() == IndexType.NONE) {
+            throw new IllegalArgumentException(
+                "Cannot search on field [" + fqn + "] since it is not indexed.");
+        }
+        return switch (dataType.id()) {
+            case ObjectType.ID -> refEqObject(function, fqn, (ObjectType) dataType, (Map<String, Object>) value, context);
+            case ArrayType.ID -> refEqArray(function, fqn, (Collection) value, context);
+            default -> fromPrimitive(dataType, fqn, value);
+        };
+    }
+
+    private Query refEqArray(Function function, String column, Collection<?> values, LuceneQueryBuilder.Context context) {
+        List<?> list = values.stream().filter(Objects::nonNull).toList();
+        if (list.isEmpty()) {
+            return genericFunctionFilter(function, context);
+        }
+        MappedFieldType fieldType = context.getFieldTypeOrNull(column);
+        if (fieldType == null) {
+            // field doesn't exist, can't match
+            return Queries.newMatchNoDocsQuery("column does not exist in this index");
+        }
+        Query termsQuery = fieldType.termsQuery(list, context.queryShardContext());
+
+        // wrap boolTermsFilter and genericFunction filter in an additional BooleanFilter to control the ordering of the filters
+        // termsFilter is applied first
+        // afterwards the more expensive genericFunctionFilter
+        BooleanQuery.Builder filterClauses = new BooleanQuery.Builder();
+        filterClauses.add(termsQuery, BooleanClause.Occur.MUST);
+        filterClauses.add(genericFunctionFilter(function, context), BooleanClause.Occur.MUST);
+        return filterClauses.build();
+    }
+
+    private static Query fromPrimitive(DataType<?> type, String column, Object value) {
+        if (column.equals(DocSysColumns.ID.name())) {
+            return new TermQuery(new Term(column, Uid.encodeId((String) value)));
+        }
+        return switch (type.id()) {
+            case ByteType.ID, ShortType.ID, IntegerType.ID -> IntPoint.newExactQuery(column, (int) value);
+            case LongType.ID -> LongPoint.newExactQuery(column, (long) value);
+            case FloatType.ID -> FloatPoint.newExactQuery(column, (float) value);
+            case DoubleType.ID -> DoublePoint.newExactQuery(column, (double) value);
+            case StringType.ID -> new TermQuery(new Term(column, BytesRefs.toBytesRef(value)));
+            default -> null;
+        };
+    }
+
+
+    /**
+     * Query for object columns that tries to utilize efficient termQueries for the objects children.
+     * <pre>
+     * {@code
+     *      // If x and y are known columns
+     *      o = {x=10, y=20}    -> o.x=10 and o.y=20
+     *
+     *      // Only x is known:
+     *      o = {x=10, y=20}    -> o.x=10 and generic(o == {x=10, y=20})
+     *
+     *      // No column is known:
+     *      o = {x=10, y=20}    -> generic(o == {x=10, y=20})
+     * }
+     * </pre>
+     **/
+    private static Query refEqObject(Function eq,
+                                     String fqn,
+                                     ObjectType type,
+                                     Map<String, Object> value,
+                                     LuceneQueryBuilder.Context context) {
+        BooleanQuery.Builder boolBuilder = new BooleanQuery.Builder();
+        int preFilters = 0;
+        for (Map.Entry<String, Object> entry : value.entrySet()) {
+            String key = entry.getKey();
+            DataType<?> innerType = type.innerType(key);
+            if (innerType == null) {
+                // could be a nested object or not part of meta data; skip pre-filtering
+                continue;
+            }
+            String fqNestedColumn = fqn + '.' + key;
+            Query innerQuery = fromPrimitive(innerType, fqNestedColumn, entry.getValue());
+            if (innerQuery == null) {
+                continue;
+            }
+
+            preFilters++;
+            boolBuilder.add(innerQuery, BooleanClause.Occur.MUST);
+        }
+        if (preFilters == value.size()) {
+            return boolBuilder.build();
+        } else {
+            Query genericEqFilter = genericFunctionFilter(eq, context);
+            if (preFilters == 0) {
+                return genericEqFilter;
+            } else {
+                boolBuilder.add(genericFunctionFilter(eq, context), BooleanClause.Occur.FILTER);
+                return boolBuilder.build();
+            }
+        }
     }
 }

server/src/test/java/io/crate/lucene/CommonQueryBuilderTest.java
@@ -211,10 +211,12 @@ public class CommonQueryBuilderTest extends LuceneQueryBuilderTest {
     @Test
     public void testIdQuery() throws Exception {
         Query query = convert("_id = 'i1'");
-        assertThat(query, instanceOf(TermInSetQuery.class));
+        assertThat(query, instanceOf(TermQuery.class));
+        assertThat(query.toString(), is("_id:[ff 69 31]"));
 
         query = convert("_id = 1");
-        assertThat(query, instanceOf(TermInSetQuery.class));
+        assertThat(query, instanceOf(TermQuery.class));
+        assertThat(query.toString(), is("_id:[fe 1f]"));
     }
 
     @Test

server/src/main/java/io/crate/lucene/EqQuery.java
@@ -1,121 +1,0 @@
-/*
- * Licensed to Crate.io GmbH ("Crate") under one or more contributor
- * license agreements.  See the NOTICE file distributed with this work for
- * additional information regarding copyright ownership.  Crate licenses
- * this file to you under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.  You may
- * obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
- * License for the specific language governing permissions and limitations
- * under the License.
- *
- * However, if you have executed another commercial license agreement
- * with Crate these terms will supersede the license and you may use the
- * software solely pursuant to the terms of the relevant commercial agreement.
- */
-
-package io.crate.lucene;
-
-import io.crate.expression.symbol.Function;
-import io.crate.expression.symbol.Literal;
-import io.crate.metadata.Reference;
-import io.crate.types.DataTypes;
-import io.crate.types.ObjectType;
-import org.apache.lucene.search.BooleanClause;
-import org.apache.lucene.search.BooleanQuery;
-import org.apache.lucene.search.Query;
-import org.elasticsearch.common.lucene.search.Queries;
-import org.elasticsearch.index.mapper.MappedFieldType;
-
-import java.util.List;
-import java.util.Map;
-
-import static io.crate.lucene.LuceneQueryBuilder.genericFunctionFilter;
-
-class EqQuery implements FunctionToQuery {
-
-    @Override
-    public Query apply(Function input, LuceneQueryBuilder.Context context) {
-        RefAndLiteral refAndLiteral = RefAndLiteral.of(input);
-        if (refAndLiteral == null) {
-            return null;
-        }
-        Reference reference = refAndLiteral.reference();
-        Literal literal = refAndLiteral.literal();
-        String columnName = reference.column().fqn();
-        MappedFieldType fieldType = context.getFieldTypeOrNull(columnName);
-        if (reference.valueType().id() == ObjectType.ID) {
-            //noinspection unchecked
-            return refEqObject(input, reference, (Map<String, Object>) literal.value(), context);
-        }
-        if (fieldType == null) {
-            // field doesn't exist, can't match
-            return Queries.newMatchNoDocsQuery("column does not exist in this index");
-        }
-        if (DataTypes.isArray(reference.valueType()) &&
-            DataTypes.isArray(literal.valueType())) {
-
-            List values = LuceneQueryBuilder.asList(literal);
-            if (values.isEmpty()) {
-                return genericFunctionFilter(input, context);
-            }
-            Query termsQuery = LuceneQueryBuilder.termsQuery(fieldType, values, context.queryShardContext);
-
-            // wrap boolTermsFilter and genericFunction filter in an additional BooleanFilter to control the ordering of the filters
-            // termsFilter is applied first
-            // afterwards the more expensive genericFunctionFilter
-            BooleanQuery.Builder filterClauses = new BooleanQuery.Builder();
-            filterClauses.add(termsQuery, BooleanClause.Occur.MUST);
-            filterClauses.add(genericFunctionFilter(input, context), BooleanClause.Occur.MUST);
-            return filterClauses.build();
-        }
-        return fieldType.termQuery(literal.value(), context.queryShardContext);
-    }
-
-    /**
-     * Query for object columns that tries to utilize efficient termQueries for the objects children.
-     * <pre>
-     * {@code
-     *      // If x and y are known columns
-     *      o = {x=10, y=20}    -> o.x=10 and o.y=20
-     *
-     *      // Only x is known:
-     *      o = {x=10, y=20}    -> o.x=10 and generic(o == {x=10, y=20})
-     *
-     *      // No column is known:
-     *      o = {x=10, y=20}    -> generic(o == {x=10, y=20})
-     * }
-     * </pre>
-     */
-    private static Query refEqObject(Function eq,
-                                     Reference reference,
-                                     Map<String, Object> value,
-                                     LuceneQueryBuilder.Context context) {
-        BooleanQuery.Builder boolBuilder = new BooleanQuery.Builder();
-        int preFilters = 0;
-        for (Map.Entry<String, Object> entry : value.entrySet()) {
-            String nestedColumn = entry.getKey();
-            MappedFieldType fieldType = context.getFieldTypeOrNull(reference.column().fqn() + "." + nestedColumn);
-            if (fieldType == null) {
-                // could be a nested object; skip pre-filtering
-                continue;
-            }
-            preFilters++;
-            boolBuilder.add(fieldType.termQuery(entry.getValue(), context.queryShardContext), BooleanClause.Occur.MUST);
-        }
-        Query genericFilter = genericFunctionFilter(eq, context);
-        if (preFilters == 0) {
-            return genericFilter;
-        } else if (preFilters == value.size()) {
-            return boolBuilder.build();
-        } else {
-            boolBuilder.add(genericFilter, BooleanClause.Occur.FILTER);
-            return boolBuilder.build();
-        }
-    }
-}

server/src/main/java/io/crate/lucene/FunctionToQuery.java
@@ -27,7 +27,7 @@ import org.apache.lucene.search.Query;
 import javax.annotation.Nullable;
 import java.io.IOException;
 
-interface FunctionToQuery {
+public interface FunctionToQuery {
 
     @Nullable
     Query apply(Function input, LuceneQueryBuilder.Context context) throws IOException;

server/src/main/java/io/crate/lucene/LuceneQueryBuilder.java
@@ -21,6 +21,35 @@
 
 package io.crate.lucene;
 
+import static io.crate.expression.eval.NullEliminator.eliminateNullsIfPossible;
+import static io.crate.metadata.DocReferences.inverseSourceLookup;
+import static java.util.Map.entry;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import javax.annotation.Nullable;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.lucene.search.BooleanClause;
+import org.apache.lucene.search.BooleanQuery;
+import org.apache.lucene.search.Query;
+import org.elasticsearch.ExceptionsHelper;
+import org.elasticsearch.common.inject.Inject;
+import org.elasticsearch.common.inject.Singleton;
+import org.elasticsearch.common.lucene.search.Queries;
+import org.elasticsearch.index.cache.IndexCache;
+import org.elasticsearch.index.mapper.MappedFieldType;
+import org.elasticsearch.index.mapper.MapperService;
+import org.elasticsearch.index.query.QueryShardContext;
+
 import io.crate.data.Input;
 import io.crate.exceptions.UnsupportedFeatureException;
 import io.crate.exceptions.VersioninigValidationException;
@@ -29,7 +58,6 @@ import io.crate.expression.InputFactory;
 import io.crate.expression.eval.EvaluatingNormalizer;
 import io.crate.expression.operator.AndOperator;
 import io.crate.expression.operator.CIDROperator;
-import io.crate.expression.operator.EqOperator;
 import io.crate.expression.operator.GtOperator;
 import io.crate.expression.operator.GteOperator;
 import io.crate.expression.operator.LikeOperators;
@@ -59,41 +87,16 @@ import io.crate.expression.symbol.Symbols;
 import io.crate.expression.symbol.format.Style;
 import io.crate.metadata.CoordinatorTxnCtx;
 import io.crate.metadata.DocReferences;
+import io.crate.metadata.FunctionImplementation;
 import io.crate.metadata.NodeContext;
 import io.crate.metadata.Reference;
 import io.crate.metadata.RowGranularity;
+import io.crate.metadata.Scalar;
 import io.crate.metadata.TransactionContext;
 import io.crate.metadata.doc.DocSysColumns;
 import io.crate.metadata.doc.DocTableInfo;
 import io.crate.sql.tree.ColumnPolicy;
 import io.crate.types.DataTypes;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.apache.lucene.search.BooleanClause;
-import org.apache.lucene.search.BooleanQuery;
-import org.apache.lucene.search.Query;
-import org.elasticsearch.ExceptionsHelper;
-import org.elasticsearch.common.inject.Inject;
-import org.elasticsearch.common.inject.Singleton;
-import org.elasticsearch.common.lucene.search.Queries;
-import org.elasticsearch.index.cache.IndexCache;
-import org.elasticsearch.index.mapper.MappedFieldType;
-import org.elasticsearch.index.mapper.MapperService;
-import org.elasticsearch.index.query.QueryShardContext;
-
-import javax.annotation.Nullable;
-import java.io.IOException;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Set;
-import java.util.stream.Collectors;
-
-import static io.crate.expression.eval.NullEliminator.eliminateNullsIfPossible;
-import static io.crate.metadata.DocReferences.inverseSourceLookup;
-import static java.util.Map.entry;
 
 
 @Singleton
@@ -141,7 +144,7 @@ public class LuceneQueryBuilder {
         return ctx;
     }
 
-    static Query termsQuery(@Nullable MappedFieldType fieldType, List values, QueryShardContext context) {
+    public static Query termsQuery(@Nullable MappedFieldType fieldType, List values, QueryShardContext context) {
         if (fieldType == null) {
             return Queries.newMatchNoDocsQuery("column does not exist in this index");
         }
@@ -164,6 +167,8 @@ public class LuceneQueryBuilder {
         private final TransactionContext txnCtx;
         final QueryShardContext queryShardContext;
 
+        final NodeContext nodeContext;
+
         Context(TransactionContext txnCtx,
                 NodeContext nodeCtx,
                 MapperService mapperService,
@@ -171,6 +176,7 @@ public class LuceneQueryBuilder {
                 QueryShardContext queryShardContext,
                 String indexName,
                 List<Reference> partitionColumns) {
+            this.nodeContext = nodeCtx;
             this.txnCtx = txnCtx;
             this.queryShardContext = queryShardContext;
             FieldTypeLookup typeLookup = mapperService::fullName;
@@ -225,7 +231,7 @@ public class LuceneQueryBuilder {
         );
 
         @Nullable
-        MappedFieldType getFieldTypeOrNull(String fqColumnName) {
+        public MappedFieldType getFieldTypeOrNull(String fqColumnName) {
             return mapperService.fullName(fqColumnName);
         }
 
@@ -274,7 +280,6 @@ public class LuceneQueryBuilder {
         }
 
 
-        private static final EqQuery EQ_QUERY = new EqQuery();
         private static final RangeQuery LT_QUERY = new RangeQuery("lt");
         private static final RangeQuery LTE_QUERY = new RangeQuery("lte");
         private static final RangeQuery GT_QUERY = new RangeQuery("gt");
@@ -285,7 +290,6 @@ public class LuceneQueryBuilder {
             entry(WithinFunction.NAME, WITHIN_QUERY),
             entry(AndOperator.NAME, new AndQuery()),
             entry(OrOperator.NAME, new OrQuery()),
-            entry(EqOperator.NAME, EQ_QUERY),
             entry(LtOperator.NAME, LT_QUERY),
             entry(LteOperator.NAME, LTE_QUERY),
             entry(GteOperator.NAME, GTE_QUERY),
@@ -329,7 +333,14 @@ public class LuceneQueryBuilder {
 
             FunctionToQuery toQuery = functions.get(function.name());
             if (toQuery == null) {
-                return genericFunctionFilter(function, context);
+                FunctionImplementation implementation = context.nodeContext.functions().getQualified(
+                    function,
+                    context.txnCtx.sessionSettings().searchPath()
+                );
+                Query query = implementation instanceof Scalar<?, ?> scalar
+                    ? scalar.toQuery(function, context)
+                    : null;
+                return returnQueryOrTryFallsbacks(query, function, context);
             }
 
             Query query;
@@ -340,13 +351,14 @@ public class LuceneQueryBuilder {
             } catch (UnsupportedOperationException e) {
                 return genericFunctionFilter(function, context);
             }
+            return returnQueryOrTryFallsbacks(query, function, context);
+        }
+
+        private Query returnQueryOrTryFallsbacks(@Nullable Query query, Function function, Context context) {
             if (query == null) {
                 query = queryFromInnerFunction(function, context);
-                if (query == null) {
-                    return genericFunctionFilter(function, context);
-                }
             }
-            return query;
+            return query == null ? genericFunctionFilter(function, context) : query;
         }
 
         private Query queryFromInnerFunction(Function function, Context context) {
@@ -451,7 +463,7 @@ public class LuceneQueryBuilder {
         }
     }
 
-    static Query genericFunctionFilter(Function function, Context context) {
+    public static Query genericFunctionFilter(Function function, Context context) {
         if (function.valueType() != DataTypes.BOOLEAN) {
             raiseUnsupported(function);
         }

server/src/main/java/io/crate/metadata/Scalar.java
@@ -21,15 +21,20 @@
 
 package io.crate.metadata;
 
-import io.crate.data.Input;
-import io.crate.expression.symbol.Function;
-import io.crate.expression.symbol.Literal;
-import io.crate.expression.symbol.Symbol;
-
 import java.util.Collection;
 import java.util.EnumSet;
 import java.util.List;
 import java.util.Set;
+
+import javax.annotation.Nullable;
+
+import org.apache.lucene.search.Query;
+
+import io.crate.data.Input;
+import io.crate.expression.symbol.Function;
+import io.crate.expression.symbol.Literal;
+import io.crate.expression.symbol.Symbol;
+import io.crate.lucene.LuceneQueryBuilder;
 
 /**
  * Base class for Scalar functions in crate.
@@ -83,6 +88,11 @@ public abstract class Scalar<ReturnType, InputType> implements FunctionImplement
      */
     public Scalar<ReturnType, InputType> compile(List<Symbol> arguments) {
         return this;
+    }
+
+    @Nullable
+    public Query toQuery(Function function, LuceneQueryBuilder.Context context) {
+        return null;
     }
 
     @Override

server/src/main/java/org/elasticsearch/index/mapper/MappedFieldType.java
@@ -34,6 +34,8 @@ import org.elasticsearch.index.analysis.NamedAnalyzer;
 import org.elasticsearch.index.query.QueryShardContext;
 import org.elasticsearch.index.query.QueryShardException;
 
+import io.crate.metadata.Scalar;
+
 /**
  * This defines the core properties and functions to operate on a field.
  */
@@ -116,9 +118,14 @@ public abstract class MappedFieldType {
      *  @throws ElasticsearchParseException if {@code value} cannot be converted to the expected data type
      *  @throws UnsupportedOperationException if the field is not searchable regardless of options
      *  @throws QueryShardException if the field is not searchable regardless of options
+     *  @deprecated EqOperator implements {@link Scalar#toQuery(io.crate.expression.symbol.Function, io.crate.lucene.LuceneQueryBuilder.Context)}.
      */
     // TODO: Standardize exception types
-    public abstract Query termQuery(Object value, @Nullable QueryShardContext context);
+    @Deprecated
+    public Query termQuery(Object value, @Nullable QueryShardContext context) {
+        throw new UnsupportedOperationException(
+            "termQuery is depreacted. EqOperator.toQuery must be used instead");
+    }
 
     /** Build a constant-scoring query that matches all values. The default implementation uses a
      * {@link ConstantScoreQuery} around a {@link BooleanQuery} whose {@link Occur#SHOULD} clauses

commit d466ad94822336e81699d178163b3ba5ec3f8c70

Ensure EQ (ANY) on BitString uses Term(s)Query
Follow up to https://github.com/crate/crate/pull/11814 / fixes a
regression introduced with https://github.com/crate/crate/pull/11669

server/src/main/java/io/crate/expression/operator/EqOperator.java
@@ -46,6 +46,7 @@ import org.apache.lucene.search.ConstantScoreQuery;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.TermInSetQuery;
 import org.apache.lucene.search.TermQuery;
+import org.apache.lucene.util.BytesRef;
 import org.elasticsearch.common.lucene.BytesRefs;
 import org.elasticsearch.common.lucene.search.Queries;
 import org.elasticsearch.common.network.InetAddresses;
@@ -64,7 +65,9 @@ import io.crate.metadata.Reference.IndexType;
 import io.crate.metadata.TransactionContext;
 import io.crate.metadata.doc.DocSysColumns;
 import io.crate.metadata.functions.Signature;
+import io.crate.sql.tree.BitString;
 import io.crate.types.ArrayType;
+import io.crate.types.BitStringType;
 import io.crate.types.ByteType;
 import io.crate.types.DataType;
 import io.crate.types.DoubleType;
@@ -168,6 +171,10 @@ public final class EqOperator extends Operator<Object> {
                 column,
                 nonNullValues.stream().map(x -> InetAddresses.forString((String) x)).toArray(InetAddress[]::new)
             );
+            case BitStringType.ID -> new TermInSetQuery(
+                column,
+                nonNullValues.stream().map(x -> new BytesRef(((BitString) x).bitSet().toByteArray())).toList()
+            );
             default -> booleanShould(column, type, nonNullValues);
         };
     }
@@ -212,6 +219,7 @@ public final class EqOperator extends Operator<Object> {
             case DoubleType.ID -> DoublePoint.newExactQuery(column, (double) value);
             case StringType.ID -> new TermQuery(new Term(column, BytesRefs.toBytesRef(value)));
             case IpType.ID -> InetAddressPoint.newExactQuery(column, InetAddresses.forString((String) value));
+            case BitStringType.ID -> new TermQuery(new Term(column, new BytesRef(((BitString) value).bitSet().toByteArray())));
             default -> null;
         };
     }

server/src/test/java/io/crate/lucene/CommonQueryBuilderTest.java
@@ -588,4 +588,18 @@ public class CommonQueryBuilderTest extends LuceneQueryBuilderTest {
         Query query = convert("f = ANY([42.0, 41.0])");
         assertThat(query.toString(), is("f:{41.0 42.0}"));
     }
+
+    @Test
+    public void test_eq_on_bits_uses_term_query() throws Exception {
+        Query query = convert("bits = B'01001110'");
+        assertThat(query, instanceOf(TermQuery.class));
+        assertThat(query.toString(), is("bits:r"));
+    }
+
+    @Test
+    public void test_eq_any_on_bits_uses_termset_query() throws Exception {
+        Query query = convert("bits = ANY([B'01001110', B'11111111'])");
+        assertThat(query, instanceOf(TermInSetQuery.class));
+        assertThat(query.toString(), is("bits:(r [ff])"));
+    }
 }