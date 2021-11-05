ceca1bfb6557d27487819535f25c5729f1c863c6
Sebastian Utz <su@rtme.net>
2021-11-05 14:48:01 +0100

    Fix logical replication initialization, must only run on the master node
    Only the master not must trigger the inital restore, while all
    nodes must create a connection to the remote cluster to be able
    to process the restore.
    Also shard changes tracker must ensure that a correct connection
    is established before requesting changes.
    The subscription name which connections are based on and are required
    for connection checking/creation is now stored inside the index setting
    which also serves as a marker indicating that an index is a replicated
    one (-> uses special SubscriberEngine).

--- server/src/main/java/io/crate/replication/logical/ShardReplicationService.java
+++ server/src/main/java/io/crate/replication/logical/ShardReplicationService.java
@@ -21,21 +21,32 @@
 
 package io.crate.replication.logical;
 
+import static io.crate.replication.logical.LogicalReplicationSettings.REPLICATION_SUBSCRIPTION_NAME;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Consumer;
+
+import javax.annotation.Nullable;
+
 import org.apache.logging.log4j.Logger;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.common.logging.Loggers;
 import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.util.concurrent.ConcurrentCollections;
+import org.elasticsearch.index.Index;
+import org.elasticsearch.index.IndexService;
 import org.elasticsearch.index.shard.IndexEventListener;
 import org.elasticsearch.index.shard.IndexShard;
 import org.elasticsearch.index.shard.ShardId;
+import org.elasticsearch.indices.cluster.IndicesClusterStateService.AllocatedIndices.IndexRemovalReason;
 import org.elasticsearch.threadpool.ThreadPool;
 
-import javax.annotation.Nullable;
-import java.io.Closeable;
-import java.io.IOException;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+import io.crate.replication.logical.exceptions.NoSubscriptionForIndexException;
+import io.crate.replication.logical.metadata.Subscription;
 
 public class ShardReplicationService implements Closeable, IndexEventListener {
 
@@ -46,6 +57,7 @@ public class ShardReplicationService implements Closeable, IndexEventListener {
     private final Client client;
     private final ThreadPool threadPool;
     private final Map<ShardId, ShardReplicationChangesTracker> shards = new ConcurrentHashMap<>();
+    private final Map<String, String> subscribedIndices = ConcurrentCollections.newConcurrentMap();
 
     @Inject
     public ShardReplicationService(LogicalReplicationService logicalReplicationService,
@@ -65,25 +77,70 @@ public class ShardReplicationService implements Closeable, IndexEventListener {
         }
     }
 
-    @Nullable
-    private Client getRemoteClusterClient(String indexName) {
-        var subscriptionName = logicalReplicationService.subscriptionName(indexName);
+    void getRemoteClusterClient(Index index, Consumer<Client> onSuccess, Consumer<Exception> onFailure) {
+        var subscriptionName = subscriptionName(index.getUUID());
         if (subscriptionName != null) {
-            return logicalReplicationService.getRemoteClusterClient(threadPool, subscriptionName);
+            var subscription = logicalReplicationService.subscriptions().get(subscriptionName);
+            if (subscription != null) {
+                logicalReplicationService.updateRemoteConnection(
+                    subscriptionName,
+                    subscription.connectionInfo().settings(),
+                    ignored -> onSuccess.accept(
+                        logicalReplicationService.getRemoteClusterClient(threadPool, subscriptionName)
+                    ),
+                    onFailure
+                );
+            } else {
+                onFailure.accept(new NoSubscriptionForIndexException(index));
+            }
+
+        } else {
+            onFailure.accept(new NoSubscriptionForIndexException(index));
+        }
+    }
+
+    @Nullable
+    private String subscriptionName(String indexUUID) {
+        return subscribedIndices.get(indexUUID);
+    }
+
+    @Nullable
+    private Subscription subscription(String indexUUID) {
+        var subscriptionName = subscribedIndices.get(indexUUID);
+        if (subscriptionName != null) {
+            return logicalReplicationService.subscriptions().get(subscriptionName);
         }
         return null;
     }
 
     @Override
+    public void afterIndexCreated(IndexService indexService) {
+        var settings = indexService.getIndexSettings().getSettings();
+        var subscriptionName = REPLICATION_SUBSCRIPTION_NAME.get(settings);
+        if (subscriptionName != null) {
+            subscribedIndices.put(indexService.indexUUID(), subscriptionName);
+        }
+    }
+
+    @Override
+    public void beforeIndexRemoved(IndexService indexService, IndexRemovalReason reason) {
+        var settings = indexService.getIndexSettings().getSettings();
+        var subscriptionName = REPLICATION_SUBSCRIPTION_NAME.get(settings);
+        if (subscriptionName != null) {
+            subscribedIndices.remove(indexService.indexUUID());
+        }
+    }
+
+    @Override
     public void afterIndexShardStarted(IndexShard indexShard) {
         if (indexShard.routingEntry().primary()) {
-            var subscription = logicalReplicationService.subscription(indexShard.shardId().getIndexName());
+            var subscription = subscription(indexShard.shardId().getIndex().getUUID());
             if (subscription != null) {
                 var tracker = new ShardReplicationChangesTracker(
                     indexShard,
                     replicationSettings,
                     threadPool,
-                    ShardReplicationService.this::getRemoteClusterClient,
+                    ShardReplicationService.this,
                     client
                 );
                 shards.put(indexShard.shardId(), tracker);
