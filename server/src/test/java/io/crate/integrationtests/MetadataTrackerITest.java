9d6523a52a9cc6123f3cdbc759781a9c2ec956f8
Sebastian Utz <su@rtme.net>
2021-12-14 13:03:34 +0100

    Simplified metadata tracker integration tests

--- server/src/test/java/io/crate/integrationtests/MetadataTrackerITest.java
+++ server/src/test/java/io/crate/integrationtests/MetadataTrackerITest.java
@@ -21,13 +21,14 @@
 
 package io.crate.integrationtests;
 
+import io.crate.replication.logical.LogicalReplicationService;
+import io.crate.replication.logical.MetadataTracker;
 import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.test.junit.annotations.TestLogging;
 import org.junit.Test;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 import static io.crate.replication.logical.LogicalReplicationSettings.REPLICATION_READ_POLL_DURATION;
 import static io.crate.testing.TestingHelpers.printedTable;
@@ -46,43 +47,34 @@ public class MetadataTrackerITest extends LogicalReplicationITestCase {
     }
 
     @Test
-    public void test_subscribed_tables_are_followed_when_schema_changed() throws Exception {
-        executeOnPublisher("CREATE TABLE doc.t1 (id INT) CLUSTERED INTO 1 SHARDS WITH(" +
-                           defaultTableSettings() +")");
+    public void test_schema_changes_of_subscribed_table_is_replicated() throws Exception {
+        executeOnPublisher("CREATE TABLE doc.t1 (id INT) WITH(" + defaultTableSettings() +")");
         executeOnPublisher("INSERT INTO doc.t1 (id) VALUES (1), (2)");
-
         createPublication("pub1", false, List.of("doc.t1"));
-
         createSubscription("sub1", "pub1");
 
-        assertBusy(() -> {
-            var response = executeOnSubscriber("SELECT * FROM doc.t1");
-            assertThat(printedTable(response.rows()), is("1\n" +
-                                                         "2\n"));
-        }, 10, TimeUnit.SECONDS);
+        // Ensure tracker has started
+        assertBusy(() -> assertThat(isTrackerActive(), is(true)));
 
         executeOnPublisher("ALTER TABLE doc.t1 ADD COLUMN value string");
-        executeOnPublisher("REFRESH TABLE doc.t1");
-
         assertBusy(() -> {
-            executeOnSubscriber("REFRESH TABLE doc.t1");
-            var r = executeOnSubscriber("SELECT * FROM doc.t1");
-            assertThat(printedTable(r.rows()), is("1| NULL\n" +
-                                                  "2| NULL\n"));
-
-        }, 10, TimeUnit.SECONDS);
+            var r = executeOnSubscriber("SELECT column_name FROM information_schema.columns" +
+                                        " WHERE table_name = 't1'" +
+                                        " ORDER BY ordinal_position");
+            assertThat(printedTable(r.rows()), is("id\n" +
+                                                  "value\n"));
+        });
     }
 
-
     @Test
-    public void test_subscribed_tables_are_followed_when_schema_changed_and_new_data_is_synced() throws Exception {
-        executeOnPublisher("CREATE TABLE doc.t1 (id INT) CLUSTERED INTO 1 SHARDS WITH(" +
-                           defaultTableSettings() +")");
+    public void test_schema_changes_of_subscribed_table_is_replicated_and_new_data_is_synced() throws Exception {
+        executeOnPublisher("CREATE TABLE doc.t1 (id INT) WITH(" + defaultTableSettings() +")");
         executeOnPublisher("INSERT INTO doc.t1 (id) VALUES (1), (2)");
-
         createPublication("pub1", false, List.of("doc.t1"));
-
         createSubscription("sub1", "pub1");
+
+        // Ensure tracker has started
+        assertBusy(() -> assertThat(isTrackerActive(), is(true)));
 
         executeOnPublisher("ALTER TABLE doc.t1 ADD COLUMN name varchar");
         //This insert is synced to the subscriber and the mapping might not be updated yet
@@ -97,7 +89,7 @@ public class MetadataTrackerITest extends LogicalReplicationITestCase {
 
         assertBusy(() -> {
             executeOnSubscriber("REFRESH TABLE doc.t1");
-            var r = executeOnSubscriber("SELECT * FROM doc.t1");
+            var r = executeOnSubscriber("SELECT * FROM doc.t1 ORDER BY id");
             assertThat(printedTable(r.rows()), is("1| NULL| NULL\n" +
                                                   "2| NULL| NULL\n" +
                                                   "3| chewbacca| NULL\n"+
@@ -105,54 +97,46 @@ public class MetadataTrackerITest extends LogicalReplicationITestCase {
                                                   "5| luke| 37\n" +
                                                   "6| yoda| 900\n"));
 
-        }, 10, TimeUnit.SECONDS);
-
+        });
     }
 
     @Test
-    public void test_add_multiple_tables() throws Exception {
+    public void test_schema_changes_of_multiple_tables_are_replicated() throws Exception {
         int numberOfTables = randomIntBetween(1,10);
         var tableNames = new ArrayList<String>();
         for (int i = 1; i <= numberOfTables; i++) {
-            tableNames.add("doc.t" + i);
-        }
-
-        for (String tableName : tableNames) {
-            executeOnPublisher("CREATE TABLE " + tableName + " (id INT) CLUSTERED INTO 1 SHARDS WITH(" +
-                               defaultTableSettings() +")");
-            executeOnPublisher("INSERT INTO " + tableName + " (id) VALUES (1), (2)");
+            executeOnPublisher("CREATE TABLE t" + i + " (id INT) WITH(" + defaultTableSettings() +")");
+            tableNames.add("t" + i);
         }
 
         createPublication("pub1", false, tableNames);
-
         createSubscription("sub1", "pub1");
+
+        // Ensure tracker has started
+        assertBusy(() -> assertThat(isTrackerActive(), is(true)));
 
         for (String tableName : tableNames) {
             executeOnPublisher("ALTER TABLE " + tableName + " ADD COLUMN name varchar");
-            executeOnPublisher("INSERT INTO " + tableName + " (id, name) VALUES (3, 'chewbacca'), (4, 'r2d2')");
-        }
-
-        executeOnPublisher("REFRESH TABLE " + String.join(",", tableNames));
-
-        for (String tableName : tableNames) {
-            executeOnPublisher("ALTER TABLE " + tableName + " ADD COLUMN age integer");
-        }
-
-        for (String tableName : tableNames) {
-            executeOnPublisher("INSERT INTO " + tableName + "(id, name, age) VALUES (5, 'luke', 37), (6, 'yoda', 900)");
         }
 
         assertBusy(() -> {
-            executeOnSubscriber("REFRESH TABLE " + String.join(",", tableNames));
             for (String tableName : tableNames) {
-                var r = executeOnSubscriber("SELECT * FROM " + tableName);
-                assertThat(printedTable(r.rows()), is("1| NULL| NULL\n" +
-                                                      "2| NULL| NULL\n" +
-                                                      "3| chewbacca| NULL\n" +
-                                                      "4| r2d2| NULL\n" +
-                                                      "5| luke| 37\n" +
-                                                      "6| yoda| 900\n"));
+                var r = executeOnSubscriber("SELECT column_name FROM information_schema.columns" +
+                                            " WHERE table_name = '" + tableName + "'" +
+                                            " ORDER BY ordinal_position");
+                assertThat(printedTable(r.rows()), is("id\n" +
+                                                      "name\n"));
             }
-        }, 10, TimeUnit.SECONDS);
+        });
+    }
+
+    private boolean isTrackerActive() throws Exception {
+        var replicationService = subscriberCluster.getInstance(LogicalReplicationService.class, subscriberCluster.getMasterName());
+        Field m = replicationService.getClass().getDeclaredField("metadataTracker");
+        m.setAccessible(true);
+        MetadataTracker metadataTracker = (MetadataTracker) m.get(replicationService);
+        Field f1 = metadataTracker.getClass().getDeclaredField("isActive");
+        f1.setAccessible(true);
+        return (boolean) f1.get(metadataTracker);
     }
 }
