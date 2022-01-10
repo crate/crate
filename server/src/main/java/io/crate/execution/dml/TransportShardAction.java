5ab9acd65e847bad43cf35b78cf52abb2ea423eb
Michael Kleen <michael.kleen@crate.io>
2022-01-10 17:22:53 +0100

    Remove guava Throwable.propagate
    See https://github.com/google/guava/wiki/Why-we-deprecated-Throwables.propagate
    for details

--- server/src/main/java/io/crate/execution/dml/TransportShardAction.java
+++ server/src/main/java/io/crate/execution/dml/TransportShardAction.java
@@ -21,7 +21,6 @@
 
 package io.crate.execution.dml;
 
-import com.google.common.base.Throwables;
 import io.crate.common.CheckedSupplier;
 import io.crate.common.annotations.VisibleForTesting;
 import io.crate.exceptions.JobKilledException;
@@ -142,13 +141,18 @@ public abstract class TransportShardAction<Request extends ShardRequest<Request,
         WrapperResponse response;
         try {
             response = callable.call();
-        } catch (Throwable e) {
-            throw Throwables.propagate(e);
+        } catch (Throwable t) {
+            if (t instanceof RuntimeException r) {
+                throw r;
+            } else if (t instanceof Error err) {
+                throw err;
+            } else {
+                throw new RuntimeException(t);
+            }
         } finally {
             activeOperations.remove(id, callable);
         }
         return response;
-
     }
 
     @Override
