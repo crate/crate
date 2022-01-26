433f9abcf862b8f4ce1a746e12e64168d4724898
Mathias Fussenegger <f.mathias@zignar.net>
2022-01-26 09:16:42 +0100

    Bump version to 4.8.0-SNAPSHOT

--- server/src/main/java/org/elasticsearch/Version.java
+++ server/src/main/java/org/elasticsearch/Version.java
@@ -129,7 +129,9 @@ public class Version implements Comparable<Version>, ToXContentFragment {
 
     public static final Version V_4_7_0 = new Version(7_07_00_99, false, org.apache.lucene.util.Version.LUCENE_8_11_0);
 
-    public static final Version CURRENT = V_4_7_0;
+    public static final Version V_4_8_0 = new Version(7_08_00_99, true, org.apache.lucene.util.Version.LUCENE_8_11_0);
+
+    public static final Version CURRENT = V_4_8_0;
 
     private static final ImmutableOpenIntMap<Version> ID_TO_VERSION;
 
