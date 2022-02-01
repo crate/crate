f57bf7564e443f884835bddba3cda7d02ff852a0
Michael Kleen <michael.kleen@crate.io>
2022-02-01 08:55:09 +0100

    Fix syncGlobalCheckpointAfterOperation flag in TransportWriteAction
    This corrects the syncGlobalCheckpointAfterOperation flag to true in
    TransportWriteAction. This was caused by mistake in the backport
    https://github.com/crate/crate/commit/206ee29e167c3cb01695af77b1207d086177e871.
    
    (cherry picked from commit abd07cad1bfea9f59f6c80955507582be9238bae)

--- server/src/main/java/org/elasticsearch/action/support/replication/TransportWriteAction.java
+++ server/src/main/java/org/elasticsearch/action/support/replication/TransportWriteAction.java
@@ -73,7 +73,7 @@ public abstract class TransportWriteAction<
             reader,
             replicaReader,
             executor,
-            false,
+            true,
             forceExecutionOnPrimary);
     }
 
