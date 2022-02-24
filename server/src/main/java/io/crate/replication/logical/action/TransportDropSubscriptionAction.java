2021844c47f14f1cc1112d12009b9f5837036ad7
Sebastian Utz <su@rtme.net>
2022-02-24 09:30:28 +0100

    Resolve subscripted relations by subscription metadata
    The subscription metadata contains all relations belonging to it.
    Use this information instead of filtering all tables by the subscription
    index setting.

--- server/src/main/java/io/crate/replication/logical/action/TransportDropSubscriptionAction.java
+++ server/src/main/java/io/crate/replication/logical/action/TransportDropSubscriptionAction.java
@@ -21,10 +21,8 @@
 
 package io.crate.replication.logical.action;
 
-import io.crate.execution.ddl.AbstractDDLTransportAction;
-import io.crate.metadata.cluster.DDLClusterStateTaskExecutor;
-import io.crate.replication.logical.exceptions.SubscriptionUnknownException;
-import io.crate.replication.logical.metadata.SubscriptionsMetadata;
+import static io.crate.replication.logical.LogicalReplicationSettings.REPLICATION_SUBSCRIPTION_NAME;
+
 import org.elasticsearch.action.support.master.AcknowledgedResponse;
 import org.elasticsearch.cluster.ClusterState;
 import org.elasticsearch.cluster.ClusterStateTaskExecutor;
@@ -34,17 +32,17 @@ import org.elasticsearch.cluster.metadata.IndexMetadata;
 import org.elasticsearch.cluster.metadata.IndexNameExpressionResolver;
 import org.elasticsearch.cluster.metadata.Metadata;
 import org.elasticsearch.cluster.service.ClusterService;
-import org.elasticsearch.common.collect.ImmutableOpenMap;
 import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.common.inject.Singleton;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.threadpool.ThreadPool;
 import org.elasticsearch.transport.TransportService;
 
-import javax.annotation.Nonnull;
-import java.util.Iterator;
-
-import static io.crate.replication.logical.LogicalReplicationSettings.REPLICATION_SUBSCRIPTION_NAME;
+import io.crate.execution.ddl.AbstractDDLTransportAction;
+import io.crate.metadata.cluster.DDLClusterStateTaskExecutor;
+import io.crate.replication.logical.exceptions.SubscriptionUnknownException;
+import io.crate.replication.logical.metadata.Subscription;
+import io.crate.replication.logical.metadata.SubscriptionsMetadata;
 
 @Singleton
 public class TransportDropSubscriptionAction extends AbstractDDLTransportAction<DropSubscriptionRequest, AcknowledgedResponse> {
@@ -81,11 +79,11 @@ public class TransportDropSubscriptionAction extends AbstractDDLTransportAction<
                     // This is second step out of 3. Tracking of shards is already stopped on the first step (CLOSE TABLE).
                     // Removing setting so that last step (OPEN TABLE) will update engine.
                     SubscriptionsMetadata newMetadata = SubscriptionsMetadata.newInstance(oldMetadata);
-                    newMetadata.subscription().remove(request.name());
+                    var subscription = newMetadata.subscription().remove(request.name());
                     assert !newMetadata.equals(oldMetadata) : "must not be equal to guarantee the cluster change action";
                     mdBuilder.putCustom(SubscriptionsMetadata.TYPE, newMetadata);
 
-                    mdBuilder = removeSubscriptionSetting(currentMetadata.indices(), mdBuilder, request.name());
+                    removeSubscriptionSetting(subscription, currentMetadata, mdBuilder);
 
                     return ClusterState.builder(currentState).metadata(mdBuilder).build();
                 } else if (request.ifExists() == false) {
@@ -103,28 +101,23 @@ public class TransportDropSubscriptionAction extends AbstractDDLTransportAction<
     }
 
     /**
-     * Removes the REPLICATION_SUBSCRIPTION_NAME index setting from all relevant to subscription indices.
+     * Removes the REPLICATION_SUBSCRIPTION_NAME index setting from all indices included by the subscription.
      */
-    private Metadata.Builder removeSubscriptionSetting(ImmutableOpenMap<String, IndexMetadata> indices,
-                                                       Metadata.Builder mdBuilder,
-                                                       @Nonnull String subscriptionToDrop) {
-        Iterator<IndexMetadata> indicesIterator = indices.valuesIt();
-        while (indicesIterator.hasNext()) {
-            IndexMetadata indexMetadata = indicesIterator.next();
-            var settings = indexMetadata.getSettings();
-            var subscriptionName = REPLICATION_SUBSCRIPTION_NAME.get(settings); // Can be null.
-            if (subscriptionToDrop.equals(subscriptionName)) {
-                var settingsBuilder = Settings.builder().put(settings);
-                settingsBuilder.remove(REPLICATION_SUBSCRIPTION_NAME.getKey());
-                mdBuilder.put(
-                    IndexMetadata
-                        .builder(indexMetadata)
-                        .settingsVersion(1 + indexMetadata.getSettingsVersion())
-                        .settings(settingsBuilder)
-                );
-            }
+    private void removeSubscriptionSetting(Subscription subscription,
+                                           Metadata metadata,
+                                           Metadata.Builder mdBuilder) {
+        for (var relationName : subscription.relations().keySet()) {
+            IndexMetadata indexMetadata = metadata.index(relationName.indexNameOrAlias());
+            assert indexMetadata != null : "Cannot resolve indexMetadata for relation=" + relationName;
+            var settingsBuilder = Settings.builder().put(indexMetadata.getSettings());
+            settingsBuilder.remove(REPLICATION_SUBSCRIPTION_NAME.getKey());
+            mdBuilder.put(
+                IndexMetadata
+                    .builder(indexMetadata)
+                    .settingsVersion(1 + indexMetadata.getSettingsVersion())
+                    .settings(settingsBuilder)
+            );
         }
-        return mdBuilder;
     }
 
 }
