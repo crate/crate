bc7ba16384c753e9e6078908e09590bc749b7cca
Sebastian Utz <su@rtme.net>
2022-04-28 16:00:53 +0200

    Release 4.7.2

--- server/src/main/java/org/elasticsearch/Version.java
+++ server/src/main/java/org/elasticsearch/Version.java
@@ -130,7 +130,7 @@ public class Version implements Comparable<Version>, ToXContentFragment {
 
     public static final Version V_4_7_0 = new Version(7_07_00_99, false, org.apache.lucene.util.Version.LUCENE_8_11_0);
     public static final Version V_4_7_1 = new Version(7_07_01_99, false, org.apache.lucene.util.Version.LUCENE_8_11_0);
-    public static final Version V_4_7_2 = new Version(7_07_02_99, true, org.apache.lucene.util.Version.LUCENE_8_11_0);
+    public static final Version V_4_7_2 = new Version(7_07_02_99, false, org.apache.lucene.util.Version.LUCENE_8_11_0);
 
     public static final Version CURRENT = V_4_7_2;
 
