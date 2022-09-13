5f773087999bbafb571d4399371d00fc11a45ee3
Mathias Fussenegger <f.mathias@zignar.net>
2022-09-13 11:50:03 +0200

    Fix race that could lead to pg wire messages being out-of-order
    The `completionFuture` of a `ResultReceiver` was used to both:
    
    - Write out pending messages
    - Trigger the next execution if there are multiple deferred statements
    
    There was a race where the next execution could write out
    `commandComplete` _before_ the "write pending messages" routine
    triggered, leading to out-of-order messages on the client side.
    
    This caused the JDBC client to fail with:
    
    	java.util.NoSuchElementException
    		at __randomizedtesting.SeedInfo.seed([FAA2E13C629A53DB:BF7BE6AFD08EA59]:0)
    		at java.base/java.util.ArrayDeque.removeFirst(ArrayDeque.java:362)
    		at org.postgresql.core.v3.QueryExecutorImpl.processResults(QueryExecutorImpl.java:2129)
    		at org.postgresql.core.v3.QueryExecutorImpl.execute(QueryExecutorImpl.java:356)
    		at org.postgresql.jdbc.PgStatement.executeInternal(PgStatement.java:496)
    		at org.postgresql.jdbc.PgStatement.execute(PgStatement.java:413)
    		at org.postgresql.jdbc.PgStatement.executeWithFlags(PgStatement.java:333)
    		at org.postgresql.jdbc.PgStatement.executeCachedSql(PgStatement.java:319)
    		at org.postgresql.jdbc.PgStatement.executeWithFlags(PgStatement.java:295)
    		at org.postgresql.jdbc.PgStatement.executeUpdate(PgStatement.java:268)
    
    To remove the race this moves the "write pending messages" trigger into
    the ResultReciver to ensure it's run _before_ triggering the completion
    future.
    
    (cherry picked from commit 744dea03fca169c1257b4e302b3dc278ef54b1b0)

--- server/src/test/java/io/crate/protocols/postgres/DelayableWriteChannelTest.java
+++ server/src/test/java/io/crate/protocols/postgres/DelayableWriteChannelTest.java
@@ -24,8 +24,6 @@ package io.crate.protocols.postgres;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-import java.util.concurrent.CompletableFuture;
-
 import org.junit.Test;
 
 import io.netty.buffer.ByteBuf;
@@ -37,8 +35,7 @@ public class DelayableWriteChannelTest {
     @Test
     public void test_delayed_writes_are_released_on_close() throws Exception {
         var channel = new DelayableWriteChannel(new EmbeddedChannel());
-        var future = new CompletableFuture<>();
-        channel.delayWritesUntil(future);
+        channel.delayWrites();
         ByteBuf buffer = Unpooled.buffer();
         channel.write(buffer);
         channel.close();
