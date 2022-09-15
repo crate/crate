5efcc9d765d242e4e60c8a16c244c2fbe6ebdf8b
baur <baurzhansahariev@gmail.com>
2022-09-15 14:30:01 +0200

    bump version to 4.8.5-SNAPSHOT

--- server/src/main/java/org/elasticsearch/Version.java
+++ server/src/main/java/org/elasticsearch/Version.java
@@ -138,8 +138,9 @@ public class Version implements Comparable<Version>, ToXContentFragment {
     public static final Version V_4_8_2 = new Version(7_08_02_99, false, org.apache.lucene.util.Version.LUCENE_8_11_0);
     public static final Version V_4_8_3 = new Version(7_08_03_99, false, org.apache.lucene.util.Version.LUCENE_8_11_0);
     public static final Version V_4_8_4 = new Version(7_08_04_99, false, org.apache.lucene.util.Version.LUCENE_8_11_0);
+    public static final Version V_4_8_5 = new Version(7_08_05_99, true, org.apache.lucene.util.Version.LUCENE_8_11_0);
 
-    public static final Version CURRENT = V_4_8_4;
+    public static final Version CURRENT = V_4_8_5;
 
     private static final ImmutableOpenIntMap<Version> ID_TO_VERSION;
 
