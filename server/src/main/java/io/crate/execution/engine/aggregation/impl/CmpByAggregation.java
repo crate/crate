8f70b55988f65105ca92a31e1ac57e94a0a1b240
Mathias Fussenegger <f.mathias@zignar.net>
2022-10-04 17:43:53 +0200

    Add `max_by` and `min_by` aggregation functions
    Closes https://github.com/crate/crate/issues/10528

---
+++ server/src/main/java/io/crate/execution/engine/aggregation/impl/CmpByAggregation.java
@@ -0,0 +1,174 @@
+/*
+ * Licensed to Crate.io GmbH ("Crate") under one or more contributor
+ * license agreements.  See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.  Crate licenses
+ * this file to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.  You may
+ * obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * However, if you have executed another commercial license agreement
+ * with Crate these terms will supersede the license and you may use the
+ * software solely pursuant to the terms of the relevant commercial agreement.
+ */
+
+package io.crate.execution.engine.aggregation.impl;
+
+import static io.crate.types.TypeSignature.parseTypeSignature;
+
+import javax.annotation.Nullable;
+
+import org.elasticsearch.Version;
+import org.elasticsearch.common.breaker.CircuitBreakingException;
+
+import io.crate.breaker.RamAccounting;
+import io.crate.data.Input;
+import io.crate.execution.engine.aggregation.AggregationFunction;
+import io.crate.memory.MemoryManager;
+import io.crate.metadata.functions.BoundSignature;
+import io.crate.metadata.functions.Signature;
+import io.crate.metadata.functions.TypeVariableConstraint;
+import io.crate.types.DataType;
+import io.crate.types.DataTypes;
+import io.crate.types.TypeSignature;
+
+public final class CmpByAggregation extends AggregationFunction<CmpByAggregation.CompareBy, Object> {
+
+    public static final String MAX_BY = "max_by";
+    public static final String MIN_BY = "min_by";
+
+    static class CompareBy {
+
+        Comparable<Object> cmpValue;
+        Object resultValue;
+    }
+
+    static {
+        DataTypes.register(CompareByType.ID, CompareByType::new);
+    }
+
+    public static void register(AggregationImplModule mod) {
+        TypeSignature returnValueType = parseTypeSignature("A");
+        TypeSignature cmpType = parseTypeSignature("B");
+        var variableConstraintA = TypeVariableConstraint.typeVariableOfAnyType("A");
+        var variableConstraintB = TypeVariableConstraint.typeVariableOfAnyType("B");
+        mod.register(
+            Signature.aggregate(
+                MAX_BY,
+                returnValueType,
+                cmpType,
+                returnValueType
+            ).withTypeVariableConstraints(
+                variableConstraintA,
+                variableConstraintB
+            ),
+            (signature, boundSignature) -> new CmpByAggregation(1, signature, boundSignature)
+        );
+        mod.register(
+            Signature.aggregate(
+                MIN_BY,
+                returnValueType,
+                cmpType,
+                returnValueType
+            ).withTypeVariableConstraints(
+                variableConstraintA,
+                variableConstraintB
+            ),
+            (signature, boundSignature) -> new CmpByAggregation(-1, signature, boundSignature)
+        );
+    }
+
+    private final Signature signature;
+    private final BoundSignature boundSignature;
+    private final CompareByType partialType;
+    private final int cmpResult;
+
+    public CmpByAggregation(int cmpResult, Signature signature, BoundSignature boundSignature) {
+        this.cmpResult = cmpResult;
+        this.signature = signature;
+        this.boundSignature = boundSignature;
+        int size = boundSignature.argTypes().size();
+        if (size == 1) {
+            this.partialType = (CompareByType) boundSignature.argTypes().get(0);
+        } else {
+            this.partialType = new CompareByType(
+                boundSignature.argTypes().get(0),
+                boundSignature.argTypes().get(1)
+            );
+        }
+    }
+
+    @Override
+    public Signature signature() {
+        return signature;
+    }
+
+    @Override
+    public BoundSignature boundSignature() {
+        return boundSignature;
+    }
+
+    @Override
+    @Nullable
+    public CompareBy newState(RamAccounting ramAccounting,
+                              Version indexVersionCreated,
+                              Version minNodeInCluster,
+                              MemoryManager memoryManager) {
+        return new CompareBy();
+    }
+
+    @Override
+    @SuppressWarnings({"unchecked", "rawtypes"})
+    public CompareBy iterate(RamAccounting ramAccounting,
+                             MemoryManager memoryManager,
+                             CompareBy state,
+                             Input... args) throws CircuitBreakingException {
+        Object cmpVal = args[1].value();
+
+        if (cmpVal instanceof Comparable comparable) {
+            Comparable<Object> currentValue = state.cmpValue;
+            if (currentValue == null || comparable.compareTo(currentValue) == cmpResult) {
+                state.cmpValue = (Comparable<Object>) comparable;
+                state.resultValue = args[0].value();
+            }
+        } else if (cmpVal != null) {
+            throw new UnsupportedOperationException(
+                "Cannot use `" + signature.getName().displayName() + "` on values of type " + boundSignature.argTypes().get(1).getName());
+        }
+        return state;
+    }
+
+    @Override
+    public CompareBy reduce(RamAccounting ramAccounting,
+                            CompareBy state1,
+                            CompareBy state2) {
+        if (state1.cmpValue == null) {
+            return state2;
+        }
+        if (state2.cmpValue == null) {
+            return state1;
+        }
+        if (state2.cmpValue.compareTo(state1.cmpValue) == cmpResult) {
+            return state2;
+        } else {
+            return state1;
+        }
+    }
+
+    @Override
+    public Object terminatePartial(RamAccounting ramAccounting, CompareBy state) {
+        return state.resultValue;
+    }
+
+    @Override
+    public DataType<?> partialType() {
+        return partialType;
+    }
+}
---
+++ server/src/test/java/io/crate/execution/engine/aggregation/impl/CmpByAggregationTest.java
@@ -0,0 +1,182 @@
+/*
+ * Licensed to Crate.io GmbH ("Crate") under one or more contributor
+ * license agreements.  See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.  Crate licenses
+ * this file to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.  You may
+ * obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * However, if you have executed another commercial license agreement
+ * with Crate these terms will supersede the license and you may use the
+ * software solely pursuant to the terms of the relevant commercial agreement.
+ */
+
+package io.crate.execution.engine.aggregation.impl;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Test;
+
+import io.crate.metadata.functions.Signature;
+import io.crate.operation.aggregation.AggregationTestCase;
+import io.crate.types.DataTypes;
+
+public class CmpByAggregationTest extends AggregationTestCase {
+
+    @Test
+    public void test_max_by() throws Exception {
+        Signature signature = Signature.aggregate(
+            "max_by",
+            DataTypes.STRING.getTypeSignature(),
+            DataTypes.INTEGER.getTypeSignature(),
+            DataTypes.STRING.getTypeSignature()
+        );
+        Object result = executeAggregation(
+            signature,
+            new Object[][] {
+                new Object[] { "foo", 10 },
+                new Object[] { "bar", 15 },
+                new Object[] { "baz", 12 },
+            },
+            List.of()
+        );
+        assertThat(result).isEqualTo("bar");
+    }
+
+    @Test
+    public void test_cmp_by_returns_null_if_all_search_fields_are_null() throws Exception {
+        for (String func : List.of("max_by", "min_by")) {
+            Signature signature = Signature.aggregate(
+                func,
+                DataTypes.STRING.getTypeSignature(),
+                DataTypes.INTEGER.getTypeSignature(),
+                DataTypes.STRING.getTypeSignature()
+            );
+            Object result = executeAggregation(
+                signature,
+                new Object[][] {
+                    new Object[] { "foo", null },
+                    new Object[] { "bar", null },
+                    new Object[] { "baz", null },
+                },
+                List.of()
+            );
+            assertThat(result).isNull();
+        }
+    }
+
+    @Test
+    public void test_cmp_by_returns_null_if_all_return_fields_are_null() throws Exception {
+        for (String func : List.of("max_by", "min_by")) {
+            Signature signature = Signature.aggregate(
+                func,
+                DataTypes.STRING.getTypeSignature(),
+                DataTypes.INTEGER.getTypeSignature(),
+                DataTypes.STRING.getTypeSignature()
+            );
+            Object result = executeAggregation(
+                signature,
+                new Object[][] {
+                    new Object[] { null, 1 },
+                    new Object[] { null, 2 },
+                    new Object[] { null, 3 },
+                },
+                List.of()
+            );
+            assertThat(result).isNull();
+        }
+    }
+
+    @Test
+    public void test_max_by_returns_null_if_return_field_of_max_search_field_is_null() throws Exception {
+        Signature signature = Signature.aggregate(
+            "max_by",
+            DataTypes.STRING.getTypeSignature(),
+            DataTypes.INTEGER.getTypeSignature(),
+            DataTypes.STRING.getTypeSignature()
+        );
+        Object result = executeAggregation(
+            signature,
+            new Object[][] {
+                new Object[] { "a", 1 },
+                new Object[] { "b", 2 },
+                new Object[] { null, 3 },
+            },
+            List.of()
+        );
+        assertThat(result).isNull();
+    }
+
+    @Test
+    public void test_cmp_by_result_is_nondeterministic_on_ties() throws Exception {
+        for (String func : List.of("max_by", "min_by")) {
+            Signature signature = Signature.aggregate(
+                func,
+                DataTypes.STRING.getTypeSignature(),
+                DataTypes.INTEGER.getTypeSignature(),
+                DataTypes.STRING.getTypeSignature()
+            );
+            Object result = executeAggregation(
+                signature,
+                new Object[][] {
+                    new Object[] { "a", 2 },
+                    new Object[] { "b", 2 },
+                    new Object[] { "c", 2 },
+                },
+                List.of()
+            );
+            assertThat(result).isIn("a", "b", "c");
+        }
+    }
+
+    @Test
+    public void test_min_by() throws Exception {
+        Signature signature = Signature.aggregate(
+            "min_by",
+            DataTypes.STRING.getTypeSignature(),
+            DataTypes.INTEGER.getTypeSignature(),
+            DataTypes.STRING.getTypeSignature()
+        );
+        Object result = executeAggregation(
+            signature,
+            new Object[][] {
+                new Object[] { "foo", 10 },
+                new Object[] { "bar", 15 },
+                new Object[] { null, 15 },
+                new Object[] { "baz", null },
+            },
+            List.of()
+        );
+        assertThat(result).isEqualTo("foo");
+    }
+
+    @Test
+    public void test_cannot_use_max_by_on_non_comparable_types() throws Exception {
+        Signature signature = Signature.aggregate(
+            "max_by",
+            DataTypes.STRING.getTypeSignature(),
+            DataTypes.UNTYPED_OBJECT.getTypeSignature(),
+            DataTypes.STRING.getTypeSignature()
+        );
+        assertThatThrownBy(() -> executeAggregation(
+            signature,
+            new Object[][] {
+                new Object[] { "foo", Map.of("x", 10) },
+            },
+            List.of()
+        )).isExactlyInstanceOf(UnsupportedOperationException.class)
+            .hasMessage("Cannot use `max_by` on values of type object");
+    }
+}
