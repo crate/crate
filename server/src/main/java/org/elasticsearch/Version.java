e1964f431b4aab02c02ff3d9341bb4888f0bea10
Marios Trivyzas <matriv@gmail.com>
2022-12-06 21:12:28 +0200

    Upgrade `Lucene` to latest version `9.4.2`.

--- server/src/main/java/org/elasticsearch/Version.java
+++ server/src/main/java/org/elasticsearch/Version.java
@@ -145,7 +145,7 @@ public class Version implements Comparable<Version>, ToXContentFragment {
     public static final Version V_5_1_0 = new Version(8_01_00_99, false, org.apache.lucene.util.Version.LUCENE_9_3_0);
     public static final Version V_5_1_1 = new Version(8_01_01_99, false, org.apache.lucene.util.Version.LUCENE_9_3_0);
 
-    public static final Version V_5_2_0 = new Version(8_02_00_99, true, org.apache.lucene.util.Version.LUCENE_9_4_0);
+    public static final Version V_5_2_0 = new Version(8_02_00_99, true, org.apache.lucene.util.Version.LUCENE_9_4_2);
 
     public static final Version CURRENT = V_5_2_0;
 
