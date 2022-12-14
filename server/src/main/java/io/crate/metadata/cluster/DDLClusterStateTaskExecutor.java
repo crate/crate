52c95e6afff397b95d7dfe529a233f5dca6b0231
Mathias Fussenegger <f.mathias@zignar.net>
2022-12-14 14:54:52 +0100

    Propagate assertion errors in DDLClusterStateTaskExecutor
    The DDLClusterStateTaskExecutor swallowed assertion errors, preventing
    listeners from triggering and causing queries to get stuck.
    
    This isn't an issue in production were we do not expect assertions to
    trigger, but during development it's helpful to track down the
    assertion error if the listeners do get triggered.

--- server/src/main/java/io/crate/metadata/cluster/DDLClusterStateTaskExecutor.java
+++ server/src/main/java/io/crate/metadata/cluster/DDLClusterStateTaskExecutor.java
@@ -21,26 +21,26 @@
 
 package io.crate.metadata.cluster;
 
+import java.util.List;
+
 import org.elasticsearch.cluster.ClusterState;
 import org.elasticsearch.cluster.ClusterStateTaskExecutor;
 
-import java.util.List;
+import io.crate.exceptions.Exceptions;
 
 public abstract class DDLClusterStateTaskExecutor<Request> implements ClusterStateTaskExecutor<Request> {
 
     @Override
     public ClusterTasksResult<Request> execute(ClusterState currentState, List<Request> requests) throws Exception {
         ClusterTasksResult.Builder<Request> builder = ClusterTasksResult.builder();
-
         for (Request request : requests) {
             try {
                 currentState = execute(currentState, request);
                 builder.success(request);
-            } catch (Exception e) {
-                builder.failure(request, e);
+            } catch (Throwable e) {
+                builder.failure(request, Exceptions.toException(e));
             }
         }
-
         return builder.build(currentState);
     }
 
