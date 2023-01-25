2d7f7a9f7ae78164dc251450f42ea0cd03dbc43a
Mathias Fussenegger <f.mathias@zignar.net>
2023-01-25 08:50:32 +0100

    bp: Fix up roles after rolling upgrade
    https://github.com/elastic/elasticsearch/commit/33f703ef1f826900a285dee419cd5fc55f20faae

--- server/src/test/java/org/elasticsearch/cluster/coordination/JoinTaskExecutorTests.java
+++ server/src/test/java/org/elasticsearch/cluster/coordination/JoinTaskExecutorTests.java
@@ -18,13 +18,29 @@
  */
 package org.elasticsearch.cluster.coordination;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.HashSet;
+import java.util.List;
+
 import org.elasticsearch.Version;
+import org.elasticsearch.cluster.ClusterName;
+import org.elasticsearch.cluster.ClusterState;
+import org.elasticsearch.cluster.ClusterStateTaskExecutor;
 import org.elasticsearch.cluster.metadata.IndexMetadata;
 import org.elasticsearch.cluster.metadata.Metadata;
+import org.elasticsearch.cluster.node.DiscoveryNode;
+import org.elasticsearch.cluster.node.DiscoveryNodes;
+import org.elasticsearch.cluster.routing.RerouteService;
+import org.elasticsearch.cluster.routing.allocation.AllocationService;
+import org.elasticsearch.common.UUIDs;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.test.ESTestCase;
 import org.elasticsearch.test.VersionUtils;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 public class JoinTaskExecutorTests extends ESTestCase {
 
@@ -77,5 +93,39 @@ public class JoinTaskExecutorTests extends ESTestCase {
 
         JoinTaskExecutor.ensureIndexCompatibility(Version.V_4_3_2,
             metadata);
+    }
+
+    @Test
+    public void testUpdatesNodeWithNewRoles() throws Exception {
+        // Node roles vary by version, and new roles are suppressed for BWC. This means we can receive a join from a node that's already
+        // in the cluster but with a different set of roles: the node didn't change roles, but the cluster state came via an older master.
+        // In this case we must properly process its join to ensure that the roles are correct.
+
+        final AllocationService allocationService = mock(AllocationService.class);
+        when(allocationService.adaptAutoExpandReplicas(Mockito.any())).then(invocationOnMock -> invocationOnMock.getArguments()[0]);
+        final RerouteService rerouteService = (reason, priority, listener) -> listener.onResponse(null);
+
+        final JoinTaskExecutor joinTaskExecutor = new JoinTaskExecutor(allocationService, logger, rerouteService);
+
+        final DiscoveryNode masterNode = new DiscoveryNode(UUIDs.base64UUID(), buildNewFakeTransportAddress(), Version.CURRENT);
+
+        final DiscoveryNode actualNode = new DiscoveryNode(UUIDs.base64UUID(), buildNewFakeTransportAddress(), Version.CURRENT);
+        final DiscoveryNode bwcNode = new DiscoveryNode(actualNode.getName(), actualNode.getId(), actualNode.getEphemeralId(),
+                actualNode.getHostName(), actualNode.getHostAddress(), actualNode.getAddress(), actualNode.getAttributes(),
+                new HashSet<>(randomSubsetOf(actualNode.getRoles())), actualNode.getVersion());
+        final ClusterState clusterState = ClusterState.builder(ClusterName.DEFAULT).nodes(DiscoveryNodes.builder()
+                .add(masterNode)
+                .localNodeId(masterNode.getId())
+                .masterNodeId(masterNode.getId())
+                .add(bwcNode)
+        ).build();
+
+        final ClusterStateTaskExecutor.ClusterTasksResult<JoinTaskExecutor.Task> result
+                = joinTaskExecutor.execute(clusterState, List.of(new JoinTaskExecutor.Task(actualNode, "test")));
+        assertThat(result.executionResults.entrySet()).hasSize(1);
+        final ClusterStateTaskExecutor.TaskResult taskResult = result.executionResults.values().iterator().next();
+        assertThat(taskResult.isSuccess()).isTrue();
+
+        assertThat(result.resultingState.getNodes().get(actualNode.getId()).getRoles()).isEqualTo(actualNode.getRoles());
     }
 }
