b860509d8b8ce2593362afe0332959cb4cfeed5b
Michael Kleen <michael.kleen@crate.io>
2023-02-15 16:32:49 +0100

    Let isFiltered return isFiltered field in NestedLoopJoin

--- server/src/main/java/io/crate/planner/operators/NestedLoopJoin.java
+++ server/src/main/java/io/crate/planner/operators/NestedLoopJoin.java
@@ -137,7 +137,7 @@ public class NestedLoopJoin implements LogicalPlan {
     }
 
     public boolean isFiltered() {
-        return true;
+        return isFiltered;
     }
 
     public AnalyzedRelation topMostLeftRelation() {
