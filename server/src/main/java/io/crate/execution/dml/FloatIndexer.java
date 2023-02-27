ae091deae2f3182b794818f6b46449eae1cdb43b
Mathias Fussenegger <f.mathias@zignar.net>
2023-02-27 09:22:41 +0100

    Add FloatIndexer
    Can't re-use the DoubleIndexer for float as float columns must have a
    `FloatPoint`

---
+++ server/src/main/java/io/crate/execution/dml/FloatIndexer.java
@@ -0,0 +1,72 @@
+/*
+ * Licensed to Crate.io GmbH ("Crate") under one or more contributor
+ * license agreements.  See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.  Crate licenses
+ * this file to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.  You may
+ * obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * However, if you have executed another commercial license agreement
+ * with Crate these terms will supersede the license and you may use the
+ * software solely pursuant to the terms of the relevant commercial agreement.
+ */
+
+package io.crate.execution.dml;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.function.Consumer;
+
+import org.apache.lucene.document.FieldType;
+import org.apache.lucene.document.FloatPoint;
+import org.apache.lucene.document.SortedNumericDocValuesField;
+import org.apache.lucene.document.StoredField;
+import org.apache.lucene.index.IndexableField;
+import org.apache.lucene.util.NumericUtils;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+
+import io.crate.execution.dml.Indexer.ColumnConstraint;
+import io.crate.execution.dml.Indexer.Synthetic;
+import io.crate.metadata.ColumnIdent;
+import io.crate.metadata.Reference;
+
+public class FloatIndexer implements ValueIndexer<Float> {
+
+    private final Reference ref;
+    private final FieldType fieldType;
+    private String name;
+
+    public FloatIndexer(Reference ref, FieldType fieldType) {
+        this.ref = ref;
+        this.name = ref.column().fqn();
+        this.fieldType = fieldType;
+    }
+
+    @Override
+    public void indexValue(Float value,
+                           XContentBuilder xcontentBuilder,
+                           Consumer<? super IndexableField> addField,
+                           Consumer<? super Reference> onDynamicColumn,
+                           Map<ColumnIdent, Synthetic> synthetics,
+                           Map<ColumnIdent, ColumnConstraint> toValidate) throws IOException {
+        xcontentBuilder.value(value);
+        float floatValue = value.floatValue();
+        addField.accept(new FloatPoint(name, floatValue));
+        if (ref.hasDocValues()) {
+            addField.accept(new SortedNumericDocValuesField(
+                name,
+                NumericUtils.floatToSortableInt(floatValue)));
+        }
+        if (fieldType.stored()) {
+            addField.accept(new StoredField(name, floatValue));
+        }
+    }
+}
