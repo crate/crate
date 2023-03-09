07667d2cdcd621c4a70dbc4514cbed6e31651464
Sebastian Utz <su@rtme.net>
2023-03-09 16:56:27 +0100

    Fix SAME_NODE optimization for join operations
    The SAME_NODE optimization is based on comparing
    `ResultDescription.nodeIds()` for equality while this method
    returns Collection instances.
    
    When comparing unknown Collection instances, one instance's
    equal() will always return false due to the different classes used.
    E.g. List.equal(Set) will return false.
    
    This adds and uses valid comparison method for generic collections.

--- server/src/main/java/io/crate/planner/operators/NestedLoopJoin.java
+++ server/src/main/java/io/crate/planner/operators/NestedLoopJoin.java
@@ -372,7 +372,7 @@ public class NestedLoopJoin implements JoinPlan {
         MergePhase rightMerge = null;
 
         if (leftResultDesc.nodeIds().size() == 1
-            && leftResultDesc.nodeIds().equals(rightResultDesc.nodeIds())
+            && Lists2.equals(leftResultDesc.nodeIds(), rightResultDesc.nodeIds())
             && !rightResultDesc.hasRemainingLimitOrOffset()) {
             // if the left and the right plan are executed on the same single node the mergePhase
             // should be omitted. This is the case if the left and right table have only one shards which
@@ -426,7 +426,7 @@ public class NestedLoopJoin implements JoinPlan {
     private static boolean isBlockNlPossible(ExecutionPlan left, ExecutionPlan right) {
         return left.resultDescription().orderBy() == null &&
                left.resultDescription().nodeIds().size() <= 1 &&
-               left.resultDescription().nodeIds().equals(right.resultDescription().nodeIds());
+               Lists2.equals(left.resultDescription().nodeIds(), right.resultDescription().nodeIds());
     }
 
     public boolean orderByWasPushedDown() {
