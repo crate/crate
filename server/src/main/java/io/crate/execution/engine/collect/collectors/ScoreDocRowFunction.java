crate/crate

commit 9eef7dc728c31efdfa0a8beedcbafcce60038e31

Fix reader.document deprecation warnings

server/src/main/java/io/crate/execution/engine/collect/collectors/ScoreDocRowFunction.java
@@ -21,25 +21,27 @@
 
 package io.crate.execution.engine.collect.collectors;
 
-import io.crate.data.Input;
-import io.crate.data.Row;
-import io.crate.execution.engine.fetch.ReaderContext;
-import io.crate.expression.InputRow;
-import io.crate.expression.reference.doc.lucene.LuceneCollectorExpression;
-import io.crate.expression.reference.doc.lucene.OrderByCollectorExpression;
-import org.apache.lucene.index.IndexReader;
-import org.apache.lucene.index.LeafReaderContext;
-import org.apache.lucene.index.ReaderUtil;
-import org.apache.lucene.search.FieldDoc;
-import org.apache.lucene.search.ScoreDoc;
-
-import javax.annotation.Nullable;
 import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.function.Function;
+
+import javax.annotation.Nullable;
+
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.LeafReaderContext;
+import org.apache.lucene.index.ReaderUtil;
+import org.apache.lucene.search.FieldDoc;
+import org.apache.lucene.search.ScoreDoc;
+
+import io.crate.data.Input;
+import io.crate.data.Row;
+import io.crate.execution.engine.fetch.ReaderContext;
+import io.crate.expression.InputRow;
+import io.crate.expression.reference.doc.lucene.LuceneCollectorExpression;
+import io.crate.expression.reference.doc.lucene.OrderByCollectorExpression;
 
 class ScoreDocRowFunction implements Function<ScoreDoc, Row> {
 
@@ -83,14 +85,14 @@ class ScoreDocRowFunction implements Function<ScoreDoc, Row> {
         int readerIndex = ReaderUtil.subIndex(fieldDoc.doc, leaves);
         LeafReaderContext subReaderContext = leaves.get(readerIndex);
         int subDoc = fieldDoc.doc - subReaderContext.docBase;
-        var readerContext = new ReaderContext(subReaderContext);
-        for (LuceneCollectorExpression<?> expression : expressions) {
-            try {
+        try {
+            var readerContext = new ReaderContext(subReaderContext);
+            for (LuceneCollectorExpression<?> expression : expressions) {
                 expression.setNextReader(readerContext);
                 expression.setNextDocId(subDoc);
-            } catch (IOException e) {
-                throw new UncheckedIOException(e);
             }
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
         }
         return inputRow;
     }

server/src/main/java/io/crate/execution/engine/fetch/ReaderContext.java
@@ -26,6 +26,7 @@ import java.io.IOException;
 import org.apache.lucene.index.LeafReader;
 import org.apache.lucene.index.LeafReaderContext;
 import org.apache.lucene.index.StoredFieldVisitor;
+import org.apache.lucene.index.StoredFields;
 
 public class ReaderContext {
 
@@ -43,10 +44,11 @@ public class ReaderContext {
         this.storedFieldsReader = storedFieldsReader;
     }
 
-    public ReaderContext(LeafReaderContext leafReaderContext) {
+    public ReaderContext(LeafReaderContext leafReaderContext) throws IOException {
         LeafReader reader = leafReaderContext.reader();
+        StoredFields storedFields = reader.storedFields();
         this.leafReaderContext = leafReaderContext;
-        this.storedFieldsReader = reader::document;
+        this.storedFieldsReader = storedFields::document;
     }
 
     public void visitDocument(int docId, StoredFieldVisitor visitor) throws IOException {

server/src/main/java/io/crate/statistics/ReservoirSampler.java
@@ -319,14 +319,14 @@ public final class ReservoirSampler {
             int readerIndex = ReaderUtil.subIndex(docId, leaves);
             LeafReaderContext leafContext = leaves.get(readerIndex);
             int subDoc = docId - leafContext.docBase;
-            var readerContext = new ReaderContext(leafContext);
-            for (LuceneCollectorExpression<?> expression : expressions) {
-                try {
+            try {
+                var readerContext = new ReaderContext(leafContext);
+                for (LuceneCollectorExpression<?> expression : expressions) {
                     expression.setNextReader(readerContext);
                     expression.setNextDocId(subDoc);
-                } catch (IOException e) {
-                    throw new UncheckedIOException(e);
                 }
+            } catch (IOException e) {
+                throw new UncheckedIOException(e);
             }
             Object[] cells = new Object[inputs.size()];
             for (int i = 0; i < cells.length; i++) {

server/src/main/java/org/elasticsearch/gateway/PersistedClusterStateService.java
@@ -51,6 +51,7 @@ import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.index.LeafReaderContext;
 import org.apache.lucene.index.SerialMergeScheduler;
+import org.apache.lucene.index.StoredFields;
 import org.apache.lucene.index.Term;
 import org.apache.lucene.search.DocIdSetIterator;
 import org.apache.lucene.search.IndexSearcher;
@@ -441,11 +442,13 @@ public class PersistedClusterStateService {
                 final Bits liveDocs = leafReaderContext.reader().getLiveDocs();
                 final IntPredicate isLiveDoc = liveDocs == null ? i -> true : liveDocs::get;
                 final DocIdSetIterator docIdSetIterator = scorer.iterator();
+
+                StoredFields storedFields = leafReaderContext.reader().storedFields();
                 while (docIdSetIterator.nextDoc() != DocIdSetIterator.NO_MORE_DOCS) {
                     if (isLiveDoc.test(docIdSetIterator.docID())) {
                         LOGGER.trace("processing doc {}", docIdSetIterator.docID());
                         bytesRefConsumer.accept(
-                            leafReaderContext.reader().document(docIdSetIterator.docID()).getBinaryValue(DATA_FIELD_NAME));
+                            storedFields.document(docIdSetIterator.docID()).getBinaryValue(DATA_FIELD_NAME));
                     }
                 }
             }

server/src/main/java/org/elasticsearch/index/engine/InternalEngine.java
@@ -55,6 +55,7 @@ import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.index.IndexableField;
+import org.apache.lucene.index.LeafReader;
 import org.apache.lucene.index.LeafReaderContext;
 import org.apache.lucene.index.LiveIndexWriterConfig;
 import org.apache.lucene.index.MergePolicy;
@@ -62,6 +63,7 @@ import org.apache.lucene.index.SegmentCommitInfo;
 import org.apache.lucene.index.SegmentInfos;
 import org.apache.lucene.index.ShuffleForcedMergePolicy;
 import org.apache.lucene.index.SoftDeletesRetentionMergePolicy;
+import org.apache.lucene.index.StoredFields;
 import org.apache.lucene.index.Term;
 import org.apache.lucene.search.BooleanClause;
 import org.apache.lucene.search.BooleanQuery;
@@ -2863,7 +2865,9 @@ public class InternalEngine extends Engine {
             if (scorer == null) {
                 continue;
             }
-            final CombinedDocValues dv = new CombinedDocValues(leaf.reader());
+            final LeafReader reader = leaf.reader();
+            final StoredFields storedFields = reader.storedFields();
+            final CombinedDocValues dv = new CombinedDocValues(reader);
             final IDVisitor idFieldVisitor = new IDVisitor(IdFieldMapper.NAME);
             final DocIdSetIterator iterator = scorer.iterator();
             int docId;
@@ -2873,7 +2877,7 @@ public class InternalEngine extends Engine {
                 localCheckpointTracker.markSeqNoAsProcessed(seqNo);
                 localCheckpointTracker.markSeqNoAsPersisted(seqNo);
                 idFieldVisitor.reset();
-                leaf.reader().document(docId, idFieldVisitor);
+                storedFields.document(docId, idFieldVisitor);
                 if (idFieldVisitor.getId() == null) {
                     assert dv.isTombstone(docId);
                     continue;

server/src/main/java/org/elasticsearch/index/engine/LuceneChangesSnapshot.java
@@ -29,6 +29,7 @@ import org.apache.lucene.document.LongPoint;
 import org.apache.lucene.index.LeafReader;
 import org.apache.lucene.index.LeafReaderContext;
 import org.apache.lucene.index.NumericDocValues;
+import org.apache.lucene.index.StoredFields;
 import org.apache.lucene.index.Term;
 import org.apache.lucene.search.BooleanClause;
 import org.apache.lucene.search.BooleanQuery;
@@ -238,7 +239,8 @@ final class LuceneChangesSnapshot implements Translog.Snapshot {
         final String sourceField = parallelArray.hasRecoverySource[docIndex] ? SourceFieldMapper.RECOVERY_SOURCE_NAME :
             SourceFieldMapper.NAME;
         final FieldsVisitor fields = new FieldsVisitor(true, sourceField);
-        leaf.reader().document(segmentDocID, fields);
+        StoredFields storedFields = leaf.reader().storedFields();
+        storedFields.document(segmentDocID, fields);
 
         final Translog.Operation op;
         final boolean isTombstone = parallelArray.isTombStone[docIndex];

commit 77d4f3601ef006c49a52b8e72ccdd741b8e7dc54

Don't create ReaderContext per row in ScoreDocRowFunction
Fixes a performance regression caused by https://github.com/crate/crate/pull/13881

The deprecated `reader.document()` version internally uses a
thread-local `StoredFieldsReader`. The new code path that goes through
`reader.storedFields()` cloned a new instance each time.

server/src/main/java/io/crate/execution/engine/collect/collectors/ScoreDocRowFunction.java
@@ -51,6 +51,7 @@ class ScoreDocRowFunction implements Function<ScoreDoc, Row> {
     private final DummyScorer scorer;
     private final InputRow inputRow;
     private final Runnable onScoreDoc;
+    private final ReaderContext[] readerContexts;
 
     ScoreDocRowFunction(IndexReader indexReader,
                         List<? extends Input<?>> inputs,
@@ -63,9 +64,18 @@ class ScoreDocRowFunction implements Function<ScoreDoc, Row> {
         this.inputRow = new InputRow(inputs);
         this.onScoreDoc = onScoreDoc;
         for (LuceneCollectorExpression<?> expression : this.expressions) {
-            if (expression instanceof OrderByCollectorExpression) {
-                orderByCollectorExpressions.add((OrderByCollectorExpression) expression);
+            if (expression instanceof OrderByCollectorExpression orderByExpr) {
+                orderByCollectorExpressions.add(orderByExpr);
             }
+        }
+        List<LeafReaderContext> leaves = indexReader.leaves();
+        readerContexts = new ReaderContext[leaves.size()];
+        try {
+            for (int i = 0; i < readerContexts.length; i++) {
+                readerContexts[i] = new ReaderContext(leaves.get(i));
+            }
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
         }
     }
 
@@ -84,9 +94,9 @@ class ScoreDocRowFunction implements Function<ScoreDoc, Row> {
         List<LeafReaderContext> leaves = indexReader.leaves();
         int readerIndex = ReaderUtil.subIndex(fieldDoc.doc, leaves);
         LeafReaderContext subReaderContext = leaves.get(readerIndex);
+        var readerContext = readerContexts[readerIndex];
         int subDoc = fieldDoc.doc - subReaderContext.docBase;
         try {
-            var readerContext = new ReaderContext(subReaderContext);
             for (LuceneCollectorExpression<?> expression : expressions) {
                 expression.setNextReader(readerContext);
                 expression.setNextDocId(subDoc);