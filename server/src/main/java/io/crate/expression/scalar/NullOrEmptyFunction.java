0af2fcadf5ef1df77b4193b19838c6765feaaf5a
Mathias Fussenegger <f.mathias@zignar.net>
2023-04-12 11:34:11 +0200

    Add a `null_or_empty` scalar
    This can be used instead of `IS NULL` if it's okay to match on empty
    objects.
    Although kind of a feature this is added as a fix because it allows to
    mitigate a performance regression introduced with https://github.com/crate/crate/pull/13106
    We can't revert the behavior for `IS NULL` without breaking SQL
    semantics.
    
    An earlier fix (https://github.com/crate/crate/pull/13771) only helped
    if the table contained no null values.
    
    Closes https://github.com/crate/crate/issues/13769
    
        Statement:
          SELECT count(*) FROM many_null WHERE null_or_empty(o)
        Concurrency: 1
        Iterations: 10
        Runtime (in ms):
            mean:    1.152 ± 0.444
            min/max: 0.843 → 3.186
        Percentile:
            50:   0.924 ± 0.717 (stdev)
            95:   3.186
            99.9: 3.186
    
        Statement:
          SELECT count(*) FROM many_null WHERE NOT null_or_empty(o)
        Concurrency: 1
        Iterations: 10
        Runtime (in ms):
            mean:    31.275 ± 40.573
            min/max: 1.179 → 199.507
        Percentile:
            50:   1.753 ± 65.460 (stdev)
            95:   199.507
            99.9: 199.507
    
    (cherry picked from commit f0f3c494a554cb8d057c54ce4f87809918a0314b)
    
    # Conflicts:
    #	server/src/main/java/io/crate/expression/scalar/ScalarFunctionModule.java
    #	server/src/testFixtures/java/io/crate/testing/QueryTester.java

---
+++ server/src/main/java/io/crate/expression/scalar/NullOrEmptyFunction.java
@@ -0,0 +1,135 @@
+/*
+ * Licensed to Crate.io GmbH ("Crate") under one or more contributor
+ * license agreements.  See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.  Crate licenses
+ * this file to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.  You may
+ * obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * However, if you have executed another commercial license agreement
+ * with Crate these terms will supersede the license and you may use the
+ * software solely pursuant to the terms of the relevant commercial agreement.
+ */
+
+package io.crate.expression.scalar;
+
+import java.util.Collection;
+import java.util.Map;
+
+import org.apache.lucene.search.BooleanClause.Occur;
+import org.apache.lucene.search.BooleanQuery;
+import org.apache.lucene.search.FieldExistsQuery;
+import org.apache.lucene.search.Query;
+import org.elasticsearch.common.lucene.search.Queries;
+
+import io.crate.data.Input;
+import io.crate.expression.predicate.IsNullPredicate;
+import io.crate.expression.symbol.Function;
+import io.crate.expression.symbol.Symbol;
+import io.crate.lucene.LuceneQueryBuilder.Context;
+import io.crate.metadata.NodeContext;
+import io.crate.metadata.Reference;
+import io.crate.metadata.Scalar;
+import io.crate.metadata.TransactionContext;
+import io.crate.metadata.functions.BoundSignature;
+import io.crate.metadata.functions.Signature;
+import io.crate.metadata.functions.TypeVariableConstraint;
+import io.crate.types.ArrayType;
+import io.crate.types.DataType;
+import io.crate.types.DataTypes;
+import io.crate.types.ObjectType;
+import io.crate.types.TypeSignature;
+
+public final class NullOrEmptyFunction extends Scalar<Boolean, Object> {
+
+    public static void register(ScalarFunctionModule module) {
+        module.register(
+            Signature.scalar(
+                "null_or_empty",
+                DataTypes.UNTYPED_OBJECT.getTypeSignature(),
+                DataTypes.BOOLEAN.getTypeSignature()
+            ),
+            NullOrEmptyFunction::new
+        );
+        module.register(
+            Signature.scalar(
+                "null_or_empty",
+                TypeSignature.parseTypeSignature("array(E)"),
+                DataTypes.BOOLEAN.getTypeSignature()
+            ).withTypeVariableConstraints(TypeVariableConstraint.typeVariableOfAnyType("E")),
+            NullOrEmptyFunction::new
+        );
+    }
+
+    private final Signature signature;
+    private final BoundSignature boundSignature;
+
+    private NullOrEmptyFunction(Signature signature, BoundSignature boundSignature) {
+        this.signature = signature;
+        this.boundSignature = boundSignature;
+    }
+
+    @Override
+    public Signature signature() {
+        return signature;
+    }
+
+    @Override
+    public BoundSignature boundSignature() {
+        return boundSignature;
+    }
+
+    @Override
+    @SafeVarargs
+    public final Boolean evaluate(TransactionContext txnCtx, NodeContext nodeContext, Input<Object>... args) {
+        Object value = args[0].value();
+        if (value instanceof Map<?, ?> map) {
+            return map.isEmpty();
+        }
+        if (value instanceof Collection<?> collection) {
+            return collection.isEmpty();
+        }
+        return value == null;
+    }
+
+    @Override
+    public Query toQuery(Function function, Context context) {
+        assert function.arguments().size() == 1 : "Function has a single argument";
+        Symbol arg = function.arguments().get(0);
+        if (!(arg instanceof Reference ref)) {
+            return null;
+        }
+        DataType<?> valueType = ref.valueType();
+        if (valueType instanceof ObjectType objectType) {
+            if (objectType.innerTypes().isEmpty()) {
+                return null;
+            }
+            BooleanQuery.Builder booleanQuery = new BooleanQuery.Builder()
+                .setMinimumNumberShouldMatch(1);
+            for (var entry : objectType.innerTypes().entrySet()) {
+                String childColumn = entry.getKey();
+                Reference childRef = context.getRef(ref.column().append(childColumn));
+                if (childRef == null) {
+                    return null;
+                }
+                Query refExistsQuery = IsNullPredicate.refExistsQuery(childRef, context, true);
+                if (refExistsQuery == null) {
+                    return null;
+                }
+                booleanQuery.add(refExistsQuery, Occur.SHOULD);
+            }
+            return Queries.not(booleanQuery.build());
+        } else if (valueType instanceof ArrayType<?> && ref.hasDocValues()) {
+            return Queries.not(new FieldExistsQuery(ref.column().fqn()));
+        }
+        return null;
+    }
+}
