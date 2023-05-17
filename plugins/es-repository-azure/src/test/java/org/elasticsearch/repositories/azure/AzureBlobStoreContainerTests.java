crate/crate

commit 0e43064908c8698ca63a7efbe981558bd3cf52e0

Remove unsued code that supports multiple clients in Azure repo.

es/es-repository-azure/src/main/java/org/elasticsearch/repositories/azure/AzureBlobStore.java
@@ -33,24 +33,20 @@ import java.io.InputStream;
 import java.net.URISyntaxException;
 import java.util.Map;
 
-import static java.util.Collections.emptyMap;
-
 public class AzureBlobStore implements BlobStore {
 
     private final AzureStorageService service;
 
-    private final String clientName;
     private final String container;
     private final LocationMode locationMode;
 
     public AzureBlobStore(RepositoryMetaData metadata, AzureStorageService service) {
         this.container = Repository.CONTAINER_SETTING.get(metadata.settings());
-        this.clientName = Repository.CLIENT_NAME.get(metadata.settings());
         this.service = service;
         // locationMode is set per repository, not per client
         this.locationMode = Repository.LOCATION_MODE_SETTING.get(metadata.settings());
-        final Map<String, AzureStorageSettings> prevSettings = this.service.refreshAndClearCache(emptyMap());
-        final Map<String, AzureStorageSettings> newSettings = AzureStorageSettings.overrideLocationMode(prevSettings, this.locationMode);
+        final AzureStorageSettings prevSettings = this.service.refreshAndClearCache(null);
+        final AzureStorageSettings newSettings = AzureStorageSettings.overrideLocationMode(prevSettings, this.locationMode);
         this.service.refreshAndClearCache(newSettings);
     }
 
@@ -66,10 +62,6 @@ public class AzureBlobStore implements BlobStore {
         return locationMode;
     }
 
-    public String getClientName() {
-        return clientName;
-    }
-
     @Override
     public BlobContainer blobContainer(BlobPath path) {
         return new AzureBlobContainer(path, this);
@@ -79,7 +71,7 @@ public class AzureBlobStore implements BlobStore {
     public void delete(BlobPath path) throws IOException {
         final String keyPath = path.buildAsString();
         try {
-            service.deleteFiles(clientName, container, keyPath);
+            service.deleteFiles(container, keyPath);
         } catch (URISyntaxException | StorageException e) {
             throw new IOException(e);
         }
@@ -90,28 +82,28 @@ public class AzureBlobStore implements BlobStore {
     }
 
     public boolean containerExist() throws URISyntaxException, StorageException {
-        return service.doesContainerExist(clientName, container);
+        return service.doesContainerExist(container);
     }
 
     public boolean blobExists(String blob) throws URISyntaxException, StorageException {
-        return service.blobExists(clientName, container, blob);
+        return service.blobExists(container, blob);
     }
 
     public void deleteBlob(String blob) throws URISyntaxException, StorageException {
-        service.deleteBlob(clientName, container, blob);
+        service.deleteBlob(container, blob);
     }
 
     public InputStream getInputStream(String blob) throws URISyntaxException, StorageException, IOException {
-        return service.getInputStream(clientName, container, blob);
+        return service.getInputStream(container, blob);
     }
 
     public Map<String, BlobMetaData> listBlobsByPrefix(String keyPath, String prefix)
         throws URISyntaxException, StorageException {
-        return service.listBlobsByPrefix(clientName, container, keyPath, prefix);
+        return service.listBlobsByPrefix(container, keyPath, prefix);
     }
 
     public void writeBlob(String blobName, InputStream inputStream, long blobSize, boolean failIfAlreadyExists)
         throws URISyntaxException, StorageException, IOException {
-        service.writeBlob(this.clientName, container, blobName, inputStream, blobSize, failIfAlreadyExists);
+        service.writeBlob(container, blobName, inputStream, blobSize, failIfAlreadyExists);
     }
 }

es/es-repository-azure/src/main/java/org/elasticsearch/repositories/azure/AzureRepository.java
@@ -19,6 +19,7 @@
 
 package org.elasticsearch.repositories.azure;
 
+import com.google.common.annotations.VisibleForTesting;
 import com.microsoft.azure.storage.LocationMode;
 import com.microsoft.azure.storage.StorageException;
 import org.apache.logging.log4j.LogManager;
@@ -52,9 +53,9 @@ import static org.elasticsearch.repositories.azure.AzureStorageService.MIN_CHUNK
  * <p>
  * Azure file system repository supports the following settings:
  * <dl>
- * <dt>{@code container}</dt><dd>Azure container name. Defaults to elasticsearch-snapshots</dd>
+ * <dt>{@code container}</dt><dd>Azure container name. Defaults to crate-snapshots</dd>
  * <dt>{@code base_path}</dt><dd>Specifies the path within bucket to repository data. Defaults to root directory.</dd>
- * <dt>{@code chunk_size}</dt><dd>Large file can be divided into chunks. This parameter specifies the chunk size. Defaults to 64mb.</dd>
+ * <dt>{@code chunk_size}</dt><dd>Large file can be divided into chunks. This parameter specifies the chunk size. Defaults to 256mb.</dd>
  * <dt>{@code compress}</dt><dd>If set to true metadata files will be stored compressed. Defaults to false.</dd>
  * </dl>
  */
@@ -64,8 +65,6 @@ public class AzureRepository extends BlobStoreRepository {
     public static final String TYPE = "azure";
 
     public static final class Repository {
-        static final Setting<String> CLIENT_NAME =
-            new Setting<>("client", "default", Function.identity(), Property.NodeScope);
         static final Setting<String> CONTAINER_SETTING =
             new Setting<>("container", "crate-snapshots", Function.identity(), Property.NodeScope);
         static final Setting<String> BASE_PATH_SETTING = Setting.simpleString("base_path", Property.NodeScope);
@@ -119,7 +118,7 @@ public class AzureRepository extends BlobStoreRepository {
         }
     }
 
-    // only use for testing
+    @VisibleForTesting
     @Override
     protected BlobStore getBlobStore() {
         return super.getBlobStore();
@@ -129,7 +128,7 @@ public class AzureRepository extends BlobStoreRepository {
      * {@inheritDoc}
      */
     @Override
-    protected AzureBlobStore createBlobStore() throws URISyntaxException, StorageException {
+    protected AzureBlobStore createBlobStore() {
         final AzureBlobStore blobStore = new AzureBlobStore(metadata, storageService);
 
         LOGGER.debug((org.apache.logging.log4j.util.Supplier<?>) () -> new ParameterizedMessage(

es/es-repository-azure/src/main/java/org/elasticsearch/repositories/azure/AzureRepositoryPlugin.java
@@ -19,9 +19,9 @@
 
 package org.elasticsearch.repositories.azure;
 
+import com.google.common.annotations.VisibleForTesting;
 import org.elasticsearch.common.settings.Setting;
 import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.common.settings.SettingsException;
 import org.elasticsearch.common.xcontent.NamedXContentRegistry;
 import org.elasticsearch.env.Environment;
 import org.elasticsearch.plugins.Plugin;
@@ -39,7 +39,7 @@ import java.util.Map;
  */
 public class AzureRepositoryPlugin extends Plugin implements RepositoryPlugin, ReloadablePlugin {
 
-    // protected for testing
+    @VisibleForTesting
     final AzureStorageService azureStoreService;
 
     public AzureRepositoryPlugin(Settings settings) {
@@ -70,11 +70,7 @@ public class AzureRepositoryPlugin extends Plugin implements RepositoryPlugin, R
 
     @Override
     public void reload(Settings settings) {
-        // secure settings should be readable
-        final Map<String, AzureStorageSettings> clientsSettings = AzureStorageSettings.load(settings);
-        if (clientsSettings.isEmpty()) {
-            throw new SettingsException("If you want to use an azure repository, you need to define a client configuration.");
-        }
+        final AzureStorageSettings clientsSettings = AzureStorageSettings.getClientSettings(settings);
         azureStoreService.refreshAndClearCache(clientsSettings);
     }
 }

commit 3ad6979ed13a94d9ae5967347ab64d64f7dca493

Fix issue with Azure snapshots
Previously, there was only one instance of `AzureStorageService` and
actions were relying into `refreshSettings` to use this same instance
but pass the relevant settings for the repository used for the action.
As a consequence, if 2 or more Azure repos were used, it was possible
so have wrong results in `sys.snaphots`, create snapshots in the wrong
repo, drop snapshots from the wrong repo, etc.

Issue seems to have been introduced with: #8707

To fix this, create one instance of `AzureStorageService` per
`AzureBlobStore`.

Fixes: #14135

plugins/es-repository-azure/src/main/java/org/elasticsearch/repositories/azure/AzureBlobStore.java
@@ -39,6 +39,8 @@ import org.elasticsearch.repositories.azure.AzureRepository.Repository;
 import com.microsoft.azure.storage.LocationMode;
 import com.microsoft.azure.storage.StorageException;
 
+import io.crate.common.annotations.VisibleForTesting;
+
 public class AzureBlobStore implements BlobStore {
 
     private final AzureStorageService service;
@@ -47,16 +49,21 @@ public class AzureBlobStore implements BlobStore {
     private final String container;
     private final LocationMode locationMode;
 
-    public AzureBlobStore(RepositoryMetadata metadata, AzureStorageService service) {
-        this.service = service;
+    public AzureBlobStore(RepositoryMetadata metadata) {
+        this(metadata, new AzureStorageService(AzureStorageSettings.getClientSettings(metadata.settings())));
+    }
+
+    @VisibleForTesting
+    AzureBlobStore(RepositoryMetadata metadata, AzureStorageService service) {
         this.container = Repository.CONTAINER_SETTING.get(metadata.settings());
         this.clientName = Repository.CLIENT_NAME.get(metadata.settings());
         this.locationMode = Repository.LOCATION_MODE_SETTING.get(metadata.settings());
 
         AzureStorageSettings repositorySettings = AzureStorageSettings
             .getClientSettings(metadata.settings());
+        service.refreshSettings(repositorySettings);
 
-        this.service.refreshSettings(repositorySettings);
+        this.service = service;
     }
 
     @Override

plugins/es-repository-azure/src/main/java/org/elasticsearch/repositories/azure/AzureRepository.java
@@ -27,10 +27,6 @@ import java.util.List;
 import java.util.Locale;
 import java.util.function.Function;
 
-import com.microsoft.azure.storage.LocationMode;
-import com.microsoft.azure.storage.RetryPolicy;
-
-import io.crate.common.annotations.VisibleForTesting;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.message.ParameterizedMessage;
@@ -47,6 +43,10 @@ import org.elasticsearch.common.xcontent.NamedXContentRegistry;
 import org.elasticsearch.indices.recovery.RecoverySettings;
 import org.elasticsearch.repositories.blobstore.BlobStoreRepository;
 
+import com.microsoft.azure.storage.LocationMode;
+import com.microsoft.azure.storage.RetryPolicy;
+
+import io.crate.common.annotations.VisibleForTesting;
 import io.crate.common.unit.TimeValue;
 import io.crate.types.DataTypes;
 
@@ -162,17 +162,14 @@ public class AzureRepository extends BlobStoreRepository {
     }
 
     private final ByteSizeValue chunkSize;
-    private final AzureStorageService storageService;
     private final boolean readonly;
 
     public AzureRepository(RepositoryMetadata metadata,
                            NamedXContentRegistry namedXContentRegistry,
-                           AzureStorageService storageService,
                            ClusterService clusterService,
                            RecoverySettings recoverySettings) {
         super(metadata, namedXContentRegistry, clusterService, recoverySettings, buildBasePath(metadata));
         this.chunkSize = Repository.CHUNK_SIZE_SETTING.get(metadata.settings());
-        this.storageService = storageService;
 
         // If the user explicitly did not define a readonly value, we set it by ourselves depending on the location mode setting.
         // For secondary_only setting, the repository should be read only
@@ -211,7 +208,7 @@ public class AzureRepository extends BlobStoreRepository {
      */
     @Override
     protected AzureBlobStore createBlobStore() {
-        final AzureBlobStore blobStore = new AzureBlobStore(metadata, storageService);
+        final AzureBlobStore blobStore = new AzureBlobStore(metadata);
 
         LOGGER.debug((org.apache.logging.log4j.util.Supplier<?>) () -> new ParameterizedMessage(
             "using container [{}], chunk_size [{}], compress [{}], base_path [{}]",

plugins/es-repository-azure/src/main/java/org/elasticsearch/repositories/azure/AzureRepositoryPlugin.java
@@ -19,6 +19,10 @@
 
 package org.elasticsearch.repositories.azure;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
 import org.elasticsearch.cluster.metadata.RepositoryMetadata;
 import org.elasticsearch.cluster.service.ClusterService;
 import org.elasticsearch.common.settings.Setting;
@@ -31,20 +35,10 @@ import org.elasticsearch.repositories.Repository;
 
 import io.crate.analyze.repositories.TypeSettings;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-
 /**
  * A plugin to add a repository type that writes to and from the Azure cloud storage service.
  */
 public class AzureRepositoryPlugin extends Plugin implements RepositoryPlugin {
-
-    private final AzureStorageService azureStoreService;
-
-    public AzureRepositoryPlugin() {
-        this.azureStoreService = new AzureStorageService();
-    }
 
     @Override
     public Map<String, Repository.Factory> getRepositories(Environment env,
@@ -66,7 +60,6 @@ public class AzureRepositoryPlugin extends Plugin implements RepositoryPlugin {
                     return new AzureRepository(
                         metadata,
                         namedXContentRegistry,
-                        azureStoreService,
                         clusterService,
                         recoverySettings
                     );

plugins/es-repository-azure/src/main/java/org/elasticsearch/repositories/azure/AzureStorageService.java
@@ -77,7 +77,11 @@ public class AzureStorageService {
     public static final ByteSizeValue MAX_CHUNK_SIZE = new ByteSizeValue(256, ByteSizeUnit.MB);
 
     @VisibleForTesting
-    volatile AzureStorageSettings storageSettings;
+    AzureStorageSettings storageSettings;
+
+    AzureStorageService(AzureStorageSettings storageSettings) {
+        this.storageSettings = storageSettings;
+    }
 
     /**
      * Creates a {@code CloudBlobClient} on each invocation using the current client
@@ -128,7 +132,8 @@ public class AzureStorageService {
         return context;
     }
 
-    public void refreshSettings(AzureStorageSettings clientSettings) {
+    @VisibleForTesting
+    void refreshSettings(AzureStorageSettings clientSettings) {
         this.storageSettings = AzureStorageSettings.copy(clientSettings);
     }
 

plugins/es-repository-azure/src/main/java/org/elasticsearch/repositories/azure/AzureStorageSettings.java
@@ -19,18 +19,21 @@
 
 package org.elasticsearch.repositories.azure;
 
-import com.microsoft.azure.storage.LocationMode;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.net.UnknownHostException;
+
 import org.elasticsearch.common.Strings;
 import org.elasticsearch.common.settings.SecureString;
 import org.elasticsearch.common.settings.Setting;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.settings.SettingsException;
-import io.crate.common.unit.TimeValue;
 
-import java.net.InetAddress;
-import java.net.InetSocketAddress;
-import java.net.Proxy;
-import java.net.UnknownHostException;
+import com.microsoft.azure.storage.LocationMode;
+
+import io.crate.common.annotations.VisibleForTesting;
+import io.crate.common.unit.TimeValue;
 
 public final class AzureStorageSettings {
 
@@ -42,13 +45,14 @@ public final class AzureStorageSettings {
     private final Proxy proxy;
     private final LocationMode locationMode;
 
-    private AzureStorageSettings(String account,
-                                 String key,
-                                 String endpointSuffix,
-                                 TimeValue timeout,
-                                 int maxRetries,
-                                 Proxy proxy,
-                                 LocationMode locationMode) {
+    @VisibleForTesting
+    AzureStorageSettings(String account,
+                         String key,
+                         String endpointSuffix,
+                         TimeValue timeout,
+                         int maxRetries,
+                         Proxy proxy,
+                         LocationMode locationMode) {
         this.account = account;
         this.key = key;
         this.endpointSuffix = endpointSuffix;

plugins/es-repository-azure/src/test/java/org/elasticsearch/repositories/azure/AzureBlobStoreContainerTests.java
@@ -19,19 +19,30 @@
 
 package org.elasticsearch.repositories.azure;
 
+import java.util.Locale;
+
 import org.elasticsearch.cluster.metadata.RepositoryMetadata;
 import org.elasticsearch.common.blobstore.BlobStore;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.repositories.ESBlobStoreContainerTestCase;
+
+import io.crate.common.unit.TimeValue;
 
 public class AzureBlobStoreContainerTests extends ESBlobStoreContainerTestCase {
 
     @Override
     protected BlobStore newBlobStore() {
         RepositoryMetadata repositoryMetadata = new RepositoryMetadata("azure", "ittest", Settings.EMPTY);
-        AzureStorageServiceMock client = new AzureStorageServiceMock();
+        AzureStorageServiceMock client = new AzureStorageServiceMock(randomMockAzureStorageSettings());
         try (AzureBlobStore azureBlobStore = new AzureBlobStore(repositoryMetadata, client)) {
             return azureBlobStore;
         }
+    }
+
+    static AzureStorageSettings randomMockAzureStorageSettings() {
+        String account = randomAlphaOfLength(randomIntBetween(1, 10)).toLowerCase(Locale.ROOT);
+        String key = randomAlphaOfLength(randomIntBetween(10, 20)).toLowerCase(Locale.ROOT);
+        String endpointSuffix = randomAlphaOfLength(randomIntBetween(1, 10)).toLowerCase(Locale.ROOT);
+        return new AzureStorageSettings(account, key, endpointSuffix, TimeValue.timeValueMinutes(-1), 3, null, null);
     }
 }

plugins/es-repository-azure/src/test/java/org/elasticsearch/repositories/azure/AzureBlobStoreTests.java
@@ -18,6 +18,8 @@
  */
 package org.elasticsearch.repositories.azure;
 
+import static org.elasticsearch.repositories.azure.AzureBlobStoreContainerTests.randomMockAzureStorageSettings;
+
 import org.elasticsearch.cluster.metadata.RepositoryMetadata;
 import org.elasticsearch.common.blobstore.BlobStore;
 import org.elasticsearch.common.settings.Settings;
@@ -28,7 +30,7 @@ public class AzureBlobStoreTests extends ESBlobStoreTestCase {
     @Override
     protected BlobStore newBlobStore() {
         RepositoryMetadata repositoryMetadata = new RepositoryMetadata("azure", "ittest", Settings.EMPTY);
-        AzureStorageServiceMock client = new AzureStorageServiceMock();
+        AzureStorageServiceMock client = new AzureStorageServiceMock(randomMockAzureStorageSettings());
         try (AzureBlobStore azureBlobStore = new AzureBlobStore(repositoryMetadata, client)) {
             return azureBlobStore;
         }

plugins/es-repository-azure/src/test/java/org/elasticsearch/repositories/azure/AzureRepositorySettingsTests.java
@@ -21,7 +21,6 @@ package org.elasticsearch.repositories.azure;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.Mockito.mock;
 
 import org.elasticsearch.cluster.metadata.RepositoryMetadata;
 import org.elasticsearch.common.settings.ClusterSettings;
@@ -47,7 +46,6 @@ public class AzureRepositorySettingsTests extends ESTestCase {
         final AzureRepository azureRepository = new AzureRepository(
             new RepositoryMetadata("foo", "azure", internalSettings),
             NamedXContentRegistry.EMPTY,
-            mock(AzureStorageService.class),
             BlobStoreTestUtil.mockClusterService(),
             new RecoverySettings(settings, new ClusterSettings(settings, ClusterSettings.BUILT_IN_CLUSTER_SETTINGS))
         );

plugins/es-repository-azure/src/test/java/org/elasticsearch/repositories/azure/AzureStorageServiceMock.java
@@ -19,15 +19,6 @@
 
 package org.elasticsearch.repositories.azure;
 
-import com.microsoft.azure.storage.OperationContext;
-import com.microsoft.azure.storage.StorageException;
-import com.microsoft.azure.storage.blob.CloudBlobClient;
-import org.elasticsearch.common.blobstore.BlobMetadata;
-import org.elasticsearch.common.blobstore.support.PlainBlobMetadata;
-import io.crate.common.collections.Tuple;
-import org.elasticsearch.common.settings.Settings;
-import io.crate.common.io.Streams;
-
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -42,12 +33,26 @@ import java.util.function.Supplier;
 
 import javax.annotation.Nullable;
 
+import org.elasticsearch.common.blobstore.BlobMetadata;
+import org.elasticsearch.common.blobstore.support.PlainBlobMetadata;
+
+import com.microsoft.azure.storage.OperationContext;
+import com.microsoft.azure.storage.StorageException;
+import com.microsoft.azure.storage.blob.CloudBlobClient;
+
+import io.crate.common.collections.Tuple;
+import io.crate.common.io.Streams;
+
 /**
  * In memory storage for unit tests
  */
 public class AzureStorageServiceMock extends AzureStorageService {
 
     protected final Map<String, ByteArrayOutputStream> blobs = new ConcurrentHashMap<>();
+
+    AzureStorageServiceMock(AzureStorageSettings storageSettings) {
+        super(storageSettings);
+    }
 
     @Override
     public boolean doesContainerExist(String container) {
@@ -145,10 +150,5 @@ public class AzureStorageServiceMock extends AzureStorageService {
     @Override
     public Tuple<CloudBlobClient, Supplier<OperationContext>> client() {
         return null;
-    }
-
-    @Override
-    public void refreshSettings(AzureStorageSettings clientsSettings) {
-        AzureStorageSettings.getClientSettings(Settings.EMPTY);
     }
 }

plugins/es-repository-azure/src/test/java/org/elasticsearch/repositories/azure/AzureStorageServiceTests.java
@@ -42,9 +42,7 @@ import com.microsoft.azure.storage.core.Base64;
 public class AzureStorageServiceTests extends ESTestCase {
 
     private AzureStorageService storageServiceWithSettings(Settings settings) {
-        AzureStorageService storageService = new AzureStorageService();
-        storageService.refreshSettings(AzureStorageSettings.getClientSettings(settings));
-        return storageService;
+        return new AzureStorageService(AzureStorageSettings.getClientSettings(settings));
     }
 
     public void testCreateClientWithEndpointSuffix() {
@@ -52,8 +50,7 @@ public class AzureStorageServiceTests extends ESTestCase {
             .put("endpoint_suffix", "my_endpoint_suffix").build();
         final AzureStorageService azureStorageService = storageServiceWithSettings(settings);
         final CloudBlobClient client = azureStorageService.client().v1();
-        assertThat(client.getEndpoint().toString())
-            .isEqualTo("https://myaccount1.blob.my_endpoint_suffix");
+        assertThat(client.getEndpoint()).hasToString("https://myaccount1.blob.my_endpoint_suffix");
     }
 
     public void testGetSelectedClientDefaultTimeout() {