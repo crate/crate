0a1c841d835617c2aa789299d7d99e474aa537c5
Minsoo Jee <jeeminso@gmail.com>
2023-05-19 19:41:10 +0900

    Adjust allowed array index range from Integer.MIN_VALUE to Integer.MAX_VALUE

--- server/src/main/java/io/crate/analyze/SubscriptValidator.java
+++ server/src/main/java/io/crate/analyze/SubscriptValidator.java
@@ -24,12 +24,10 @@ package io.crate.analyze;
 import io.crate.sql.tree.ArrayLiteral;
 import io.crate.sql.tree.AstVisitor;
 import io.crate.sql.tree.Cast;
-import io.crate.sql.tree.ColumnType;
 import io.crate.sql.tree.Expression;
 import io.crate.sql.tree.FunctionCall;
 import io.crate.sql.tree.IntegerLiteral;
 import io.crate.sql.tree.LongLiteral;
-import io.crate.sql.tree.NegativeExpression;
 import io.crate.sql.tree.ObjectLiteral;
 import io.crate.sql.tree.ParameterExpression;
 import io.crate.sql.tree.QualifiedNameReference;
@@ -40,9 +38,13 @@ import io.crate.sql.tree.TryCast;
 
 import java.util.Locale;
 
+/**
+ * NOTE: Array index out of bound validation cannot happen here since array index expressions such as
+ * `-1` or `x + 1` are not yet evaluated. The actual validation is handled by implicit cast of
+ * the array indexes to the target type (integer) of {@link io.crate.expression.scalar.SubscriptFunction}.
+ * See {@link io.crate.analyze.expressions.ExpressionAnalyzer#cast}
+ */
 public final class SubscriptValidator {
-
-    private static final Long MAX_VALUE = Integer.MAX_VALUE + 1L;
 
     private SubscriptValidator() {
     }
@@ -116,12 +118,6 @@ public final class SubscriptValidator {
 
         private static final SubscriptIndexVisitor INSTANCE = new SubscriptIndexVisitor();
 
-        private static void raiseInvalidIndexValue() {
-            throw new UnsupportedOperationException(
-                String.format(Locale.ENGLISH, "Array index must be in range 1 to %s",
-                              MAX_VALUE));
-        }
-
         @Override
         public Void visitParameterExpression(ParameterExpression node, SubscriptContext context) {
             throw new UnsupportedOperationException("Parameter substitution is not supported in subscript index");
@@ -133,35 +129,18 @@ public final class SubscriptValidator {
             return null;
         }
 
-
         @Override
         protected Void visitLongLiteral(LongLiteral node, SubscriptContext context) {
             validateNestedArrayAccess(context);
-            long value = node.getValue();
-
-            if (value < 1 || value > MAX_VALUE) {
-                raiseInvalidIndexValue();
-            }
-            context.index(new Cast(node, new ColumnType<>("integer")));
+            context.index(node);
             return null;
         }
-
 
         @Override
         protected Void visitIntegerLiteral(IntegerLiteral node, SubscriptContext context) {
             validateNestedArrayAccess(context);
-            if (node.getValue() < 1) {
-                raiseInvalidIndexValue();
-            }
-            context.index(new Cast(node, new ColumnType<>("integer")));
+            context.index(node);
             return null;
-        }
-
-        @Override
-        protected Void visitNegativeExpression(NegativeExpression node, SubscriptContext context) {
-            throw new UnsupportedOperationException(
-                String.format(Locale.ENGLISH, "Array index must be in range 1 to %s",
-                    MAX_VALUE));
         }
 
         @Override
