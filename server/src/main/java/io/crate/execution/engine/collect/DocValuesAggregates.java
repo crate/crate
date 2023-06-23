3e168f90732e55d66d9b88da7dbf9b047d962d0e
Michael Kleen <michael.kleen@crate.io>
2023-06-23 10:12:29 +0200

    Make aggregations in INSERT INTO work
    Aggregations used in INSERT INTO statement will return null e.g.:
    
    INSERT INTO t2 SELECT AVG(a) FROM t1;
    
    will write null to t2, no matter of the values of t1.a.
    
    This applies to all types of aggregations used in this context. The root cause is that references
    in the INSERT INTO have a hint to prefer source look-ups, which should be ignored by aggregations.
    
    (cherry picked from commit a5b40fe0235c0768bbab89071a0cd8db751a4387)
    
    # Conflicts:
    #	docs/appendices/release-notes/unreleased.rst

--- server/src/main/java/io/crate/execution/engine/collect/DocValuesAggregates.java
+++ server/src/main/java/io/crate/execution/engine/collect/DocValuesAggregates.java
@@ -68,6 +68,7 @@ import io.crate.memory.MemoryManager;
 import io.crate.metadata.FunctionImplementation;
 import io.crate.metadata.Functions;
 import io.crate.metadata.Reference;
+import io.crate.metadata.doc.DocSysColumns;
 import io.crate.metadata.doc.DocTableInfo;
 import io.crate.types.DataTypes;
 
@@ -160,6 +161,8 @@ public class DocValuesAggregates {
                         // to the normal aggregation implementation
                         return null;
                     }
+                    assert reference.ident().columnIdent().fqn().startsWith(DocSysColumns.Names.DOC) == false :
+                        "Source look-up for Reference " + reference + " is not allowed in DocValuesAggregates.";
                     aggregationReferences.add(reference);
                 }
             }
