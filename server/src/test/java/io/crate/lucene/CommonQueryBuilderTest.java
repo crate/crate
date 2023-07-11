d3b4382ca6a63e01f50c67c4a5f613f20905e3fc
jeeminso <jeeminso@gmail.com>
2023-07-11 15:56:36 -0400

    Fix IndexOutOfBoundsException due to `IS NULL` over a subscript function

--- server/src/test/java/io/crate/lucene/CommonQueryBuilderTest.java
+++ server/src/test/java/io/crate/lucene/CommonQueryBuilderTest.java
@@ -472,6 +472,12 @@ public class CommonQueryBuilderTest extends LuceneQueryBuilderTest {
     }
 
     @Test
+    public void test_is_null_on_subscript_function() {
+        Query query = convert("o_array[1]['xs'] is null");
+        assertThat(query).isExactlyInstanceOf(GenericFunctionQuery.class);
+    }
+
+    @Test
     public void testMatchWithOperator() {
         assertThat(convert("match(tags, 'foo bar') using best_fields with (operator='and')")).hasToString(
             "+tags:foo +tags:bar");
