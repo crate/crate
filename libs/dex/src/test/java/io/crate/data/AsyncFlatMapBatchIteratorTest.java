e87541d9c2460a928f8e1a3b631b99ece7c80e84
Mathias Fussenegger <f.mathias@zignar.net>
2023-07-19 11:33:36 +0200

    Fix NPE with cross joins and fetch phases (Reset state on moveToStart in AsyncFlatMapBatchIterator)
    Fixes https://github.com/crate/crate/issues/14381
    
    There were two issues with the `moveToStart()` implementation in
    `AsyncFlatMapBatchIterator`:
    
    - `isLast` was always set to `true` after a `moveToStart()` after the
      iterator was previously exhausted. This could close resources early in
      a fetch phase, making them unavailable for further
      `moveNext()/loadNextBatch()` calls.
    
    - It continued outputting mapped rows if `moveToStart()` was called
      _before_ the iterator was exhausted. This was currently a non-issue
      as `moveToStart()` is only called after an iterator is exhausted, but
      it's still wrong.

--- libs/dex/src/test/java/io/crate/data/AsyncFlatMapBatchIteratorTest.java
+++ libs/dex/src/test/java/io/crate/data/AsyncFlatMapBatchIteratorTest.java
@@ -29,6 +29,8 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 import org.junit.Test;
@@ -122,5 +124,71 @@ public class AsyncFlatMapBatchIteratorTest {
                 new Object[] { 3 }
             )
         );
+    }
+
+    @Test
+    public void test_repeats_source_after_intermediate_move_to_start() throws Exception {
+        var source = TestingBatchIterators.range(1, 4);
+        AsyncFlatMapper<Row, Row> duplicateRow = (row, isLast) -> {
+            CloseableIterator<Row> result = mkIter(
+                new RowN(row.materialize()),
+                new RowN(row.materialize())
+            );
+            return CompletableFuture.completedFuture(result);
+        };
+        var it = new AsyncFlatMapBatchIterator<>(source, duplicateRow);
+        assertThat(it.moveNext()).as("needs to load source first").isFalse();
+        assertThat(it.loadNextBatch()).succeedsWithin(1, TimeUnit.SECONDS);
+        // first duplicated row
+        assertThat(it.moveNext()).isTrue();
+        assertThat(it.currentElement().get(0)).isEqualTo(1);
+        assertThat(it.moveNext()).isTrue();
+        assertThat(it.currentElement().get(0)).isEqualTo(1);
+        assertThat(it.moveNext()).isFalse();
+
+        // load second duplicated row
+        assertThat(it.loadNextBatch()).succeedsWithin(1, TimeUnit.SECONDS);
+        assertThat(it.moveNext()).isTrue();
+        assertThat(it.currentElement().get(0)).isEqualTo(2);
+
+        it.moveToStart();
+        // needs to start with first row again
+        assertThat(it.moveNext()).isFalse();
+        assertThat(it.loadNextBatch()).succeedsWithin(1, TimeUnit.SECONDS);
+        assertThat(it.moveNext()).isTrue();
+        assertThat(it.currentElement().get(0))
+            .as("first row after moveToStart must contain 1")
+            .isEqualTo(1);
+    }
+
+    @Test
+    public void test_calls_mapper_with_isLast_set_to_false_after_move_to_start() throws Exception {
+        AtomicInteger numCalls = new AtomicInteger();
+        AtomicBoolean wasLast = new AtomicBoolean();
+        AsyncFlatMapper<Row, Row> duplicateRow = (row, isLast) -> {
+            wasLast.set(isLast);
+            CloseableIterator<Row> result = mkIter(
+                new RowN(row.materialize()),
+                new RowN(row.materialize())
+            );
+            numCalls.incrementAndGet();
+            return CompletableFuture.completedFuture(result);
+        };
+        var source = TestingBatchIterators.range(1, 2);
+        var it = new AsyncFlatMapBatchIterator<>(source, duplicateRow);
+
+        assertThat(it.moveNext()).isFalse();
+        assertThat(it.loadNextBatch()).succeedsWithin(1, TimeUnit.SECONDS);
+        assertThat(numCalls).hasValue(1);
+        assertThat(wasLast).isFalse();
+        assertThat(it.moveNext()).isTrue();
+        assertThat(it.moveNext()).isTrue();
+        assertThat(it.moveNext()).isFalse();
+        assertThat(it.allLoaded()).isTrue();
+
+        it.moveToStart();
+        it.moveNext();
+        assertThat(it.loadNextBatch()).succeedsWithin(1, TimeUnit.SECONDS);
+        assertThat(wasLast).as("move to start needs to reset sourceExhausted").isFalse();
     }
 }
