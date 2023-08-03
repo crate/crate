d1aaa7c876f61ee43ebe82dbbd179062d8a8b426
Marios Trivyzas <matriv@gmail.com>
2023-08-03 13:08:01 +0300

    Add Analyzer support for DROP COLUMN

---
+++ server/src/main/java/io/crate/analyze/AlterTableDropColumnAnalyzer.java
@@ -0,0 +1,58 @@
+/*
+ * Licensed to Crate.io GmbH ("Crate") under one or more contributor
+ * license agreements.  See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.  Crate licenses
+ * this file to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.  You may
+ * obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * However, if you have executed another commercial license agreement
+ * with Crate these terms will supersede the license and you may use the
+ * software solely pursuant to the terms of the relevant commercial agreement.
+ */
+
+package io.crate.analyze;
+
+import io.crate.metadata.CoordinatorTxnCtx;
+import io.crate.metadata.NodeContext;
+import io.crate.metadata.Schemas;
+import io.crate.metadata.doc.DocTableInfo;
+import io.crate.metadata.table.Operation;
+import io.crate.sql.tree.AlterTableDropColumn;
+import io.crate.sql.tree.Expression;
+
+class AlterTableDropColumnAnalyzer {
+
+    private final Schemas schemas;
+    private final NodeContext nodeCtx;
+
+    AlterTableDropColumnAnalyzer(Schemas schemas, NodeContext nodeCtx) {
+        this.schemas = schemas;
+        this.nodeCtx = nodeCtx;
+    }
+
+    public AnalyzedAlterTableDropColumn analyze(AlterTableDropColumn<Expression> alterTable,
+                                                ParamTypeHints paramTypeHints,
+                                                CoordinatorTxnCtx txnCtx) {
+        if (!alterTable.table().partitionProperties().isEmpty()) {
+            throw new UnsupportedOperationException("Dropping a column from a single partition is not supported");
+        }
+
+        DocTableInfo tableInfo = (DocTableInfo) schemas.resolveTableInfo(
+            alterTable.table().getName(),
+            Operation.ALTER,
+            txnCtx.sessionSettings().sessionUser(),
+            txnCtx.sessionSettings().searchPath());
+
+        var analyzer = new TableElementsAnalyzer(tableInfo, txnCtx, nodeCtx, paramTypeHints);
+        return analyzer.analyze(alterTable);
+    }
+}
