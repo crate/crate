8a67d7e633dc8bee70de1be8e8816b552ed88da6
jeeminso <jeeminso@gmail.com>
2023-08-29 16:17:55 -0400

    Fix Lucene query building for Long types

--- server/src/test/java/io/crate/lucene/BitStringEqQueryTest.java
+++ server/src/test/java/io/crate/lucene/BitStringEqQueryTest.java
@@ -36,7 +36,7 @@ import io.crate.sql.tree.BitString;
 public class BitStringEqQueryTest extends LuceneQueryBuilderTest {
     private static final BitString BIT_STRING = BitString.ofBitString("B'001'");
     private static final BytesRef BIT_STRING_IN_BYTES_REF = new BytesRef(BIT_STRING.bitSet().toByteArray());
-    
+
     @Override
     protected String createStmt() {
         // `columnstore = false` is not supported
