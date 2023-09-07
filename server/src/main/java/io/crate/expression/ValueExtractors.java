bc2d02d6a4f4ae87f5efb6214c5b5e6cad51e4d8
baur <baurzhansahariev@gmail.com>
2023-09-07 09:27:18 +0200

    Remove unused FromRowWithSubscript

--- server/src/main/java/io/crate/expression/ValueExtractors.java
+++ server/src/main/java/io/crate/expression/ValueExtractors.java
@@ -28,7 +28,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 
-import io.crate.data.Row;
 import io.crate.metadata.ColumnIdent;
 import io.crate.types.DataType;
 
@@ -61,28 +60,5 @@ public final class ValueExtractors {
 
     public static Function<Map<String, Object>, Object> fromMap(ColumnIdent column, DataType<?> type) {
         return map -> type.implicitCast(fromMap(map, column));
-    }
-
-    public static Function<Row, Object> fromRow(int idx, List<String> subscript) {
-        return new FromRowWithSubscript(idx, subscript);
-    }
-
-    private static class FromRowWithSubscript implements Function<Row, Object> {
-        private final int idx;
-        private final List<String> subscript;
-
-        FromRowWithSubscript(int idx, List<String> subscript) {
-            this.idx = idx;
-            this.subscript = subscript;
-        }
-
-        @Override
-        public Object apply(Row row) {
-            Object o = row.get(idx);
-            if (o instanceof Map) {
-                return getByPath((Map) o, subscript);
-            }
-            return null;
-        }
     }
 }
