e4c4fd76a2a4d3c604c8de887d2d2c53fe798199
baur <baurzhansahariev@gmail.com>
2023-09-13 01:56:31 +0200

    fixup: mixed select of doc and regular column, ensure registering all

--- server/src/test/java/io/crate/integrationtests/TransportSQLActionTest.java
+++ server/src/test/java/io/crate/integrationtests/TransportSQLActionTest.java
@@ -1465,6 +1465,8 @@ public class TransportSQLActionTest extends IntegTestCase {
         Map<String, Object> _doc2 = (Map<String, Object>) response.rows()[1][0];
         assertEquals(_doc1.get("id"), "2");
         assertEquals(_doc2.get("id"), "3");
+        assertEquals(_doc1.get("name"), "Outer Eastern Rim");
+        assertEquals(_doc2.get("name"), "Galactic Sector QQ7 Active J Gamma");
 
         execute("select name, kind from locations where id in (2,3) order by id");
         assertEquals(printedTable(response.rows()), "Outer Eastern Rim| Galaxy\n" +
