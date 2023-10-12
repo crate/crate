deb2e37618a5d1da71347abc65b958ed0791c3ce
Marios Trivyzas <matriv@gmail.com>
2023-10-12 18:34:26 +0300

    Reflect release 5.3.7

--- server/src/main/java/org/elasticsearch/Version.java
+++ server/src/main/java/org/elasticsearch/Version.java
@@ -169,6 +169,7 @@ public class Version implements Comparable<Version>, ToXContentFragment {
     public static final Version V_5_3_4 = new Version(8_03_04_99, false, org.apache.lucene.util.Version.LUCENE_9_5_0);
     public static final Version V_5_3_5 = new Version(8_03_05_99, false, org.apache.lucene.util.Version.LUCENE_9_5_0);
     public static final Version V_5_3_6 = new Version(8_03_06_99, false, org.apache.lucene.util.Version.LUCENE_9_5_0);
+    public static final Version V_5_3_7 = new Version(8_03_07_99, false, org.apache.lucene.util.Version.LUCENE_9_5_0);
 
     public static final Version V_5_4_0 = new Version(8_04_00_99, false, org.apache.lucene.util.Version.LUCENE_9_6_0);
     public static final Version V_5_4_1 = new Version(8_04_01_99, false, org.apache.lucene.util.Version.LUCENE_9_6_0);
