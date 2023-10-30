28f35828cdae3be97fcdbe25fcde0b5209510897
Michael Kleen <mkleen@gmail.com>
2023-10-30 11:10:20 +0100

    Fix imports

--- server/src/main/java/io/crate/planner/optimizer/rule/MoveFilterIntoCrossJoin.java
+++ server/src/main/java/io/crate/planner/optimizer/rule/MoveFilterIntoCrossJoin.java
@@ -1,21 +1,37 @@
+/*
+ * Licensed to Crate.io GmbH ("Crate") under one or more contributor
+ * license agreements.  See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.  Crate licenses
+ * this file to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.  You may
+ * obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * However, if you have executed another commercial license agreement
+ * with Crate these terms will supersede the license and you may use the
+ * software solely pursuant to the terms of the relevant commercial agreement.
+ */
+
 package io.crate.planner.optimizer.rule;
 
 import static io.crate.planner.optimizer.matcher.Pattern.typeOf;
 import static io.crate.planner.optimizer.matcher.Patterns.source;
-import static io.crate.planner.optimizer.rule.FilterOnJoinsUtil.moveQueryBelowJoin;
 
 import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
 import java.util.function.Function;
 
 import io.crate.analyze.relations.QuerySplitter;
-import io.crate.expression.symbol.Symbol;
 import io.crate.metadata.NodeContext;
 import io.crate.metadata.RelationName;
 import io.crate.metadata.TransactionContext;
 import io.crate.planner.operators.Filter;
-import io.crate.planner.operators.HashJoin;
 import io.crate.planner.operators.JoinPlan;
 import io.crate.planner.operators.LogicalPlan;
 import io.crate.planner.optimizer.Rule;
@@ -25,7 +41,7 @@ import io.crate.planner.optimizer.matcher.Captures;
 import io.crate.planner.optimizer.matcher.Pattern;
 import io.crate.sql.tree.JoinType;
 
-public class MoveFilterIntoCrossJoin implements Rule<Filter>  {
+public class MoveFilterIntoCrossJoin implements Rule<Filter> {
 
     private final Capture<JoinPlan> joinCapture;
     private final Pattern<Filter> pattern;
@@ -62,6 +78,6 @@ public class MoveFilterIntoCrossJoin implements Rule<Filter>  {
         if (new HashSet<>(join.getRelationNames()).containsAll(relationNames)) {
             return new JoinPlan(join.lhs(), join.rhs(), JoinType.INNER, query);
         }
-    return null;
+        return null;
     }
 }
