13278ecc34c2f3ec5315133ab0e9ed8c575a4791
jeeminso <jeeminso@gmail.com>
2023-11-29 10:12:27 -0500

    Refactored 'RewriteFilterOnOuterJoinToInnerJoin' and 'MoveFilterBeneathJoin'
    as part of adding a missing filter push down logic identified(see the PR or
    issue for the specific scenario).
    
    The refactoring was necessary because it made more sense to handle the missing
    filter push-down and the related push-downs by MoveFilterBeneathJoin NOT
    RewriteFilterOnOuterJoinToInnerJoin.
    
    RewriteFilterOnOuterJoinToInnerJoin is now responsible for:
      1) filter push-downs to non-preserved sides of joins and
      2) rewriting outer joins to inner joins
    MoveFilterBeneathJoin is now responsible for:
      filter push-downs to preserved sides of joins

--- server/src/test/java/io/crate/planner/optimizer/rule/MoveFilterBeneathJoinTest.java
+++ server/src/test/java/io/crate/planner/optimizer/rule/MoveFilterBeneathJoinTest.java
@@ -313,12 +313,261 @@ public class MoveFilterBeneathJoinTest extends CrateDummyClusterServiceUnitTest 
 
         assertThat(result).hasOperators(
             "Join[INNER | (a = c)]",
-            "  ├ Filter[(b < 2)]",
-            "  │  └ Filter[(a > 1)]",
-            "  │    └ Join[INNER | (a = b)]",
-            "  │      ├ Collect[doc.t1 | [a] | true]",
-            "  │      └ Collect[doc.t2 | [b] | true]",
+            "  ├ Filter[((a > 1) AND (b < 2))]",
+            "  │  └ Join[INNER | (a = b)]",
+            "  │    ├ Collect[doc.t1 | [a] | true]",
+            "  │    └ Collect[doc.t2 | [b] | true]",
             "  └ Collect[doc.t3 | [c] | true]"
+        );
+    }
+
+    @Test
+    public void test_push_filter_down_to_preserved_side_of_left_join() {
+        var joinCondition = sqlExpressions.asSymbol("doc.t1.a = doc.t2.b");
+        var join = new JoinPlan(t1, t2, JoinType.LEFT, joinCondition);
+        var filter = new Filter(join, sqlExpressions.asSymbol("doc.t1.a > 1"));
+        assertThat(filter).hasOperators(
+            "Filter[(a > 1)]",
+            "  └ Join[LEFT | (a = b)]",
+            "    ├ Collect[doc.t1 | [a] | true]",
+            "    └ Collect[doc.t2 | [b] | true]"
+        );
+
+        var rule = new MoveFilterBeneathJoin();
+        var match = rule.pattern().accept(filter, Captures.empty());
+
+        assertThat(match.isPresent()).isTrue();
+        assertThat(match.value()).isEqualTo(filter);
+
+        var result = rule.apply(match.value(),
+            match.captures(),
+            planStats,
+            CoordinatorTxnCtx.systemTransactionContext(),
+            sqlExpressions.nodeCtx,
+            Function.identity());
+
+        assertThat(result).hasOperators(
+            "Join[LEFT | (a = b)]",
+            "  ├ Filter[(a > 1)]",
+            "  │  └ Collect[doc.t1 | [a] | true]",
+            "  └ Collect[doc.t2 | [b] | true]"
+        );
+    }
+
+    @Test
+    public void test_cannot_push_filter_down_to_non_preserved_side_of_left_join() {
+        var joinCondition = sqlExpressions.asSymbol("doc.t1.a = doc.t2.b");
+        var join = new JoinPlan(t1, t2, JoinType.LEFT, joinCondition);
+        var filter = new Filter(join, sqlExpressions.asSymbol("doc.t2.b > 1"));
+        assertThat(filter).hasOperators(
+            "Filter[(b > 1)]",
+            "  └ Join[LEFT | (a = b)]",
+            "    ├ Collect[doc.t1 | [a] | true]",
+            "    └ Collect[doc.t2 | [b] | true]"
+        );
+
+        var rule = new MoveFilterBeneathJoin();
+        var match = rule.pattern().accept(filter, Captures.empty());
+
+        assertThat(match.isPresent()).isTrue();
+        assertThat(match.value()).isEqualTo(filter);
+
+        var result = rule.apply(match.value(),
+            match.captures(),
+            planStats,
+            CoordinatorTxnCtx.systemTransactionContext(),
+            sqlExpressions.nodeCtx,
+            Function.identity());
+
+        assertThat(result).isNull();
+    }
+
+    @Test
+    public void test_push_filter_down_to_preserved_of_right_join() {
+        var joinCondition = sqlExpressions.asSymbol("doc.t1.a = doc.t2.b");
+        var join = new JoinPlan(t1, t2, JoinType.RIGHT, joinCondition);
+        var filter = new Filter(join, sqlExpressions.asSymbol("doc.t2.b > 1"));
+        assertThat(filter).hasOperators(
+            "Filter[(b > 1)]",
+            "  └ Join[RIGHT | (a = b)]",
+            "    ├ Collect[doc.t1 | [a] | true]",
+            "    └ Collect[doc.t2 | [b] | true]"
+        );
+
+        var rule = new MoveFilterBeneathJoin();
+        var match = rule.pattern().accept(filter, Captures.empty());
+
+        assertThat(match.isPresent()).isTrue();
+        assertThat(match.value()).isEqualTo(filter);
+
+        var result = rule.apply(match.value(),
+            match.captures(),
+            planStats,
+            CoordinatorTxnCtx.systemTransactionContext(),
+            sqlExpressions.nodeCtx,
+            Function.identity());
+
+        assertThat(result).hasOperators(
+            "Join[RIGHT | (a = b)]",
+            "  ├ Collect[doc.t1 | [a] | true]",
+            "  └ Filter[(b > 1)]",
+            "    └ Collect[doc.t2 | [b] | true]"
+        );
+    }
+
+    @Test
+    public void test_cannot_push_filter_down_to_non_preserved_side_of_right_join() {
+        var joinCondition = sqlExpressions.asSymbol("doc.t1.a = doc.t2.b");
+        var join = new JoinPlan(t1, t2, JoinType.RIGHT, joinCondition);
+        var filter = new Filter(join, sqlExpressions.asSymbol("doc.t1.a > 1"));
+        assertThat(filter).hasOperators(
+            "Filter[(a > 1)]",
+            "  └ Join[RIGHT | (a = b)]",
+            "    ├ Collect[doc.t1 | [a] | true]",
+            "    └ Collect[doc.t2 | [b] | true]"
+        );
+
+        var rule = new MoveFilterBeneathJoin();
+        var match = rule.pattern().accept(filter, Captures.empty());
+
+        assertThat(match.isPresent()).isTrue();
+        assertThat(match.value()).isEqualTo(filter);
+
+        var result = rule.apply(match.value(),
+            match.captures(),
+            planStats,
+            CoordinatorTxnCtx.systemTransactionContext(),
+            sqlExpressions.nodeCtx,
+            Function.identity());
+
+        assertThat(result).isNull();
+    }
+
+    @Test
+    public void test_cannot_push_filter_down_to_full_join() {
+        var joinCondition = sqlExpressions.asSymbol("doc.t1.a = doc.t2.b");
+        var join = new JoinPlan(t1, t2, JoinType.FULL, joinCondition);
+        var filter = new Filter(join, sqlExpressions.asSymbol("doc.t1.a > 1 and doc.t2.b > 1"));
+        assertThat(filter).hasOperators(
+            "Filter[((a > 1) AND (b > 1))]",
+            "  └ Join[FULL | (a = b)]",
+            "    ├ Collect[doc.t1 | [a] | true]",
+            "    └ Collect[doc.t2 | [b] | true]"
+        );
+
+        var rule = new MoveFilterBeneathJoin();
+        var match = rule.pattern().accept(filter, Captures.empty());
+
+        assertThat(match.isPresent()).isFalse();
+    }
+
+    @Test
+    public void test_push_filter_down_to_cross_join() {
+        var join = new JoinPlan(t1, t2, JoinType.CROSS, null);
+        var filter = new Filter(join, sqlExpressions.asSymbol("doc.t2.b > 1"));
+        assertThat(filter).hasOperators(
+            "Filter[(b > 1)]",
+            "  └ Join[CROSS]",
+            "    ├ Collect[doc.t1 | [a] | true]",
+            "    └ Collect[doc.t2 | [b] | true]"
+        );
+
+        var rule = new MoveFilterBeneathJoin();
+        var match = rule.pattern().accept(filter, Captures.empty());
+
+        assertThat(match.isPresent()).isTrue();
+        assertThat(match.value()).isEqualTo(filter);
+
+        var result = rule.apply(match.value(),
+            match.captures(),
+            planStats,
+            CoordinatorTxnCtx.systemTransactionContext(),
+            sqlExpressions.nodeCtx,
+            Function.identity());
+
+        assertThat(result).hasOperators(
+            "Join[CROSS]",
+            "  ├ Collect[doc.t1 | [a] | true]",
+            "  └ Filter[(b > 1)]",
+            "    └ Collect[doc.t2 | [b] | true]"
+        );
+    }
+
+    @Test
+    public void test_push_filter_down_to_preserved_side_of_left_nested_join() {
+        var joinCondition1 = sqlExpressions.asSymbol("doc.t1.a = doc.t2.b");
+        var join1 = new JoinPlan(t1, t2, JoinType.LEFT, joinCondition1);
+        var joinCondition2 = sqlExpressions.asSymbol("doc.t1.a = doc.t3.c");
+        var join2 = new JoinPlan(join1, t3, JoinType.LEFT, joinCondition2);
+        var filter = new Filter(join2, sqlExpressions.asSymbol("doc.t1.a > 1"));
+        assertThat(filter).hasOperators(
+            "Filter[(a > 1)]",
+            "  └ Join[LEFT | (a = c)]",
+            "    ├ Join[LEFT | (a = b)]",
+            "    │  ├ Collect[doc.t1 | [a] | true]",
+            "    │  └ Collect[doc.t2 | [b] | true]",
+            "    └ Collect[doc.t3 | [c] | true]"
+        );
+
+        var rule = new MoveFilterBeneathJoin();
+        var match = rule.pattern().accept(filter, Captures.empty());
+
+        assertThat(match.isPresent()).isTrue();
+        assertThat(match.value()).isEqualTo(filter);
+
+        var result = rule.apply(match.value(),
+            match.captures(),
+            planStats,
+            CoordinatorTxnCtx.systemTransactionContext(),
+            sqlExpressions.nodeCtx,
+            Function.identity());
+
+        assertThat(result).hasOperators(
+            "Join[LEFT | (a = c)]",
+            "  ├ Filter[(a > 1)]",
+            "  │  └ Join[LEFT | (a = b)]",
+            "  │    ├ Collect[doc.t1 | [a] | true]",
+            "  │    └ Collect[doc.t2 | [b] | true]",
+            "  └ Collect[doc.t3 | [c] | true]"
+        );
+    }
+
+    @Test
+    public void test_push_filter_down_to_preserved_side_of_right_nested_join() {
+        var joinCondition1 = sqlExpressions.asSymbol("doc.t1.a = doc.t2.b");
+        var join1 = new JoinPlan(t1, t2, JoinType.RIGHT, joinCondition1);
+        var joinCondition2 = sqlExpressions.asSymbol("doc.t2.b = doc.t3.c");
+        var join2 = new JoinPlan(t3, join1, JoinType.RIGHT, joinCondition2);
+        var filter = new Filter(join2, sqlExpressions.asSymbol("doc.t2.b > 1"));
+        assertThat(filter).hasOperators(
+            "Filter[(b > 1)]",
+            "  └ Join[RIGHT | (b = c)]",
+            "    ├ Collect[doc.t3 | [c] | true]",
+            "    └ Join[RIGHT | (a = b)]",
+            "      ├ Collect[doc.t1 | [a] | true]",
+            "      └ Collect[doc.t2 | [b] | true]"
+        );
+
+        var rule = new MoveFilterBeneathJoin();
+        var match = rule.pattern().accept(filter, Captures.empty());
+
+        assertThat(match.isPresent()).isTrue();
+        assertThat(match.value()).isEqualTo(filter);
+
+        var result = rule.apply(match.value(),
+            match.captures(),
+            planStats,
+            CoordinatorTxnCtx.systemTransactionContext(),
+            sqlExpressions.nodeCtx,
+            Function.identity());
+
+        assertThat(result).hasOperators(
+            "Join[RIGHT | (b = c)]",
+            "  ├ Collect[doc.t3 | [c] | true]",
+            "  └ Filter[(b > 1)]",
+            "    └ Join[RIGHT | (a = b)]",
+            "      ├ Collect[doc.t1 | [a] | true]",
+            "      └ Collect[doc.t2 | [b] | true]"
         );
     }
 }
