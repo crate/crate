47fac3f2496ceea4c300fba2beb94090dc62a97f
Sebastian Utz <su@rtme.net>
2023-12-21 22:50:47 +0100

    Reflect releases 5.2.11 and 5.3.8 at 5.4

--- server/src/main/java/org/elasticsearch/Version.java
+++ server/src/main/java/org/elasticsearch/Version.java
@@ -161,6 +161,7 @@ public class Version implements Comparable<Version>, ToXContentFragment {
     public static final Version V_5_2_8 = new Version(8_02_08_99, false, org.apache.lucene.util.Version.LUCENE_9_4_2);
     public static final Version V_5_2_9 = new Version(8_02_09_99, false, org.apache.lucene.util.Version.LUCENE_9_4_2);
     public static final Version V_5_2_10 = new Version(8_02_10_99, false, org.apache.lucene.util.Version.LUCENE_9_4_2);
+    public static final Version V_5_2_11 = new Version(8_02_11_99, false, org.apache.lucene.util.Version.LUCENE_9_4_2);
 
     public static final Version V_5_3_0 = new Version(8_03_00_99, false, org.apache.lucene.util.Version.LUCENE_9_5_0);
     public static final Version V_5_3_1 = new Version(8_03_01_99, false, org.apache.lucene.util.Version.LUCENE_9_5_0);
@@ -170,6 +171,7 @@ public class Version implements Comparable<Version>, ToXContentFragment {
     public static final Version V_5_3_5 = new Version(8_03_05_99, false, org.apache.lucene.util.Version.LUCENE_9_5_0);
     public static final Version V_5_3_6 = new Version(8_03_06_99, false, org.apache.lucene.util.Version.LUCENE_9_5_0);
     public static final Version V_5_3_7 = new Version(8_03_07_99, false, org.apache.lucene.util.Version.LUCENE_9_5_0);
+    public static final Version V_5_3_8 = new Version(8_03_08_99, false, org.apache.lucene.util.Version.LUCENE_9_5_0);
 
     public static final Version V_5_4_0 = new Version(8_04_00_99, false, org.apache.lucene.util.Version.LUCENE_9_6_0);
     public static final Version V_5_4_1 = new Version(8_04_01_99, false, org.apache.lucene.util.Version.LUCENE_9_6_0);
