bcaa3db4626bb0a32424959500220df9b86e74d7
Mathias Fussenegger <f.mathias@zignar.net>
2024-01-15 16:06:48 +0100

    Move Sorted from libs/shared to test fixtures
    It's only used there and we should avoid using that functionality
    elsewhere given that it's expensive to copy & sort maps recursively

--- server/src/testFixtures/java/io/crate/testing/TestingHelpers.java
+++ server/src/testFixtures/java/io/crate/testing/TestingHelpers.java
@@ -37,8 +37,11 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
@@ -65,7 +68,6 @@ import com.carrotsearch.hppc.IntArrayList;
 import com.carrotsearch.randomizedtesting.RandomizedTest;
 
 import io.crate.analyze.BoundCreateTable;
-import io.crate.common.collections.Sorted;
 import io.crate.data.Row;
 import io.crate.execution.ddl.tables.MappingUtil;
 import io.crate.execution.engine.aggregation.impl.AggregationImplModule;
@@ -129,6 +131,7 @@ public class TestingHelpers {
         return os.toString();
     }
 
+    @SuppressWarnings("unchecked")
     private static boolean printObject(PrintStream out, boolean first, Object o) {
         if (!first) {
             out.print("| ");
@@ -160,8 +163,7 @@ public class TestingHelpers {
             out.print("]");
         } else if (o instanceof Map) {
             out.print("{");
-            //noinspection unchecked
-            Map<String, Object> map = Sorted.sortRecursive((Map<String, Object>) o, true);
+            Map<String, Object> map = sortRecursive((Map<String, Object>) o, true);
             Iterator<String> it = map.keySet().iterator();
             while (it.hasNext()) {
                 String key = it.next();
@@ -176,6 +178,46 @@ public class TestingHelpers {
             out.print(o.toString());
         }
         return first;
+    }
+
+    @SuppressWarnings("unchecked")
+    private static LinkedHashMap<String, Object> sortRecursive(Map<String, Object> map, boolean sortOnlyMaps) {
+        LinkedHashMap<String, Object> sortedMap = new LinkedHashMap<>(map.size(), 1.0f);
+        ArrayList<String> sortedKeys = new ArrayList<>(map.keySet());
+        Collections.sort(sortedKeys);
+        for (String sortedKey : sortedKeys) {
+            Object o = map.get(sortedKey);
+            if (o instanceof Map) {
+                //noinspection unchecked
+                sortedMap.put(sortedKey, sortRecursive((Map<String, Object>) o, sortOnlyMaps));
+            } else if (o instanceof Collection<?> collection) {
+                sortedMap.put(sortedKey, sortOnlyMaps ? o : sortRecursive(collection));
+            } else {
+                sortedMap.put(sortedKey, o);
+            }
+        }
+        return sortedMap;
+    }
+
+    @SuppressWarnings("unchecked")
+    private static Collection<?> sortRecursive(Collection<?> collection) {
+        if (collection.size() == 0) {
+            return collection;
+        }
+        Object firstElement = collection.iterator().next();
+        if (firstElement instanceof Map) {
+            ArrayList<Object> sortedList = new ArrayList<>(collection.size());
+            for (Object obj : collection) {
+                //noinspection unchecked
+                sortedList.add(sortRecursive((Map<String, Object>) obj, true));
+            }
+            sortedList.sort(null);
+            return sortedList;
+        }
+
+        ArrayList<Object> sortedList = new ArrayList<>(collection);
+        sortedList.sort(null);
+        return sortedList;
     }
 
     public static NodeContext createNodeContext(AbstractModule... additionalModules) {
