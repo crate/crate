739db965f626d2a819526f66d61f6d26902374c2
Michael Kleen <mkleen@gmail.com>
2024-01-17 14:36:01 +0100

    Fix any regression

--- server/src/main/java/io/crate/expression/predicate/NotPredicate.java
+++ server/src/main/java/io/crate/expression/predicate/NotPredicate.java
@@ -21,6 +21,8 @@
 
 package io.crate.expression.predicate;
 
+import static io.crate.expression.predicate.IsNullPredicate.isNullFuncToQuery;
+
 import java.util.HashSet;
 import java.util.Set;
 
@@ -204,10 +206,9 @@ public class NotPredicate extends Scalar<Boolean, Boolean> {
         if (ctx.enforceThreeValuedLogic()) {
             // we require strict 3vl logic, therefore we need to add the function as generic function filter
             // which is less efficient
-            return new BooleanQuery.Builder()
-                .add(notX, Occur.MUST)
-                .add(LuceneQueryBuilder.genericFunctionFilter(input, context), Occur.FILTER)
-                .build();
+            return new BooleanQuery.Builder().add(notX, BooleanClause.Occur.MUST).add(
+                Queries.not(isNullFuncToQuery(arg, context)),
+                BooleanClause.Occur.MUST).build();
         } else {
             BooleanQuery.Builder builder = new BooleanQuery.Builder();
             builder.add(notX, BooleanClause.Occur.MUST);
