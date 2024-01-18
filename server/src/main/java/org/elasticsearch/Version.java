d99e40540b043c81aa13dc7d4763bcb0b9315d81
Michael Kleen <mkleen@gmail.com>
2024-01-18 16:58:47 +0100

    Release 5.6.0

--- server/src/main/java/org/elasticsearch/Version.java
+++ server/src/main/java/org/elasticsearch/Version.java
@@ -187,7 +187,7 @@ public class Version implements Comparable<Version>, ToXContentFragment {
     public static final Version V_5_5_2 = new Version(8_05_02_99, false, org.apache.lucene.util.Version.LUCENE_9_8_0);
     public static final Version V_5_5_3 = new Version(8_05_03_99, false, org.apache.lucene.util.Version.LUCENE_9_8_0);
 
-    public static final Version V_5_6_0 = new Version(8_06_00_99, true, org.apache.lucene.util.Version.LUCENE_9_9_1);
+    public static final Version V_5_6_0 = new Version(8_06_00_99, false, org.apache.lucene.util.Version.LUCENE_9_9_1);
 
     public static final Version CURRENT = V_5_6_0;
 
