8e71c42cb5926a4728627ce40e3c862922941225
Marios Trivyzas <matriv@gmail.com>
2024-01-29 16:28:31 +0200

    Revert "Normalize `<non_null_ref> is null` to `false`"
    This reverts commit 9c4876523ce39835eb22e0ec7fe8915d30e780bd.
    
    Follows: #15401
    
    The optimization doesn't handle properly the case of OUTER JOINS
    where we want data to be retrieved, produce `NULL` for the columns
    of the outer table, and be filtered out later on.
    
    (cherry picked from commit c1f4a8e48ad31b1eaf4febcf22c3e8bb1a6fd4ce)
    
    # Conflicts:
    #	docs/appendices/release-notes/5.6.1.rst
    #	server/src/test/java/io/crate/lucene/CommonQueryBuilderTest.java

--- server/src/testFixtures/java/io/crate/expression/scalar/ScalarTestCase.java
+++ server/src/testFixtures/java/io/crate/expression/scalar/ScalarTestCase.java
@@ -82,7 +82,6 @@ public abstract class ScalarTestCase extends CrateDummyClusterServiceUnitTest {
             "  tags array(text)," +
             "  age int," +
             "  a int," +
-            "  b int not null," +
             "  ip ip," +
             "  c byte," +
             "  x bigint," +
