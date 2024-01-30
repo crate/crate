crate/crate

commit 4428eb1555069f2325277ee99bd271af651b8425

Improve three-valued-logic processing for scalar function evalution
This improves the three-valued-logic (3vl) processing by grouping all
scalar functions into the three categories:

   - Nullable means the function returns a null value for a null argument.

   - Non-nullable means the function returns never a null values for null
     arguments.

   - Conditional means it follows it's own logic. By default each scalar
    function is conditional.

Based on these groups the generation of the Lucene queries for the
NotPredicate can be optimized in the following way:

    - If all involved scalar functions are nullable, no 3vl is required
      and the query will be negated and null values can be excluded by
      using a field exist query.

    - If all involved scalar functions are non-nullable or nullable,
      no 3vl is required and the query can be simply negated.

    - If any of the scalar function is conditional, 3vl is required,
      the query will be negated and further processed by converting
      the relevanting scalar function to generic function filters.

server/src/main/java/io/crate/expression/scalar/ConcatWsFunction.java
@@ -30,7 +30,6 @@ import io.crate.metadata.functions.Signature;
 import io.crate.types.DataTypes;
 
 public class ConcatWsFunction extends Scalar<String, String> {
-
     public static final String NAME = "concat_ws";
 
     public static void register(ScalarFunctionModule module) {
@@ -39,7 +38,9 @@ public class ConcatWsFunction extends Scalar<String, String> {
                 NAME,
                 DataTypes.STRING.getTypeSignature(),
                 DataTypes.STRING.getTypeSignature()
-            ).withVariableArity(),
+            )
+            .withFeature(Feature.NULLABLE)
+            .withVariableArity(),
             ConcatWsFunction::new
         );
     }

server/src/test/java/io/crate/lucene/ThreeValuedLogicQueryBuilderTest.java
@@ -70,13 +70,13 @@ public class ThreeValuedLogicQueryBuilderTest extends LuceneQueryBuilderTest {
     @Test
     public void test_negated_concat_with_three_valued_logic() {
         assertThat(convert("NOT (x || 1) < -1")).hasToString(
-            "+(+*:* -(concat(x, '1') < -1)) #(NOT (concat(x, '1') < -1))");
+            "+(+*:* -(concat(x, '1') < -1))");
     }
 
     @Test
     public void test_nullif() {
         assertThat(convert("NULLIF(2, x) != 1")).hasToString(
-            "+(+*:* -(nullif(2, x) = 1)) #(NOT (nullif(2, x) = 1))");
+            "+(+*:* -(nullif(2, x) = 1))");
     }
 
     @Test
@@ -84,5 +84,4 @@ public class ThreeValuedLogicQueryBuilderTest extends LuceneQueryBuilderTest {
         assertThat(convert("NOT (x OR y) > true")).hasToString(
             "+(+*:* -((x OR y) > true)) #(NOT ((x OR y) > true))");
     }
-
 }

commit 45a058382af92e2fb744798d09722e4230ec5a0e

Don't mark concat_ws as Nullable
Otherwise FieldExistsQuery breaks the query for cases when column contains NULL-s.
Semantics of that optimization works only for functions with semantics
"at least one arg is NULL -> return NULL" and concat_ws is not like that.

Fixes a regression, introduced by https://github.com/crate/crate/commit/4428eb1555069f2325277ee99bd271af651b8425

server/src/main/java/io/crate/expression/scalar/ConcatWsFunction.java
@@ -39,7 +39,6 @@ public class ConcatWsFunction extends Scalar<String, String> {
                 DataTypes.STRING.getTypeSignature(),
                 DataTypes.STRING.getTypeSignature()
             )
-            .withFeature(Feature.NULLABLE)
             .withVariableArity(),
             ConcatWsFunction::new
         );

server/src/test/java/io/crate/lucene/ThreeValuedLogicQueryBuilderTest.java
@@ -74,6 +74,13 @@ public class ThreeValuedLogicQueryBuilderTest extends LuceneQueryBuilderTest {
     }
 
     @Test
+    public void test_negated_concat_ws_with_three_valued_logic() {
+        // Important, y is nullable column.
+        assertThat(convert("NOT CONCAT_WS('dummy', y, false)")).hasToString(
+            "+(+*:* -concat_ws('dummy', y, 'f')) #(NOT concat_ws('dummy', y, 'f'))");
+    }
+
+    @Test
     public void test_nullif() {
         assertThat(convert("NULLIF(2, x) != 1")).hasToString(
             "+(+*:* -(nullif(2, x) = 1)) #(NOT (nullif(2, x) = 1))");