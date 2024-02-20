05f12adf7bf263ffb47b1c236d6b91c4496c954e
Michael Kleen <mkleen@gmail.com>
2024-02-20 21:07:58 +0100

    Update to Lucene 9.10.0
    https://lucene.apache.org/core/9_10_0/changes/Changes.html

--- server/src/main/java/org/elasticsearch/index/engine/TranslogLeafReader.java
+++ server/src/main/java/org/elasticsearch/index/engine/TranslogLeafReader.java
@@ -57,10 +57,10 @@ final class TranslogLeafReader extends LeafReader {
     private final Translog.Index operation;
     private static final FieldInfo FAKE_SOURCE_FIELD
         = new FieldInfo(SourceFieldMapper.NAME, 1, false, false, false, IndexOptions.NONE, DocValuesType.NONE, -1, Collections.emptyMap(),
-        0, 0, 0, 0, VectorEncoding.BYTE, VectorSimilarityFunction.EUCLIDEAN, false);
+        0, 0, 0, 0, VectorEncoding.BYTE, VectorSimilarityFunction.EUCLIDEAN, false, false);
     private static final FieldInfo FAKE_ID_FIELD
         = new FieldInfo(IdFieldMapper.NAME, 3, false, false, false, IndexOptions.NONE, DocValuesType.NONE, -1, Collections.emptyMap(),
-        0, 0, 0, 0, VectorEncoding.BYTE, VectorSimilarityFunction.EUCLIDEAN, false);
+        0, 0, 0, 0, VectorEncoding.BYTE, VectorSimilarityFunction.EUCLIDEAN, false, false);
 
     TranslogLeafReader(Translog.Index operation) {
         this.operation = operation;
