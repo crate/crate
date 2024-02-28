crate/crate

commit 71110d8c012ddc2a8abc736ce102cca57b19bdc6

Add information_schema.user_mapping_options

server/src/main/java/io/crate/fdw/JdbcForeignDataWrapper.java
@@ -27,7 +27,6 @@ import java.net.URISyntaxException;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.CompletableFuture;
 
@@ -49,7 +48,6 @@ import io.crate.metadata.RelationName;
 import io.crate.metadata.TransactionContext;
 import io.crate.metadata.settings.SessionSettings;
 import io.crate.role.Role;
-import io.crate.types.DataTypes;
 
 final class JdbcForeignDataWrapper implements ForeignDataWrapper {
 
@@ -71,7 +69,6 @@ final class JdbcForeignDataWrapper implements ForeignDataWrapper {
         foreignUser,
         foreignPw
     );
-
 
     JdbcForeignDataWrapper(Settings settings, InputFactory inputFactory) {
         this.settings = settings;
@@ -100,15 +97,15 @@ final class JdbcForeignDataWrapper implements ForeignDataWrapper {
                                                              TransactionContext txnCtx,
                                                              List<Symbol> collect) {
         SessionSettings sessionSettings = txnCtx.sessionSettings();
-        Map<String, Object> userOptions = server.users().get(sessionSettings.userName());
+        Settings userOptions = server.users().get(sessionSettings.userName());
         if (userOptions == null) {
-            userOptions = Map.of();
+            userOptions = Settings.EMPTY;
         }
-        String user = DataTypes.STRING.implicitCast(userOptions.get("user"));
-        String password = DataTypes.STRING.implicitCast(userOptions.get("password"));
+        String user = foreignUser.get(userOptions);
+        String password = foreignPw.get(userOptions);
         var properties = new Properties();
-        properties.setProperty("user", user == null ? sessionSettings.userName() : user);
-        if (password != null) {
+        properties.setProperty("user", user.isEmpty() ? sessionSettings.userName() : user);
+        if (password.isEmpty()) {
             properties.setProperty("password", password);
         }
 

server/src/main/java/io/crate/execution/engine/collect/sources/InformationSchemaIterables.java
@@ -67,6 +67,7 @@ import io.crate.metadata.RoutineInfos;
 import io.crate.metadata.Schemas;
 import io.crate.metadata.blob.BlobSchemaInfo;
 import io.crate.metadata.information.InformationSchemaInfo;
+import io.crate.metadata.information.UserMappingOptionsTableInfo;
 import io.crate.metadata.information.UserMappingsTableInfo.UserMapping;
 import io.crate.metadata.pgcatalog.OidHash;
 import io.crate.metadata.pgcatalog.PgCatalogSchemaInfo;
@@ -375,6 +376,12 @@ public class InformationSchemaIterables implements ClusterStateListener {
         Metadata metadata = clusterService.state().metadata();
         ServersMetadata servers = metadata.custom(ServersMetadata.TYPE, ServersMetadata.EMPTY);
         return servers.getUserMappings();
+    }
+
+    public Iterable<UserMappingOptionsTableInfo.UserMappingOptions> userMappingOptions() {
+        Metadata metadata = clusterService.state().metadata();
+        ServersMetadata servers = metadata.custom(ServersMetadata.TYPE, ServersMetadata.EMPTY);
+        return servers.getUserMappingOptions();
     }
 
     public Iterable<ForeignTable> foreignTables() {

server/src/main/java/io/crate/fdw/CreateUserMappingRequest.java
@@ -22,23 +22,23 @@
 package io.crate.fdw;
 
 import java.io.IOException;
-import java.util.Map;
 
 import org.elasticsearch.action.support.master.AcknowledgedRequest;
 import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.io.stream.StreamOutput;
+import org.elasticsearch.common.settings.Settings;
 
 public class CreateUserMappingRequest extends AcknowledgedRequest<CreateUserMappingRequest> {
 
     private final boolean ifNotExists;
     private final String userName;
     private final String server;
-    private final Map<String, Object> options;
+    private final Settings options;
 
     public CreateUserMappingRequest(boolean ifNotExists,
                                     String userName,
                                     String server,
-                                    Map<String, Object> options) {
+                                    Settings options) {
         this.ifNotExists = ifNotExists;
         this.userName = userName;
         this.server = server;
@@ -49,7 +49,7 @@ public class CreateUserMappingRequest extends AcknowledgedRequest<CreateUserMapp
         this.ifNotExists = in.readBoolean();
         this.userName = in.readString();
         this.server = in.readString();
-        this.options = in.readMap(StreamInput::readString, StreamInput::readGenericValue);
+        this.options = Settings.readSettingsFromStream(in);
     }
 
     @Override
@@ -57,7 +57,7 @@ public class CreateUserMappingRequest extends AcknowledgedRequest<CreateUserMapp
         out.writeBoolean(ifNotExists);
         out.writeString(userName);
         out.writeString(server);
-        out.writeMap(options, StreamOutput::writeString, StreamOutput::writeGenericValue);
+        Settings.writeSettingsToStream(out, options);
     }
 
     public boolean ifNotExists() {
@@ -72,7 +72,7 @@ public class CreateUserMappingRequest extends AcknowledgedRequest<CreateUserMapp
         return server;
     }
 
-    public Map<String, Object> options() {
+    public Settings options() {
         return options;
     }
 }

server/src/main/java/io/crate/fdw/ServersMetadata.java
@@ -50,7 +50,9 @@ import org.elasticsearch.common.xcontent.XContentParser;
 
 import io.crate.fdw.ServersMetadata.Server;
 import io.crate.fdw.ServersMetadata.Server.Option;
+import io.crate.metadata.information.UserMappingOptionsTableInfo;
 import io.crate.metadata.information.UserMappingsTableInfo.UserMapping;
+import io.crate.server.xcontent.XContentParserUtils;
 import io.crate.sql.tree.CascadeMode;
 import io.crate.types.DataTypes;
 
@@ -63,7 +65,7 @@ public final class ServersMetadata extends AbstractNamedDiffable<Metadata.Custom
     public record Server(String name,
                          String fdw,
                          String owner,
-                         Map<String, Map<String, Object>> users,
+                         Map<String, Settings> users,
                          Settings options) implements Writeable, ToXContent {
 
 
@@ -72,7 +74,7 @@ public final class ServersMetadata extends AbstractNamedDiffable<Metadata.Custom
                 in.readString(),
                 in.readString(),
                 in.readString(),
-                in.readMap(StreamInput::readString, StreamInput::readMap),
+                in.readMap(StreamInput::readString, Settings::readSettingsFromStream),
                 Settings.readSettingsFromStream(in)
             );
         }
@@ -82,7 +84,7 @@ public final class ServersMetadata extends AbstractNamedDiffable<Metadata.Custom
             out.writeString(name);
             out.writeString(fdw);
             out.writeString(owner);
-            out.writeMap(users, StreamOutput::writeString, StreamOutput::writeMap);
+            out.writeMap(users, StreamOutput::writeString, Settings::writeSettingsToStream);
             Settings.writeSettingsToStream(out, options);
         }
 
@@ -91,7 +93,7 @@ public final class ServersMetadata extends AbstractNamedDiffable<Metadata.Custom
             String name = null;
             String fdw = null;
             String owner = null;
-            Map<String, Map<String, Object>> users = null;
+            Map<String, Settings> users = new HashMap<>();
             Settings options = null;
             while (parser.nextToken() != END_OBJECT) {
                 if (parser.currentToken() == FIELD_NAME) {
@@ -111,7 +113,17 @@ public final class ServersMetadata extends AbstractNamedDiffable<Metadata.Custom
                             break;
 
                         case "users":
-                            users = (Map<String, Map<String, Object>>)(Map) parser.map();
+                            XContentParserUtils.ensureExpectedToken(START_OBJECT, parser.currentToken(), parser);
+                            parser.nextToken();
+                            while (parser.currentToken() == FIELD_NAME) {
+                                parser.nextToken();
+                                String key = parser.currentName();
+                                Settings settings = Settings.fromXContent(parser);
+                                users.put(key, settings);
+                                XContentParserUtils.ensureExpectedToken(END_OBJECT, parser.currentToken(), parser);
+                                parser.nextToken();
+                            }
+                            XContentParserUtils.ensureExpectedToken(END_OBJECT, parser.currentToken(), parser);
                             break;
 
                         case "options":
@@ -137,10 +149,19 @@ public final class ServersMetadata extends AbstractNamedDiffable<Metadata.Custom
             builder.field("name", name);
             builder.field("fdw", fdw);
             builder.field("owner", owner);
-            builder.field("users", users);
+
+            builder.startObject("users");
+            for (var user : users.entrySet()) {
+                builder.startObject(user.getKey());
+                user.getValue().toXContent(builder, params);
+                builder.endObject();
+            }
+            builder.endObject();
+
             builder.startObject("options");
             options.toXContent(builder, params);
             builder.endObject();
+
             return builder;
         }
 
@@ -223,7 +244,7 @@ public final class ServersMetadata extends AbstractNamedDiffable<Metadata.Custom
     public ServersMetadata addUser(String serverName,
                                    boolean ifNotExists,
                                    String userName,
-                                   Map<String, Object> options) {
+                                   Settings options) {
         Server server = get(serverName);
         if (server.users.containsKey(userName)) {
             if (ifNotExists) {
@@ -232,7 +253,7 @@ public final class ServersMetadata extends AbstractNamedDiffable<Metadata.Custom
             throw new UserMappingAlreadyExists(userName, serverName);
         }
         HashMap<String, Server> newServers = new HashMap<>(this.servers);
-        HashMap<String, Map<String, Object>> newUsers = new HashMap<>(server.users);
+        HashMap<String, Settings> newUsers = new HashMap<>(server.users);
         newUsers.put(userName, options);
         Server newServer = new Server(serverName, server.fdw, server.owner, newUsers, server.options);
         newServers.put(serverName, newServer);
@@ -300,8 +321,8 @@ public final class ServersMetadata extends AbstractNamedDiffable<Metadata.Custom
 
     public ServersMetadata dropUser(String serverName, String userName, boolean ifExists) {
         Server server = get(serverName);
-        HashMap<String, Map<String, Object>> newUsers = new HashMap<>(server.users);
-        Map<String, Object> removed = newUsers.remove(userName);
+        HashMap<String, Settings> newUsers = new HashMap<>(server.users);
+        Settings removed = newUsers.remove(userName);
         if (removed == null && !ifExists) {
             throw new ResourceNotFoundException(String.format(
                 Locale.ENGLISH,
@@ -337,6 +358,17 @@ public final class ServersMetadata extends AbstractNamedDiffable<Metadata.Custom
                 .flatMap(server -> server.users.keySet().stream()
                     .map(userName -> new UserMapping(userName, server.name()))
                 ).iterator();
+    }
+
+    public Iterable<UserMappingOptionsTableInfo.UserMappingOptions> getUserMappingOptions() {
+        return () ->
+            servers.values().stream()
+                .map(server -> server)
+                .flatMap(server -> server.users.entrySet().stream()
+                    .flatMap(e -> e.getValue().getAsStructuredMap().entrySet().stream()
+                        .map(setting -> new UserMappingOptionsTableInfo.UserMappingOptions(
+                            e.getKey(), server.name(), setting.getKey(), setting.getValue().toString())
+                ))).iterator();
     }
 
     public Iterable<Option> getOptions() {

server/src/main/java/io/crate/metadata/information/InformationSchemaInfo.java
@@ -59,6 +59,7 @@ public class InformationSchemaInfo implements SchemaInfo {
             .put(ForeignTableTableInfo.NAME, ForeignTableTableInfo.create())
             .put(ForeignTableOptionsTableInfo.NAME, ForeignTableOptionsTableInfo.create())
             .put(UserMappingsTableInfo.NAME, UserMappingsTableInfo.create())
+            .put(UserMappingOptionsTableInfo.NAME, UserMappingOptionsTableInfo.create())
             .immutableMap();
     }
 

server/src/main/java/io/crate/metadata/information/InformationSchemaTableDefinitions.java
@@ -26,6 +26,7 @@ import static java.util.concurrent.CompletableFuture.completedFuture;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.StreamSupport;
 
 import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.common.inject.Singleton;
@@ -128,6 +129,25 @@ public class InformationSchemaTableDefinitions {
             informationSchemaIterables::userMappings,
             (user, t) -> roles.hasPrivilege(user, Permission.AL, Securable.CLUSTER, null),
             UserMappingsTableInfo.create().expressions()
+        ));
+        tableDefinitions.put(UserMappingOptionsTableInfo.IDENT, new StaticTableDefinition<>(
+            (txnCtx, user) -> completedFuture(
+                () -> StreamSupport.stream(informationSchemaIterables.userMappingOptions().spliterator(), false)
+                    .filter(userMappingOptions -> roles.hasPrivilege(user, Permission.AL, Securable.CLUSTER, null))
+                    .map(userMappingOptions -> {
+                        if ((user.name().equals(userMappingOptions.userName()) || user.isSuperUser()) == false) {
+                            return new UserMappingOptionsTableInfo.UserMappingOptions(
+                                userMappingOptions.userName(),
+                                userMappingOptions.serverName(),
+                                userMappingOptions.optionName(),
+                                null); // mask
+                        }
+                        return userMappingOptions;
+                    })
+                    .iterator()
+            ),
+            UserMappingOptionsTableInfo.create().expressions(),
+            true
         ));
     }
 

server/src/main/java/io/crate/metadata/information/UserMappingOptionsTableInfo.java
@@ -1,0 +1,46 @@
+/*
+ * Licensed to Crate.io GmbH ("Crate") under one or more contributor
+ * license agreements.  See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.  Crate licenses
+ * this file to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.  You may
+ * obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * However, if you have executed another commercial license agreement
+ * with Crate these terms will supersede the license and you may use the
+ * software solely pursuant to the terms of the relevant commercial agreement.
+ */
+
+package io.crate.metadata.information;
+
+import io.crate.Constants;
+import io.crate.metadata.RelationName;
+import io.crate.metadata.SystemTable;
+import io.crate.types.DataTypes;
+
+public class UserMappingOptionsTableInfo {
+
+    public static final String NAME = "user_mapping_options";
+    public static final RelationName IDENT = new RelationName(InformationSchemaInfo.NAME, NAME);
+
+    public record UserMappingOptions(String userName, String serverName, String optionName, String optionValue) {
+    }
+
+    public static SystemTable<UserMappingOptions> create() {
+        return SystemTable.<UserMappingOptions>builder(IDENT)
+            .add("authorization_identifier", DataTypes.STRING, UserMappingOptions::userName)
+            .add("foreign_server_catalog", DataTypes.STRING, ignored -> Constants.DB_NAME)
+            .add("foreign_server_name", DataTypes.STRING, UserMappingOptions::serverName)
+            .add("option_name", DataTypes.STRING, UserMappingOptions::optionName)
+            .add("option_value", DataTypes.STRING, UserMappingOptions::optionValue)
+            .build();
+    }
+}

server/src/main/java/io/crate/planner/CreateUserMappingPlan.java
@@ -22,13 +22,11 @@
 package io.crate.planner;
 
 import java.util.Locale;
-import java.util.Map;
-import java.util.Map.Entry;
 import java.util.function.Function;
-import java.util.stream.Collectors;
 
 import org.elasticsearch.cluster.metadata.Metadata;
 import org.elasticsearch.common.settings.Setting;
+import org.elasticsearch.common.settings.Settings;
 
 import io.crate.analyze.AnalyzedCreateUserMapping;
 import io.crate.analyze.SymbolEvaluator;
@@ -69,32 +67,33 @@ public class CreateUserMappingPlan implements Plan {
         ServersMetadata serversMetadata = metadata.custom(ServersMetadata.TYPE, ServersMetadata.EMPTY);
         ServersMetadata.Server server = serversMetadata.get(createUserMapping.serverName());
         var supportedUserOptions = Lists.map(foreignDataWrappers.get(server.fdw()).optionalUserOptions(), Setting::getKey);
-        createUserMapping.options().keySet().forEach(
-            option -> {
-                if (!supportedUserOptions.contains(option.toLowerCase(Locale.ROOT))) {
-                    throw new IllegalArgumentException(
-                        String.format(
-                            Locale.ENGLISH,
-                            "Invalid option '%s' provided, the supported options are: %s",
-                            option,
-                            supportedUserOptions)
-                    );
-                }
-            });
 
         Function<Symbol, Object> toValue = new SymbolEvaluator(
             plannerContext.transactionContext(),
             plannerContext.nodeContext(),
             subQueryResults
         ).bind(params);
-        Map<String, Object> options = createUserMapping.options().entrySet().stream()
-            .collect(Collectors.toMap(Entry::getKey, entry -> toValue.apply(entry.getValue())));
+        Settings.Builder optionsBuilder = Settings.builder();
+        createUserMapping.options().forEach((optionName, option) -> {
+            optionName = optionName.toLowerCase(Locale.ROOT);
+            if (supportedUserOptions.contains(optionName)) {
+                optionsBuilder.put(optionName, toValue.apply(option));
+            } else {
+                throw new IllegalArgumentException(
+                        String.format(
+                                Locale.ENGLISH,
+                                "Invalid option '%s' provided, the supported options are: %s",
+                                optionName,
+                                supportedUserOptions)
+                );
+            }
+        });
 
         var request = new CreateUserMappingRequest(
             createUserMapping.ifNotExists(),
             createUserMapping.user().name(),
             createUserMapping.serverName(),
-            options
+            optionsBuilder.build()
         );
         dependencies.client()
             .execute(TransportCreateUserMappingAction.ACTION, request)

commit c8d72e8a0bc08d27049f80c503507e1edd4ea990

Fix a bug introduced by #15591

server/src/main/java/io/crate/fdw/JdbcForeignDataWrapper.java
@@ -105,7 +105,7 @@ final class JdbcForeignDataWrapper implements ForeignDataWrapper {
         String password = foreignPw.get(userOptions);
         var properties = new Properties();
         properties.setProperty("user", user.isEmpty() ? sessionSettings.userName() : user);
-        if (password.isEmpty()) {
+        if (!password.isEmpty()) {
             properties.setProperty("password", password);
         }
 