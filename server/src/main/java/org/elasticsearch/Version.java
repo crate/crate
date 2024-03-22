cccdf780837799780dcf3bba6f5140fdceb7a55a
baur <baurzhansahariev@gmail.com>
2024-03-22 11:06:08 +0100

    Reflect release 5.6.3

--- server/src/main/java/org/elasticsearch/Version.java
+++ server/src/main/java/org/elasticsearch/Version.java
@@ -193,6 +193,7 @@ public class Version implements Comparable<Version>, ToXContentFragment {
     public static final Version V_5_6_0 = new Version(8_06_00_99, false, org.apache.lucene.util.Version.LUCENE_9_9_1);
     public static final Version V_5_6_1 = new Version(8_06_01_99, false, org.apache.lucene.util.Version.LUCENE_9_9_1);
     public static final Version V_5_6_2 = new Version(8_06_02_99, false, org.apache.lucene.util.Version.LUCENE_9_9_1);
+    public static final Version V_5_6_3 = new Version(8_06_03_99, false, org.apache.lucene.util.Version.LUCENE_9_9_1);
 
     public static final Version V_5_7_0 = new Version(8_07_00_99, true, org.apache.lucene.util.Version.LUCENE_9_10_0);
 
