e5abd82d8776c1e353bfbc14f9d2bb09c9d66fd3
Marios Trivyzas <matriv@gmail.com>
2024-04-18 18:47:45 +0300

    Rename NodeStatsTest to NodeStatsITest

--- server/src/test/java/io/crate/integrationtests/NodeStatsITest.java
+++ server/src/test/java/io/crate/integrationtests/NodeStatsITest.java
@@ -39,7 +39,7 @@ import io.crate.testing.SQLResponse;
 import io.crate.testing.UseJdbc;
 
 @IntegTestCase.ClusterScope(numClientNodes = 0, numDataNodes = 2, supportsDedicatedMasters = false)
-public class NodeStatsTest extends IntegTestCase {
+public class NodeStatsITest extends IntegTestCase {
 
     @Test
     public void testSysNodesMem() throws Exception {
