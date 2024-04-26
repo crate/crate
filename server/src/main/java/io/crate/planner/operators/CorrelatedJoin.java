7db76a2c195c725d9dc2e76aca19c2ddd426bc21
baur <baurzhansahariev@gmail.com>
2024-04-26 14:35:52 +0200

    Update outputs of the CorrelatedJoin after the prunning

--- server/src/main/java/io/crate/planner/operators/CorrelatedJoin.java
+++ server/src/main/java/io/crate/planner/operators/CorrelatedJoin.java
@@ -21,6 +21,7 @@
 
 package io.crate.planner.operators;
 
+import java.util.ArrayList;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
@@ -162,12 +163,18 @@ public class CorrelatedJoin implements LogicalPlan {
         if (inputPlan == newInputPlan) {
             return this;
         }
+        List<Symbol> newOutputs = new ArrayList<>();
+        for (Symbol output: outputs) {
+            if (toCollect.contains(output)) {
+                newOutputs.add(output);
+            }
+        }
 
         return new CorrelatedJoin(
             newInputPlan,
             selectSymbol,
             subQueryPlan,
-            outputs
+            newOutputs
         );
     }
 
