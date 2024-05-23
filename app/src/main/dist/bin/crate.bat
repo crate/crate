bd1f808c2c19b5207f91315d87298e1c3c149c53
baur <baurzhansahariev@gmail.com>
2024-05-23 14:13:39 +0200

    Set JVM variable rejectClientInitiatedRenegotiation to true by default
    Mitigates https://nvd.nist.gov/vuln/detail/cve-2021-3449

--- app/src/main/dist/bin/crate.bat
+++ app/src/main/dist/bin/crate.bat
@@ -55,7 +55,7 @@ REM Enable aggressive optimizations in the JVM
 REM    - Disabled by default as it might cause the JVM to crash
 REM set JAVA_OPTS=%JAVA_OPTS% -XX:+AggressiveOpts
 
-set JAVA_OPTS=%JAVA_OPTS% -XX:+UseG1GC -XX:G1ReservePercent=25 -XX:InitiatingHeapOccupancyPercent=30
+set JAVA_OPTS=%JAVA_OPTS% -XX:+UseG1GC -XX:G1ReservePercent=25 -XX:InitiatingHeapOccupancyPercent=30 -Djdk.tls.rejectClientInitiatedRenegotiation=true
 
 REM GC logging default values
 SET GC_LOG_DIR=%CRATE_HOME%\logs
