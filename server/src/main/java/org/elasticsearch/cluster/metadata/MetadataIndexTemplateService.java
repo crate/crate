7f40bb4c621df52f5758ea967a601c9b4848fae6
Mathias Fussenegger <f.mathias@zignar.net>
2024-05-27 09:42:47 +0200

    Specialize putTemplate for CrateDB table use-case

--- server/src/main/java/org/elasticsearch/cluster/metadata/MetadataIndexTemplateService.java
+++ server/src/main/java/org/elasticsearch/cluster/metadata/MetadataIndexTemplateService.java
@@ -26,14 +26,12 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.elasticsearch.Version;
 import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.action.admin.indices.alias.Alias;
-import org.elasticsearch.action.support.master.MasterNodeRequest;
 import org.elasticsearch.cluster.ClusterState;
 import org.elasticsearch.cluster.ClusterStateUpdateTask;
 import org.elasticsearch.cluster.service.ClusterService;
@@ -50,11 +48,8 @@ import org.elasticsearch.common.xcontent.json.JsonXContent;
 import org.elasticsearch.index.Index;
 import org.elasticsearch.index.IndexService;
 import org.elasticsearch.index.mapper.MapperParsingException;
-import org.elasticsearch.index.mapper.MapperService;
-import org.elasticsearch.index.mapper.MapperService.MergeReason;
 import org.elasticsearch.indices.IndicesService;
 import org.elasticsearch.indices.InvalidIndexTemplateException;
-import org.jetbrains.annotations.Nullable;
 
 import io.crate.Constants;
 import io.crate.common.unit.TimeValue;
@@ -62,10 +57,12 @@ import io.crate.execution.ddl.tables.CreateTableRequest;
 import io.crate.execution.ddl.tables.CreateTableResponse;
 import io.crate.execution.ddl.tables.MappingUtil;
 import io.crate.metadata.DocReferences;
+import io.crate.metadata.PartitionName;
 import io.crate.metadata.Reference;
+import io.crate.metadata.RelationName;
 
 /**
- * Service responsible for submitting index templates updates
+ * Service responsible for creating templates
  */
 public class MetadataIndexTemplateService {
 
@@ -90,38 +87,34 @@ public class MetadataIndexTemplateService {
         this.xContentRegistry = xContentRegistry;
     }
 
-    public void putTemplate(final PutRequest request,
-                            @Nullable final CreateTableRequest createTableRequest,
+    public void putTemplate(CreateTableRequest request,
                             ActionListener<CreateTableResponse> listener) {
         Settings.Builder updatedSettingsBuilder = Settings.builder();
-        updatedSettingsBuilder.put(request.settings).normalizePrefix(IndexMetadata.INDEX_SETTING_PREFIX);
-        request.settings(updatedSettingsBuilder.build());
+        updatedSettingsBuilder.put(request.settings()).normalizePrefix(IndexMetadata.INDEX_SETTING_PREFIX);
+        Settings settings = updatedSettingsBuilder.build();
+        indexScopedSettings.validate(settings, true);
 
-        if (request.name == null) {
-            listener.onFailure(new IllegalArgumentException("index_template must provide a name"));
-            return;
-        }
-        if (request.indexPatterns == null) {
-            listener.onFailure(new IllegalArgumentException("index_template must provide a template"));
-            return;
-        }
+        RelationName relationName = request.getTableName();
+        String templateName = PartitionName.templateName(relationName.schema(), relationName.name());
+        String templatePrefix = PartitionName.templatePrefix(relationName.schema(), relationName.name());
+        Alias alias = new Alias(relationName.indexNameOrAlias());
 
         try {
-            validate(request);
+            validate(templateName, templatePrefix, settings, alias);
         } catch (Exception e) {
             listener.onFailure(e);
             return;
         }
 
-        final IndexTemplateMetadata.Builder templateBuilder = IndexTemplateMetadata.builder(request.name);
+        final IndexTemplateMetadata.Builder templateBuilder = IndexTemplateMetadata.builder(templateName);
 
         clusterService.submitStateUpdateTask(
-            "create-index-template [" + request.name + "], cause [" + request.cause + "]",
+            "create-index-template [" + templateName + "], cause [create table]",
             new ClusterStateUpdateTask(Priority.URGENT) {
 
                 @Override
                 public TimeValue timeout() {
-                    return request.masterTimeout;
+                    return request.masterNodeTimeout();
                 }
 
                 @Override
@@ -131,21 +124,25 @@ public class MetadataIndexTemplateService {
 
                 @Override
                 public ClusterState execute(ClusterState currentState) throws Exception {
-                    if (request.create && currentState.metadata().templates().containsKey(request.name)) {
-                        throw new IllegalArgumentException("index_template [" + request.name + "] already exists");
+                    if (currentState.metadata().templates().containsKey(templateName)) {
+                        throw new IllegalArgumentException("index_template [" + templateName + "] already exists");
                     }
                     Metadata.Builder metadataBuilder = Metadata.builder(currentState.metadata());
-                    validateAndAddTemplate(request, createTableRequest, metadataBuilder, templateBuilder, indicesService, xContentRegistry, currentState);
+                    validateAndAddTemplate(
+                        request,
+                        templatePrefix,
+                        settings,
+                        metadataBuilder,
+                        templateBuilder,
+                        indicesService,
+                        xContentRegistry,
+                        currentState
+                    );
 
-                    for (Alias alias : request.aliases) {
-                        AliasMetadata aliasMetadata = new AliasMetadata(alias.name());
-                        templateBuilder.putAlias(aliasMetadata);
-                    }
-                    IndexTemplateMetadata template = templateBuilder.build();
+                    templateBuilder.putAlias(new AliasMetadata(alias.name()));
+                    metadataBuilder.put(templateBuilder);
 
-                    metadataBuilder.put(template);
-
-                    LOGGER.info("adding template [{}] for index patterns {}", request.name, request.indexPatterns);
+                    LOGGER.info("adding template [{}] for index pattern {}", templateName, templatePrefix);
                     return ClusterState.builder(currentState).metadata(metadataBuilder).build();
                 }
 
@@ -157,8 +154,9 @@ public class MetadataIndexTemplateService {
         );
     }
 
-    private static void validateAndAddTemplate(final PutRequest request,
-                                               @Nullable final CreateTableRequest createTableRequest,
+    private static void validateAndAddTemplate(CreateTableRequest createTableRequest,
+                                               String templatePrefix,
+                                               Settings settings,
                                                Metadata.Builder metadataBuilder,
                                                IndexTemplateMetadata.Builder templateBuilder,
                                                IndicesService indicesService,
@@ -168,14 +166,14 @@ public class MetadataIndexTemplateService {
         final String temporaryIndexName = UUIDs.randomBase64UUID();
         try {
             // use the provided values, otherwise just pick valid dummy values
-            int dummyPartitionSize = IndexMetadata.INDEX_ROUTING_PARTITION_SIZE_SETTING.get(request.settings);
-            int dummyShards = request.settings.getAsInt(IndexMetadata.SETTING_NUMBER_OF_SHARDS,
+            int dummyPartitionSize = IndexMetadata.INDEX_ROUTING_PARTITION_SIZE_SETTING.get(settings);
+            int dummyShards = settings.getAsInt(IndexMetadata.SETTING_NUMBER_OF_SHARDS,
                     dummyPartitionSize == 1 ? 1 : dummyPartitionSize + 1);
 
             //create index service for parsing and validating "mappings"
             Settings dummySettings = Settings.builder()
                 .put(IndexMetadata.SETTING_VERSION_CREATED, Version.CURRENT)
-                .put(request.settings)
+                .put(settings)
                 .put(IndexMetadata.SETTING_NUMBER_OF_SHARDS, dummyShards)
                 .put(IndexMetadata.SETTING_NUMBER_OF_REPLICAS, 0)
                 .put(IndexMetadata.SETTING_INDEX_UUID, UUIDs.randomBase64UUID())
@@ -185,47 +183,30 @@ public class MetadataIndexTemplateService {
             IndexService dummyIndexService = indicesService.createIndex(tmpIndexMetadata, Collections.emptyList(), false);
             createdIndex = dummyIndexService.index();
 
-            templateBuilder.version(request.version);
-            templateBuilder.patterns(request.indexPatterns);
+            templateBuilder.version(null);
+            templateBuilder.patterns(List.of(templatePrefix));
 
             // inject `index.version.created` to the template settings to flag version of template creation (partitioned table)
-            var templateSettingsBuilder = Settings.builder().put(request.settings);
+            var templateSettingsBuilder = Settings.builder().put(settings);
             setIndexVersionCreatedSetting(templateSettingsBuilder, currentState);
 
             templateBuilder.settings(templateSettingsBuilder.build());
-            if (createTableRequest != null) {
-                // New code path where we create mapping at the last stage and assign OID in the same cluster state update with template creation.
-                List<Reference> references = DocReferences.applyOid(
-                        createTableRequest.references(),
-                        metadataBuilder.columnOidSupplier()
-                );
-                var mapping = MappingUtil.createMapping(
-                    MappingUtil.AllocPosition.forNewTable(),
-                    createTableRequest.pkConstraintName(),
-                    references,
-                    createTableRequest.pKeyIndices(),
-                    createTableRequest.checkConstraints(),
-                    createTableRequest.partitionedBy(),
-                    createTableRequest.tableColumnPolicy(),
-                    createTableRequest.routingColumn()
-                );
-                mapping = Map.of(Constants.DEFAULT_MAPPING_TYPE, mapping); // We used PutIndexTemplateRequest.mapping which wraps mapping with default type
-                try {
-                    templateBuilder.putMapping(new CompressedXContent(Strings.toString(JsonXContent.builder().map(mapping))));
-                } catch (Exception e) {
-                    throw new MapperParsingException("Failed to parse mapping: {}", e, e.getMessage());
-                }
-                dummyIndexService.mapperService().merge(mapping, MergeReason.MAPPING_UPDATE);
-            } else {
-                // BWC code path. Do not remove in 5.5 or later since it's used on upgrade as well.
-                if (request.mapping != null) {
-                    try {
-                        templateBuilder.putMapping(request.mapping);
-                    } catch (Exception e) {
-                        throw new MapperParsingException("Failed to parse mapping: {}", e, e.getMessage());
-                    }
-                    dummyIndexService.mapperService().merge(MapperService.parseMapping(xContentRegistry, request.mapping), MergeReason.MAPPING_UPDATE);
-                }
+            List<Reference> references = DocReferences.applyOid(createTableRequest.references(), metadataBuilder.columnOidSupplier());
+            var mapping = MappingUtil.createMapping(
+                MappingUtil.AllocPosition.forNewTable(),
+                createTableRequest.pkConstraintName(),
+                references,
+                createTableRequest.pKeyIndices(),
+                createTableRequest.checkConstraints(),
+                createTableRequest.partitionedBy(),
+                createTableRequest.tableColumnPolicy(),
+                createTableRequest.routingColumn()
+            );
+            mapping = Map.of(Constants.DEFAULT_MAPPING_TYPE, mapping); // We used PutIndexTemplateRequest.mapping which wraps mapping with default type
+            try {
+                templateBuilder.putMapping(new CompressedXContent(Strings.toString(JsonXContent.builder().map(mapping))));
+            } catch (Exception e) {
+                throw new MapperParsingException("Failed to parse mapping: {}", e, e.getMessage());
             }
         } finally {
             if (createdIndex != null) {
@@ -234,108 +215,51 @@ public class MetadataIndexTemplateService {
         }
     }
 
-    private void validate(PutRequest request) {
+    private void validate(String templateName, String templatePrefix, Settings settings, Alias alias) {
         List<String> validationErrors = new ArrayList<>();
-        if (request.name.contains(" ")) {
+        if (templateName.contains(" ")) {
             validationErrors.add("name must not contain a space");
         }
-        if (request.name.contains(",")) {
+        if (templateName.contains(",")) {
             validationErrors.add("name must not contain a ','");
         }
-        if (request.name.contains("#")) {
+        if (templateName.contains("#")) {
             validationErrors.add("name must not contain a '#'");
         }
-        for (String indexPattern : request.indexPatterns) {
-            if (indexPattern.contains(" ")) {
-                validationErrors.add("template must not contain a space");
-            }
-            if (indexPattern.contains(",")) {
-                validationErrors.add("template must not contain a ','");
-            }
-            if (indexPattern.contains("#")) {
-                validationErrors.add("template must not contain a '#'");
-            }
-            if (!Strings.validFileNameExcludingAstrix(indexPattern)) {
-                validationErrors.add("template must not contain the following characters " + Strings.INVALID_FILENAME_CHARS);
-            }
+        if (templatePrefix.contains(" ")) {
+            validationErrors.add("template must not contain a space");
+        }
+        if (templatePrefix.contains(",")) {
+            validationErrors.add("template must not contain a ','");
+        }
+        if (templatePrefix.contains("#")) {
+            validationErrors.add("template must not contain a '#'");
+        }
+        if (!Strings.validFileNameExcludingAstrix(templatePrefix)) {
+            validationErrors.add("template must not contain the following characters " + Strings.INVALID_FILENAME_CHARS);
         }
 
         try {
-            indexScopedSettings.validate(request.settings, true); // templates must be consistent with regards to dependencies
+            indexScopedSettings.validate(settings, true); // templates must be consistent with regards to dependencies
         } catch (IllegalArgumentException iae) {
             validationErrors.add(iae.getMessage());
             for (Throwable t : iae.getSuppressed()) {
                 validationErrors.add(t.getMessage());
             }
         }
-        List<String> indexSettingsValidation = metadataCreateIndexService.getIndexSettingsValidationErrors(request.settings, true);
+        List<String> indexSettingsValidation = metadataCreateIndexService.getIndexSettingsValidationErrors(settings, true);
         validationErrors.addAll(indexSettingsValidation);
         if (!validationErrors.isEmpty()) {
             ValidationException validationException = new ValidationException();
             validationException.addValidationErrors(validationErrors);
-            throw new InvalidIndexTemplateException(request.name, validationException.getMessage());
+            throw new InvalidIndexTemplateException(templateName, validationException.getMessage());
         }
 
-        for (Alias alias : request.aliases) {
-            //we validate the alias only partially, as we don't know yet to which index it'll get applied to
-            AliasValidator.validateAliasStandalone(alias);
-            if (request.indexPatterns.contains(alias.name())) {
-                throw new IllegalArgumentException("Alias [" + alias.name() +
-                    "] cannot be the same as any pattern in [" + String.join(", ", request.indexPatterns) + "]");
-            }
-        }
-    }
-
-    public static class PutRequest {
-        final String name;
-        final String cause;
-        boolean create;
-        Integer version;
-        List<String> indexPatterns;
-        Settings settings = Settings.Builder.EMPTY_SETTINGS;
-        String mapping;
-        List<Alias> aliases = new ArrayList<>();
-
-        TimeValue masterTimeout = MasterNodeRequest.DEFAULT_MASTER_NODE_TIMEOUT;
-
-        public PutRequest(String cause, String name) {
-            this.cause = cause;
-            this.name = name;
-        }
-
-        public PutRequest patterns(List<String> indexPatterns) {
-            this.indexPatterns = indexPatterns;
-            return this;
-        }
-
-        public PutRequest create(boolean create) {
-            this.create = create;
-            return this;
-        }
-
-        public PutRequest settings(Settings settings) {
-            this.settings = settings;
-            return this;
-        }
-
-        public PutRequest mapping(String mapping) {
-            this.mapping = mapping;
-            return this;
-        }
-
-        public PutRequest aliases(Set<Alias> aliases) {
-            this.aliases.addAll(aliases);
-            return this;
-        }
-
-        public PutRequest masterTimeout(TimeValue masterTimeout) {
-            this.masterTimeout = masterTimeout;
-            return this;
-        }
-
-        public PutRequest version(Integer version) {
-            this.version = version;
-            return this;
+        //we validate the alias only partially, as we don't know yet to which index it'll get applied to
+        AliasValidator.validateAliasStandalone(alias);
+        if (templatePrefix.equals(alias.name())) {
+            throw new IllegalArgumentException("Alias [" + alias.name() +
+                "] cannot be the same as template prefix [" + templatePrefix + "]");
         }
     }
 }
