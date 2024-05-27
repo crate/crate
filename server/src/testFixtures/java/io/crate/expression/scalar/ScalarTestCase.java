1c688951b48d8f07580cba45f004bfda03744b6a
Minsoo Jee <jeeminso@gmail.com>
2024-05-27 15:34:03 -0400

    Expand ScalarTestCase.assertEvaluate to verify NON_NULLABLE scalars to never return NULL
    (cherry picked from commit 4713f405f6b9d6c8cceb01504d069089fd4da4b4)

--- server/src/testFixtures/java/io/crate/expression/scalar/ScalarTestCase.java
+++ server/src/testFixtures/java/io/crate/expression/scalar/ScalarTestCase.java
@@ -23,7 +23,6 @@ package io.crate.expression.scalar;
 
 import static io.crate.testing.Asserts.assertThat;
 import static io.crate.testing.Asserts.isNull;
-import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Arrays;
 import java.util.LinkedList;
@@ -257,6 +256,9 @@ public abstract class ScalarTestCase extends CrateDummyClusterServiceUnitTest {
 
         actualValue = scalar.evaluate(txnCtx, sqlExpressions.nodeCtx, arguments);
         assertThat((T) actualValue).satisfies(expectedValue);
+        if (scalar.signature().hasFeature(Scalar.Feature.NON_NULLABLE)) {
+            assertThat(actualValue).isNotNull();
+        }
     }
 
     @SuppressWarnings("rawtypes")
