af06810bfba07a5f86f874f042f477da7eabb119
Mathias Fussenegger <f.mathias@zignar.net>
2024-05-29 11:16:01 +0200

    Make use of `withTempIndexService` in MetadataIndexTemplateService

--- server/src/main/java/org/elasticsearch/cluster/metadata/MetadataIndexTemplateService.java
+++ server/src/main/java/org/elasticsearch/cluster/metadata/MetadataIndexTemplateService.java
@@ -19,11 +19,7 @@
 
 package org.elasticsearch.cluster.metadata;
 
-import static org.elasticsearch.cluster.metadata.MetadataCreateIndexService.setIndexVersionCreatedSetting;
-import static org.elasticsearch.indices.cluster.IndicesClusterStateService.AllocatedIndices.IndexRemovalReason.NO_LONGER_ASSIGNED;
-
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
@@ -45,8 +41,6 @@ import org.elasticsearch.common.settings.IndexScopedSettings;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.xcontent.NamedXContentRegistry;
 import org.elasticsearch.common.xcontent.json.JsonXContent;
-import org.elasticsearch.index.Index;
-import org.elasticsearch.index.IndexService;
 import org.elasticsearch.index.mapper.MapperParsingException;
 import org.elasticsearch.indices.IndicesService;
 import org.elasticsearch.indices.InvalidIndexTemplateException;
@@ -106,8 +100,6 @@ public class MetadataIndexTemplateService {
             return;
         }
 
-        final IndexTemplateMetadata.Builder templateBuilder = IndexTemplateMetadata.builder(templateName);
-
         clusterService.submitStateUpdateTask(
             "create-index-template [" + templateName + "], cause [create table]",
             new ClusterStateUpdateTask(Priority.URGENT) {
@@ -127,23 +119,16 @@ public class MetadataIndexTemplateService {
                     if (currentState.metadata().templates().containsKey(templateName)) {
                         throw new IllegalArgumentException("index_template [" + templateName + "] already exists");
                     }
-                    Metadata.Builder metadataBuilder = Metadata.builder(currentState.metadata());
-                    validateAndAddTemplate(
+                    return addTemplate(
                         request,
+                        alias,
+                        templateName,
                         templatePrefix,
                         settings,
-                        metadataBuilder,
-                        templateBuilder,
                         indicesService,
                         xContentRegistry,
                         currentState
                     );
-
-                    templateBuilder.putAlias(new AliasMetadata(alias.name()));
-                    metadataBuilder.put(templateBuilder);
-
-                    LOGGER.info("adding template [{}] for index pattern {}", templateName, templatePrefix);
-                    return ClusterState.builder(currentState).metadata(metadataBuilder).build();
                 }
 
                 @Override
@@ -154,45 +139,44 @@ public class MetadataIndexTemplateService {
         );
     }
 
-    private static void validateAndAddTemplate(CreateTableRequest createTableRequest,
-                                               String templatePrefix,
-                                               Settings settings,
-                                               Metadata.Builder metadataBuilder,
-                                               IndexTemplateMetadata.Builder templateBuilder,
-                                               IndicesService indicesService,
-                                               NamedXContentRegistry xContentRegistry,
-                                               ClusterState currentState) throws Exception {
-        Index createdIndex = null;
+    private static ClusterState addTemplate(CreateTableRequest createTableRequest,
+                                            Alias alias,
+                                            String templateName,
+                                            String templatePrefix,
+                                            Settings settings,
+                                            IndicesService indicesService,
+                                            NamedXContentRegistry xContentRegistry,
+                                            ClusterState currentState) throws Exception {
         final String temporaryIndexName = UUIDs.randomBase64UUID();
-        try {
-            // use the provided values, otherwise just pick valid dummy values
-            int dummyPartitionSize = IndexMetadata.INDEX_ROUTING_PARTITION_SIZE_SETTING.get(settings);
-            int dummyShards = settings.getAsInt(IndexMetadata.SETTING_NUMBER_OF_SHARDS,
-                    dummyPartitionSize == 1 ? 1 : dummyPartitionSize + 1);
 
-            //create index service for parsing and validating "mappings"
-            Settings dummySettings = Settings.builder()
-                .put(IndexMetadata.SETTING_VERSION_CREATED, Version.CURRENT)
-                .put(settings)
-                .put(IndexMetadata.SETTING_NUMBER_OF_SHARDS, dummyShards)
-                .put(IndexMetadata.SETTING_NUMBER_OF_REPLICAS, 0)
-                .put(IndexMetadata.SETTING_INDEX_UUID, UUIDs.randomBase64UUID())
-                .build();
+        // use the provided values, otherwise just pick valid dummy values
+        int dummyPartitionSize = IndexMetadata.INDEX_ROUTING_PARTITION_SIZE_SETTING.get(settings);
+        int dummyShards = settings.getAsInt(IndexMetadata.SETTING_NUMBER_OF_SHARDS,
+                dummyPartitionSize == 1 ? 1 : dummyPartitionSize + 1);
 
-            final IndexMetadata tmpIndexMetadata = IndexMetadata.builder(temporaryIndexName).settings(dummySettings).build();
-            IndexService dummyIndexService = indicesService.createIndex(tmpIndexMetadata, Collections.emptyList(), false);
-            createdIndex = dummyIndexService.index();
+        //create index service for parsing and validating "mappings"
+        Settings dummySettings = Settings.builder()
+            .put(IndexMetadata.SETTING_VERSION_CREATED, Version.CURRENT)
+            .put(settings)
+            .put(IndexMetadata.SETTING_NUMBER_OF_SHARDS, dummyShards)
+            .put(IndexMetadata.SETTING_NUMBER_OF_REPLICAS, 0)
+            .put(IndexMetadata.SETTING_INDEX_UUID, UUIDs.randomBase64UUID())
+            .build();
 
-            templateBuilder.version(null);
-            templateBuilder.patterns(List.of(templatePrefix));
+        final IndexMetadata tmpIndexMetadata = IndexMetadata.builder(temporaryIndexName).settings(dummySettings).build();
+        return indicesService.withTempIndexService(tmpIndexMetadata, indexService -> {
+            Version versionCreated = Version.min(Version.CURRENT, currentState.nodes().getSmallestNonClientNodeVersion());
+            Metadata.Builder metadataBuilder = Metadata.builder(currentState.metadata());
+            IndexTemplateMetadata.Builder templateBuilder = IndexTemplateMetadata.builder(templateName)
+                .version(null)
+                .patterns(List.of(templatePrefix))
+                .putAlias(new AliasMetadata(alias.name()))
+                .settings(Settings.builder()
+                    .put(settings)
+                    .put(IndexMetadata.SETTING_INDEX_VERSION_CREATED.getKey(), versionCreated));
 
-            // inject `index.version.created` to the template settings to flag version of template creation (partitioned table)
-            var templateSettingsBuilder = Settings.builder().put(settings);
-            setIndexVersionCreatedSetting(templateSettingsBuilder, currentState);
-
-            templateBuilder.settings(templateSettingsBuilder.build());
             List<Reference> references = DocReferences.applyOid(createTableRequest.references(), metadataBuilder.columnOidSupplier());
-            var mapping = MappingUtil.createMapping(
+            var mapping = Map.of(Constants.DEFAULT_MAPPING_TYPE, MappingUtil.createMapping(
                 MappingUtil.AllocPosition.forNewTable(),
                 createTableRequest.pkConstraintName(),
                 references,
@@ -201,18 +185,18 @@ public class MetadataIndexTemplateService {
                 createTableRequest.partitionedBy(),
                 createTableRequest.tableColumnPolicy(),
                 createTableRequest.routingColumn()
-            );
-            mapping = Map.of(Constants.DEFAULT_MAPPING_TYPE, mapping); // We used PutIndexTemplateRequest.mapping which wraps mapping with default type
+            ));
             try {
                 templateBuilder.putMapping(new CompressedXContent(Strings.toString(JsonXContent.builder().map(mapping))));
             } catch (Exception e) {
                 throw new MapperParsingException("Failed to parse mapping: {}", e, e.getMessage());
             }
-        } finally {
-            if (createdIndex != null) {
-                indicesService.removeIndex(createdIndex, NO_LONGER_ASSIGNED, " created for parsing template mapping");
-            }
-        }
+            LOGGER.info("adding template [{}] for index pattern {}", templateName, templatePrefix);
+            return ClusterState.builder(currentState)
+                .metadata(metadataBuilder.put(templateBuilder))
+                .build();
+        });
+
     }
 
     private void validate(String templateName, String templatePrefix, Settings settings, Alias alias) {
