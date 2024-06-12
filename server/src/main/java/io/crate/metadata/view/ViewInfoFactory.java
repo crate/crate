92b11f5235bdfb62d6092a3e73210621ac42bd26
baur <baurzhansahariev@gmail.com>
2024-06-12 15:36:06 +0200

    Show view sub-columns in information_schema.columns
    (cherry picked from commit c1dc582bfbdd50117e70612c744173e8b4475028)

--- server/src/main/java/io/crate/metadata/view/ViewInfoFactory.java
+++ server/src/main/java/io/crate/metadata/view/ViewInfoFactory.java
@@ -34,6 +34,7 @@ import io.crate.analyze.ParamTypeHints;
 import io.crate.analyze.relations.AnalyzedRelation;
 import io.crate.analyze.relations.RelationAnalyzer;
 import io.crate.expression.symbol.Symbols;
+import io.crate.metadata.ColumnIdent;
 import io.crate.metadata.CoordinatorTxnCtx;
 import io.crate.metadata.Reference;
 import io.crate.metadata.ReferenceIdent;
@@ -42,6 +43,8 @@ import io.crate.metadata.RowGranularity;
 import io.crate.metadata.SimpleReference;
 import io.crate.sql.parser.SqlParser;
 import io.crate.sql.tree.Query;
+import io.crate.types.DataType;
+import io.crate.types.ObjectType;
 
 public class ViewInfoFactory {
 
@@ -72,16 +75,29 @@ public class ViewInfoFactory {
                 ParamTypeHints.EMPTY
             );
             final List<Reference> collectedColumns = new ArrayList<>(relation.outputs().size());
+            List<Reference> subColumns = new ArrayList<>();
             int position = 1;
             for (var field : relation.outputs()) {
+                ColumnIdent columnIdent = Symbols.pathFromSymbol(field);
                 collectedColumns.add(
-                    new SimpleReference(new ReferenceIdent(ident, Symbols.pathFromSymbol(field).sqlFqn()),
-                                  RowGranularity.DOC,
-                                  field.valueType(),
-                                  position++,
-                                  null));
+                    new SimpleReference(
+                        new ReferenceIdent(ident, columnIdent.sqlFqn()),
+                        RowGranularity.DOC,
+                        field.valueType(),
+                        position++,
+                        null
+                    )
+                );
+
             }
             columns = collectedColumns;
+            // Now add all sub-columns.
+            // We do it after handling top level columns to ensure that ordinals in the information_schema.columns are stable
+            // and sub-columns, added or dropped after view definition don't change ordinals of other columns in the view.
+            for (Reference ref: columns) {
+                position = addSubColumns(subColumns, ident, ref.column(), ref.valueType(), position);
+            }
+            columns.addAll(subColumns);
         } catch (Exception e) {
             // Statement could not be analyzed, because the referenced table either not found
             // or has been updated and view definition became incompatible with the new schema (https://github.com/crate/crate/issues/14377).
@@ -90,5 +106,31 @@ public class ViewInfoFactory {
         }
         String viewDefinition = analyzeError ? String.format(Locale.ENGLISH, "/* Corrupted view, needs fix */\n%s", view.stmt()) : view.stmt();
         return new ViewInfo(ident, viewDefinition, columns, view.owner(), view.searchPath());
+    }
+
+    private static int addSubColumns(List<Reference> subColumns,
+                                     RelationName ident,
+                                     ColumnIdent parent,
+                                     DataType<?> parentType,
+                                     int position) {
+        int updatedPosition = position;
+        if (parentType instanceof ObjectType objectType) {
+            for (var entry : objectType.innerTypes().entrySet()) {
+                String childName = entry.getKey();
+                ColumnIdent childColumn = parent.getChild(childName);
+                DataType<?> childType = entry.getValue();
+                subColumns.add(
+                    new SimpleReference(
+                        new ReferenceIdent(ident, childColumn.sqlFqn()),
+                        RowGranularity.DOC,
+                        childType,
+                        position++,
+                        null
+                    )
+                );
+                updatedPosition = addSubColumns(subColumns, ident, childColumn, childType, position);
+            }
+        }
+        return updatedPosition;
     }
 }
