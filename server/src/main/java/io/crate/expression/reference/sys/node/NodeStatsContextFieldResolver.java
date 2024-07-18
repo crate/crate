crate/crate

commit 0653a1f99fbb14574d607e90f4a5523726ff7479

Remove `sys.nodes.network` column
The column has been deprecated since `2.3.0` and all of the sub-columns
were returning `0` value.

server/src/main/java/io/crate/expression/reference/sys/node/NodeStatsContextFieldResolver.java
@@ -243,8 +243,6 @@ public class NodeStatsContextFieldResolver {
                 context.threadPools(threadPool.stats());
             }
         }),
-        entry(SysNodesTableInfo.Columns.NETWORK, context -> {
-        }),
         entry(SysNodesTableInfo.Columns.CONNECTIONS, new Consumer<>() {
             @Override
             public void accept(NodeStatsContext nodeStatsContext) {

server/src/main/java/io/crate/metadata/sys/SysNodesTableInfo.java
@@ -57,7 +57,6 @@ public class SysNodesTableInfo {
     private static final String SYS_COL_HEAP = "heap";
     private static final String SYS_COL_VERSION = "version";
     private static final String SYS_COL_THREAD_POOLS = "thread_pools";
-    private static final String SYS_COL_NETWORK = "network";
     private static final String SYS_COL_OS = "os";
     private static final String SYS_COL_OS_INFO = "os_info";
     private static final String SYS_COL_PROCESS = "process";
@@ -82,8 +81,6 @@ public class SysNodesTableInfo {
         public static final ColumnIdent VERSION = new ColumnIdent(SYS_COL_VERSION);
 
         public static final ColumnIdent THREAD_POOLS = new ColumnIdent(SYS_COL_THREAD_POOLS);
-
-        public static final ColumnIdent NETWORK = new ColumnIdent(SYS_COL_NETWORK);
 
         public static final ColumnIdent CONNECTIONS = new ColumnIdent("connections");
 
@@ -148,25 +145,6 @@ public class SysNodesTableInfo {
                 .add("threads", INTEGER, ThreadPoolStats.Stats::getThreads)
                 .add("queue", INTEGER, ThreadPoolStats.Stats::getQueue)
             .endObjectArray()
-            .startObject("network")
-                .add("probe_timestamp", TIMESTAMPZ, x -> 0L)
-                .startObject("tcp")
-                    .startObject("connections")
-                        .add("initiated", LONG, x -> 0L)
-                        .add("accepted", LONG, x -> 0L)
-                        .add("curr_established", LONG, x -> 0L)
-                        .add("dropped", LONG, x -> 0L)
-                        .add("embryonic_dropped", LONG, x -> 0L)
-                    .endObject()
-                    .startObject("packets")
-                        .add("sent", LONG, x -> 0L)
-                        .add("received", LONG, x -> 0L)
-                        .add("retransmitted", LONG, x -> 0L)
-                        .add("errors_received", LONG, x -> 0L)
-                        .add("rst_sent", LONG, x -> 0L)
-                    .endObject()
-                .endObject()
-            .endObject()
             .startObject("connections")
                 .startObject("http")
                     .add("open", LONG, x -> x.httpStats().getServerOpen())

server/src/test/java/io/crate/analyze/SelectStatementAnalyzerTest.java
@@ -32,7 +32,6 @@ import static io.crate.testing.Asserts.isLiteral;
 import static io.crate.testing.Asserts.isReference;
 import static io.crate.testing.Asserts.toCondition;
 import static org.assertj.core.api.Assertions.anyOf;
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.io.IOException;
@@ -209,7 +208,6 @@ public class SelectStatementAnalyzerTest extends CrateDummyClusterServiceUnitTes
             "load",
             "mem",
             "name",
-            "network",
             "os",
             "os_info",
             "port",

server/src/test/java/io/crate/integrationtests/InformationSchemaTest.java
@@ -569,7 +569,7 @@ public class InformationSchemaTest extends IntegTestCase {
     @Test
     public void testDefaultColumns() {
         execute("select * from information_schema.columns order by table_schema, table_name");
-        assertThat(response.rowCount()).isEqualTo(1016);
+        assertThat(response.rowCount()).isEqualTo(1001);
     }
 
     @Test

server/src/test/java/io/crate/integrationtests/NodeStatsTest.java
@@ -22,7 +22,6 @@
 package io.crate.integrationtests;
 
 import static io.crate.testing.Asserts.assertThat;
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.HashMap;
@@ -100,67 +99,6 @@ public class NodeStatsTest extends IntegTestCase {
         List<?> queues = (List<?>) response.rows()[0][1];
         assertThat(queues.size()).isGreaterThanOrEqualTo(1);
         assertThat((Integer) queues.get(0)).isGreaterThanOrEqualTo(0);
-    }
-
-    @Test
-    @SuppressWarnings("unchecked")
-    public void testNetwork() throws Exception {
-        SQLResponse response = execute("select network from sys.nodes limit 1");
-        assertThat(response).hasRowCount(1L);
-
-        Map<String, Object> network = (Map<String, Object>) response.rows()[0][0];
-        assertThat(network).containsKey("tcp");
-        Map<String, Object> tcp = (Map<String, Object>) network.get("tcp");
-        assertNetworkTCP(tcp);
-
-
-        response = execute("select network['tcp'] from sys.nodes limit 1");
-        assertThat(response).hasRowCount(1L);
-        tcp = (Map<String, Object>) response.rows()[0][0];
-        assertNetworkTCP(tcp);
-    }
-
-    @SuppressWarnings("unchecked")
-    private void assertNetworkTCP(Map<String, Object> tcp) {
-        assertThat(tcp).containsOnlyKeys("packets", "connections");
-
-        Map<String, Object> connections = (Map<String, Object>) tcp.get("connections");
-        assertThat(connections).containsOnlyKeys(
-            "initiated", "accepted", "curr_established", "dropped", "embryonic_dropped");
-
-        Map<String, Object> packets = (Map<String, Object>) tcp.get("packets");
-        assertThat(packets).containsOnlyKeys(
-            "sent", "received", "errors_received", "retransmitted", "rst_sent");
-    }
-
-    @Test
-    public void testNetworkTcpConnectionFields() throws Exception {
-        SQLResponse response = execute("select " +
-                                       "network['tcp']['connections']['initiated'], " +
-                                       "network['tcp']['connections']['accepted'], " +
-                                       "network['tcp']['connections']['curr_established']," +
-                                       "network['tcp']['connections']['dropped']," +
-                                       "network['tcp']['connections']['embryonic_dropped']" +
-                                       " from sys.nodes limit 1");
-        assertThat(response).hasRowCount(1L);
-        for (int i = 0; i < response.cols().length; i++) {
-            assertThat((Long) response.rows()[0][i]).isGreaterThanOrEqualTo(-1L);
-        }
-    }
-
-    @Test
-    public void testNetworkTcpPacketsFields() throws Exception {
-        SQLResponse response = execute("select " +
-                                       "network['tcp']['packets']['sent'], " +
-                                       "network['tcp']['packets']['received'], " +
-                                       "network['tcp']['packets']['retransmitted'], " +
-                                       "network['tcp']['packets']['errors_received'], " +
-                                       "network['tcp']['packets']['rst_sent'] " +
-                                       "from sys.nodes limit 1");
-        assertThat(response).hasRowCount(1L);
-        for (int i = 0; i < response.cols().length; i++) {
-            assertThat((Long) response.rows()[0][i]).isGreaterThanOrEqualTo(-1L);
-        }
     }
 
     @Test

server/src/test/java/io/crate/integrationtests/PgCatalogITest.java
@@ -22,7 +22,6 @@
 package io.crate.integrationtests;
 
 import static io.crate.testing.Asserts.assertThat;
-import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -91,7 +90,7 @@ public class PgCatalogITest extends IntegTestCase {
     public void testPgClassTable() {
         execute("select * from pg_catalog.pg_class where relname in ('t1', 'v1', 'tables', 'nodes') order by relname");
         assertThat(response).hasRows(
-            "-1420189195| NULL| 0| 0| 0| 0| false| 0| false| false| false| false| false| true| false| r| 0| nodes| -458336339| 18| 0| NULL| 0| 0| NULL| p| p| 0| false| 0| 0| -1.0| 0",
+            "-1420189195| NULL| 0| 0| 0| 0| false| 0| false| false| false| false| false| true| false| r| 0| nodes| -458336339| 17| 0| NULL| 0| 0| NULL| p| p| 0| false| 0| 0| -1.0| 0",
             "728874843| NULL| 0| 0| 0| 0| false| 0| false| false| false| false| false| true| false| r| 0| t1| -2048275947| 4| 0| NULL| 0| 0| NULL| p| p| 0| false| 0| 0| -1.0| 0",
             "-1689918046| NULL| 0| 0| 0| 0| false| 0| false| false| false| false| false| true| false| r| 0| tables| 204690627| 16| 0| NULL| 0| 0| NULL| p| p| 0| false| 0| 0| -1.0| 0",
             "845171032| NULL| 0| 0| 0| 0| false| 0| false| false| false| false| false| true| false| v| 0| v1| -2048275947| 1| 0| NULL| 0| 0| NULL| p| p| 0| false| 0| 0| -1.0| 0");

commit 460632790366e48effd1c97dcd61f7c1d5cb3eb5

Fix BWC issue of the sys.nodes table
Fixes regression introduced in https://github.com/crate/crate/commit/0653a1f99fbb14574d607e90f4a5523726ff7479

server/src/main/java/io/crate/expression/reference/sys/node/NodeStatsContextFieldResolver.java
@@ -241,6 +241,10 @@ public class NodeStatsContextFieldResolver {
                 context.threadPools(threadPool.stats());
             }
         }),
+        // TODO: Added due to BWC reasons, <= 5.7 nodes should be able to query this column.
+        // Remove it in 6.0
+        entry(SysNodesTableInfo.Columns.NETWORK, context -> {
+        }),
         entry(SysNodesTableInfo.Columns.CONNECTIONS, new Consumer<>() {
             @Override
             public void accept(NodeStatsContext nodeStatsContext) {

server/src/main/java/io/crate/metadata/sys/SysNodesTableInfo.java
@@ -57,6 +57,7 @@ public class SysNodesTableInfo {
     private static final String SYS_COL_HEAP = "heap";
     private static final String SYS_COL_VERSION = "version";
     private static final String SYS_COL_THREAD_POOLS = "thread_pools";
+    private static final String SYS_COL_NETWORK = "network";
     private static final String SYS_COL_OS = "os";
     private static final String SYS_COL_OS_INFO = "os_info";
     private static final String SYS_COL_PROCESS = "process";
@@ -81,6 +82,8 @@ public class SysNodesTableInfo {
         public static final ColumnIdent VERSION = ColumnIdent.of(SYS_COL_VERSION);
 
         public static final ColumnIdent THREAD_POOLS = ColumnIdent.of(SYS_COL_THREAD_POOLS);
+
+        public static final ColumnIdent NETWORK = ColumnIdent.of(SYS_COL_NETWORK);
 
         public static final ColumnIdent CONNECTIONS = ColumnIdent.of("connections");
 