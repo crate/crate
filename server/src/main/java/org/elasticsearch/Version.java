cd72cadadb33dae1cbf1a25a7eeeac46ee14c12d
baur <baurzhansahariev@gmail.com>
2024-07-29 15:56:35 +0500

    Release 5.8.1

--- server/src/main/java/org/elasticsearch/Version.java
+++ server/src/main/java/org/elasticsearch/Version.java
@@ -204,7 +204,7 @@ public class Version implements Comparable<Version>, ToXContentFragment {
     public static final Version V_5_7_4 = new Version(8_07_04_99, false, org.apache.lucene.util.Version.LUCENE_9_10_0);
 
     public static final Version V_5_8_0 = new Version(8_08_00_99, false, org.apache.lucene.util.Version.LUCENE_9_11_1);
-    public static final Version V_5_8_1 = new Version(8_08_01_99, true, org.apache.lucene.util.Version.LUCENE_9_11_1);
+    public static final Version V_5_8_1 = new Version(8_08_01_99, false, org.apache.lucene.util.Version.LUCENE_9_11_1);
 
     public static final Version CURRENT = V_5_8_1;
 
