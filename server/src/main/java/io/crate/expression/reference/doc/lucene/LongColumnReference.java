eea3f5ecf38d7a712b6300db58c2e11a1c32ac51
Mathias Fussenegger <f.mathias@zignar.net>
2024-08-07 16:34:45 +0200

    Use NumericColumnReference in LongColumnReference implementation

--- server/src/main/java/io/crate/expression/reference/doc/lucene/LongColumnReference.java
+++ server/src/main/java/io/crate/expression/reference/doc/lucene/LongColumnReference.java
@@ -21,52 +21,15 @@
 
 package io.crate.expression.reference.doc.lucene;
 
-import io.crate.exceptions.ArrayViaDocValuesUnsupportedException;
-import io.crate.execution.engine.fetch.ReaderContext;
-import org.apache.lucene.index.DocValues;
-import org.apache.lucene.index.SortedNumericDocValues;
-
-import java.io.IOException;
-import java.io.UncheckedIOException;
-
-public class LongColumnReference extends LuceneCollectorExpression<Long> {
-
-    private final String columnName;
-    private SortedNumericDocValues values;
-    private int docId;
+public class LongColumnReference extends NumericColumnReference<Long> {
 
     public LongColumnReference(String columnName) {
-        this.columnName = columnName;
+        super(columnName);
     }
 
     @Override
-    public Long value() {
-        try {
-            if (values.advanceExact(docId)) {
-                switch (values.docValueCount()) {
-                    case 1:
-                        return values.nextValue();
-
-                    default:
-                        throw new ArrayViaDocValuesUnsupportedException(columnName);
-                }
-            } else {
-                return null;
-            }
-        } catch (IOException e) {
-            throw new UncheckedIOException(e);
-        }
-    }
-
-    @Override
-    public void setNextDocId(int docId) {
-        this.docId = docId;
-    }
-
-    @Override
-    public void setNextReader(ReaderContext context) throws IOException {
-        super.setNextReader(context);
-        values = DocValues.getSortedNumeric(context.reader(), columnName);
+    protected Long convert(long input) {
+        return input;
     }
 }
 
