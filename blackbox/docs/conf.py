0889d2e1fe3d01c4f08d0c4ea43f20b2c61fdd2c
Matthias <matthias@crate.io>
2024-08-14 15:38:04 +0200

    Enable missing version-chooser in documentation
    (cherry picked from commit 741ba94f77cdbb798ab4c01868c75569a6740f4d)
    
    # Conflicts:
    #	blackbox/shared/src/testutils/paths.py

--- blackbox/docs/conf.py
+++ blackbox/docs/conf.py
@@ -3,3 +3,8 @@ from crate.theme.rtd.conf.crate_reference import *
 exclude_patterns = ['out/**', 'tmp/**', 'eggs/**', 'requirements.txt', 'README.rst']
 
 extensions = ['crate.sphinx.csv', 'sphinx_sitemap']
+
+# Enable version chooser.
+html_context.update({
+    "display_version": True,
+})
