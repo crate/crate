crate/crate

commit 111ffe166e523a4a5cd3278975772ce365112b64

Change drop column to operate on DocTable, then serialize to mapping
We have several places where we directly modify the mapping of
`IndexMetadata` and `IndexTemplateMetadata`

One example is the `DropColumnTask`.
Manipulating the mapping directly is a bit tedious because of the
weak typing of `Map<String, Object>`.

This changes the approach:

- Use rich structures in `DocTableInfo` to alter the state
- Then write out the _full_ table to the cluster state/meta data

server/src/main/java/io/crate/metadata/doc/DocTableInfo.java
@@ -22,13 +22,16 @@
 package io.crate.metadata.doc;
 
 import static io.crate.expression.reference.doc.lucene.SourceParser.UNKNOWN_COLUMN_PREFIX;
-import static org.elasticsearch.cluster.metadata.Metadata.COLUMN_OID_UNASSIGNED;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
@@ -40,16 +43,33 @@ import org.elasticsearch.Version;
 import org.elasticsearch.cluster.ClusterState;
 import org.elasticsearch.cluster.metadata.IndexMetadata;
 import org.elasticsearch.cluster.metadata.IndexNameExpressionResolver;
+import org.elasticsearch.cluster.metadata.IndexTemplateMetadata;
+import org.elasticsearch.cluster.metadata.MappingMetadata;
+import org.elasticsearch.cluster.metadata.Metadata;
+import org.elasticsearch.common.Strings;
 import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.xcontent.json.JsonXContent;
+import org.elasticsearch.index.mapper.DocumentMapper;
+import org.elasticsearch.index.mapper.MapperService;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import com.carrotsearch.hppc.IntArrayList;
+
+import io.crate.analyze.BoundCreateTable;
+import io.crate.analyze.DropColumn;
 import io.crate.analyze.NumberOfReplicas;
 import io.crate.analyze.WhereClause;
+import io.crate.common.CheckedFunction;
 import io.crate.common.collections.Lists2;
 import io.crate.exceptions.ColumnUnknownException;
+import io.crate.execution.ddl.tables.MappingUtil;
+import io.crate.execution.ddl.tables.MappingUtil.AllocPosition;
 import io.crate.expression.symbol.DynamicReference;
+import io.crate.expression.symbol.RefReplacer;
+import io.crate.expression.symbol.RefVisitor;
 import io.crate.expression.symbol.Symbol;
+import io.crate.expression.symbol.Symbols;
 import io.crate.expression.symbol.VoidReference;
 import io.crate.metadata.ColumnIdent;
 import io.crate.metadata.GeneratedReference;
@@ -69,6 +89,9 @@ import io.crate.metadata.table.StoredTable;
 import io.crate.metadata.table.TableInfo;
 import io.crate.sql.tree.CheckConstraint;
 import io.crate.sql.tree.ColumnPolicy;
+import io.crate.types.ArrayType;
+import io.crate.types.DataType;
+import io.crate.types.ObjectType;
 
 
 /**
@@ -211,7 +234,7 @@ public class DocTableInfo implements TableInfo, ShardedTable, StoredTable {
         this.indexColumns = indexColumns;
         leafNamesByOid = new HashMap<>();
         Stream.concat(Stream.concat(this.references.values().stream(), indexColumns.values().stream()), droppedColumns.stream())
-            .filter(r -> r.oid() != COLUMN_OID_UNASSIGNED)
+            .filter(r -> r.oid() != Metadata.COLUMN_OID_UNASSIGNED)
             .forEach(r -> leafNamesByOid.put(Long.toString(r.oid()), r.column().leafName()));
         this.analyzers = analyzers;
         this.ident = ident;
@@ -580,5 +603,195 @@ public class DocTableInfo implements TableInfo, ShardedTable, StoredTable {
         } else {
             return Function.identity();
         }
+    }
+
+
+    private void validateDropColumns(List<DropColumn> dropColumns) {
+        var leftOverCols = columns().stream().map(Reference::column).collect(Collectors.toSet());
+        for (int i = 0 ; i < dropColumns.size(); i++) {
+            var refToDrop = dropColumns.get(i).ref();
+            var colToDrop = refToDrop.column();
+            for (var indexRef : indexColumns()) {
+                if (indexRef.columns().contains(refToDrop)) {
+                    throw new UnsupportedOperationException("Dropping column: " + colToDrop.sqlFqn() + " which " +
+                                                            "is part of INDEX: " + indexRef + " is not allowed");
+                }
+            }
+            for (var genRef : generatedColumns()) {
+                if (genRef.referencedReferences().contains(refToDrop)) {
+                    throw new UnsupportedOperationException(String.format(
+                        Locale.ENGLISH,
+                        "Cannot drop column `%s`. It's used in generated column `%s`: %s",
+                        colToDrop.sqlFqn(),
+                        genRef.column().sqlFqn(),
+                        genRef.formattedGeneratedExpression()
+                    ));
+                }
+            }
+            for (var checkConstraint : checkConstraints()) {
+                Set<ColumnIdent> columnsInConstraint = new HashSet<>();
+                RefVisitor.visitRefs(checkConstraint.expression(), r -> columnsInConstraint.add(r.column()));
+                if (columnsInConstraint.size() > 1 && columnsInConstraint.contains(colToDrop)) {
+                    throw new UnsupportedOperationException("Dropping column: " + colToDrop.sqlFqn() + " which " +
+                        "is used in CHECK CONSTRAINT: " + checkConstraint.name() + " is not allowed");
+                }
+                boolean constraintColIsSubColOfColToDrop = false;
+                for (var columnInConstraint : columnsInConstraint) {
+                    if (columnInConstraint.isChildOf(colToDrop)) {
+                        constraintColIsSubColOfColToDrop = true; // subcol of the dropped col referred in constraint
+                    }
+                }
+                if (constraintColIsSubColOfColToDrop) {
+                    for (var columnInConstraint : columnsInConstraint) {
+                        // Check if sibling, parent, or cols of another object are contained in the same constraint
+                        if (columnInConstraint.isChildOf(colToDrop) == false
+                            && columnInConstraint.path().equals(colToDrop.path()) == false) {
+                            throw new UnsupportedOperationException("Dropping column: " + colToDrop.sqlFqn() +
+                                " which is used in CHECK CONSTRAINT: " + checkConstraint.name() +
+                                " is not allowed");
+                        }
+                    }
+                }
+            }
+            leftOverCols.remove(colToDrop);
+        }
+        if (leftOverCols.isEmpty()) {
+            throw new UnsupportedOperationException("Dropping all columns of a table is not allowed");
+        }
+    }
+
+    public DocTableInfo dropColumns(List<DropColumn> columns) {
+        validateDropColumns(columns);
+        HashSet<Reference> toDrop = HashSet.newHashSet(columns.size());
+        HashMap<ColumnIdent, Reference> newReferences = new HashMap<>(references);
+        droppedColumns.forEach(ref -> newReferences.put(ref.column(), ref));
+        HashMap<Reference, Reference> changedReferences = new HashMap<>();
+        for (var column : columns) {
+            ColumnIdent columnIdent = column.ref().column();
+            Reference reference = references.get(columnIdent);
+            if (reference == null || reference.isDropped()) {
+                if (!column.ifExists()) {
+                    throw new ColumnUnknownException(columnIdent, ident);
+                }
+                continue;
+            }
+            ColumnIdent parent = columnIdent.getParent();
+            if (parent != null) {
+                Reference parentRef = references.get(parent);
+                DataType<?> parentType = parentRef.valueType();
+                int dimensions = ArrayType.dimensions(parentType);
+                DataType<?> parentTypeElement = ArrayType.unnest(parentType);
+                ObjectType newObjectType = ((ObjectType) parentTypeElement)
+                    .withoutChild(columnIdent.leafName());
+                DataType<?> newParentType = ArrayType.makeArray(newObjectType, dimensions);
+                Reference updatedParent = parentRef.withValueType(newParentType);
+                newReferences.replace(parent, updatedParent);
+                changedReferences.put(parentRef, updatedParent);
+            }
+            toDrop.add(reference.withDropped(true));
+            newReferences.replace(columnIdent, reference.withDropped(true));
+            for (var ref : references.values()) {
+                if (ref.column().isChildOf(columnIdent)) {
+                    toDrop.add(ref);
+                    newReferences.remove(ref.column());
+                }
+            }
+        }
+        if (toDrop.isEmpty()) {
+            return this;
+        }
+        Function<Symbol, Symbol> updateRef = symbol -> RefReplacer.replaceRefs(symbol, ref -> changedReferences.getOrDefault(ref, ref));
+        ArrayList<CheckConstraint<Symbol>> newCheckConstraints = new ArrayList<>(checkConstraints.size());
+        for (var constraint : checkConstraints) {
+            boolean drop = false;
+            for (var ref : toDrop) {
+                drop = Symbols.containsColumn(constraint.expression(), ref.column());
+                if (drop) {
+                    break;
+                }
+            }
+            if (!drop) {
+                newCheckConstraints.add(constraint.map(updateRef));
+            }
+        }
+        return new DocTableInfo(
+            ident,
+            newReferences,
+            indexColumns,
+            analyzers,
+            pkConstraintName,
+            primaryKeys,
+            newCheckConstraints,
+            clusteredBy,
+            concreteIndices,
+            concreteOpenIndices,
+            tableParameters,
+            partitionedBy,
+            partitions,
+            columnPolicy,
+            versionCreated,
+            versionUpgraded,
+            closed,
+            supportedOperations
+        );
+    }
+
+    public Metadata.Builder writeTo(CheckedFunction<IndexMetadata, MapperService, IOException> createMapperService,
+                                    Metadata metadata,
+                                    Metadata.Builder metadataBuilder) throws IOException {
+        List<Reference> allColumns = Stream.concat(references.values().stream(), droppedColumns.stream())
+            .filter(ref -> !ref.column().isSystemColumn())
+            .sorted(Reference.CMP_BY_POSITION_THEN_NAME)
+            .toList();
+        IntArrayList pKeyIndices = new IntArrayList(primaryKeys.size());
+        for (ColumnIdent pk : primaryKeys) {
+            int idx = Reference.indexOf(allColumns, pk);
+            if (idx >= 0) {
+                pKeyIndices.add(idx);
+            }
+        }
+        Map<String, String> checkConstraintMap = HashMap.newHashMap(checkConstraints.size());
+        for (var check : checkConstraints) {
+            checkConstraintMap.put(check.name(), check.expressionStr());
+        }
+        AllocPosition allocPosition = AllocPosition.forTable(this);
+        Map<String, Object> mapping = Map.of("default", MappingUtil.createMapping(
+            allocPosition,
+            pkConstraintName,
+            allColumns,
+            pKeyIndices,
+            checkConstraintMap,
+            Lists2.map(partitionedByColumns, BoundCreateTable::toPartitionMapping),
+            columnPolicy,
+            clusteredBy == DocSysColumns.ID ? null : clusteredBy.fqn()
+        ));
+        for (String indexName : concreteIndices) {
+            IndexMetadata indexMetadata = metadata.index(indexName);
+            if (indexMetadata == null) {
+                throw new UnsupportedOperationException("Cannot create index via DocTableInfo.writeTo");
+            }
+            MapperService mapperService = createMapperService.apply(indexMetadata);
+            DocumentMapper mapper = mapperService.merge(mapping, MapperService.MergeReason.MAPPING_UPDATE);
+            metadataBuilder.put(
+                IndexMetadata.builder(indexMetadata)
+                    .putMapping(new MappingMetadata(mapper.mappingSource()))
+                    .numberOfShards(numberOfShards)
+                    .mappingVersion(indexMetadata.getMappingVersion() + 1)
+            );
+        }
+        if (isPartitioned) {
+            String templateName = PartitionName.templateName(ident.schema(), ident.name());
+            IndexTemplateMetadata indexTemplateMetadata = metadata.templates().get(templateName);
+            if (indexTemplateMetadata == null) {
+                throw new UnsupportedOperationException("Cannot create template via DocTableInfo.writeTo");
+            }
+            Integer version = indexTemplateMetadata.getVersion();
+            var template = new IndexTemplateMetadata.Builder(indexTemplateMetadata)
+                .putMapping(Strings.toString(JsonXContent.builder().map(mapping)))
+                .version(version == null ? 1 : version + 1)
+                .build();
+            metadataBuilder.put(template);
+        }
+        return metadataBuilder;
     }
 }

server/src/test/java/io/crate/metadata/doc/DocTableInfoTest.java
@@ -21,6 +21,7 @@
 
 package io.crate.metadata.doc;
 
+import static io.crate.testing.Asserts.assertThat;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.elasticsearch.cluster.metadata.Metadata.COLUMN_OID_UNASSIGNED;
 
@@ -35,6 +36,7 @@ import org.elasticsearch.cluster.metadata.IndexMetadata;
 import org.elasticsearch.common.settings.Settings;
 import org.junit.Test;
 
+import io.crate.analyze.DropColumn;
 import io.crate.exceptions.ColumnUnknownException;
 import io.crate.expression.symbol.DynamicReference;
 import io.crate.expression.symbol.VoidReference;
@@ -52,6 +54,7 @@ import io.crate.sql.tree.ColumnPolicy;
 import io.crate.test.integration.CrateDummyClusterServiceUnitTest;
 import io.crate.testing.SQLExecutor;
 import io.crate.types.DataTypes;
+import io.crate.types.ObjectType;
 
 public class DocTableInfoTest extends CrateDummyClusterServiceUnitTest {
 
@@ -300,5 +303,41 @@ public class DocTableInfoTest extends CrateDummyClusterServiceUnitTest {
         );
 
         assertThat(info.lookupNameBySourceKey().apply("2")).isEqualTo("b");
+    }
+
+    @Test
+    public void test_drop_column_updates_type_of_parent_ref() throws Exception {
+        SQLExecutor e = SQLExecutor.builder(clusterService)
+            .addTable("create table tbl (o1 object as (o2 object as (x int)))")
+            .build();
+        DocTableInfo table = e.resolveTableInfo("tbl");
+        ColumnIdent o1o2 = new ColumnIdent("o1", "o2");
+        Reference o1o2Ref = table.getReference(o1o2);
+        DropColumn dropColumn = new DropColumn(o1o2Ref, true);
+        DocTableInfo updatedTable = table.dropColumns(List.of(dropColumn));
+
+        Reference o1Ref = updatedTable.getReference(new ColumnIdent("o1"));
+        assertThat(o1Ref.valueType()).isExactlyInstanceOf(ObjectType.class);
+        ObjectType objectType = ((ObjectType) o1Ref.valueType());
+        assertThat(objectType.innerTypes()).isEmpty();
+    }
+
+    @Test
+    public void test_drop_column_after_drop_column_preserves_previous_dropped_columns() throws Exception {
+        SQLExecutor e = SQLExecutor.builder(clusterService)
+            .addTable("create table tbl (x int, y int, z int)")
+            .build();
+        DocTableInfo table1 = e.resolveTableInfo("tbl");
+        Reference xref = table1.getReference(new ColumnIdent("x"));
+        Reference yref = table1.getReference(new ColumnIdent("y"));
+        DocTableInfo table2 = table1.dropColumns(List.of(new DropColumn(xref, true)));
+        assertThat(table2.droppedColumns()).satisfiesExactlyInAnyOrder(
+            x -> assertThat(x).isReference().hasName("x")
+        );
+        DocTableInfo table3 = table2.dropColumns(List.of(new DropColumn(yref, true)));
+        assertThat(table3.droppedColumns()).satisfiesExactlyInAnyOrder(
+            x -> assertThat(x).isReference().hasName("x"),
+            x -> assertThat(x).isReference().hasName("y")
+        );
     }
 }

libs/sql-parser/src/main/java/io/crate/sql/tree/CheckConstraint.java
@@ -23,6 +23,7 @@ package io.crate.sql.tree;
 
 import java.util.Objects;
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 import org.jetbrains.annotations.Nullable;
 
@@ -47,6 +48,10 @@ public class CheckConstraint<T> extends TableElement<T> {
 
     public T expression() {
         return expression;
+    }
+
+    public <U> CheckConstraint<U> map(Function<? super T, ? extends U> mapper) {
+        return new CheckConstraint<U>(name, mapper.apply(expression), expressionStr);
     }
 
     public String expressionStr() {

server/src/main/java/io/crate/analyze/AlterTableDropColumnAnalyzer.java
@@ -24,9 +24,7 @@ package io.crate.analyze;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Locale;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 import org.elasticsearch.Version;
 
@@ -35,7 +33,6 @@ import io.crate.analyze.expressions.ExpressionAnalyzer;
 import io.crate.analyze.relations.DocTableRelation;
 import io.crate.analyze.relations.NameFieldProvider;
 import io.crate.exceptions.ColumnUnknownException;
-import io.crate.expression.symbol.RefVisitor;
 import io.crate.expression.symbol.SymbolType;
 import io.crate.metadata.ColumnIdent;
 import io.crate.metadata.CoordinatorTxnCtx;
@@ -91,9 +88,8 @@ public class AlterTableDropColumnAnalyzer {
                 }
             }
         }
-        dropColumns = validateDynamic(tableInfo, dropColumns);
         validateStatic(tableInfo, dropColumns);
-
+        tableInfo.dropColumns(dropColumns);
         return new AnalyzedAlterTableDropColumn(tableInfo, dropColumns);
     }
 
@@ -119,13 +115,6 @@ public class AlterTableDropColumnAnalyzer {
                 throw new UnsupportedOperationException("Dropping INDEX column '" + colToDrop.fqn() + "' is not supported");
             }
 
-            for (var indexRef : tableInfo.indexColumns()) {
-                if (indexRef.columns().contains(refToDrop)) {
-                    throw new UnsupportedOperationException("Dropping column: " + colToDrop.sqlFqn() + " which " +
-                                                            "is part of INDEX: " + indexRef +
-                                                            " is not allowed");
-                }
-            }
             if (tableInfo.primaryKey().contains(colToDrop)) {
                 throw new UnsupportedOperationException("Dropping column: " + colToDrop.sqlFqn() + " which " +
                                                         "is part of the PRIMARY KEY is not allowed");
@@ -139,71 +128,5 @@ public class AlterTableDropColumnAnalyzer {
                                                         "is part of the 'PARTITIONED BY' columns is not allowed");
             }
         }
-    }
-
-    /** Validate restrictions based on properties that change and need to be rechecked during execution*/
-    public static List<DropColumn> validateDynamic(DocTableInfo tableInfo, List<DropColumn> dropColumns) {
-        var generatedColRefs = new HashSet<>();
-        for (var genRef : tableInfo.generatedColumns()) {
-            generatedColRefs.addAll(genRef.referencedReferences());
-        }
-        var leftOverCols = tableInfo.columns().stream().map(Reference::column).collect(Collectors.toSet());
-        ArrayList<DropColumn> validatedDropCols = new ArrayList<>(dropColumns.size());
-
-        for (int i = 0 ; i < dropColumns.size(); i++) {
-            var refToDrop = dropColumns.get(i).ref();
-            var colToDrop = refToDrop.column();
-
-            for (var indexRef : tableInfo.indexColumns()) {
-                if (indexRef.columns().contains(refToDrop)) {
-                    throw new UnsupportedOperationException("Dropping column: " + colToDrop.sqlFqn() + " which " +
-                                                            "is part of INDEX: " + indexRef + " is not allowed");
-                }
-            }
-            for (var genRef : tableInfo.generatedColumns()) {
-                if (genRef.referencedReferences().contains(refToDrop)) {
-                    throw new UnsupportedOperationException(String.format(
-                        Locale.ENGLISH,
-                        "Cannot drop column `%s`. It's used in generated column `%s`: %s",
-                        colToDrop.sqlFqn(),
-                        genRef.column().sqlFqn(),
-                        genRef.formattedGeneratedExpression()
-                    ));
-                }
-            }
-            for (var checkConstraint : tableInfo.checkConstraints()) {
-                Set<ColumnIdent> columnsInConstraint = new HashSet<>();
-                RefVisitor.visitRefs(checkConstraint.expression(), r -> columnsInConstraint.add(r.column()));
-                if (columnsInConstraint.size() > 1 && columnsInConstraint.contains(colToDrop)) {
-                    throw new UnsupportedOperationException("Dropping column: " + colToDrop.sqlFqn() + " which " +
-                        "is used in CHECK CONSTRAINT: " + checkConstraint.name() + " is not allowed");
-                }
-                boolean constraintColIsSubColOfColToDrop = false;
-                for (var columnInConstraint : columnsInConstraint) {
-                    if (columnInConstraint.isChildOf(colToDrop)) {
-                        constraintColIsSubColOfColToDrop = true; // subcol of the dropped col referred in constraint
-                    }
-                }
-                if (constraintColIsSubColOfColToDrop) {
-                    for (var columnInConstraint : columnsInConstraint) {
-                        // Check if sibling, parent, or cols of another object are contained in the same constraint
-                        if (columnInConstraint.isChildOf(colToDrop) == false
-                            && columnInConstraint.path().equals(colToDrop.path()) == false) {
-                            throw new UnsupportedOperationException("Dropping column: " + colToDrop.sqlFqn() +
-                                " which is used in CHECK CONSTRAINT: " + checkConstraint.name() +
-                                " is not allowed");
-                        }
-                    }
-                }
-            }
-
-            leftOverCols.remove(colToDrop);
-            validatedDropCols.add(new DropColumn(refToDrop, dropColumns.get(i).ifExists()));
-        }
-
-        if (leftOverCols.isEmpty()) {
-            throw new UnsupportedOperationException("Dropping all columns of a table is not allowed");
-        }
-        return validatedDropCols;
     }
 }

server/src/main/java/io/crate/analyze/BoundCreateTable.java
@@ -73,7 +73,7 @@ public record BoundCreateTable(
         return Lists2.map(partitionedByColumns, BoundCreateTable::toPartitionMapping);
     }
 
-    private static List<String> toPartitionMapping(Symbol symbol) {
+    public static List<String> toPartitionMapping(Symbol symbol) {
         String fqn = Symbols.pathFromSymbol(symbol).fqn();
         String typeMappingName = DataTypes.esMappingNameFrom(symbol.valueType().id());
         return List.of(fqn, typeMappingName);

commit 51b96dee0449f5e2c27000d427eed8b0cb28c67f

Preserve the number of shards of existing partitions when altering the table
Fixes a regression introduced by https://github.com/crate/crate/commit/111ffe166e523a4a5cd3278975772ce365112b64
which falsely applied the current number of shards of the parititioned
table to existing partitions when altering the table.
This causes an exception as changing the number of shards is only
possible by using the special `resize` operation.

Fixes #16521.

server/src/main/java/io/crate/metadata/doc/DocTableInfo.java
@@ -85,6 +85,7 @@ import io.crate.expression.symbol.format.Style;
 import io.crate.metadata.ColumnIdent;
 import io.crate.metadata.CoordinatorTxnCtx;
 import io.crate.metadata.GeneratedReference;
+import io.crate.metadata.IndexParts;
 import io.crate.metadata.IndexReference;
 import io.crate.metadata.NodeContext;
 import io.crate.metadata.PartitionInfo;
@@ -1055,10 +1056,17 @@ public class DocTableInfo implements TableInfo, ShardedTable, StoredTable {
             if (allColumns.size() > allowedTotalColumns) {
                 throw new IllegalArgumentException("Limit of total columns [" + allowedTotalColumns + "] in table [" + ident + "] exceeded");
             }
+            var indexNumberOfShards = numberOfShards;
+            if (isPartitioned && IndexParts.isPartitioned(indexName)) {
+                // if the index is a part of a partitioned table,
+                // the actual value of the index must be used as the value for the whole partitioned table may have changed
+                indexNumberOfShards = indexMetadata.getNumberOfShards();
+            }
+
             metadataBuilder.put(
                 IndexMetadata.builder(indexMetadata)
                     .putMapping(new MappingMetadata(mapping))
-                    .numberOfShards(numberOfShards)
+                    .numberOfShards(indexNumberOfShards)
                     .mappingVersion(indexMetadata.getMappingVersion() + 1)
             );
         }

server/src/test/java/io/crate/metadata/doc/DocTableInfoTest.java
@@ -22,7 +22,7 @@
 package io.crate.metadata.doc;
 
 import static io.crate.testing.Asserts.assertThat;
-import static org.assertj.core.api.Assertions.assertThat;
+import static java.util.Collections.singletonList;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.elasticsearch.cluster.metadata.Metadata.COLUMN_OID_UNASSIGNED;
 
@@ -36,6 +36,7 @@ import org.assertj.core.api.Assertions;
 import org.elasticsearch.Version;
 import org.elasticsearch.cluster.ClusterState;
 import org.elasticsearch.cluster.metadata.IndexMetadata;
+import org.elasticsearch.cluster.metadata.IndexTemplateMetadata;
 import org.elasticsearch.cluster.metadata.Metadata;
 import org.elasticsearch.common.settings.Settings;
 import org.junit.Test;
@@ -49,6 +50,7 @@ import io.crate.expression.symbol.VoidReference;
 import io.crate.metadata.ColumnIdent;
 import io.crate.metadata.IndexReference;
 import io.crate.metadata.IndexType;
+import io.crate.metadata.PartitionName;
 import io.crate.metadata.Reference;
 import io.crate.metadata.ReferenceIdent;
 import io.crate.metadata.RelationName;
@@ -60,6 +62,7 @@ import io.crate.metadata.table.TableInfo;
 import io.crate.sql.tree.ColumnPolicy;
 import io.crate.test.integration.CrateDummyClusterServiceUnitTest;
 import io.crate.testing.SQLExecutor;
+import io.crate.testing.UseRandomizedSchema;
 import io.crate.types.DataType;
 import io.crate.types.DataTypes;
 import io.crate.types.ObjectType;
@@ -568,5 +571,47 @@ public class DocTableInfoTest extends CrateDummyClusterServiceUnitTest {
         assertThat(table.getReference(oo)).hasName("o['o']")
             .hasType(ooType);
         assertThat(table.getReference(o)).hasName("o").hasType(oType);
+    }
+
+
+    /**
+     * Tests a regression introduced by https://github.com/crate/crate/commit/111ffe166e523a4a5cd3278975772ce365112b64
+     * where the number of shards of a partitioned table was not preserved when writing the table info to metadata.
+     */
+    @UseRandomizedSchema(random = false)
+    @Test
+    public void test_write_to_preserves_number_of_shards_of_partitions() throws Exception {
+        var partitionIndexName = new PartitionName(new RelationName("doc", "tbl"), singletonList("1")).asIndexName();
+        SQLExecutor e = SQLExecutor.of(clusterService)
+            .addPartitionedTable(
+                """
+                create table tbl (
+                    id int,
+                    p int
+                ) clustered into 2 shards partitioned by (p) with (number_of_replicas=0)
+                """,
+                partitionIndexName
+            );
+
+        ClusterState state = clusterService.state();
+        Metadata metadata = state.metadata();
+
+        // Change the number of shards of the partition table/template aka. ALTER TABLE tbl SET (number_of_shards=3)
+        var oldTemplate = metadata.templates().get(PartitionName.templateName("doc", "tbl"));
+        var newTemplate = new IndexTemplateMetadata.Builder(oldTemplate);
+        newTemplate.settings(Settings.builder().put(oldTemplate.settings()).put("index.number_of_shards", 3));
+
+        Metadata.Builder builder = new Metadata.Builder(metadata);
+        builder.put(newTemplate);
+        var newMetadata = builder.build();
+
+        DocTableInfoFactory docTableInfoFactory = new DocTableInfoFactory(e.nodeCtx);
+        DocTableInfo tbl = docTableInfoFactory.create(RelationName.fromIndexName("tbl"), newMetadata);
+
+        var newBuilder = new Metadata.Builder(metadata);
+        tbl.writeTo(newMetadata, newBuilder);
+
+        var partitionIndex = newBuilder.build().index(partitionIndexName);
+        assertThat(partitionIndex.getNumberOfShards()).isEqualTo(2);
     }
 }