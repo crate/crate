ddca5c0b7ace095aee2c9f92af64d1b6764597b0
Mathias Fussenegger <f.mathias@zignar.net>
2024-08-28 14:49:26 +0200

    Remove unused properties from IndexStats and IndexShardStats

--- server/src/test/java/io/crate/integrationtests/DiskThresholdDeciderIT.java
+++ server/src/test/java/io/crate/integrationtests/DiskThresholdDeciderIT.java
@@ -37,6 +37,7 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
@@ -250,9 +251,9 @@ public class DiskThresholdDeciderIT extends IntegTestCase {
             refresh();
 
             var indicesStats = client().admin().indices().stats(new IndicesStatsRequest().indices(indexName).store(true)).get();
-            final ShardStats[] shardStatses = indicesStats.getIndex(indexName).getShards();
+            final List<ShardStats> shardStatses = indicesStats.getIndex(indexName).getShards();
 
-            final long[] shardSizes = new long[shardStatses.length];
+            final long[] shardSizes = new long[shardStatses.size()];
             for (ShardStats shardStats : shardStatses) {
                 shardSizes[shardStats.getShardRouting().id()] = shardStats.getStats().getStore().sizeInBytes();
             }
