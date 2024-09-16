80a508aa7a3c46b1eccfdf9f4adca224715a72d9
jeeminso <jeeminso@gmail.com>
2024-09-16 14:26:12 -0400

    Do not treat LHS of `ALL` operator as patterns

--- server/src/main/java/io/crate/expression/operator/all/AllNotLikeOperator.java
+++ server/src/main/java/io/crate/expression/operator/all/AllNotLikeOperator.java
@@ -50,9 +50,7 @@ public final class AllNotLikeOperator extends AllOperator<String> {
 
     @Override
     boolean matches(String probe, String candidate) {
-        // Accept both sides of arguments to be patterns
-        return !LikeOperators.matches(probe, candidate, LikeOperators.DEFAULT_ESCAPE, caseSensitivity) &&
-            !LikeOperators.matches(candidate, probe, LikeOperators.DEFAULT_ESCAPE, caseSensitivity);
+        return !LikeOperators.matches(probe, candidate, LikeOperators.DEFAULT_ESCAPE, caseSensitivity);
     }
 
     @Override
