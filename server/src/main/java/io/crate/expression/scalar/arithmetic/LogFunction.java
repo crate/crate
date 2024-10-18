2a65bada7f214fe6e77d0a0cb0287a14c9c4300a
Marios Trivyzas <matriv@gmail.com>
2024-10-18 22:36:29 +0300

    Add support for `NUMERIC` arg to `log()` and `ln()`
    Add support to `ln()` and `log()` but only for the one arg version,
    so the log base 10 version, as the `BigDecimalMath` library doesn't
    support log with arbitrary base.
    
    Since `BigDecimalMaths`'s `log10` and `ln` throw different error
    messages, already during their calculations, change `validateResult`
    to `validateArgument` to do a consistent pre-validation for doubles
    and BigDecimals.
    
    Follows: #16818
    Relates: #15632

--- server/src/main/java/io/crate/expression/scalar/arithmetic/LogFunction.java
+++ server/src/main/java/io/crate/expression/scalar/arithmetic/LogFunction.java
@@ -21,7 +21,12 @@
 
 package io.crate.expression.scalar.arithmetic;
 
+import java.math.BigDecimal;
+import java.math.MathContext;
+
+import ch.obermuhlner.math.big.BigDecimalMath;
 import io.crate.data.Input;
+import io.crate.expression.scalar.UnaryScalar;
 import io.crate.metadata.FunctionType;
 import io.crate.metadata.Functions;
 import io.crate.metadata.NodeContext;
@@ -47,18 +52,39 @@ public abstract class LogFunction extends Scalar<Number, Number> {
     }
 
     /**
-     * @param result the value to validate
+     * @param argument the value to validate
      * @param caller used in the error message for clarification purposes.
-     * @return the validated result
+     * @return the validated argument
      */
-    Double validateResult(Double result, String caller) {
-        if (result == null) {
+    Double validateArgument(Double argument, String caller) {
+        if (argument == null) {
             return null;
         }
-        if (Double.isNaN(result) || Double.isInfinite(result)) {
-            throw new IllegalArgumentException(caller + ": given arguments would result in: '" + result + "'");
+        if (argument == 0.0) {
+            throw new IllegalArgumentException(caller + ": given arguments would result in: '-Infinity'");
         }
-        return result;
+        if (argument < 0.0) {
+            throw new IllegalArgumentException(caller + ": given arguments would result in: 'NaN'");
+        }
+        return argument;
+    }
+
+    /**
+     * @param argument the value to validate
+     * @param caller used in the error message for clarification purposes.
+     * @return the validated argument
+     */
+    static BigDecimal validateArgument(BigDecimal argument, String caller) {
+        if (argument == null) {
+            return null;
+        }
+        if (argument.compareTo(BigDecimal.ZERO) == 0) {
+            throw new IllegalArgumentException(caller + ": given arguments would result in: '-Infinity'");
+        }
+        if (argument.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException(caller + ": given arguments would result in: 'NaN'");
+        }
+        return argument;
     }
 
     static class LogBaseFunction extends LogFunction {
@@ -90,12 +116,14 @@ public abstract class LogFunction extends Scalar<Number, Number> {
                 return null;
             }
             double value = value1.doubleValue();
+            validateArgument(value, "log(x, b)");
             double base = value2.doubleValue();
+            validateArgument(base, "log(x, b)");
             double baseResult = Math.log(base);
             if (baseResult == 0) {
                 throw new IllegalArgumentException("log(x, b): given 'base' would result in a division by zero.");
             }
-            return validateResult(Math.log(value) / baseResult, "log(x, b)");
+            return Math.log(value) / baseResult;
         }
 
     }
@@ -111,6 +139,20 @@ public abstract class LogFunction extends Scalar<Number, Number> {
                     .features(Feature.DETERMINISTIC, Feature.STRICTNULL)
                     .build(),
                 Log10Function::new
+            );
+            // log(numeric) : numeric
+            builder.add(
+                Signature.builder(NAME, FunctionType.SCALAR)
+                    .argumentTypes(DataTypes.NUMERIC.getTypeSignature())
+                    .returnType(DataTypes.NUMERIC.getTypeSignature())
+                    .features(Feature.DETERMINISTIC, Feature.STRICTNULL)
+                    .build(),
+                (signature, boundSignature) -> new UnaryScalar<>(
+                    signature,
+                    boundSignature,
+                    DataTypes.NUMERIC,
+                    x -> BigDecimalMath.log10(validateArgument(x, "log(x)"), MathContext.DECIMAL128)
+                )
             );
         }
 
@@ -129,9 +171,8 @@ public abstract class LogFunction extends Scalar<Number, Number> {
         }
 
         protected Double evaluate(double value) {
-            return validateResult(Math.log10(value), "log(x)");
+            return Math.log10(validateArgument(value, "log(x)"));
         }
-
     }
 
     public static class LnFunction extends Log10Function {
@@ -146,6 +187,20 @@ public abstract class LogFunction extends Scalar<Number, Number> {
                     .build(),
                 LnFunction::new
             );
+            // ln(numeric) : numeric
+            builder.add(
+                Signature.builder(LnFunction.NAME, FunctionType.SCALAR)
+                    .argumentTypes(DataTypes.NUMERIC.getTypeSignature())
+                    .returnType(DataTypes.NUMERIC.getTypeSignature())
+                    .features(Feature.DETERMINISTIC, Feature.STRICTNULL)
+                    .build(),
+                (signature, boundSignature) -> new UnaryScalar<>(
+                    signature,
+                    boundSignature,
+                    DataTypes.NUMERIC,
+                    x -> BigDecimalMath.log(validateArgument(x, "ln(x)"), MathContext.DECIMAL128)
+                )
+            );
         }
 
         public static final String NAME = "ln";
@@ -156,7 +211,7 @@ public abstract class LogFunction extends Scalar<Number, Number> {
 
         @Override
         protected Double evaluate(double value) {
-            return validateResult(Math.log(value), "ln(x)");
+            return Math.log(validateArgument(value, "ln(x)"));
         }
     }
 }
