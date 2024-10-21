bbbd5489dc26362ebb80124d4a9dd90e96dc2bcf
Mathias Fussenegger <f.mathias@zignar.net>
2024-10-21 16:36:35 +0200

    Remove toXContent from AllocationCommand

--- server/src/testFixtures/java/org/elasticsearch/test/XContentTestUtils.java
+++ server/src/testFixtures/java/org/elasticsearch/test/XContentTestUtils.java
@@ -21,7 +21,6 @@ package org.elasticsearch.test;
 
 import static com.carrotsearch.randomizedtesting.generators.RandomStrings.randomAsciiOfLength;
 import static io.crate.server.xcontent.XContentHelper.createParser;
-import static org.elasticsearch.common.xcontent.ToXContent.EMPTY_PARAMS;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -36,106 +35,16 @@ import java.util.stream.Collectors;
 import org.elasticsearch.common.bytes.BytesReference;
 import org.elasticsearch.common.xcontent.DeprecationHandler;
 import org.elasticsearch.common.xcontent.NamedXContentRegistry;
-import org.elasticsearch.common.xcontent.ToXContent;
 import org.elasticsearch.common.xcontent.XContent;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.common.xcontent.XContentParser;
 import org.elasticsearch.common.xcontent.XContentType;
-import org.elasticsearch.common.xcontent.json.JsonXContent;
 import org.elasticsearch.test.rest.yaml.ObjectPath;
 
-import io.crate.server.xcontent.XContentHelper;
-
 public final class XContentTestUtils {
+
     private XContentTestUtils() {
 
-    }
-
-    public static Map<String, Object> convertToMap(ToXContent part) throws IOException {
-        XContentBuilder builder = JsonXContent.builder();
-        builder.startObject();
-        part.toXContent(builder, EMPTY_PARAMS);
-        builder.endObject();
-        return XContentHelper.convertToMap(BytesReference.bytes(builder), false, builder.contentType()).map();
-    }
-
-
-    /**
-     * Compares two maps generated from XContentObjects. The order of elements in arrays is ignored.
-     *
-     * @return null if maps are equal or path to the element where the difference was found
-     */
-    public static String differenceBetweenMapsIgnoringArrayOrder(Map<String, Object> first, Map<String, Object> second) {
-        return differenceBetweenMapsIgnoringArrayOrder("", first, second);
-    }
-
-    private static String differenceBetweenMapsIgnoringArrayOrder(String path, Map<String, Object> first, Map<String, Object> second) {
-        if (first.size() != second.size()) {
-            return path + ": sizes of the maps don't match: " + first.size() + " != " + second.size();
-        }
-
-        for (String key : first.keySet()) {
-            String reason = differenceBetweenObjectsIgnoringArrayOrder(path + "/" + key, first.get(key), second.get(key));
-            if (reason != null) {
-                return reason;
-            }
-        }
-        return null;
-    }
-
-    @SuppressWarnings("unchecked")
-    private static String differenceBetweenObjectsIgnoringArrayOrder(String path, Object first, Object second) {
-        if (first == null) {
-            if (second == null) {
-                return null;
-            } else {
-                return path + ": first element is null, the second element is not null";
-            }
-        } else if (first instanceof List) {
-            if (second instanceof List) {
-                List<Object> secondList = new ArrayList<>((List<Object>) second);
-                List<Object> firstList = (List<Object>) first;
-                if (firstList.size() == secondList.size()) {
-                    String reason = path + ": no matches found";
-                    for (Object firstObj : firstList) {
-                        boolean found = false;
-                        for (Object secondObj : secondList) {
-                            reason = differenceBetweenObjectsIgnoringArrayOrder(path + "/*", firstObj, secondObj);
-                            if (reason == null) {
-                                secondList.remove(secondObj);
-                                found = true;
-                                break;
-                            }
-                        }
-                        if (found == false) {
-                            return reason;
-                        }
-                    }
-                    if (secondList.isEmpty()) {
-                        return null;
-                    } else {
-                        return path + ": the second list is not empty";
-                    }
-                } else {
-                    return path + ": sizes of the arrays don't match: " + firstList.size() + " != " + secondList.size();
-                }
-            } else {
-                return path + ": the second element is not an array";
-            }
-        } else if (first instanceof Map) {
-            if (second instanceof Map) {
-                return differenceBetweenMapsIgnoringArrayOrder(path, (Map<String, Object>) first, (Map<String, Object>) second);
-            } else {
-                return path + ": the second element is not a map (got " + second +")";
-            }
-        } else {
-            if (first.equals(second)) {
-                return null;
-            } else {
-                return path + ": the elements don't match: [" + first + "] != [" + second + "]";
-            }
-
-        }
     }
 
     /**
