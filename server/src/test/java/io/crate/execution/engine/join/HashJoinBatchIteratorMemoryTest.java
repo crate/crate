e765877721270f8e0e65c0d070e756c141559475
Michael Kleen <mkleen@gmail.com>
2024-10-24 14:16:56 +0200

    Add left join to HashJoinBatchIteratorMemoryTest

--- server/src/test/java/io/crate/execution/engine/join/HashJoinBatchIteratorMemoryTest.java
+++ server/src/test/java/io/crate/execution/engine/join/HashJoinBatchIteratorMemoryTest.java
@@ -59,7 +59,7 @@ public class HashJoinBatchIteratorMemoryTest {
     }
 
     @Test
-    public void testReleaseAccountingRows() throws Exception {
+    public void test_release_accounting_rows_inner_join() throws Exception {
         BatchSimulatingIterator<Row> leftIterator = new BatchSimulatingIterator<>(
             TestingBatchIterators.range(0, 12),
             3,
@@ -82,6 +82,38 @@ public class HashJoinBatchIteratorMemoryTest {
             getHashForRight(),
             ignored -> 2,
             false
+        );
+        TestingRowConsumer consumer = new TestingRowConsumer();
+        consumer.accept(it, null);
+        consumer.getResult();
+        verify(rowAccounting, times(8)).release();
+        verify(rowAccounting, times(12)).accountForAndMaybeBreak(Mockito.any(Object[].class));
+    }
+
+    @Test
+    public void test_release_accounting_rows_left_outer_join() throws Exception {
+        BatchSimulatingIterator<Row> leftIterator = new BatchSimulatingIterator<>(
+            TestingBatchIterators.range(0, 12),
+            3,
+            3,
+            null
+        );
+        BatchIterator<Row> rightIterator = new BatchSimulatingIterator<>(TestingBatchIterators.range(0, 10), 2, 4, null);
+
+        when(circuitBreaker.getLimit()).thenReturn(110L);
+        when(circuitBreaker.getUsed()).thenReturn(10L);
+
+        RowAccounting<Object[]> rowAccounting = mock(RowAccounting.class);
+        BatchIterator<Row> it = new HashJoinBatchIterator(
+            leftIterator,
+            rightIterator,
+            rowAccounting,
+            new CombinedRow(1, 1),
+            getCol0EqCol1JoinCondition(),
+            getHashForLeft(),
+            getHashForRight(),
+            ignored -> 2,
+            true
         );
         TestingRowConsumer consumer = new TestingRowConsumer();
         consumer.accept(it, null);
