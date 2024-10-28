9252bfaa7eb41f3de40d17d3a831f83a2e3bcccf
jeeminso <jeeminso@gmail.com>
2024-10-27 21:23:20 -0400

    Fix filtering by comparisons of different data types
    (cherry picked from commit 77caeaf06fa6b211f18995099e99c0ab74c0a267)

--- server/src/main/java/io/crate/planner/optimizer/symbol/rule/SwapCastsInComparisonOperators.java
+++ server/src/main/java/io/crate/planner/optimizer/symbol/rule/SwapCastsInComparisonOperators.java
@@ -23,15 +23,19 @@ package io.crate.planner.optimizer.symbol.rule;
 
 import static io.crate.expression.operator.Operators.COMPARISON_OPERATORS;
 import static io.crate.planner.optimizer.matcher.Pattern.typeOf;
+import static io.crate.types.DataTypes.isSafeConversion;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import io.crate.expression.scalar.cast.CastMode;
 import io.crate.expression.scalar.cast.ImplicitCastFunction;
 import io.crate.expression.symbol.Function;
+import io.crate.expression.symbol.Literal;
 import io.crate.expression.symbol.Symbol;
 import io.crate.expression.symbol.SymbolType;
 import io.crate.metadata.NodeContext;
+import io.crate.metadata.Reference;
 import io.crate.planner.optimizer.matcher.Capture;
 import io.crate.planner.optimizer.matcher.Captures;
 import io.crate.planner.optimizer.matcher.Pattern;
@@ -41,11 +45,50 @@ import io.crate.types.ArrayType;
 import io.crate.types.DataType;
 import io.crate.types.DataTypes;
 
-
 public class SwapCastsInComparisonOperators implements Rule<Function> {
 
     private final Capture<Function> castCapture;
     private final Pattern<Function> pattern;
+
+    /**
+     * A cast is 'swappable' if the cast does not cause loss of precision. This method executes the casts in uncertain
+     * situations. i.e.:: casting 126 type of int to a byte, which is a narrowing conversion, is convertable since 126
+     * does fit in a byte.
+     */
+    private static boolean isSwappable(Function cmpOp) {
+        if (cmpOp.arguments().get(1) instanceof Literal<?> literal) {
+            Function cast = (Function) cmpOp.arguments().getFirst();
+            Reference ref = (Reference) cast.arguments().getFirst();
+            DataType<?> refInnerType = ArrayType.unnest(ref.valueType());
+            DataType<?> literalInnerType = ArrayType.unnest(literal.valueType());
+            if (!DataTypes.isNumeric(literalInnerType) || !DataTypes.isNumeric(refInnerType)) {
+                return true;
+            }
+            if (isSafeConversion(literalInnerType, refInnerType)) {
+                return true;
+            }
+            return isNarrowingConversionPossible(literal.value(), ref.valueType());
+        }
+        return true;
+    }
+
+    /**
+     * Executes a narrowing conversion to verify that it is possible.
+     */
+    private static boolean isNarrowingConversionPossible(Object value, DataType<?> targetType) {
+        if (DataTypes.isArray(targetType)) {
+            return ((List<?>) value).stream().allMatch(e -> isNarrowingConversionPossible(e, ((ArrayType<?>) targetType).innerType()));
+        } else {
+            try {
+                Number n = (Number) value;
+                var literalStr = String.valueOf(n);
+                var castedStr = String.valueOf(targetType.implicitCast(n));
+                return new BigDecimal(literalStr).compareTo(new BigDecimal(castedStr)) == 0;
+            } catch (IllegalArgumentException | ClassCastException e) {
+                return false;
+            }
+        }
+    }
 
     public SwapCastsInComparisonOperators() {
         this.castCapture = new Capture<>();
@@ -56,7 +99,7 @@ public class SwapCastsInComparisonOperators implements Rule<Function> {
                 // We have to respect explicit casts, see https://github.com/crate/crate/issues/12135
                 .with(f -> f.name().equals(ImplicitCastFunction.NAME))
                 .with(f -> f.arguments().get(0).symbolType() == SymbolType.REFERENCE)
-            );
+            ).with(SwapCastsInComparisonOperators::isSwappable);
     }
 
     @Override
@@ -74,14 +117,6 @@ public class SwapCastsInComparisonOperators implements Rule<Function> {
         var castFunction = captures.get(castCapture);
         var reference = castFunction.arguments().get(0);
         DataType<?> targetType = reference.valueType();
-
-        // For `WHERE <numeric_array_ref> = <numeric_array_literal>`, where the ref is type of NUMERIC(x,y) and casted
-        // to NUMERIC(null,null), the cast to be moved must be NUMERIC(null, null) not NUMERIC(x,y). Because NUMERIC(x,y)
-        // may cause the numeric array literal to be rounded.
-        if (ArrayType.unnest(targetType).id() == DataTypes.NUMERIC.id()) {
-            targetType = castFunction.valueType();
-        }
-
         CastMode castMode = castFunction.castMode();
         assert castMode != null : "Pattern matched, function must be a cast";
         Symbol castedLiteral = literalOrParam.cast(targetType, castMode);
