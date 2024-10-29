06a83ce3f994fb75808df6a198200d7d1e08ab3a
baur <baurzhansahariev@gmail.com>
2024-10-29 10:14:32 +0100

    Fix BWC issue when reading Role.Properties from the cluster state
    (cherry picked from commit dea6e9b5ff707dd58ab9cebf9219d4565dd42e50)

--- server/src/main/java/io/crate/role/Role.java
+++ server/src/main/java/io/crate/role/Role.java
@@ -118,7 +118,7 @@ public class Role implements Writeable, ToXContent {
             boolean login = false;
             SecureHash secureHash = null;
             JwtProperties jwtProperties = null;
-            Map<String, Object> sessionSettings = null;
+            Map<String, Object> sessionSettings = new HashMap<>();
             while (parser.nextToken() == XContentParser.Token.FIELD_NAME) {
                 switch (parser.currentName()) {
                     case "login":
