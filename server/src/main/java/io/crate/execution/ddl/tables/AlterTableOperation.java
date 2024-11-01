defaaa6960c9773a38b483058a2d556f52ce6f2f
Sebastian Utz <su@rtme.net>
2024-11-01 15:03:54 +0100

    Forbid adding a sub-column to an object(ignored) if table isn't empty
    If the table is not empty it may already contain data for the
    explicit added new column. The data may be of a different type,
    which then breaks collecting and streaming the data.
    Thus this may break querying the data or running `ANALYZE`.
    
    As we do not know which data the table already has without processing
    each row, we simply throw an exception if we detect that the table
    isn't empty.
    
    (cherry picked from commit d150a2e57f8a6da80bacf146f934d8e0cd87466b)

--- server/src/main/java/io/crate/execution/ddl/tables/AlterTableOperation.java
+++ server/src/main/java/io/crate/execution/ddl/tables/AlterTableOperation.java
@@ -53,8 +53,6 @@ import org.jetbrains.annotations.Nullable;
 import org.jetbrains.annotations.VisibleForTesting;
 
 import io.crate.action.FutureActionListener;
-import io.crate.session.CollectingResultReceiver;
-import io.crate.session.Sessions;
 import io.crate.analyze.AnalyzedAlterTableRenameTable;
 import io.crate.analyze.BoundAlterTable;
 import io.crate.data.Row;
@@ -64,10 +62,14 @@ import io.crate.execution.support.ChainableAction;
 import io.crate.execution.support.ChainableActions;
 import io.crate.metadata.GeneratedReference;
 import io.crate.metadata.PartitionName;
+import io.crate.metadata.Reference;
 import io.crate.metadata.RelationName;
 import io.crate.metadata.table.TableInfo;
 import io.crate.replication.logical.LogicalReplicationService;
 import io.crate.replication.logical.metadata.Publication;
+import io.crate.session.CollectingResultReceiver;
+import io.crate.session.Sessions;
+import io.crate.sql.tree.ColumnPolicy;
 
 @Singleton
 public class AlterTableOperation {
@@ -122,11 +124,20 @@ public class AlterTableOperation {
     }
 
     public CompletableFuture<Long> addColumn(AddColumnRequest addColumnRequest) {
+        var newReferences = addColumnRequest.references();
         String subject = null;
         if (addColumnRequest.pKeyIndices().isEmpty() == false) {
             subject = "primary key";
         } else if (addColumnRequest.references().stream().anyMatch(ref -> ref instanceof GeneratedReference)) {
             subject = "generated";
+        } else {
+            for (Reference newRef : newReferences) {
+                if (newReferences.stream().anyMatch(r -> r.column().isChildOf(newRef.column()))
+                    && newRef.columnPolicy().equals(ColumnPolicy.IGNORED)) {
+                    subject = "sub column to an OBJECT(IGNORED) parent";
+                    break;
+                }
+            }
         }
         if (subject != null) {
             String finalSubject = subject;
