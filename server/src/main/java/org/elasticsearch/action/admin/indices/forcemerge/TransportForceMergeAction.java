b3fc83e6434d063678240766856a2c4a9e9e7b1d
Alan Woodward <romseygeek@apache.org>
2024-11-04 15:24:47 +0000

    Allow TransportBroadcastByNodeAction subclasses to respond asynchronously

--- server/src/main/java/org/elasticsearch/action/admin/indices/forcemerge/TransportForceMergeAction.java
+++ server/src/main/java/org/elasticsearch/action/admin/indices/forcemerge/TransportForceMergeAction.java
@@ -19,6 +19,10 @@
 
 package org.elasticsearch.action.admin.indices.forcemerge;
 
+import java.io.IOException;
+import java.util.List;
+
+import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.action.support.DefaultShardOperationFailedException;
 import org.elasticsearch.action.support.broadcast.node.TransportBroadcastByNodeAction;
 import org.elasticsearch.cluster.ClusterState;
@@ -33,9 +37,6 @@ import org.elasticsearch.index.shard.IndexShard;
 import org.elasticsearch.indices.IndicesService;
 import org.elasticsearch.threadpool.ThreadPool;
 import org.elasticsearch.transport.TransportService;
-
-import java.io.IOException;
-import java.util.List;
 
 /**
  * ForceMerge index/indices action.
@@ -75,10 +76,10 @@ public class TransportForceMergeAction extends TransportBroadcastByNodeAction<Fo
     }
 
     @Override
-    protected EmptyResult shardOperation(ForceMergeRequest request, ShardRouting shardRouting) throws IOException {
+    protected void shardOperation(ForceMergeRequest request, ShardRouting shardRouting, ActionListener<EmptyResult> listener) throws IOException {
         IndexShard indexShard = indicesService.indexServiceSafe(shardRouting.shardId().getIndex()).getShard(shardRouting.shardId().id());
         indexShard.forceMerge(request);
-        return EmptyResult.INSTANCE;
+        listener.onResponse(EmptyResult.INSTANCE);
     }
 
     /**
