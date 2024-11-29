7bef789ccad250d70b4d4e977526f173cabdd054
baur <baurzhansahariev@gmail.com>
2024-11-29 09:44:21 +0100

    Eval prunning must also check that there are no outputs to prune
    Following documented behavior:
    > If there are no outputs to prune and if the source also didn't change, `this` must be returned
    
    (cherry picked from commit 38226a5d019873e01d51ca779c0ad457db50e082)
    
    # Conflicts:
    #	server/src/test/java/io/crate/integrationtests/JoinIntegrationTest.java
    #	server/src/test/java/io/crate/integrationtests/LookupJoinIntegrationTest.java
    #	server/src/test/java/io/crate/planner/ViewPlannerTest.java
    #	server/src/test/java/io/crate/planner/operators/JoinTest.java

--- server/src/test/java/io/crate/integrationtests/JoinIntegrationTest.java
+++ server/src/test/java/io/crate/integrationtests/JoinIntegrationTest.java
@@ -1538,12 +1538,11 @@ public class JoinIntegrationTest extends IntegTestCase {
         var stmt = "SELECT t3.e FROM t1 JOIN t3 ON t1.b = t3.f JOIN t2 ON t1.a = t2.c WHERE t2.d =t3.e";
         assertThat(execute("explain " + stmt)).hasLines(
                 "Eval[e] (rows=0)",
-                "  └ Eval[b, a, e, f, c, d] (rows=0)",
-                "    └ HashJoin[((a = c) AND (d = e))] (rows=0)",
-                "      ├ Collect[doc.t2 | [c, d] | true] (rows=2)",
-                "      └ HashJoin[(b = f)] (rows=1)",
-                "        ├ Collect[doc.t1 | [b, a] | true] (rows=1)",
-                "        └ Collect[doc.t3 | [e, f] | true] (rows=1)"
+                "  └ HashJoin[((a = c) AND (d = e))] (rows=0)",
+                "    ├ Collect[doc.t2 | [c, d] | true] (rows=2)",
+                "    └ HashJoin[(b = f)] (rows=1)",
+                "      ├ Collect[doc.t1 | [b, a] | true] (rows=1)",
+                "      └ Collect[doc.t3 | [e, f] | true] (rows=1)"
         );
 
         execute(stmt);
