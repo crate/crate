0d330469fd25865f25c6d870c7c49c1e051bfa2f
Michael Kleen <mkleen@gmail.com>
2024-12-02 16:55:02 +0100

    Move cross-join to inner-join logic into Optimizer
    This moves the logic to convert a filter with an equi-join condition
    on top of cross-join into an optimizer rule.

--- server/src/test/java/io/crate/integrationtests/ShowIntegrationTest.java
+++ server/src/test/java/io/crate/integrationtests/ShowIntegrationTest.java
@@ -434,6 +434,7 @@ public class ShowIntegrationTest extends IntegTestCase {
             "optimizer_remove_redundant_eval| true| Indicates if the optimizer rule RemoveRedundantEval is activated.",
             "optimizer_reorder_hash_join| true| Indicates if the optimizer rule ReorderHashJoin is activated.",
             "optimizer_reorder_nested_loop_join| true| Indicates if the optimizer rule ReorderNestedLoopJoin is activated.",
+            "optimizer_rewrite_filter_on_cross_join_to_inner_join| true| Indicates if the optimizer rule RewriteFilterOnCrossJoinToInnerJoin is activated.",
             "optimizer_rewrite_filter_on_outer_join_to_inner_join| true| Indicates if the optimizer rule RewriteFilterOnOuterJoinToInnerJoin is activated.",
             "optimizer_rewrite_group_by_keys_limit_to_limit_distinct| true| Indicates if the optimizer rule RewriteGroupByKeysLimitToLimitDistinct is activated.",
             "optimizer_rewrite_left_outer_join_to_hash_join| true| Indicates if the optimizer rule RewriteLeftOuterJoinToHashJoin is activated.",
