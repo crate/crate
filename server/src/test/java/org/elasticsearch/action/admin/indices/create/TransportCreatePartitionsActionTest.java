crate/crate

commit 8014ef9a2f9acd352ca67f5629952f9a3b847b78

Simplify adding version created setting
- `Version.min` is redundant because `getSmallestNonClientNodeVersion`
  is already the min version including `Version.CURRENT`

- We don't set the setting elsewhere, and even if we did we're creating
  a new index which should have the current min version.

server/src/main/java/org/elasticsearch/action/admin/indices/create/TransportCreatePartitionsAction.java
@@ -23,7 +23,6 @@ package org.elasticsearch.action.admin.indices.create;
 
 import static org.elasticsearch.cluster.metadata.IndexMetadata.INDEX_NUMBER_OF_SHARDS_SETTING;
 import static org.elasticsearch.cluster.metadata.IndexMetadata.SETTING_WAIT_FOR_ACTIVE_SHARDS;
-import static org.elasticsearch.cluster.metadata.MetadataCreateIndexService.setIndexVersionCreatedSetting;
 import static org.elasticsearch.cluster.metadata.MetadataCreateIndexService.validateSoftDeletesSetting;
 
 import java.io.IOException;
@@ -326,13 +325,15 @@ public class TransportCreatePartitionsAction extends TransportMasterNodeAction<C
     }
 
     private Settings createCommonIndexSettings(ClusterState currentState, @Nullable IndexTemplateMetadata template) {
-        Settings.Builder indexSettingsBuilder = Settings.builder();
+        Version minVersion = currentState.nodes().getSmallestNonClientNodeVersion();
+        Settings.Builder indexSettingsBuilder = Settings.builder()
+            .put(IndexMetadata.SETTING_INDEX_VERSION_CREATED.getKey(), minVersion);
+
         // apply template
         if (template != null) {
             indexSettingsBuilder.put(template.settings());
         }
 
-        setIndexVersionCreatedSetting(indexSettingsBuilder, currentState);
         validateSoftDeletesSetting(indexSettingsBuilder.build());
 
         if (indexSettingsBuilder.get(IndexMetadata.SETTING_CREATION_DATE) == null) {

server/src/main/java/io/crate/execution/ddl/tables/TransportCreateTableAction.java
@@ -21,7 +21,6 @@
 
 package io.crate.execution.ddl.tables;
 
-import static org.elasticsearch.cluster.metadata.MetadataCreateIndexService.setIndexVersionCreatedSetting;
 import static org.elasticsearch.cluster.metadata.MetadataCreateIndexService.validateSoftDeletesSetting;
 
 import java.io.IOException;
@@ -138,9 +137,11 @@ public class TransportCreateTableAction extends TransportMasterNodeAction<Create
 
         Settings.Builder settingsBuilder = Settings.builder()
             .put(request.settings())
+            .put(
+                IndexMetadata.SETTING_INDEX_VERSION_CREATED.getKey(),
+                state.nodes().getSmallestNonClientNodeVersion())
             .normalizePrefix(IndexMetadata.INDEX_SETTING_PREFIX);
 
-        setIndexVersionCreatedSetting(settingsBuilder, state);
         Settings normalizedSettings = settingsBuilder.build();
 
         validateSoftDeletesSetting(normalizedSettings);

server/src/main/java/org/elasticsearch/cluster/metadata/MetadataCreateIndexService.java
@@ -54,7 +54,6 @@ import org.elasticsearch.cluster.ack.CreateIndexClusterStateUpdateResponse;
 import org.elasticsearch.cluster.block.ClusterBlockLevel;
 import org.elasticsearch.cluster.block.ClusterBlocks;
 import org.elasticsearch.cluster.metadata.IndexMetadata.State;
-import org.elasticsearch.cluster.node.DiscoveryNodes;
 import org.elasticsearch.cluster.routing.IndexRoutingTable;
 import org.elasticsearch.cluster.routing.RoutingTable;
 import org.elasticsearch.cluster.routing.ShardRouting;
@@ -391,7 +390,10 @@ public class MetadataCreateIndexService {
             Settings.Builder indexSettingsBuilder = Settings.builder()
                 .put(request.settings())
                 .put(SETTING_INDEX_UUID, UUIDs.randomBase64UUID())
-                .put(SETTING_CREATION_DATE, Instant.now().toEpochMilli());
+                .put(SETTING_CREATION_DATE, Instant.now().toEpochMilli())
+                .put(
+                    IndexMetadata.SETTING_INDEX_VERSION_CREATED.getKey(),
+                    currentState.nodes().getSmallestNonClientNodeVersion());
 
             if (request.resizeType() == null && request.copySettings() == false) {
                 if (indexSettingsBuilder.get(SETTING_NUMBER_OF_REPLICAS) == null) {
@@ -401,7 +403,6 @@ public class MetadataCreateIndexService {
                     indexSettingsBuilder.put(AutoExpandReplicas.SETTING_KEY, settings.get(AutoExpandReplicas.SETTING_KEY));
                 }
             }
-            setIndexVersionCreatedSetting(indexSettingsBuilder, currentState);
             validateSoftDeletesSetting(indexSettingsBuilder.build());
 
             final IndexMetadata.Builder tmpImdBuilder = IndexMetadata.builder(request.index());
@@ -693,15 +694,6 @@ public class MetadataCreateIndexService {
                 "Creating tables with soft-deletes disabled is no longer supported. "
                 + "Please do not specify a value for setting [soft_deletes.enabled]."
             );
-        }
-    }
-
-    public static void setIndexVersionCreatedSetting(Settings.Builder indexSettingsBuilder, ClusterState clusterState) {
-        if (indexSettingsBuilder.get(IndexMetadata.SETTING_INDEX_VERSION_CREATED.getKey()) == null) {
-            final DiscoveryNodes nodes = clusterState.nodes();
-            final Version createdVersion = Version.min(Version.CURRENT,
-                                                       nodes.getSmallestNonClientNodeVersion());
-            indexSettingsBuilder.put(IndexMetadata.SETTING_INDEX_VERSION_CREATED.getKey(), createdVersion);
         }
     }
 

commit 8d8d9ce136b39efbc7445008c0bd9673b276d779

Set new partitions' version created to the smallest non-client node version
Follow up to 8014ef9 which caused new indices to be created with
the template's created version instead of the current min-node version.
Partitions must use newer versions if possible.

server/src/main/java/org/elasticsearch/action/admin/indices/create/TransportCreatePartitionsAction.java
@@ -366,14 +366,15 @@ public class TransportCreatePartitionsAction extends TransportMasterNodeAction<C
     }
 
     private Settings createCommonIndexSettings(ClusterState currentState, @Nullable IndexTemplateMetadata template) {
-        Version minVersion = currentState.nodes().getSmallestNonClientNodeVersion();
-        Settings.Builder indexSettingsBuilder = Settings.builder()
-            .put(IndexMetadata.SETTING_INDEX_VERSION_CREATED.getKey(), minVersion);
+        Settings.Builder indexSettingsBuilder = Settings.builder();
 
         // apply template
         if (template != null) {
             indexSettingsBuilder.put(template.settings());
         }
+
+        Version minVersion = currentState.nodes().getSmallestNonClientNodeVersion();
+        indexSettingsBuilder.put(IndexMetadata.SETTING_INDEX_VERSION_CREATED.getKey(), minVersion);
 
         validateSoftDeletesSetting(indexSettingsBuilder.build());
 

server/src/test/java/org/elasticsearch/action/admin/indices/create/TransportCreatePartitionsActionTest.java
@@ -199,4 +199,40 @@ public class TransportCreatePartitionsActionTest extends IntegTestCase {
         // Without do-once logic would have been 2
         verify(actionSpy, times(1)).upgradeTemplates(any(), any());
     }
+
+    @Test
+    public void test_version_created_settings_for_new_partitions_from_old_template_do_not_follow_old_templates_version() throws Exception {
+        execute("create table tbl (a int) partitioned by (a) ");
+        ensureYellow();
+
+        ClusterState clusterState = cluster().clusterService().state();
+        Metadata.Builder metadataBuilder = Metadata.builder(clusterState.metadata());
+
+        String tableTemplateName = PartitionName.templateName("doc", "tbl");
+        IndexTemplateMetadata indexTemplateMetadata = clusterState.metadata().templates().get(tableTemplateName);
+        assertThat(indexTemplateMetadata).isNotNull();
+
+        // modify the template's version created to V_5_7_5
+        metadataBuilder.removeTemplate(tableTemplateName);
+        IndexTemplateMetadata.Builder templateBuilder = IndexTemplateMetadata.builder(tableTemplateName)
+            .version(1)
+            .patterns(indexTemplateMetadata.patterns())
+            .putMapping(indexTemplateMetadata.mapping())
+            .settings(Settings.builder()
+                .put(indexTemplateMetadata.settings())
+                .put(IndexMetadata.SETTING_INDEX_VERSION_CREATED.getKey(), Version.V_5_7_5)
+            );
+        metadataBuilder.put(templateBuilder);
+        ClusterState artificialState = new ClusterState.Builder(clusterState).metadata(metadataBuilder).build();
+
+        // Imitation of "insert into tbl (a) values (1)".
+        CreatePartitionsRequest request = new CreatePartitionsRequest(RelationName.fromIndexName(tableTemplateName), List.of(List.of("1")));
+
+        ClusterState newState = action.executeCreateIndices(artificialState, request);
+        assertThat(newState.metadata().indices().values().size()).isEqualTo(1);
+        IndexMetadata indexMetadata = newState.metadata().indices().values().iterator().next().value;
+        Version newPartitionVersion = indexMetadata.getSettings().getAsVersion(IndexMetadata.SETTING_INDEX_VERSION_CREATED.getKey(), Version.V_EMPTY);
+        assertThat(newPartitionVersion).isEqualTo(clusterState.nodes().getSmallestNonClientNodeVersion());
+        assertThat(newPartitionVersion).isNotEqualTo(Version.V_5_7_5);
+    }
 }