d4cd89bc013156156d8de1bb763e0b7f623c7673
baur <baurzhansahariev@gmail.com>
2025-01-24 10:40:28 +0100

    Add support for lowercase patterns to the to_char scalar function

--- server/src/test/java/io/crate/expression/scalar/formatting/ToCharFunctionTest.java
+++ server/src/test/java/io/crate/expression/scalar/formatting/ToCharFunctionTest.java
@@ -21,15 +21,84 @@
 
 package io.crate.expression.scalar.formatting;
 
+import static io.crate.expression.scalar.formatting.DateTimeFormatter.Token.ABBREVIATED_DAY_CAPITALIZED;
+import static io.crate.expression.scalar.formatting.DateTimeFormatter.Token.ABBREVIATED_DAY_LOWER;
+import static io.crate.expression.scalar.formatting.DateTimeFormatter.Token.ABBREVIATED_DAY_UPPER;
+import static io.crate.expression.scalar.formatting.DateTimeFormatter.Token.ABBREVIATED_MONTH_CAPITALIZED;
+import static io.crate.expression.scalar.formatting.DateTimeFormatter.Token.ABBREVIATED_MONTH_LOWER;
+import static io.crate.expression.scalar.formatting.DateTimeFormatter.Token.ABBREVIATED_MONTH_UPPER;
+import static io.crate.expression.scalar.formatting.DateTimeFormatter.Token.AD_ERA_LOWER;
+import static io.crate.expression.scalar.formatting.DateTimeFormatter.Token.AD_ERA_UPPER;
+import static io.crate.expression.scalar.formatting.DateTimeFormatter.Token.AM_LOWER;
+import static io.crate.expression.scalar.formatting.DateTimeFormatter.Token.AM_UPPER;
+import static io.crate.expression.scalar.formatting.DateTimeFormatter.Token.A_D_ERA_LOWER;
+import static io.crate.expression.scalar.formatting.DateTimeFormatter.Token.A_D_ERA_UPPER;
+import static io.crate.expression.scalar.formatting.DateTimeFormatter.Token.A_M_LOWER;
+import static io.crate.expression.scalar.formatting.DateTimeFormatter.Token.A_M_UPPER;
+import static io.crate.expression.scalar.formatting.DateTimeFormatter.Token.BC_ERA_LOWER;
+import static io.crate.expression.scalar.formatting.DateTimeFormatter.Token.BC_ERA_UPPER;
+import static io.crate.expression.scalar.formatting.DateTimeFormatter.Token.B_C_ERA_LOWER;
+import static io.crate.expression.scalar.formatting.DateTimeFormatter.Token.B_C_ERA_UPPER;
+import static io.crate.expression.scalar.formatting.DateTimeFormatter.Token.DAY_CAPITALIZED;
+import static io.crate.expression.scalar.formatting.DateTimeFormatter.Token.DAY_LOWER;
+import static io.crate.expression.scalar.formatting.DateTimeFormatter.Token.DAY_UPPER;
+import static io.crate.expression.scalar.formatting.DateTimeFormatter.Token.MONTH_CAPITALIZED;
+import static io.crate.expression.scalar.formatting.DateTimeFormatter.Token.MONTH_LOWER;
+import static io.crate.expression.scalar.formatting.DateTimeFormatter.Token.MONTH_UPPER;
+import static io.crate.expression.scalar.formatting.DateTimeFormatter.Token.PM_LOWER;
+import static io.crate.expression.scalar.formatting.DateTimeFormatter.Token.PM_UPPER;
+import static io.crate.expression.scalar.formatting.DateTimeFormatter.Token.P_M_LOWER;
+import static io.crate.expression.scalar.formatting.DateTimeFormatter.Token.P_M_UPPER;
+import static io.crate.expression.scalar.formatting.DateTimeFormatter.Token.ROMAN_MONTH_LOWER;
+import static io.crate.expression.scalar.formatting.DateTimeFormatter.Token.ROMAN_MONTH_UPPER;
 import static io.crate.testing.Asserts.isNotSameInstance;
 import static io.crate.testing.Asserts.isSameInstance;
+
+import java.util.Locale;
+import java.util.Set;
 
 import org.junit.Test;
 
 import io.crate.expression.scalar.ScalarTestCase;
+import io.crate.expression.symbol.Literal;
 
 
 public class ToCharFunctionTest extends ScalarTestCase {
+
+    // Patterns that must NOT behave in the same way for upper/lower case,
+    private static Set<DateTimeFormatter.Token> FIXED_CASE_PATTERNS =
+        Set.of(
+            MONTH_UPPER,
+            MONTH_CAPITALIZED,
+            MONTH_LOWER,
+            ABBREVIATED_MONTH_UPPER,
+            ABBREVIATED_MONTH_CAPITALIZED,
+            ABBREVIATED_MONTH_LOWER,
+            DAY_UPPER,
+            DAY_CAPITALIZED,
+            DAY_LOWER,
+            ABBREVIATED_DAY_UPPER,
+            ABBREVIATED_DAY_CAPITALIZED,
+            ABBREVIATED_DAY_LOWER,
+            AM_UPPER,
+            AM_LOWER,
+            PM_UPPER,
+            PM_LOWER,
+            A_M_UPPER,
+            A_M_LOWER,
+            P_M_UPPER,
+            P_M_LOWER,
+            BC_ERA_UPPER,
+            BC_ERA_LOWER,
+            AD_ERA_UPPER,
+            AD_ERA_LOWER,
+            B_C_ERA_UPPER,
+            B_C_ERA_LOWER,
+            A_D_ERA_UPPER,
+            A_D_ERA_LOWER,
+            ROMAN_MONTH_UPPER,
+            ROMAN_MONTH_LOWER
+        );
 
     @Test
     public void testEvaluateTimestamp() {
@@ -37,6 +106,34 @@ public class ToCharFunctionTest extends ScalarTestCase {
             "to_char(timestamp '1970-01-01T17:31:12.12345', 'Day,  DD  HH12:MI:SS')",
             "Thursday,  01  05:31:12"
         );
+    }
+
+    @Test
+    public void test_lower_case_patterns() throws Exception {
+        assertEvaluate("to_char('2024-12-13'::timestamp, 'yyyy-mm-dd')", "2024-12-13");
+        assertEvaluate("to_char('2024-12-13'::timestamp, 'mm')", "12");
+        assertEvaluate("to_char('2024-12-13'::timestamp, 'miss-')", "0000-");
+        assertEvaluate("to_char('2024-12-13'::timestamp, 'd')", "6");
+
+        assertEvaluate("to_char('2024-12-13'::timestamp, 'w')", "2");
+        assertEvaluate("to_char('2024-12-13'::timestamp, 'cc')", "21");
+        assertEvaluate("to_char('2024-12-13'::timestamp, 'j')", "2460658");
+        assertEvaluate("to_char('2024-12-13'::timestamp, 'iw')", "50");
+    }
+
+    @Test
+    public void test_lower_case_same_result_as_upper_case_for_non_mixed_case_patterns() throws Exception {
+        for (DateTimeFormatter.Token token: DateTimeFormatter.Token.values()) {
+            var tokenValue = token.toString();
+            if (FIXED_CASE_PATTERNS.contains(token) == false && Character.isLowerCase(tokenValue.charAt(0))) {
+                assertEvaluate(
+                    "to_char(timestamp '1970-01-01', name) = to_char(timestamp '1970-01-01', name)",
+                    true,
+                    Literal.of(tokenValue),
+                    Literal.of(tokenValue.toUpperCase(Locale.ENGLISH))
+                );
+            }
+        }
     }
 
     @Test
