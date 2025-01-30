dbd9e53951476d2e1c2b0a90f248b36aa4e6d334
Sebastian Utz <su@rtme.net>
2025-01-30 16:41:08 +0100

    Release 5.8.6

--- server/src/main/java/org/elasticsearch/Version.java
+++ server/src/main/java/org/elasticsearch/Version.java
@@ -210,7 +210,7 @@ public class Version implements Comparable<Version>, ToXContentFragment {
     public static final Version V_5_8_3 = new Version(8_08_03_99, false, org.apache.lucene.util.Version.LUCENE_9_11_1);
     public static final Version V_5_8_4 = new Version(8_08_04_99, false, org.apache.lucene.util.Version.LUCENE_9_11_1);
     public static final Version V_5_8_5 = new Version(8_08_05_99, false, org.apache.lucene.util.Version.LUCENE_9_11_1);
-    public static final Version V_5_8_6 = new Version(8_08_06_99, true, org.apache.lucene.util.Version.LUCENE_9_11_1);
+    public static final Version V_5_8_6 = new Version(8_08_06_99, false, org.apache.lucene.util.Version.LUCENE_9_11_1);
 
     public static final Version CURRENT = V_5_8_6;
 
