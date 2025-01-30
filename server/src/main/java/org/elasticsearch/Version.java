f4892db0e2035f517855ec753b3fc884b4184e60
Sebastian Utz <su@rtme.net>
2025-01-30 20:04:16 +0100

    Bump version to 5.9.10

--- server/src/main/java/org/elasticsearch/Version.java
+++ server/src/main/java/org/elasticsearch/Version.java
@@ -221,8 +221,9 @@ public class Version implements Comparable<Version>, ToXContentFragment {
     public static final Version V_5_9_7 = new Version(8_09_07_99, false, org.apache.lucene.util.Version.LUCENE_9_11_1);
     public static final Version V_5_9_8 = new Version(8_09_08_99, false, org.apache.lucene.util.Version.LUCENE_9_11_1);
     public static final Version V_5_9_9 = new Version(8_09_09_99, false, org.apache.lucene.util.Version.LUCENE_9_11_1);
+    public static final Version V_5_9_10 = new Version(8_09_10_99, true, org.apache.lucene.util.Version.LUCENE_9_11_1);
 
-    public static final Version CURRENT = V_5_9_9;
+    public static final Version CURRENT = V_5_9_10;
 
     private static final ImmutableOpenIntMap<Version> ID_TO_VERSION;
     private static final ImmutableOpenMap<String, Version> STRING_TO_VERSION;
