crate/crate

commit c7fb24dd00e0f14c8a2ab6f9c99ef5c663e85e8b

Add error message + code to failing bulk operations on the HTTP interface
Closes #16649.

server/src/main/java/io/crate/execution/dml/ShardResponse.java
@@ -21,6 +21,9 @@
 
 package io.crate.execution.dml;
 
+import static io.crate.common.exceptions.Exceptions.userFriendlyMessageInclNested;
+import static io.crate.execution.engine.indexing.ShardingUpsertExecutor.BULK_RESPONSE_MAX_ERRORS_PER_SHARD;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.BitSet;
@@ -35,6 +38,7 @@ import org.elasticsearch.common.io.stream.Writeable;
 import org.jetbrains.annotations.Nullable;
 
 import com.carrotsearch.hppc.IntArrayList;
+import com.carrotsearch.hppc.IntObjectHashMap;
 
 import io.crate.Streamer;
 import io.crate.execution.dml.upsert.ShardUpsertRequest;
@@ -49,25 +53,41 @@ public class ShardResponse extends ReplicationResponse implements WriteResponse 
     public static class Failure implements Writeable {
 
         private final String id;
-        private final String message;
+        @Nullable
+        private final Throwable error;
         private final boolean versionConflict;
 
-        public Failure(String id, String message, boolean versionConflict) {
+        private Failure(String id, @Nullable Throwable error, boolean versionConflict) {
             this.id = id;
-            this.message = message;
+            this.error = error;
             this.versionConflict = versionConflict;
         }
 
-        public Failure(StreamInput in) throws IOException {
+        private Failure(StreamInput in) throws IOException {
             id = in.readString();
-            message = in.readString();
+            if (in.getVersion().onOrAfter(Version.V_5_10_0)) {
+                if (in.readBoolean()) {
+                    error = in.readException();
+                } else {
+                    error = null;
+                }
+            } else {
+                error = new RuntimeException(in.readString());
+            }
             versionConflict = in.readBoolean();
         }
 
         @Override
         public void writeTo(StreamOutput out) throws IOException {
             out.writeString(id);
-            out.writeString(message);
+            if (out.getVersion().onOrAfter(Version.V_5_10_0)) {
+                out.writeBoolean(error != null);
+                if (error != null) {
+                    out.writeException(error);
+                }
+            } else {
+                out.writeString(userFriendlyMessageInclNested(error));
+            }
             out.writeBoolean(versionConflict);
         }
 
@@ -75,8 +95,9 @@ public class ShardResponse extends ReplicationResponse implements WriteResponse 
             return id;
         }
 
-        public String message() {
-            return this.message;
+        @Nullable
+        public Throwable error() {
+            return error;
         }
 
         public boolean versionConflict() {
@@ -87,7 +108,7 @@ public class ShardResponse extends ReplicationResponse implements WriteResponse 
         public String toString() {
             return "Failure{" +
                    "id='" + id + '\'' +
-                   ", message='" + message + '\'' +
+                   ", exception='" + error + '\'' +
                    ", versionConflict=" + versionConflict +
                    '}';
         }
@@ -125,9 +146,16 @@ public class ShardResponse extends ReplicationResponse implements WriteResponse 
         failures.add(null);
     }
 
-    public void add(int location, Failure failure) {
+    public void add(int location, String id, Throwable error, boolean versionConflict) {
         locations.add(location);
-        failures.add(failure);
+        var errorCount = failures.size();
+        failures.add(
+            new Failure(
+                id,
+                errorCount >= BULK_RESPONSE_MAX_ERRORS_PER_SHARD ? null : error,
+                versionConflict
+            )
+        );
     }
 
     public void addResultRows(Object[] rows) {
@@ -260,6 +288,7 @@ public class ShardResponse extends ReplicationResponse implements WriteResponse 
         private final BitSet successfulWrites = new BitSet();
         private final BitSet failureLocations = new BitSet();
         private final ArrayList<Object[]> resultRows = new ArrayList<>();
+        private final IntObjectHashMap<Throwable> failures = new IntObjectHashMap<>();
 
         public void update(ShardResponse response) {
             IntArrayList itemIndices = response.itemIndices();
@@ -271,6 +300,7 @@ public class ShardResponse extends ReplicationResponse implements WriteResponse 
                     successfulWrites.set(location, true);
                 } else {
                     failureLocations.set(location, true);
+                    this.failures.put(location, failure.error());
                 }
             }
             List<Object[]> resultRows = response.getResultRows();
@@ -285,6 +315,10 @@ public class ShardResponse extends ReplicationResponse implements WriteResponse 
 
         public boolean failed(int location) {
             return failureLocations.get(location);
+        }
+
+        public Throwable failure(int location) {
+            return failures.get(location);
         }
 
         public List<Object[]> resultRows() {

server/src/main/java/io/crate/execution/engine/indexing/UpsertResultCollectors.java
@@ -32,9 +32,11 @@ import org.elasticsearch.cluster.node.DiscoveryNode;
 
 import com.carrotsearch.hppc.IntArrayList;
 
+import io.crate.auth.AccessControl;
 import io.crate.data.CollectionBucket;
 import io.crate.data.Row;
 import io.crate.data.Row1;
+import io.crate.exceptions.SQLExceptions;
 import io.crate.execution.dml.ShardResponse;
 import io.crate.execution.dml.ShardResponse.Failure;
 
@@ -48,11 +50,11 @@ final class UpsertResultCollectors {
         return new RowCountCollector();
     }
 
-    static UpsertResultCollector newSummaryCollector(DiscoveryNode localNode) {
+    static UpsertResultCollector newSummaryCollector(DiscoveryNode localNode, AccessControl accessControl) {
         return new SummaryCollector(Map.of(
             "id", localNode.getId(),
             "name", localNode.getName()
-        ));
+        ), accessControl);
     }
 
     private static class ResultRowCollector implements UpsertResultCollector {
@@ -130,7 +132,7 @@ final class UpsertResultCollectors {
                                   ShardResponse shardResponse,
                                   List<RowSourceInfo> rowSourceInfosIgnored) {
             Failure failure = shardResponse.failures().stream()
-                .filter(x -> x != null && x.message() != null)
+                .filter(x -> x != null && x.error() != null)
                 .findAny()
                 .orElse(null);
             synchronized (lock) {
@@ -142,11 +144,13 @@ final class UpsertResultCollectors {
     private static class SummaryCollector implements UpsertResultCollector {
 
         private final Map<String, String> nodeInfo;
+        private final AccessControl accessControl;
 
         private final Object lock = new Object();
 
-        SummaryCollector(Map<String, String> nodeInfo) {
+        SummaryCollector(Map<String, String> nodeInfo, AccessControl accessControl) {
             this.nodeInfo = nodeInfo;
+            this.accessControl = accessControl;
         }
 
         @Override
@@ -184,7 +188,11 @@ final class UpsertResultCollectors {
                     ShardResponse.Failure failure = failures.get(i);
                     int location = locations.get(i);
                     RowSourceInfo rowSourceInfo = rowSourceInfos.get(location);
-                    String msg = failure == null ? null : failure.message();
+                    String msg = null;
+                    if (failure != null) {
+                        var throwable = SQLExceptions.prepareForClientTransmission(accessControl, failure.error());
+                        msg = throwable.getMessage();
+                    }
                     upsertResults.addResult(rowSourceInfo.sourceUri, msg, rowSourceInfo.lineNumber);
                 }
             }

server/src/main/java/io/crate/execution/engine/indexing/UpsertResults.java
@@ -141,7 +141,7 @@ class UpsertResults {
             }
         }
         if (results.failure != null) {
-            sb.append(results.failure.message());
+            sb.append(results.failure.error().getMessage());
         }
         var it = results.resultsByUri.entrySet().iterator();
         while (it.hasNext()) {

server/src/main/java/io/crate/execution/dml/BulkResponse.java
@@ -1,0 +1,118 @@
+/*
+ * Licensed to Crate.io GmbH ("Crate") under one or more contributor
+ * license agreements.  See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.  Crate licenses
+ * this file to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.  You may
+ * obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * However, if you have executed another commercial license agreement
+ * with Crate these terms will supersede the license and you may use the
+ * software solely pursuant to the terms of the relevant commercial agreement.
+ */
+
+package io.crate.execution.dml;
+
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.VisibleForTesting;
+
+import com.carrotsearch.hppc.IntCollection;
+import com.carrotsearch.hppc.cursors.IntCursor;
+
+import io.crate.data.Row1;
+
+public final class BulkResponse {
+
+    private final int size;
+    private final long[] rowCounts;
+    @Nullable
+    private final Throwable[] failures;
+
+    public BulkResponse(int size) {
+        this.size = size;
+        rowCounts = new long[size];
+        failures = new Throwable[size];
+    }
+
+    /**
+     * Create bulk-response depending on number of bulk responses
+     * <pre>
+     *     compressedResult
+     *          success: [1, 1, 1, 1]
+     *          failure: []
+     *
+     *     insert into t (x) values (?), (?)   -- bulkParams: [[1, 2], [3, 4]]
+     *     Response:
+     *      [2, 2]
+     *
+     *     insert into t (x) values (?)        -- bulkParams: [[1], [2], [3], [4]]
+     *     Response:
+     *      BulkResponse (rowCounts=[1, 1, 1, 1], failures=[null, null, null, null])
+     * </pre>
+     */
+    public BulkResponse update(ShardResponse.CompressedResult result, IntCollection items) {
+        for (IntCursor c : items) {
+            int itemLocation = c.index;
+            int resultIdx = c.value;
+            if (result.successfulWrites(itemLocation)) {
+                incrementRowCount(resultIdx);
+            } else if (result.failed(itemLocation)) {
+                setFailure(resultIdx, result.failure(itemLocation));
+            }
+        }
+        return this;
+    }
+
+    /**
+     * Update the bulk response with the row count and failure for the given bulk parameter index.
+     *
+     * @param idx       the index of the bulk parameter
+     * @param rowCount  The row count for the bulk parameter, expect null if failure is not null
+     * @param failure   The optional failure for the bulk parameter, expect null if rowCount is not null
+     * @return this
+     */
+    public BulkResponse update(int idx, @Nullable Long rowCount, @Nullable Throwable failure) {
+        if (failure == null) {
+            assert rowCount != null : "rowCount must not be null if failure is null";
+            rowCounts[idx] = rowCount;
+        } else {
+            setFailure(idx, failure);
+        }
+        return this;
+    }
+
+    public void incrementRowCount(int idx) {
+        rowCounts[idx]++;
+    }
+
+    public void setFailure(int idx, Throwable throwable) {
+        rowCounts[idx] = Row1.ERROR;
+        failures[idx] = throwable;
+    }
+
+    public long rowCount(int idx) {
+        return rowCounts[idx];
+    }
+
+    @Nullable
+    public Throwable failure(int idx) {
+        return failures[idx];
+    }
+
+    public int size() {
+        return size;
+    }
+
+    @VisibleForTesting
+    public long[] rowCounts() {
+        return rowCounts;
+    }
+}

server/src/main/java/io/crate/execution/dml/BulkShardResponseListener.java
@@ -21,9 +21,6 @@
 
 package io.crate.execution.dml;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 import org.elasticsearch.action.ActionListener;
@@ -31,9 +28,8 @@ import org.elasticsearch.index.engine.DocumentMissingException;
 import org.elasticsearch.index.engine.VersionConflictEngineException;
 
 import com.carrotsearch.hppc.IntCollection;
-import com.carrotsearch.hppc.cursors.IntCursor;
 
-import io.crate.data.Row1;
+import io.crate.action.FutureActionListener;
 import io.crate.exceptions.SQLExceptions;
 import io.crate.execution.support.MultiActionListener;
 
@@ -43,28 +39,26 @@ import io.crate.execution.support.MultiActionListener;
 final class BulkShardResponseListener implements ActionListener<ShardResponse> {
 
     private final ShardResponse.CompressedResult compressedResult;
-    private final ArrayList<CompletableFuture<Long>> results;
-    private final MultiActionListener<ShardResponse, ?, long[]> listener;
+    private final FutureActionListener<BulkResponse> results;
+    private final MultiActionListener<ShardResponse, ShardResponse.CompressedResult, BulkResponse> listener;
 
     /**
      * @param resultIndices a list containing one element per shardRequest-item across all shardRequests being made.
      *                      the values must contain the resultIdx;
-     *                      (See {@link #toRowCounts(io.crate.execution.dml.ShardResponse.CompressedResult, IntCollection, int)})
+     *                      (See {@link BulkResponse#update(int, long, Throwable)} )
      */
     BulkShardResponseListener(int numCallbacks,
                               int numBulkParams,
                               IntCollection resultIndices) {
-        this.results = new ArrayList<>();
-        for (int i = 0; i < numBulkParams; i++) {
-            results.add(new CompletableFuture<>());
-        }
+        var bulkResponse = new BulkResponse(numBulkParams);
+        this.results = new FutureActionListener<>();
         this.compressedResult = new ShardResponse.CompressedResult();
         listener = new MultiActionListener<>(
             numCallbacks,
             () -> compressedResult,
             BulkShardResponseListener::onResponse,
-            responses -> toRowCounts(responses, resultIndices, numBulkParams),
-            new SetResultFutures(results)
+            responses -> bulkResponse.update(responses, resultIndices),
+            results
         );
     }
 
@@ -80,7 +74,7 @@ final class BulkShardResponseListener implements ActionListener<ShardResponse> {
         }
     }
 
-    public List<CompletableFuture<Long>> rowCountFutures() {
+    public CompletableFuture<BulkResponse> bulkResponseFuture() {
         return results;
     }
 
@@ -92,72 +86,5 @@ final class BulkShardResponseListener implements ActionListener<ShardResponse> {
     @Override
     public void onFailure(Exception e) {
         listener.onFailure(e);
-    }
-
-    /**
-     * This calculates & returns the correct rowCount specific to the bulk individual bulk-params.
-     *
-     * Example:
-     *
-     * <pre>
-     * where id = ? or id = ?
-     * bulkParams: [ [1, 2], [3, 4] ]
-     *
-     * ->
-     *  3 requests (per shard) (they can span across *all* parameters)
-     *
-     *   shard0: [0, 2]      // item locations
-     *   shard1: [1]
-     *   shard2: [3]
-     *
-     *  numBulkParams: 2 -> 2 items in the result
-     *
-     *  items:
-     *     idx  value
-     *      0 -> 0
-     *      1 -> 0
-     *      2 -> 1
-     *      3 -> 1
-     *
-     *   result:
-     *      long[] {2, 2}
-     * </pre>
-     */
-    private static long[] toRowCounts(ShardResponse.CompressedResult result, IntCollection items, int numBulkParams) {
-        long[] rowCounts = new long[numBulkParams];
-        Arrays.fill(rowCounts, 0L);
-        for (IntCursor c : items) {
-            int itemLocation = c.index;
-            int resultIdx = c.value;
-            if (result.successfulWrites(itemLocation)) {
-                rowCounts[resultIdx]++;
-            } else if (result.failed(itemLocation)) {
-                rowCounts[resultIdx] = Row1.ERROR;
-            }
-        }
-        return rowCounts;
-    }
-
-    private static class SetResultFutures implements ActionListener<long[]> {
-
-        private final ArrayList<CompletableFuture<Long>> results;
-
-        SetResultFutures(ArrayList<CompletableFuture<Long>> results) {
-            this.results = results;
-        }
-
-        @Override
-        public void onResponse(long[] rowCounts) {
-            for (int i = 0; i < rowCounts.length; i++) {
-                results.get(i).complete(rowCounts[i]);
-            }
-        }
-
-        @Override
-        public void onFailure(Exception e) {
-            for (CompletableFuture<Long> result : results) {
-                result.completeExceptionally(e);
-            }
-        }
     }
 }

server/src/main/java/io/crate/execution/dml/ObjectIndexer.java
@@ -35,6 +35,7 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import io.crate.data.Input;
+import io.crate.exceptions.ConversionException;
 import io.crate.expression.reference.doc.lucene.SourceParser;
 import io.crate.metadata.ColumnIdent;
 import io.crate.metadata.Reference;
@@ -141,8 +142,15 @@ public class ObjectIndexer implements ValueIndexer<Map<String, Object>> {
             }
             // valueIndexer is null for partitioned columns
             if (child.indexer != null) {
+                var type = child.reference.valueType();
+                try {
+                    innerValue = type.sanitizeValue(innerValue);
+                } catch (ClassCastException | IllegalArgumentException e) {
+                    throw new ConversionException(innerValue, type);
+                }
+
                 child.indexer.collectSchemaUpdates(
-                    child.reference.valueType().sanitizeValue(innerValue),
+                    innerValue,
                     onDynamicColumn,
                     synthetics
                 );

server/src/main/java/io/crate/execution/dml/ShardRequestExecutor.java
@@ -124,7 +124,7 @@ public class ShardRequestExecutor<Req> {
         }
     }
 
-    public List<CompletableFuture<Long>> executeBulk(List<Row> bulkParams, SubQueryResults subQueryResults) {
+    public CompletableFuture<BulkResponse> executeBulk(List<Row> bulkParams, SubQueryResults subQueryResults) {
         HashMap<ShardId, Req> requests = new HashMap<>();
         IntArrayList bulkIndices = new IntArrayList(bulkParams.size() * docKeys.size());
         int location = 0;
@@ -142,7 +142,7 @@ public class ShardRequestExecutor<Req> {
         for (Req req : requests.values()) {
             transportAction.accept(req, listener);
         }
-        return listener.rowCountFutures();
+        return listener.bulkResponseFuture();
     }
 
     private int addRequests(int location, Row parameters, Map<ShardId, Req> requests, SubQueryResults subQueryResults) {
@@ -249,8 +249,8 @@ public class ShardRequestExecutor<Req> {
             ShardResponse.Failure failure = response.failures().get(i);
             if (failure == null) {
                 f.accept(acc, response);
-            } else if (!failure.versionConflict() && !(failure.message().contains("Document not found") || failure.message().contains("document missing"))) {
-                throw new RuntimeException(failure.message());
+            } else if (!failure.versionConflict() && !(failure.error().getMessage().contains("Document not found") || failure.error().getMessage().contains("document missing"))) {
+                throw new RuntimeException(failure.error());
             }
         }
     }

server/src/main/java/io/crate/execution/dml/delete/TransportShardDeleteAction.java
@@ -21,8 +21,6 @@
 
 package io.crate.execution.dml.delete;
 
-import static io.crate.common.exceptions.Exceptions.userFriendlyMessageInclNested;
-
 import java.io.IOException;
 import java.util.concurrent.atomic.AtomicBoolean;
 
@@ -33,6 +31,7 @@ import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.common.inject.Singleton;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.index.VersionType;
+import org.elasticsearch.index.engine.DocumentMissingException;
 import org.elasticsearch.index.engine.Engine;
 import org.elasticsearch.index.engine.VersionConflictEngineException;
 import org.elasticsearch.index.seqno.SequenceNumbers;
@@ -42,6 +41,7 @@ import org.elasticsearch.indices.IndicesService;
 import org.elasticsearch.threadpool.ThreadPool;
 import org.elasticsearch.transport.TransportService;
 
+import io.crate.Constants;
 import io.crate.exceptions.JobKilledException;
 import io.crate.execution.dml.ShardResponse;
 import io.crate.execution.dml.TransportShardAction;
@@ -103,22 +103,20 @@ public class TransportShardDeleteAction extends TransportShardAction<ShardDelete
                             logger.debug("shardId={} failed to execute delete for id={}, doc not found",
                                 request.shardId(), item.id());
                         }
-                        shardResponse.add(location,
-                            new ShardResponse.Failure(
-                                item.id(),
-                                "Document not found while deleting",
-                                false));
+                        Throwable ex = new DocumentMissingException(indexShard.shardId(), Constants.DEFAULT_MAPPING_TYPE, item.id());
+                        shardResponse.add(location, item.id(), ex, false);
                     }
                 } else {
                     if (debugEnabled) {
                         logger.debug("shardId={} failed to execute delete for id={}: {}",
                             request.shardId(), item.id(), failure);
                     }
-                    shardResponse.add(location,
-                        new ShardResponse.Failure(
-                            item.id(),
-                            userFriendlyMessageInclNested(failure),
-                            (failure instanceof VersionConflictEngineException)));
+                    shardResponse.add(
+                        location,
+                        item.id(),
+                        failure,
+                        (failure instanceof VersionConflictEngineException)
+                    );
                 }
             } catch (Exception e) {
                 if (!TransportActions.isShardNotAvailableException(e)) {
@@ -128,11 +126,12 @@ public class TransportShardDeleteAction extends TransportShardAction<ShardDelete
                         logger.debug("shardId={} failed to execute delete for id={}: {}",
                                      request.shardId(), item.id(), e);
                     }
-                    shardResponse.add(location,
-                        new ShardResponse.Failure(
-                            item.id(),
-                            userFriendlyMessageInclNested(e),
-                            (e instanceof VersionConflictEngineException)));
+                    shardResponse.add(
+                        location,
+                        item.id(),
+                        e,
+                        (e instanceof VersionConflictEngineException)
+                    );
                 }
             }
         }

commit d28edefd5a95197e0bf825e0b4beca90880d7784

Fix NPE in RETURN SUMMARY handling leading to stuck queries
Fixes a regression introduced with https://github.com/crate/crate/commit/c7fb24dd00e0f14c8a2ab6f9c99ef5c663e85e8b

The entries in the `failures` of `ShardResponse` can be `null` - that
led to a (uncaught) NPE within the `SummaryCollector`.

This also happened more often than it should have due to the failure
count in `errorCount >= BULK_RESPONSE_MAX_ERRORS_PER_SHARD ? null :
error` being based on the size of `failures` - and there being 1 entry
per bulk item.

server/src/main/java/io/crate/execution/dml/ShardResponse.java
@@ -148,7 +148,12 @@ public class ShardResponse extends ReplicationResponse implements WriteResponse 
 
     public void add(int location, String id, Throwable error, boolean versionConflict) {
         locations.add(location);
-        var errorCount = failures.size();
+        int errorCount = 0;
+        for (var failure : failures) {
+            if (failure != null) {
+                errorCount++;
+            }
+        }
         failures.add(
             new Failure(
                 id,

server/src/main/java/io/crate/execution/engine/indexing/UpsertResultCollectors.java
@@ -190,8 +190,16 @@ final class UpsertResultCollectors {
                     RowSourceInfo rowSourceInfo = rowSourceInfos.get(location);
                     String msg = null;
                     if (failure != null) {
-                        var throwable = SQLExceptions.prepareForClientTransmission(accessControl, failure.error());
-                        msg = throwable.getMessage();
+                        Throwable error = failure.error();
+                        if (error == null) {
+                            msg = "unknown failure";
+                        } else {
+                            var throwable = SQLExceptions.prepareForClientTransmission(
+                                accessControl,
+                                failure.error()
+                            );
+                            msg = throwable.getMessage();
+                        }
                     }
                     upsertResults.addResult(rowSourceInfo.sourceUri, msg, rowSourceInfo.lineNumber);
                 }

server/src/main/java/io/crate/execution/engine/indexing/UpsertResults.java
@@ -140,7 +140,7 @@ class UpsertResults {
                 sb.append("NODE: ").append(nodeName);
             }
         }
-        if (results.failure != null) {
+        if (results.failure != null && results.failure.error() != null) {
             sb.append(results.failure.error().getMessage());
         }
         var it = results.resultsByUri.entrySet().iterator();

server/src/test/java/io/crate/execution/engine/indexing/UpsertResultsTest.java
@@ -23,11 +23,17 @@ package io.crate.execution.engine.indexing;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.ArrayList;
 import java.util.Map;
 
+import org.elasticsearch.Version;
+import org.elasticsearch.cluster.node.DiscoveryNode;
+import org.elasticsearch.test.ESTestCase;
 import org.junit.Test;
 
+import io.crate.auth.AccessControl;
 import io.crate.data.Row;
+import io.crate.execution.dml.ShardResponse;
 import io.crate.execution.engine.indexing.UpsertResults.Result;
 import io.crate.testing.TestingHelpers;
 
@@ -104,5 +110,27 @@ public class UpsertResultsTest {
         }
         Result result = upsertResults.getResultSafe("dummyUri");
         assertThat(result.errors).hasSize(25);
+    }
+
+    @Test
+    public void test_summary_collector_can_handle_null_failures() throws Exception {
+        DiscoveryNode node1 = new DiscoveryNode("n1", ESTestCase.buildNewFakeTransportAddress(), Version.CURRENT);
+        UpsertResultCollector summaryCollector = UpsertResultCollectors.newSummaryCollector(
+            node1,
+            AccessControl.DISABLED
+        );
+        UpsertResults upsertResults = new UpsertResults();
+        ArrayList<RowSourceInfo> sourceInfos = new ArrayList<>();
+        ShardResponse shardResponse = new ShardResponse();
+        IllegalStateException dummyError = new IllegalStateException("dummy");
+        // Errors are ignored and null value is used once the failure count is > BULK_RESPONSE_MAX_ERRORS_PER_SHARD
+        for (int i = 0; i < ShardingUpsertExecutor.BULK_RESPONSE_MAX_ERRORS_PER_SHARD + 1; i++) {
+            shardResponse.add(i, Integer.toString(i), dummyError, false);
+            sourceInfos.add(RowSourceInfo.emptyMarkerOrNewInstance(null, 1L));
+        }
+        summaryCollector.accumulator().accept(upsertResults, shardResponse, sourceInfos);
+        assertThat(upsertResults.getSuccessRowCountForAllUris()).isEqualTo(0);
+        assertThat(upsertResults.containsErrors()).isTrue();
+        assertThat(upsertResults.getResultSafe(null).errors).containsKeys("dummy", "unknown failure");
     }
 }