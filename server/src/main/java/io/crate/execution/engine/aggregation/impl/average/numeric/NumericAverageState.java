7235c7a2445e30c82689f0f076a7df4eb8f2cedf
baur <baurzhansahariev@gmail.com>
2025-02-07 19:13:52 +0100

    Use math context with maximal (finite) precision when computing average state
    Helps to void ArithmeticException on infinite fractions

--- server/src/main/java/io/crate/execution/engine/aggregation/impl/average/numeric/NumericAverageState.java
+++ server/src/main/java/io/crate/execution/engine/aggregation/impl/average/numeric/NumericAverageState.java
@@ -22,6 +22,7 @@
 package io.crate.execution.engine.aggregation.impl.average.numeric;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
 import java.util.Objects;
 
 import org.jetbrains.annotations.NotNull;
@@ -44,7 +45,10 @@ public class NumericAverageState<T extends NumericValueHolder> implements Compar
 
     public BigDecimal value() {
         if (count > 0) {
-            return sum.value().divide(BigDecimal.valueOf(count));
+            // We need to use divide with a MathContext to avoid ArithmeticException on infinite fractions.
+            // Using best (finite) precision as we want to compute final result with the best precision.
+            // If users want to reduce the precision of the final result, they can use explicit cast.
+            return sum.value().divide(BigDecimal.valueOf(count), MathContext.DECIMAL128);
         } else {
             return null;
         }
