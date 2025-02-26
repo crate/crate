d26581b54952d12cde730c9721683bfe62bce799
baur <baurzhansahariev@gmail.com>
2025-02-26 10:40:03 +0100

    200 seconds: eventually available and q2 is gone (RelationUnknown)

--- server/src/test/java/io/crate/integrationtests/AlterTableIntegrationTest.java
+++ server/src/test/java/io/crate/integrationtests/AlterTableIntegrationTest.java
@@ -371,20 +371,20 @@ public class AlterTableIntegrationTest extends IntegTestCase {
         assertBusy(() -> {
             execute("SELECT * FROM q1_old order by a");
             assertThat(response).hasRows("1| 2", "2| 3", "3| 4");
-        }, 20, TimeUnit.SECONDS);
+        }, 200, TimeUnit.SECONDS);
 
 
         assertBusy(() -> {
             execute("SELECT * FROM q1 order by a");
             assertThat(response).hasRows("1| 2", "2| 3", "3| 4");
-        }, 20, TimeUnit.SECONDS);
+        }, 200, TimeUnit.SECONDS);
 
         assertBusy(
             () -> assertSQLError(() -> execute("SELECT * FROM q2"))
             .hasPGError(UNDEFINED_TABLE)
             .hasHTTPError(NOT_FOUND, 4041)
             .hasMessageContaining("Relation 'q2' unknown"),
-            20,
+            200,
             TimeUnit.SECONDS
         );
     }
