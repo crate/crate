crate/crate

commit 8f941f829d1fe4193f968af9a96bd3e120708fd4

Change ChecksumBlobStoreFormat to use Writeable instead of xContent

server/src/main/java/org/elasticsearch/index/snapshots/blobstore/BlobStoreIndexShardSnapshots.java
@@ -19,14 +19,7 @@
 
 package org.elasticsearch.index.snapshots.blobstore;
 
-import org.elasticsearch.ElasticsearchParseException;
-import org.elasticsearch.common.xcontent.ParseField;
-import org.elasticsearch.common.xcontent.ToXContentFragment;
-import org.elasticsearch.common.xcontent.XContentBuilder;
-import org.elasticsearch.common.xcontent.XContentParser;
-import org.elasticsearch.index.snapshots.blobstore.BlobStoreIndexShardSnapshot.FileInfo;
-
-import io.crate.server.xcontent.XContentParserUtils;
+import static java.util.Collections.unmodifiableMap;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -36,7 +29,17 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
-import static java.util.Collections.unmodifiableMap;
+import org.elasticsearch.ElasticsearchParseException;
+import org.elasticsearch.common.io.stream.StreamInput;
+import org.elasticsearch.common.io.stream.StreamOutput;
+import org.elasticsearch.common.io.stream.Writeable;
+import org.elasticsearch.common.xcontent.ParseField;
+import org.elasticsearch.common.xcontent.ToXContentFragment;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.common.xcontent.XContentParser;
+import org.elasticsearch.index.snapshots.blobstore.BlobStoreIndexShardSnapshot.FileInfo;
+
+import io.crate.server.xcontent.XContentParserUtils;
 
 /**
  * Contains information about all snapshots for the given shard in repository
@@ -44,7 +47,7 @@ import static java.util.Collections.unmodifiableMap;
  * This class is used to find files that were already snapshotted and clear out files that no longer referenced by any
  * snapshots.
  */
-public class BlobStoreIndexShardSnapshots implements Iterable<SnapshotFiles>, ToXContentFragment {
+public class BlobStoreIndexShardSnapshots implements Iterable<SnapshotFiles>, ToXContentFragment, Writeable {
 
     public static final BlobStoreIndexShardSnapshots EMPTY = new BlobStoreIndexShardSnapshots(Collections.emptyList());
 
@@ -94,6 +97,19 @@ public class BlobStoreIndexShardSnapshots implements Iterable<SnapshotFiles>, To
             mapBuilder.put(entry.getKey(), List.copyOf(entry.getValue()));
         }
         this.physicalFiles = unmodifiableMap(mapBuilder);
+    }
+
+    public BlobStoreIndexShardSnapshots(StreamInput in) throws IOException {
+        this.shardSnapshots = in.readList(SnapshotFiles::new);
+        this.physicalFiles = in.readMap(StreamInput::readString, x -> x.readList(FileInfo::new));
+        this.files = in.readMap(StreamInput::readString, FileInfo::new);
+    }
+
+    @Override
+    public void writeTo(StreamOutput out) throws IOException {
+        out.writeList(shardSnapshots);
+        out.writeMap(physicalFiles, StreamOutput::writeString, (o, v) -> o.writeList(v));
+        out.writeMap(files, StreamOutput::writeString, (o, v) -> v.writeTo(out));
     }
 
     /**

server/src/main/java/org/elasticsearch/repositories/blobstore/BlobStoreRepository.java
@@ -92,6 +92,7 @@ import org.elasticsearch.common.bytes.BytesReference;
 import org.elasticsearch.common.component.AbstractLifecycleComponent;
 import org.elasticsearch.common.compress.NotXContentException;
 import org.elasticsearch.common.io.Streams;
+import org.elasticsearch.common.io.stream.NamedWriteableRegistry;
 import org.elasticsearch.common.lease.Releasable;
 import org.elasticsearch.common.lucene.Lucene;
 import org.elasticsearch.common.lucene.store.InputStreamIndexInput;
@@ -213,22 +214,42 @@ public abstract class BlobStoreRepository extends AbstractLifecycleComponent imp
 
     private final CounterMetric restoreRateLimitingTimeInNanos = new CounterMetric();
 
-    public static final ChecksumBlobStoreFormat<Metadata> GLOBAL_METADATA_FORMAT =
-            new ChecksumBlobStoreFormat<>("metadata", METADATA_NAME_FORMAT, Metadata::fromXContent);
+    public static final ChecksumBlobStoreFormat<Metadata> GLOBAL_METADATA_FORMAT = new ChecksumBlobStoreFormat<>(
+        "metadata",
+        METADATA_NAME_FORMAT,
+        Metadata::fromXContent,
+        Metadata::readFrom
+    );
 
-    public static final ChecksumBlobStoreFormat<IndexMetadata> INDEX_METADATA_FORMAT =
-            new ChecksumBlobStoreFormat<>("index-metadata", METADATA_NAME_FORMAT, IndexMetadata::fromXContent);
+    public static final ChecksumBlobStoreFormat<IndexMetadata> INDEX_METADATA_FORMAT = new ChecksumBlobStoreFormat<>(
+        "index-metadata",
+        METADATA_NAME_FORMAT,
+        IndexMetadata::fromXContent,
+        IndexMetadata::readFrom
+    );
 
     private static final String SNAPSHOT_CODEC = "snapshot";
 
-    public static final ChecksumBlobStoreFormat<SnapshotInfo> SNAPSHOT_FORMAT =
-            new ChecksumBlobStoreFormat<>(SNAPSHOT_CODEC, SNAPSHOT_NAME_FORMAT, SnapshotInfo::fromXContentInternal);
+    public static final ChecksumBlobStoreFormat<SnapshotInfo> SNAPSHOT_FORMAT = new ChecksumBlobStoreFormat<>(
+        SNAPSHOT_CODEC,
+        SNAPSHOT_NAME_FORMAT,
+        SnapshotInfo::fromXContentInternal,
+        SnapshotInfo::new
+    );
 
-    public static final ChecksumBlobStoreFormat<BlobStoreIndexShardSnapshot> INDEX_SHARD_SNAPSHOT_FORMAT =
-            new ChecksumBlobStoreFormat<>(SNAPSHOT_CODEC, SNAPSHOT_NAME_FORMAT, BlobStoreIndexShardSnapshot::fromXContent);
+    public static final ChecksumBlobStoreFormat<BlobStoreIndexShardSnapshot> INDEX_SHARD_SNAPSHOT_FORMAT = new ChecksumBlobStoreFormat<>(
+        SNAPSHOT_CODEC,
+        SNAPSHOT_NAME_FORMAT,
+        BlobStoreIndexShardSnapshot::fromXContent,
+        BlobStoreIndexShardSnapshot::new
+    );
 
-    public static final ChecksumBlobStoreFormat<BlobStoreIndexShardSnapshots> INDEX_SHARD_SNAPSHOTS_FORMAT =
-            new ChecksumBlobStoreFormat<>("snapshots", SNAPSHOT_INDEX_NAME_FORMAT, BlobStoreIndexShardSnapshots::fromXContent);
+    public static final ChecksumBlobStoreFormat<BlobStoreIndexShardSnapshots> INDEX_SHARD_SNAPSHOTS_FORMAT = new ChecksumBlobStoreFormat<>(
+        "snapshots",
+        SNAPSHOT_INDEX_NAME_FORMAT,
+        BlobStoreIndexShardSnapshots::fromXContent,
+        BlobStoreIndexShardSnapshots::new
+    );
 
     private final boolean readOnly;
 
@@ -243,6 +264,8 @@ public abstract class BlobStoreRepository extends AbstractLifecycleComponent imp
     private final ClusterService clusterService;
 
     private final RecoverySettings recoverySettings;
+
+    private final NamedWriteableRegistry namedWriteableRegistry;
 
     private final NamedXContentRegistry namedXContentRegistry;
 
@@ -279,6 +302,7 @@ public abstract class BlobStoreRepository extends AbstractLifecycleComponent imp
      */
     protected final int bufferSize;
 
+
     /**
      * Constructs new BlobStoreRepository
      * @param metadata   The metadata for this repository including name and settings
@@ -286,11 +310,13 @@ public abstract class BlobStoreRepository extends AbstractLifecycleComponent imp
      */
     protected BlobStoreRepository(
         final RepositoryMetadata metadata,
+        final NamedWriteableRegistry namedWriteableRegistry,
         final NamedXContentRegistry namedXContentRegistry,
         final ClusterService clusterService,
         final RecoverySettings recoverySettings,
         final BlobPath basePath) {
         this.metadata = metadata;
+        this.namedWriteableRegistry = namedWriteableRegistry;
         this.namedXContentRegistry = namedXContentRegistry;
         this.threadPool = clusterService.getClusterApplierService().threadPool();
         this.clusterService = clusterService;
@@ -707,7 +733,7 @@ public abstract class BlobStoreRepository extends AbstractLifecycleComponent imp
             for (String indexMetaGeneration : indexMetaGenerations) {
                 executor.execute(ActionRunnable.supply(allShardCountsListener, () -> {
                     try {
-                        return INDEX_METADATA_FORMAT.read(indexContainer, indexMetaGeneration, namedXContentRegistry).getNumberOfShards();
+                        return INDEX_METADATA_FORMAT.read(indexContainer, indexMetaGeneration, namedWriteableRegistry, namedXContentRegistry).getNumberOfShards();
                     } catch (Exception ex) {
                         LOGGER.warn(() -> new ParameterizedMessage(
                             "[{}] [{}] failed to read metadata for index", indexMetaGeneration, indexId.getName()), ex);
@@ -1043,7 +1069,7 @@ public abstract class BlobStoreRepository extends AbstractLifecycleComponent imp
     @Override
     public CompletableFuture<SnapshotInfo> getSnapshotInfo(SnapshotId snapshotId) {
         try {
-            return CompletableFuture.completedFuture(SNAPSHOT_FORMAT.read(blobContainer(), snapshotId.getUUID(), namedXContentRegistry));
+            return CompletableFuture.completedFuture(SNAPSHOT_FORMAT.read(blobContainer(), snapshotId.getUUID(), namedWriteableRegistry, namedXContentRegistry));
         } catch (NoSuchFileException ex) {
             return CompletableFuture.failedFuture(new SnapshotMissingException(metadata.name(), snapshotId, ex));
         } catch (IOException | NotXContentException ex) {
@@ -1056,7 +1082,7 @@ public abstract class BlobStoreRepository extends AbstractLifecycleComponent imp
     @Override
     public CompletableFuture<Metadata> getSnapshotGlobalMetadata(final SnapshotId snapshotId) {
         try {
-            return CompletableFuture.completedFuture(GLOBAL_METADATA_FORMAT.read(blobContainer(), snapshotId.getUUID(), namedXContentRegistry));
+            return CompletableFuture.completedFuture(GLOBAL_METADATA_FORMAT.read(blobContainer(), snapshotId.getUUID(), namedWriteableRegistry, namedXContentRegistry));
         } catch (NoSuchFileException ex) {
             return CompletableFuture.failedFuture(new SnapshotMissingException(metadata.name(), snapshotId, ex));
         } catch (IOException ex) {
@@ -1067,7 +1093,7 @@ public abstract class BlobStoreRepository extends AbstractLifecycleComponent imp
     public CompletableFuture<IndexMetadata> getSnapshotIndexMetadata(RepositoryData repositoryData, SnapshotId snapshotId, IndexId index) {
         try {
             IndexMetadata result = INDEX_METADATA_FORMAT.read(indexContainer(index),
-                repositoryData.indexMetaDataGenerations().indexMetaBlobId(snapshotId, index), namedXContentRegistry);
+                repositoryData.indexMetaDataGenerations().indexMetaBlobId(snapshotId, index), namedWriteableRegistry, namedXContentRegistry);
             return CompletableFuture.completedFuture(result);
         } catch (IOException ex) {
             return CompletableFuture.failedFuture(ex);
@@ -1081,7 +1107,7 @@ public abstract class BlobStoreRepository extends AbstractLifecycleComponent imp
             var result = new ArrayList<IndexMetadata>(indexIds.size());
             for (IndexId index : indexIds) {
                 result.add(INDEX_METADATA_FORMAT.read(indexContainer(index),
-                    repositoryData.indexMetaDataGenerations().indexMetaBlobId(snapshotId, index), namedXContentRegistry));
+                    repositoryData.indexMetaDataGenerations().indexMetaBlobId(snapshotId, index), namedWriteableRegistry, namedXContentRegistry));
             }
             return CompletableFuture.completedFuture(result);
         } catch (IOException ex) {
@@ -2211,7 +2237,7 @@ public abstract class BlobStoreRepository extends AbstractLifecycleComponent imp
      */
     public BlobStoreIndexShardSnapshot loadShardSnapshot(BlobContainer shardContainer, SnapshotId snapshotId) {
         try {
-            return INDEX_SHARD_SNAPSHOT_FORMAT.read(shardContainer, snapshotId.getUUID(), namedXContentRegistry);
+            return INDEX_SHARD_SNAPSHOT_FORMAT.read(shardContainer, snapshotId.getUUID(), namedWriteableRegistry, namedXContentRegistry);
         } catch (NoSuchFileException ex) {
             throw new SnapshotMissingException(metadata.name(), snapshotId, ex);
         } catch (IOException ex) {
@@ -2237,7 +2263,7 @@ public abstract class BlobStoreRepository extends AbstractLifecycleComponent imp
             if (generation.equals(ShardGenerations.NEW_SHARD_GEN)) {
                 return new Tuple<>(BlobStoreIndexShardSnapshots.EMPTY, ShardGenerations.NEW_SHARD_GEN);
             }
-            return new Tuple<>(INDEX_SHARD_SNAPSHOTS_FORMAT.read(shardContainer, generation, namedXContentRegistry), generation);
+            return new Tuple<>(INDEX_SHARD_SNAPSHOTS_FORMAT.read(shardContainer, generation, namedWriteableRegistry, namedXContentRegistry), generation);
         }
         final Tuple<BlobStoreIndexShardSnapshots, Long> legacyIndex = buildBlobStoreIndexShardSnapshots(blobs, shardContainer);
         return new Tuple<>(legacyIndex.v1(), String.valueOf(legacyIndex.v2()));
@@ -2254,7 +2280,7 @@ public abstract class BlobStoreRepository extends AbstractLifecycleComponent imp
         long latest = latestGeneration(blobs);
         if (latest >= 0) {
             final BlobStoreIndexShardSnapshots shardSnapshots =
-                    INDEX_SHARD_SNAPSHOTS_FORMAT.read(shardContainer, Long.toString(latest), namedXContentRegistry);
+                    INDEX_SHARD_SNAPSHOTS_FORMAT.read(shardContainer, Long.toString(latest), namedWriteableRegistry, namedXContentRegistry);
             return new Tuple<>(shardSnapshots, latest);
         } else if (blobs.stream().anyMatch(b -> b.startsWith(SNAPSHOT_PREFIX) || b.startsWith(INDEX_FILE_PREFIX)
                                                 || b.startsWith(UPLOADED_DATA_BLOB_PREFIX))) {

plugins/es-repository-azure/src/main/java/org/elasticsearch/repositories/azure/AzureRepository.java
@@ -35,6 +35,7 @@ import org.elasticsearch.cluster.service.ClusterService;
 import org.elasticsearch.common.Strings;
 import org.elasticsearch.common.blobstore.BlobPath;
 import org.elasticsearch.common.blobstore.BlobStore;
+import org.elasticsearch.common.io.stream.NamedWriteableRegistry;
 import org.elasticsearch.common.settings.SecureString;
 import org.elasticsearch.common.settings.Setting;
 import org.elasticsearch.common.settings.Setting.Property;
@@ -42,11 +43,11 @@ import org.elasticsearch.common.unit.ByteSizeValue;
 import org.elasticsearch.common.xcontent.NamedXContentRegistry;
 import org.elasticsearch.indices.recovery.RecoverySettings;
 import org.elasticsearch.repositories.blobstore.BlobStoreRepository;
+import org.jetbrains.annotations.VisibleForTesting;
 
 import com.microsoft.azure.storage.LocationMode;
 import com.microsoft.azure.storage.RetryPolicy;
 
-import org.jetbrains.annotations.VisibleForTesting;
 import io.crate.common.unit.TimeValue;
 import io.crate.types.DataTypes;
 
@@ -176,10 +177,11 @@ public class AzureRepository extends BlobStoreRepository {
     private final boolean readonly;
 
     public AzureRepository(RepositoryMetadata metadata,
+                           NamedWriteableRegistry namedWriteableRegistry,
                            NamedXContentRegistry namedXContentRegistry,
                            ClusterService clusterService,
                            RecoverySettings recoverySettings) {
-        super(metadata, namedXContentRegistry, clusterService, recoverySettings, buildBasePath(metadata));
+        super(metadata, namedWriteableRegistry, namedXContentRegistry, clusterService, recoverySettings, buildBasePath(metadata));
         this.chunkSize = Repository.CHUNK_SIZE_SETTING.get(metadata.settings());
 
         // If the user explicitly did not define a readonly value, we set it by ourselves depending on the location mode setting.

commit bc00a1bc4164ba7f791c3b8995523519f169fec5

shard snapshot deserialization must re-use FileInfo instances
Serialization code must reflect this idea as well
and write files and then only links to those files like it was done before 5.10

Fixes a regression introduced in https://github.com/crate/crate/commit/8f941f829d1fe4193f968af9a96bd3e120708fd4

server/src/main/java/org/elasticsearch/index/snapshots/blobstore/BlobStoreIndexShardSnapshots.java
@@ -30,6 +30,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.elasticsearch.ElasticsearchParseException;
+import org.elasticsearch.Version;
 import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.io.stream.StreamOutput;
 import org.elasticsearch.common.io.stream.Writeable;
@@ -97,17 +98,61 @@ public class BlobStoreIndexShardSnapshots implements Iterable<SnapshotFiles>, Wr
         this.physicalFiles = unmodifiableMap(mapBuilder);
     }
 
-    public BlobStoreIndexShardSnapshots(StreamInput in) throws IOException {
-        this.shardSnapshots = in.readList(SnapshotFiles::new);
-        this.physicalFiles = in.readMap(StreamInput::readString, x -> x.readList(FileInfo::new));
-        this.files = in.readMap(StreamInput::readString, FileInfo::new);
+    public static BlobStoreIndexShardSnapshots fromStream(StreamInput in) throws IOException {
+        if (in.getVersion().after(Version.V_5_10_2)) {
+            Map<String, FileInfo> filesByName = in.readMap(StreamInput::readString, FileInfo::new);
+            List<SnapshotFiles> shardSnapshots = new ArrayList<>();
+            int shardSnapshotsSize = in.readVInt();
+            for (int i = 0; i < shardSnapshotsSize; i++) {
+                shardSnapshots.add(
+                    new SnapshotFiles(
+                        in.readString(),
+                        in.readList(input -> filesByName.get(input.readString())), // reuse instance
+                        in.readOptionalString())
+                );
+            }
+            return new BlobStoreIndexShardSnapshots(filesByName, shardSnapshots);
+        } else {
+            return new BlobStoreIndexShardSnapshots(
+                in.readList(SnapshotFiles::new),
+                in.readMap(StreamInput::readString, x -> x.readList(FileInfo::new)),
+                in.readMap(StreamInput::readString, FileInfo::new)
+            );
+        }
+    }
+
+    private BlobStoreIndexShardSnapshots(List<SnapshotFiles> shardSnapshots,
+                                         Map<String, List<FileInfo>> physicalFiles,
+                                         Map<String, FileInfo> files) {
+        this.shardSnapshots = shardSnapshots;
+        this.physicalFiles = physicalFiles;
+        this.files = files;
     }
 
     @Override
     public void writeTo(StreamOutput out) throws IOException {
-        out.writeList(shardSnapshots);
-        out.writeMap(physicalFiles, StreamOutput::writeString, (o, v) -> o.writeList(v));
-        out.writeMap(files, StreamOutput::writeString, (o, v) -> v.writeTo(out));
+        if (out.getVersion().after(Version.V_5_10_2)) {
+            // Don't write "shardSnapshots" as is.
+            // Each SnapshotFiles has list of FileInfo (indexFiles) and those lists can overlap across many SnapshotFiles instances.
+            // We use 5.9 approach: write names that are basically links to the "files".
+            // This way, deserialization code can read "files" once and re-use those instances across all SnapshotFiles.
+
+            // Same applies for physicalFiles, when derived from files and shardSnapshots,
+            // it re-uses FileInfo instances from "files", so we shouldn't write it as well.
+
+            // In addition to having lower memory footprint on reading, writing less data leads to smaller snapshots.
+            out.writeMap(files, StreamOutput::writeString, (o, fileInfo) -> fileInfo.writeTo(o));
+            out.writeVInt(shardSnapshots.size());
+            for (SnapshotFiles snapshotFiles: shardSnapshots) {
+                out.writeString(snapshotFiles.snapshot());
+                out.writeCollection(snapshotFiles.indexFiles(), (o, fileInfo) -> o.writeString(fileInfo.name()));
+                out.writeOptionalString(snapshotFiles.shardStateIdentifier());
+            }
+        } else {
+            out.writeList(shardSnapshots);
+            out.writeMap(physicalFiles, StreamOutput::writeString, (o, v) -> o.writeList(v));
+            out.writeMap(files, StreamOutput::writeString, (o, v) -> v.writeTo(out));
+        }
     }
 
     /**
@@ -117,6 +162,14 @@ public class BlobStoreIndexShardSnapshots implements Iterable<SnapshotFiles>, Wr
      */
     public List<SnapshotFiles> snapshots() {
         return this.shardSnapshots;
+    }
+
+    public Map<String, FileInfo> files() {
+        return files;
+    }
+
+    public Map<String, List<FileInfo>> physicalFiles() {
+        return physicalFiles;
     }
 
     /**

server/src/main/java/org/elasticsearch/repositories/blobstore/BlobStoreRepository.java
@@ -248,7 +248,7 @@ public abstract class BlobStoreRepository extends AbstractLifecycleComponent imp
         "snapshots",
         SNAPSHOT_INDEX_NAME_FORMAT,
         BlobStoreIndexShardSnapshots::fromXContent,
-        BlobStoreIndexShardSnapshots::new
+        BlobStoreIndexShardSnapshots::fromStream
     );
 
     private final boolean readOnly;

server/src/test/java/org/elasticsearch/index/snapshots/blobstore/BlobStoreIndexShardSnapshotsTest.java
@@ -1,0 +1,175 @@
+/*
+ * Licensed to Crate.io GmbH ("Crate") under one or more contributor
+ * license agreements.  See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.  Crate licenses
+ * this file to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.  You may
+ * obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * However, if you have executed another commercial license agreement
+ * with Crate these terms will supersede the license and you may use the
+ * software solely pursuant to the terms of the relevant commercial agreement.
+ */
+
+package org.elasticsearch.index.snapshots.blobstore;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.elasticsearch.repositories.blobstore.BlobStoreRepository.INDEX_SHARD_SNAPSHOTS_FORMAT;
+
+import java.lang.management.ManagementFactory;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+import org.elasticsearch.Version;
+import org.elasticsearch.common.bytes.BytesReference;
+import org.elasticsearch.common.io.stream.BytesStreamOutput;
+import org.elasticsearch.common.io.stream.StreamInput;
+import org.elasticsearch.common.unit.ByteSizeValue;
+import org.elasticsearch.index.snapshots.blobstore.BlobStoreIndexShardSnapshot.FileInfo;
+import org.elasticsearch.index.store.StoreFileMetadata;
+import org.elasticsearch.test.ESTestCase;
+import org.junit.Test;
+
+import com.sun.management.ThreadMXBean;
+
+public class BlobStoreIndexShardSnapshotsTest extends ESTestCase {
+
+    @Test
+    public void test_bwc_streaming() throws Exception {
+        BlobStoreIndexShardSnapshots blobStoreIndexShardSnapshots = prepareData();
+        BytesStreamOutput out = new BytesStreamOutput();
+        blobStoreIndexShardSnapshots.writeTo(out);
+
+        StreamInput in = out.bytes().streamInput();
+        BlobStoreIndexShardSnapshots fromStream = BlobStoreIndexShardSnapshots.fromStream(in);
+        assertThat(isSame(blobStoreIndexShardSnapshots, fromStream)).isTrue();
+
+        out = new BytesStreamOutput();
+        out.setVersion(Version.V_5_10_1);
+        blobStoreIndexShardSnapshots.writeTo(out);
+
+        in = out.bytes().streamInput();
+        in.setVersion(org.elasticsearch.Version.V_5_10_1);
+        fromStream = BlobStoreIndexShardSnapshots.fromStream(in);
+        assertThat(isSame(blobStoreIndexShardSnapshots, fromStream)).isTrue();
+
+    }
+
+    @Test
+    public void test_read_from_stream_allocations() throws Exception {
+        BytesReference bytesReference = INDEX_SHARD_SNAPSHOTS_FORMAT.serialize(prepareData(), "dummyBlobName", true);
+
+        ThreadMXBean threadMXBean = ManagementFactory.getPlatformMXBean(ThreadMXBean.class);
+        long threadId = Thread.currentThread().threadId();
+        long allocatedBytesBegin = threadMXBean.getThreadAllocatedBytes(threadId);
+
+        INDEX_SHARD_SNAPSHOTS_FORMAT.deserialize(
+            "dummyBlobname",
+            writableRegistry(),
+            xContentRegistry(),
+            bytesReference
+        );
+
+        long allocatedBytesAfter = threadMXBean.getThreadAllocatedBytes(threadId);
+        long allocatedBytes = allocatedBytesAfter - allocatedBytesBegin;
+        // Actual value fluctuates around 9_877_672.
+        // Used to fluctuate around 29_228_160 on 5.10.2 when instances were not re-used.
+        assertThat(allocatedBytes).isLessThanOrEqualTo(10_000_000L);
+    }
+
+    /**
+     * Returns an instance with SnapshotFiles instances having their indexFiles overlapping
+     * and those overlaps being re-used as a same instance.
+     *
+     * We need to ensure that we can read such optimal file in a way that deserialized instance also has an optimal structure.
+     */
+    private static BlobStoreIndexShardSnapshots prepareData() throws Exception {
+        // Method getThreadAllocatedBytes is a rough estimation and returned value fluctuates a little bit.
+        // Generate a bigger list so that difference with/without the fix is visible (like 3x) and can't be treated as a noise.
+        List<FileInfo> fileInfos = new ArrayList<>();
+        for (int i = 1; i <= 10_000; i++) {
+            String name = "name" + i;
+            fileInfos.add(
+                new FileInfo(
+                    name,
+                    new StoreFileMetadata(name, 1, "dummy_checksum", org.apache.lucene.util.Version.LUCENE_9_12_0),
+                    new ByteSizeValue(i)
+                )
+            );
+        }
+
+        // Different SnapshotFiles can refer to the same FileInfo-s in the SnapshotFiles.indexFiles field.
+        // Making 2 lists almost completely overlapping to make difference visible and eliminate getThreadAllocatedBytes fluctuation noise.
+        SnapshotFiles snapshot1 = new SnapshotFiles("1", fileInfos.subList(10, fileInfos.size()), null);
+        SnapshotFiles snapshot2 = new SnapshotFiles("1", fileInfos.subList(0, fileInfos.size() - 10), null);
+        return new BlobStoreIndexShardSnapshots(List.of(snapshot1, snapshot2));
+    }
+
+    private static boolean isSame(BlobStoreIndexShardSnapshots shardSnapshot1, BlobStoreIndexShardSnapshots shardSnapshot2) {
+        Map<String, FileInfo> files1 = shardSnapshot1.files();
+        Map<String, FileInfo> files2 = shardSnapshot1.files();
+        if (files1.size() != files2.size()) {
+            return false;
+        }
+        for (Map.Entry<String, FileInfo> entry : files1.entrySet()) {
+            if (entry.getValue().isSame(files2.get(entry.getKey())) == false) {
+                return false;
+            }
+        }
+
+        Map<String, List<FileInfo>> physicalFiles1 = shardSnapshot1.physicalFiles();
+        Map<String, List<FileInfo>> physicalFiles2 = shardSnapshot1.physicalFiles();
+        if (physicalFiles1.size() != physicalFiles2.size()) {
+            return false;
+        }
+        for (Map.Entry<String, List<FileInfo>> entry : physicalFiles1.entrySet()) {
+            List<FileInfo> list1 = entry.getValue();
+            List<FileInfo> list2 = physicalFiles2.get(entry.getKey());
+            if (list1.size() != list2.size()) {
+                return false;
+            }
+            for (int i = 0; i < list1.size(); i++) {
+                if (list1.get(i).isSame(list2.get(i)) == false) {
+                    return false;
+                }
+            }
+        }
+
+        List<SnapshotFiles> shapshotFiles1 = shardSnapshot1.snapshots();
+        List<SnapshotFiles> shapshotFiles2 = shardSnapshot1.snapshots();
+        if (shapshotFiles1.size() != shapshotFiles2.size()) {
+            return false;
+        }
+        for (int i = 0; i < shapshotFiles1.size(); i++) {
+            SnapshotFiles sf1 = shapshotFiles1.get(i);
+            SnapshotFiles sf2 = shapshotFiles2.get(i);
+            if (sf1.snapshot().equals(sf2.snapshot()) == false) {
+                return false;
+            }
+            if (Objects.equals(sf1.shardStateIdentifier(), sf2.shardStateIdentifier()) == false) {
+                return false;
+            }
+            List<FileInfo> list1 = sf1.indexFiles();
+            List<FileInfo> list2 = sf2.indexFiles();
+            if (list1.size() != list2.size()) {
+                return false;
+            }
+            for (int j = 0; j < list1.size(); j++) {
+                if (list1.get(j).isSame(list2.get(j)) == false) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}