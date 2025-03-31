cc15821905a8d249538011d2e27013f9b31352fe
Sebastian Utz <su@rtme.net>
2025-03-31 13:46:19 +0200

    Fix new column collection to not contain duplicates
    Duplicated new columns weren't a real issue besides memory consumption
    until the field limit detection on dynamically added columns was fixed
    by https://github.com/crate/crate/commit/ea6fa68af2.
    
    Fixes #17691.
    
    (cherry picked from commit 2aa7e269ec7bc92d6f4dfa68392f6293905861d7)

--- server/src/main/java/io/crate/execution/dml/Indexer.java
+++ server/src/main/java/io/crate/execution/dml/Indexer.java
@@ -31,6 +31,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
@@ -678,7 +679,7 @@ public class Indexer {
      * Looks for new columns in the values of the given IndexItem and returns them.
      */
     public List<Reference> collectSchemaUpdates(IndexItem item) throws IOException {
-        ArrayList<Reference> newColumns = new ArrayList<>();
+        LinkedHashSet<Reference> newColumns = new LinkedHashSet<>();
         Consumer<? super Reference> onDynamicColumn = ref -> {
             ref.column().validForCreate();
             newColumns.add(ref);
@@ -717,7 +718,7 @@ public class Indexer {
                 synthetics::get
             );
         }
-        return newColumns;
+        return newColumns.stream().toList();
     }
 
     /**
