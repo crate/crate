636fca3e3e7961488a0239abd70f9171cd76721e
Michael Kleen <mkleen@gmail.com>
2025-06-10 10:15:50 +0200

    wip

--- server/src/main/java/io/crate/statistics/arrow/Statistics.java
+++ server/src/main/java/io/crate/statistics/arrow/Statistics.java
@@ -22,91 +22,120 @@
 package io.crate.statistics.arrow;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.apache.arrow.memory.BufferAllocator;
-import org.apache.arrow.memory.RootAllocator;
 import org.apache.arrow.vector.BigIntVector;
-import org.apache.arrow.vector.FieldVector;
-import org.apache.arrow.vector.IntVector;
-import org.apache.arrow.vector.ValueVector;
 import org.apache.arrow.vector.VarCharVector;
 import org.apache.arrow.vector.VectorSchemaRoot;
 import org.apache.arrow.vector.complex.ListVector;
-import org.apache.arrow.vector.complex.MapVector;
-import org.apache.arrow.vector.complex.StructVector;
+import org.apache.arrow.vector.complex.impl.UnionListReader;
 import org.apache.arrow.vector.complex.impl.UnionListWriter;
-import org.apache.arrow.vector.complex.impl.UnionMapReader;
-import org.apache.arrow.vector.complex.impl.UnionMapWriter;
+import org.apache.arrow.vector.complex.reader.BaseReader;
+import org.apache.arrow.vector.complex.reader.Float8Reader;
+import org.apache.arrow.vector.complex.reader.VarCharReader;
 import org.apache.arrow.vector.complex.writer.BaseWriter;
 import org.apache.arrow.vector.types.FloatingPointPrecision;
 import org.apache.arrow.vector.types.pojo.ArrowType;
 import org.apache.arrow.vector.types.pojo.Field;
 import org.apache.arrow.vector.types.pojo.FieldType;
 import org.apache.arrow.vector.types.pojo.Schema;
-import org.apache.arrow.vector.util.JsonStringHashMap;
-import org.apache.arrow.vector.util.Text;
 import org.jetbrains.annotations.Nullable;
 
 
 import io.crate.metadata.ColumnIdent;
-import io.crate.sql.tree.Extract;
+import io.crate.metadata.RelationName;
 import io.crate.statistics.ColumnStats;
 
 public class Statistics {
 
-    private final VectorSchemaRoot vector;
-    private final Map<ColumnIdent, ColumnStats<?>> statsByColumn;
+    private static final Field RELATION_NAME = Field.notNullable("relationName", new ArrowType.Utf8());
+    private static final Field NUM_DOCS = Field.notNullable("numDocs", new ArrowType.Int(64, true));
+    private static final Field SIZE_IN_BYTES = Field.notNullable("sizeInBytes", new ArrowType.Int(64, true));
 
-    public Statistics(BufferAllocator bufferAllocator, long numDocs, long sizeInBytes, Map<ColumnIdent, ColumnStats<?>> statsByColumn) {
-        vector = VectorSchemaRoot.create(schema(), bufferAllocator);
-        BigIntVector numDocsVector = (BigIntVector) vector.getVector("numDocs");
+    private static final Field COLUMN_IDENT = Field.notNullable("columnIdent", new ArrowType.Utf8()));
+    private static final Field NULL_FRACTION = Field.notNullable("nullFraction", new ArrowType.FloatingPoint(FloatingPointPrecision.DOUBLE));
+    private static final Field AVERAGE_SIZE_IN_BYTES = Field.notNullable("averageSizeInBytes", new ArrowType.FloatingPoint(FloatingPointPrecision.DOUBLE));
+    private static final Field APPROX_DISTINCT = Field.notNullable("approxDistinct", new ArrowType.FloatingPoint(FloatingPointPrecision.DOUBLE));
+
+    private final VectorSchemaRoot root;
+
+    public Statistics(BufferAllocator bufferAllocator,
+                      RelationName relationName,
+                      long numDocs,
+                      long sizeInBytes,
+                      Map<ColumnIdent, ColumnStats<?>> statsByColumn) {
+        root = VectorSchemaRoot.create(schema(), bufferAllocator);
+        VarCharVector relationNameVector = (VarCharVector) root.getVector(RELATION_NAME);
+        relationNameVector.allocateNew(1);
+        relationNameVector.set(0, relationName.fqn().getBytes());
+        BigIntVector numDocsVector = (BigIntVector) root.getVector(NUM_DOCS);
         numDocsVector.allocateNew(1);
         numDocsVector.set(0, numDocs);
-        BigIntVector sizeInBytesVector = (BigIntVector) vector.getVector("sizeInBytes");
+        BigIntVector sizeInBytesVector = (BigIntVector) root.getVector(SIZE_IN_BYTES);
         sizeInBytesVector.allocateNew(1);
         sizeInBytesVector.set(0, sizeInBytes);
-        vector.setRowCount(1);
-        this.statsByColumn = statsByColumn;
+        int index = 0;
+        for (Map.Entry<ColumnIdent, ColumnStats<?>> columnIdentColumnStatsEntry : statsByColumn.entrySet()) {
+            ColumnIdent columnIdent = columnIdentColumnStatsEntry.getKey();
+            ColumnStats<?> columnStats = columnIdentColumnStatsEntry.getValue();
+            ListVector statsByColumnVector = (ListVector) root.getVector("statsByColumn");
+            UnionListWriter listWriter = statsByColumnVector.getWriter();
+            listWriter.startList();
+            listWriter.setPosition(index);
+            BaseWriter.StructWriter struct = listWriter.struct();
+            struct.start();
+            struct.varChar("columnIdent").writeVarChar(columnIdent.fqn());
+            struct.float8("nullFraction").writeFloat8(columnStats.nullFraction());
+            struct.float8("averageSizeInBytes").writeFloat8(columnStats.averageSizeInBytes());
+            struct.float8("approxDistinct").writeFloat8(columnStats.approxDistinct());
+            struct.end();
+            listWriter.endList();
+            listWriter.setValueCount(index);
+            index++;
+        }
+        root.setRowCount(1);
     }
 
     public long numDocs() {
-        BigIntVector numDocsVector = (BigIntVector) vector.getVector("numDocs");
+        BigIntVector numDocsVector = (BigIntVector) root.getVector(NUM_DOCS);
         return numDocsVector.get(0);
     }
 
     public long sizeInBytes() {
-        BigIntVector sizeInBytesVector = (BigIntVector) vector.getVector("sizeInBytes");
+        BigIntVector sizeInBytesVector = (BigIntVector) root.getVector(SIZE_IN_BYTES);
         return sizeInBytesVector.get(0);
     }
 
     public Map<ColumnIdent, ColumnStats<?>> statsByColumn() {
-        return statsByColumn;
+        Map<ColumnIdent, ColumnStats<?>> result = new HashMap<>();
+        ListVector statsByColumn = (ListVector) root.getVector("statsByColumn");
+        UnionListReader listReader = statsByColumn.getReader();
+        for (int i = 0; i < statsByColumn.getValueCount(); i++) {
+            listReader.setPosition(i);
+            while (listReader.next()) {
+                BaseReader.StructReader structReader = listReader.reader();
+                VarCharReader columnIdenReader = structReader.reader("columnIdent");
+                ColumnIdent columnIdent  = ColumnIdent.of(columnIdenReader.readText().toString());
+                Float8Reader nullFraction = (Float8Reader) structReader.reader("nullFraction");
+                Float8Reader averageSizeInBytes = (Float8Reader) structReader.reader("averageSizeInBytes");
+                Float8Reader approxDistinct = (Float8Reader) structReader.reader("approxDistinct");
+                ColumnStats<?> columnStats = new ColumnStats<?>(
+                    nullFraction.readDouble(),
+                    averageSizeInBytes.readDouble(),
+                    approxDistinct.readDouble(),
+            }
+        }
     }
 
     @Nullable
     public ColumnStats<?> getColumnStats(ColumnIdent column) {
-        return statsByColumn.get(column);
+        return null;
     }
 
     private static Schema schema() {
-
-        List<Field> columnStats = new ArrayList<>();
-        columnStats.add(Field.notNullable("nullFraction", new ArrowType.FloatingPoint(FloatingPointPrecision.DOUBLE)));
-        columnStats.add(Field.notNullable("averageSizeInBytes", new ArrowType.FloatingPoint(FloatingPointPrecision.DOUBLE)));
-        columnStats.add(Field.notNullable("approxDistinct", new ArrowType.FloatingPoint(FloatingPointPrecision.DOUBLE)));
-        columnStats.add(Field.notNullable("type", new ArrowType.Int(16, true)));
-
-        List<Field> stats = new ArrayList<>();
-        stats.add(Field.notNullable("numDocs", new ArrowType.Int(64, true)));
-        stats.add(Field.notNullable("sizeInBytes", new ArrowType.Int(64, true)));
-        stats.add(new Field("statsByColumn", FieldType.nullable(new ArrowType.Map(false)), columnStats));
-        return new Schema(stats);
-    }
-
-    public static void main(String[] args) {
-
         List<Field> stats = new ArrayList<>();
         stats.add(Field.notNullable("relationName", new ArrowType.Utf8()));
         stats.add(Field.notNullable("numDocs", new ArrowType.Int(64, true)));
@@ -115,55 +144,11 @@ public class Statistics {
         List<Field> columnStats = new ArrayList<>();
         columnStats.add(Field.notNullable("columnIdent", new ArrowType.Utf8()));
         columnStats.add(Field.notNullable("nullFraction", new ArrowType.FloatingPoint(FloatingPointPrecision.DOUBLE)));
-//        columnStats.add(Field.notNullable("averageSizeInBytes", new ArrowType.FloatingPoint(FloatingPointPrecision.DOUBLE)));
-//        columnStats.add(Field.notNullable("approxDistinct", new ArrowType.FloatingPoint(FloatingPointPrecision.DOUBLE)));
+        columnStats.add(Field.notNullable("averageSizeInBytes", new ArrowType.FloatingPoint(FloatingPointPrecision.DOUBLE)));
+        columnStats.add(Field.notNullable("approxDistinct", new ArrowType.FloatingPoint(FloatingPointPrecision.DOUBLE)));
         Field columnStatsStruct = new Field("statsByColumn", FieldType.nullable(new ArrowType.Struct()), columnStats);
         FieldType listType = new FieldType(true, new ArrowType.List(), null);
         stats.add(new Field("statsByColumn",listType, List.of(columnStatsStruct)));
-        Schema schema =  new Schema(stats);
-        System.out.println("schema = " + schema);
-
-        VectorSchemaRoot root = VectorSchemaRoot.create(schema, new RootAllocator());
-
-        VarCharVector relationNameVector = (VarCharVector) root.getVector("relationName");
-
-        relationNameVector.allocateNew(1);
-        relationNameVector.set(0,"doc.test".getBytes());
-        relationNameVector.setValueCount(1);
-
-        BigIntVector numDocsVector = (BigIntVector) root.getVector("numDocs");
-        numDocsVector.allocateNew(1);
-        numDocsVector.set(0, 100);
-        numDocsVector.setValueCount(1);
-
-        BigIntVector sizeInBytesVector = (BigIntVector) root.getVector("sizeInBytes");
-        sizeInBytesVector.allocateNew(1);
-        sizeInBytesVector.set(0, 1000);
-
-        sizeInBytesVector.setValueCount(1);
-
-        ListVector statsByColumn = (ListVector) root.getVector("statsByColumn");
-        UnionListWriter listWriter = statsByColumn.getWriter();
-        listWriter.startList();
-        listWriter.setPosition(0);
-        BaseWriter.StructWriter struct = listWriter.struct();
-        struct.start();
-        struct.varChar("columnIdent").writeVarChar("column");
-        struct.float8("nullFraction").writeFloat8(10.0);
-        struct.end();
-        listWriter.endList();
-        listWriter.setValueCount(1);
-
-        root.setRowCount(1);
-
-        VarCharVector relationName = (VarCharVector) root.getVector("relationName");
-        System.out.println("relationName = " + new String(relationName.get(0)));
-        BigIntVector numDocs = (BigIntVector) root.getVector("numDocs");
-        System.out.println("numDocs = " + numDocs.get(0));
-        BigIntVector sizeInBytes = (BigIntVector) root.getVector("numDocs");
-        System.out.println("sizeInBytes = " + sizeInBytes.get(0));
-        ListVector statsByColumn1 = (ListVector) root.getVector("statsByColumn");
-        System.out.println("statsByColumn1 = " + statsByColumn1);
-        
+        return new Schema(stats);
     }
 }
