crate/crate

commit 9b61be3c7aef89289e8575e23dad7ca8e9bdc12e

Fail RestResultSetReceiver on circuit breaker exceptions
Non-bulk execution depends on receiver's future being completed to complete request processing.
If we don't fail on CBE, 'SqlHttpHandler.handleSQLRequest.whenComplete' is never done.

server/src/main/java/io/crate/rest/action/RestResultSetReceiver.java
@@ -68,7 +68,7 @@ class RestResultSetReceiver implements ResultReceiver<XContentBuilder> {
             builder.addRow(row, outputFields.size());
             rowCount++;
             return null;
-        } catch (IOException e) {
+        } catch (Exception e) {
             fail(e);
             return null;
         }

server/src/test/java/io/crate/rest/action/RestActionReceiversTest.java
@@ -21,6 +21,7 @@
 
 package io.crate.rest.action;
 
+import static io.crate.data.breaker.BlockBasedRamAccounting.MAX_BLOCK_SIZE_IN_BYTES;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.IOException;
@@ -28,6 +29,7 @@ import java.util.Collections;
 import java.util.List;
 
 import org.elasticsearch.common.Strings;
+import org.elasticsearch.common.breaker.TestCircuitBreaker;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.common.xcontent.json.JsonXContent;
 import org.elasticsearch.index.engine.VersionConflictEngineException;
@@ -37,15 +39,18 @@ import org.junit.Test;
 
 import io.crate.auth.AccessControl;
 import io.crate.breaker.TypedRowAccounting;
+import io.crate.common.collections.Lists;
 import io.crate.data.Row;
 import io.crate.data.Row1;
 import io.crate.data.RowN;
+import io.crate.data.breaker.BlockBasedRamAccounting;
 import io.crate.data.breaker.RamAccounting;
 import io.crate.expression.symbol.ScopedSymbol;
 import io.crate.expression.symbol.Symbol;
 import io.crate.expression.symbol.Symbols;
 import io.crate.metadata.ColumnIdent;
 import io.crate.metadata.RelationName;
+import io.crate.session.ResultReceiver;
 import io.crate.types.DataTypes;
 
 public class RestActionReceiversTest extends ESTestCase {
@@ -144,5 +149,31 @@ public class RestActionReceiversTest extends ESTestCase {
         bulkRowCountReceiver.allFinished();
         assertThat(results[0].rowCount()).isEqualTo(1L);
         assertThat(results[0].error()).isNull();
+    }
+
+    @Test
+    public void test_result_reciever_future_completed_on_cbe() throws Exception {
+        TestCircuitBreaker breaker = new TestCircuitBreaker();
+        breaker.startBreaking();
+        RamAccounting ramAccounting = new BlockBasedRamAccounting(
+            b -> breaker.addEstimateBytesAndMaybeBreak(b, "http-result"),
+            MAX_BLOCK_SIZE_IN_BYTES);
+
+        ResultReceiver<XContentBuilder> resultReceiver = new RestResultSetReceiver(
+            JsonXContent.builder(),
+            fields,
+            fieldNames,
+            System.currentTimeMillis(),
+            new TypedRowAccounting(
+                Lists.map(fields, Symbol::valueType),
+                ramAccounting
+            ),
+            false
+        );
+
+        // Fails with CBE, resultReceiver's future must be completed, so that sys.jobs entry is cleared.
+        resultReceiver.setNextRow(rows.get(0));
+        assertThat(resultReceiver.completionFuture().isDone()).isTrue();
+        assertThat(resultReceiver.completionFuture().isCompletedExceptionally()).isTrue();
     }
 }

commit b9826e8f062fb4059770efdd3264dfd4fc1a3a9a

Ensure that RestResultSetReceiver.setNext throws an error
Callers must handle thrown exceptions.

Fixes a regression introduced in 9b61be3

server/src/main/java/io/crate/rest/action/RestResultSetReceiver.java
@@ -22,6 +22,7 @@
 package io.crate.rest.action;
 
 import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
@@ -68,9 +69,8 @@ class RestResultSetReceiver implements ResultReceiver<XContentBuilder> {
             builder.addRow(row, outputFields.size());
             rowCount++;
             return null;
-        } catch (Exception e) {
-            fail(e);
-            return null;
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
         }
     }
 

server/src/test/java/io/crate/rest/action/RestActionReceiversTest.java
@@ -23,12 +23,14 @@ package io.crate.rest.action;
 
 import static io.crate.data.breaker.BlockBasedRamAccounting.MAX_BLOCK_SIZE_IN_BYTES;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 
 import org.elasticsearch.common.Strings;
+import org.elasticsearch.common.breaker.CircuitBreakingException;
 import org.elasticsearch.common.breaker.TestCircuitBreaker;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.common.xcontent.json.JsonXContent;
@@ -152,7 +154,7 @@ public class RestActionReceiversTest extends ESTestCase {
     }
 
     @Test
-    public void test_result_reciever_future_completed_on_cbe() throws Exception {
+    public void test_result_reciever_future_is_not_completed_on_cbe() throws Exception {
         TestCircuitBreaker breaker = new TestCircuitBreaker();
         breaker.startBreaking();
         RamAccounting ramAccounting = new BlockBasedRamAccounting(
@@ -171,9 +173,10 @@ public class RestActionReceiversTest extends ESTestCase {
             false
         );
 
-        // Fails with CBE, resultReceiver's future must be completed, so that sys.jobs entry is cleared.
-        resultReceiver.setNextRow(rows.get(0));
-        assertThat(resultReceiver.completionFuture().isDone()).isTrue();
-        assertThat(resultReceiver.completionFuture().isCompletedExceptionally()).isTrue();
+        // Fails with CBE, resultReceiver's future must not be completed,
+        // it's handled by the consumer/response emitter which also closes iterator/clears sys.jobs entry
+        assertThatThrownBy(() -> resultReceiver.setNextRow(rows.get(0)))
+            .isExactlyInstanceOf(CircuitBreakingException.class);
+        assertThat(resultReceiver.completionFuture().isDone()).isFalse();
     }
 }