0ce13db97eee63f04f07a9823e956a451ed7b9af
baur <baurzhansahariev@gmail.com>
2025-06-25 10:47:30 +0200

    Complete IncrementalPageBucketReceiver's future when processingRows throws CBE

--- server/src/test/java/io/crate/execution/IncrementalPageBucketReceiverTest.java
+++ server/src/test/java/io/crate/execution/IncrementalPageBucketReceiverTest.java
@@ -23,7 +23,15 @@ package io.crate.execution;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
@@ -31,9 +39,12 @@ import org.elasticsearch.common.breaker.CircuitBreakingException;
 import org.junit.Test;
 
 import io.crate.Streamer;
+import io.crate.data.ArrayBucket;
 import io.crate.data.Bucket;
 import io.crate.data.Row;
 import io.crate.data.testing.TestingRowConsumer;
+import io.crate.execution.engine.distribution.DistributedResultResponse;
+import io.crate.execution.jobs.PageResultListener;
 
 public class IncrementalPageBucketReceiverTest {
 
@@ -53,5 +64,76 @@ public class IncrementalPageBucketReceiverTest {
         pageBucketReceiver.setBucket(0, Bucket.EMPTY, true, _ -> {});
         assertThat(pageBucketReceiver.completionFuture()).completesExceptionallyWithin(1, TimeUnit.SECONDS);
         assertThat(batchConsumer.completionFuture()).completesExceptionallyWithin(1, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void test_listener_doesnt_need_more_when_processRows_throws() {
+        TestingRowConsumer batchConsumer = new TestingRowConsumer();
+        Collector<Row, Object, Iterable<Row>> collector = new Collector<>() {
+            @Override
+            public Supplier<Object> supplier() {
+                return ArrayList::new;
+            }
+
+            @Override
+            public BiConsumer<Object, Row> accumulator() {
+                return (_, _) -> {
+                    throw new CircuitBreakingException("dummy");
+                };
+            }
+
+            @Override
+            public BinaryOperator<Object> combiner() {
+                return null;
+            }
+
+            @Override
+            public Function<Object, Iterable<Row>> finisher() {
+                return null;
+            }
+
+            @Override
+            public Set<Characteristics> characteristics() {
+                return Set.of();
+            }
+        };
+
+        var pageBucketReceiver = new IncrementalPageBucketReceiver<>(
+            collector,
+            batchConsumer,
+            Runnable::run,
+            new Streamer[1],
+            1
+        );
+
+        // First call goes to currentlyAccumulating == null, needMore must be true after the call
+        final CompletableFuture<DistributedResultResponse> result = new CompletableFuture<>();
+        PageResultListener listener = needMore -> result.complete(new DistributedResultResponse(needMore));
+        pageBucketReceiver.setBucket(0, Bucket.EMPTY, false, listener);
+        assertThat(result).isCompletedWithValueMatchingWithin(
+            distributedResultResponse -> distributedResultResponse.needMore() == true,
+            Duration.ofSeconds(1)
+        );
+
+        // Second call goes to currentlyAccumulating != null, use non-empty bucket to provoke CBE
+        Bucket bucket = new ArrayBucket(new Object[][]{
+            new Object[]{1},
+        });
+        final CompletableFuture<DistributedResultResponse> result2 = new CompletableFuture<>();
+        PageResultListener listener2 = needMore -> result2.complete(new DistributedResultResponse(needMore));
+        pageBucketReceiver.setBucket(0, bucket, false, listener2);
+        assertThat(result2).isCompletedWithValueMatchingWithin(
+            distributedResultResponse -> distributedResultResponse.needMore() == true,
+            Duration.ofSeconds(1)
+        );
+
+        // Call after failed processRows, listener must see that previous call was completed exceptionally
+        final CompletableFuture<DistributedResultResponse> result3 = new CompletableFuture<>();
+        PageResultListener listener3 = needMore -> result3.complete(new DistributedResultResponse(needMore));
+        pageBucketReceiver.setBucket(0, Bucket.EMPTY, false, listener3);
+        assertThat(result3).isCompletedWithValueMatchingWithin(
+            distributedResultResponse -> distributedResultResponse.needMore() == false,
+            Duration.ofSeconds(1)
+        );
     }
 }
