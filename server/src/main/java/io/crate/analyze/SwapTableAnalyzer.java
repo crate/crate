cd0319ce17321d3b23fa1b205ba83368018bfa39
jeeminso <jeeminso@gmail.com>
2025-07-02 13:00:58 -0400

    Do not allow SWAP TABLE on LR published tables
    SWAP TABLE is a series of ALTER RENAME operations, so it should be
    classified as Operation.ALTER_TABLE_RENAME instead of Operation.ALTER
    
    (cherry picked from commit 7326dfbf6d8ba8bdcbbd25c20d7ecbba2bc83cc1)
    
    # Conflicts:
    #	docs/appendices/release-notes/5.10.11.rst

--- server/src/main/java/io/crate/analyze/SwapTableAnalyzer.java
+++ server/src/main/java/io/crate/analyze/SwapTableAnalyzer.java
@@ -22,17 +22,20 @@
 package io.crate.analyze;
 
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
 import io.crate.analyze.expressions.ExpressionAnalysisContext;
 import io.crate.analyze.expressions.ExpressionAnalyzer;
 import io.crate.analyze.relations.FieldProvider;
+import io.crate.exceptions.OperationOnInaccessibleRelationException;
 import io.crate.expression.symbol.Literal;
 import io.crate.expression.symbol.Symbol;
 import io.crate.metadata.CoordinatorTxnCtx;
 import io.crate.metadata.NodeContext;
 import io.crate.metadata.Schemas;
 import io.crate.metadata.SearchPath;
+import io.crate.metadata.doc.DocTableInfo;
 import io.crate.metadata.table.Operation;
 import io.crate.role.Role;
 import io.crate.sql.tree.Expression;
@@ -68,10 +71,26 @@ public final class SwapTableAnalyzer {
         }
         SearchPath searchPath = txnCtx.sessionSettings().searchPath();
         Role user = txnCtx.sessionSettings().sessionUser();
-        return new AnalyzedSwapTable(
-            schemas.findRelation(swapTable.source(), Operation.ALTER, user, searchPath),
-            schemas.findRelation(swapTable.target(), Operation.ALTER, user, searchPath),
-            dropSource
-        );
+
+        var source = schemas.findRelation(swapTable.source(), Operation.ALTER_TABLE_RENAME, user, searchPath);
+        if (!(source instanceof DocTableInfo sourceRelation)) {
+            throw new OperationOnInaccessibleRelationException(
+                source.ident(),
+                String.format(
+                    Locale.ENGLISH, "The relation \"%s\" doesn't support or allow %s operations",
+                    source.ident().name(), Operation.ALTER_TABLE_RENAME
+                ));
+        }
+        var target = schemas.findRelation(swapTable.target(), Operation.ALTER_TABLE_RENAME, user, searchPath);
+        if (!(target instanceof DocTableInfo targetRelation)) {
+            throw new OperationOnInaccessibleRelationException(
+                target.ident(),
+                String.format(
+                    Locale.ENGLISH, "The relation \"%s\" doesn't support or allow %s operations",
+                    target.ident().name(), Operation.ALTER_TABLE_RENAME
+                ));
+        }
+
+        return new AnalyzedSwapTable(sourceRelation, targetRelation, dropSource);
     }
 }
