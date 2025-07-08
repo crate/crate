9b177338ffbe71dd3260a0993b42004400673acb
Mathias Fussenegger <f.mathias@zignar.net>
2025-07-08 12:16:46 +0200

    Fix flaky testPrimaryTermIsIncreasedOnReplicaPromotion
    Test failed with seed `D9B7E91DF5B8C37A:524AE1236DE758B` because it can
    happen that the first update completes after the disruption recovery.
    
    The test scenario was like this:
    
    - It simulates a network disruption
    - Executes an update that runs into a client timeout because it targets
      the isolated node. On the server side the statement can continue with
      retries.
    - Waits for the replica to be promoted to primary due to the disruption
    - Executes two more updates against the master node. These succeed
      because they hits the working part of the network partition
    - Disruption is stopped
    - The first update could succeed with a retry, bumping the seqNo to 3
      instead of 2. Before https://github.com/crate/crate/pull/17973 this
      always failed due to a version conflict, but now it is using the
      unassigned seqNo/version for the primary retry and can succeed - which
      is allowed as long as the client receives the correct response in
      proper order.
    
    To account for this case, this changes the test to execute the first
    statement async and checks its completion state throughout.

--- server/src/test/java/io/crate/integrationtests/disruption/seqno/SequenceConsistencyIT.java
+++ server/src/test/java/io/crate/integrationtests/disruption/seqno/SequenceConsistencyIT.java
@@ -22,12 +22,11 @@
 package io.crate.integrationtests.disruption.seqno;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.fail;
 
 import java.util.Set;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 
-import org.elasticsearch.ElasticsearchTimeoutException;
 import org.elasticsearch.action.admin.cluster.state.ClusterStateRequest;
 import org.elasticsearch.cluster.ClusterState;
 import org.elasticsearch.cluster.routing.ShardRouting;
@@ -35,8 +34,15 @@ import org.elasticsearch.test.IntegTestCase;
 import org.elasticsearch.test.disruption.NetworkDisruption;
 import org.junit.Test;
 
+import io.crate.auth.Protocol;
 import io.crate.integrationtests.disruption.discovery.AbstractDisruptionTestCase;
 import io.crate.metadata.IndexName;
+import io.crate.protocols.postgres.ConnectionProperties;
+import io.crate.role.Role;
+import io.crate.session.Session;
+import io.crate.session.Sessions;
+import io.crate.testing.SQLResponse;
+import io.crate.testing.SQLTransportExecutor;
 
 @IntegTestCase.ClusterScope(scope = IntegTestCase.Scope.TEST, numDataNodes = 0, numClientNodes = 0)
 @IntegTestCase.Slow
@@ -93,11 +99,13 @@ public class SequenceConsistencyIT extends AbstractDisruptionTestCase {
         partition.startDisrupting();
 
         String schema = sqlExecutor.getCurrentSchema();
-        try {
-            execute("update registers set value = 'value whilst disrupted' where id = 1", null, isolatedNode);
-            fail("expected timeout exception");
-        } catch (ElasticsearchTimeoutException elasticsearchTimeoutException) {
-        }
+        Sessions sessions = cluster().getInstance(Sessions.class, isolatedNode);
+        ConnectionProperties connectionProperties = new ConnectionProperties(null, null, Protocol.HTTP, null);
+        Session session = sessions.newSession(connectionProperties, schema, Role.CRATE_USER);
+        CompletableFuture<SQLResponse> firstUpdate = SQLTransportExecutor.execute(
+            "update registers set value = 'value whilst disrupted' where id = 1", null, session);
+        firstUpdate.whenComplete((_, _) -> session.close());
+        assertThat(firstUpdate).isNotDone();
 
         String nonIsolatedDataNodeId = firstDataNodeHasPrimary ? secondDataNodeId : firstDataNodeId;
         logger.info("wait for replica on the partition with the master to be promoted to primary");
@@ -115,6 +123,8 @@ public class SequenceConsistencyIT extends AbstractDisruptionTestCase {
                 null,
                 masterNodeName);
 
+        assertThat(firstUpdate).isNotDone();
+
         logger.info("heal disruption");
         partition.stopDisrupting();
 
@@ -122,15 +132,35 @@ public class SequenceConsistencyIT extends AbstractDisruptionTestCase {
         ensureGreen();
 
         execute("refresh table registers");
-        execute("select value, _seq_no, _primary_term from registers where id = 1", null, masterNodeName);
-        String finalValue = (String) response.rows()[0][0];
-        long finalSequenceNumber = (long) response.rows()[0][1];
-        long finalPrimaryTerm = (long) response.rows()[0][2];
 
-        assertThat(finalSequenceNumber)
-            .as("We executed 2 updates on the new primary")
-            .isEqualTo(2L);
-        assertThat(finalPrimaryTerm).as("Primary promotion should've triggered a bump in primary term").isEqualTo(2L);
-        assertThat(finalValue).isEqualTo("value set on master the second time");
+        assertBusy(() -> {
+            execute("select value, _seq_no, _primary_term from registers where id = 1", null, masterNodeName);
+            String finalValue = (String) response.rows()[0][0];
+            long finalSequenceNumber = (long) response.rows()[0][1];
+            long finalPrimaryTerm = (long) response.rows()[0][2];
+
+            assertThat(finalSequenceNumber).satisfiesAnyOf(
+                x -> {
+                    assertThat(firstUpdate).isNotDone();
+                    assertThat(x)
+                        .as("We executed 2 updates on the new primary")
+                        .isEqualTo(2L);
+                    assertThat(finalPrimaryTerm)
+                        .as("Primary promotion should've triggered a bump in primary term")
+                        .isEqualTo(2L);
+                    assertThat(finalValue).isEqualTo("value set on master the second time");
+                },
+                x -> {
+                    assertThat(firstUpdate).isDone();
+                    assertThat(x)
+                        .as("We executed 3 updates on the new primary")
+                        .isEqualTo(3L);
+                    assertThat(finalPrimaryTerm)
+                        .as("Primary promotion should've triggered a bump in primary term")
+                        .isEqualTo(2L);
+                    assertThat(finalValue).isEqualTo("value whilst disrupted");
+                }
+            );
+        });
     }
 }
