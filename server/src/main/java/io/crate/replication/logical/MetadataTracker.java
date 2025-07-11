be6886e5cf10eed62607d14fa6c1ec29ac536a6f
jeeminso <jeeminso@gmail.com>
2025-07-11 11:22:13 -0400

    expr12

--- server/src/main/java/io/crate/replication/logical/MetadataTracker.java
+++ server/src/main/java/io/crate/replication/logical/MetadataTracker.java
@@ -36,6 +36,7 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -229,7 +230,8 @@ public final class MetadataTracker implements Closeable {
                     }
                 }
             }
-        } catch (Exception e) {
+        } catch (InterruptedException e) {
+        } catch (ExecutionException e2) {
         }
         CompletableFuture<Boolean> updatedClusterState = publicationsState.thenCompose(response -> {
             if (response.unknownPublications().containsAll(subscription.publications())) {
