eed33dd6398c027a1e9830736e495fc8127c8e2e
Marios Trivyzas <matriv@gmail.com>
2025-07-14 11:13:59 +0300

    Release 5.10.11

--- server/src/main/java/org/elasticsearch/Version.java
+++ server/src/main/java/org/elasticsearch/Version.java
@@ -238,7 +238,7 @@ public class Version implements Comparable<Version> {
     public static final Version V_5_10_8 = new Version(8_10_08_99, false, org.apache.lucene.util.Version.LUCENE_9_12_0);
     public static final Version V_5_10_9 = new Version(8_10_09_99, false, org.apache.lucene.util.Version.LUCENE_9_12_0);
     public static final Version V_5_10_10 = new Version(8_10_10_99, false, org.apache.lucene.util.Version.LUCENE_9_12_0);
-    public static final Version V_5_10_11 = new Version(8_10_11_99, true, org.apache.lucene.util.Version.LUCENE_9_12_0);
+    public static final Version V_5_10_11 = new Version(8_10_11_99, false, org.apache.lucene.util.Version.LUCENE_9_12_0);
 
     public static final Version CURRENT = V_5_10_11;
 
