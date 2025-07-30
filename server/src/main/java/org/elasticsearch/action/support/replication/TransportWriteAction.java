crate/crate

commit 3b3a480e8f2870ecdee11f5d525728f97f36ff9c

bp: Do not fail replica shard due to primary closure
Backport of the https://github.com/opensearch-project/OpenSearch/commit/1dbb63a8ee04812bd5f5ef5bc4995eac8b090438

Relates to https://github.com/elastic/elasticsearch/issues/101180 and https://github.com/opensearch-project/OpenSearch/issues/16817

server/src/main/java/org/elasticsearch/action/support/replication/TransportWriteAction.java
@@ -23,6 +23,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 
+import org.elasticsearch.index.shard.PrimaryShardClosedException;
 import org.jetbrains.annotations.Nullable;
 
 import org.apache.logging.log4j.message.ParameterizedMessage;
@@ -305,8 +306,20 @@ public abstract class TransportWriteAction<
             if (TransportActions.isShardNotAvailableException(exception) == false) {
                 logger.warn(new ParameterizedMessage("[{}] {}", replica.shardId(), message), exception);
             }
-            shardStateAction.remoteShardFailed(
-                replica.shardId(), replica.allocationId().getId(), primaryTerm, true, message, exception, listener);
+            // If a write action fails due to the closure of the primary shard
+            // then the replicas should not be marked as failed since they are
+            // still up-to-date with the (now closed) primary shard
+            if (exception instanceof PrimaryShardClosedException == false) {
+                shardStateAction.remoteShardFailed(
+                    replica.shardId(),
+                    replica.allocationId().getId(),
+                    primaryTerm,
+                    true,
+                    message,
+                    exception,
+                    listener
+                );
+            }
         }
 
         @Override

server/src/main/java/org/elasticsearch/ElasticsearchException.java
@@ -38,6 +38,7 @@ import org.elasticsearch.common.io.stream.StreamOutput;
 import org.elasticsearch.common.io.stream.Writeable;
 import org.elasticsearch.common.logging.LoggerMessageFormat;
 import org.elasticsearch.index.Index;
+import org.elasticsearch.index.shard.PrimaryShardClosedException;
 import org.elasticsearch.index.shard.ShardId;
 import org.elasticsearch.rest.RestStatus;
 import org.elasticsearch.transport.TcpTransport;
@@ -747,6 +748,12 @@ public class ElasticsearchException extends RuntimeException implements Writeabl
             RoleUnknownException::new,
             181,
             Version.V_5_7_0
+        ),
+        PRIMARY_SHARD_CLOSED_EXCEPTION(
+            PrimaryShardClosedException.class,
+            PrimaryShardClosedException::new,
+            182,
+            Version.V_6_0_0
         );
 
         final Class<? extends ElasticsearchException> exceptionClass;

server/src/main/java/org/elasticsearch/action/support/replication/PendingReplicationActions.java
@@ -25,10 +25,12 @@ import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 import org.elasticsearch.action.support.RetryableAction;
 import org.elasticsearch.common.lease.Releasable;
 import org.elasticsearch.index.shard.IndexShardClosedException;
+import org.elasticsearch.index.shard.PrimaryShardClosedException;
 import org.elasticsearch.index.shard.ReplicationGroup;
 import org.elasticsearch.index.shard.ShardId;
 import org.elasticsearch.threadpool.ThreadPool;
@@ -97,7 +99,7 @@ public class PendingReplicationActions implements Consumer<ReplicationGroup>, Re
             }
         }
 
-        cancelActions(toCancel, "Replica left ReplicationGroup");
+        cancelActions(toCancel, () -> new IndexShardClosedException(shardId, "Replica left ReplicationGroup"));
     }
 
     @Override
@@ -105,12 +107,13 @@ public class PendingReplicationActions implements Consumer<ReplicationGroup>, Re
         ArrayList<Set<RetryableAction<?>>> toCancel = new ArrayList<>(onGoingReplicationActions.values());
         onGoingReplicationActions.clear();
 
-        cancelActions(toCancel, "Primary closed.");
+        cancelActions(toCancel, () -> new PrimaryShardClosedException(shardId));
     }
 
-    private void cancelActions(ArrayList<Set<RetryableAction<?>>> toCancel, String message) {
-        threadPool.executor(ThreadPool.Names.GENERIC).execute(() -> toCancel.stream()
+    private void cancelActions(ArrayList<Set<RetryableAction<?>>> toCancel, Supplier<IndexShardClosedException> exceptionSupplier) {
+        threadPool.executor(ThreadPool.Names.GENERIC)
+            .execute(() -> toCancel.stream()
             .flatMap(Collection::stream)
-            .forEach(action -> action.cancel(new IndexShardClosedException(shardId, message))));
+            .forEach(action -> action.cancel(exceptionSupplier.get())));
     }
 }

server/src/main/java/org/elasticsearch/index/shard/PrimaryShardClosedException.java
@@ -1,0 +1,36 @@
+/*
+ * Licensed to Crate.io GmbH ("Crate") under one or more contributor
+ * license agreements.  See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.  Crate licenses
+ * this file to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.  You may
+ * obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * However, if you have executed another commercial license agreement
+ * with Crate these terms will supersede the license and you may use the
+ * software solely pursuant to the terms of the relevant commercial agreement.
+ */
+
+package org.elasticsearch.index.shard;
+
+import java.io.IOException;
+
+import org.elasticsearch.common.io.stream.StreamInput;
+
+public class PrimaryShardClosedException extends IndexShardClosedException {
+    public PrimaryShardClosedException(ShardId shardId) {
+        super(shardId, "Primary closed");
+    }
+
+    public PrimaryShardClosedException(StreamInput in) throws IOException {
+        super(in);
+    }
+}

server/src/test/java/org/elasticsearch/action/support/replication/PendingReplicationActionsTests.java
@@ -28,6 +28,7 @@ import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.action.support.RetryableAction;
 import org.elasticsearch.common.UUIDs;
 import org.elasticsearch.index.shard.IndexShardClosedException;
+import org.elasticsearch.index.shard.PrimaryShardClosedException;
 import org.elasticsearch.index.shard.ShardId;
 import org.elasticsearch.test.ESTestCase;
 import org.elasticsearch.threadpool.TestThreadPool;
@@ -98,7 +99,7 @@ public class PendingReplicationActionsTests extends ESTestCase {
         pendingReplication.addPendingAction(allocationId, action);
         action.run();
         pendingReplication.close();
-        assertThatThrownBy(future::get).hasCauseExactlyInstanceOf(IndexShardClosedException.class);
+        assertThatThrownBy(future::get).hasCauseExactlyInstanceOf(PrimaryShardClosedException.class);
     }
 
     private class TestAction extends RetryableAction<Void> {

commit 62156ee50058a9ba994d39e5fed29f6cc1e482f2

Fix INSERT INTO getting stuck due to PrimaryShardClosedException
Fixes a regression introduced by https://github.com/crate/crate/pull/17879
The `listener` was never called, leading to a stuck replica operation.

`ClusterDisruptionIT.testAckedIndexing` was flaky because of this and
failed with the number of inflight requests being 1 instead of 0.

server/src/main/java/org/elasticsearch/action/support/replication/TransportWriteAction.java
@@ -33,7 +33,6 @@ import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.index.engine.Engine;
 import org.elasticsearch.index.mapper.MapperParsingException;
 import org.elasticsearch.index.shard.IndexShard;
-import org.elasticsearch.index.shard.PrimaryShardClosedException;
 import org.elasticsearch.index.shard.ShardId;
 import org.elasticsearch.index.translog.Translog;
 import org.elasticsearch.index.translog.Translog.Location;
@@ -244,20 +243,15 @@ public abstract class TransportWriteAction<
             if (SQLExceptions.isShardNotAvailable(exception) == false) {
                 logger.warn(new ParameterizedMessage("[{}] {}", replica.shardId(), message), exception);
             }
-            // If a write action fails due to the closure of the primary shard
-            // then the replicas should not be marked as failed since they are
-            // still up-to-date with the (now closed) primary shard
-            if (exception instanceof PrimaryShardClosedException == false) {
-                shardStateAction.remoteShardFailed(
-                    replica.shardId(),
-                    replica.allocationId().getId(),
-                    primaryTerm,
-                    true,
-                    message,
-                    exception,
-                    listener
-                );
-            }
+            shardStateAction.remoteShardFailed(
+                replica.shardId(),
+                replica.allocationId().getId(),
+                primaryTerm,
+                true,
+                message,
+                exception,
+                listener
+            );
         }
 
         @Override