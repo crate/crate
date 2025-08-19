crate/crate

commit 40a432bf84dc786964c694270425009c343d35e8

Fix request mutation interfering with primary operation retry logic
This is a follow up to https://github.com/crate/crate/pull/17973

Primary retries can happen in two places:

- Triggered from within the primary operation (e.g. shard not found, due
  to a race where it is being moved)
- Triggered due to the replica operation failing

Both scenarios were problematic with the mutations happening on the
primary request. Items could have had their seqNo changed to
SKIP_ON_REPLICA and insertColumns/insertItems could also change, making
a retry unsafe.

To solve this, this introduces a dedicated replica request which will
only contain items actually inserted on the primary. The original
request is no longer mutated to ensure it can be reused for a retry.

As a side effect of this change, it also fixes another issue where a
statement like:

    INSERT INTO tbl (id, value) VALUES (?, ?)
    ON CONFLICT (id) DO UPDATE SET value = excluded.value

With a table that has the columns:

- id
- value
- other
- rnd_col (being undeterministic generated using something like `random()`)

This could lead to serialization errors if used in a bulk operation where
some items ran into a conflict and others did not.
The update items had insertValues for `[id, value, other, rnd_col]`, while
the non-conflict items had insertValues for `[id, value, rnd_col]`, the
third position having a mix of string/int caused the streaming errors.

server/src/main/java/io/crate/execution/dml/delete/TransportShardDeleteAction.java
@@ -35,8 +35,10 @@ import org.elasticsearch.index.engine.Engine;
 import org.elasticsearch.index.engine.VersionConflictEngineException;
 import org.elasticsearch.index.seqno.SequenceNumbers;
 import org.elasticsearch.index.shard.IndexShard;
+import org.elasticsearch.index.shard.ShardId;
 import org.elasticsearch.index.translog.Translog;
 import org.elasticsearch.indices.IndicesService;
+import org.elasticsearch.indices.breaker.CircuitBreakerService;
 import org.elasticsearch.threadpool.ThreadPool;
 import org.elasticsearch.transport.TransportService;
 
@@ -44,10 +46,15 @@ import io.crate.common.exceptions.Exceptions;
 import io.crate.exceptions.JobKilledException;
 import io.crate.execution.dml.ShardResponse;
 import io.crate.execution.dml.TransportShardAction;
+import io.crate.execution.dml.delete.ShardDeleteRequest.Item;
 import io.crate.execution.jobs.TasksService;
 
 @Singleton
-public class TransportShardDeleteAction extends TransportShardAction<ShardDeleteRequest, ShardDeleteRequest.Item> {
+public class TransportShardDeleteAction extends TransportShardAction<
+        ShardDeleteRequest,
+        ShardDeleteRequest,
+        ShardDeleteRequest.Item,
+        ShardDeleteRequest.Item> {
 
     @Inject
     public TransportShardDeleteAction(Settings settings,
@@ -56,7 +63,8 @@ public class TransportShardDeleteAction extends TransportShardAction<ShardDelete
                                       IndicesService indicesService,
                                       TasksService tasksService,
                                       ThreadPool threadPool,
-                                      ShardStateAction shardStateAction) {
+                                      ShardStateAction shardStateAction,
+                                      CircuitBreakerService circuitBreakerService) {
         super(
             settings,
             ShardDeleteAction.NAME,
@@ -66,6 +74,8 @@ public class TransportShardDeleteAction extends TransportShardAction<ShardDelete
             tasksService,
             threadPool,
             shardStateAction,
+            circuitBreakerService,
+            ShardDeleteRequest::new,
             ShardDeleteRequest::new
         );
     }
@@ -77,39 +87,45 @@ public class TransportShardDeleteAction extends TransportShardAction<ShardDelete
         ShardResponse shardResponse = new ShardResponse();
         Translog.Location translogLocation = null;
         boolean debugEnabled = logger.isDebugEnabled();
+        ShardDeleteRequest replicaRequest = new ShardDeleteRequest(request.shardId(), request.jobId());
         for (ShardDeleteRequest.Item item : request.items()) {
             int location = item.location();
             if (killed.get()) {
                 // set failure on response, mark current item and skip all next items.
                 // this way replica operation will be executed, but only items already processed here
                 // will be processed on the replica
-                request.skipFromLocation(location);
+                replicaRequest.skipFromLocation(location);
                 shardResponse.failure(new InterruptedException(JobKilledException.MESSAGE));
                 break;
             }
             try {
-                Engine.DeleteResult deleteResult = shardDeleteOperationOnPrimary(item, indexShard);
+                Engine.DeleteResult deleteResult = indexShard.applyDeleteOperationOnPrimary(
+                    item.version(),
+                    item.id(),
+                    VersionType.INTERNAL,
+                    item.seqNo(),
+                    item.primaryTerm()
+                );
                 translogLocation = deleteResult.getTranslogLocation();
                 Exception failure = deleteResult.getFailure();
+                if (debugEnabled) {
+                    logResult("primary", request.shardId(), item.id(), deleteResult);
+                }
                 if (failure == null) {
                     if (deleteResult.isFound()) {
-                        if (debugEnabled) {
-                            logger.debug("shardId={} successfully deleted id={}", request.shardId(), item.id());
-                        }
                         shardResponse.add(location);
+                        Item resultItem = new Item(
+                            item.id(),
+                            deleteResult.getSeqNo(),
+                            deleteResult.getTerm(),
+                            deleteResult.getVersion()
+                        );
+                        replicaRequest.add(location, resultItem);
                     } else {
-                        if (debugEnabled) {
-                            logger.debug("shardId={} failed to execute delete for id={}, doc not found",
-                                request.shardId(), item.id());
-                        }
-                        Throwable ex = new DocumentMissingException(indexShard.shardId(), item.id());
+                        var ex = new DocumentMissingException(indexShard.shardId(), item.id());
                         shardResponse.add(location, item.id(), ex, false);
                     }
                 } else {
-                    if (debugEnabled) {
-                        logger.debug("shardId={} failed to execute delete for id={}: {}",
-                            request.shardId(), item.id(), failure);
-                    }
                     shardResponse.add(
                         location,
                         item.id(),
@@ -134,13 +150,28 @@ public class TransportShardDeleteAction extends TransportShardAction<ShardDelete
                 }
             }
         }
+        return new WritePrimaryResult<>(replicaRequest, shardResponse, translogLocation, indexShard);
+    }
 
-        return new WritePrimaryResult<>(request, shardResponse, translogLocation, indexShard);
+    private void logResult(String origin,
+                           ShardId shardId,
+                           String id,
+                           Engine.DeleteResult deleteResult) {
+        logger.debug(
+            "shardId={} delete {} op id={} failure={} seqNo={} found={}",
+            shardId,
+            origin,
+            id,
+            deleteResult.getFailure(),
+            deleteResult.getSeqNo(),
+            deleteResult.isFound()
+        );
     }
 
     @Override
     protected WriteReplicaResult processRequestItemsOnReplica(IndexShard indexShard, ShardDeleteRequest request) throws IOException {
         Translog.Location translogLocation = null;
+        boolean traceEnabled = logger.isTraceEnabled();
         for (ShardDeleteRequest.Item item : request.items()) {
             int location = item.location();
             if (request.skipFromLocation() == location) {
@@ -158,25 +189,12 @@ public class TransportShardDeleteAction extends TransportShardAction<ShardDelete
                     item.version(),
                     item.id()
                 );
-
                 translogLocation = deleteResult.getTranslogLocation();
-                if (logger.isTraceEnabled()) {
-                    logger.trace("shardId={} REPLICA: successfully deleted id={}", request.shardId(), item.id());
+                if (traceEnabled) {
+                    logResult("replica", request.shardId(), item.id(), deleteResult);
                 }
             }
         }
         return new WriteReplicaResult(translogLocation, indexShard);
-    }
-
-    private Engine.DeleteResult shardDeleteOperationOnPrimary(ShardDeleteRequest.Item item, IndexShard indexShard) throws IOException {
-        Engine.DeleteResult deleteResult = indexShard.applyDeleteOperationOnPrimary(
-            item.version(), item.id(), VersionType.INTERNAL, item.seqNo(), item.primaryTerm());
-
-        // set version and sequence number for replica
-        item.version(deleteResult.getVersion());
-        item.seqNo(deleteResult.getSeqNo());
-        item.primaryTerm(deleteResult.getTerm());
-
-        return deleteResult;
     }
 }

server/src/main/java/io/crate/execution/dml/Indexer.java
@@ -743,7 +743,12 @@ public class Indexer {
         }
 
         TranslogWriter translogWriter = new XContentTranslogWriter();
-        IndexDocumentBuilder docBuilder = new IndexDocumentBuilder(translogWriter, synthetics::get, columnConstraints, tableVersionCreated);
+        IndexDocumentBuilder docBuilder = new IndexDocumentBuilder(
+            translogWriter,
+            synthetics::get,
+            columnConstraints,
+            tableVersionCreated
+        );
         Object[] values = item.insertValues();
 
         for (int i = 0; i < values.length; i++) {
@@ -887,15 +892,11 @@ public class Indexer {
         };
     }
 
-    public boolean hasUndeterministicSynthetics() {
-        return !undeterministic.isEmpty();
-    }
-
     public List<Reference> columns() {
         return columns;
     }
 
-    public Collection<Reference> insertColumns(List<Reference> columns) {
+    public List<Reference> insertColumns() {
         if (undeterministic.isEmpty()) {
             return columns;
         }
@@ -921,9 +922,11 @@ public class Indexer {
         return newColumns;
     }
 
-    @SuppressWarnings("unchecked")
     public Object[] addGeneratedValues(IndexItem item) {
         Object[] insertValues = item.insertValues();
+        if (undeterministic.isEmpty()) {
+            return insertValues;
+        }
         //  We don't know in advance how many values we will add: we can have multiple generated sub-columns.
         //  Some of them can have their root listed in the insert/upsert targets (and thus not causing array expansion) and some not.
         List<Object> extendedValues = new ArrayList<>(insertValues.length);
@@ -960,5 +963,9 @@ public class Indexer {
             }
         }
         return extendedValues.toArray();
+    }
+
+    public boolean hasReturnValues() {
+        return returnValueInputs != null;
     }
 }

server/src/main/java/io/crate/execution/dml/RawIndexer.java
@@ -35,7 +35,6 @@ import org.elasticsearch.index.mapper.ParsedDocument;
 import org.jetbrains.annotations.NotNull;
 
 import io.crate.exceptions.ConversionException;
-import io.crate.execution.dml.upsert.ShardUpsertRequest;
 import io.crate.expression.symbol.Symbol;
 import io.crate.metadata.ColumnIdent;
 import io.crate.metadata.NodeContext;
@@ -163,11 +162,7 @@ public class RawIndexer {
         return currentRowIndexer.index(currentItem);
     }
 
-    public boolean hasUndeterministicSynthetics() {
-        return currentRowIndexer.hasUndeterministicSynthetics();
-    }
-
-    public Object[] addGeneratedValues(ShardUpsertRequest.Item item) {
+    public Object[] addGeneratedValues(IndexItem item) {
         return currentRowIndexer.addGeneratedValues(item);
     }
 }

server/src/main/java/io/crate/execution/dml/ShardRequest.java
@@ -147,6 +147,14 @@ public abstract class ShardRequest<T extends ShardRequest<T, I>, I extends Shard
             primaryTerm = in.readLong();
         }
 
+        public void writeTo(StreamOutput out) throws IOException {
+            out.writeString(id);
+            out.writeLong(version);
+            out.writeInt(location);
+            out.writeLong(seqNo);
+            out.writeLong(primaryTerm);
+        }
+
         @Override
         public long ramBytesUsed() {
             return RamUsageEstimator.sizeOf(id)
@@ -164,10 +172,6 @@ public abstract class ShardRequest<T extends ShardRequest<T, I>, I extends Shard
             return version;
         }
 
-        public void version(long version) {
-            this.version = version;
-        }
-
         public void location(int location) {
             this.location = location;
         }
@@ -180,24 +184,8 @@ public abstract class ShardRequest<T extends ShardRequest<T, I>, I extends Shard
             return seqNo;
         }
 
-        public void seqNo(long seqNo) {
-            this.seqNo = seqNo;
-        }
-
         public long primaryTerm() {
             return primaryTerm;
-        }
-
-        public void primaryTerm(long primaryTerm) {
-            this.primaryTerm = primaryTerm;
-        }
-
-        public void writeTo(StreamOutput out) throws IOException {
-            out.writeString(id);
-            out.writeLong(version);
-            out.writeInt(location);
-            out.writeLong(seqNo);
-            out.writeLong(primaryTerm);
         }
 
         @Override

server/src/main/java/io/crate/execution/dml/TransportShardAction.java
@@ -32,11 +32,14 @@ import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.action.support.replication.TransportWriteAction;
 import org.elasticsearch.cluster.action.shard.ShardStateAction;
 import org.elasticsearch.cluster.service.ClusterService;
+import org.elasticsearch.common.breaker.CircuitBreaker;
 import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.io.stream.Writeable;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.index.shard.IndexShard;
 import org.elasticsearch.indices.IndicesService;
+import org.elasticsearch.indices.breaker.CircuitBreakerService;
+import org.elasticsearch.indices.breaker.HierarchyCircuitBreakerService;
 import org.elasticsearch.tasks.TaskId;
 import org.elasticsearch.threadpool.ThreadPool;
 import org.elasticsearch.transport.TransportService;
@@ -51,12 +54,17 @@ import io.crate.execution.jobs.kill.KillableCallable;
  * Base class for performing Crate-specific TransportWriteActions like Delete or Upsert.
  * @param <Request> The ShardRequest implementation including a replicated write request
  */
-public abstract class TransportShardAction<Request extends ShardRequest<Request, Item>, Item extends ShardRequest.Item>
-        extends TransportWriteAction<Request, Request, ShardResponse>
+public abstract class TransportShardAction<
+            Request extends ShardRequest<Request, Item>,
+            ReplicaReq extends ShardRequest<ReplicaReq, ReplicaItem>,
+            Item extends ShardRequest.Item,
+            ReplicaItem extends ShardRequest.Item>
+        extends TransportWriteAction<Request, ReplicaReq, ShardResponse>
         implements KillAllListener {
 
     private final ConcurrentHashMap<TaskId, KillableCallable<?>> activeOperations = new ConcurrentHashMap<>();
     private final TasksService tasksService;
+    private final CircuitBreakerService circuitBreakerService;
 
     protected TransportShardAction(Settings settings,
                                    String actionName,
@@ -66,7 +74,9 @@ public abstract class TransportShardAction<Request extends ShardRequest<Request,
                                    TasksService tasksService,
                                    ThreadPool threadPool,
                                    ShardStateAction shardStateAction,
-                                   Writeable.Reader<Request> reader) {
+                                   CircuitBreakerService circuitBreakerService,
+                                   Writeable.Reader<Request> reader,
+                                   Writeable.Reader<ReplicaReq> replicaReader) {
         super(
             settings,
             actionName,
@@ -76,10 +86,11 @@ public abstract class TransportShardAction<Request extends ShardRequest<Request,
             threadPool,
             shardStateAction,
             reader,
-            reader,
+            replicaReader,
             ThreadPool.Names.WRITE,
             false
         );
+        this.circuitBreakerService = circuitBreakerService;
         this.tasksService = tasksService;
     }
 
@@ -88,30 +99,31 @@ public abstract class TransportShardAction<Request extends ShardRequest<Request,
         return new ShardResponse(in);
     }
 
+
     @Override
     protected void shardOperationOnPrimary(Request request,
                                            IndexShard primary,
-                                           ActionListener<PrimaryResult<Request, ShardResponse>> listener) {
+                                           ActionListener<PrimaryResult<ReplicaReq, ShardResponse>> listener) {
         if (tasksService.recentlyFailed(request.jobId())) {
             listener.onFailure(JobKilledException.of(JobKilledException.MESSAGE));
             return;
         }
         try {
-            KillableCallable<WritePrimaryResult<Request, ShardResponse>> callable = new KillableCallable<>(request.jobId()) {
+            KillableCallable<WritePrimaryResult<ReplicaReq, ShardResponse>> callable = new KillableCallable<>(request.jobId()) {
 
                 @Override
-                public WritePrimaryResult<Request, ShardResponse> call() throws Exception {
+                public WritePrimaryResult<ReplicaReq, ShardResponse> call() throws Exception {
                     return processRequestItems(primary, request, killed);
                 }
             };
             listener.onResponse(withActiveOperation(request, callable));
-        } catch (Exception ex) {
-            listener.onFailure(ex);
+        } catch (Throwable t) {
+            listener.onFailure(Exceptions.toRuntimeException(t));
         }
     }
 
     @Override
-    protected WriteReplicaResult shardOperationOnReplica(Request replicaRequest, IndexShard indexShard) {
+    protected WriteReplicaResult shardOperationOnReplica(ReplicaReq replicaRequest, IndexShard indexShard) {
         KillableCallable<WriteReplicaResult> callable = new KillableCallable<>(replicaRequest.jobId()) {
 
             @Override
@@ -122,7 +134,13 @@ public abstract class TransportShardAction<Request extends ShardRequest<Request,
         return withActiveOperation(replicaRequest, callable);
     }
 
-    private <WrapperResponse> WrapperResponse withActiveOperation(Request request, KillableCallable<WrapperResponse> callable) {
+    private <WrapperResponse> WrapperResponse withActiveOperation(ShardRequest<?, ?> request,
+                                                                  KillableCallable<WrapperResponse> callable) {
+        CircuitBreaker breaker = circuitBreakerService.getBreaker(HierarchyCircuitBreakerService.QUERY);
+        // Request is already accounted by the transport layer, but we account extra to account for the replica request copy
+        long ramBytesUsed = request.ramBytesUsed();
+        breaker.addEstimateBytesAndMaybeBreak(ramBytesUsed, "upsert request");
+
         TaskId id = request.getParentTask();
         activeOperations.put(id, callable);
         try {
@@ -131,6 +149,7 @@ public abstract class TransportShardAction<Request extends ShardRequest<Request,
             throw Exceptions.toRuntimeException(t);
         } finally {
             activeOperations.remove(id, callable);
+            breaker.addWithoutBreaking(- ramBytesUsed);
         }
     }
 
@@ -159,7 +178,7 @@ public abstract class TransportShardAction<Request extends ShardRequest<Request,
         }
     }
 
-    protected abstract WritePrimaryResult<Request, ShardResponse> processRequestItems(IndexShard indexShard, Request request, AtomicBoolean killed) throws InterruptedException, IOException;
+    protected abstract WritePrimaryResult<ReplicaReq, ShardResponse> processRequestItems(IndexShard indexShard, Request request, AtomicBoolean killed) throws InterruptedException, IOException;
 
-    protected abstract WriteReplicaResult processRequestItemsOnReplica(IndexShard indexShard, Request replicaRequest) throws IOException;
+    protected abstract WriteReplicaResult processRequestItemsOnReplica(IndexShard indexShard, ReplicaReq replicaRequest) throws IOException;
 }

commit 883f7fce81fd8a5973c105af39f50867d9550b3d

Include items with delete failure in replica request
Regression introduced by https://github.com/crate/crate/pull/18087
Caused flaky test failures with errors like:

    java.lang.AssertionError:
    shard [insert_test/-PvTnzheRtuup9kF8777XA][0] on node [node_sd3] has pending operations:
     --> 0 marking VbGKCMhdSDi9SAU9NFt30g as in sync
    	at org.elasticsearch.index.shard.IndexShardOperationPermits.acquire(IndexShardOperationPermits.java:242)
    	at org.elasticsearch.index.shard.IndexShard.acquirePrimaryOperationPermit(IndexShard.java:2692)
    	at org.elasticsearch.index.shard.IndexShard.acquirePrimaryOperationPermit(IndexShard.java:2684)
    	at org.elasticsearch.indices.recovery.RecoverySourceHandler.lambda$runUnderPrimaryPermit$0(RecoverySourceHandler.java:405)
    	at org.elasticsearch.common.util.CancellableThreads.execute(CancellableThreads.java:97)
    	at org.elasticsearch.indices.recovery.RecoverySourceHandler.runUnderPrimaryPermit(RecoverySourceHandler.java:390)
    	at org.elasticsearch.indices.recovery.RecoverySourceHandler.finalizeRecovery(RecoverySourceHandler.java:873)
    	at org.elasticsearch.indices.recovery.RecoverySourceHandler.lambda$recoverToTarget$15(RecoverySourceHandler.java:361)

No new changes because https://github.com/crate/crate/pull/18236 sort of covers it too

server/src/main/java/io/crate/execution/dml/delete/TransportShardDeleteAction.java
@@ -112,15 +112,15 @@ public class TransportShardDeleteAction extends TransportShardAction<
                     logResult("primary", request.shardId(), item.id(), deleteResult);
                 }
                 if (failure == null) {
+                    Item resultItem = new Item(
+                        item.id(),
+                        deleteResult.getSeqNo(),
+                        deleteResult.getTerm(),
+                        deleteResult.getVersion()
+                    );
+                    replicaRequest.add(location, resultItem);
                     if (deleteResult.isFound()) {
                         shardResponse.add(location);
-                        Item resultItem = new Item(
-                            item.id(),
-                            deleteResult.getSeqNo(),
-                            deleteResult.getTerm(),
-                            deleteResult.getVersion()
-                        );
-                        replicaRequest.add(location, resultItem);
                     } else {
                         var ex = new DocumentMissingException(indexShard.shardId(), item.id());
                         shardResponse.add(location, item.id(), ex, false);

server/src/main/java/org/elasticsearch/index/engine/Engine.java
@@ -433,13 +433,6 @@ public abstract class Engine implements Closeable {
             this.found = found;
         }
 
-        /**
-         * use in case of the delete operation failed before getting to internal engine
-         **/
-        public DeleteResult(Exception failure, long version, long term) {
-            this(failure, version, term, SequenceNumbers.UNASSIGNED_SEQ_NO, false);
-        }
-
         public DeleteResult(Exception failure, long version, long term, long seqNo, boolean found) {
             super(failure, version, term, seqNo);
             this.found = found;