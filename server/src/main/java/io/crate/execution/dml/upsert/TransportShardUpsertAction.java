1175c822ad898cd4b07f8f2cbfcb3b6df4cfecb5
jeeminso <jeeminso@gmail.com>
2025-09-04 15:31:44 -0400

    Use the latest DoctableInfo.rootColumns as indexing order on primary and replica

--- server/src/main/java/io/crate/execution/dml/upsert/TransportShardUpsertAction.java
+++ server/src/main/java/io/crate/execution/dml/upsert/TransportShardUpsertAction.java
@@ -273,6 +273,7 @@ public class TransportShardUpsertAction extends TransportShardAction<
 
     @Override
     protected WriteReplicaResult processRequestItemsOnReplica(IndexShard indexShard, UpsertReplicaRequest request) throws IOException {
+        System.out.println("on replica");
         List<Reference> columns = request.columns();
         Translog.Location location = null;
         String indexUUID = indexShard.shardId().getIndexUUID();
