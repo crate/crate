9b1022a067fe3c1946bc7098f477844bfb046cd0
Mathias Fussenegger <f.mathias@zignar.net>
2025-09-08 09:00:08 +0200

    Remove unused test-only settings

--- server/src/testFixtures/java/org/elasticsearch/test/InternalSettingsPlugin.java
+++ server/src/testFixtures/java/org/elasticsearch/test/InternalSettingsPlugin.java
@@ -22,10 +22,8 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-import org.elasticsearch.cluster.metadata.IndexMetadata;
 import org.elasticsearch.common.settings.Setting;
 import org.elasticsearch.common.settings.Setting.Property;
-import org.elasticsearch.index.IndexModule;
 import org.elasticsearch.index.IndexService;
 import org.elasticsearch.index.IndexSettings;
 import org.elasticsearch.monitor.fs.FsService;
@@ -35,25 +33,15 @@ import io.crate.common.unit.TimeValue;
 
 public final class InternalSettingsPlugin extends Plugin {
 
-    public static final Setting<Boolean> MERGE_ENABLED =
-        Setting.boolSetting("index.merge.enabled", true, Property.IndexScope, Property.NodeScope);
-    public static final Setting<Long> INDEX_CREATION_DATE_SETTING =
-        Setting.longSetting(IndexMetadata.SETTING_CREATION_DATE, -1, -1, Property.IndexScope, Property.NodeScope);
     public static final Setting<TimeValue> TRANSLOG_RETENTION_CHECK_INTERVAL_SETTING =
         Setting.timeSetting("index.translog.retention.check_interval", new TimeValue(10, TimeUnit.MINUTES),
             new TimeValue(-1, TimeUnit.MILLISECONDS), Property.Dynamic, Property.IndexScope);
-    public static final Setting<String> INDEX_UUID =
-        Setting.simpleString("index.uuid", Property.IndexScope, Property.NodeScope);
 
     @Override
     public List<Setting<?>> getSettings() {
         return Arrays.asList(
-                MERGE_ENABLED,
-                INDEX_CREATION_DATE_SETTING,
                 TRANSLOG_RETENTION_CHECK_INTERVAL_SETTING,
                 IndexService.GLOBAL_CHECKPOINT_SYNC_INTERVAL_SETTING,
-                IndexModule.INDEX_QUERY_CACHE_EVERYTHING_SETTING,
-                INDEX_UUID,
                 IndexSettings.INDEX_TRANSLOG_RETENTION_TOTAL_FILES_SETTING,
                 IndexSettings.FILE_BASED_RECOVERY_THRESHOLD_SETTING,
                 FsService.ALWAYS_REFRESH_SETTING
