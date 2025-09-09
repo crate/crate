9607550ffcfb817e928c40013dafc3fde9790a16
Mathias Fussenegger <f.mathias@zignar.net>
2025-09-09 08:51:16 +0200

    Move SysSnapshot factory methods into SysSnapshot

--- server/src/main/java/io/crate/expression/reference/sys/snapshot/SysSnapshots.java
+++ server/src/main/java/io/crate/expression/reference/sys/snapshot/SysSnapshots.java
@@ -23,29 +23,22 @@ package io.crate.expression.reference.sys.snapshot;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.function.Supplier;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.elasticsearch.Version;
+import org.elasticsearch.cluster.ClusterState;
 import org.elasticsearch.cluster.SnapshotsInProgress;
-import org.elasticsearch.cluster.metadata.RelationMetadata;
 import org.elasticsearch.cluster.service.ClusterService;
 import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.common.inject.Singleton;
-import org.elasticsearch.repositories.IndexId;
 import org.elasticsearch.repositories.RepositoriesService;
 import org.elasticsearch.repositories.Repository;
 import org.elasticsearch.snapshots.SnapshotException;
 import org.elasticsearch.snapshots.SnapshotId;
-import org.elasticsearch.snapshots.SnapshotInfo;
-import org.elasticsearch.snapshots.SnapshotShardFailure;
-import org.elasticsearch.snapshots.SnapshotState;
 import org.elasticsearch.snapshots.SnapshotsService;
-import org.jetbrains.annotations.Nullable;
 import org.jetbrains.annotations.VisibleForTesting;
 
 import io.crate.common.collections.Lists;
@@ -73,21 +66,24 @@ public class SysSnapshots {
 
     public CompletableFuture<Iterable<SysSnapshot>> currentSnapshots() {
         ArrayList<CompletableFuture<List<SysSnapshot>>> sysSnapshots = new ArrayList<>();
+        ClusterState state = clusterService.state();
         for (Repository repository : getRepositories.get()) {
-            var futureSnapshots = repository.getRepositoryData()
-                .thenCompose(repositoryData -> {
-                    Collection<SnapshotId> snapshotIds = repositoryData.getSnapshotIds();
-                    ArrayList<CompletableFuture<SysSnapshot>> snapshots = new ArrayList<>(snapshotIds.size());
-                    for (SnapshotId snapshotId : snapshotIds) {
-                        snapshots.add(createSysSnapshot(repository, snapshotId));
-                    }
-                    return CompletableFutures.allSuccessfulAsList(snapshots);
-                });
+            var futureSnapshots = repository.getRepositoryData().thenCompose(repositoryData -> {
+                Collection<SnapshotId> snapshotIds = repositoryData.getSnapshotIds();
+                ArrayList<CompletableFuture<SysSnapshot>> snapshots = new ArrayList<>(snapshotIds.size());
+                for (SnapshotId snapshotId : snapshotIds) {
+                    snapshots.add(createSysSnapshot(repository, snapshotId));
+                }
+                return CompletableFutures.allSuccessfulAsList(snapshots);
+            });
             sysSnapshots.add(futureSnapshots);
 
-            // Add snapshots in progress in this repository.
-            final SnapshotsInProgress snapshotsInProgress = clusterService.state().custom(SnapshotsInProgress.TYPE);
-            List<SysSnapshot> inProgressSnapshots = snapshotsInProgress(snapshotsInProgress, repository.getMetadata().name());
+
+            final SnapshotsInProgress snapshotsInProgress = state.custom(SnapshotsInProgress.TYPE);
+            List<SysSnapshot> inProgressSnapshots = Lists.map(
+                SnapshotsService.currentSnapshots(snapshotsInProgress, repository.getMetadata().name(), List.of()),
+                SysSnapshot::of
+            );
             sysSnapshots.add(CompletableFuture.completedFuture(inProgressSnapshots));
         }
         return CompletableFutures.allSuccessfulAsList(sysSnapshots).thenApply(data -> {
@@ -99,104 +95,19 @@ public class SysSnapshots {
         });
     }
 
-    private static SysSnapshot toSysSnapshot(Repository repository,
-                                             SnapshotId snapshotId,
-                                             SnapshotInfo snapshotInfo,
-                                             List<String> concreteIndices,
-                                             List<String> partedTables) {
-        Version version = snapshotInfo.version();
-        return new SysSnapshot(
-            snapshotId.getUUID(),
-            snapshotId.getName(),
-            repository.getMetadata().name(),
-            concreteIndices,
-            partedTables,
-            snapshotInfo.startTime(),
-            snapshotInfo.endTime(),
-            version == null ? null : version.toString(),
-            snapshotInfo.state().name(),
-            Lists.map(snapshotInfo.shardFailures(), SnapshotShardFailure::toString),
-            snapshotInfo.reason(),
-            snapshotInfo.totalShards(),
-            snapshotInfo.includeGlobalState()
-        );
-    }
-
-    private static SysSnapshot toSysSnapshot(String repositoryName,
-                                             SnapshotsInProgress.Entry entry,
-                                             List<String> partedTables) {
-        return new SysSnapshot(
-            entry.snapshot().getSnapshotId().getUUID(),
-            entry.snapshot().getSnapshotId().getName(),
-            repositoryName,
-            entry.indices().stream().map(IndexId::getName).toList(),
-            partedTables,
-            entry.startTime(),
-            0L,
-            Version.CURRENT.toString(),
-            SnapshotState.IN_PROGRESS.name(),
-            Collections.emptyList(),
-            entry.failure(),
-            entry.shards().size(),
-            entry.includeGlobalState()
-        );
-    }
-
     private static CompletableFuture<SysSnapshot> createSysSnapshot(Repository repository, SnapshotId snapshotId) {
-        return repository.getSnapshotGlobalMetadata(snapshotId).thenCombine(
-            repository.getSnapshotInfo(snapshotId),
-            (metadata, snapshotInfo) -> {
-                List<String> partedTables = metadata.relations(RelationMetadata.Table.class).stream()
-                    .filter(t -> !t.partitionedBy().isEmpty())
-                    .map(t -> t.name().fqn())
-                    .toList();
-                List<String> concreteIndices = snapshotInfo.indices();
-                return SysSnapshots.toSysSnapshot(repository, snapshotId, snapshotInfo, concreteIndices, partedTables);
-            }).exceptionally(t -> {
+        String repoName = repository.getMetadata().name();
+        return repository.getSnapshotGlobalMetadata(snapshotId)
+            .thenCombine(repository.getSnapshotInfo(snapshotId), (metadata, info) -> SysSnapshot.of(metadata, repoName, info))
+            .exceptionally(t -> {
                 var err = SQLExceptions.unwrap(t);
                 if (err instanceof SnapshotException) {
                     if (LOGGER.isDebugEnabled()) {
                         LOGGER.debug("Couldn't retrieve snapshotId={} error={}", snapshotId, err);
                     }
-                    return new SysSnapshot(
-                        snapshotId.getUUID(),
-                        snapshotId.getName(),
-                        repository.getMetadata().name(),
-                        Collections.emptyList(),
-                        Collections.emptyList(),
-                        null,
-                        null,
-                        null,
-                        SnapshotState.FAILED.name(),
-                        List.of(),
-                        null, // We don't show info retrieval error, "reason" shows only snapshotting operation error.
-                        0,
-                        null
-                    );
+                    return SysSnapshot.ofMissingInfo(repository.getMetadata().name(), snapshotId);
                 }
                 throw Exceptions.toRuntimeException(err);
             });
-    }
-
-    /**
-     * Returns a list of currently running snapshots from repository sorted by snapshot creation date
-     *
-     * @param snapshotsInProgress snapshots in progress in the cluster state
-     * @param repositoryName repository to check running snapshots
-     * @return list of snapshots
-     */
-    public static List<SysSnapshot> snapshotsInProgress(@Nullable SnapshotsInProgress snapshotsInProgress,
-                                                        String repositoryName) {
-        List<SysSnapshot> sysSnapshots = new ArrayList<>();
-        List<SnapshotsInProgress.Entry> entries =
-            SnapshotsService.currentSnapshots(snapshotsInProgress, repositoryName, Collections.emptyList());
-        for (SnapshotsInProgress.Entry entry : entries) {
-            List<String> partedTables = new ArrayList<>();
-            for (var relationName : entry.relationNames()) {
-                partedTables.add(relationName.fqn());
-            }
-            sysSnapshots.add(SysSnapshots.toSysSnapshot(repositoryName, entry, partedTables));
-        }
-        return sysSnapshots;
     }
 }
