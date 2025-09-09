8e843ddde74a3e6c516d1f2727b80ef9de555525
Mathias Fussenegger <f.mathias@zignar.net>
2025-09-09 08:51:16 +0200

    Move SysSnapshot factory methods into SysSnapshot

--- server/src/main/java/io/crate/expression/reference/sys/snapshot/SysSnapshot.java
+++ server/src/main/java/io/crate/expression/reference/sys/snapshot/SysSnapshot.java
@@ -24,6 +24,18 @@ package io.crate.expression.reference.sys.snapshot;
 import java.util.List;
 import java.util.stream.Stream;
 
+import org.elasticsearch.Version;
+import org.elasticsearch.cluster.SnapshotsInProgress;
+import org.elasticsearch.cluster.metadata.Metadata;
+import org.elasticsearch.cluster.metadata.RelationMetadata;
+import org.elasticsearch.repositories.IndexId;
+import org.elasticsearch.snapshots.Snapshot;
+import org.elasticsearch.snapshots.SnapshotId;
+import org.elasticsearch.snapshots.SnapshotInfo;
+import org.elasticsearch.snapshots.SnapshotShardFailure;
+import org.elasticsearch.snapshots.SnapshotState;
+
+import io.crate.common.collections.Lists;
 import io.crate.metadata.IndexName;
 import io.crate.metadata.IndexParts;
 import io.crate.metadata.PartitionName;
@@ -42,6 +54,67 @@ public record SysSnapshot(String uuid,
                           String reason,
                           int totalShards,
                           Boolean includeGlobalState) {
+
+    public static SysSnapshot of(SnapshotsInProgress.Entry inProgressEntry) {
+        Snapshot snapshot = inProgressEntry.snapshot();
+        SnapshotId snapshotId = snapshot.getSnapshotId();
+        return new SysSnapshot(
+            snapshotId.getUUID(),
+            snapshotId.getName(),
+            snapshot.getRepository(),
+            Lists.map(inProgressEntry.indices(), IndexId::getName),
+            Lists.map(inProgressEntry.relationNames(), RelationName::fqn),
+            inProgressEntry.startTime(),
+            0L,
+            Version.CURRENT.toString(),
+            SnapshotState.IN_PROGRESS.name(),
+            List.of(),
+            inProgressEntry.failure(),
+            inProgressEntry.shards().size(),
+            inProgressEntry.includeGlobalState()
+        );
+    }
+
+    public static SysSnapshot of(Metadata metadata, String repoName, SnapshotInfo info) {
+        SnapshotId snapshotId = info.snapshotId();
+        Version version = info.version();
+        return new SysSnapshot(
+            snapshotId.getUUID(),
+            snapshotId.getName(),
+            repoName,
+            info.indices(),
+            metadata.relations(RelationMetadata.Table.class).stream()
+                .filter(x -> !x.partitionedBy().isEmpty())
+                .map(x -> x.name().fqn())
+                .toList(),
+            info.startTime(),
+            info.endTime(),
+            version == null ? null : version.toString(),
+            info.state().name(),
+            Lists.map(info.shardFailures(), SnapshotShardFailure::toString),
+            info.reason(),
+            info.totalShards(),
+            info.includeGlobalState()
+        );
+    }
+
+    public static SysSnapshot ofMissingInfo(String repoName, SnapshotId snapshotId) {
+        return new SysSnapshot(
+            snapshotId.getUUID(),
+            snapshotId.getName(),
+            repoName,
+            List.of(),
+            List.of(),
+            null,
+            null,
+            null,
+            SnapshotState.FAILED.name(),
+            List.of(),
+            null, // We don't show info retrieval error, "reason" shows only snapshotting operation error.
+            0,
+            null
+        );
+    }
 
 
     public List<String> tables() {
