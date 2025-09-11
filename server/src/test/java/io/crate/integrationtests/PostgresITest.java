902cccf0ce7f0e94c67df34453a2e3df7bd46006
baur <baurzhansahariev@gmail.com>
2025-09-11 18:57:18 +0200

    more iterations

--- server/src/test/java/io/crate/integrationtests/PostgresITest.java
+++ server/src/test/java/io/crate/integrationtests/PostgresITest.java
@@ -72,7 +72,7 @@ import io.crate.testing.UseJdbc;
 import io.crate.types.DataTypes;
 
 @IntegTestCase.ClusterScope(numDataNodes = 2, numClientNodes = 0, supportsDedicatedMasters = false)
-@Repeat(iterations = 100)
+@Repeat(iterations = 120)
 public class PostgresITest extends IntegTestCase {
 
     private static final String NO_IPV6 = "CRATE_TESTS_NO_IPV6";
