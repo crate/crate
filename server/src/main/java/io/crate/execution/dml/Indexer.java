8f5a166fb285a3919b01831a3242591966cf5ef8
jeeminso <jeeminso@gmail.com>
2025-10-03 17:41:42 -0400

    Fix `RETURNING` columns omitting generated sub-columns

--- server/src/main/java/io/crate/execution/dml/Indexer.java
+++ server/src/main/java/io/crate/execution/dml/Indexer.java
@@ -202,7 +202,12 @@ public class Indexer {
             }
             int index = targetColumns.indexOf(ref);
             if (index > -1) {
-                return NestableCollectExpression.forFunction(item -> item.insertValues()[index]);
+                return NestableCollectExpression.forFunction(
+                    item -> {
+                        var val = item.insertValues()[index];
+                        return overwriteGeneratedChildren(item, val, ref);
+                    }
+                );
             }
             if (ref.granularity() == RowGranularity.PARTITION) {
                 int pIndex = table.partitionedByColumns().indexOf(ref);
@@ -266,6 +271,27 @@ public class Indexer {
             });
             Input<?> accept = generatedExpression.accept(symbolEval, Row.EMPTY);
             return accept.value();
+        }
+
+        private Object overwriteGeneratedChildren(IndexItem item, Object parent, Reference parentRef) {
+            if (parentRef.valueType().id() != ObjectType.ID) {
+                return parent;
+            }
+            for (var child : table.getLeafReferences(parentRef)) {
+                if ((child.isGenerated() || child.defaultExpression() != null)) {
+                    var childCollectExpression = getImplementation(child);
+                    childCollectExpression.setNextRow(item);
+                    var childPath = child.column().path();
+                    Maps.mergeInto(
+                        (Map<String, Object>) parent,
+                        childPath.getFirst(),
+                        childPath.subList(1, childPath.size()),
+                        childCollectExpression.value(),
+                        Map::put,
+                        false);
+                }
+            }
+            return parent;
         }
     }
 
