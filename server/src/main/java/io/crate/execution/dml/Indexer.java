42ac0897ae872e9d7e832c59d3c6f779c79b83e2
jeeminso <jeeminso@gmail.com>
2025-10-09 21:57:11 -0400

    Cannot directly re-use, the logic is not meant to merge but to retreive

--- server/src/main/java/io/crate/execution/dml/Indexer.java
+++ server/src/main/java/io/crate/execution/dml/Indexer.java
@@ -37,7 +37,6 @@ import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
-import java.util.function.BiFunction;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -201,27 +200,14 @@ public class Indexer {
                     }
                 });
             }
-
-            BiFunction<IndexItem, Integer, Object> getValue = (item, idx) -> {
-                Object val = item.insertValues()[idx];
-                if (val instanceof Map<?, ?> m) {
-                    List<String> path = column.path();
-                    val = Maps.getByPath(m, path);
-                }
-                if (val == null) {
-                    Symbol defaultExpression = ref.defaultExpression();
-                    if (defaultExpression != null) {
-                        val = defaultExpression.accept(symbolEval, Row.EMPTY).value();
-                    } else if (ref instanceof GeneratedReference generated) {
-                        return fromGenerated(generated, item);
-                    }
-                }
-                return val;
-            };
-
-            final int index = targetColumns.indexOf(ref);
+            int index = targetColumns.indexOf(ref);
             if (index > -1) {
-                return NestableCollectExpression.forFunction(item -> getValue.apply(item, index));
+                return NestableCollectExpression.forFunction(
+                    item -> {
+                        var val = item.insertValues()[index];
+                        return overwriteGeneratedChildren(item, val, ref);
+                    }
+                );
             }
             if (ref.granularity() == RowGranularity.PARTITION) {
                 int pIndex = table.partitionedByColumns().indexOf(ref);
@@ -258,7 +244,23 @@ public class Indexer {
             if (rootIndex == -1) {
                 return NestableCollectExpression.constant(null);
             }
-            return NestableCollectExpression.forFunction(item -> getValue.apply(item, rootIndex));
+            Function<IndexItem, Object> getValue = item -> {
+                Object val = item.insertValues()[rootIndex];
+                if (val instanceof Map<?, ?> m) {
+                    List<String> path = column.path();
+                    val = Maps.getByPath(m, path);
+                }
+                if (val == null) {
+                    Symbol defaultExpression = ref.defaultExpression();
+                    if (defaultExpression != null) {
+                        val = defaultExpression.accept(symbolEval, Row.EMPTY).value();
+                    } else if (ref instanceof GeneratedReference generated) {
+                        return fromGenerated(generated, item);
+                    }
+                }
+                return val;
+            };
+            return NestableCollectExpression.forFunction(getValue);
         }
 
         private Object fromGenerated(GeneratedReference generated, IndexItem item) {
@@ -269,6 +271,28 @@ public class Indexer {
             });
             Input<?> accept = generatedExpression.accept(symbolEval, Row.EMPTY);
             return accept.value();
+        }
+
+        private Object overwriteGeneratedChildren(IndexItem item, Object parent, Reference parentRef) {
+            if (parentRef.valueType().id() != ObjectType.ID) {
+                return parent;
+            }
+            Map<String, Object> m = (Map<String, Object>) parent;
+            for (var child : table.getLeafReferences(parentRef)) {
+                if ((child.isGenerated() || child.defaultExpression() != null)) {
+                    var childCollectExpression = getImplementation(child);
+                    childCollectExpression.setNextRow(item);
+                    var childPath = child.column().path();
+                    Maps.mergeInto(
+                        m,
+                        childPath.getFirst(),
+                        childPath.subList(1, childPath.size()),
+                        childCollectExpression.value(),
+                        Map::put,
+                        false);
+                }
+            }
+            return parent;
         }
     }
 
