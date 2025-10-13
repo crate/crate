b386b24870e00a0c83439adeb015efbc387b5a0e
Mathias Fussenegger <f.mathias@zignar.net>
2025-10-13 15:11:24 +0200

    Release 5.10.13

--- server/src/main/java/org/elasticsearch/Version.java
+++ server/src/main/java/org/elasticsearch/Version.java
@@ -240,7 +240,7 @@ public class Version implements Comparable<Version> {
     public static final Version V_5_10_10 = new Version(8_10_10_99, false, org.apache.lucene.util.Version.LUCENE_9_12_0);
     public static final Version V_5_10_11 = new Version(8_10_11_99, false, org.apache.lucene.util.Version.LUCENE_9_12_0);
     public static final Version V_5_10_12 = new Version(8_10_12_99, false, org.apache.lucene.util.Version.LUCENE_9_12_0);
-    public static final Version V_5_10_13 = new Version(8_10_13_99, true, org.apache.lucene.util.Version.LUCENE_9_12_3);
+    public static final Version V_5_10_13 = new Version(8_10_13_99, false, org.apache.lucene.util.Version.LUCENE_9_12_3);
 
     public static final Version CURRENT = V_5_10_13;
 
