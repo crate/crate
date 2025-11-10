crate/crate

commit f6a71240db2f96aeb4761c7c4d28468b7fcc3c37

Deprecate global columnOID

server/src/main/java/org/elasticsearch/cluster/metadata/Metadata.java
@@ -85,6 +85,7 @@ import io.crate.metadata.IndexReference;
 import io.crate.metadata.PartitionName;
 import io.crate.metadata.Reference;
 import io.crate.metadata.RelationName;
+import io.crate.metadata.doc.DocTableInfo;
 import io.crate.metadata.view.ViewsMetadata;
 import io.crate.rest.action.HttpErrorStatus;
 import io.crate.sql.tree.ColumnPolicy;
@@ -147,6 +148,7 @@ public class Metadata implements Iterable<IndexMetadata>, Diffable<Metadata> {
     private final String clusterUUID;
     private final boolean clusterUUIDCommitted;
     private final long version;
+    @Deprecated
     private final long columnOID;
 
     private final CoordinationMetadata coordinationMetadata;
@@ -492,7 +494,7 @@ public class Metadata implements Iterable<IndexMetadata>, Diffable<Metadata> {
             clusterUUID = in.readString();
             clusterUUIDCommitted = in.readBoolean();
             version = in.readLong();
-            if (in.getVersion().onOrAfter(Version.V_5_5_0)) {
+            if (in.getVersion().onOrAfter(Version.V_5_5_0) && in.getVersion().onOrBefore(Version.V_6_0_3)) {
                 columnOID = in.readLong();
             } else {
                 columnOID = COLUMN_OID_UNASSIGNED;
@@ -520,7 +522,7 @@ public class Metadata implements Iterable<IndexMetadata>, Diffable<Metadata> {
             out.writeString(clusterUUID);
             out.writeBoolean(clusterUUIDCommitted);
             out.writeLong(version);
-            if (out.getVersion().onOrAfter(Version.V_5_5_0)) {
+            if (out.getVersion().onOrAfter(Version.V_5_5_0) && out.getVersion().onOrBefore(Version.V_6_0_3)) {
                 out.writeLong(columnOID);
             }
             coordinationMetadata.writeTo(out);
@@ -555,10 +557,8 @@ public class Metadata implements Iterable<IndexMetadata>, Diffable<Metadata> {
     public static Metadata readFrom(StreamInput in) throws IOException {
         Builder builder = new Builder();
         builder.version = in.readLong();
-        if (in.getVersion().onOrAfter(Version.V_5_5_0)) {
+        if (in.getVersion().onOrAfter(Version.V_5_5_0) && in.getVersion().onOrBefore(Version.V_6_0_3)) {
             builder.columnOID(in.readLong());
-        } else {
-            builder.columnOID(COLUMN_OID_UNASSIGNED);
         }
         builder.clusterUUID = in.readString();
         builder.clusterUUIDCommitted = in.readBoolean();
@@ -598,7 +598,7 @@ public class Metadata implements Iterable<IndexMetadata>, Diffable<Metadata> {
     @Override
     public void writeTo(StreamOutput out) throws IOException {
         out.writeLong(version);
-        if (out.getVersion().onOrAfter(Version.V_5_5_0)) {
+        if (out.getVersion().onOrAfter(Version.V_5_5_0) && out.getVersion().onOrBefore(Version.V_6_0_3)) {
             out.writeLong(columnOID);
         }
         out.writeString(clusterUUID);
@@ -654,28 +654,13 @@ public class Metadata implements Iterable<IndexMetadata>, Diffable<Metadata> {
         return new Builder(metadata);
     }
 
-    private static class ColumnOidSupplier implements LongSupplier {
-        private long columnOID;
-
-        @VisibleForTesting
-        public ColumnOidSupplier(long columnOID) {
-            this.columnOID = columnOID;
-        }
-
-        @Override
-        public long getAsLong() {
-            columnOID++;
-            return columnOID;
-        }
-    }
-
     public static class Builder {
 
         public static final LongSupplier NO_OID_COLUMN_OID_SUPPLIER = () -> COLUMN_OID_UNASSIGNED;
         private String clusterUUID;
         private boolean clusterUUIDCommitted;
         private long version;
-        private ColumnOidSupplier columnOidSupplier;
+        private long columnOID;
         private CoordinationMetadata coordinationMetadata = CoordinationMetadata.EMPTY_METADATA;
         private Settings transientSettings = Settings.EMPTY;
         private Settings persistentSettings = Settings.EMPTY;
@@ -691,7 +676,7 @@ public class Metadata implements Iterable<IndexMetadata>, Diffable<Metadata> {
             templates = ImmutableOpenMap.builder();
             customs = ImmutableOpenMap.builder();
             schemas = ImmutableOpenMap.builder();
-            columnOidSupplier = new ColumnOidSupplier(COLUMN_OID_UNASSIGNED);
+            columnOID = COLUMN_OID_UNASSIGNED;
             indexGraveyard(IndexGraveyard.builder().build()); // create new empty index graveyard to initialize
         }
 
@@ -702,7 +687,7 @@ public class Metadata implements Iterable<IndexMetadata>, Diffable<Metadata> {
             this.transientSettings = metadata.transientSettings;
             this.persistentSettings = metadata.persistentSettings;
             this.version = metadata.version;
-            this.columnOidSupplier = new ColumnOidSupplier(metadata.columnOID);
+            this.columnOID = metadata.columnOID;
             this.indices = ImmutableOpenMap.builder(metadata.indices);
             this.templates = ImmutableOpenMap.builder(metadata.templates);
             this.customs = ImmutableOpenMap.builder(metadata.customs);
@@ -918,8 +903,9 @@ public class Metadata implements Iterable<IndexMetadata>, Diffable<Metadata> {
             return this;
         }
 
+        @Deprecated
         public Builder columnOID(long columnOID) {
-            this.columnOidSupplier = new ColumnOidSupplier(columnOID);
+            this.columnOID = columnOID;
             return this;
         }
 
@@ -938,10 +924,6 @@ public class Metadata implements Iterable<IndexMetadata>, Diffable<Metadata> {
                 clusterUUID = UUIDs.randomBase64UUID();
             }
             return this;
-        }
-
-        public LongSupplier columnOidSupplier() {
-            return columnOidSupplier;
         }
 
         public Metadata build() {
@@ -982,7 +964,7 @@ public class Metadata implements Iterable<IndexMetadata>, Diffable<Metadata> {
                 clusterUUID,
                 clusterUUIDCommitted,
                 version,
-                columnOidSupplier.columnOID,
+                columnOID,
                 coordinationMetadata,
                 transientSettings,
                 persistentSettings,
@@ -1085,7 +1067,7 @@ public class Metadata implements Iterable<IndexMetadata>, Diffable<Metadata> {
                     if ("version".equals(currentFieldName)) {
                         builder.version = parser.longValue();
                     } else if ("column_oid".equals(currentFieldName)) {
-                        builder.columnOidSupplier = new ColumnOidSupplier(parser.longValue());
+                        builder.columnOID = parser.longValue();
                     } else if ("cluster_uuid".equals(currentFieldName) || "uuid".equals(currentFieldName)) {
                         builder.clusterUUID = parser.text();
                     } else if ("cluster_uuid_committed".equals(currentFieldName)) {
@@ -1183,7 +1165,7 @@ public class Metadata implements Iterable<IndexMetadata>, Diffable<Metadata> {
                                 List<String> indexUUIDs,
                                 long tableVersion) {
             return setTable(
-                columnOidSupplier,
+                new DocTableInfo.OidSupplier(0),
                 relationName,
                 columns,
                 settings,

server/src/test/java/io/crate/integrationtests/PgCatalogITest.java
@@ -463,9 +463,8 @@ public class PgCatalogITest extends IntegTestCase {
             order by attnum"""
         );
 
-        // Column 'a' has OID 6 because first 5 are taken by the table, created in createRelations().
         assertThat(response).hasRows(
-            "1| true| _dropped_6| 0",
+            "1| true| _dropped_1| 0",
             "2| false| o| 114",
             "3| false| o['oo']| 114",
             "4| false| o['oo']['a']| 23",
@@ -484,10 +483,10 @@ public class PgCatalogITest extends IntegTestCase {
             order by attnum"""
         );
         assertThat(response).hasRows(
-            "1| true| _dropped_6",
+            "1| true| _dropped_1",
             "2| false| o",
-            "3| true| _dropped_8",
-            "4| true| _dropped_9",
+            "3| true| _dropped_3",
+            "4| true| _dropped_4",
             "5| false| a",
             "6| false| o['oo']",
             "7| false| o['oo']['a']"

server/src/test/java/org/elasticsearch/gateway/PersistedClusterStateServiceTests.java
@@ -679,26 +679,6 @@ public class PersistedClusterStateServiceTests extends ESTestCase {
     }
 
     @Test
-    public void test_persists_updated_column_oid() throws IOException {
-        try (NodeEnvironment nodeEnvironment = newNodeEnvironment(createDataPaths())) {
-            final PersistedClusterStateService persistedClusterStateService = newPersistedClusterStateService(nodeEnvironment);
-            final long columnOid = 123L;
-
-            try (Writer writer = persistedClusterStateService.createWriter()) {
-                // Need to write full state first before doing incremental writes.
-                writer.writeFullStateAndCommit(1L, ClusterState.EMPTY_STATE);
-
-                ClusterState clusterState = loadPersistedClusterState(persistedClusterStateService);
-                writer.writeIncrementalStateAndCommit(clusterState.term(), clusterState, ClusterState.builder(clusterState)
-                    .metadata(Metadata.builder(clusterState.metadata()).columnOID(columnOid)).build());
-
-                clusterState = loadPersistedClusterState(persistedClusterStateService);
-                assertThat(clusterState.metadata().columnOID()).isEqualTo(columnOid);
-            }
-        }
-    }
-
-    @Test
     public void testPersistsAndReloadsIndexMetadataForMultipleIndices() throws IOException {
         try (NodeEnvironment nodeEnvironment = newNodeEnvironment(createDataPaths())) {
             final PersistedClusterStateService persistedClusterStateService = newPersistedClusterStateService(nodeEnvironment);

commit 0288df3ba458926e53b5607c9e33b46494ee667f

Fix BWC streaming with 6.1.0 nodes
Fixes regression introduced with https://github.com/crate/crate/commit/f6a71240db2f96aeb4761c7c4d28468b7fcc3c37

server/src/main/java/org/elasticsearch/cluster/metadata/Metadata.java
@@ -454,6 +454,11 @@ public class Metadata implements Iterable<IndexMetadata>, Diffable<Metadata> {
         return Builder.fromXContent(parser, false);
     }
 
+    private static boolean hasGlobalColumnOID(Version version) {
+        return version.onOrAfter(Version.V_5_5_0) &&
+            (version.onOrBefore(Version.V_6_0_3) || version.equals(Version.V_6_1_0));
+    }
+
     private static class MetadataDiff implements Diff<Metadata> {
 
         private final long version;
@@ -494,7 +499,7 @@ public class Metadata implements Iterable<IndexMetadata>, Diffable<Metadata> {
             clusterUUID = in.readString();
             clusterUUIDCommitted = in.readBoolean();
             version = in.readLong();
-            if (in.getVersion().onOrAfter(Version.V_5_5_0) && in.getVersion().onOrBefore(Version.V_6_0_3)) {
+            if (hasGlobalColumnOID(in.getVersion())) {
                 columnOID = in.readLong();
             } else {
                 columnOID = COLUMN_OID_UNASSIGNED;
@@ -522,7 +527,7 @@ public class Metadata implements Iterable<IndexMetadata>, Diffable<Metadata> {
             out.writeString(clusterUUID);
             out.writeBoolean(clusterUUIDCommitted);
             out.writeLong(version);
-            if (out.getVersion().onOrAfter(Version.V_5_5_0) && out.getVersion().onOrBefore(Version.V_6_0_3)) {
+            if (hasGlobalColumnOID(out.getVersion())) {
                 out.writeLong(columnOID);
             }
             coordinationMetadata.writeTo(out);
@@ -557,7 +562,7 @@ public class Metadata implements Iterable<IndexMetadata>, Diffable<Metadata> {
     public static Metadata readFrom(StreamInput in) throws IOException {
         Builder builder = new Builder();
         builder.version = in.readLong();
-        if (in.getVersion().onOrAfter(Version.V_5_5_0) && in.getVersion().onOrBefore(Version.V_6_0_3)) {
+        if (hasGlobalColumnOID(in.getVersion())) {
             builder.columnOID(in.readLong());
         }
         builder.clusterUUID = in.readString();
@@ -598,7 +603,7 @@ public class Metadata implements Iterable<IndexMetadata>, Diffable<Metadata> {
     @Override
     public void writeTo(StreamOutput out) throws IOException {
         out.writeLong(version);
-        if (out.getVersion().onOrAfter(Version.V_5_5_0) && out.getVersion().onOrBefore(Version.V_6_0_3)) {
+        if (hasGlobalColumnOID(out.getVersion())) {
             out.writeLong(columnOID);
         }
         out.writeString(clusterUUID);

server/src/test/java/org/elasticsearch/cluster/metadata/MetadataTest.java
@@ -1,0 +1,50 @@
+/*
+ * Licensed to Crate.io GmbH ("Crate") under one or more contributor
+ * license agreements.  See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.  Crate licenses
+ * this file to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.  You may
+ * obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * However, if you have executed another commercial license agreement
+ * with Crate these terms will supersede the license and you may use the
+ * software solely pursuant to the terms of the relevant commercial agreement.
+ */
+
+package org.elasticsearch.cluster.metadata;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.elasticsearch.Version;
+import org.elasticsearch.common.io.stream.BytesStreamOutput;
+import org.junit.Test;
+
+public class MetadataTest {
+
+    @Test
+    public void test_bwc_read_writes_with_6_1_0() throws Exception {
+        Metadata metadata = Metadata.builder()
+                .columnOID(123L)
+                // builder() adds IndexGraveyard custom, which causes "can't read named writeable from StreamInput" error on reads.
+                // In production NamedWriteableAwareStreamInput is used.
+                // Resetting it here for simplicity as it's irrelevant for the test.
+                .removeCustom(IndexGraveyard.TYPE)
+                .build();
+
+        BytesStreamOutput out = new BytesStreamOutput();
+        out.setVersion(Version.fromString("6.1.0"));
+        metadata.writeTo(out); // OID should be written, 6.1.0 expects it.
+        var in = out.bytes().streamInput();
+        in.setVersion(Version.fromString("6.1.0"));
+        Metadata recievedMetadata = Metadata.readFrom(in); // We are reading from 6.1.0, which sends out OID.
+        assertThat(recievedMetadata.columnOID()).isEqualTo(123L);
+    }
+}