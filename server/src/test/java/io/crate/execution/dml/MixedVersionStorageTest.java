crate/crate

commit 0e0dd739ac60fb9db95be668067d2630a72db71c

Fix false omission of NULL-assigned sub-columns during replica recovery
Ensure null-assigned sub-columns are included in translog entries used
for replica recovery

server/src/main/java/io/crate/execution/dml/ObjectIndexer.java
@@ -102,11 +102,11 @@ public class ObjectIndexer implements ValueIndexer<Map<String, Object>> {
             String innerName = entry.getKey();
             Child child = entry.getValue();
             if (value.containsKey(innerName) == false) {
-                var synth = docBuilder.getSyntheticValue(child.ident());
+                var synth = docBuilder.getSynthetic(child.ident());
                 if (synth != null) {
                     // directly modify the map so that containing types will see the value
                     // if they need to write stored fields
-                    value.put(innerName, synth);
+                    value.put(innerName, synth.value());
                 }
             }
             var innerValue = value.get(innerName);
@@ -131,6 +131,8 @@ public class ObjectIndexer implements ValueIndexer<Map<String, Object>> {
                 columnsToStore.put(k, v);
             }
             if (v == null) {
+                translogWriter.writeFieldName(k);
+                translogWriter.writeValue(null);
                 columnsToStore.put(k, null);
             }
         });
@@ -155,7 +157,7 @@ public class ObjectIndexer implements ValueIndexer<Map<String, Object>> {
         translogWriter.endObject();
     }
 
-    private BytesReference toBytes(Map<String, Object> v, Version version) {
+    public static BytesReference toBytes(Map<String, Object> v, Version version) {
         try (BytesStreamOutput out = new BytesStreamOutput()) {
             out.setVersion(version);
             out.writeMap(v, StreamOutput::writeString, StreamOutput::writeGenericValue);

server/src/main/java/io/crate/execution/dml/IndexDocumentBuilder.java
@@ -106,12 +106,8 @@ public class IndexDocumentBuilder {
         return translogWriter;
     }
 
-    /**
-     * @return a generated value for the given column if one exists, otherwise null
-     */
-    public Object getSyntheticValue(ColumnIdent columnIdent) {
-        Input<Object> input = synthetics.get(columnIdent);
-        return input == null ? null : input.value();
+    public Input<Object> getSynthetic(ColumnIdent columnIdent) {
+        return synthetics.get(columnIdent);
     }
 
     /**

server/src/test/java/io/crate/execution/dml/IndexerTest.java
@@ -650,33 +650,6 @@ public class IndexerTest extends CrateDummyClusterServiceUnitTest {
     }
 
     @Test
-    public void test_fields_are_omitted_in_source_for_null_values() throws Exception {
-        SQLExecutor e = SQLExecutor.of(clusterService)
-            .addTable("create table tbl (x int, o object as (y int))");
-
-        DocTableInfo table = e.resolveTableInfo("tbl");
-        Indexer indexer = new Indexer(
-            List.of(),
-            table,
-            Version.CURRENT,
-            new CoordinatorTxnCtx(e.getSessionSettings()),
-            e.nodeCtx,
-            List.of(
-                table.getReference(ColumnIdent.of("x")),
-                table.getReference(ColumnIdent.of("o"))
-            ),
-            null, null
-        );
-
-        HashMap<String, Object> o = new HashMap<>();
-        o.put("y", null);
-        ParsedDocument doc = indexer.index(item(null, o));
-        assertThat(source(doc, table)).isEqualTo(
-            "{\"o\":{}}"
-        );
-    }
-
-    @Test
     public void test_indexing_float_results_in_float_field() throws Exception {
         SQLExecutor e = SQLExecutor.of(clusterService)
             .addTable("create table tbl (x float)");
@@ -1582,6 +1555,41 @@ public class IndexerTest extends CrateDummyClusterServiceUnitTest {
         );
         var item = new IndexItem.StaticItem("0", List.of(), new Object[]{-1, 0}, 0L, 0L);
         indexer.collectSchemaUpdates(item); // checks that it does not throw any IndexOutOfBoundsExceptions
+    }
+
+    @Test
+    public void test_null_valued_sub_columns_are_stored() throws Exception {
+        SQLExecutor executor = SQLExecutor.of(clusterService)
+            .addTable("""
+                create table t (
+                    o object as (a int, b int as o['a']+1, c int)
+                )
+                """
+            );
+        DocTableInfo table = executor.resolveTableInfo("t");
+
+        Indexer indexer = new Indexer(
+            List.of(),
+            table,
+            Version.CURRENT,
+            new CoordinatorTxnCtx(executor.getSessionSettings()),
+            executor.nodeCtx,
+            new ArrayList<>(List.of(table.getReference(ColumnIdent.of("o")))),
+            null, null
+        );
+        // update t set o['a']=null, o['c']=1
+        ParsedDocument parsedDocument = indexer.index(item(MapBuilder.newMapBuilder().put("a", null).put("c", 1).map()));
+        assertThat(source(parsedDocument, table)).isEqualTo("{\"o\":{\"a\":null,\"b\":null,\"c\":1}}");
+
+        // notice null valued sub-columns and non-null sub-columns are persisted differently
+        Map<String, Object> storedValue = new HashMap<>();
+        storedValue.put("a", null);
+        storedValue.put("b", null);
+        assertThat(parsedDocument.doc().getField("1").binaryValue()) // "1" is the oid for 'o'
+            .isEqualTo(ObjectIndexer.toBytes(storedValue, Version.CURRENT).toBytesRef());
+        assertThat(parsedDocument.doc().getField("4").numericValue()).isEqualTo(1); // "4" is the oid for 'c'
+
+        assertTranslogParses(parsedDocument, table);
     }
 
     public static void assertTranslogParses(ParsedDocument doc, DocTableInfo info) {

commit a07d61b6dffc0546bf71e78f35d5d5e7d5c1750c

Don't write key twice to the translog
Happens on tables without oids when a null subcolumn is written into IGNORED object

Fixes a regression introduced with https://github.com/crate/crate/pull/18369

server/src/main/java/io/crate/execution/dml/ObjectIndexer.java
@@ -132,8 +132,7 @@ public class ObjectIndexer implements ValueIndexer<Map<String, Object>> {
                     translogWriter.writeFieldName(this.unknownColumnPrefix + k);
                     translogWriter.writeValue(v);
                     columnsToStore.put(k, v);
-                }
-                if (v == null) {
+                } else if (v == null) {
                     translogWriter.writeFieldName(k);
                     translogWriter.writeValue(null);
                     columnsToStore.put(k, null);

server/src/test/java/io/crate/execution/dml/MixedVersionStorageTest.java
@@ -24,7 +24,9 @@ package io.crate.execution.dml;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.elasticsearch.Version;
 import org.junit.Test;
@@ -55,6 +57,32 @@ public class MixedVersionStorageTest extends CrateDummyClusterServiceUnitTest {
             var row = lookup.getStoredRow(new ReaderContext(reader), 0);
             assertThat(row.asRaw()).isNotEmpty();
             assertThat(row.get(List.of("numbers"))).isOfAnyClassIn(ArrayList.class);
+        }
+    }
+
+    @Test
+    public void test_writing_null_subcolumn_to_ignored_object_in_table_without_oids() throws Exception {
+        var columns = List.of("obj");
+        var builder = new QueryTester.Builder(
+            THREAD_POOL,
+            clusterService,
+            Version.V_5_4_0, // No oids
+            "create table t (obj object(ignored))"
+        );
+        Map<String, Object> map = new HashMap<>();
+        map.put("key", null);
+        builder.indexValues(columns, map);
+        try (var tester = builder.build()) {
+            // We write NULL-s to translog since 5.10.13 (https://github.com/crate/crate/pull/18369).
+            // This change introduced a regression, causing NULL values in the OBJECT(IGNORED)
+            // to be persisted twice in the translog, if table had no oids.
+            // We imitate translog lookup to ensure that we are not hitting JSON parsing exception when parsing the source.
+            boolean fromTranslog = true;
+            var lookup = StoredRowLookup.create(Version.V_5_4_0, tester.tableInfo(), List.of(), List.of(), fromTranslog);
+            var reader = tester.searcher().getTopReaderContext().leaves().getFirst();
+            var row = lookup.getStoredRow(new ReaderContext(reader), 0);
+            // Used to fail with "JsonParseException: Duplicate field 'key'".
+            assertThat(row.asMap().get("obj")).isEqualTo(map);
         }
     }
 }