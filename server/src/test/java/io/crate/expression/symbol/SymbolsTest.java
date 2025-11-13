b4566ceb598ab230630a7e5d6dbce1b655ec8b93
jeeminso <jeeminso@gmail.com>
2025-11-13 18:21:31 -0500

    Fix resolution of same named columns in nested join conditions
    (cherry picked from commit 6a47f8482c7f059e7b465ee36edc41bf7048bd70)

--- server/src/test/java/io/crate/expression/symbol/SymbolsTest.java
+++ server/src/test/java/io/crate/expression/symbol/SymbolsTest.java
@@ -24,11 +24,16 @@ package io.crate.expression.symbol;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 import org.elasticsearch.common.io.stream.BytesStreamOutput;
 import org.elasticsearch.common.io.stream.StreamInput;
 import org.junit.Test;
 
+import io.crate.metadata.ColumnIdent;
+import io.crate.metadata.RelationName;
+import io.crate.metadata.SimpleReference;
 import io.crate.test.integration.CrateDummyClusterServiceUnitTest;
 import io.crate.testing.SQLExecutor;
 
@@ -65,5 +70,32 @@ public class SymbolsTest extends CrateDummyClusterServiceUnitTest {
             .addTable("create table tbl (x int)");
         Symbol symbol = e.asSymbol("x + 10 > 100");
         assertThat(symbol.ramBytesUsed()).isEqualTo(912L);
+    }
+
+    @Test
+    public void test_lookupValueByColumn_must_compare_scoped_symbols_relations() throws IOException {
+        var e = SQLExecutor.of(clusterService)
+            .addTable("create table tbl1 (o object)")
+            .addTable("create table tbl2 (o object)")
+            .addTable("create table tbl3 (o object)")
+            .addTable("create table tbl4 (o object)");
+
+        ScopedSymbol t1o = (ScopedSymbol) e.analyze("select t1.o from tbl1 as t1").outputs().getFirst();
+        ScopedSymbol t2o = (ScopedSymbol) e.analyze("select t2.o from tbl2 as t2").outputs().getFirst();
+        SimpleReference tbl3oRef = (SimpleReference) e.analyze("select o from tbl3").outputs().getFirst();
+        SimpleReference tbl4oRef = (SimpleReference) e.analyze("select o from tbl4").outputs().getFirst();
+
+        Map<Symbol, String> valuesBySymbol = new LinkedHashMap<>();
+        valuesBySymbol.put(t1o, "lookupValueByColumn returned ScopedSymbol t1.o");
+        valuesBySymbol.put(t2o, "lookupValueByColumn returned ScopedSymbol t2.o");
+        valuesBySymbol.put(tbl3oRef, "lookupValueByColumn returned Reference tbl3.o");
+        valuesBySymbol.put(tbl4oRef, "lookupValueByColumn returned Reference tbl4.o");
+
+        ColumnIdent o = ColumnIdent.of("o");
+
+        assertThat(Symbols.lookupValueByColumn(new RelationName(null, "t1"), valuesBySymbol, o)).isEqualTo("lookupValueByColumn returned ScopedSymbol t1.o");
+        assertThat(Symbols.lookupValueByColumn(new RelationName(null, "t2"), valuesBySymbol, o)).isEqualTo("lookupValueByColumn returned ScopedSymbol t2.o");
+        assertThat(Symbols.lookupValueByColumn(new RelationName("doc", "tbl3"), valuesBySymbol, o)).isEqualTo("lookupValueByColumn returned Reference tbl3.o");
+        assertThat(Symbols.lookupValueByColumn(new RelationName("doc", "tbl4"), valuesBySymbol, o)).isEqualTo("lookupValueByColumn returned Reference tbl4.o");
     }
 }
