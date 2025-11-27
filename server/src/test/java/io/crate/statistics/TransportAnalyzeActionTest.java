4e15925a2389aa87c398e8bf25df1f68e20a4bfe
baur <baurzhansahariev@gmail.com>
2025-11-27 14:06:27 +0100

    Don't use columns info to resolve active execution for ANALYZE
    In https://github.com/crate/crate/commit/e1cc214cafe59a7dd0160658269b75ea76482540,
    we added future-per-table in request hadler to track active samplings in order to re-use them.
    
    Originally it was done as a follow up to https://github.com/crate/crate/commit/7f03ad64d5df3a077c80149c49643476757775ad
    but it differes from it in a way that automatic ANALYZE doesn't care about tables state, it doesn't run until previous is done.
    
    With schema changes in between, manual run can start collecting samples even if we had already running,
    which defeats the idea of having only one active ANALYZE

--- server/src/test/java/io/crate/statistics/TransportAnalyzeActionTest.java
+++ server/src/test/java/io/crate/statistics/TransportAnalyzeActionTest.java
@@ -22,23 +22,62 @@
 package io.crate.statistics;
 
 import static io.crate.testing.Asserts.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import java.util.Collections;
 import java.util.List;
 
-import org.elasticsearch.test.ESTestCase;
+import org.elasticsearch.Version;
+import org.elasticsearch.common.io.stream.NamedWriteableRegistry;
+import org.elasticsearch.common.network.NetworkService;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.util.PageCacheRecycler;
+import org.elasticsearch.indices.breaker.NoneCircuitBreakerService;
+import org.elasticsearch.test.transport.MockTransportService;
+import org.elasticsearch.transport.TransportService;
+import org.elasticsearch.transport.netty4.Netty4Transport;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
+import io.crate.auth.AlwaysOKAuthentication;
+import io.crate.metadata.ColumnIdent;
 import io.crate.metadata.Reference;
 import io.crate.metadata.ReferenceIdent;
 import io.crate.metadata.RelationName;
 import io.crate.metadata.RowGranularity;
 import io.crate.metadata.Schemas;
 import io.crate.metadata.SimpleReference;
+import io.crate.metadata.doc.DocTableInfo;
+import io.crate.metadata.doc.DocTableInfoFactory;
+import io.crate.netty.NettyBootstrap;
+import io.crate.protocols.ssl.SslContextProvider;
+import io.crate.role.Role;
+import io.crate.test.integration.CrateDummyClusterServiceUnitTest;
+import io.crate.testing.SQLExecutor;
 import io.crate.types.ArrayType;
 import io.crate.types.DataTypes;
 
 
-public class TransportAnalyzeActionTest extends ESTestCase {
+public class TransportAnalyzeActionTest extends CrateDummyClusterServiceUnitTest {
+
+    private NettyBootstrap nettyBootstrap;
+
+    @Before
+    public void setupNetty() {
+        nettyBootstrap = new NettyBootstrap(Settings.EMPTY);
+        nettyBootstrap.start();
+    }
+
+    @After
+    public void teardownNetty() {
+        nettyBootstrap.close();
+    }
 
     @Test
     public void test_create_stats_for_tables_with_array_columns_with_nulls() {
@@ -63,5 +102,69 @@ public class TransportAnalyzeActionTest extends ESTestCase {
         );
         var stats = samples.createTableStats(references);
         assertThat(stats.numDocs()).isEqualTo(2L);
+    }
+
+    @Test
+    public void test_existing_run_reused_even_if_table_schema_was_updated() throws Exception {
+        SQLExecutor executor = SQLExecutor.of(clusterService)
+            .addTable(
+                """
+                create table doc.t (
+                    a int
+                )
+                """
+            );
+        ReservoirSampler mockSampler = mock(ReservoirSampler.class);
+        when(mockSampler.getSamples(any(), anyList())).thenAnswer(_ -> {
+            // Sleep a bit to be sure that second request always arrives when first is still running.
+            Thread.sleep(100);
+            return Samples.EMPTY;
+        });
+
+        try (TransportService service = createTransportService()) {
+            TransportAnalyzeAction transportAnalyzeAction = new TransportAnalyzeAction(
+                service,
+                mockSampler,
+                executor.nodeCtx,
+                clusterService,
+                null // TableStatsService won't be used, is irrelevant for this test.
+            );
+
+            RelationName relationName = new RelationName(Schemas.DOC_SCHEMA_NAME, "t");
+            DocTableInfo table = new DocTableInfoFactory(executor.nodeCtx)
+                .create(relationName, clusterService.state().metadata());
+            Reference ref = table.getReference(ColumnIdent.of("a"));
+            assert ref != null : "ref must be resolved";
+            transportAnalyzeAction.fetchSamplesThenGenerateAndPublishStats();
+
+            transportAnalyzeAction.fetchSamplesThenGenerateAndPublishStats();
+            // assertBusy because of the sleep.
+            assertBusy(() -> verify(mockSampler, times(1)).getSamples(any(), anyList()));
+        }
+    }
+
+    private TransportService createTransportService() {
+        var transport = new Netty4Transport(
+            Settings.EMPTY,
+            Version.CURRENT,
+            THREAD_POOL,
+            new NetworkService(Collections.emptyList()),
+            PageCacheRecycler.NON_RECYCLING_INSTANCE,
+            new NamedWriteableRegistry(Collections.emptyList()),
+            new NoneCircuitBreakerService(),
+            nettyBootstrap,
+            new AlwaysOKAuthentication(() -> List.of(Role.CRATE_USER)),
+            new SslContextProvider(Settings.EMPTY)
+        );
+        TransportService transportService = new MockTransportService(
+            Settings.EMPTY,
+            transport,
+            THREAD_POOL,
+            _ -> clusterService.localNode(),
+            clusterService.getClusterSettings()
+        );
+        transportService.start();
+        transportService.acceptIncomingRequests();
+        return transportService;
     }
 }
