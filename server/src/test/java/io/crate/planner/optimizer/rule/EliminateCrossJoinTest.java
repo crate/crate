crate/crate

commit 1ece4b40e51ea2b95af0ac8894318cba5359bbda

Fix cross-join elimination
Allow cross joins in the result of the cross join elimination.

server/src/main/java/io/crate/planner/optimizer/joinorder/JoinGraph.java
@@ -24,16 +24,13 @@ package io.crate.planner.optimizer.joinorder;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
-import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.function.UnaryOperator;
 
 import io.crate.analyze.relations.QuerySplitter;
 import io.crate.common.collections.Lists;
 import io.crate.common.collections.Maps;
-import io.crate.common.collections.Sets;
 import io.crate.expression.operator.EqOperator;
 import io.crate.expression.symbol.ScopedSymbol;
 import io.crate.expression.symbol.Symbol;
@@ -83,7 +80,7 @@ import io.crate.sql.tree.JoinType;
  * </pre>
  */
 public record JoinGraph(List<LogicalPlan> nodes,
-                        Map<LogicalPlan, Set<Edge>> edges,
+                        Map<LogicalPlan, List<Edge>> edges,
                         List<Symbol> filters,
                         boolean hasCrossJoin) {
 
@@ -95,7 +92,7 @@ public record JoinGraph(List<LogicalPlan> nodes,
         }
 
         var newNodes = Lists.concat(this.nodes, other.nodes);
-        var newEdges = Maps.merge(this.edges, other.edges, Sets::union);
+        var newEdges = Maps.merge(this.edges, other.edges, Lists::concat);
         var newFilters = Lists.concat(this.filters, other.filters);
         var hasCrossJoin = this.hasCrossJoin || other.hasCrossJoin();
 
@@ -107,8 +104,8 @@ public record JoinGraph(List<LogicalPlan> nodes,
         );
     }
 
-    JoinGraph withEdges(Map<LogicalPlan, Set<Edge>> edges) {
-        var newEdges = Maps.merge(this.edges, edges, Sets::union);
+    JoinGraph withEdges(Map<LogicalPlan, List<Edge>> edges) {
+        var newEdges = Maps.merge(this.edges, edges, Lists::concat);
         return new JoinGraph(this.nodes, newEdges, this.filters, this.hasCrossJoin);
     }
 
@@ -128,10 +125,10 @@ public record JoinGraph(List<LogicalPlan> nodes,
         return nodes.size();
     }
 
-    public Set<Edge> edges(LogicalPlan node) {
+    public List<Edge> edges(LogicalPlan node) {
         var result = edges.get(node);
         if (result == null) {
-            return Set.of();
+            return List.of();
         }
         return result;
     }
@@ -204,7 +201,7 @@ public record JoinGraph(List<LogicalPlan> nodes,
 
         private static class EdgeCollector extends SymbolVisitor<Map<Symbol, LogicalPlan>, Void> {
 
-            private final Map<LogicalPlan, Set<Edge>> edges = new HashMap<>();
+            private final Map<LogicalPlan, List<Edge>> edges = new HashMap<>();
             private final List<LogicalPlan> sources = new ArrayList<>();
 
             @Override
@@ -246,9 +243,9 @@ public record JoinGraph(List<LogicalPlan> nodes,
             private void addEdge(LogicalPlan from, Edge edge) {
                 var values = edges.get(from);
                 if (values == null) {
-                    values = Set.of(edge);
+                    values = List.of(edge);
                 } else {
-                    values = new LinkedHashSet<>(values);
+                    values = new ArrayList<>(values);
                     values.add(edge);
                 }
                 edges.put(from, values);

server/src/test/java/io/crate/planner/optimizer/rule/EliminateCrossJoinTest.java
@@ -175,8 +175,14 @@ public class EliminateCrossJoinTest extends CrateDummyClusterServiceUnitTest {
             "  └ Collect[doc.a | [x] | true]"
         );
 
-        var invalidOrder = EliminateCrossJoin.reorder(joinGraph, List.of(a, c, b));
-        assertThat(invalidOrder).isNull();
+        var orderWithCrossJoin = EliminateCrossJoin.reorder(joinGraph, List.of(a, c, b));
+        assertThat(orderWithCrossJoin).hasOperators(
+            "Join[INNER | ((x = y) AND (y = z))]",
+            "  ├ Join[CROSS]",
+            "  │  ├ Collect[doc.a | [x] | true]",
+            "  │  └ Collect[doc.c | [z] | true]",
+            "  └ Collect[doc.b | [y] | true]"
+        );
     }
 
     @Test
@@ -210,16 +216,6 @@ public class EliminateCrossJoinTest extends CrateDummyClusterServiceUnitTest {
             "  │  │  └ Collect[doc.c | [z] | true]\n" +
             "  │  └ Collect[doc.b | [y] | true]\n" +
             "  └ Collect[doc.d | [w] | true]"
-        );
-
-        assertThat(EliminateCrossJoin.reorder(joinGraph, List.of(b, d, a, c))).isEqualTo(
-            "Join[INNER | (x = z)]\n" +
-            "  ├ Join[INNER | (x = y)]\n" +
-            "  │  ├ Join[INNER | (y = w)]\n" +
-            "  │  │  ├ Collect[doc.b | [y] | true]\n" +
-            "  │  │  └ Collect[doc.d | [w] | true]\n" +
-            "  │  └ Collect[doc.a | [x] | true]\n" +
-            "  └ Collect[doc.c | [z] | true]"
         );
     }
 
@@ -547,6 +543,43 @@ public class EliminateCrossJoinTest extends CrateDummyClusterServiceUnitTest {
             "  └ Join[INNER | (x = y)]",
             "    ├ Collect[doc.b | [y] | true]",
             "    └ Collect[doc.a | [x] | true]"
+        );
+    }
+
+
+    @Test
+    public void test_eliminate_cross_joins_with_remaining_cross_joins() throws Exception {
+        var join1 = new JoinPlan(a, b, JoinType.CROSS, null);
+        var join2 = new JoinPlan(join1, c, JoinType.CROSS, null);
+        var join3 = new JoinPlan(join2, d, JoinType.INNER, e.asSymbol("a.x = b.y AND c.z = d.w"));
+
+        assertThat(join3).hasOperators(
+            "Join[INNER | ((x = y) AND (z = w))]",
+            "  ├ Join[CROSS]",
+            "  │  ├ Join[CROSS]",
+            "  │  │  ├ Collect[doc.a | [x] | true]",
+            "  │  │  └ Collect[doc.b | [y] | true]",
+            "  │  └ Collect[doc.c | [z] | true]",
+            "  └ Collect[doc.d | [w] | true]"
+        );
+
+        var rule = new EliminateCrossJoin();
+        var match = rule.pattern().accept(join3, Captures.empty());
+
+        assertThat(match.isPresent()).isTrue();
+
+        var result = rule.apply(match.value(),
+            match.captures(),
+            e.ruleContext());
+
+        assertThat(result).hasOperators(
+            "Join[INNER | (z = w)]",
+            "  ├ Join[CROSS]",
+            "  │  ├ Join[INNER | (x = y)]",
+            "  │  │  ├ Collect[doc.a | [x] | true]",
+            "  │  │  └ Collect[doc.b | [y] | true]",
+            "  │  └ Collect[doc.c | [z] | true]",
+            "  └ Collect[doc.d | [w] | true]"
         );
     }
 }

server/src/main/java/io/crate/planner/operators/JoinPlan.java
@@ -46,6 +46,7 @@ public class JoinPlan extends AbstractJoinPlan {
     private final boolean rewriteFilterOnOuterJoinToInnerJoinDone;
     private final boolean lookUpJoinRuleApplied;
     private final boolean moveConstantJoinConditionRuleApplied;
+    private final boolean eliminateCrossJoinRuleIsApplied;
 
     public JoinPlan(LogicalPlan lhs,
                     LogicalPlan rhs,
@@ -55,7 +56,7 @@ public class JoinPlan extends AbstractJoinPlan {
                     boolean rewriteFilterOnOuterJoinToInnerJoinDone,
                     boolean lookUpJoinRuleApplied,
                     LookUpJoin lookUpJoin) {
-        this(lhs, rhs, joinType, joinCondition, isFiltered, rewriteFilterOnOuterJoinToInnerJoinDone, lookUpJoinRuleApplied, false, lookUpJoin);
+        this(lhs, rhs, joinType, joinCondition, isFiltered, rewriteFilterOnOuterJoinToInnerJoinDone, lookUpJoinRuleApplied, false, false, lookUpJoin);
     }
 
     @VisibleForTesting
@@ -63,7 +64,7 @@ public class JoinPlan extends AbstractJoinPlan {
                     LogicalPlan rhs,
                     JoinType joinType,
                     @Nullable Symbol joinCondition) {
-        this(lhs, rhs, joinType, joinCondition, false, false, false, false, LookUpJoin.NONE);
+        this(lhs, rhs, joinType, joinCondition, false, false, false, false, false, LookUpJoin.NONE);
     }
 
     public JoinPlan(LogicalPlan lhs,
@@ -74,12 +75,14 @@ public class JoinPlan extends AbstractJoinPlan {
                      boolean rewriteFilterOnOuterJoinToInnerJoinDone,
                      boolean lookUpJoinRuleApplied,
                      boolean moveConstantJoinConditionRuleApplied,
+                     boolean eliminateCrossJoinRuleIsApplied,
                      LookUpJoin lookUpJoin) {
         super(lhs, rhs, joinCondition, joinType, lookUpJoin);
         this.isFiltered = isFiltered;
         this.rewriteFilterOnOuterJoinToInnerJoinDone = rewriteFilterOnOuterJoinToInnerJoinDone;
         this.lookUpJoinRuleApplied = lookUpJoinRuleApplied;
         this.moveConstantJoinConditionRuleApplied = moveConstantJoinConditionRuleApplied;
+        this.eliminateCrossJoinRuleIsApplied = eliminateCrossJoinRuleIsApplied;
     }
 
     public boolean isLookUpJoinRuleApplied() {
@@ -98,6 +101,10 @@ public class JoinPlan extends AbstractJoinPlan {
         return moveConstantJoinConditionRuleApplied;
     }
 
+    public boolean eliminateCrossJoinRuleIsApplied() {
+        return eliminateCrossJoinRuleIsApplied;
+    }
+
     public JoinPlan withMoveConstantJoinConditionRuleApplied(boolean moveConstantJoinConditionRuleApplied) {
         return new JoinPlan(
             lhs,
@@ -108,6 +115,7 @@ public class JoinPlan extends AbstractJoinPlan {
             rewriteFilterOnOuterJoinToInnerJoinDone,
             lookUpJoinRuleApplied,
             moveConstantJoinConditionRuleApplied,
+            eliminateCrossJoinRuleIsApplied,
             lookupJoin
         );
     }
@@ -158,6 +166,7 @@ public class JoinPlan extends AbstractJoinPlan {
             rewriteFilterOnOuterJoinToInnerJoinDone,
             lookUpJoinRuleApplied,
             moveConstantJoinConditionRuleApplied,
+            eliminateCrossJoinRuleIsApplied,
             lookupJoin
         );
     }
@@ -188,6 +197,7 @@ public class JoinPlan extends AbstractJoinPlan {
             rewriteFilterOnOuterJoinToInnerJoinDone,
             lookUpJoinRuleApplied,
             moveConstantJoinConditionRuleApplied,
+            eliminateCrossJoinRuleIsApplied,
             lookupJoin
         );
     }

server/src/main/java/io/crate/planner/optimizer/rule/EliminateCrossJoin.java
@@ -30,8 +30,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.PriorityQueue;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.Nullable;
 
 import io.crate.exceptions.InvalidArgumentException;
@@ -51,8 +49,8 @@ import io.crate.sql.tree.JoinType;
 
 public class EliminateCrossJoin implements Rule<JoinPlan> {
 
-    private static final Logger LOGGER = LogManager.getLogger(EliminateCrossJoin.class);
-    private final Pattern<JoinPlan> pattern = typeOf(JoinPlan.class);
+    private final Pattern<JoinPlan> pattern = typeOf(JoinPlan.class)
+        .with(j -> j.eliminateCrossJoinRuleIsApplied() == false);
 
     @Override
     public Pattern<JoinPlan> pattern() {
@@ -157,25 +155,27 @@ public class EliminateCrossJoin implements Rule<JoinPlan> {
                 }
             }
 
+            final JoinType joinType;
+            final Symbol joinCondition;
+
             if (criteria.isEmpty()) {
-                var errorMessage = new ArrayList<String>();
-                for (var plan : order) {
-                    for (var relationName : plan.relationNames()) {
-                        errorMessage.add(relationName.fqn());
-                    }
-                }
-                LOGGER.trace("JoinPlan cannot be built with the provided order {}", errorMessage);
-                return null;
+                joinType = JoinType.CROSS;
+                joinCondition = null;
+            } else {
+                joinType = JoinType.INNER;
+                joinCondition = AndOperator.join(criteria, null);
             }
 
             result = new JoinPlan(
                 result,
                 rightNode,
-                JoinType.INNER,
-                AndOperator.join(criteria, null),
+                joinType,
+                joinCondition,
                 false,
                 false,
                 false,
+                false,
+                true,
                 AbstractJoinPlan.LookUpJoin.NONE
             );
         }

server/src/main/java/io/crate/planner/optimizer/rule/MoveConstantJoinConditionsBeneathJoin.java
@@ -136,6 +136,7 @@ public class MoveConstantJoinConditionsBeneathJoin implements Rule<JoinPlan> {
                 joinPlan.isRewriteFilterOnOuterJoinToInnerJoinDone(),
                 joinPlan.isLookUpJoinRuleApplied(),
                 true,
+                joinPlan.eliminateCrossJoinRuleIsApplied(),
                 joinPlan.lookUpJoin()
             );
         } else {

commit e1cfcdc300e494e275af02d86dec7c314bc437a6

Ensure that JoinGraph adds only edges for equi-join conditions
Fixes a regression, introduced with https://github.com/crate/crate/commit/1ece4b40e51ea2b95af0ac8894318cba5359bbda

In fact, referenced commit only revealed issue with join graphs construction as it enabled rule being applied for broken graphs.

The root cause is JoinGraph is not fulfilling the contract declared in it
>  we are only interested in equi-join conditions

Solution is to use existing isEquiJoin check before adding an edge.
EquiJoinDetector can handle exactly the same JOIN ON CASE condition,
it was done in https://github.com/crate/crate/commit/d85eba28a369253079aca3676f102996e75fbc96

With an empty graph,
EliminateCrossJoin hits joinGraph.edges().isEmpty() check and gets short-circuited

server/src/main/java/io/crate/planner/optimizer/joinorder/JoinGraph.java
@@ -21,6 +21,8 @@
 
 package io.crate.planner.optimizer.joinorder;
 
+import static io.crate.planner.operators.EquiJoinDetector.isEquiJoin;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
@@ -189,7 +191,7 @@ public record JoinGraph(List<LogicalPlan> nodes,
                     // (a,b) -> (a.x = b.y) and we can ignore any other
                     // filters. Therefore, we only want entries where we have
                     // two keys.
-                    if (entry.getKey().size() == 2) {
+                    if (entry.getKey().size() == 2 && isEquiJoin(entry.getValue())) {
                         entry.getValue().accept(edgeCollector, context);
                     } else {
                         filters.add(entry.getValue());

server/src/test/java/io/crate/integrationtests/JoinIntegrationTest.java
@@ -1835,4 +1835,21 @@ public class JoinIntegrationTest extends IntegTestCase {
         execute(query);
         assertThat(response.rows()).isEmpty();
     }
+
+
+    @Test
+    @UseRandomizedSchema(random = false)
+    public void test_do_not_apply_eliminate_cross_join_for_non_equi_join() throws Exception {
+        execute("CREATE TABLE t0(c0 BOOLEAN);");
+        execute("CREATE TABLE t1(c0 VARCHAR);");
+
+        execute("INSERT INTO t1(c0) VALUES ('1');");
+        execute("INSERT INTO t0(c0) VALUES (false);");
+
+        execute("refresh table t0");
+        execute("refresh table t1");
+        execute("SELECT * FROM t1, t0 " +
+            "INNER JOIN (SELECT 1 AS col0) AS sub0 ON (CASE sub0.col0 WHEN sub0.col0 THEN t0.c0 ELSE false END)");
+        assertThat(response.rowCount()).isEqualTo(0);
+    }
 }

server/src/test/java/io/crate/planner/optimizer/rule/EliminateCrossJoinTest.java
@@ -44,8 +44,10 @@ import io.crate.metadata.table.Operation;
 import io.crate.planner.operators.Collect;
 import io.crate.planner.operators.Filter;
 import io.crate.planner.operators.JoinPlan;
+import io.crate.planner.operators.NestedLoopJoin;
 import io.crate.planner.operators.Order;
 import io.crate.planner.operators.Rename;
+import io.crate.planner.operators.RootRelationBoundary;
 import io.crate.planner.operators.Union;
 import io.crate.planner.optimizer.joinorder.JoinGraph;
 import io.crate.planner.optimizer.matcher.Captures;
@@ -581,5 +583,40 @@ public class EliminateCrossJoinTest extends CrateDummyClusterServiceUnitTest {
             "  │  └ Collect[doc.c | [z] | true]",
             "  └ Collect[doc.d | [w] | true]"
         );
+    }
+
+    @Test
+    public void test_do_not_apply_eliminate_cross_join_for_non_equi_join() throws Exception {
+        // Using a dummy query that generates exactly same Rename as we need to avoid building Rename manually.
+        // This query also generates condition that we can use for join expression.
+        // Smth like e.asSymbol("CASE sub0.col0 WHEN sub0.col0 THEN b.y ELSE false END")
+        // wouldn't work as sub0.col0 is unknown, and we would have to build it manually otherwise.
+
+        // We cannot create a plan for the whole query that is actually being tested
+        // as capturing requires JoinPlan and planner would create NestedLoopJoin.
+        // We manually build JoinPlan out of parts that we got from dummy query (solely to simplify construction).
+        RootRelationBoundary plan = e.logicalPlan("""
+            SELECT * FROM a, b, (SELECT 1 AS col0) AS sub0
+            WHERE (CASE sub0.col0 WHEN sub0.col0 THEN b.y ELSE false END)
+            """
+        );
+        Filter filter = ((Filter) plan.source());
+        Rename rename = (Rename) ((NestedLoopJoin) filter.source()).rhs();
+        Symbol joinCondition = filter.query();
+
+        // Building plan for a query:
+        // SELECT * FROM a, b
+        // INNER JOIN (SELECT 1 AS col0) AS sub0 ON (CASE sub0.col0 WHEN sub0.col0 THEN b.y ELSE false END)
+        var join1 = new JoinPlan(a, b, JoinType.CROSS, null);
+        var join2 = new JoinPlan(join1, rename, JoinType.INNER, joinCondition);
+
+        JoinGraph joinGraph = JoinGraph.create(join2, UnaryOperator.identity());
+        assertThat(joinGraph.edges()).isEmpty(); // No equi join condition, no edges.
+
+        var rule = new EliminateCrossJoin();
+        var match = rule.pattern().accept(join2, Captures.empty());
+        assertThat(match.isPresent()).isTrue();
+        var result = rule.apply(match.value(), match.captures(), e.ruleContext());
+        assertThat(result).isNull();
     }
 }