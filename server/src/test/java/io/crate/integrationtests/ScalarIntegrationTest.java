crate/crate

commit 88e23272403a17f70d7625019cc923674c4a287e

Try to use optimized subscript function with concrete return type
Try to detect the data type a subscript function will return an
allocate the function directly with this return type.
This is only possible if the subscript path value is known
(is a literal expression), such this does not work with the
generic function matching as this is working purely on argument
data types and not values.

This is especially needed if a subscript's return type is used
as the input to any other expression, e.g. a nested subscript
call. Without the concrete return type, the sub-sequent call
receives an undefined type and any column policy based
error handling isn't possible anymore.

Related to #14828.

server/src/main/java/io/crate/expression/scalar/SubscriptFunction.java
@@ -55,6 +55,8 @@ import io.crate.metadata.Scalar;
 import io.crate.metadata.TransactionContext;
 import io.crate.metadata.functions.BoundSignature;
 import io.crate.metadata.functions.Signature;
+import io.crate.sql.tree.ColumnPolicy;
+import io.crate.types.ArrayType;
 import io.crate.types.DataType;
 import io.crate.types.DataTypes;
 import io.crate.types.EqQuery;
@@ -75,24 +77,40 @@ public class SubscriptFunction extends Scalar<Object, Object> {
 
     public static final String NAME = "subscript";
 
+    public static final Signature SIGNATURE_ARRAY_OF_OBJECTS =
+        Signature.builder(NAME, FunctionType.SCALAR)
+            .argumentTypes(
+                TypeSignature.parse("array(object)"),
+                DataTypes.STRING.getTypeSignature())
+            .returnType(TypeSignature.parse("array(undefined)"))
+            .features(Feature.DETERMINISTIC)
+            .forbidCoercion()
+            .build();
+
+    public static final Signature SIGNATURE_OBJECT =
+        Signature.builder(NAME, FunctionType.SCALAR)
+            .argumentTypes(
+                DataTypes.UNTYPED_OBJECT.getTypeSignature(),
+                DataTypes.STRING.getTypeSignature())
+            .returnType(DataTypes.UNDEFINED.getTypeSignature())
+            .features(Feature.DETERMINISTIC)
+            .forbidCoercion()
+            .build();
+
+
+
     public static void register(Functions.Builder module) {
         // All signatures but `array[int]` must forbid coercion.
-        // Otherwise they would also match for non-int numeric indices like e.g. `array[long]`
+        // Otherwise, they would also match for non-int numeric indices like e.g. `array[long]`
 
         // subscript(array(object)), text) -> array(undefined)
         module.add(
-            Signature.builder(NAME, FunctionType.SCALAR)
-                .argumentTypes(TypeSignature.parse("array(object)"),
-                    DataTypes.STRING.getTypeSignature())
-                .returnType(TypeSignature.parse("array(undefined)"))
-                .features(Feature.DETERMINISTIC)
-                .forbidCoercion()
-                .build(),
+            SIGNATURE_ARRAY_OF_OBJECTS,
             (signature, boundSignature) ->
                 new SubscriptFunction(
                     signature,
                     boundSignature,
-                    SubscriptFunction::lookupIntoListObjectsByName
+                    SubscriptFunction::lookupByName
                 )
         );
         // subscript(array(any)), integer) -> any
@@ -113,13 +131,7 @@ public class SubscriptFunction extends Scalar<Object, Object> {
         );
         // subscript(object(text, element), text) -> undefined
         module.add(
-            Signature.builder(NAME, FunctionType.SCALAR)
-                .argumentTypes(DataTypes.UNTYPED_OBJECT.getTypeSignature(),
-                    DataTypes.STRING.getTypeSignature())
-                .returnType(DataTypes.UNDEFINED.getTypeSignature())
-                .features(Feature.DETERMINISTIC)
-                .forbidCoercion()
-                .build(),
+            SIGNATURE_OBJECT,
             (signature, boundSignature) ->
                 new SubscriptFunction(
                     signature,
@@ -188,16 +200,6 @@ public class SubscriptFunction extends Scalar<Object, Object> {
         return lookup.apply(boundSignature.argTypes(), element, index, txnCtx.sessionSettings().errorOnUnknownObjectKey());
     }
 
-    static Object lookupIntoListObjectsByName(List<DataType<?>> argTypes, Object base, Object name, boolean errorOnUnknownObjectKey) {
-        List<?> values = (List<?>) base;
-        List<Object> result = new ArrayList<>(values.size());
-        for (int i = 0; i < values.size(); i++) {
-            Map<?, ?> map = (Map<?, ?>) values.get(i);
-            result.add(map.get(name));
-        }
-        return result;
-    }
-
     static Object lookupByNumericIndex(List<DataType<?>> argTypes, Object base, Object index, boolean errorOnUnknownObjectKey) {
         List<?> values = (List<?>) base;
         int idx = ((Number) index).intValue();
@@ -209,26 +211,32 @@ public class SubscriptFunction extends Scalar<Object, Object> {
     }
 
     static Object lookupByName(List<DataType<?>> argTypes, Object base, Object name, boolean errorOnUnknownObjectKey) {
-        if (!(base instanceof Map<?, ?> map)) {
-            if (base instanceof List<?> list) {
-                List<Object> result = new ArrayList<>(list.size());
-                for (Object item : list) {
-                    result.add(lookupByName(argTypes, item, name, errorOnUnknownObjectKey));
-                }
-                return result;
+        DataType<?> baseType = argTypes.getFirst();
+        if (base instanceof List<?> list) {
+            List<Object> result = new ArrayList<>(list.size());
+            for (Object item : list) {
+                ArrayList<DataType<?>> unnestedArgTypes = new ArrayList<>();
+                unnestedArgTypes.add(ArrayType.unnest(baseType));
+                unnestedArgTypes.add(argTypes.get(1));
+                result.add(lookupByName(unnestedArgTypes, item, name, errorOnUnknownObjectKey));
             }
-            throw new IllegalArgumentException("Base argument to subscript must be an object, not " + base);
-        } else if (errorOnUnknownObjectKey && !map.containsKey(name)) {
-            DataType<?> objectArgType = argTypes.get(0);
-            // Type could also be "undefined"
-            if (objectArgType instanceof ObjectType objType) {
-                if (objType.innerTypes().containsKey(name)) {
+            return result;
+        } else if (base instanceof Map<?, ?> map) {
+            if (!map.containsKey(name)) {
+                ColumnPolicy columnPolicy = baseType.columnPolicy();
+                assert baseType instanceof ObjectType;
+                ObjectType objType = (ObjectType) baseType;
+                if (columnPolicy == ColumnPolicy.IGNORED
+                    || (columnPolicy == ColumnPolicy.DYNAMIC && !errorOnUnknownObjectKey)
+                    || objType.innerTypes().containsKey(name)) {
                     return null;
                 }
+                throw ColumnUnknownException.ofUnknownRelation("The object `" + base + "` does not contain the key `" + name + "`");
+            } else {
+                return map.get(name);
             }
-            throw ColumnUnknownException.ofUnknownRelation("The object `" + base + "` does not contain the key `" + name + "`");
         } else {
-            return map.get(name);
+            throw new IllegalArgumentException("Base argument to subscript must be an object or array-of-objects, not " + base);
         }
     }
 

server/src/test/java/io/crate/integrationtests/ScalarIntegrationTest.java
@@ -53,10 +53,11 @@ public class ScalarIntegrationTest extends IntegTestCase {
                         session))
                 .isExactlyInstanceOf(ColumnUnknownException.class)
                 .hasMessageContaining("The object `{y=2, z=3}` does not contain the key `x`");
-            // This is documenting a bug. If this fails, it is a breaking change.
-            var response = sqlExecutor.exec("SELECT [unnest]['x'] FROM UNNEST(['{\"x\":1,\"y\":2}','{\"y\":2,\"z\":3}']::ARRAY(OBJECT))",
-                            session);
-            assertThat(TestingHelpers.printedTable(response.rows())).isEqualTo("[1]\n[NULL]\n");
+            Assertions.assertThatThrownBy(() -> sqlExecutor.exec(
+                    "SELECT [unnest]['x'] FROM UNNEST(['{\"x\":1,\"y\":2}','{\"y\":2,\"z\":3}']::ARRAY(OBJECT))",
+                    session))
+                .isExactlyInstanceOf(ColumnUnknownException.class)
+                .hasMessageContaining("The object `{y=2, z=3}` does not contain the key `x`");
         }
 
         try (var session2 = sqlExecutor.newSession()) {

extensions/lang-js/src/test/java/io/crate/operation/language/JavaScriptUDFIntegrationTest.java
@@ -22,6 +22,7 @@
 package io.crate.operation.language;
 
 import static io.crate.testing.Asserts.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -33,6 +34,7 @@ import org.elasticsearch.test.IntegTestCase;
 import org.junit.Test;
 
 import io.crate.module.JavaScriptLanguageModule;
+import io.crate.session.Session;
 import io.crate.types.ArrayType;
 import io.crate.types.DataTypes;
 
@@ -150,6 +152,65 @@ public class JavaScriptUDFIntegrationTest extends IntegTestCase {
             assertThat(response).hasRows("1");
         } finally {
             execute("drop function shapeudf()");
+        }
+    }
+
+    @Test
+    public void test_subscript_on_udf_returns_correct_type_or_error() {
+        execute("CREATE FUNCTION my_func_dynamic()" +
+            "    RETURNS OBJECT(DYNAMIC) AS (x LONG)" +
+            "    LANGUAGE JAVASCRIPT" +
+            "    AS $$" +
+            "    function my_func_dynamic(){" +
+            "      return {x:1};" +
+            "    };$$");
+        execute("CREATE FUNCTION my_func_dynamic_untyped()" +
+            "    RETURNS OBJECT(DYNAMIC)" +
+            "    LANGUAGE JAVASCRIPT" +
+            "    AS $$" +
+            "    function my_func_dynamic_untyped(){" +
+            "      return {x:1};" +
+            "    };$$");
+        execute("CREATE FUNCTION my_func_ignored()" +
+            "    RETURNS OBJECT(IGNORED) AS (x LONG)" +
+            "    LANGUAGE JAVASCRIPT" +
+            "    AS $$" +
+            "    function my_func_ignored(){" +
+            "      return {x:1};" +
+            "    };$$");
+        execute("CREATE FUNCTION my_func_ignored_untyped()" +
+            "    RETURNS OBJECT(IGNORED)" +
+            "    LANGUAGE JAVASCRIPT" +
+            "    AS $$" +
+            "    function my_func_ignored_untyped(){" +
+            "      return {x:1};" +
+            "    };$$");
+
+        // Field exists
+        // DYNAMIC with defined type
+        execute("SELECT pg_typeof(my_func_dynamic()['x'])");
+        assertThat(response).hasRows("bigint");
+        // DYNAMIC empty type definition, type determined dynamically
+        execute("SELECT pg_typeof(my_func_dynamic_untyped()['x'])");
+        assertThat(response).hasRows("integer");
+        // IGNORED with empty type definition, type determined dynamically
+        execute("SELECT pg_typeof(my_func_ignored_untyped()['x'])");
+        assertThat(response).hasRows("integer");
+
+        // Field does not exist
+        // DYNAMIC
+        assertThatThrownBy(() -> execute("SELECT my_func_dynamic()['y']"))
+            .hasMessageContaining("The object `{x=1}` does not contain the key `y`");
+        // IGNORED
+        execute("SELECT my_func_ignored()['y']");
+        assertThat(response).hasRows("NULL");
+
+        try (Session session = sqlExecutor.newSession()) {
+            execute("SET SESSION error_on_unknown_object_key=false", session);
+
+            // DYNAMIC
+            execute("SELECT my_func_dynamic()['y']", session);
+            assertThat(response).hasRows("NULL");
         }
     }
 }

server/src/main/java/io/crate/analyze/expressions/ExpressionAnalyzer.java
@@ -680,9 +680,17 @@ public class ExpressionAnalyzer {
 
             if (subscriptContext.hasExpression()) {
                 // The left hand side of the expression isn't a column, it's something like a
-                // static array or a cast, so we recurse into it.
+                // static array, function or a cast, so we recurse into it.
                 Symbol base = node.base().accept(this, context);
                 Symbol index = node.index().accept(this, context);
+                if (index.valueType() == DataTypes.STRING) {
+                    // If the index is a string, we can try to optimize the subscript function
+                    // to avoid the function call and directly access the object key.
+                    Function optimizedSubscript = optimizedSubscriptFunction(base, index, subscriptContext.parts(), context, null);
+                    if (optimizedSubscript != null) {
+                        return optimizedSubscript;
+                    }
+                }
                 return allocateFunction(SubscriptFunction.NAME, List.of(base, index), context);
             }
 
@@ -699,6 +707,8 @@ public class ExpressionAnalyzer {
             // - We want to avoid subscript functions if possible (we've nested object values in a column store)
             // - In DDL statement we can't turn a `PRIMARY KEY o['x']` into a subscript either
             // - In DML statements we can have assignments: obj['x'] = 30
+            // - We want a good error message if the subscript is invalid including the column name and relation. This
+            //   is not possible with the subscript function as it misses all these information.
             // We should come up with a design that addresses those and remove the duct-tape logic below.
 
             Symbol ref;
@@ -734,43 +744,71 @@ public class ExpressionAnalyzer {
                     List.of(),
                     operation,
                     context.errorOnUnknownObjectKey());
-                DataType<?> baseType = base.valueType();
-                // Need to double-check that the base type does not hold the inner type before throwing. For instance,
-                //     create table t (o array(object as (a int)));
-                //     select col['a'] from (select unnest(o) as col from t) t_alias;
-                // `col['a']` cannot be resolved(due to unnest), although we know o['a'] exists. Here `col`(the base)
-                // is resolved to a ScopedSymbol which holds `a` as the inner type.
-                // There are test cases that fail without checking inner types:
-                //     SysSnapshotsTest.test_sys_snapshots_returns_table_partition_information
-                //     AlterTableIntegrationTest.test_alter_table_drop_leaf_subcolumn_with_parent_object_array
-                // Another example is selecting sub-column of an object that is union-ed.
-                if (baseType != UndefinedType.INSTANCE
-                    && baseType != ObjectType.UNTYPED
-                    && !DataTypes.hasPath(baseType, parts)) {
-                    DataType<?> currentType = baseType;
-                    ColumnPolicy parentPolicy = baseType.columnPolicy();
-                    for (String p : parts) {
-                        if (ArrayType.unnest(currentType) instanceof ObjectType objectType) {
-                            parentPolicy = objectType.columnPolicy();
-                            currentType = objectType.innerType(p);
-                        }
-                    }
-                    if (parentPolicy == ColumnPolicy.STRICT ||
-                        (parentPolicy == ColumnPolicy.DYNAMIC && context.errorOnUnknownObjectKey())) {
-                        throw e;
-                    }
+                Symbol index = node.index().accept(this, context);
+                Function optimizedSubscript = optimizedSubscriptFunction(base, index, parts, context, e);
+                if (optimizedSubscript != null) {
+                    return optimizedSubscript;
                 }
+
                 return allocateFunction(
                     SubscriptFunction.NAME,
-                    List.of(
-                        node.base().accept(this, context),
-                        node.index().accept(this, context)
-                    ),
+                    List.of(base, index),
                     context
                 );
             } catch (ColumnUnknownException e2) {
                 throw e;
             }
+        }
+
+        /**
+         * Tries to detect the subscript return type based on the given (typed) base type (object or arrayOfObjects).
+         *
+         * @return Subscript function with concrete return type or NULL if it cannot be detected.
+         * @throws ColumnUnknownException if the wanted column cannot be found and the parent policy condition requires
+         *         an error to be thrown. Only thrown if the exception is passed in as an argument. Otherwise, it falls
+         *         through and relies on the SubscriptFunction to throw the error.
+         */
+        @Nullable
+        private Function optimizedSubscriptFunction(Symbol base,
+                                                    Symbol index,
+                                                    List<String> path,
+                                                    ExpressionAnalysisContext context,
+                                                    @Nullable ColumnUnknownException e) {
+            if (path.isEmpty()) {
+                return null;
+            }
+            DataType<?> baseType = base.valueType();
+            DataType<?> innerType = DataTypes.innerType(baseType, path);
+            if (innerType == null) {
+                // Not an object or array of objects
+                return null;
+            }
+            if (innerType != UndefinedType.INSTANCE && !DataTypes.isArrayOfNulls(innerType)) {
+                Signature signature = SubscriptFunction.SIGNATURE_OBJECT;
+                if (baseType instanceof ArrayType<?>) {
+                    signature = SubscriptFunction.SIGNATURE_ARRAY_OF_OBJECTS;
+                }
+                return new Function(
+                    signature,
+                    List.of(base, index),
+                    innerType
+                );
+            }
+            DataType<?> currentType = baseType;
+            ColumnPolicy parentPolicy = baseType.columnPolicy();
+            for (String p : path) {
+                if (ArrayType.unnest(currentType) instanceof ObjectType objectType) {
+                    parentPolicy = objectType.columnPolicy();
+                    currentType = objectType.innerType(p);
+                }
+            }
+            if (parentPolicy == ColumnPolicy.STRICT ||
+                (parentPolicy == ColumnPolicy.DYNAMIC && context.errorOnUnknownObjectKey())) {
+                if (e != null) {
+                    throw e;
+                }
+            }
+            return null;
         }
 
         @Override

server/src/main/java/io/crate/expression/scalar/SubscriptObjectFunction.java
@@ -38,9 +38,11 @@ import io.crate.metadata.Scalar;
 import io.crate.metadata.TransactionContext;
 import io.crate.metadata.functions.BoundSignature;
 import io.crate.metadata.functions.Signature;
+import io.crate.types.ArrayType;
 import io.crate.types.DataType;
 import io.crate.types.DataTypes;
 import io.crate.types.ObjectType;
+import io.crate.types.UndefinedType;
 
 /**
  * Scalar function to resolve elements inside a map.
@@ -120,16 +122,31 @@ public class SubscriptObjectFunction extends Scalar<Object, Map<String, Object>>
     public final Object evaluate(TransactionContext txnCtx, NodeContext ndeCtx, Input<Map<String, Object>>... args) {
         assert args.length >= 2 : NAME + " takes 2 or more arguments, got " + args.length;
         Object mapValue = args[0].value();
+        ObjectType objectType = (ObjectType) boundSignature.argTypes().getFirst();
         for (var i = 1; i < args.length; i++) {
             if (mapValue == null) {
                 return null;
             }
+            String innerValue = DataTypes.STRING.sanitizeValue(args[i].value());
             mapValue = SubscriptFunction.lookupByName(
-                boundSignature.argTypes(),
+                List.of(objectType, DataTypes.STRING),
                 mapValue,
-                args[i].value(),
+                innerValue,
                 txnCtx.sessionSettings().errorOnUnknownObjectKey()
             );
+            if (mapValue == null) {
+                return null;
+            }
+            DataType<?> innerType = ArrayType.unnest(objectType.innerType(innerValue));
+            if (innerType instanceof ObjectType objectType1) {
+                objectType = objectType1;
+            } else if (i < args.length - 1) {
+                assert innerType instanceof UndefinedType;
+                // Returned innerType is already undefined, but we have more arguments
+                // Create an empty object based on the parent policy to continue the lookup as we need to call
+                // SubscriptFunction.lookupByName to honor the error handling.
+                objectType = ObjectType.of(objectType.columnPolicy()).build();
+            }
         }
         return mapValue;
     }

server/src/main/java/io/crate/metadata/Scalar.java
@@ -34,6 +34,8 @@ import io.crate.lucene.FunctionToQuery;
 import io.crate.metadata.functions.BoundSignature;
 import io.crate.metadata.functions.Signature;
 import io.crate.role.Roles;
+import io.crate.types.DataType;
+import io.crate.types.DataTypes;
 
 /**
  * Base class for Scalar functions in crate.
@@ -145,7 +147,12 @@ public abstract class Scalar<ReturnType, InputType> implements FunctionImplement
             inputs[idx] = (Input<?>) arg;
             idx++;
         }
-        return Literal.ofUnchecked(function.valueType(), scalar.evaluate(txnCtx, nodeCtx, inputs));
+        Object result = scalar.evaluate(txnCtx, nodeCtx, inputs);
+        DataType<?> returnType = function.valueType();
+        if (returnType == DataTypes.UNDEFINED) {
+            returnType = DataTypes.guessType(result);
+        }
+        return Literal.ofUnchecked(returnType, result);
     }
 
     public enum Feature {

server/src/main/java/io/crate/types/DataTypes.java
@@ -21,7 +21,6 @@
 
 package io.crate.types;
 
-import static io.crate.metadata.table.TableInfo.IS_OBJECT_ARRAY;
 import static java.util.Map.entry;
 import static java.util.stream.Collectors.toSet;
 
@@ -633,22 +632,18 @@ public final class DataTypes {
         return ArrayType.unnest(type) == UndefinedType.INSTANCE;
     }
 
-    public static boolean hasPath(DataType<?> dataType, List<String> path) {
-        assert path.isEmpty() == false : "DataTypes.hasPath expects path to be not empty";
-        if (!IS_OBJECT_ARRAY.test(dataType) && !(dataType instanceof ObjectType)) {
-            return false;
+    @Nullable
+    public static DataType<?> innerType(DataType<?> dataType, List<String> path) {
+        boolean isArray = false;
+        if (dataType instanceof ArrayType<?> arrayType) {
+            dataType = arrayType.innerType();
+            isArray = true;
         }
-        for (int i = 0; i < path.size(); i++) {
-            dataType = ArrayType.unnest(dataType);
-            if (dataType instanceof ObjectType objectType) {
-                dataType = objectType.innerType(path.get(i));
-                if (dataType instanceof UndefinedType) {
-                    return false;
-                }
-            } else {
-                return false;
-            }
+
+        if (dataType instanceof ObjectType objectType) {
+            DataType<?> innerType = objectType.innerType(path);
+            return isArray ? new ArrayType<>(innerType) : innerType;
         }
-        return true;
+        return null;
     }
 }

server/src/main/java/io/crate/types/ObjectType.java
@@ -147,11 +147,21 @@ public class ObjectType extends DataType<Map<String, Object>> implements Streame
         }
         DataType<?> innerType = DataTypes.UNDEFINED;
         ObjectType currentObject = this;
+        int arrayNesting = 0;
         for (int i = 0; i < path.size(); i++) {
             innerType = currentObject.innerType(path.get(i));
-            if (innerType.id() == ID) {
-                currentObject = (ObjectType) innerType;
+            while (innerType instanceof ArrayType<?> arrayType) {
+                arrayNesting++;
+                innerType = arrayType.innerType();
             }
+            if (innerType instanceof ObjectType objectType) {
+                currentObject = objectType;
+            } else if (i < path.size() - 1) {
+                return DataTypes.UNDEFINED;
+            }
+        }
+        for (int i = 0; i < arrayNesting; i++) {
+            innerType = new ArrayType<>(innerType);
         }
         return innerType;
     }

commit 1a1d6804611d9ca33d524473e20b4f679accaea9

Re-add handling of UndefinedType to the SubscriptFunction
Fixes a regression introduced with https://github.com/crate/crate/commit/88e23272403a17f70d7625019cc923674c4a287e

server/src/main/java/io/crate/expression/scalar/SubscriptFunction.java
@@ -224,13 +224,13 @@ public class SubscriptFunction extends Scalar<Object, Object> {
         } else if (base instanceof Map<?, ?> map) {
             Object value = map.get(name);
             ColumnPolicy columnPolicy = baseType.columnPolicy();
-            if (value == null) {
-                assert baseType instanceof ObjectType;
-                ObjectType objType = (ObjectType) baseType;
-                if (columnPolicy == ColumnPolicy.IGNORED
-                    || (columnPolicy == ColumnPolicy.DYNAMIC && !errorOnUnknownObjectKey)
-                    || (objType.innerTypes().containsKey(name))) {
-                    return null;
+            if (value == null && errorOnUnknownObjectKey) {
+                // Type could also be "undefined"
+                if (baseType instanceof ObjectType objType) {
+                    if (columnPolicy == ColumnPolicy.IGNORED
+                        || objType.innerTypes().containsKey(name)) {
+                        return null;
+                    }
                 }
                 throw ColumnUnknownException.ofUnknownRelation("The object `" + base + "` does not contain the key `" + name + "`");
             }

server/src/test/java/io/crate/integrationtests/ScalarIntegrationTest.java
@@ -21,14 +21,15 @@
 
 package io.crate.integrationtests;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static io.crate.testing.Asserts.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import org.assertj.core.api.Assertions;
+import org.elasticsearch.common.io.stream.NotSerializableExceptionWrapper;
 import org.elasticsearch.test.IntegTestCase;
 import org.junit.Test;
 
 import io.crate.exceptions.ColumnUnknownException;
-import io.crate.testing.Asserts;
+
 import io.crate.testing.TestingHelpers;
 import io.crate.testing.UseJdbc;
 import io.crate.types.DataTypes;
@@ -49,13 +50,13 @@ public class ScalarIntegrationTest extends IntegTestCase {
     public void testSubscriptFunctionFromUnnest() {
         try (var session = sqlExecutor.newSession()) {
             session.sessionSettings().setErrorOnUnknownObjectKey(true);
-            Assertions.assertThatThrownBy(() -> sqlExecutor.exec(
+            assertThatThrownBy(() -> sqlExecutor.exec(
                         "SELECT unnest['x'] FROM UNNEST(['{\"x\":1,\"y\":2}','{\"y\":2,\"z\":3}']::ARRAY(OBJECT))",
                         session))
                 .isExactlyInstanceOf(ColumnUnknownException.class)
                 .hasMessageContaining("The object `{y=2, z=3}` does not contain the key `x`");
             // Different error due to wrapping the unnest in an array
-            Assertions.assertThatThrownBy(() -> sqlExecutor.exec(
+            assertThatThrownBy(() -> sqlExecutor.exec(
                     "SELECT [unnest]['x'] FROM UNNEST(['{\"x\":1,\"y\":2}','{\"y\":2,\"z\":3}']::ARRAY(OBJECT))",
                     session))
                 .isExactlyInstanceOf(ColumnUnknownException.class)
@@ -76,8 +77,29 @@ public class ScalarIntegrationTest extends IntegTestCase {
     @Test
     public void testCurrentDatabase() {
         execute("select * FROM (SELECT current_database()) as vt");
-        Asserts.assertThat(response)
+        assertThat(response)
             .hasColumns("current_database")
             .hasRows("crate");
+    }
+
+    @Test
+    @UseJdbc(value = 0) // To avoid usage of assertSQLError that cannot handle multiple errors
+    public void test_subscript_of_missing_column_when_object_type_cannot_be_inferred() {
+        execute("CREATE TABLE tbl (obj OBJECT(IGNORED))");
+        execute("INSERT INTO tbl VALUES ({arr = [{}]})");
+        execute("REFRESH TABLE tbl");
+
+        // Default error_on_unknown_object_key = true, must throw
+        assertThatThrownBy(() -> execute("SELECT unnest(obj['arr'])['missing'] FROM tbl"))
+            .isOfAnyClassIn(ColumnUnknownException.class, NotSerializableExceptionWrapper.class)
+            .hasMessageContaining("The object `{}` does not contain the key `missing`");
+
+
+        // Must return null
+        try (var session = sqlExecutor.newSession()) {
+            execute("SET SESSION error_on_unknown_object_key=false", session);
+            execute("SELECT unnest(obj['arr'])['missing'] FROM tbl", session);
+            assertThat(response).hasRows("NULL");
+        }
     }
 }