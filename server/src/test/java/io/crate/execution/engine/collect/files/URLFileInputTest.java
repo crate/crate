7ccc7d1b4ca02c3fe9fb51115d57aea832b05813
Varun Bhardwaj <varunbhardwajsde@gmail.com>
2026-02-04 22:00:31 +0530

    Changes done

--- server/src/test/java/io/crate/execution/engine/collect/files/URLFileInputTest.java
+++ server/src/test/java/io/crate/execution/engine/collect/files/URLFileInputTest.java
@@ -26,6 +26,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.URI;
 import java.nio.file.Path;
 
 import org.elasticsearch.test.ESTestCase;
@@ -47,5 +48,70 @@ public class URLFileInputTest extends ESTestCase {
         assertThatThrownBy(() -> input.getStream(file.toURI()))
             .isExactlyInstanceOf(FileNotFoundException.class)
             .hasMessageContaining(expectedMessage);
+    }
+
+    @Test
+    public void testProxyConfigurationIsRespected() throws IOException {
+        // This test verifies that proxy system properties are read
+        // Note: We can't easily test actual proxy connection without a real proxy server
+        // This test just ensures the code doesn't throw exceptions with proxy settings
+        
+        String originalHttpProxyHost = System.getProperty("http.proxyHost");
+        String originalHttpProxyPort = System.getProperty("http.proxyPort");
+        
+        try {
+            // Set proxy properties
+            System.setProperty("http.proxyHost", "proxy.example.com");
+            System.setProperty("http.proxyPort", "8080");
+            
+            Path tempDir = createTempDir();
+            File file = new File(tempDir.toFile(), "test_file");
+            file.createNewFile();
+            
+            URLFileInput input = new URLFileInput(file.toURI());
+            
+            // Should not throw exception even with proxy settings configured
+            // (file:// URLs don't use HTTP proxy)
+            input.getStream(file.toURI()).close();
+            
+        } finally {
+            // Restore original values
+            if (originalHttpProxyHost != null) {
+                System.setProperty("http.proxyHost", originalHttpProxyHost);
+            } else {
+                System.clearProperty("http.proxyHost");
+            }
+            if (originalHttpProxyPort != null) {
+                System.setProperty("http.proxyPort", originalHttpProxyPort);
+            } else {
+                System.clearProperty("http.proxyPort");
+            }
+        }
+    }
+
+    @Test
+    public void testNonProxyHostsBypass() throws IOException {
+        // Test that http.nonProxyHosts is respected
+        String originalNonProxyHosts = System.getProperty("http.nonProxyHosts");
+        
+        try {
+            System.setProperty("http.nonProxyHosts", "localhost|127.0.0.1|*.local");
+            
+            Path tempDir = createTempDir();
+            File file = new File(tempDir.toFile(), "test_file");
+            file.createNewFile();
+            
+            URLFileInput input = new URLFileInput(file.toURI());
+            
+            // Should work fine with nonProxyHosts configured
+            input.getStream(file.toURI()).close();
+            
+        } finally {
+            if (originalNonProxyHosts != null) {
+                System.setProperty("http.nonProxyHosts", originalNonProxyHosts);
+            } else {
+                System.clearProperty("http.nonProxyHosts");
+            }
+        }
     }
 }
