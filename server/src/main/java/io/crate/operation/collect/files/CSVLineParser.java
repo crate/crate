51180ed5c0ad570631ab6180ec5659278e973cb9
Mathias Fussenegger <f.mathias@zignar.net>
2026-03-23 10:28:40 +0100

    Fix resource leak warnings in CSVLineParser

--- server/src/main/java/io/crate/operation/collect/files/CSVLineParser.java
+++ server/src/main/java/io/crate/operation/collect/files/CSVLineParser.java
@@ -21,15 +21,6 @@
 
 package io.crate.operation.collect.files;
 
-import com.fasterxml.jackson.databind.MappingIterator;
-import com.fasterxml.jackson.databind.ObjectReader;
-import com.fasterxml.jackson.dataformat.csv.CsvMapper;
-import com.fasterxml.jackson.dataformat.csv.CsvParser;
-import io.crate.analyze.CopyFromParserProperties;
-
-import org.elasticsearch.common.xcontent.XContentBuilder;
-import org.elasticsearch.common.xcontent.json.JsonXContent;
-
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -38,20 +29,30 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
 
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.common.xcontent.json.JsonXContent;
+
+import com.fasterxml.jackson.databind.MappingIterator;
+import com.fasterxml.jackson.databind.ObjectReader;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvParser;
+
+import io.crate.analyze.CopyFromParserProperties;
+
 public class CSVLineParser {
 
     private final ByteArrayOutputStream out = new ByteArrayOutputStream();
     private final ArrayList<String> headerKeyList = new ArrayList<>();
-    private String[] columnNamesArray;
+    private String[] columnNames;
     private final List<String> targetColumns;
     private final ObjectReader csvReader;
 
     public CSVLineParser(CopyFromParserProperties properties, List<String> columns) {
         targetColumns = columns;
         if (!properties.fileHeader()) {
-            columnNamesArray = new String[targetColumns.size()];
+            columnNames = new String[targetColumns.size()];
             for (int i = 0; i < targetColumns.size(); i++) {
-                columnNamesArray[i] = targetColumns.get(i);
+                columnNames[i] = targetColumns.get(i);
             }
         }
         var mapper = new CsvMapper()
@@ -68,13 +69,13 @@ public class CSVLineParser {
     }
 
     public void parseHeader(String header) throws IOException {
-        MappingIterator<String> iterator = csvReader.readValues(header.getBytes(StandardCharsets.UTF_8));
-        iterator.readAll(headerKeyList);
-        columnNamesArray = new String[headerKeyList.size()];
+        MappingIterator<String> values = csvReader.readValues(header.getBytes(StandardCharsets.UTF_8));
+        values.readAll(headerKeyList);
+        columnNames = new String[headerKeyList.size()];
         for (int i = 0; i < headerKeyList.size(); i++) {
             String headerKey = headerKeyList.get(i);
             if (targetColumns.isEmpty() || targetColumns.contains(headerKey)) {
-                columnNamesArray[i] = headerKey;
+                columnNames[i] = headerKey;
             }
         }
         HashSet<String> keySet = new HashSet<>(headerKeyList);
@@ -85,52 +86,56 @@ public class CSVLineParser {
     }
 
     public byte[] parse(String row, long rowNumber) throws IOException {
-        MappingIterator<Object> iterator = csvReader.readValues(row.getBytes(StandardCharsets.UTF_8));
+        MappingIterator<Object> values = csvReader.readValues(row.getBytes(StandardCharsets.UTF_8));
         out.reset();
-        XContentBuilder jsonBuilder = new XContentBuilder(JsonXContent.JSON_XCONTENT, out).startObject();
-        int i = 0, j = 0;
-        while (iterator.hasNext()) {
-            if (i >= headerKeyList.size()) {
-                throw new IllegalArgumentException(String.format(Locale.ENGLISH, "Number of values exceeds " +
-                                                                "number of keys in csv file at line %d", rowNumber));
+        try (XContentBuilder jsonBuilder = new XContentBuilder(JsonXContent.JSON_XCONTENT, out)) {
+            jsonBuilder.startObject();
+            int i = 0, j = 0;
+            while (values.hasNext()) {
+                if (i >= headerKeyList.size()) {
+                    throw new IllegalArgumentException(String.format(Locale.ENGLISH, "Number of values exceeds " +
+                                                                    "number of keys in csv file at line %d", rowNumber));
+                }
+                if (columnNames.length == j || i >= columnNames.length) {
+                    break;
+                }
+                String key = columnNames[i];
+                Object value = values.next();
+                i++;
+                if (key != null) {
+                    jsonBuilder.field(key, value);
+                    j++;
+                }
             }
-            if (columnNamesArray.length == j || i >= columnNamesArray.length) {
-                break;
-            }
-            var key = columnNamesArray[i];
-            var value = iterator.next();
-            i++;
-            if (key != null) {
-                jsonBuilder.field(key, value);
-                j++;
-            }
+            jsonBuilder.endObject();
         }
-        jsonBuilder.endObject().close();
         return out.toByteArray();
     }
 
     public byte[] parseWithoutHeader(String row, long rowNumber) throws IOException {
         MappingIterator<String> iterator = csvReader.readValues(row.getBytes(StandardCharsets.UTF_8));
         out.reset();
-        XContentBuilder jsonBuilder = new XContentBuilder(JsonXContent.JSON_XCONTENT, out).startObject();
-        int i = 0;
-        while (iterator.hasNext()) {
-            if (i >= columnNamesArray.length) {
-                break;
+        try (XContentBuilder jsonBuilder = new XContentBuilder(JsonXContent.JSON_XCONTENT, out)) {
+            jsonBuilder.startObject();
+            int i = 0;
+            while (iterator.hasNext()) {
+                if (i >= columnNames.length) {
+                    break;
+                }
+                String key = columnNames[i];
+                String value = iterator.next();
+                i++;
+                if (key != null) {
+                    jsonBuilder.field(key, value);
+                }
             }
-            var key = columnNamesArray[i];
-            var value = iterator.next();
-            i++;
-            if (key != null) {
-                jsonBuilder.field(key, value);
+            if (columnNames.length > i) {
+                throw new IllegalArgumentException(String.format(Locale.ENGLISH, "Expected %d values, " +
+                                                "encountered %d at line %d. This is not allowed when there " +
+                                                "is no header provided)",columnNames.length, i, rowNumber));
             }
+            jsonBuilder.endObject();
         }
-        if (columnNamesArray.length > i) {
-            throw new IllegalArgumentException(String.format(Locale.ENGLISH, "Expected %d values, " +
-                                               "encountered %d at line %d. This is not allowed when there " +
-                                               "is no header provided)",columnNamesArray.length, i, rowNumber));
-        }
-        jsonBuilder.endObject().close();
         return out.toByteArray();
     }
 }
