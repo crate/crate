fa90791d0034d6917e42e579bbb738ab5dd2ca08
Mathias Fussenegger <f.mathias@zignar.net>
2026-04-01 08:32:23 +0200

    Set param type hints for http arguments
    Parameter placeholders default to type `undefined` unless parameter type
    hints are set. If a function has multiple overloads this could lead to
    function lookup errors because a `undefined` signature would match
    multiple/all overloads.
    
    So far the type hints were only set via the postgres protocol - if the
    client provided them.
    For HTTP they weren't set. Given we have access to the params
    immediately, we can guess the types and set them as hint.
    
    Relates to https://github.com/crate/crate/issues/19207

--- server/src/test/java/io/crate/integrationtests/AggregateExpressionIntegrationTest.java
+++ server/src/test/java/io/crate/integrationtests/AggregateExpressionIntegrationTest.java
@@ -22,6 +22,8 @@
 package io.crate.integrationtests;
 
 import static io.crate.testing.Asserts.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.util.Map;
 
 import org.elasticsearch.test.IntegTestCase;
