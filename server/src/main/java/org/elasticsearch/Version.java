1c6b4650fee0c4a08884c4efae4cdcc3246d7811
baur <baurzhansahariev@gmail.com>
2026-04-27 09:50:12 +0200

    Release 6.2.7

--- server/src/main/java/org/elasticsearch/Version.java
+++ server/src/main/java/org/elasticsearch/Version.java
@@ -225,7 +225,7 @@ public class Version implements Comparable<Version> {
     public static final Version V_6_2_4 = new Version(9_02_04_99, false, org.apache.lucene.util.Version.LUCENE_10_3_2);
     public static final Version V_6_2_5 = new Version(9_02_05_99, false, org.apache.lucene.util.Version.LUCENE_10_3_2);
     public static final Version V_6_2_6 = new Version(9_02_06_99, false, org.apache.lucene.util.Version.LUCENE_10_3_2);
-    public static final Version V_6_2_7 = new Version(9_02_07_99, true, org.apache.lucene.util.Version.LUCENE_10_3_2);
+    public static final Version V_6_2_7 = new Version(9_02_07_99, false, org.apache.lucene.util.Version.LUCENE_10_3_2);
 
     public static final Version CURRENT = V_6_2_7;
 
