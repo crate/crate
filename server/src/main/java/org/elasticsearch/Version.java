325a1c110af1d854f49b5d4cb9eb6a794b0e008e
baur <baurzhansahariev@gmail.com>
2026-04-27 10:28:40 +0200

    Bump version to 6.2.8-SNAPSHOT

--- server/src/main/java/org/elasticsearch/Version.java
+++ server/src/main/java/org/elasticsearch/Version.java
@@ -226,8 +226,9 @@ public class Version implements Comparable<Version> {
     public static final Version V_6_2_5 = new Version(9_02_05_99, false, org.apache.lucene.util.Version.LUCENE_10_3_2);
     public static final Version V_6_2_6 = new Version(9_02_06_99, false, org.apache.lucene.util.Version.LUCENE_10_3_2);
     public static final Version V_6_2_7 = new Version(9_02_07_99, false, org.apache.lucene.util.Version.LUCENE_10_3_2);
+    public static final Version V_6_2_8 = new Version(9_02_08_99, true, org.apache.lucene.util.Version.LUCENE_10_3_2);
 
-    public static final Version CURRENT = V_6_2_7;
+    public static final Version CURRENT = V_6_2_8;
 
     private static final ImmutableOpenIntMap<Version> ID_TO_VERSION;
     private static final ImmutableOpenMap<String, Version> STRING_TO_VERSION;
