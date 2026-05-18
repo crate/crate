a30f7356c86c59f53c84896ccedeba86db7f2fa5
baur <baurzhansahariev@gmail.com>
2026-05-18 08:40:17 +0200

    Bump assertBusy timeout in ensureState to 20 seconds
    Health state query can fail due to other assertion errors or exceptions
    (master not yet discovered, timeout, CoordinationStateRejectedException e.t.c)
    and those can eat up assertBusy retry time.
    
    We are often seeing pending_tasks assertion failure, so giving extra 10 seconds to the query
    to reflect the fact that pending tasks alone might need 10 seconds (see waitNoPendingTasksOnAll)
    
    (cherry picked from commit 91f428102458c671be8d96eb058b4d6fe9048c9d)

--- server/src/testFixtures/java/io/crate/testing/SQLTransportExecutor.java
+++ server/src/testFixtures/java/io/crate/testing/SQLTransportExecutor.java
@@ -610,7 +610,7 @@ public class SQLTransportExecutor {
                 x -> assertThat(x).isEmpty(),
                 x -> assertThat(x).isEqualTo(List.of("STARTED"))
             );
-        });
+        }, 20, TimeUnit.SECONDS);
     }
 
     public interface ClientProvider {
