4e0772717e418cd2490ab54fc3060e2fde9cf4d9
Marios Trivyzas <matriv@gmail.com>
2026-06-01 10:56:39 +0300

    Fix Numeric binary de-/ser for pg wire protocol
    Previously we used a char[] to reconstruct the number as string where
    the logic of positioning the . to separate the integer from them decimal
    digits was wrong. Use the code from pgjdbc `ByteConverter` but convert
    it slightly to use `ByteBuf` instead of `byte[]`. Inherit the tests from
    https://github.com/pgjdbc/pgjdbc/blob/master/pgjdbc/src/test/java/org/postgresql/util/BigDecimalByteConverterTest.java
    
    Fixes: #19415

--- server/src/main/java/io/crate/protocols/postgres/types/NumericType.java
+++ server/src/main/java/io/crate/protocols/postgres/types/NumericType.java
@@ -22,8 +22,11 @@
 package io.crate.protocols.postgres.types;
 
 import java.math.BigDecimal;
-import java.math.MathContext;
+import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
+import org.postgresql.util.ByteConverter;
 
 import io.crate.metadata.RelationLookup;
 import io.crate.types.Regproc;
@@ -36,9 +39,18 @@ class NumericType extends PGType<BigDecimal> {
     private static final int TYPE_LEN = -1;
     private static final int TYPE_MOD = -1;
 
-    private static final short DEC_DIGITS = 4;
     private static final short NUMERIC_POS = 0x0000;
     private static final short NUMERIC_NEG = 0x4000;
+    private static final BigInteger[] BI_TEN_POWERS = new BigInteger[32];
+    private static final BigInteger BI_TEN_THOUSAND = BigInteger.valueOf(10000);
+    private static final BigInteger BI_MAX_LONG = BigInteger.valueOf(Long.MAX_VALUE);
+
+    static {
+        for (int i = 0; i < BI_TEN_POWERS.length; i++) {
+            BI_TEN_POWERS[i] = BigInteger.TEN.pow(i);
+        }
+    }
+
 
     public static final NumericType INSTANCE = new NumericType();
 
@@ -71,105 +83,155 @@ class NumericType extends PGType<BigDecimal> {
         return Regproc.of("numeric_recv");
     }
 
+    /**
+     * Copied from {@link ByteConverter#numeric(BigDecimal)} but use a {@link ByteBuf}
+     */
     @Override
     public int writeAsBinary(ByteBuf buffer, BigDecimal value) {
-        // Taken from https://github.com/cockroachdb/cockroach/blob/master/pkg/sql/pgwire/types.go#L336
-        // and https://github.com/postgres/postgres/blob/master/src/backend/utils/adt/numeric.c#L6760.
-        // The number is split into chunks of DEC_DIGITS short values while leading and trailing 0's are omitted.
-        // Examples:
-        //  * 01234       -> [1234]
-        //  * 1234567     -> [0123, 4567], scale 0
-        //  * 1.234500    -> [0001, 2345], scale 4
-        //  * 1234567.12  -> [0123, 4567, 1200], scale 1
-        //  * 1234.0      -> [1234], scale 1
-        //  * 0123.45     -> [0123, 4500], scale 2
-        var digits = value.unscaledValue().toString().toCharArray();
-        int start = 0;
-        int end = digits.length;
-        while (start < end && (digits[start] == '0' || digits[start] == '-')) {
-            start++;
+        final PositiveShorts shorts = new PositiveShorts();
+        BigInteger unscaled = value.unscaledValue().abs();
+        int scale = value.scale();
+        if (unscaled.equals(BigInteger.ZERO)) {
+            final byte[] bytes = new byte[]{0, 0, -1, -1, 0, 0, 0, 0};
+            ByteConverter.int2(bytes, 6, Math.max(0, scale));
+            buffer.writeInt(bytes.length);
+            buffer.writeBytes(bytes);
+            return INT32_BYTE_SIZE + bytes.length;
         }
-        int dWeight = end - start - value.scale() - 1;
-        while (start < end && digits[end - 1] == '0') {
-            end--;
-        }
-
-        int len = end - start;
-        short weight = 0;       // Max DEC_DIGIT block index before decimal point
-        int offset = 0;         // Offset inside the first block, e.g. 234.23 has and offset of 1
-        short nDigits = 0;      // Number of DEC_DIGIT blocks
-        if (len != 0) {
-            if (dWeight >= 0) {
-                weight = (short) ((dWeight + 1 + DEC_DIGITS - 1) / DEC_DIGITS - 1);
-            } else {
-                weight = (short) (-((-dWeight - 1) / DEC_DIGITS + 1));
+        int weight = -1;
+        if (scale <= 0) {
+            //this means we have an integer
+            //adjust unscaled and weight
+            if (scale < 0) {
+                scale = Math.abs(scale);
+                //weight value covers 4 digits
+                weight += scale / 4;
+                //whatever remains needs to be incorporated to the unscaled value
+                int mod = scale % 4;
+                unscaled = unscaled.multiply(tenPower(mod));
+                scale = 0;
             }
-            offset = (weight + 1) * DEC_DIGITS - (dWeight + 1);
-            nDigits = (short)((len + offset + DEC_DIGITS - 1) / DEC_DIGITS);
-        }
-        int typeLen = 2 * (4 + nDigits);
 
-        buffer.writeInt(typeLen);
-        buffer.writeShort(nDigits);
-        buffer.writeShort(weight);
-        if (value.signum() == -1) {
-            buffer.writeShort(NUMERIC_NEG);
+            while (unscaled.compareTo(BI_MAX_LONG) > 0) {
+                final BigInteger[] pair = unscaled.divideAndRemainder(BI_TEN_THOUSAND);
+                unscaled = pair[0];
+                final short shortValue = pair[1].shortValue();
+                if (shortValue != 0 || shorts.isNotEmpty()) {
+                    shorts.push(shortValue);
+                }
+                ++weight;
+            }
+            long unscaledLong = unscaled.longValueExact();
+            do {
+                final short shortValue = (short) (unscaledLong % 10000);
+                if (shortValue != 0 || shorts.isNotEmpty()) {
+                    shorts.push(shortValue);
+                }
+                unscaledLong = unscaledLong / 10000L;
+                ++weight;
+            } while (unscaledLong != 0);
         } else {
-            buffer.writeShort(NUMERIC_POS);
-        }
-        buffer.writeShort(value.scale());
+            final BigInteger[] split = unscaled.divideAndRemainder(tenPower(scale));
+            BigInteger decimal = split[1];
+            BigInteger wholes = split[0];
+            if (!BigInteger.ZERO.equals(decimal)) {
+                int mod = scale % 4;
+                int segments = scale / 4;
+                if (mod != 0) {
+                    decimal = decimal.multiply(tenPower(4 - mod));
+                    ++segments;
+                }
+                do {
+                    final BigInteger[] pair = decimal.divideAndRemainder(BI_TEN_THOUSAND);
+                    decimal = pair[0];
+                    final short shortValue = pair[1].shortValue();
+                    if (shortValue != 0 || shorts.isNotEmpty()) {
+                        shorts.push(shortValue);
+                    }
+                    --segments;
+                } while (!BigInteger.ZERO.equals(decimal));
 
-        int digitIdx = -offset + start;
-        while (nDigits-- > 0) {
-            short ndigit = 0;
-            // Encode 4 digits into a 16 bit short value
-            for (var nextDigitIdx = digitIdx + DEC_DIGITS; digitIdx < nextDigitIdx; digitIdx++) {
-                ndigit *= 10;
-                if (digitIdx >= start && digitIdx < end) {
-                    ndigit += (short) (digits[digitIdx] - '0');
+                //for the leading 0 shorts we either adjust weight (if no wholes)
+                // or push shorts
+                if (BigInteger.ZERO.equals(wholes)) {
+                    weight -= segments;
+                } else {
+                    //now add leading 0 shorts
+                    for (int i = 0; i < segments; i++) {
+                        shorts.push((short) 0);
+                    }
                 }
             }
-            buffer.writeShort(ndigit);
+
+            while (!BigInteger.ZERO.equals(wholes)) {
+                ++weight;
+                final BigInteger[] pair = wholes.divideAndRemainder(BI_TEN_THOUSAND);
+                wholes = pair[0];
+                final short shortValue = pair[1].shortValue();
+                if (shortValue != 0 || shorts.isNotEmpty()) {
+                    shorts.push(shortValue);
+                }
+            }
         }
 
-        return INT32_BYTE_SIZE + typeLen;
+        //8 bytes for "header" and then 2 for each short
+        final byte[] bytes = new byte[8 + (2 * shorts.size())];
+        int idx = 0;
+
+        //number of 2-byte shorts representing 4 decimal digits
+        ByteConverter.int2(bytes, idx, shorts.size());
+        idx += 2;
+        //0 based number of 4 decimal digits (i.e. 2-byte shorts) before the decimal
+        ByteConverter.int2(bytes, idx, weight);
+        idx += 2;
+        //indicates positive, negative or NaN
+        ByteConverter.int2(bytes, idx, value.signum() == -1 ? NUMERIC_NEG : NUMERIC_POS);
+        idx += 2;
+        //number of digits after the decimal
+        ByteConverter.int2(bytes, idx, scale);
+        idx += 2;
+
+        short s;
+        while ((s = shorts.pop()) != -1) {
+            ByteConverter.int2(bytes, idx, s);
+            idx += 2;
+        }
+
+        buffer.writeInt(bytes.length);
+        buffer.writeBytes(bytes);
+        return INT32_BYTE_SIZE + bytes.length;
     }
 
+    /**
+     * Simplified version of {@link org.postgresql.util.ByteConverter#numeric(byte[], int, int)}
+     *
+     * <p>The unscaled value is reconstructed arithmetically:
+     * <pre>
+     *   N = groups[0]*10000^(nDigits-1) + ... + groups[nDigits-1]
+     *   E = 4*(weight - nDigits + 1) + scale
+     *   unscaled = N * 10^E   (or N / 10^|E| when E < 0)
+     * </pre>
+     */
     @Override
     public BigDecimal readBinaryValue(ByteBuf buffer, int valueLength) {
-        // Number of DEC_DIGIT blocks
-        short nDigits = buffer.readShort();
-        // DEC_DIGIT blocks before decimal point
+        int nDigits = buffer.readShort() & 0xFFFF;
         short weight = buffer.readShort();
         short sign = buffer.readShort();
         short scale = buffer.readShort();
 
         if (nDigits == 0) {
-            return BigDecimal.ZERO;
+            return new BigDecimal(BigInteger.ZERO, scale);
         }
 
-        boolean has_dp = scale > 0;
-        int sizeOfBytes = (nDigits * DEC_DIGITS) + (has_dp ? 1 : 0);
-        char[] decDigits = new char[sizeOfBytes];
-
-        int decDigitsIdx = 0;
+        BigInteger bigInt = BigInteger.ZERO;
         for (int i = 0; i < nDigits; i++) {
-            int decDigit = buffer.readShort();
-            if (decDigit > 0) {
-                // Decode 4 digits from a 16 bit short
-                for (int j = 1000; j > 0 && decDigitsIdx < sizeOfBytes; j /= 10) {
-                    int d1 = (decDigit / j);
-                    decDigit -= d1 * j;
-                    decDigits[decDigitsIdx++] = (char) (d1 + '0');
-                }
-            }
-            if (has_dp && i == weight) {
-                decDigits[decDigitsIdx++] = '.';
-            }
+            bigInt = bigInt.multiply(BI_TEN_THOUSAND).add(BigInteger.valueOf(buffer.readShort() & 0xFFFF));
         }
 
-        var bd = new BigDecimal(decDigits)
-            .setScale(scale, MathContext.UNLIMITED.getRoundingMode());
+        int e = 4 * (weight - nDigits + 1) + scale;
+        BigInteger unscaled = e >= 0 ? bigInt.multiply(tenPower(e)) : bigInt.divide(tenPower(-e));
+
+        BigDecimal bd = new BigDecimal(unscaled, scale);
         return sign == NUMERIC_NEG ? bd.negate() : bd;
     }
 
@@ -181,5 +243,47 @@ class NumericType extends PGType<BigDecimal> {
     @Override
     BigDecimal decodeUTF8Text(byte[] bytes, RelationLookup relationLookup) {
         return new BigDecimal(new String(bytes, StandardCharsets.UTF_8));
+    }
+
+    private static BigInteger tenPower(int exponent) {
+        return BI_TEN_POWERS.length > exponent ? BI_TEN_POWERS[exponent] : BigInteger.TEN.pow(exponent);
+    }
+
+    /**
+     * Simple stack structure for non-negative {@code short} values.
+     */
+    private static final class PositiveShorts {
+        private short[] shorts = new short[8];
+        private int idx;
+
+        PositiveShorts() {
+        }
+
+        void push(short s) {
+            if (s < 0) {
+                throw new IllegalArgumentException("only non-negative values accepted: " + s);
+            }
+            if (idx == shorts.length) {
+                grow();
+            }
+            shorts[idx++] = s;
+        }
+
+        int size() {
+            return idx;
+        }
+
+        boolean isNotEmpty() {
+            return idx != 0;
+        }
+
+        short pop() {
+            return idx > 0 ? shorts[--idx] : -1;
+        }
+
+        private void grow() {
+            final int newSize = shorts.length <= 1024 ? shorts.length << 1 : (int) (shorts.length * 1.5);
+            shorts = Arrays.copyOf(shorts, newSize);
+        }
     }
 }
