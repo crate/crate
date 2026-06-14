4bf9c4365bb22c759fe2a48731b4b1fb9c7d7f5f
jeeminso <jeeminso@gmail.com>
2026-06-14 15:07:23 -0400

    Test: increase timeout for test_concurrent_table_swaps_with_insert_from_values

--- server/src/test/java/io/crate/integrationtests/PartitionedTableConcurrentIntegrationTest.java
+++ server/src/test/java/io/crate/integrationtests/PartitionedTableConcurrentIntegrationTest.java
@@ -561,7 +561,7 @@ public class PartitionedTableConcurrentIntegrationTest extends IntegTestCase {
                     (21), (22), (23), (24), (25), (26), (27), (28), (29), (30),
                     (31), (32), (33), (34), (35), (36), (37), (38), (39), (40),
                     (41), (42), (43), (44), (45), (46), (47), (48), (49), (50);
-                    """);
+                    """, null, TimeValue.timeValueSeconds(30));
             } catch (Throwable e) {
                 error.set(e);
             }
