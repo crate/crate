9cd72d35ef96e534de757d841ef17a0813328c17
jeeminso <jeeminso@gmail.com>
2026-07-07 16:53:01 -0400

    Fix INSERT into wrong table due to concurrent SWAP TABLE
    Once an INSERT is analyzed, the table OID captured must be used
    for subsequent table lookups throughout the execution for consistency.
    
    (cherry picked from commit 09149008a58029e1c6cfecd1e7c566686d72a2bf)

--- server/src/main/java/io/crate/execution/dsl/projection/AbstractIndexWriterProjection.java
+++ server/src/main/java/io/crate/execution/dsl/projection/AbstractIndexWriterProjection.java
@@ -25,6 +25,8 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.elasticsearch.Version;
+import org.elasticsearch.cluster.metadata.Metadata;
 import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.io.stream.StreamOutput;
 import org.elasticsearch.common.settings.Setting;
@@ -53,6 +55,7 @@ public abstract class AbstractIndexWriterProjection extends Projection {
 
     private final int bulkActions;
     protected RelationName relationName;
+    protected int tableOid;
     protected String partitionIdent;
     protected List<ColumnIdent> primaryKeys;
 
@@ -69,6 +72,7 @@ public abstract class AbstractIndexWriterProjection extends Projection {
 
 
     protected AbstractIndexWriterProjection(RelationName relationName,
+                                            int tableOid,
                                             @Nullable String partitionIdent,
                                             List<ColumnIdent> primaryKeys,
                                             @Nullable ColumnIdent clusteredByColumn,
@@ -76,6 +80,7 @@ public abstract class AbstractIndexWriterProjection extends Projection {
                                             List<Symbol> idSymbols,
                                             boolean autoCreateIndices) {
         this.relationName = relationName;
+        this.tableOid = tableOid;
         this.partitionIdent = partitionIdent;
         this.primaryKeys = primaryKeys;
         this.clusteredByColumn = clusteredByColumn;
@@ -90,6 +95,11 @@ public abstract class AbstractIndexWriterProjection extends Projection {
 
     protected AbstractIndexWriterProjection(StreamInput in) throws IOException {
         relationName = new RelationName(in);
+        if (in.getVersion().onOrAfter(Version.V_6_3_6)) {
+            tableOid = in.readInt();
+        } else {
+            tableOid = Metadata.OID_UNASSIGNED;
+        }
 
         partitionIdent = in.readOptionalString();
         idSymbols = Symbols.fromStream(in);
@@ -149,6 +159,10 @@ public abstract class AbstractIndexWriterProjection extends Projection {
         return relationName;
     }
 
+    public int tableOid() {
+        return tableOid;
+    }
+
     @Nullable
     public String partitionIdent() {
         return partitionIdent;
@@ -174,6 +188,7 @@ public abstract class AbstractIndexWriterProjection extends Projection {
             return false;
         if (!primaryKeys.equals(that.primaryKeys)) return false;
         if (!relationName.equals(that.relationName)) return false;
+        if (tableOid != that.tableOid) return false;
         if (partitionIdent != null ? !partitionIdent.equals(that.partitionIdent) : that.partitionIdent != null)
             return false;
 
@@ -185,6 +200,7 @@ public abstract class AbstractIndexWriterProjection extends Projection {
         int result = super.hashCode();
         result = 31 * result + Integer.hashCode(bulkActions);
         result = 31 * result + relationName.hashCode();
+        result = 31 * result + tableOid;
         result = 31 * result + (partitionIdent != null ? partitionIdent.hashCode() : 0);
         result = 31 * result + primaryKeys.hashCode();
         result = 31 * result + (clusteredByColumn != null ? clusteredByColumn.hashCode() : 0);
@@ -198,6 +214,9 @@ public abstract class AbstractIndexWriterProjection extends Projection {
     @Override
     public void writeTo(StreamOutput out) throws IOException {
         relationName.writeTo(out);
+        if (out.getVersion().onOrAfter(Version.V_6_3_6)) {
+            out.writeInt(tableOid);
+        }
         out.writeOptionalString(partitionIdent);
 
         Symbols.toStream(idSymbols, out);
