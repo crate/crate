4140ce6ba542dd8128e43338395ca06e22b51bc3
jeeminso <jeeminso@gmail.com>
2026-07-13 15:26:35 -0400

    Fix `<foreign-table>::regclass` to return OidHash instead of OID_UNASSIGNED
    (cherry picked from commit 1bb71862d5171111a0ff0287c88c146c1979ae73)
    
    # Conflicts:
    #	docs/appendices/release-notes/6.4.1.rst
    #	server/src/test/java/io/crate/types/RegclassTypeTest.java

--- server/src/main/java/org/elasticsearch/cluster/metadata/RelationMetadata.java
+++ server/src/main/java/org/elasticsearch/cluster/metadata/RelationMetadata.java
@@ -442,7 +442,7 @@ public sealed interface RelationMetadata extends Diffable<RelationMetadata>
 
         @Override
         public int oid() {
-            return Metadata.OID_UNASSIGNED;
+            return OidHash.relationOid(OidHash.Type.TABLE, name);
         }
 
         @Override
