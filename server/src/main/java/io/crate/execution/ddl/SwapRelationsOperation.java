97d04eeafe9f31b63393964969790ded6ad4eab4
Mathias Fussenegger <f.mathias@zignar.net>
2026-07-27 09:49:12 +0200

    Revert "Rename and reallocate index names again for table swap/rename"
    Follow up to https://github.com/crate/crate/pull/19726
    
    This reverts commit 0f59a2243bde7798faba87fe551126b33a83c51b.

--- server/src/main/java/io/crate/execution/ddl/SwapRelationsOperation.java
+++ server/src/main/java/io/crate/execution/ddl/SwapRelationsOperation.java
@@ -26,6 +26,7 @@ import java.util.List;
 import java.util.Set;
 import java.util.function.Consumer;
 
+import org.elasticsearch.Version;
 import org.elasticsearch.cluster.ClusterState;
 import org.elasticsearch.cluster.block.ClusterBlocks;
 import org.elasticsearch.cluster.metadata.IndexMetadata;
@@ -169,19 +170,22 @@ public class SwapRelationsOperation {
                 );
         }
 
-        // Remove all involved indices first so that rename operations are independent of each other
-        for (RelationNameSwap swapAction : swapRelationsRequest.swapActions()) {
-            removeOccurrences(state, blocksBuilder, routingBuilder, updatedMetadata, swapAction.source());
-            removeOccurrences(state, blocksBuilder, routingBuilder, updatedMetadata, swapAction.target());
-        }
+        boolean needIndexAndRoutingUpdate = state.nodes().getSmallestNonClientNodeVersion().before(Version.V_6_5_0);
+        if (needIndexAndRoutingUpdate) {
+            // Remove all involved indices first so that rename operations are independent of each other
+            for (RelationNameSwap swapAction : swapRelationsRequest.swapActions()) {
+                removeOccurrences(state, blocksBuilder, routingBuilder, updatedMetadata, swapAction.source());
+                removeOccurrences(state, blocksBuilder, routingBuilder, updatedMetadata, swapAction.target());
+            }
 
-        for (RelationNameSwap relationNameSwap : swapRelationsRequest.swapActions()) {
-            RelationName source = relationNameSwap.source();
-            RelationName target = relationNameSwap.target();
-            addSourceIndicesRenamedToTargetName(
-                metadata, updatedMetadata, blocksBuilder, routingBuilder, source, target, newIndexNames::add);
-            addSourceIndicesRenamedToTargetName(
-                metadata, updatedMetadata, blocksBuilder, routingBuilder, target, source, newIndexNames::add);
+            for (RelationNameSwap relationNameSwap : swapRelationsRequest.swapActions()) {
+                RelationName source = relationNameSwap.source();
+                RelationName target = relationNameSwap.target();
+                addSourceIndicesRenamedToTargetName(
+                    metadata, updatedMetadata, blocksBuilder, routingBuilder, source, target, newIndexNames::add);
+                addSourceIndicesRenamedToTargetName(
+                    metadata, updatedMetadata, blocksBuilder, routingBuilder, target, source, newIndexNames::add);
+            }
         }
 
         ClusterState stateAfterSwap = applyClusterStateModifiers(
@@ -192,7 +196,9 @@ public class SwapRelationsOperation {
                 .build(),
             swapRelationsRequest.swapActions()
         );
-        ClusterState reroutedState = allocationService.reroute(stateAfterSwap, "indices name switch");
+        ClusterState reroutedState = needIndexAndRoutingUpdate
+            ? allocationService.reroute(stateAfterSwap, "indices name switch")
+            : stateAfterSwap;
         return new UpdatedState(reroutedState, newIndexNames);
     }
 
