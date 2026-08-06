b7e90a74a8fb960be9010e670a1d702f93f733d7
baur <baurzhansahariev@gmail.com>
2026-08-06 18:09:47 +0200

    Introduce ResizeAwareMap

---
+++ server/src/main/java/io/crate/execution/engine/aggregation/ResizeAwareMap.java
@@ -0,0 +1,165 @@
+/*
+ * Licensed to Crate.io GmbH ("Crate") under one or more contributor
+ * license agreements.  See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.  Crate licenses
+ * this file to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.  You may
+ * obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * However, if you have executed another commercial license agreement
+ * with Crate these terms will supersede the license and you may use the
+ * software solely pursuant to the terms of the relevant commercial agreement.
+ */
+
+package io.crate.execution.engine.aggregation;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Wrapper for maps containing large number of elements.
+ *
+ * Resize of such map can create significant number of (retained) elements,
+ * memory accounting must utilize expectedCapacityIncrease to account for that.
+ */
+public class ResizeAwareMap<K, V> implements Map<K, V> {
+
+    // Copy of java.util.HashMap.MAXIMUM_CAPACITY
+    private static final int MAXIMUM_CAPACITY = 1 << 30;
+
+    private final Map<K, V> delegate;
+    private final float loadFactor;
+    private final boolean openAddressing;
+
+    private int currentCapacity;
+
+    public ResizeAwareMap(Map<K, V> delegate,
+                          int initialCapacity,
+                          float loadFactor,
+                          boolean openAddressing) {
+        this.delegate = delegate;
+        this.currentCapacity = tableSizeFor(initialCapacity);
+        this.loadFactor = loadFactor;
+        this.openAddressing = openAddressing;
+    }
+
+    @Override
+    public int size() {
+        return delegate.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return delegate.isEmpty();
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        return delegate.containsKey(key);
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        return delegate.containsValue(value);
+    }
+
+    @Override
+    public V get(Object key) {
+        return delegate.get(key);
+    }
+
+    @Override
+    public V put(K key, V value) {
+        growIfNeeded(1);
+        V result = delegate.put(key, value);
+        return result;
+    }
+
+    @Override
+    public V remove(Object key) {
+        return delegate.remove(key);
+    }
+
+    @Override
+    public void putAll(Map<? extends K, ? extends V> m) {
+        growIfNeeded(m.size());
+        delegate.putAll(m);
+    }
+
+    @Override
+    public void clear() {
+        delegate.clear();
+    }
+
+    @Override
+    public Set<K> keySet() {
+        return delegate.keySet();
+    }
+
+    @Override
+    public Collection<V> values() {
+        return delegate.values();
+    }
+
+    @Override
+    public Set<Entry<K, V>> entrySet() {
+        return delegate.entrySet();
+    }
+
+    /**
+     * @return internal capacity increase after the next put() or putAll().
+     * can only return 2 values:
+     * <ol>
+     *   <li> 0 (No resize would happen)</li>
+     *   <li> currentCapacity * 2 (Resize would happen. Both JDK and Netty Maps double their sizes)</li>
+     * </ol>
+     * <br>
+     */
+    public int expectedCapacityIncrease(int newItems) {
+        int capacity = currentCapacity;
+        while (capacity < MAXIMUM_CAPACITY && delegate.size() + newItems > resizeThreshold(capacity)) {
+            capacity = capacity << 1;
+        }
+        return capacity - currentCapacity;
+    }
+
+    public int currentCapacity() {
+        return currentCapacity;
+    }
+
+    /**
+     * Mirrors what delegate map is doing to keep track of the current capacity.
+     * Both JDK and Netty maps grow in power of 2.
+     * JDK map has an explicit limit.
+     * Netty map's growSize() doesn't check 1<<30 and rehash with negative capacity throws.
+     */
+    private void growIfNeeded(int newItems) {
+        while (currentCapacity < MAXIMUM_CAPACITY && delegate.size() + newItems > resizeThreshold(currentCapacity)) {
+            currentCapacity <<= 1;
+        }
+    }
+
+    private int resizeThreshold(int capacity) {
+        int threshold = (int) (capacity * loadFactor);
+        // Netty maps need 1 slot to be free, hence resize happens earlier.
+        // See calcMaxSize() in Netty maps.
+        return openAddressing ? Math.min(capacity - 1, threshold) : threshold;
+    }
+
+    /**
+     * Copied from java.util.HashMap#tableSizeFor
+     */
+    private static int tableSizeFor(int cap) {
+        int n = -1 >>> Integer.numberOfLeadingZeros(cap - 1);
+        return (n < 0) ? 1 : (n >= MAXIMUM_CAPACITY) ? MAXIMUM_CAPACITY : n + 1;
+    }
+}
