52cbed7ce9ff3d0f889559a0f2afd85af35ec1d9
baur <baurzhansahariev@gmail.com>
2026-08-06 18:09:47 +0200

    Introduce ResizeAwareMap

---
+++ server/src/test/java/io/crate/execution/engine/aggregation/ResizeAwareMapTest.java
@@ -0,0 +1,110 @@
+/*
+ * Licensed to Crate.io GmbH ("Crate") under one or more contributor
+ * license agreements.  See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.  Crate licenses
+ * this file to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.  You may
+ * obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * However, if you have executed another commercial license agreement
+ * with Crate these terms will supersede the license and you may use the
+ * software solely pursuant to the terms of the relevant commercial agreement.
+ */
+
+package io.crate.execution.engine.aggregation;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+
+public class ResizeAwareMapTest {
+
+    @Test
+    public void test_put_initial_capacity_power_of_two() {
+        Map<Integer, Integer> delegate = new HashMap<>();
+        int capacity = 4;
+        float loadFactor = 0.75f;
+        int threshold = (int) (capacity * loadFactor);
+        ResizeAwareMap<Integer, Integer> map = new ResizeAwareMap<>(delegate, capacity, loadFactor, false);
+
+        for (int i = 0; i < threshold; i++) {
+            assertThat(map.expectedCapacityIncrease(1)).isEqualTo(0);
+            map.put(i, i);
+        }
+        assertThat(map.expectedCapacityIncrease(1)).isEqualTo(capacity);
+    }
+
+    @Test
+    public void test_put_initial_capacity_is_rounded_up_to_the_next_power_of_two() {
+        Map<Integer, Integer> delegate = new HashMap<>();
+        ResizeAwareMap<Integer, Integer> map = new ResizeAwareMap<>(delegate, 5, 0.75f, false);
+        // 5 is rounded up to 8, hence threshold is 8*0.75 = 6
+        for (int i = 0; i < 6; i++) {
+            assertThat(map.expectedCapacityIncrease(1)).isEqualTo(0);
+            map.put(i, i);
+        }
+        assertThat(map.expectedCapacityIncrease(1)).isEqualTo(8);
+    }
+
+    @Test
+    public void test_putAll_tracks_growth() {
+        Map<Integer, Integer> delegate = new HashMap<>();
+        float loadFactor = 0.75f;
+        ResizeAwareMap<Integer, Integer> map = new ResizeAwareMap<>(delegate, 4, loadFactor, false);
+
+        Map<Integer, Integer> bulk = new HashMap<>();
+        bulk.put(1, 1);
+        bulk.put(2, 2);
+        bulk.put(3, 3);
+        bulk.put(4, 4);
+        map.putAll(bulk);
+
+        int expectedNewCapacity = 8;
+        assertThat(map.currentCapacity()).isEqualTo(expectedNewCapacity);
+        int threshold = (int) (expectedNewCapacity * loadFactor);
+        for (int i = 0; i < threshold; i++) {
+            assertThat(map.expectedCapacityIncrease(1)).isEqualTo(0);
+            map.put(i, i);
+        }
+        assertThat(map.expectedCapacityIncrease(1)).isEqualTo(expectedNewCapacity);
+    }
+
+    @Test
+    public void test_expectedCapacityIncrease_does_not_mutate_state() {
+        Map<Integer, Integer> delegate = new HashMap<>();
+        int capacity = 4;
+        float loadFactor = 0.75f;
+        int threshold = (int) (capacity * loadFactor);
+        ResizeAwareMap<Integer, Integer> map = new ResizeAwareMap<>(delegate, capacity, loadFactor, false);
+
+        for (int i = 0; i < threshold + 1; i++) {
+            map.expectedCapacityIncrease(1);
+        }
+        // We did threshold + 1 calls, but actual capacity didn't cahnge without put calls.
+        assertThat(map.currentCapacity()).isEqualTo(capacity);
+    }
+
+    @Test
+    public void test_capacity_growth_stops_at_maximum_capacity() {
+        int maximumCapacity = 1 << 30;
+        @SuppressWarnings("unchecked")
+        Map<Integer, Integer> delegate = mock(Map.class);
+        when(delegate.size()).thenReturn(Integer.MAX_VALUE - 1);
+
+        ResizeAwareMap<Integer, Integer> map = new ResizeAwareMap<>(delegate, maximumCapacity, 0.75f, false);
+        assertThat(map.expectedCapacityIncrease(1)).isEqualTo(0);
+    }
+}
