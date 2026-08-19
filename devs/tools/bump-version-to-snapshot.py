1e1feaad6f66de91851250cf931d267f94b814f3
Haris <haris.osmanagic@gmail.com>
2026-08-19 15:31:26 +0200

    remove generateBackupPoms=false

--- devs/tools/bump-version-to-snapshot.py
+++ devs/tools/bump-version-to-snapshot.py
@@ -217,8 +217,7 @@ def set_pom_version(root, version, previous):
         raise ValueError(f"pom.xml is at {match.group(1)}, not at {previous}")
 
     print(f"Setting the pom.xml version to {version}...")
-    run("./mvnw", "--quiet", "versions:set", f"-DnewVersion={version}",
-        "-DgenerateBackupPoms=false", cwd=root, quiet=False)
+    run("./mvnw", "--quiet", "versions:set", f"-DnewVersion={version}", cwd=root, quiet=False)
 
 
 def main():
