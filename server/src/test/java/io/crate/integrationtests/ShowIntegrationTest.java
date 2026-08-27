fee3cd5c69bc5b854c7b1aeb2f34780b8e93edf7
somiljain2006 <somil16022006@gmail.com>
2026-08-28 03:47:41 +0530

    Fix test failures

--- server/src/test/java/io/crate/integrationtests/ShowIntegrationTest.java
+++ server/src/test/java/io/crate/integrationtests/ShowIntegrationTest.java
@@ -440,6 +440,7 @@ public class ShowIntegrationTest extends IntegTestCase {
             "optimizer_optimize_collect_where_clause_access| true| Indicates if the optimizer rule OptimizeCollectWhereClauseAccess is activated.",
             "optimizer_remove_order_beneath_insert| true| Indicates if the optimizer rule RemoveOrderBeneathInsert is activated.",
             "optimizer_remove_redundant_eval| true| Indicates if the optimizer rule RemoveRedundantEval is activated.",
+            "optimizer_remove_redundant_having_count| true| Indicates if the optimizer rule RemoveRedundantHavingCount is activated.",
             "optimizer_reorder_hash_join| true| Indicates if the optimizer rule ReorderHashJoin is activated.",
             "optimizer_reorder_nested_loop_join| true| Indicates if the optimizer rule ReorderNestedLoopJoin is activated.",
             "optimizer_rewrite_distinct_agg_to_group_by| true| Indicates if the optimizer rule RewriteDistinctAggToGroupBy is activated.",
