85546b5f2324f33ba717ae3d98dfc1eb86f5ec1c
Bing O'Dowd <odowd.bing@gmail.com>
2026-08-28 14:29:51 +0200

    Add simple experiment to read parquet files with Hardwood

---
+++ server/src/main/java/io/crate/fdw/ParquetBatchIterator.java
@@ -0,0 +1,135 @@
+/*
+ * Licensed to Crate.io GmbH ("Crate") under one or more contributor
+ * license agreements.  See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.  Crate licenses
+ * this file to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.  You may
+ * obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * However, if you have executed another commercial license agreement
+ * with Crate these terms will supersede the license and you may use the
+ * software solely pursuant to the terms of the relevant commercial agreement.
+ */
+
+package io.crate.fdw;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import dev.hardwood.InputFile;
+import dev.hardwood.reader.ParquetFileReader;
+import dev.hardwood.reader.RowReader;
+import dev.hardwood.schema.ColumnProjection;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+
+import io.crate.data.BatchIterator;
+import io.crate.data.Row;
+import io.crate.data.RowN;
+import io.crate.exceptions.ConversionException;
+import io.crate.metadata.Reference;
+
+public class ParquetBatchIterator implements BatchIterator<Row> {
+
+    private static final Logger LOGGER = LogManager.getLogger(ParquetBatchIterator.class);
+
+    private final Object[] cells;
+    private final Row row;
+    private ParquetFileReader reader;
+    private RowReader rowReader;
+    private final List<Reference> columns;
+    private final Path parquetFile;
+
+    public ParquetBatchIterator(Path parquetFile, List<Reference> columns) {
+        this.parquetFile = parquetFile;
+        this.columns = columns;
+        this.cells = new Object[columns.size()];
+        this.row = new RowN(cells);
+    }
+
+    @Override
+    public void kill(Throwable throwable) {
+
+    }
+
+    @Override
+    public Row currentElement() {
+        return row;
+    }
+
+    @Override
+    public boolean moveNext() {
+        assert (rowReader != null);
+        while (rowReader.hasNext()) {
+            rowReader.next();
+            for (int i = 0; i < columns.size(); i++) {
+                Reference ref = columns.get(i);
+                // TODO: do we need something like getObject from ResultSetParser? Something to map between the object
+                // from Hardwood/parquet and what Crate uses internally
+                // TODO: need a way to know method to call on rowReader (getDouble, or getString or whatever)
+                double val = rowReader.getDouble(ref.toString());
+                try {
+                    cells[i] = ref.valueType().implicitCast(val);
+                } catch (ClassCastException | IllegalArgumentException e) {
+                    var conversionException = new ConversionException(val, ref.valueType());
+                    conversionException.addSuppressed(e);
+                    throw conversionException;
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void moveToStart() {
+    }
+
+    @Override
+    public void close() {
+        if (reader != null) {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                // Placeholder error
+                throw new Error("Could not close: " + e.getMessage());
+            }
+        }
+    }
+
+    @Override
+    public boolean allLoaded() {
+        return true;
+    }
+
+    @Override
+    public boolean hasLazyResultSet() {
+        return true;
+    }
+
+    @Override
+    public CompletionStage<?> loadNextBatch() throws Exception {
+        if (reader == null) {
+            reader = ParquetFileReader.open(InputFile.of(parquetFile));
+            rowReader = reader.buildRowReader()
+                    .projection(ColumnProjection.columns(columns.stream()
+                            .map(ref -> ref.column().fqn())
+                            .toArray(String[]::new)))
+                    .build();
+            System.out.println("Loaded reader");
+        }
+        return CompletableFuture.completedFuture(null);
+    }
+}
---
+++ server/src/test/java/io/crate/fdw/ParquetBatchIteratorTest.java
@@ -0,0 +1,60 @@
+/*
+ * Licensed to Crate.io GmbH ("Crate") under one or more contributor
+ * license agreements.  See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.  Crate licenses
+ * this file to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.  You may
+ * obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * However, if you have executed another commercial license agreement
+ * with Crate these terms will supersede the license and you may use the
+ * software solely pursuant to the terms of the relevant commercial agreement.
+ */
+
+package io.crate.fdw;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import io.crate.metadata.ColumnIdent;
+import io.crate.metadata.Reference;
+import io.crate.metadata.doc.DocTableInfo;
+import io.crate.test.integration.CrateDummyClusterServiceUnitTest;
+import io.crate.testing.SQLExecutor;
+
+public class ParquetBatchIteratorTest extends CrateDummyClusterServiceUnitTest {
+    private final Path parquetFile = Paths.get(getClass().getResource("/essetup/data/parquet").toURI())
+            .resolve("yellow_tripdata_2026-10rows.parquet");
+
+    public ParquetBatchIteratorTest() throws URISyntaxException {
+    }
+
+    @Test
+    public void test_reads_all_records() throws Exception {
+        var e = SQLExecutor.of(clusterService)
+                .addTable("create table doc.taxi (trip_distance double)");
+        DocTableInfo table = e.resolveTableInfo("doc.taxi");
+        List<Reference> columns = List.of(
+                table.getReadReference(ColumnIdent.of("trip_distance")));
+        ParquetBatchIterator it = new ParquetBatchIterator(parquetFile, columns);
+        it.loadNextBatch();
+        while (it.moveNext()) {
+            System.out.println("Current row: " + it.currentElement());
+        }
+        it.close();
+
+    }
+}
