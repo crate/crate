f453d0148944f8e648cec0542d7c914445a4ab96
baur <baurzhansahariev@gmail.com>
2026-08-31 18:42:12 +0200

    Replace assertions with validateOutputsOrder
    Unifying validation for all operators
    
    (cherry picked from commit 5b3c31f35708e59951c5d378bb3eff4b4c47268d)
    
    # Conflicts:
    #	server/src/main/java/io/crate/planner/operators/HashAggregate.java

--- server/src/main/java/io/crate/planner/operators/HashAggregate.java
+++ server/src/main/java/io/crate/planner/operators/HashAggregate.java
@@ -198,9 +198,9 @@ public class HashAggregate extends ForwardingLogicalPlan {
             return this;
         }
         List<Function> prunedOutputs = Lists.intersection(aggregates, newAggregates);
-        boolean isSubsequence = Lists.isSubsequence(prunedOutputs, aggregates);
-        assert isSubsequence : "pruneOutputsExcept must not shuffle the outputs, it can only remove some of them.";
-        return new HashAggregate(newSource, prunedOutputs);
+        HashAggregate newPlan = new HashAggregate(newSource, prunedOutputs);
+        validateOutputsOrder(newPlan.outputs());
+        return newPlan;
     }
 
     @Override
