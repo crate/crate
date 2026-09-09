1f4591a2408919189f53b12b98c49157765d00e8
Manish Singh <manish9363@gmail.com>
2026-09-09 00:08:03 -0700

    Add pg_is_in_recovery() scalar function
    Adds the pg_catalog.pg_is_in_recovery() scalar function for improved compatibility with PostgreSQL tools (e.g. DataGrip, Pgpool-II). CrateDB has no PostgreSQL-style recovery/standby state, so it always returns false.
    
    Closes #12085

---
+++ server/src/main/java/io/crate/expression/scalar/postgres/PgIsInRecoveryFunction.java
@@ -0,0 +1,72 @@
+/*
+ * Licensed to Crate.io GmbH ("Crate") under one or more contributor
+ * license agreements.  See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.  Crate licenses
+ * this file to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.  You may
+ * obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * However, if you have executed another commercial license agreement
+ * with Crate these terms will supersede the license and you may use the
+ * software solely pursuant to the terms of the relevant commercial agreement.
+ */
+
+package io.crate.expression.scalar.postgres;
+
+import java.util.EnumSet;
+
+import io.crate.data.Input;
+import io.crate.expression.symbol.Function;
+import io.crate.expression.symbol.Literal;
+import io.crate.expression.symbol.Symbol;
+import io.crate.metadata.FunctionName;
+import io.crate.metadata.FunctionType;
+import io.crate.metadata.Functions;
+import io.crate.metadata.NodeContext;
+import io.crate.metadata.Scalar;
+import io.crate.metadata.TransactionContext;
+import io.crate.metadata.functions.BoundSignature;
+import io.crate.metadata.functions.Signature;
+import io.crate.metadata.pgcatalog.PgCatalogSchemaInfo;
+import io.crate.types.DataTypes;
+
+public class PgIsInRecoveryFunction extends Scalar<Boolean, Void> {
+
+    public static final String NAME = "pg_is_in_recovery";
+    private static final FunctionName FQN = new FunctionName(PgCatalogSchemaInfo.NAME, NAME);
+
+    public static void register(Functions.Builder module) {
+        module.add(
+            Signature.builder(FQN, FunctionType.SCALAR)
+                .argumentTypes()
+                .returnType(DataTypes.BOOLEAN.getTypeSignature())
+                .features(EnumSet.of(Feature.NOTNULL))
+                .build(),
+            PgIsInRecoveryFunction::new
+        );
+    }
+
+    public PgIsInRecoveryFunction(Signature signature, BoundSignature boundSignature) {
+        super(signature, boundSignature);
+    }
+
+    @Override
+    public Symbol normalizeSymbol(Function symbol, TransactionContext txnCtx, NodeContext nodeCtx) {
+        assert symbol.arguments().size() == 0 : "function's number of arguments must be 0";
+        return Literal.of(false);
+    }
+
+    @Override
+    public Boolean evaluate(TransactionContext txnCtx, NodeContext nodeCtx, Input[] args) {
+        assert args.length == 0 : "number of args must be 0";
+        return false;
+    }
+}
