8f3990e4dfda491264013b25ba1ccde079b8f76a
gohri81-tech <gohri81@gmail.com>
2026-09-12 15:31:54 -0500

    Harden regexp_match tests and align source style

--- server/src/test/java/io/crate/expression/scalar/regex/RegexpMatchFunctionTest.java
+++ server/src/test/java/io/crate/expression/scalar/regex/RegexpMatchFunctionTest.java
@@ -3,8 +3,8 @@
  * license agreements.  See the NOTICE file distributed with this work for
  * additional information regarding copyright ownership.  Crate licenses
  * this file to you under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * you may not use this file except in compliance with the License.  You may
+ * obtain a copy of the License at
  *
  *   http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -13,11 +13,15 @@
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
  * License for the specific language governing permissions and limitations
  * under the License.
+ *
+ * However, if you have executed another commercial license agreement
+ * with Crate these terms will supersede the license and you may use the
+ * software solely pursuant to the terms of the relevant commercial agreement.
  */
 
 package io.crate.expression.scalar.regex;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.Test;
 
@@ -27,24 +31,44 @@ import io.crate.expression.symbol.Literal;
 public class RegexpMatchFunctionTest extends ScalarTestCase {
 
     @Test
-    public void test_noMatch() {
+    public void test_no_match() {
         assertEvaluateNull("regexp_match(name, 'crate')", Literal.of("foobarbequebaz"));
     }
 
     @Test
-    public void test_matchWithoutGroups() {
+    public void test_match_without_groups() {
         assertEvaluate("regexp_match(name, 'bar')", new String[]{"bar"}, Literal.of("foobarbequebaz"));
     }
 
     @Test
-    public void test_matchWithGroups() {
+    public void test_match_with_groups() {
         assertEvaluate("regexp_match(name, '(bar)(beque)')", new String[]{"bar", "beque"}, Literal.of("foobarbequebaz"));
+    }
+
+    @Test
+    public void test_match_with_unmatched_group() {
+        assertEvaluate("regexp_match('b', '(a)?b')", new String[]{null}, Literal.of("b"));
     }
 
     @Test
     public void test_flags() {
         assertEvaluate("regexp_match(name, '(BAR)', 'i')", new String[]{"BAR"}, Literal.of("foobarbequebaz"));
         assertEvaluateNull("regexp_match(name, '(BAR)', '')", Literal.of("foobarbequebaz"));
+    }
+
+    @Test
+    public void test_global_flag_is_rejected() {
+        assertThatThrownBy(() -> assertEvaluate("regexp_match('aba', 'a', 'g')", new String[]{"a"}))
+            .isExactlyInstanceOf(IllegalArgumentException.class)
+            .hasMessage("The regular expression flag is unknown: g");
+    }
+
+    @Test
+    public void test_global_flag_is_rejected_during_compile() {
+        assertThatThrownBy(() -> assertCompile("regexp_match(name, 'a', 'g')", ignored -> ignored2 -> {
+        }))
+            .isExactlyInstanceOf(IllegalArgumentException.class)
+            .hasMessage("The regular expression flag is unknown: g");
     }
 
     @Test
@@ -57,7 +81,6 @@ public class RegexpMatchFunctionTest extends ScalarTestCase {
     @Test
     public void test_compile() {
         assertCompile("regexp_match(name, '(bar)(beque)')", scalar -> compiledScalar -> {
-            assertThat(scalar).isInstanceOf(RegexpMatchFunction.class);
             assertThat(compiledScalar).isInstanceOf(RegexpMatchFunction.class);
         });
     }
