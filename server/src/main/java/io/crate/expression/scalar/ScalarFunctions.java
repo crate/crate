90b6fff4efebbb65bce40fa4a077cb398f76d098
gohri81-tech <gohri81@gmail.com>
2026-09-12 15:32:12 -0500

    Restore ScalarFunctions license header

--- server/src/main/java/io/crate/expression/scalar/ScalarFunctions.java
+++ server/src/main/java/io/crate/expression/scalar/ScalarFunctions.java
@@ -3,8 +3,8 @@
  * license agreements.  See the NOTICE file distributed with this work for
  * additional information regarding copyright ownership.  Crate licenses
  * this file to you under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * you may not use this file except in compliance with the License.  You may
+ * obtain a copy of the License at
  *
  *   http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -15,7 +15,8 @@
  * under the License.
  *
  * However, if you have executed another commercial license agreement
- * with Crate these terms will supersede the terms of the relevant commercial agreement.
+ * with Crate these terms will supersede the license and you may use the
+ * software solely pursuant to the terms of the relevant commercial agreement.
  */
 
 package io.crate.expression.scalar;
