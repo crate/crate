5a786fca8ca2775cf62dd2769b351bfcf4e84739
Bing O'Dowd <odowd.bing@gmail.com>
2026-09-18 14:11:11 +0200

    Add NullAwareNumber and NullAwareNumericFieldComparator
    Fixes https://github.com/crate/crate/issues/20107
    
    When using a REAL or DOUBLE column as the ORDER BY key, a Lucene
    FieldDoc (part of a Lucene ScoreDoc) is used to store sorted values
    from the referenced document.
    For some References it is not possible to use null values in the FieldDoc
    (see NullSentinelValues.java), so a sentinel was used.
    
    Previously, we interpreted the sentinel after sorting, but this led to a
    collision between Infinity and NULL for REAL and DOUBLE. We couldn't
    tell if 'Infinity' in the FieldDoc meant Infinity or if it meant null.
    
    A few alternative approaches to fix this were tried.
    
    First, we tracked if the Reference was a column where the sentinel could
    be a valid entry (i.e. Infinity is valid as a REAL and DOUBLE, and also
    the sentinel). If so, we ran a second lookup
    in the document for the real value. This was very slow. In benchmarks
    vs the master branch at the time, this approach was ~14x slower.
    
    Second, we tried restricting the input of REAL and DOUBLE to use the
    MIN/MAX values as sentinel, similar to BIGINT. However, this causes
    issues with NULLS FIRST/LAST because +/-Infinity is always
    larger/smaller than the min/max.
    
    In this commit, we now store whether or not a value is NULL when the value
    is collected from the document before the sorting. This way, no sentinel
    is required and the collision between sentinel and Infinity is no longer
    a problem.
    
    In benchmarks, this is only slightly slower than using the sentinel
    approach; ~1.7s/op vs ~2.7s/op

---
+++ benchmarks/src/main/java/io/crate/execution/engine/collect/collectors/OrderedLuceneBatchIteratorFloatBenchmark.java
@@ -0,0 +1,170 @@
+/*
+ * Licensed to Crate.io GmbH ("Crate") under one or more contributor
+ * license agreements.  See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.  Crate licenses
+ * this file to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.  You may
+ * obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * However, if you have executed another commercial license agreement
+ * with Crate these terms will supersede the license and you may use the
+ * software solely pursuant to the terms of the relevant commercial agreement.
+ */
+
+package io.crate.execution.engine.collect.collectors;
+
+import static io.crate.execution.engine.sort.LuceneSort.nullAwareNumericSortField;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.FloatDocValuesField;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.MatchAllDocsQuery;
+import org.apache.lucene.search.Sort;
+import org.apache.lucene.search.SortField;
+// import org.apache.lucene.search.SortedNumericSortField;
+import org.apache.lucene.store.ByteBuffersDirectory;
+import org.elasticsearch.common.UUIDs;
+import org.elasticsearch.common.util.concurrent.EsExecutors;
+import org.elasticsearch.index.shard.ShardId;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.infra.Blackhole;
+
+import io.crate.analyze.OrderBy;
+import io.crate.breaker.TypedRowAccounting;
+import io.crate.data.BatchIterator;
+import io.crate.data.Row;
+import io.crate.data.breaker.RamAccounting;
+import io.crate.execution.engine.sort.NullValueOrder;
+// import io.crate.execution.engine.sort.NullAwareNumber;
+// import io.crate.execution.engine.sort.NullValueOrder;
+import io.crate.execution.engine.sort.OrderingByPosition;
+import io.crate.expression.reference.doc.lucene.CollectorContext;
+import io.crate.expression.reference.doc.lucene.LuceneCollectorExpression;
+// import io.crate.expression.reference.doc.lucene.NullSentinelValues;
+import io.crate.expression.reference.doc.lucene.OrderByCollectorExpression;
+import io.crate.metadata.ColumnIdent;
+import io.crate.metadata.Reference;
+import io.crate.metadata.RelationName;
+import io.crate.metadata.RowGranularity;
+import io.crate.metadata.SimpleReference;
+import io.crate.metadata.doc.DocSchemaInfo;
+import io.crate.types.DataTypes;
+import io.crate.types.LongType;
+
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.SECONDS)
+@State(Scope.Benchmark)
+public class OrderedLuceneBatchIteratorFloatBenchmark {
+
+    private static final TypedRowAccounting ROW_ACCOUNTING = new TypedRowAccounting(List.of(LongType.INSTANCE), RamAccounting.NO_ACCOUNTING);
+
+    private String columnName;
+    private IndexSearcher indexSearcher;
+    private final boolean[] reverseFlags = new boolean[]{true};
+    private final boolean[] nullsFirst = new boolean[]{true};
+    private Reference reference;
+    private OrderBy orderBy;
+    private CollectorContext collectorContext;
+    private ShardId dummyShardId;
+
+    @Setup
+    public void createLuceneBatchIterator() throws Exception {
+        try (IndexWriter iw = new IndexWriter(
+            new ByteBuffersDirectory(), new IndexWriterConfig(new StandardAnalyzer()))) {
+            dummyShardId = new ShardId("dummy", UUIDs.randomBase64UUID(), 1);
+            columnName = "x";
+            boolean noNulls = false;
+            System.out.println("No Nulls: " + noNulls);
+            for (int i = 0; i < 10_000_000; i++) {
+                Document doc = new Document();
+                if (noNulls) {
+                    doc.add(new FloatDocValuesField(columnName, i));
+                } else {
+                    if (i % 2 != 0) {
+                        doc.add(new FloatDocValuesField(columnName, i));
+                    }
+                }
+                iw.addDocument(doc);
+            }
+            iw.commit();
+            iw.forceMerge(1, true);
+            indexSearcher = new IndexSearcher(DirectoryReader.open(iw, true, true));
+            collectorContext = new CollectorContext(() -> null);
+            reference = new SimpleReference(
+                new RelationName(DocSchemaInfo.NAME, "dummyTable"),
+                ColumnIdent.of(columnName),
+                RowGranularity.DOC,
+                DataTypes.FLOAT,
+                1,
+                null
+            );
+            orderBy = new OrderBy(
+                Collections.singletonList(reference),
+                reverseFlags,
+                nullsFirst
+            );
+        }
+    }
+
+    @Benchmark
+    public void measureLoadAndConsumeOrderedLuceneBatchIterator(Blackhole blackhole) throws Exception {
+        BatchIterator<Row> it = OrderedLuceneBatchIteratorFactory.newInstance(
+            Collections.singletonList(createOrderedCollector(indexSearcher, columnName)),
+            OrderingByPosition.rowOrdering(List.of(DataTypes.FLOAT), new int[]{0}, reverseFlags, nullsFirst),
+            ROW_ACCOUNTING,
+            EsExecutors.directExecutor(),
+            () -> 1,
+            false
+        );
+        while (!it.allLoaded()) {
+            it.loadNextBatch().toCompletableFuture().join();
+        }
+        while (it.moveNext()) {
+            blackhole.consume(it.currentElement().get(0));
+        }
+    }
+
+    private LuceneOrderedDocCollector createOrderedCollector(IndexSearcher searcher,
+                                                             String sortByColumnName) {
+
+        System.out.println("Using nullAwareNumericSortField");
+        NullValueOrder nullValueOrder = nullsFirst[0] ? NullValueOrder.FIRST : NullValueOrder.LAST;
+        SortField sortField = nullAwareNumericSortField(sortByColumnName, reverseFlags[0], nullValueOrder, SortField.Type.FLOAT);
+        List<LuceneCollectorExpression<?>> expressions = Collections.singletonList(new OrderByCollectorExpression(reference, orderBy, o -> {
+            // if (o instanceof NullAwareNumber n) {
+            //     return n.isNull() ? null : (float) n.value();
+            // }
+            return o;
+        }));
+
+        // System.out.println("Using old SortedNumericSortField");
+        // SortField sortField = new SortedNumericSortField(sortByColumnName, SortField.Type.FLOAT, reverseFlags[0]);
+        // sortField.setMissingValue(NullSentinelValues.nullSentinelForScoreDoc(DataTypes.FLOAT, reverseFlags[0], nullsFirst[0]));
+        //
+        // List<LuceneCollectorExpression<?>> expressions = Collections.singletonList(new OrderByCollectorExpression(reference, orderBy, o -> o));
+        return new LuceneOrderedDocCollector(dummyShardId, searcher, MatchAllDocsQuery.INSTANCE, null, false, 10_000_000, RamAccounting.NO_ACCOUNTING, collectorContext, f -> null, new Sort(sortField), expressions, expressions);
+    }
+}
+
--- server/src/main/java/io/crate/expression/reference/doc/lucene/OrderByCollectorExpression.java
+++ server/src/main/java/io/crate/expression/reference/doc/lucene/OrderByCollectorExpression.java
@@ -26,6 +26,7 @@ import java.util.function.UnaryOperator;
 import org.apache.lucene.search.FieldDoc;
 
 import io.crate.analyze.OrderBy;
+import io.crate.execution.engine.sort.NullAwareNumber;
 import io.crate.metadata.Reference;
 
 /**
@@ -48,7 +49,14 @@ public class OrderByCollectorExpression extends LuceneCollectorExpression<Object
     }
 
     private void value(Object value) {
-        if (missingValue != null && missingValue.equals(value)) {
+        // NOTE: this currently knows internally how to interpret NullAwareNumber
+        // but callers of OrderByCollectorExpression usually pass in an anonymous function
+        // `valueConversion`. Should we change all callers or have it know internally?
+        if (value instanceof NullAwareNumber nullAwareNumber) {
+            this.value = nullAwareNumber.isNull()
+                ? null
+                : valueConversion.apply(nullAwareNumber.value());
+        } else if (missingValue != null && missingValue.equals(value)) {
             this.value = null;
         } else {
             this.value = valueConversion.apply(value);
