8f6c81418daba8fb42b506a18935f74b816dfc92
Mathias Fussenegger <f.mathias@zignar.net>
2026-09-22 15:11:37 +0200

    Remove unused XContent writers/transformers
    Those types aren't sent to clients via HTTP

--- server/src/main/java/io/crate/server/xcontent/ServerXContentExtension.java
+++ server/src/main/java/io/crate/server/xcontent/ServerXContentExtension.java
@@ -19,22 +19,6 @@
 
 package io.crate.server.xcontent;
 
-import java.time.DayOfWeek;
-import java.time.Duration;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.Month;
-import java.time.MonthDay;
-import java.time.OffsetDateTime;
-import java.time.OffsetTime;
-import java.time.Period;
-import java.time.Year;
-import java.time.ZoneOffset;
-import java.time.ZonedDateTime;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
@@ -42,20 +26,9 @@ import java.util.function.UnaryOperator;
 
 import org.apache.lucene.util.BytesRef;
 import org.elasticsearch.common.bytes.BytesReference;
-import org.elasticsearch.common.time.DateFormatter;
-import org.elasticsearch.common.time.DateFormatters;
 import org.elasticsearch.common.unit.ByteSizeValue;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.common.xcontent.XContentBuilderExtension;
-import org.joda.time.DateTime;
-import org.joda.time.DateTimeZone;
-import org.joda.time.Instant;
-import org.joda.time.MutableDateTime;
-import org.joda.time.ReadableInstant;
-import org.joda.time.format.DateTimeFormatter;
-import org.joda.time.format.ISODateTimeFormat;
-import org.joda.time.tz.CachedDateTimeZone;
-import org.joda.time.tz.FixedDateTimeZone;
 import org.locationtech.spatial4j.shape.Point;
 import org.locationtech.spatial4j.shape.impl.PointImpl;
 import org.locationtech.spatial4j.shape.jts.JtsPoint;
@@ -76,36 +49,13 @@ import io.crate.types.TimeTZ;
  */
 public class ServerXContentExtension implements XContentBuilderExtension {
 
-    public static final DateTimeFormatter DEFAULT_DATE_PRINTER = ISODateTimeFormat.dateTime().withZone(DateTimeZone.UTC);
-    public static final DateFormatter DEFAULT_FORMATTER = DateFormatters.forPattern("strict_date_optional_time_nanos");
-    public static final DateFormatter LOCAL_TIME_FORMATTER = DateFormatters.forPattern("HH:mm:ss.SSS");
-    public static final DateFormatter OFFSET_TIME_FORMATTER = DateFormatters.forPattern("HH:mm:ss.SSSZZZZZ");
-
     @Override
     public Map<Class<?>, XContentBuilder.Writer> getXContentWriters() {
         Map<Class<?>, XContentBuilder.Writer> writers = new HashMap<>();
 
         // Fully-qualified here to reduce ambiguity around our (ES') Version class
         writers.put(org.apache.lucene.util.Version.class, (b, v) -> b.value(Objects.toString(v)));
-        writers.put(DateTimeZone.class, (b, v) -> b.value(Objects.toString(v)));
-        writers.put(CachedDateTimeZone.class, (b, v) -> b.value(Objects.toString(v)));
-        writers.put(FixedDateTimeZone.class, (b, v) -> b.value(Objects.toString(v)));
-        writers.put(MutableDateTime.class, XContentBuilder::timeValue);
-        writers.put(DateTime.class, XContentBuilder::timeValue);
         writers.put(TimeValue.class, (b, v) -> b.value(v.toString()));
-        writers.put(ZonedDateTime.class, XContentBuilder::timeValue);
-        writers.put(OffsetDateTime.class, XContentBuilder::timeValue);
-        writers.put(OffsetTime.class, XContentBuilder::timeValue);
-        writers.put(java.time.Instant.class, XContentBuilder::timeValue);
-        writers.put(LocalDateTime.class, XContentBuilder::timeValue);
-        writers.put(LocalDate.class, XContentBuilder::timeValue);
-        writers.put(LocalTime.class, XContentBuilder::timeValue);
-        writers.put(DayOfWeek.class, (b, v) -> b.value(v.toString()));
-        writers.put(Month.class, (b, v) -> b.value(v.toString()));
-        writers.put(MonthDay.class, (b, v) -> b.value(v.toString()));
-        writers.put(Year.class, (b, v) -> b.value(v.toString()));
-        writers.put(Duration.class, (b, v) -> b.value(v.toString()));
-        writers.put(Period.class, (b, v) -> b.value(v.toString()));
         writers.put(org.joda.time.Period.class, (b, v) -> {
             org.joda.time.Period period = (org.joda.time.Period) v;
             b.value(IntervalType.PERIOD_FORMATTER.print(period));
@@ -167,31 +117,14 @@ public class ServerXContentExtension implements XContentBuilderExtension {
 
     @Override
     public Map<Class<?>, XContentBuilder.HumanReadableTransformer> getXContentHumanReadableTransformers() {
-        Map<Class<?>, XContentBuilder.HumanReadableTransformer> transformers = new HashMap<>();
-        transformers.put(TimeValue.class, v -> ((TimeValue) v).millis());
-        transformers.put(ByteSizeValue.class, v -> ((ByteSizeValue) v).getBytes());
-        return transformers;
+        return Map.of(
+            TimeValue.class, v -> ((TimeValue) v).millis(),
+            ByteSizeValue.class, v -> ((ByteSizeValue) v).getBytes()
+        );
     }
 
     @Override
     public Map<Class<?>, UnaryOperator<Object>> getDateTransformers() {
-        Map<Class<?>, UnaryOperator<Object>> transformers = new HashMap<>();
-        transformers.put(Date.class, d -> DEFAULT_DATE_PRINTER.print(((Date) d).getTime()));
-        transformers.put(DateTime.class, d -> DEFAULT_DATE_PRINTER.print((DateTime) d));
-        transformers.put(MutableDateTime.class, d -> DEFAULT_DATE_PRINTER.print((MutableDateTime) d));
-        transformers.put(ReadableInstant.class, d -> DEFAULT_DATE_PRINTER.print((ReadableInstant) d));
-        transformers.put(Long.class, d -> DEFAULT_DATE_PRINTER.print((long) d));
-        transformers.put(Calendar.class, d -> DEFAULT_DATE_PRINTER.print(((Calendar) d).getTimeInMillis()));
-        transformers.put(GregorianCalendar.class, d -> DEFAULT_DATE_PRINTER.print(((Calendar) d).getTimeInMillis()));
-        transformers.put(Instant.class, d -> DEFAULT_DATE_PRINTER.print((Instant) d));
-        transformers.put(ZonedDateTime.class, d -> DEFAULT_FORMATTER.format((ZonedDateTime) d));
-        transformers.put(OffsetDateTime.class, d -> DEFAULT_FORMATTER.format((OffsetDateTime) d));
-        transformers.put(OffsetTime.class, d -> OFFSET_TIME_FORMATTER.format((OffsetTime) d));
-        transformers.put(LocalDateTime.class, d -> DEFAULT_FORMATTER.format((LocalDateTime) d));
-        transformers.put(java.time.Instant.class,
-            d -> DEFAULT_FORMATTER.format(ZonedDateTime.ofInstant((java.time.Instant) d, ZoneOffset.UTC)));
-        transformers.put(LocalDate.class, d -> ((LocalDate) d).toString());
-        transformers.put(LocalTime.class, d -> LOCAL_TIME_FORMATTER.format((LocalTime) d));
-        return transformers;
+        return Map.of();
     }
 }
